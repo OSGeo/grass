aa6938adb0c3b4606a5be81104a47fc228d0d96a
Glynn Clements <glynn@gclements.plus.com>
2008-06-03 19:07:04 +0000

    Add skewness, kurtosis to lib/stats and r.series
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@31617 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ lib/stats/c_skew.c
@@ -0,0 +1,93 @@
+#include <math.h>
+#include <grass/gis.h>
+
+void c_skew(DCELL *result, DCELL *values, int n)
+{
+	DCELL sum, ave, sumsq, sumcb, sdev;
+	int count;
+	int i;
+
+	sum = 0.0;
+	count = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (G_is_d_null_value(&values[i]))
+			continue;
+
+		sum += values[i];
+		count++;
+	}
+
+	if (count == 0)
+	{
+		G_set_d_null_value(result, 1);
+		return;
+	}
+
+	ave = sum / count;
+
+	sumsq = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		DCELL d;
+
+		if (G_is_d_null_value(&values[i]))
+			continue;
+
+		d = values[i] - ave;
+		sumsq += d * d;
+		sumcb += d * d * d;
+	}
+
+        sdev = sqrt(sumsq / count);
+
+	*result = sumcb / (count * sdev * sdev * sdev);
+}
+
+void w_skew(DCELL *result, DCELL (*values)[2], int n)
+{
+	DCELL sum, ave, sumsq, sumcb, sdev;
+	int count;
+	int i;
+
+	sum = 0.0;
+	count = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		if (G_is_d_null_value(&values[i][0]))
+			continue;
+
+		sum += values[i][0] * values[i][1];
+		count += values[i][1];
+	}
+
+	if (count == 0)
+	{
+		G_set_d_null_value(result, 1);
+		return;
+	}
+
+	ave = sum / count;
+
+	sumsq = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		DCELL d;
+
+		if (G_is_d_null_value(&values[i][0]))
+			continue;
+
+		d = values[i][0] - ave;
+		sumsq += d * d * values[i][1];
+		sumcb += d * d * d * values[i][1];
+	}
+
+        sdev = sqrt(sumsq / count);
+
+	*result = sumcb / (count * sdev * sdev * sdev);
+}
+
