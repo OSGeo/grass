a3babbfcb3428acfb46399ad43f679f396018abd
Moritz Lennert <mlennert@club.worldonline.be>
2008-07-23 21:16:34 +0000

    added new statistics and new function for chosing algorithm
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@32234 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/arraystats.h
+++ include/arraystats.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <grass/gis.h>
 #include <grass/glocale.h>
 #include <grass/dbmi.h>
 
@@ -11,12 +12,14 @@ struct GASTATS
     double max;
     double sum;
     double sumsq;
+    double sumabs;
     double mean;
+    double meanabs;
     double var;
     double stdev;
 };
 
-
+double class_apply_algorithm(char *, double *, int, int *, double *);
 int class_interval(double *, int, int, double *);
 int class_quant(double *, int, int, double *);
 double class_discont(double *, int, int, double *);
