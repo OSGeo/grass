9333cfd18010649cfd1aa398ed29d299af7dd731
Glynn Clements <glynn@gclements.plus.com>
2008-07-31 21:03:16 +0000

    Fix sense of directory test
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@32431 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/grass_indent_ALL.sh
+++ tools/grass_indent_ALL.sh
@@ -4,7 +4,7 @@
 # loops over entire GRASS source code tree and indents source code according to SUBMITTING rules
 
 #are we in the tools/ dir? We should not.
-if [ -d tools ] ; then
+if [ ! -d tools ] ; then
  echo "ERROR: this script must be run from the main GRASS source code directory" >&2
  exit 1
 fi
