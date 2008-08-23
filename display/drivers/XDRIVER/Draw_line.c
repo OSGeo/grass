31bd31a224e4ec1a079b035059c463ab5e7b9209
Glynn Clements <glynn@gclements.plus.com>
2008-08-23 21:08:33 +0000

    Remove display/drivers
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@33037 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/drivers/XDRIVER/Draw_line.c
+++
@@ -1,8 +0,0 @@
-#include "includes.h"
-/* draw a line between two given points in the current color. X version */
-
-void XD_draw_line(int cur_x, int cur_y, int x, int y)
-{
-    XDrawLine(dpy, bkupmap, gc, cur_x, cur_y, x, y);
-    needs_flush = 1;
-}
--- display/drivers/XDRIVER/Draw_point.c
+++
@@ -1,8 +0,0 @@
-#include "includes.h"
-/* draw a point in the current color. X version */
-
-void XD_draw_point(int x, int y)
-{
-    XDrawPoint(dpy, bkupmap, gc, x, y);
-    needs_flush = 1;
-}
