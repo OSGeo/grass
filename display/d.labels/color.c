a8693c9731c18e2ee43f1fff80a57c33a34b4068
Glynn Clements <glynn@gclements.plus.com>
2008-08-27 20:07:18 +0000

    Fix warning (include correct header)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@33126 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.labels/color.c
+++ display/d.labels/color.c
@@ -16,7 +16,7 @@
 /**  ??? move these into libgis ??? (libraster for set_color_from_RGBA()) **/
 
 #include <grass/gis.h>
-#include <grass/raster.h>
+#include <grass/display.h>
 #include <grass/glocale.h>
 
 /* fill RGBA array from RGB components (0-255) */
