8320ea2907880f68c4359412c880db48372704bf
Glynn Clements <glynn@gclements.plus.com>
2008-09-27 23:18:20 +0000

    Remove erroneous svn:executable properties Add svn:ignore properties Convert to ANSI C (C89) Use strcmp instead of strcoll Read data as CELL (using map's quant rules) rather than using C typecast Add ->options to qcname= Fix formatting Miscellaneous cleanup
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@33574 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.albedo/main.c
+++ imagery/i.albedo/main.c
@@ -34,67 +34,51 @@ double bb_alb_modis(double redchan, double nirchan, double chan3,
 int main(int argc, char *argv[])
 {
     struct Cell_head cellhd;	/*region+header info */
-
-    char *mapset;		/*mapset name */
-
     int nrows, ncols;
-
     int row, col;
-
     struct GModule *module;
-
     struct Option *input, *output;
-
     struct Flag *flag1, *flag2, *flag3;
-
     struct Flag *flag4, *flag5, *flag6;
-
     struct History history;	/*metadata */
-
     struct Colors colors;	/*Color rules */
 
-	/************************************/
+    /************************************/
     /* FMEO Declarations**************** */
     char *name;			/*input raster name */
-
     char *result;		/*output raster name */
-
     /*File Descriptors */
     int nfiles;
-
     int infd[MAXFILES];
-
     int outfd;
-
     char **names;
-
     char **ptr;
-
-    int ok;
-
     int i = 0, j = 0;
-
     int modis = 0, aster = 0, avhrr = 0, landsat = 0;
-
     void *inrast[MAXFILES];
-
     unsigned char *outrast;
-
     int data_format;		/* 0=double  1=float  2=32bit signed int  5=8bit unsigned int (ie text) */
-
     RASTER_MAP_TYPE in_data_type[MAXFILES];	/* 0=numbers  1=text */
-
     RASTER_MAP_TYPE out_data_type = DCELL_TYPE;
 
-	/************************************/
+    /************************************/
+    int peak1, peak2, peak3;
+    int i_peak1, i_peak2, i_peak3;
 
-	/************************************/
+    int bottom1a, bottom1b;
+    int bottom2a, bottom2b;
+    int bottom3a, bottom3b;
+    int i_bottom1a, i_bottom1b;
+    int i_bottom2a, i_bottom2b;
+    int i_bottom3a, i_bottom3b;
+
+    /************************************/
     int histogram[100];
 
     /* Albedo correction coefficients*** */
     double a, b;
 
-	/************************************/
+    /************************************/
 
     G_gisinit(argv[0]);
 
@@ -115,39 +99,37 @@ int main(int argc, char *argv[])
     /* Define the different flags */
 
     flag1 = G_define_flag();
-    flag1->key = _('m');
+    flag1->key = 'm';
     flag1->description = _("Modis (7 input bands:1,2,3,4,5,6,7)");
 
     flag2 = G_define_flag();
-    flag2->key = _('n');
+    flag2->key = 'n';
     flag2->description = _("NOAA AVHRR (2 input bands:1,2)");
 
     flag3 = G_define_flag();
-    flag3->key = _('l');
+    flag3->key = 'l';
     flag3->description = _("Landsat (6 input bands:1,2,3,4,5,7)");
 
     flag4 = G_define_flag();
-    flag4->key = _('a');
+    flag4->key = 'a';
     flag4->description = _("Aster (6 input bands:1,3,5,6,8,9)");
 
     flag5 = G_define_flag();
-    flag5->key = _('c');
+    flag5->key = 'c';
     flag5->description =
 	_("Albedo dry run to calculate some water to beach/sand/desert stretching, a kind of simple atmospheric correction. Agressive mode (Landsat).");
 
     flag6 = G_define_flag();
-    flag6->key = _('d');
+    flag6->key = 'd';
     flag6->description =
 	_("Albedo dry run to calculate some water to beach/sand/desert stretching, a kind of simple atmospheric correction. Soft mode (Modis).");
 
     /* FMEO init nfiles */
     nfiles = 1;
 
-	/********************/
     if (G_parser(argc, argv))
 	exit(-1);
 
-    ok = 1;
     names = input->answers;
     ptr = input->answers;
 
@@ -158,80 +140,60 @@ int main(int argc, char *argv[])
     landsat = (flag3->answer);
     aster = (flag4->answer);
 
-	/***************************************************/
-    if (G_legal_filename(result) < 0) {
+    if (G_legal_filename(result) < 0)
 	G_fatal_error(_("[%s] is an illegal name"), result);
-    }
 
-	/***************************************************/
-
-	/***************************************************/
     for (; *ptr != NULL; ptr++) {
 	if (nfiles >= MAXFILES)
-	    G_fatal_error(_("%s - too many ETa files. Only %d allowed"),
-			  G_program_name(), MAXFILES);
+	    G_fatal_error(_("too many ETa files. Only %d allowed"), MAXFILES);
 	name = *ptr;
-	/* find map in mapset */
-	mapset = G_find_cell2(name, "");
-	if (mapset == NULL) {
-	    G_fatal_error(_("cell file [%s] not found"), name);
-	    ok = 0;
-	}
-	if (!ok) {
+
+	infd[nfiles] = G_open_cell_old(name, "");
+	if (infd[nfiles] < 0)
 	    continue;
-	}
-	infd[nfiles] = G_open_cell_old(name, mapset);
-	if (infd[nfiles] < 0) {
-	    ok = 0;
-	    continue;
-	}
+
 	/* Allocate input buffer */
-	in_data_type[nfiles] = G_raster_map_type(name, mapset);
-	if ((infd[nfiles] = G_open_cell_old(name, mapset)) < 0) {
+	in_data_type[nfiles] = G_raster_map_type(name, "");
+	if ((infd[nfiles] = G_open_cell_old(name, "")) < 0)
 	    G_fatal_error(_("Cannot open cell file [%s]"), name);
-	}
-	if ((G_get_cellhd(name, mapset, &cellhd)) < 0) {
+
+	if ((G_get_cellhd(name, "", &cellhd)) < 0)
 	    G_fatal_error(_("Cannot read file header of [%s]"), name);
-	}
+
 	inrast[nfiles] = G_allocate_raster_buf(in_data_type[nfiles]);
 	nfiles++;
     }
     nfiles--;
-    if (nfiles <= 1) {
+    if (nfiles <= 1)
 	G_fatal_error(_("The min specified input map is two (that is NOAA AVHRR)"));
-    }
 
-	/***************************************************/
     /* Allocate output buffer, use input map data_type */
     nrows = G_window_rows();
     ncols = G_window_cols();
     outrast = G_allocate_raster_buf(out_data_type);
 
     /* Create New raster files */
-    if ((outfd = G_open_raster_new(result, 1)) < 0) {
+    if ((outfd = G_open_raster_new(result, 1)) < 0)
 	G_fatal_error(_("Could not open <%s>"), result);
-    }
+
     /*START ALBEDO HISTOGRAM STRETCH */
     /*This is correcting contrast for water/sand */
     /*A poor man's atmospheric correction... */
-    for (i = 0; i < 100; i++) {
+    for (i = 0; i < 100; i++)
 	histogram[i] = 0;
-    }
+
     if (flag5->answer || flag6->answer) {
 	DCELL de;
-
 	DCELL d[MAXFILES];
 
-		/****************************/
 	/* Process pixels histogram */
 	for (row = 0; row < nrows; row++) {
 	    G_percent(row, nrows, 2);
 	    /* read input map */
 	    for (i = 1; i <= nfiles; i++) {
-		if ((G_get_raster_row
-		     (infd[i], inrast[i], row, in_data_type[i])) < 0) {
+		if ((G_get_raster_row(
+			 infd[i], inrast[i], row, in_data_type[i])) < 0)
 		    G_fatal_error(_("Could not read from <%s>"), name);
-		}
 	    }
 	    /*process the data */
 	    for (col = 0; col < ncols; col++) {
@@ -275,9 +237,6 @@ int main(int argc, char *argv[])
 	    }
 	}
 	G_message("Histogram of Albedo\n");
-	int peak1, peak2, peak3;
-
-	int i_peak1, i_peak2, i_peak3;
 
 	peak1 = 0;
 	peak2 = 0;
@@ -308,17 +267,6 @@ int main(int argc, char *argv[])
 		}
 	    }
 	}
-	int bottom1a, bottom1b;
-
-	int bottom2a, bottom2b;
-
-	int bottom3a, bottom3b;
-
-	int i_bottom1a, i_bottom1b;
-
-	int i_bottom2a, i_bottom2b;
-
-	int i_bottom3a, i_bottom3b;
 
 	bottom1a = 100000;
 	bottom1b = 100000;
@@ -391,20 +339,17 @@ int main(int argc, char *argv[])
     }				/*END OF FLAG1 */
     /* End of processing histogram */
 
-	/*******************/
     /* Process pixels */
     for (row = 0; row < nrows; row++) {
 	DCELL de;
-
 	DCELL d[MAXFILES];
 
 	G_percent(row, nrows, 2);
 	/* read input map */
 	for (i = 1; i <= nfiles; i++) {
-	    if ((G_get_raster_row(infd[i], inrast[i], row, in_data_type[i])) <
-		0) {
+	    if ((G_get_raster_row(
+		     infd[i], inrast[i], row, in_data_type[i])) < 0)
 		G_fatal_error(_("Could not read from <%s>"), name);
-	    }
 	}
 	/*process the data */
 	for (col = 0; col < ncols; col++) {
