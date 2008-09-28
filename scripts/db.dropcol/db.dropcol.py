4e31dea5fd17a410b86a6fdaaf6c66d2ba4dc561
Glynn Clements <glynn@gclements.plus.com>
2008-09-28 21:55:11 +0000

    Convert db.dropcol, v.db.dropcol, v.db.droptable, r.regression.line to Python
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@33585 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ scripts/db.dropcol/db.dropcol.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+############################################################################
+#
+# MODULE:       db.dropcolumn
+# AUTHOR(S):   	Markus Neteler
+#               Converted to Python by Glynn Clements
+# PURPOSE:      interface to db.execute to drop a column from an 
+#               attribute table
+#               - with special trick for SQLite
+# COPYRIGHT:    (C) 2007 by Markus Neteler and the GRASS Development Team
+#
+#               This program is free software under the GNU General Public
+#               License (>=v2). Read the file COPYING that comes with GRASS
+#               for details.
+#
+#############################################################################
+
+
+#%Module
+#%  description: Drops a column from selected attribute table
+#%  keywords: database, attribute table
+#%End
+
+#%flag
+#%  key: f
+#%  description: Force removal (required for actual deletion of files)
+#%end
+
+#%option
+#% key: table
+#% type: string
+#% key_desc : name
+#% description: Table from which to drop attribute column
+#% required : yes
+#% gisprompt: old,dbtable,dbtable
+#%end
+
+#%option
+#% key: column
+#% type: string
+#% description: Name of the column
+#% required : yes
+#% gisprompt: old,dbcolumn,dbcolumn
+#%end
+
+import sys
+import os
+import string
+import grass
+
+def main():
+    table = options['table']
+    column = options['column']
+    force = flags['f']
+
+    # check if DB parameters are set, and if not set them.
+    grass.run_command('db.connect', flags = 'c')
+
+    s = grass.read_command('db.connect', flags = 'p')
+    kv = grass.parse_key_val(s, sep = ':')
+    database = kv['database']
+    driver = kv['driver']
+    # schema needed for PG?
+
+    if force:
+	grass.message("Forcing ...")
+
+    if column == "cat":
+	grass.warning("Deleting <%s> column which may be needed to keep table connected to a vector map" % column)
+
+    s = grass.read_command('db.describe', flags = 'c', table = table)
+    cols = [l.split(':')[1].lstrip() for l in s.splitlines() if l.startswith('Column ')]
+    if column not in cols:
+	grass.fatal("Column <%s> not found in table" % column)
+
+    if not force:
+	grass.message("Column <%s> would be deleted." % column)
+	grass.message("")
+	grass.message("You must use the force flag to actually remove it. Exiting.")
+	sys.exit(0)
+
+    if driver == "sqlite":
+	#echo "Using special trick for SQLite"
+	# http://www.sqlite.org/faq.html#q13
+	colnames = []
+	coldef = []
+	s = grass.read_command('db.describe', flags = 'c', table = table)
+	for l in s.splitlines():
+	    if not l.startswith('Column '):
+		continue
+	    f = l.split(':')
+	    if f[1] == column:
+		continue
+	    colnames.append(f[1])
+	    coltypes.append("%s %s" % (f[1], f[2]))
+
+	colnames = ", ".join(colnames)
+	coltypes = ", ".join(coltypes)
+
+	cmds = [
+	    "BEGIN TRANSACTION",
+	    "CREATE TEMPORARY TABLE ${table}_backup(${coldef})",
+	    "INSERT INTO ${table}_backup SELECT ${colnames} FROM ${table}",
+	    "DROP TABLE ${table}",
+	    "CREATE TABLE ${table}(${coldef})",
+	    "INSERT INTO ${table} SELECT ${colnames} FROM ${table}_backup",
+	    "DROP TABLE ${table}_backup",
+	    "COMMIT"
+	    ]
+	tmpl = string.Template(';\n'.join(cmds))
+	sql = tmpl.substitute(table = table, coldef = coltypes, colnames = colnames)
+    else:
+	sql = "ALTER TABLE %s DROP COLUMN %s" % (table, column)
+
+    if grass.write_command('db.execute', database = database, driver = driver,
+			   stdin = sql) != 0:
+	grass.fatal("Cannot continue (problem deleting column).")
+
+if __name__ == "__main__":
+    options, flags = grass.parser()
+    main()
---
+++ scripts/v.db.droptable/v.db.droptable.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+#
+############################################################################
+#
+# MODULE:       v.db.droptable
+# AUTHOR(S):   	Markus Neteler 
+#               Converted to Python by Glynn Clements
+# PURPOSE:      interface to db.execute to drop an existing table of given vector map
+# COPYRIGHT:    (C) 2005, 2008 by the GRASS Development Team
+#
+#               This program is free software under the GNU General Public
+#               License (>=v2). Read the file COPYING that comes with GRASS
+#               for details.
+#
+#############################################################################
+
+
+#%Module
+#%  description: Removes existing attribute table of a vector map.
+#%  keywords: vector, database, attribute table
+#%End
+#%flag
+#%  key: f
+#%  description: Force removal (required for actual deletion of table)
+#%end
+#%option
+#% key: map
+#% type: string
+#% gisprompt: old,vector,vector
+#% description: Vector map from which to remove attribute table
+#% required : yes
+#%end
+#%option
+#% key: table
+#% type: string
+#% description: Name of existing attribute table (default: vector map name)
+#% required : no
+#%end
+#%option
+#% key: layer
+#% type: integer
+#% description: Layer from which to drop linked attribute table
+#% answer: 1
+#% required : no
+#%end
+
+import sys
+import os
+import grass
+
+def main():
+    force = flags['f']
+    map = options['map']
+    table = options['table']
+    layer = options['layer']
+
+    # do some paranoia tests as well:
+    s = grass.read_command('v.db.connect', flags = 'g', map = map, layer = layer);
+    if not s:
+	grass.fatal("An error occured while running v.db.connect")
+    f = s.split()
+    if not table:
+	# Removing table name connected to selected layer
+	table = f[1]
+	if not table:
+	    grass.fatal("No table assigned to layer <$GIS_OPT_LAYER>")
+    else:
+	# Removing user specified table
+	existingtable = f[1]
+	if existingtable != table:
+	    grass.fatal("User selected table <%s> but the table <%s> is linked to layer <%s>"
+			% (table, existingtable, layer))
+
+    # we use the DB settings selected layer 
+    database = f[3]
+    driver = f[4]
+
+    grass.message("Removing table <%s> linked to layer <%s> of vector map <%s>"
+		  % (table, layer, map)) 
+
+    if not force:
+	grass.message("You must use the -f (force) flag to actually remove the table. Exiting.")
+	grass.message("Leaving map/table unchanged.")
+	sys.exit(0)
+
+    grass.message("Dropping table <%s>..." % table)
+
+    if grass.write_command('db.execute', stdin = "DROP TABLE %s" % table) != 0:
+	grass.fatal("An error occured while running db.execute")
+
+    grass.run_command('v.db.connect', flags = 'd', map = map, layer = layer)
+
+    grass.message("Current attribute table link(s):") 
+    # silently test first to avoid confusing error messages
+    nuldev = file(os.devnull, 'w')
+    if grass.run_command('v.db.connect', flags ='p', map = map, quiet = True,
+			 stdout = nuldev, stderr = nuldev) != 0:
+	grass.message("(No database links remaining)")
+    else:
+	grass.run_command('v.db.connect', flags ='p', map = map)
+
+    # write cmd history:
+    grass.run_command('v.support', map = map, cmdhist = os.environ['CMDLINE'])
+
+if __name__ == "__main__":
+    options, flags = grass.parser()
+    main()
