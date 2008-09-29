411f25750a30dfbce1d86ec583237bdd87095d30
Glynn Clements <glynn@gclements.plus.com>
2008-09-29 23:48:38 +0000

    Fix typo (find_command -> find_program)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@33603 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/r.in.aster/r.in.aster.py
+++ scripts/r.in.aster/r.in.aster.py
@@ -108,7 +108,7 @@ def main():
     band = options['band']
 
     #check whether gdalwarp is in path and executable
-    if not grass.find_command('gdalwarp', ['--version']):
+    if not grass.find_program('gdalwarp', ['--version']):
         grass.fatal("gdalwarp is not in the path and executable")
 
     #create temporary file to hold gdalwarp output before importing to GRASS
