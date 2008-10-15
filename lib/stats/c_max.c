490975fa99a4e3b449ad13fcacadf5a1ab4a6cd3
Glynn Clements <glynn@gclements.plus.com>
2008-10-15 15:32:42 +0000

    Add "weighted" versions of min, max aggregates  (the actual weights are ignored; this just provides the stat_func_w  interface for compatibility) Change r.resamp.stats to use w_{min,max}.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@33888 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/stats/c_max.c
+++ lib/stats/c_max.c
@@ -20,3 +20,24 @@ void c_max(DCELL * result, DCELL * values, int n)
     else
 	*result = max;
 }
+
+void w_max(DCELL * result, DCELL(*values)[2], int n)
+{
+    DCELL max;
+    int i;
+
+    G_set_d_null_value(&max, 1);
+
+    for (i = 0; i < n; i++) {
+	if (G_is_d_null_value(&values[i][0]))
+	    continue;
+
+	if (G_is_d_null_value(&max) || max < values[i][0])
+	    max = values[i][0];
+    }
+
+    if (G_is_d_null_value(&max))
+	G_set_d_null_value(result, 1);
+    else
+	*result = max;
+}
--- lib/stats/c_min.c
+++ lib/stats/c_min.c
@@ -20,3 +20,24 @@ void c_min(DCELL * result, DCELL * values, int n)
     else
 	*result = min;
 }
+
+void w_min(DCELL * result, DCELL(*values)[2], int n)
+{
+    DCELL min;
+    int i;
+
+    G_set_d_null_value(&min, 1);
+
+    for (i = 0; i < n; i++) {
+	if (G_is_d_null_value(&values[i][0]))
+	    continue;
+
+	if (G_is_d_null_value(&min) || min > values[i][0])
+	    min = values[i][0];
+    }
+
+    if (G_is_d_null_value(&min))
+	G_set_d_null_value(result, 1);
+    else
+	*result = min;
+}
