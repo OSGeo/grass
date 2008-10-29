89577418d3e5addca0cc9a2ad79e0a975bcf3537
Glynn Clements <glynn@gclements.plus.com>
2008-10-29 20:52:40 +0000

    Add D_fit_u_to_d()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@34077 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/display.h
+++ include/display.h
@@ -7,6 +7,7 @@
 /* cnversions.c */
 void D_update_conversions(void);
 void D_fit_d_to_u(void);
+void D_fit_u_to_d(void);
 void D_show_conversions(void);
 
 void D_do_conversions(const struct Cell_head *, double, double, double, double);
