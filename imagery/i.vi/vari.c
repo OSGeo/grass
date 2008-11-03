2c8b15bdff853f8b8feaf4556611af523bf8992c
Markus Neteler <neteler@gmail.com>
2008-11-03 05:30:27 +0000

    stub added to get code compiled
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@34149 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ imagery/i.vi/vari.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+    /*VARI: ? */ 
+double va_ri(double redchan, double nirchan, double bluechan,
+	      double greenchan) 
+{
+    double result;
+
+     {
+
+    G_fatal_error("Not yet implemented");
+
+    } return result;
+}
+
+
