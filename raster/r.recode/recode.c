27bab8a6bd833517a7b4da3cc0bb38871dd95d67
Glynn Clements <glynn@gclements.plus.com>
2008-11-20 21:26:40 +0000

    Revert https://trac.osgeo.org/grass/changeset/34367
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@34413 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.recode/recode.c
+++ raster/r.recode/recode.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-#include <grass/glocale.h>
-
 #include "global.h"
 
 #define F2I(map_type) \
@@ -45,8 +42,7 @@ int do_recode(void)
     /* open the input file for reading */
     in_fd = G_open_cell_old(name, "");
     if (in_fd < 0)
-	G_fatal_error(_("Unable to open raster map <%s>"),
-		      G_fully_qualified_name(name, mapset));
+	G_fatal_error("Can't open input map");
 
     out_fd = G_open_raster_new(result, out_type);
 
