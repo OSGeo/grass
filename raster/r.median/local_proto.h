5dce454904e279658a6094e6b88cbed4dc0d1ff9
Glynn Clements <glynn@gclements.plus.com>
2008-11-20 19:11:28 +0000

    Remove r.sum, r.average, r.median - redundant due to r.statistics{,2,3}
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@34408 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.median/local_proto.h
+++
@@ -1,8 +0,0 @@
-/* median.c */
-long median(struct stats *);
-
-/* read_stats.c */
-int read_stats(FILE *, long *, long *, double *);
-
-/* write_rec.c */
-int write_reclass(FILE *, long, long, char *);
