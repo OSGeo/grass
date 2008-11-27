8270756484c4a2b04f6ed6c0d62462e403b3fdf9
Glynn Clements <glynn@gclements.plus.com>
2008-11-27 18:51:44 +0000

    Improve adherence to consistent directory structure
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@34542 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ db/db.select/local_proto.h
@@ -0,0 +1,8 @@
+#ifndef __LOCAL_PROTO_H__
+#define __LOCAL_PROTO_H__
+
+int print_priv(char *, int);
+int print_column_definition(dbColumn *);
+int print_table_definition(dbDriver *, dbTable *);
+
+#endif /* __LOCAL_PROTO_H__ */
