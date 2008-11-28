d4206b1e964d391c9ca098bb160c2a20c0e9ccc9
Glynn Clements <glynn@gclements.plus.com>
2008-11-28 00:48:28 +0000

    Version control: have you heard of it?
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@34579 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.watershed2/ram/do_astar.h
+++
@@ -1,7 +0,0 @@
-#ifndef __DO_ASTAR_H__
-#define __DO_ASTAR__
-
-#define GET_PARENT(c) (int) (((c) - 2) / 3 + 1)
-#define GET_CHILD(p) (int) (((p) * 3) - 1)
-
-#endif /* __DO_ASTAR_H__ */
--- raster/r.watershed2/ram/flag_set.c
+++
@@ -1,8 +0,0 @@
-#include "flag.h"
-
-int flag_set(FLAG * flags, int row, int col)
-{
-    flags->array[row][col >> 3] |= (1 << (col & 7));
-
-    return 0;
-}
