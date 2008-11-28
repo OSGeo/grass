156d2c992f97cfe67c7fcf8614f01fb8f3c8a748
Glynn Clements <glynn@gclements.plus.com>
2008-11-28 23:00:34 +0000

    Add -npro switch (don't read .indent.pro files)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@34606 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/grass_indent.sh
+++ tools/grass_indent.sh
@@ -6,7 +6,7 @@ if [ $# -lt 1 ] ; then
  echo "No files specified (give file name(s) as parameter)"
  exit 1
 else
- indent -bad -bap -bbb -br -bli0 -bls -cli0 -ncs -fc1 -hnl -i4 \
+ indent -npro -bad -bap -bbb -br -bli0 -bls -cli0 -ncs -fc1 -hnl -i4 \
       -nbbo -nbc -nbfda -nbfde -ncdb -ncdw -nce -nfca -npcs -nprs \
       -npsl -nsc -nsob -saf -sai -saw -sbi0 -ss -ts8 -ut "$@"
 
