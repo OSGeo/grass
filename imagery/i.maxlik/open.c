1ba97ff9e5de4f21c6a060280c29c72c55258d42
Martin Landa <landa.martin@gmail.com>
2008-12-08 12:32:03 +0000

    i.maxlik: message standardization 	(merge from devbr6, r34799)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@34800 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.maxlik/open.c
+++ imagery/i.maxlik/open.c
@@ -13,21 +13,21 @@ int open_files(void)
 
     I_init_group_ref(&Ref);
     if (!I_find_group(group))
-	G_fatal_error(_("group=[%s] - not found."), group);
+	G_fatal_error(_("Group <%s> not found"), group);
 
     if (!I_find_subgroup(group, subgroup))
-	G_fatal_error(_("subgroup=[%s] (of group [%s]) - not found."),
+	G_fatal_error(_("Subgroup <%s> in group <%s> not found"),
 		      subgroup, group);
 
     I_get_subgroup_ref(group, subgroup, &Ref);
 
     if (Ref.nfiles <= 1) {
 	if (Ref.nfiles <= 0)
-	    G_fatal_error(_("Subgroup [%s] of group [%s] doesn't have any files.\n"
-			   "The subgroup must have at least 2 files."));
+	    G_fatal_error(_("Subgroup <%s> of group <%s> doesn't have any raster maps. "
+			    "The subgroup must have at least 2 raster maps."));
 	else
-	    G_fatal_error(_("Subgroup [%s] of group [%s] only has 1 file.\n"
-			    "The subgroup must have at least 2 files."));
+	    G_fatal_error(_("Subgroup <%s> of group <%s> only has 1 raster map. "
+			    "The subgroup must have at least 2 raster maps."));
     }
 
     cell = (DCELL **) G_malloc(Ref.nfiles * sizeof(DCELL *));
@@ -38,21 +38,25 @@ int open_files(void)
 	name = Ref.file[n].name;
 	mapset = Ref.file[n].mapset;
 	if ((cellfd[n] = G_open_cell_old(name, mapset)) < 0)
-	    exit(EXIT_FAILURE);
+	    G_fatal_error(_("Unable to open raster map <%s>"),
+			  G_fully_qualified_name(name, mapset));
     }
 
     I_init_signatures(&S, Ref.nfiles);
     fd = I_fopen_signature_file_old(group, subgroup, sigfile);
     if (fd == NULL)
-	exit(EXIT_FAILURE);
+	G_fatal_error(_("Unable to open signature file <%s>"),
+		      sigfile);
 
     n = I_read_signatures(fd, &S);
     fclose(fd);
     if (n < 0)
-	G_fatal_error(_("Can't read signature file [%s]."), sigfile);
+	G_fatal_error(_("Unable to read signature file <%s>"),
+		      sigfile);
 
     if (S.nsigs > 255)
-	G_fatal_error(_("[%s] has more than 255 signatures."), sigfile);
+	G_fatal_error(_("<%s> has too many signatures (limit is 255)"),
+		      sigfile);
 
     B = (double *)G_malloc(S.nsigs * sizeof(double));
     invert_signatures();
@@ -67,7 +71,7 @@ int open_files(void)
     if (reject_name) {
 	reject_fd = G_open_cell_new(reject_name);
 	if (reject_fd < 0)
-	    G_fatal_error(_("Unable to create reject layer [%s]."),
+	    G_fatal_error(_("Unable to create raster map <%s>"),
 			  reject_name);
 	else
 	    reject_cell = G_allocate_cell_buf();
