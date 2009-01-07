ff5ef1b503665b4d2b8c3fd571538b3c7c001af3
Glynn Clements <glynn@gclements.plus.com>
2009-01-07 19:23:21 +0000

    Remove prototypes for non-existent functions
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@35271 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/display.h
+++ include/display.h
@@ -145,8 +145,6 @@ void D_symbol2(const SYMBOL *, double, double, const RGBA_Color *,
 
 /* tran_colr.c */
 int D_translate_color(const char *);
-int D_translate_or_add_color(const char *, int);
-int D_allocate_color(void);
 int D_parse_color(const char *, int);
 int D_use_color(int);
 int D_color_number_to_RGB(int, int *, int *, int *);
