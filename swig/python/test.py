ba5059c3172f07113b2d9e7b500846f625232620
Glynn Clements <glynn@gclements.plus.com>
2009-01-16 09:54:25 +0000

    Retain local copies of modules Fix test script
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@35429 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- swig/python/test.py
+++ swig/python/test.py
@@ -5,7 +5,7 @@
 
 
 import os, sys
-import python_grass7 as g7lib
+import grass
 
 if not os.environ.has_key("GISBASE"):
     print "You must be in GRASS GIS to run this program."
@@ -14,16 +14,16 @@ if not os.environ.has_key("GISBASE"):
 rname = 'elevation.dem'
 mapset = 'PERMANENT'
 
-g7lib.G_gisinit('')
-g7lib.G_find_cell2(rname,'')
+grass.G_gisinit('')
+grass.G_find_cell2(rname,'')
 
 print mapset
 
 print 'prints 0 if map was found'
 
 print 'roads:'
-print g7lib.G_raster_map_type('roads',mapset)
+print grass.G_raster_map_type('roads',mapset)
 
 print 'elevation.dem:'
-print g7lib.G_raster_map_type(rname,mapset)
+print grass.G_raster_map_type(rname,mapset)
 
