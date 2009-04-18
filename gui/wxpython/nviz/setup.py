6d64bee85e051cfa07a9735ffd642a4914c12051
Martin Landa <landa.martin@gmail.com>
2009-04-18 21:08:29 +0000

    Build wxGUI Python extension using distutils
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@36782 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/nviz/setup.py
+++ gui/wxpython/nviz/setup.py
@@ -1,24 +1,56 @@
-# currently used only for osgeo4w
-# TODO: use instead of Makefile
+#!/usr/bin/env python
+ 
+# Setup script for wxGUI vdigit extension.
+
+import os
+import sys
+
+sys.path.append('..')
+from build_ext import variables
+from build_ext import update_opts
+
 from distutils.core import setup, Extension
 
+macros = [('PACKAGE', '"grasslibs"')]
+inc_dirs = [os.path.join(variables['GRASS_HOME'],
+                         'dist.' + variables['ARCH'],
+                         'include')]
+lib_dirs = [os.path.join(variables['GRASS_HOME'],
+                         'dist.' + variables['ARCH'],
+                         'lib')]
+
+
+libs = ['grass_gis',
+        'grass_nviz',
+        'grass_ogsf',
+        'grass_g3d']
+
+for flag in ('GDALCFLAGS',
+             'GDALLIBS',
+             'WXWIDGETSCXXFLAGS',
+             'WXWIDGETSLIB'):
+    update_opts(flag, macros, inc_dirs, lib_dirs, libs)
+
 setup(
-	ext_modules= [
-		Extension(
-			'_grass7_wxnviz',
-			sources=[
-				"grass7_wxnviz.i",
-				"change_view.cpp",
-				"draw.cpp",
-				"init.cpp",
-				"lights.cpp",
-				"load.cpp",
-				"surface.cpp",
-				"vector.cpp",
-				"volume.cpp",
-				],
-			swig_opts=['-c++','-shadow'],
-			libraries=['grass_gis','grass_nviz','grass_ogsf','grass_g3d']
-		)
+    ext_modules= [
+        Extension(
+            name = '_grass7_wxnviz',
+            sources=["change_view.cpp",
+                     "draw.cpp",
+                     "init.cpp",
+                     "lights.cpp",
+                     "load.cpp",
+                     "surface.cpp",
+                     "vector.cpp",
+                     "volume.cpp",
+                     "grass7_wxnviz.i"],
+            swig_opts = ['-c++',
+                         '-shadow'],
+            define_macros = macros,
+            include_dirs = inc_dirs,
+            library_dirs = lib_dirs,
+            libraries = libs,
+            )
 	]
-)
+    )
+
