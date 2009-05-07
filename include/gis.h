cabe3814ec13a48352a8c7783dabea556d4db8d7
Martin Landa <landa.martin@gmail.com>
2009-05-07 15:31:36 +0000

    GIS_H_DATE added
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@37043 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/gis.h
+++ include/gis.h
@@ -39,6 +39,7 @@ static const char *GRASS_copyright __attribute__ ((unused))
     = "GRASS GNU GPL licensed Software";
 
 #define GIS_H_VERSION "$Revision$"
+#define GIS_H_DATE    "$Date$"
 
 #define G_gisinit(pgm) G__gisinit(GIS_H_VERSION, (pgm))
 #define G_no_gisinit() G__no_gisinit(GIS_H_VERSION)
