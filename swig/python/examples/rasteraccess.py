c91ed6b1996c1225ed5d8fc73ce5b53794a5d75a
Martin Landa <landa.martin@gmail.com>
2009-05-22 09:46:15 +0000

    quick fix, https://trac.osgeo.org/grass/changeset/37353
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@37355 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- swig/python/examples/rasteraccess.py
+++ swig/python/examples/rasteraccess.py
@@ -1,15 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-# run within GRASS Spearfish session
-# run this before starting python to append module search path:
-#   export PYTHONPATH=/usr/src/grass70/swig/python
-#   check with "import sys; sys.path"
-# or:
-#   sys.path.append("/usr/src/grass70/swig/python")
-# FIXME: install the grass bindings in $GISBASE/lib/ ?
+"""
+Run within GRASS session
+Run this before starting python to append module search path:
+
+@code
+export PYTHONPATH=/usr/src/grass70/swig/python
+@endcode
+
+Check with "import sys; sys.path"
+or:
+
+@code
+sys.path.append("/usr/src/grass70/swig/python")
+@endcode
+
+\todo install the grass bindings in $GISBASE/lib/ ?
+"""
 
 import os, sys
-import grass
+from grass.lib import grass
 
 if not os.environ.has_key("GISBASE"):
     print "You must be in GRASS GIS to run this program."
@@ -24,7 +34,7 @@ else:
 grass.G_gisinit('')
 
 # find map in search path
-mapset = grass.G_find_cell2(input,'')
+mapset = grass.G_find_cell2(input, '')
 
 # determine the inputmap type (CELL/FCELL/DCELL) */
 data_type = grass.G_raster_map_type(input, mapset)
@@ -32,12 +42,13 @@ data_type = grass.G_raster_map_type(input, mapset)
 infd = grass.G_open_cell_old(input, mapset)
 inrast = grass.G_allocate_raster_buf(data_type)
 
-rown=0
-while 1:
+rown = 0
+while True:
     myrow = grass.G_get_raster_row(infd, inrast, rown, data_type)
-    print rown,myrow[0:10]
-    rown = rown+1
-    if rown==476:break
+    print rown, myrow[0:10]
+    rown += 1
+    if rown == 476:
+        break
 
 grass.G_close_cell(inrast)
 grass.G_free(cell)
