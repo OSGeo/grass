6c8444d714e7640c8832eabc760a1e0059adc5ed
Markus Neteler <neteler@gmail.com>
2009-05-22 16:59:56 +0000

    update to not have it completely wrong
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@37379 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- swig/python/setup.py
+++ swig/python/setup.py
@@ -4,9 +4,9 @@ import os
 setup(
 	ext_modules= [
 		Extension(
-			'_grass6_wxvdigit',
+			'_grass7_wxvdigit',
 			sources=[
-				"python_grass6.i",
+				"python_grass7.i",
 				],
 			swig_opts=['-c++', '-shadow', "-I%s" % os.environ.get("GRASS_INC"), "-I%s\\include" % os.environ.get("OSGEO4W_ROOT") ],
 			libraries=['grass_gmath', 'grass_imagery','grass_gis','grass_vedit','gdal_i']
