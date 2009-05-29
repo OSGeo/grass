b2fd29000b0ea7cdebce3fede3081ec39496edeb
Martin Landa <landa.martin@gmail.com>
2009-05-29 19:53:35 +0000

    Suppress warnings for xy locations (trac #492) Minor Doxygen updates
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@37590 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/proj1.c
+++ lib/gis/proj1.c
@@ -1,35 +1,33 @@
+/*!
+  \file proj1.c
 
-/**********************************************************************
- *  G_projection()
- *
- *  Returns the projection type of the currently set window.
- *  (Note this is really the coordinate system, not the projection)
- *    PROJECTION_XY  0 - x,y (Raw imagery)
- *    PROJECTION_UTM 1 - UTM   Universal Transverse Mercator
- *    PROJECTION_SP  2 - State Plane (in feet)
- *    PROJECTION_LL  3 - Latitude-Longitude
- *
- **********************************************************************/
+  \brief GIS Library - Get projection info
+  
+  (C) 1999-2009 by the GRASS Development Team
+  
+  This program is free software under the GNU General Public License
+  (>=v2). Read the file COPYING that comes with GRASS for details.
+
+  \author Original author CERL
+*/
 
 #include <grass/gis.h>
 
-
 /*!
- * \brief query cartographic projection
- *
- * This routine returns a code indicating the projection for the active region.  The current
- * values are:
- * 0 unreferenced x,y (imagery data)
- * 1 UTM
- * 2 State Plane
- * 3 Latitude-Longitude\remarks{Latitude-Longitude is not yet fully supported in
- * GRASS.}
- * Others may be added in the future. HINT GRASS 5: 121 projections!!
- *
- *  \param void
- *  \return int
- */
+  \brief Query cartographic projection
+  
+  This routine returns a code indicating the projection for the active region.  The current
+  values are:
 
+   - PROJECTION_XY  0 - x,y (Raw imagery)
+   - PROJECTION_UTM 1 - UTM   Universal Transverse Mercator
+   - PROJECTION_SP  2 - State Plane (in feet)
+   - PROJECTION_LL  3 - Latitude-Longitude
+
+   Others may be added in the future.
+
+   \return projection code (see above)
+*/
 int G_projection(void)
 {
     struct Cell_head window;
