c0ce1bdacf8938663a56dd997e5a0646dbf7056c
Martin Landa <landa.martin@gmail.com>
2009-05-30 16:27:52 +0000

    wxGUI: fix locales        (merge from devbr6, https://trac.osgeo.org/grass/changeset/37625)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@37626 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/menuform.py
+++ gui/wxpython/gui_modules/menuform.py
@@ -1597,7 +1597,7 @@ class cmdPanel(wx.Panel):
             cmd = self.task.getCmd( ignoreErrors=ignoreErrors )
         except ValueError, err:
             dlg = wx.MessageDialog(parent=self,
-                                   message=str(err),
+                                   message=unicode(err),
                                    caption=_("Error in %s") % self.task.name,
                                    style=wx.OK | wx.ICON_ERROR | wx.CENTRE)
             dlg.ShowModal()
