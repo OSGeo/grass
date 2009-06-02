44f32dc8919c72d5976937e33e65cd18d3b89ef7
Glynn Clements <glynn@gclements.plus.com>
2009-06-02 11:11:27 +0000

    Don't try to parse *.make files; use environment variables
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@37703 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/vdigit/setup.py
+++ gui/wxpython/vdigit/setup.py
@@ -11,10 +11,10 @@ from build_ext import update_opts
 from distutils.core import setup, Extension
 
 macros = [('PACKAGE', '"grasslibs"')]
-inc_dirs = [os.path.join(os.path.normpath(os.environ['ARCH_DISTDIR']), 'include'),
-	    os.path.join(os.path.normpath(os.environ['GISBASE']), 'include')]
-lib_dirs = [os.path.join(os.path.normpath(os.environ['ARCH_DISTDIR']), 'lib'),
-	    os.path.join(os.path.normpath(os.environ['GISBASE']), 'lib')]
+inc_dirs = [os.path.join(os.path.normpath(os.getenv('ARCH_DISTDIR')), 'include'),
+	    os.path.join(os.path.normpath(os.getenv('GISBASE')), 'include')]
+lib_dirs = [os.path.join(os.path.normpath(os.getenv('ARCH_DISTDIR')), 'lib'),
+	    os.path.join(os.path.normpath(os.getenv('GISBASE')), 'lib')]
 libs = ['grass_dbmibase',
         'grass_dbmiclient',
         'grass_vect',
@@ -22,13 +22,13 @@ libs = ['grass_dbmibase',
         'grass_vedit']
 extras = []
 
-for flag in ('GDALCFLAGS',
+for flag in ['GDALCFLAGS',
              'GDALLIBS',
              'GEOSCFLAGS',
-             'WXWIDGETSCXXFLAGS'):
-    update_opts(flag, macros, inc_dirs, lib_dirs, libs, extras)
+             'WXWIDGETSCXXFLAGS']:
+    update_opts(os.getenv(flag), macros, inc_dirs, lib_dirs, libs, extras)
 if sys.platform != 'darwin':
-    update_opts('WXWIDGETSLIB', macros, inc_dirs, lib_dirs, libs, extras)
+    update_opts(os.getenv('WXWIDGETSLIB'), macros, inc_dirs, lib_dirs, libs, extras)
 
 setup(
     ext_modules= [
@@ -56,3 +56,4 @@ setup(
             )
 	]
     )
+
