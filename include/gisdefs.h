0b3d3b74166944129a538952e4b6c0d01e8c498e
Glynn Clements <glynn@gclements.plus.com>
2009-06-30 15:57:49 +0000

    Most color_str.c functions are declared in colors.h
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@38124 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/gisdefs.h
+++ include/gisdefs.h
@@ -120,10 +120,6 @@ void G_clicker(void);
 
 /* color_str.c */
 int G_num_standard_colors(void);
-struct color_rgb G_standard_color_rgb(int);
-int G_num_standard_color_names(void);
-const struct color_name *G_standard_color_name(int);
-int G_str_to_color(const char *, int *, int *, int *);
 
 /* commas.c */
 int G_insert_commas(char *);
