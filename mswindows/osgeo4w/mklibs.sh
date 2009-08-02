1e2b944a617dc4a7eae4369a77e253cdffb80646
Markus Neteler <neteler@gmail.com>
2009-08-02 09:01:13 +0000

    Juergen Fischer: osgeo4w additions/updates
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@38563 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ mswindows/osgeo4w/mklibs.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+PROGRAMFILES="/c/Programme"
+VSDIR="$PROGRAMFILES/Microsoft Visual Studio 9.0"
+PATH="$VSDIR/Common7/IDE:$PATH"
+PATH="$VSDIR/VC/bin:$PATH"
+PATH="$VSDIR/Common7/Tools:$PATH"
+PATH="$PATH:/c/MinGW/bin"
+export PATH
+
+[ -d mswindows/osgeo4w/vc ] || mkdir mswindows/osgeo4w/vc
+
+for dllfile in "$@"; do
+	dlldir=${dllfile%/*}
+	dllfile=${dllfile##*/}
+
+	dllbase=${dllfile%.dll}
+	dllname=${dllbase#lib}
+	dllname=${dllname%.$VERSION}
+	defname=$dllname.def
+	libname=$dllname.lib
+
+ 	echo "$dllfile => $dllname"
+
+	(cd $dlldir; dumpbin -exports $dllfile) |
+		sed -nf mswindows/osgeo4w/mklibs.sed |
+		egrep -v "^[ 	]*(_IMPORT_DESCRIPTOR_.*|_NULL_IMPORT_DESCRIPTOR)$" >mswindows/osgeo4w/vc/${defname%$VERSION}
+
+	cd mswindows/osgeo4w/vc
+	lib -nologo -def:${defname} -subsystem:windows -machine:x86
+	lib -nologo $libname || exit
+	cd ../../..
+done 
---
+++ mswindows/osgeo4w/preremove.bat
@@ -0,0 +1,15 @@
+del "%OSGEO4W_STARTMENU%\GRASS.lnk"
+del "%ALLUSERSPROFILE%\Desktop\grass.lnk"
+
+del "%OSGEO4W_STARTMENU%\GRASS GIS\wxpython.lnk"
+del "%OSGEO4W_STARTMENU%\GRASS GIS\tcltk.lnk"
+del "%OSGEO4W_STARTMENU%\GRASS GIS\text.lnk"
+rmdir "%OSGEO4W_STARTMENU%\GRASS GIS"
+
+del "%ALLUSERSPROFILE%\Desktop\GRASS GIS (wxPython).lnk"
+del "%ALLUSERSPROFILE%\Desktop\GRASS GIS (TclTk).lnk"
+del "%ALLUSERSPROFILE%\Desktop\GRASS GIS (Text).lnk"
+
+del "%OSGEO4W_ROOT%"\apps\grass\grass-7.0.0svn\etc\fontcap
+del "%OSGEO4W_ROOT%"\apps\grass\bin\grass70
+del "%OSGEO4W_ROOT%"\bin\grass70.bat
