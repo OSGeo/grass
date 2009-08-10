042cb5e30e6042d000682c531cb8c228de01c675
Markus Metz <markus.metz.giswork@gmail.com>
2009-08-10 05:59:59 +0000

    bugfix for rtree_search
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@38669 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/vector/diglib/spindex_rw.c
+++ lib/vector/diglib/spindex_rw.c
@@ -1008,7 +1008,7 @@ int rtree_search(struct RTree *t, struct Rect *r, SearchHitCallback shcb,
 		    RTreeOverlap(r, &(s[top].sn.branch[i].rect), t)) {
 		    hitCount++;
 		    if (shcb) {	/* call the user-provided callback */
-			if (!shcb((int)s[top].sn.branch[i].child, cbarg)) {
+			if (!shcb(s[top].sn.branch[i].child.id, cbarg)) {
 			    /* callback wants to terminate search early */
 			    return hitCount;
 			}
