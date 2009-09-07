6841d540e51bec12d2cc66db7a27bb705ef101a9
Martin Landa <landa.martin@gmail.com>
2009-09-07 10:47:01 +0000

    v.in.dxf: cosmetics in manual page 	  more keywords
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@39061 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.in.dxf/main.c
+++ vector/v.in.dxf/main.c
@@ -63,6 +63,7 @@ int main(int argc, char *argv[])
     module = G_define_module();
     G_add_keyword(_("vector"));
     G_add_keyword(_("import"));
+    G_add_keyword(_("dxf"));
     module->description =
 	_("Converts files in DXF format to GRASS vector map format.");
 
@@ -131,8 +132,7 @@ int main(int argc, char *argv[])
 	G_fatal_error(_("Unable to open DXF file <%s>"), opt.input->answer);
 
     if (flag_list)
-	G_verbose_message(_
-			  ("Layer number: layer name / GRASS compliant name"));
+	G_verbose_message(_("Layer number: layer name / GRASS compliant name"));
     else {
 	int i;
 
