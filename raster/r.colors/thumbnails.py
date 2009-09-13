adcaea0f0649af3eb7379b86df8c39544925b43a
Glynn Clements <glynn@gclements.plus.com>
2009-09-13 03:28:02 +0000

    Generate thumbnails for r.colors HTML file during build
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@39164 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ raster/r.colors/thumbnails.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import atexit
+import string
+import array
+import grass.script as grass
+
+tmp_img = None
+tmp_grad_abs = None
+tmp_grad_rel = None
+
+height = 85
+width = 15
+
+def cleanup():
+    if tmp_img:
+	grass.try_remove(tmp_img)
+    if tmp_grad_rel:
+	grass.run_command('g.remove', rast = tmp_grad_rel, quiet = True)
+    if tmp_grad_abs:
+	grass.run_command('g.remove', rast = tmp_grad_abs, quiet = True)
+
+# def rotate(src, dst):
+#     grass.call(["convert", "-rotate", "90", src, dst])
+
+def read_ppm(src):
+    fh = open(src, "rb")
+    text = fh.read()
+    fh.close()
+    i = 0
+    j = text.find('\n', i)
+    if text[i:j] != 'P6':
+	raise IOError
+    i = j + 1
+    j = text.find('\n', i)
+    w, h = text[i:j].split()
+    if int(w) != width or int(h) != height:
+	raise IOError
+    i = j + 1
+    j = text.find('\n', i)
+    maxval = text[i:j]
+    if int(maxval) != 255:
+	raise IOError
+    i = j + 1
+    return array.array('B', text[i:])
+
+def write_ppm(dst, data):
+    w = height
+    h = width
+    fh = open(dst, "wb")
+    fh.write("P6\n%d %d\n%d\n" % (w, h, 255))
+    data.tofile(fh)
+    fh.close()
+
+def rotate_ppm(srcd):
+    dstd = array.array('B', len(srcd) * '\0')
+    for y in xrange(height):
+	for x in xrange(width):
+	    for c in xrange(3):
+		dstd[(x * height + (height - 1 - y)) * 3 + c] = srcd[(y * width + x) * 3 + c]
+    return dstd
+
+def ppmtopng(dst, src):
+    fh = open(dst, 'wb')
+    grass.call(["pnmtopng", tmp_img], stdout = fh)
+    fh.close()
+
+def convert_and_rotate(src, dst):
+    if grass.find_program("convert"):
+	grass.call(["convert", "-rotate", "90", src, dst])
+    elif grass.find_program("pnmtopng"):
+	srcd = read_ppm(src)
+	dstd = rotate_ppm(srcd)
+	write_ppm(tmp_img, dstd)
+	ppmtopng(dst, tmp_img)
+    else:
+	grass.fatal(_("Cannot find either convert or pnmtopng"))
+
+def main():
+    global tmp_img, tmp_grad_abs, tmp_grad_rel
+
+    os.environ['GRASS_OVERWRITE'] = '1'
+
+    color_dir = os.path.join(os.environ['GISBASE'], "etc", "colors")
+    output_dir = os.path.join(os.environ['GISBASE'], "docs", "html")
+
+    if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
+
+    pid = os.getpid()
+    tmp_grad_abs = "tmp_grad_abs_%d" % pid
+    tmp_grad_rel = "tmp_grad_rel_%d" % pid
+    tmp_img = grass.tempfile() + ".ppm"
+
+    os.environ['GRASS_WIDTH'] = '%d' % width
+    os.environ['GRASS_HEIGHT'] = '%d' % height
+    os.environ['GRASS_FRAME'] = '%f,%f,%f,%f' % (0,height,0,width)
+    os.environ['GRASS_PNGFILE'] = tmp_img
+    os.environ['GRASS_TRUECOLOR'] = 'TRUE'
+    os.environ['GRASS_PNG_READ'] = 'FALSE'
+    os.environ['GRASS_PNG_MAPPED'] = 'FALSE'
+    os.environ['GRASS_TRANSPARENT'] = 'FALSE'
+    os.environ['GRASS_BACKGROUNDCOLOR'] = 'ffffff'
+
+    grass.use_temp_region()
+    grass.run_command('g.region', rows = 100, cols = 100)
+
+    grass.mapcalc("$grad = row()/1.0", grad = tmp_grad_rel)
+
+    for table in os.listdir(color_dir):
+        path = os.path.join(color_dir, table)
+        fh = open(path)
+        text = fh.read()
+        fh.close()
+        lines = text.splitlines()
+        records = [line.split() for line in lines]
+        records = [record for record in records if record[0] != 'nv']
+        relative = False
+        absolute = False
+        for record in records:
+            if record[0].endswith("%"):
+                relative = True
+                record[0] = record[0].rstrip("%")
+            else:
+                absolute = True
+
+        if absolute:
+	    if relative:
+		minval = -0.04
+		maxval = 0.04
+	    else:
+		minval = float(records[0][0])
+		maxval = float(records[-1][0])
+		maxval = min(maxval, 2500000)
+	    grad = tmp_grad_abs
+	    grass.mapcalc("$grad = if(row()==1, float($min), float($max))",
+                          grad = tmp_grad_abs, min = minval, max = maxval)
+	else:
+            grad = tmp_grad_rel
+
+        grass.run_command("r.colors", map = grad, color = table, quiet = True)
+        grass.run_command("d.colortable", flags = 'n', map = grad)
+
+	outfile = os.path.join(output_dir, "Colortable_%s.png" % table)
+	convert_and_rotate(tmp_img, outfile)
+
+if __name__ == "__main__":
+    atexit.register(cleanup)
+    main()
