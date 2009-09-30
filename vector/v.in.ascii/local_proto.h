47f1ae506d45b2c28e6f361d256a11f262993709
Martin Landa <landa.martin@gmail.com>
2009-09-30 20:32:12 +0000

    move read/write GRASS ASCII vector format functionality to Vlib
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@39364 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.in.ascii/local_proto.h
+++ vector/v.in.ascii/local_proto.h
@@ -1,25 +1,14 @@
 #ifndef __LOCAL_PROTO_H__
 #define __LOCAL_PROTO_H__
 
-
 #include <stdio.h>
 #include <grass/gis.h>
 #include <grass/dbmi.h>
-
-
-#define FORMAT_POINT 0
-#define FORMAT_ALL   1
-
 
 int points_analyse(FILE *, FILE *, char *, int *, int *, int *, int *, int **,
 		   int **, int, int, int, int);
 
 int points_to_bin(FILE *, int, struct Map_info *, dbDriver *,
 		  char *, char *, int, int, int *, int, int, int, int, int);
-
-int read_head(FILE *, struct Map_info *);
-
-int asc_to_bin(FILE *, struct Map_info *);
-
 
 #endif /* __LOCAL_PROTO_H__ */
