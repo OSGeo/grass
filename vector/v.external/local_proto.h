f07179a18171ec4cfe2c5f1410d790234bf36756
Martin Landa <landa.martin@gmail.com>
2009-10-11 07:18:58 +0000

    v.external: parse arguments in parse_args()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@39481 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.external/local_proto.h
+++ vector/v.external/local_proto.h
@@ -1,3 +1,23 @@
+#ifndef V_EXTERNAL_LOCAL_PROTO_H
+#define V_EXTERNAL_LOCAL_PROTO_H
+
+struct _options {
+    struct Option *dsn, *output, *layer;
+};
+
+struct _flags {
+    struct Flag *format, *layer, *topo;
+};
+
+/* args.c */
+void parse_args(int, char **,
+		struct _options *, struct _flags*);
+void get_args(const struct _options *, const struct _flags *,
+	      char **, char **, char **,
+	      int *, int *, int *);
+
 /* list.c */
 void list_formats(FILE *);
 int list_layers(FILE *, const char *, const char *);
+
+#endif
