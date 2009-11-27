595e925654405b98009bd136d7a9ee3b18f26bb8
Martin Landa <landa.martin@gmail.com>
2009-11-27 12:48:16 +0000

    v.delaunay: OGR support (read access) 	    cosmetics in  message output
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@39821 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.delaunay/in_out.h
+++ vector/v.delaunay/in_out.h
@@ -1,8 +1,8 @@
 #ifndef IN_OUT_H
 #define IN_OUT_H
 
-int read_sites(int mode3d, int complete_map, struct Map_info map_in,
-	       struct bound_box Box);
+int read_sites(int mode3d, int complete_map, struct Map_info* map_in,
+	       struct bound_box Box, int);
 void output_edges(struct vertex *sites_sorted[], unsigned int n, int mode3d,
 		  int Type, struct Map_info map_out);
 void output_triangles(struct vertex *sites_sorted[], unsigned int n,
