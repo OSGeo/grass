53a7939da47bdc9babd9c1bd6a412569cedf2ef5
Martin Landa <landa.martin@gmail.com>
2009-11-27 17:51:50 +0000

    v.hull: code reorganization + OGR support (read access)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@39829 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.hull/chull.h
+++
@@ -1,4 +0,0 @@
-#include <grass/vector.h>
-
-int make3DHull(double *X, double *Y, double *Z, int num_points,
-	       struct Map_info *Map);
---
+++ vector/v.hull/read.c
@@ -0,0 +1,57 @@
+#include <grass/gis.h>
+
+#include "hull.h"
+
+int loadSiteCoordinates(struct Map_info *Map, struct Point **points, int all,
+			struct Cell_head *window, int field)
+{
+    int pointIdx = 0;
+    struct line_pnts *sites;
+    struct line_cats *cats;
+    struct bound_box box;
+    int cat, type;
+
+    sites = Vect_new_line_struct();
+    cats = Vect_new_cats_struct();
+
+    *points = NULL;
+
+    /* copy window to box */
+    Vect_region_box(window, &box);
+
+    while ((type = Vect_read_next_line(Map, sites, cats)) > -1) {
+
+	if (type != GV_POINT)
+	    continue;
+
+	if (Vect_cat_get(cats, field, &cat) == 0)
+	    continue;
+
+	G_debug(4, "Point: %f|%f|%f|#%d", sites->x[0], sites->y[0],
+		sites->z[0], cat);
+
+	if (all ||
+	    Vect_point_in_box(sites->x[0], sites->y[0], sites->z[0], &box)) {
+
+	    G_debug(4, "Point in the box");
+
+	    if ((pointIdx % ALLOC_CHUNK) == 0)
+		*points =
+		    (struct Point *)G_realloc(*points,
+					      (pointIdx +
+					       ALLOC_CHUNK) *
+					      sizeof(struct Point));
+
+	    (*points)[pointIdx].x = sites->x[0];
+	    (*points)[pointIdx].y = sites->y[0];
+	    (*points)[pointIdx].z = sites->z[0];
+	    pointIdx++;
+	}
+    }
+
+    if (pointIdx > 0)
+	*points =
+	    (struct Point *)G_realloc(*points,
+				      (pointIdx + 1) * sizeof(struct Point));
+    return pointIdx;
+}
