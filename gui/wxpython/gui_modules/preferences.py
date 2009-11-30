30e5d33e08cb708cf67bb3939906a2e9bfaa2f4d
Michael Barton <michael.barton@asu.edu>
2009-11-30 06:13:59 +0000

    Fix bug that hits first time users of GRASS lacking a .grasswx6 file. Backport from develbranch_6 https://trac.osgeo.org/grass/changeset/39857 & https://trac.osgeo.org/grass/changeset/39858
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@39859 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/preferences.py
+++ gui/wxpython/gui_modules/preferences.py
@@ -1778,8 +1778,9 @@ class PreferencesDialog(wx.Dialog):
 
         type = self.settings.Get(group='display', key='outputfont', subkey='type')   
                            
-        size = float(self.settings.Get(group='display', key='outputfont', subkey='size'))
-        if size == None or size == 0: size = 9
+        size = self.settings.Get(group='display', key='outputfont', subkey='size')
+        if size == None or size == 0: size = 10
+        size = float(size)
         
         data = wx.FontData()
         data.EnableEffects(True)
@@ -1793,7 +1794,7 @@ class PreferencesDialog(wx.Dialog):
 
             self.settings.Set(group='display', value=font.GetFaceName(),
                                   key='outputfont', subkey='type')
-            self.settings.Set(group='display', value=str(font.GetPointSize()),
+            self.settings.Set(group='display', value=font.GetPointSize(),
                                   key='outputfont', subkey='size')
                 
         dlg.Destroy()
