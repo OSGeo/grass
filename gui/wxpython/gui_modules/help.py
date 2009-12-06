OSGeo/grass-legacy

commit 8d71184edcfac612e42fbf8b600ae1d993a805e8

Fixed menu tree access on Macs and double-clicking item to run command
git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@39922 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/gui_modules/help.py
@@ -64,7 +64,8 @@ class MenuTreeWindow(wx.Frame):
         wx.Frame.__init__(self, parent = parent, id = id, title = title, **kwargs)
 
         self.panel = wx.Panel(parent = self, id = wx.ID_ANY)
-        
+        self.dataBox = wx.StaticBox(parent = self.panel, id = wx.ID_ANY,
+                               label=" %s " % _("Menu tree (double-click to run command)"))        
         # tree
         self.tree = MenuTree(parent = self.panel, data = menudata.Data())
         self.tree.Load()
@@ -115,10 +116,10 @@ class MenuTreeWindow(wx.Frame):
         sizer = wx.BoxSizer(wx.VERTICAL)
         
         # body
-        dataBox = wx.StaticBox(parent = self.panel, id = wx.ID_ANY,
-                               label=" %s " % _("Menu tree (double-click to run command)"))
+#        dataBox = wx.StaticBox(parent = self.panel, id = wx.ID_ANY,
+#                               label=" %s " % _("Menu tree (double-click to run command)"))
         
-        dataSizer = wx.StaticBoxSizer(dataBox, wx.HORIZONTAL)
+        dataSizer = wx.StaticBoxSizer(self.dataBox, wx.HORIZONTAL)
         dataSizer.Add(item = self.tree, proportion =1,
                       flag = wx.EXPAND)
         
@@ -176,10 +177,14 @@ class MenuTreeWindow(wx.Frame):
             return
 
         handler = 'self.parent.' + data['handler'].lstrip('self.')
-        if data['command']:
-            eval(handler)(event = None, cmd = data['command'].split())
+        print 'handler = '+data['handler']
+        if data['handler'] == 'self.OnXTerm':
+            wx.MessageBox('You must run this command from the menu or command line',
+                          'This command require an XTerm')
+        elif data['command']:
+            self.parent.goutput.RunCmd(data['command'], switchPage = True)
         else:
-            eval(handler)(None)
+            pass
         
         if self.closeOnRun.IsChecked():
             self.OnCloseWindow(None)

OSGeo/grass

commit 325d7279558a12a0f9bb1fabaebd682c673284cc

fix bug introduced in r39922 - menu items are not grass commands, eval() required        (merge r39925 from devbr6)
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@39927 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/gui_modules/help.py
@@ -65,7 +65,7 @@ class MenuTreeWindow(wx.Frame):
 
         self.panel = wx.Panel(parent = self, id = wx.ID_ANY)
         self.dataBox = wx.StaticBox(parent = self.panel, id = wx.ID_ANY,
-                               label=" %s " % _("Menu tree (double-click to run command)"))        
+                                    label=" %s " % _("Menu tree (double-click to run command)"))        
         # tree
         self.tree = MenuTree(parent = self.panel, data = menudata.Data())
         self.tree.Load()
@@ -116,9 +116,6 @@ class MenuTreeWindow(wx.Frame):
         sizer = wx.BoxSizer(wx.VERTICAL)
         
         # body
-#        dataBox = wx.StaticBox(parent = self.panel, id = wx.ID_ANY,
-#                               label=" %s " % _("Menu tree (double-click to run command)"))
-        
         dataSizer = wx.StaticBoxSizer(self.dataBox, wx.HORIZONTAL)
         dataSizer.Add(item = self.tree, proportion =1,
                       flag = wx.EXPAND)
@@ -177,14 +174,15 @@ class MenuTreeWindow(wx.Frame):
             return
 
         handler = 'self.parent.' + data['handler'].lstrip('self.')
-        print 'handler = '+data['handler']
         if data['handler'] == 'self.OnXTerm':
-            wx.MessageBox('You must run this command from the menu or command line',
-                          'This command require an XTerm')
+            wx.MessageBox(parent = self,
+                          message = _('You must run this command from the menu or command line',
+                                      'This command require an XTerm'),
+                          caption = _('Message'), style=wx.OK | wx.ICON_ERROR | wx.CENTRE)
         elif data['command']:
-            self.parent.goutput.RunCmd(data['command'], switchPage = True)
+            eval(handler)(event = None, cmd = data['command'].split())
         else:
-            pass
+            eval(handler)(None)
         
         if self.closeOnRun.IsChecked():
             self.OnCloseWindow(None)