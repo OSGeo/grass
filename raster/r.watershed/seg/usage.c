f4020846463f817bbbdef54d71bb10ca629c2e7a
Markus Metz <markus.metz.giswork@gmail.com>
2009-12-28 10:18:57 +0000

    sync seg to changes in front r40146
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@40154 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.watershed/seg/usage.c
+++ raster/r.watershed/seg/usage.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <grass/gis.h>
 #include <grass/glocale.h>
 
 
 void usage(char *me)
 {
-    G_fatal_error(_("USAGE for basin delineation:\n%s -4 el=elevation_map "
-		    "t=swale_threshold [ov=overland_flow_map] [dr=drain_direction_map] "
-		    "[de=depression_map] [ac=accumulation_map] [di=display_map] "
-		    "ba=watershed_basin_map [se=stream_segment_map]\n\nUSAGE for "
-		    "ARMSED FILE creation:\n%s [-4] el=elevation_map "
-		    "t=swale_threshold [ov=overland_flow_map] [dr=drain_direction_map] "
-		    "[de=depression_map] [ac=accumulation_map] [di=display_map] "
-		    "[ba=watershed_basin_map] [se=stream_segment_map] "
-		    "ha=half_basin_map ar=ARMSED_file_name\n\nUSAGE for slope "
-		    "length determination:\n%s [-4] el=elevation_map "
-		    "t=swale_threshold [dr=drain_direction_map] "
-		    "[de=depression_map] [ac=accumulation_map] [di=display_map] "
-		    "[ms=max_slope_length] [ob=overland_blocking_map] "
-		    "[S=slope_steepness_map] LS=length_slope_map "
-		    "[r=rill_erosion_map] [sd=slope_deposition value or map]"),
-		  me, me, me);
+    G_fatal_error(_("USAGE for basin delineation:\n%s -4 elevation=elevation_map "
+		    "threshold=swale_threshold [flow=overland_flow_map] "
+		    "[drainage=drain_direction_map] [depression=depression_map] "
+		    "[accumulation=accumulation_map] [basin=watershed_basin_map] "
+		    "[stream=stream_segment_map]\n\n"
+		    "USAGE for slope length determination:\n%s [-4] "
+		    "elevation=elevation_map threshold=swale_threshold "
+		    "[drainage=drain_direction_map] [depression=depression_map] "
+		    "[accumulation=accumulation_map] [max_slope_length=max_slope_length] "
+		    "[blocking=overland_blocking_map] [slope_steepness=slope_steepness_map] "
+		    "length_slope=length_slope_map [disturbed_land=rill_erosion_map] "
+		    "[slope_deposition=slope_deposition value or map]"
+		    "USAGE for ARMSED FILE creation:\n%s [-4] elevation=elevation_map "
+		    "threshold=swale_threshold [flow=overland_flow_map] "
+		    "[drainage=drain_direction_map] [depression=depression_map] "
+		    "[accumulation=accumulation_map] [basin=watershed_basin_map] "
+		    "[stream=stream_segment_map] [half_basin=half_basin_map] "
+		    "ar=ARMSED_file_name\n\n"), me, me, me);
 }
