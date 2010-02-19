3d9ea0e29fcb3a63d5e0bd165eb45a7898c5f2df
Glynn Clements <glynn@gclements.plus.com>
2010-02-19 11:49:20 +0000

    Document -s flag
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@41119 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/g.parser/main.c
+++ general/g.parser/main.c
@@ -395,7 +395,7 @@ int main(int argc, char *argv[])
     if ((argc < 2) || ((strcmp(argv[1], "help") == 0) ||
 		       (strcmp(argv[1], "-help") == 0) ||
 		       (strcmp(argv[1], "--help") == 0))) {
-	fprintf(stderr, "Usage: %s [-t] <filename> [<argument> ...]\n",
+	fprintf(stderr, "Usage: %s [-t] [-s] <filename> [<argument> ...]\n",
 		argv[0]);
 	return 1;
     }
