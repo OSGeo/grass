7dcd7fc9261772f9501f5cb36ade7e9cb5097116
Hamish Bowman <hamish_b@yahoo.com>
2010-03-02 02:25:16 +0000

    fix background transparency (merge from devbr6; thanks Glynn) trac #967
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@41257 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.out.png/main.c
+++ raster/r.out.png/main.c
@@ -239,15 +239,13 @@ int main(int argc, char *argv[])
 
     png_set_compression_level(png_ptr, png_compr);
 
-/* FIXME
     if(do_alpha) {
-	png_color_16p background_color;
-	background_color->red = (png_uint_16)def_red;
-	background_color->green = (png_uint_16)def_grn;
-	background_color->blue = (png_uint_16)def_blu;
-	png_set_bKGD(png_ptr, info_ptr, background_color);
+	png_color_16 background_color;
+	background_color.red = (png_uint_16)def_red;
+	background_color.green = (png_uint_16)def_grn;
+	background_color.blue = (png_uint_16)def_blu;
+	png_set_bKGD(png_ptr, info_ptr, &background_color);
     }
-*/
 
     G_verbose_message(_("Converting <%s>..."), rast->answer);
 
