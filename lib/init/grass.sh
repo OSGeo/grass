b4051c844e99b18dd8b7002a8a2d8470b3c5cb48
Martin Landa <landa.martin@gmail.com>
2010-04-04 18:19:57 +0000

    remove init sh/bat scripts (replaced by python scripts)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@41720 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ lib/init/grass.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+#############################################################################
+#
+# MODULE:   	GRASS initialization (Shell)
+# AUTHOR(S):	Original author unknown - probably CERL
+#               Andreas Lange - Germany - andreas.lange@rhein-main.de
+#   	    	Huidae Cho - Korea - grass4u@gmail.com
+#   	    	Justin Hickey - Thailand - jhickey@hpcc.nectec.or.th
+#   	    	Markus Neteler - Germany/Italy - neteler@itc.it
+#		Hamish Bowman - New Zealand - hamish_b at yahoo,com
+# PURPOSE:  	Sets up some environment variables.
+#               It also parses any remaining command line options for
+#               setting the GISDBASE, LOCATION, and/or MAPSET.
+#               Finally it starts GRASS with the appropriate user
+#   	    	interface and cleans up after it is finished.
+# COPYRIGHT:    (C) 2000-2010 by the GRASS Development Team
+#
+#               This program is free software under the GNU General
+#   	    	Public License (>=v2). Read the file COPYING that
+#   	    	comes with GRASS for details.
+#
+#############################################################################
+
+trap "echo 'User break!' ; exit" 2 3 9 15
+
+if [ -z "$GRASS_PYTHON" ] ; then
+    GRASS_PYTHON=python
+fi
+export GRASS_PYTHON
+
+exec $GRASS_PYTHON "@BINDIR@/grass70.py" "$@" &
--- lib/init/prompt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-LOCATION="`g.gisenv GISDBASE`/`g.gisenv LOCATION_NAME`/`g.gisenv MAPSET`"
-if test -d "$LOCATION/grid3/G3D_MASK" && test -f "$LOCATION/cell/MASK" ; then
-    echo [Raster and Volume MASKs present]
-elif test -f "$LOCATION/cell/MASK" ; then
-    echo [Raster MASK present]
-elif test -d "$LOCATION/grid3/G3D_MASK" ; then
-    echo [Volume MASK present]
-fi
