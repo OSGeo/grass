64ac7480dbadd64d68937880c31db5dcec434105
Martin Landa <landa.martin@gmail.com>
2010-04-17 09:01:40 +0000

    wxGUI/modeler: data size configurable
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@41903 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/preferences.py
+++ gui/wxpython/gui_modules/preferences.py
@@ -522,6 +522,12 @@ class Settings:
                         'height' : 50,
                         },
                     },
+                'data' : { 
+                    'size' : {
+                        'width' : 175,
+                        'height' : 50,
+                        },
+                    },
                 },
             }
         
