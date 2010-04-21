bc79467fdc4aabda6ee1393ba3b19c88d0e42d37
Martin Landa <landa.martin@gmail.com>
2010-04-21 10:13:11 +0000

    wxGUI: d.rast -n
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@41944 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/gdialogs.py
+++ gui/wxpython/gui_modules/gdialogs.py
@@ -1364,8 +1364,8 @@ class MultiImportDialog(wx.Dialog):
                 if self.importType == 'gdal':
                     cmd = ['d.rast',
                            'map=%s' % name]
-                    if UserSettings.Get(group='cmd', key='rasterOverlay', subkey='enabled'):
-                        cmd.append('-o')
+                    if UserSettings.Get(group='cmd', key='rasterOpaque', subkey='enabled'):
+                        cmd.append('-n')
 
                     maptree.AddLayer(ltype='raster',
                                      lname=name,
