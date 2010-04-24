5174e3fbebc832047253566f4f43d3ecaf15c59e
Soeren Gebbert <soerengebbert@googlemail.com>
2010-04-24 22:57:31 +0000

    Typo fix
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@42024 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/parser_wps.c
+++ lib/gis/parser_wps.c
@@ -174,11 +174,11 @@ void G__wps_print_process_description(void)
     wps_print_data_inputs_begin();
 
     /* We have two default options, which define the resolution of the created mapset */
-    wps_print_literal_input_output(WPS_INPUT, 0, 1, "grass_resolution_ns", "Resolution of the mapset in north-south direction in [m] or [°]",
-        "This parameter defines the north-south resolution of the mapset in meter or degrees, which should be used ot process the input and output raster data. To enable this setting, you need to specify north-south and east-west resolution.",
+    wps_print_literal_input_output(WPS_INPUT, 0, 1, "grass_resolution_ns", "Resolution of the mapset in north-south direction in [m] or [degrees]",
+        "This parameter defines the north-south resolution of the mapset in meter or degrees, which should be used to process the input and output raster data. To enable this setting, you need to specify north-south and east-west resolution.",
         "float", 0, NULL, 0, "25", TYPE_OTHER);
-    wps_print_literal_input_output(WPS_INPUT, 0, 1, "grass_resolution_ew", "Resolution of the mapset in east-west direction in [m] or [°]",
-        "This parameter defines the east-west resolution of the mapset in meters or degrees, which should be used ot process the input and output raster data.  To enable this setting, you need to specify north-south and east-west resolution.",
+    wps_print_literal_input_output(WPS_INPUT, 0, 1, "grass_resolution_ew", "Resolution of the mapset in east-west direction in [m] or [degrees]",
+        "This parameter defines the east-west resolution of the mapset in meters or degrees, which should be used to process the input and output raster data.  To enable this setting, you need to specify north-south and east-west resolution.",
         "float", 0, NULL, 0, "25", TYPE_OTHER);
     wps_print_literal_input_output(WPS_INPUT, 0, 1, "grass_band_number", "Band to select for processing (default is all bands)",
         "This parameter defines band number of the input raster files which should be processed. As default all bands are processed and used as single and multiple inputs for raster modules.",
