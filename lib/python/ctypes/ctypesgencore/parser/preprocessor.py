49697df61360a1b7512f6f1b2ce0f28936c7f433
Martin Landa <landa.martin@gmail.com>
2010-05-26 14:26:05 +0000

    Fix compiling ctypes on Windows
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@42354 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/ctypes/ctypesgencore/parser/preprocessor.py
+++ lib/python/ctypes/ctypesgencore/parser/preprocessor.py
@@ -109,7 +109,7 @@ class PreprocessorParser(object):
 
         # On OSX, explicitly add these defines to keep from getting syntax
         # errors in the OSX standard headers.
-        if os.uname()[0] == 'Darwin':
+        if hasattr(os, 'uname') and os.uname()[0] == 'Darwin':
             self.defines += ["__uint16_t=uint16_t",
                              "__uint32_t=uint32_t",
                              "__uint64_t=uint64_t"]
