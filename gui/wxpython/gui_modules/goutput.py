12a5557e42b99b5b028f1fba636f4996de7fbda6
Martin Landa <landa.martin@gmail.com>
2010-06-24 22:39:50 +0000

    wxGUI: don't redirect on debug level > 0
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@42656 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/goutput.py
+++ gui/wxpython/gui_modules/goutput.py
@@ -341,7 +341,7 @@ class GMConsole(wx.SplitterWindow):
         @return True redirected
         @return False failed
         """
-        if Debug.get_level() == 0:
+        if Debug.get_level() == 0 and int(grass.gisenv().get('DEBUG', 0)) == 0:
             # don't redirect when debugging is enabled
             sys.stdout = self.cmd_stdout
             sys.stderr = self.cmd_stderr
@@ -817,6 +817,9 @@ class GMStderr:
         self.message = ''
         self.printMessage = False
         
+    def flush(self):
+        pass
+    
     def write(self, s):
         if "GtkPizza" in s:
             return
