11eee5ad4f983eb9f56ac14ee9348bc799d2b4e3
Markus Metz <markus.metz.giswork@gmail.com>
2010-07-13 07:43:23 +0000

    silk: zoom_extend now available
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@42778 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/icons/silk_icons.py
+++ gui/wxpython/icons/silk_icons.py
@@ -29,7 +29,7 @@ IconsSilk = {
     "zoom_out"   : 'zoom_out.png',
     "zoom_back"  : 'zoom_back.png',
     "zoommenu"   : 'zoom.png',
-    "zoom_extent" : wx.ART_ERROR, # FIXME
+    "zoom_extent" : 'zoom_extend.png',
     # analyze raster (mapdisplay)
     "analyze"    : 'application_lightning.png',
     "measure"    : 'sum.png',
