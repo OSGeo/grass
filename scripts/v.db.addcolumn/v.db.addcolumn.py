8135e1d03fd65a13ce29824a5d88ed8543056e31
Martin Landa <landa.martin@gmail.com>
2010-08-10 15:22:39 +0000

    v.db.addcolumn: inform user when column already exists
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@43037 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/v.db.addcolumn/v.db.addcolumn.py
+++ scripts/v.db.addcolumn/v.db.addcolumn.py
@@ -55,38 +55,43 @@ import os
 import grass.script as grass
 
 def main():
-    map = options['map']
-    layer = options['layer']
+    map     = options['map']
+    layer   = options['layer']
     columns = options['columns']
     columns = [col.strip() for col in columns.split(',')]
-
+    
     # does map exist in CURRENT mapset?
     mapset = grass.gisenv()['MAPSET']
     exists = bool(grass.find_file(map, element = 'vector', mapset = mapset)['file'])
-
+    
     if not exists:
 	grass.fatal(_("Vector map <%s> not found in current mapset") % map)
-
+    
     try:
         f = grass.vector_db(map)[int(layer)]
     except KeyError:
 	grass.fatal(_("There is no table connected to this map. Run v.db.connect or v.db.addtable first."))
-    table = f['table']
+    
+    table    = f['table']
     database = f['database']
-    driver = f['driver']
-
-    colnum = len(columns)
-
+    driver   = f['driver']
+    column_existing = grass.vector_columns(map, int(layer)).keys()    
+    
     for col in columns:
 	if not col:
 	    grass.fatal(_("There is an empty column. Did you leave a trailing comma?"))
-
+        col_name = col.split(' ')[0].strip()
+        if col_name in column_existing:
+            grass.error(_("Column <%s> is already in the table. Skipping.") % col_name)
+            continue
+        grass.verbose(_("Adding column <%s> to the table") % col_name)
 	p = grass.feed_command('db.execute', input = '-', database = database, driver = driver)
 	p.stdin.write("ALTER TABLE %s ADD COLUMN %s" % (table, col))
+        grass.debug("ALTER TABLE %s ADD COLUMN %s" % (table, col))
 	p.stdin.close()
 	if p.wait() != 0:
 	    grass.fatal(_("Unable to add column <%s>.") % col)
-
+    
     # write cmd history:
     grass.vector_history(map)
 
