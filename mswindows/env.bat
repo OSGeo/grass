bdc4ece789e2d4f6c838443045ac419cd4cfe8e1
Martin Landa <landa.martin@gmail.com>
2011-03-07 17:39:00 +0000

    winGRASS: move environmental variables to env.bat, init.bat: wxGUI default (merge https://trac.osgeo.org/grass/changeset/45596 from devbr6)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@45597 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ mswindows/env.bat
@@ -0,0 +1,31 @@
+set WINGISBASE=%GISBASE%
+
+rem Name of the wish (Tk) executable
+set GRASS_WISH=wish.exe
+
+rem Path to the shell command
+set GRASS_SH=%GISBASE%\msys\bin\sh.exe
+
+rem Set Path to utilities (libraries and bynaries) used by GRASS
+set PATH=%GISBASE%\msys\bin;%PATH%
+set PATH=%GISBASE%\extrabin;%GISBASE%\extralib;%PATH%
+set PATH=%GISBASE%\tcl-tk\bin;%GISBASE%\sqlite\bin;%GISBASE%\gpsbabel;%PATH%
+
+rem Set Path to default web browser
+set GRASS_HTML_BROWSER=explorer
+
+rem Path to the proj files (notably the epsg projection list)
+set GRASS_PROJSHARE=%GISBASE%\proj
+
+rem Set GDAL_DATA
+set GDAL_DATA=%GISBASE%\share\gdal
+
+rem Set PROJ_LIB
+set PROJ_LIB=%GISBASE%\proj
+
+rem Set GEOTIFF_CSV
+set GEOTIFF_CSV=%GISBASE%\share\epsg_csv
+
+rem Path to the python directory
+set PYTHONHOME=%GISBASE%\Python25
+if "x%GRASS_PYTHON%" == "x" set GRASS_PYTHON=python
