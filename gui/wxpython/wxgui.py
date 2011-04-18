71e974e4a663b0fb6b6e09c17b730cb6e7dbb0a0
Martin Landa <landa.martin@gmail.com>
2011-04-18 10:37:16 +0000

    wxGUI: fix cmd string for installing wx.psmap       (merge https://trac.osgeo.org/grass/changeset/46019 from relbr64)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@46021 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/wxgui.py
+++ gui/wxpython/wxgui.py
@@ -272,8 +272,8 @@ class GMFrame(wx.Frame):
             import psmap
         except:
             gcmd.GError(parent = self.parent,
-                        message = _("Hardcopy Map Output Utility is not available. You can install it by "
-                                    "'g.extension -s svnurl=https://svn.osgeo.org/grass/grass-addons extension=wx.psmap'"))
+                        message = _("Hardcopy Map Output Utility is not available. You can install it by %s") % \
+                            'g.extension -s svnurl=https://svn.osgeo.org/grass/grass-addons extension=wx.psmap')
             return
         
         win = psmap.PsMapFrame(parent = self)
