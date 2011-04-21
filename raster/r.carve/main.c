7c8a586debb0cdc149929527b3efdb557496e5d1
Hamish Bowman <hamish_b@yahoo.com>
2011-04-21 23:22:23 +0000

    dot @$ module desc
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@46076 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.carve/main.c
+++ raster/r.carve/main.c
@@ -73,7 +73,7 @@ int main(int argc, char **argv)
     module = G_define_module();
     G_add_keyword(_("raster"));
     G_add_keyword(_("hydrology"));
-    module->label = _("Generates stream channels");
+    module->label = _("Generates stream channels.");
     module->description = _("Takes vector stream data, transforms it "
 			    "to raster and subtracts depth from the output DEM.");
 
