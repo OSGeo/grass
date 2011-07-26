28d7e8053645f79adb8a722434ebb0ab3afb5d2b
Martin Landa <landa.martin@gmail.com>
2011-07-26 19:37:29 +0000

    wxGUI: attempt to fix #1409
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@47251 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/layertree.py
+++ gui/wxpython/gui_modules/layertree.py
@@ -845,7 +845,7 @@ class LayerTree(treemixin.DragAndDrop, CT.CustomTreeCtrl):
                 
                 render = False
                 name = None
-
+            
             if ctrl:
                 ctrlId = ctrl.GetId()
             else:
@@ -1401,7 +1401,7 @@ class LayerTree(treemixin.DragAndDrop, CT.CustomTreeCtrl):
         if not lname:
             dcmd    = self.GetPyData(item)[0]['cmd']
             lname, found = utils.GetLayerNameFromCmd(dcmd, layerType = mapLayer.GetType(),
-                                                       fullyQualified = True)
+                                                     fullyQualified = True)
             if not found:
                 return None
         
