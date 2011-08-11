89cd3b3e82fe2911f84ed7e67295f376c9cc6b69
Martin Landa <landa.martin@gmail.com>
2011-08-11 10:59:15 +0000

    rasterlib: color_remove.c: add doxygen strings
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@47554 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/raster/color_remove.c
+++ lib/raster/color_remove.c
@@ -1,26 +1,30 @@
-
-/****************************************************************************
- *
- * MODULE:       gis library
- * AUTHOR(S):    Glynn Clements <glynn@gclements.plus.com>
- * COPYRIGHT:    (C) 2007 Glynn Clements
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *****************************************************************************/
+/*!
+  \file lib/raster/color_remove.c
+ 
+  \brief Raster Library - remove color table of raster map
+  
+  (C) 2007 by the GRASS Development Team
+  
+  This program is free software under the GNU General Public License
+  (>=v2). Read the file COPYING that comes with GRASS for details.
+  
+  \author Glynn Clements
+*/
 
 #include <string.h>
 #include <stdio.h>
 #include <grass/gis.h>
 
+/*!
+  \brief Remove color table of raster map
+
+  \param name name of raster map
+  \param mapset name of mapset 
+
+  \return -1 on error
+  \return 0 color table not found
+  \return 1 on success
+*/
 int Rast_remove_colors(const char *name, const char *mapset)
 {
     char element[GMAPSET_MAX + 6];
