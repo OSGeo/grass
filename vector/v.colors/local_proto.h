f4a67c9330cf75ee81bf0c020dc594f62fc53a6c
Martin Landa <landa.martin@gmail.com>
2011-08-13 13:51:12 +0000

    v.colors: fix creating color tables when `column` given
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@47607 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.colors/local_proto.h
+++ vector/v.colors/local_proto.h
@@ -1,9 +1,10 @@
 /* scan_attr */
-int scan_attr(const struct Map_info *, int, const char *,
-	       double *, double *);
+int scan_attr(const struct Map_info *, int, const char *, const char *,
+	      struct Colors *, int *, int *);
 
 /* scan_cats */
-void scan_cats(const struct Map_info *, int, double *, double*);
+void scan_cats(const struct Map_info *, int, const char *,
+	       struct Colors *, int *, int *);
 
 /* write_rgb.c */
 void write_rgb_values(const struct Map_info *, int, const char *,
--- vector/v.colors/scan_attr.c
+++ vector/v.colors/scan_attr.c
@@ -1,25 +1,27 @@
 #include <grass/vector.h>
 #include <grass/dbmi.h>
+#include <grass/raster.h>
 #include <grass/glocale.h>
 
 #include "local_proto.h"
 
 int scan_attr(const struct Map_info *Map, int layer, const char *column_name,
-	      double *fmin, double *fmax)
+	      const char *style, struct Colors *colors, int *cmin, int *cmax)
 {
-    int ctype, is_fp, more, first;
-    double fval;
+    int ctype, is_fp, nrec, i, cat;
+    int red, grn, blu;
+    double fmin, fmax;
     
-    char buf[1024];
     struct field_info *fi;
+    struct Colors vcolors;
     dbDriver *driver;
-    dbTable *table;
-    dbColumn *column;
-    dbString stmt;
-    dbCursor cursor;
-    dbValue *value;
-     
-    *fmin = *fmax = -1;
+    dbCatVal *cv;
+    dbCatValArray cvarr;
+
+    *cmin = *cmax = -1;
+    Rast_init_colors(colors);
+    Rast_init_colors(&vcolors);
+    
     fi = Vect_get_field(Map, layer);
     if (!fi)
 	G_fatal_error(_("Database connection not defined for layer %d"),
@@ -39,45 +41,58 @@ int scan_attr(const struct Map_info *Map, int layer, const char *column_name,
 
     is_fp = ctype == DB_C_TYPE_DOUBLE;
 
-    G_snprintf(buf, 1023, "SELECT %s FROM %s", column_name, fi->table);
-    G_debug(3, "scan_attr() SQL: %s", buf);
-     
-    db_init_string(&stmt);
-    db_append_string(&stmt, buf);
+    nrec = db_select_CatValArray(driver, fi->table, fi->key, column_name,
+				 NULL, &cvarr);
+    if (nrec < 1) {
+	G_important_message(_("No data selected"));
+	return 0;
+    }
     
-    if (db_open_select_cursor(driver, &stmt, &cursor, DB_SEQUENTIAL) != DB_OK)
-	G_fatal_error(_("Unable to select data"));
-    
-    table = db_get_cursor_table(&cursor);
-    column = db_get_table_column(table, 0);
-    value = db_get_column_value(column);
-    
-    /* fetch the data */
-    first = TRUE;
-    while (TRUE) {
-	if (db_fetch(&cursor, DB_NEXT, &more) != DB_OK)
-	    return -1;
-
-	if (!more)
-	    break;
-
-	if (is_fp)
-	    fval = db_get_value_double(value);
-	else
-	    fval = db_get_value_int(value);
-
-	if (first) {
-	    *fmin = *fmax = fval;
-	    first = FALSE;
-	    continue;
-	}
-
-	if (fval <= *fmin)
-	    *fmin = fval;
-	if (fval >= *fmax)
-	    *fmax = fval;
+    /* color table for values */
+    db_CatValArray_sort_by_value(&cvarr);
+    if (is_fp) {
+	fmin = cvarr.value[0].val.d;
+	fmax = cvarr.value[cvarr.n_values-1].val.d;
+	Rast_make_fp_colors(&vcolors, style, (DCELL) fmin, (DCELL) fmax);
+    }
+    else {
+	fmin = cvarr.value[0].val.i;
+	fmax = cvarr.value[cvarr.n_values-1].val.i;
+	Rast_make_colors(&vcolors, style, (CELL) fmin, (CELL) fmax);
     }
 
+    /* color table for categories */
+    for (i = 0; i < cvarr.n_values; i++) {
+	cv = &(cvarr.value[i]);
+	cat = cv->cat;
+	if (is_fp) {
+	    if (Rast_get_d_color((const DCELL *) &(cv->val.d), &red, &grn, &blu,
+				 &vcolors) == 0) {
+		G_warning(_("No color rule defined for value %f"), cv->val.d);
+		continue;
+	    }
+	}
+	else {
+	    if (Rast_get_c_color((const CELL *) &(cv->val.i), &red, &grn, &blu,
+				 &vcolors) == 0) {
+		G_warning(_("No color rule defined for value %d"), cv->val.i);
+		continue;
+	    }
+	}
+	Rast_add_c_color_rule((const CELL*) &cat, red, grn, blu,
+			      (const CELL*) &cat, red, grn, blu, colors);
+
+	if (i == 0) {
+	    *cmin = *cmax = cat;
+	}
+	else {
+	    if (cat <= *cmin)
+		*cmin = cat;
+	    if (cat >= *cmax)
+		*cmax = cat;
+	}
+    }
+	
     db_close_database(driver);
 
     return is_fp;
