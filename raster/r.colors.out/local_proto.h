eb14c698b8b03af029a321048f9619838503622b
Martin Landa <landa.martin@gmail.com>
2011-08-14 08:45:36 +0000

    r[3].colors.out updated to use Rast_print_colors()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@47614 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.colors.out/local_proto.h
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <string.h>
-#include <grass/gis.h>
-#include <grass/raster3d.h>
-#include <grass/raster.h>
-#include <grass/glocale.h>
-
-void write_colors(struct Colors *, struct FPRange*, const char *, int);
