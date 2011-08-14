243d687d48a4f8ad237da01b4e46a6caf176e7a7
Hamish Bowman <hamish_b@yahoo.com>
2011-08-14 12:32:49 +0000

    update FSF mailing address (#1422)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@47630 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.sun/rsunlib.c
+++ raster/r.sun/rsunlib.c
@@ -22,8 +22,7 @@ email: hofierka@geomodel.sk, marcel.suri@jrc.it, suri@geomodel.sk
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the
  *   Free Software Foundation, Inc.,
- *   59 Temple Place - Suite 330,
- *   Boston, MA  02111-1307, USA.
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
 /*v. 2.0 July 2002, NULL data handling, JH */
