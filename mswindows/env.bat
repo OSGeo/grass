b69f42dfe1e88558e1b658e468da1ec33507de2e
Helmut Kudrnovsky <hkmyricaria@gmail.com>
2011-08-24 20:55:34 +0000

    attempt to fix ticket #1110
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@47871 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/env.bat
+++ mswindows/env.bat
@@ -6,10 +6,11 @@ set GRASS_WISH=wish.exe
 rem Path to the shell command
 set GRASS_SH=%GISBASE%\msys\bin\sh.exe
 
-rem Set Path to utilities (libraries and bynaries) used by GRASS
+rem Set Path to utilities (libraries and binaries) used by GRASS
 set PATH=%GISBASE%\msys\bin;%PATH%
 set PATH=%GISBASE%\extrabin;%GISBASE%\extralib;%PATH%
 set PATH=%GISBASE%\tcl-tk\bin;%GISBASE%\sqlite\bin;%GISBASE%\gpsbabel;%PATH%
+set PATH=%GISBASE%\bin;%GISBASE%\scripts;%PATH%
 
 rem Set Path to default web browser
 set GRASS_HTML_BROWSER=explorer
