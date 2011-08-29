cb83f2c21693825bae77ce162c0e3efaf4ba5208
Martin Landa <landa.martin@gmail.com>
2011-08-29 16:27:30 +0000

    gislib: update doxygen docs for get_projinfo.c
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@47942 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/get_projinfo.c
+++ lib/gis/get_projinfo.c
@@ -1,15 +1,13 @@
 /*!
- * \file get_projinfo.c
- * 
- * \brief GIS Library - Get projection info
- *
- * (C) 1999-2008 by the GRASS Development Team
- *
- * This program is free software under the GNU General Public
- * License (>=v2). Read the file COPYING that comes with GRASS
- * for details.
- *
- */
+  \file lib/gis/get_projinfo.c
+  
+  \brief GIS Library - Get projection info
+  
+  (C) 1999-2008, 2011 by the GRASS Development Team
+  
+  This program is free software under the GNU General Public License
+  (>=v2). Read the file COPYING that comes with GRASS for details.
+*/
 
 #include <unistd.h>
 #include <grass/gis.h>
@@ -18,10 +16,13 @@
 #define PERMANENT "PERMANENT"
 
 /*!
- * \brief Gets unit information for location
- *
- * \return pointer to Key_Value structure with key/value pairs
- */
+  \brief Gets units information for location
+
+  Note: Allocated Key_Value structure should be freed by
+  G_free_key_value().
+  
+  \return pointer to Key_Value structure with key/value pairs
+*/
 struct Key_Value *G_get_projunits(void)
 {
     struct Key_Value *in_units_keys;
@@ -41,10 +42,13 @@ struct Key_Value *G_get_projunits(void)
 }
 
 /*!
- * \brief Gets projection information for location
- *
- * \return pointer to Key_Value structure with key/value pairs
- */
+  \brief Gets projection information for location
+  
+  Note: Allocated Key_Value structure should be freed by
+  G_free_key_value().
+  
+  \return pointer to Key_Value structure with key/value pairs
+*/
 struct Key_Value *G_get_projinfo(void)
 {
     struct Key_Value *in_proj_keys;
