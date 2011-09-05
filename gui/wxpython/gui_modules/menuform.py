3d607dcf2c2375d6e99c8803e55c3c7186e6b71b
Martin Landa <landa.martin@gmail.com>
2011-09-05 19:31:33 +0000

    wxGUI/modeler: fix GetData()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48148 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/menuform.py
+++ gui/wxpython/gui_modules/menuform.py
@@ -219,6 +219,9 @@ class UpdateThread(Thread):
         # update reference widgets
         for uid in p['wxId-bind']:
             win = self.parent.FindWindowById(uid)
+            if not win:
+                continue
+            
             name = win.GetName()
             
             map = layer = None
