5191c8a36f4584aab4c71c106c35e6d081aef0b2
Markus Neteler <neteler@gmail.com>
2011-09-06 07:03:27 +0000

    partially revert https://trac.osgeo.org/grass/changeset/47963 to reinstate default format (keep OGR behaviour)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48159 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.out.ogr/args.c
+++ vector/v.out.ogr/args.c
@@ -44,6 +44,7 @@ void parse_args(int argc, char **argv,
     options->format->type = TYPE_STRING;
     options->format->required = YES;
     options->format->multiple = NO;
+    options->format->answer = "ESRI_Shapefile";
     options->format->options = OGR_list_write_drivers();
     options->format->description = _("Data format to write");
     
