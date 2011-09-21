b9940727fd0a1c2a3787d79129ce35c8d5f3bb9d
Soeren Gebbert <soerengebbert@googlemail.com>
2011-09-21 21:09:29 +0000

    Moved the temporal GIS code from grass.script into a separate library structure named grass.temporal.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48400 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/__init__.py
+++ lib/python/__init__.py
@@ -3,10 +3,3 @@ from db     import *
 from raster import *
 from raster3d import *
 from vector import *
-# Temporal GIS library functions
-from tgis_core import *
-from tgis_base import *
-from tgis_temporal_extent import *
-from tgis_spatial_extent import *
-from tgis_metadata import *
-from tgis_space_time_datasets import *
