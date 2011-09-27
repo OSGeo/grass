3e4c080342eb85e7fa10d00a5d94570ca56890cb
Hamish Bowman <hamish_b@yahoo.com>
2011-09-27 09:55:28 +0000

    bugfix: inch scaling was backwards
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48491 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- ps/ps.map/getgrid.c
+++ ps/ps.map/getgrid.c
@@ -18,6 +18,7 @@ static char *help[] = {
     "cross	cross_size",
     "color      color",
     "numbers    # [color]",
+    "width      #"
     ""
 };
 
@@ -125,7 +126,7 @@ int getgrid(void)
 		error(key, data, _("illegal grid width request"));
 	    }
 	    if (ch == 'i')
-		PS.grid_width = PS.grid_width / 72.0;
+		PS.grid_width = PS.grid_width * 72.0;
 	    continue;
 	}
 	error(key, data, _("illegal request (grid)"));
@@ -233,7 +234,7 @@ int getgeogrid(void)
 		error(key, data, _("illegal grid width request"));
 	    }
 	    if (ch == 'i')
-		PS.geogrid_width = PS.geogrid_width / 72.0;
+		PS.geogrid_width = PS.geogrid_width * 72.0;
 	    continue;
 	}
 	error(key, data, _("illegal request (geogrid)"));
