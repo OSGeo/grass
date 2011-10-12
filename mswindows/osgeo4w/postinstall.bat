cd796ba824d7730998f1237131715c6ee0ef9f40
Martin Landa <landa.martin@gmail.com>
2011-10-12 11:11:32 +0000

    winGRASS: cleanup start items, only one Desktop shorcut
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48751 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/postinstall.bat
+++ mswindows/osgeo4w/postinstall.bat
@@ -5,8 +5,7 @@ textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@
 textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap 
 
 mkdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\wxPython @VERSION@.lnk"   "%BATCH%" "-wxpython" \ "wxPython interface" 1 "%ICON%" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Text @VERSION@.lnk"       "%BATCH%" "-text" \ "Text interface" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Graphical Interface.lnk"   "%BATCH%" "-gui" \ "Graphical User Interface (wxGUI)" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Text Interface.lnk"  "%BATCH%" "-text" \ "Text User Interface (CLI)" 1 "%ICON%" 
 
-xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@ (wxpython).lnk" "%BATCH%" "-wxpython" \ "wxPython" 1 "%ICON%" 
-xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@ (Text).lnk" "%BATCH%" "-text" \ "Text interface" 1 "%ICON%" 
+xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk" "%BATCH%"  "-gui" \ "GRASS GUI" 1 "%ICON%" 
