2030794c4fd86b38865e680a411ec923545e308f
Martin Landa <landa.martin@gmail.com>
2011-10-12 11:20:55 +0000

    winGRASS update preremove.bat (OSGEO4w)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48754 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -1,9 +1,8 @@
-del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\wxpython.lnk" 
-del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\text.lnk" 
+del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Graphical Interface.lnk"
+del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Text Interface.lnk"
 rmdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
 
-del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@ (wxPython).lnk" 
-del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@ (Text).lnk"
+del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 
 del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap 
 del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat
