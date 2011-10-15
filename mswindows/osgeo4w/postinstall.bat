53be558ae8afabf5bd36b87e3d894cd2244ffaa2
Martin Landa <landa.martin@gmail.com>
2011-10-15 20:10:17 +0000

    winGRASS: clean up support files for osgeo4w
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48820 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/postinstall.bat
+++ mswindows/osgeo4w/postinstall.bat
@@ -2,10 +2,13 @@ set ICON=%OSGEO4W_ROOT%\apps\grass\grass-@VERSION@\etc\gui\icons\grass.ico
 set BATCH=%OSGEO4W_ROOT%\bin\grass@POSTFIX@.bat
 textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat
 textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@ 
-textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap 
+textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
 
 mkdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Graphical Interface.lnk"   "%BATCH%" "-gui" \ "Graphical User Interface (wxGUI)" 1 "%ICON%" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Text Interface.lnk"  "%BATCH%" "-text" \ "Text User Interface (CLI)" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS GUI.lnk"p   "%BATCH%" "-gui" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS Command Line.lnk"  "%BATCH%" "-text" \ "Launch GRASS GIS @VERSION@ in text mode" 1 "%ICON%" 
 
-xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk" "%BATCH%"  "-gui" \ "GRASS GUI" 1 "%ICON%" 
+xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk" "%BATCH%"  "-gui" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
+
+del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat.tmpl
+del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.tmpl
--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -1,9 +1,9 @@
-del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Graphical Interface.lnk"
-del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Text Interface.lnk"
+del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS GUI.lnk"
+del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS Command Line.lnk"
 rmdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
 
 del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap 
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
 del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat
 del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@
