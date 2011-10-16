19063ac2825b6308eae6d074e79e4eead9e46af4
Martin Landa <landa.martin@gmail.com>
2011-10-16 09:24:27 +0000

    winGRASS: fix typo in postinstall.bat
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48823 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/postinstall.bat
+++ mswindows/osgeo4w/postinstall.bat
@@ -5,7 +5,7 @@ textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@
 textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
 
 mkdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS GUI.lnk"p   "%BATCH%" "-gui" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS GUI.lnk"   "%BATCH%" "-gui" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
 xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS Command Line.lnk"  "%BATCH%" "-text" \ "Launch GRASS GIS @VERSION@ in text mode" 1 "%ICON%" 
 
 xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk" "%BATCH%"  "-gui" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
