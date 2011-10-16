f099eb963a96d00fa852c867c7550f0fcf4bf8cb
Martin Landa <landa.martin@gmail.com>
2011-10-16 09:29:56 +0000

    winGRASS: minor fixes in support scripts
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@48824 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -4,6 +4,7 @@ rmdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@"
 
 del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
+del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.py
 del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat
 del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
