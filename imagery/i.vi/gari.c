563419c54d84bf38e79509d4572bf2c6a9094df8
Yann Chemin <yann.chemin@gmail.com>
2011-11-02 20:51:27 +0000

    Added full configuration of MSAVI
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@49055 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.vi/gari.c
+++ imagery/i.vi/gari.c
@@ -2,19 +2,14 @@
 #include<math.h>
 #include<stdlib.h>
 
-    /*GARI: green atmospherically resistant vegetation index */ 
-double ga_ri(double redchan, double nirchan, double bluechan,
-	      double greenchan) 
+/*GARI: green atmospherically resistant vegetation index */ 
+double ga_ri(double redchan,double nirchan,double bluechan,double greenchan) 
 {
     double result;
-
-     {
-	result =
-	    (nirchan - (greenchan - (bluechan - redchan))) / (nirchan -
-							      (greenchan -
-							       (bluechan -
-								redchan)));
-    } return result;
+    {
+        result = (nirchan - (greenchan - (bluechan - redchan)))/(nirchan - (greenchan - (bluechan - redchan)));
+    }
+    return result;
 }
 
 
