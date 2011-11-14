60c0e38c7ce7605755e8892ba0a4ad8509605e66
Martin Landa <landa.martin@gmail.com>
2011-11-14 07:43:37 +0000

    add missing headers (after major clean up)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@49209 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ include/transform.h
@@ -0,0 +1,15 @@
+#ifndef GRASS_TRANSFORM_H
+#define GRASS_TRANSFORM_H
+
+/* This is the definition file for the libtrans functions.
+   These are the tools that move data from one coordinate system into
+   another.
+ */
+/* to give the size of the multi-dim array parameter is necessary here,
+   because DEC's cc V5.6-079 on Digital UNIX V4.0 (Rev. 878) seems to need it
+ */
+#define DIM_matrix 3
+
+#include <grass/defs/transform.h>
+
+#endif
