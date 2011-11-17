e10d1f99419e4c3db24f051a7de572e8a8855388
Martin Landa <landa.martin@gmail.com>
2011-11-17 14:53:26 +0000

    winGRASS: scripts for packaging simplified
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@49286 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -4,7 +4,6 @@ rmdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@"
 
 del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 
-del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.py
-del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat
-del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@
+del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@.bat
+del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@
 del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
