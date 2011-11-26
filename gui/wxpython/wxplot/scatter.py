e52b86170fff3d6fe651ce31d25529fbbd8aad37
Martin Landa <landa.martin@gmail.com>
2011-11-26 19:14:53 +0000

    wxGUI: cosmetics in doxygen docs
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@49368 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/wxplot/scatter.py
+++ gui/wxpython/wxplot/scatter.py
@@ -8,6 +8,7 @@ Classes:
  - scatter::ScatterToolbar
 
 (C) 2011 by the GRASS Development Team
+
 This program is free software under the GNU General Public License
 (>=v2). Read the file COPYING that comes with GRASS for details.
 
