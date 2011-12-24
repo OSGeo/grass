ea5e5344afce159d69f8f94b78b6fd0b3e74f581
Martin Landa <landa.martin@gmail.com>
2011-12-24 17:11:31 +0000

    g.extension: show make output only on verbose level (warnings/errors are shown on all verbosity levels)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@49892 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/g.extension/g.extension.py
+++ scripts/g.extension/g.extension.py
@@ -490,7 +490,7 @@ def install_extension_other():
     grass.message(_("Fetching <%s> from GRASS-Addons SVN (be patient)...") % options['extension'])
     
     os.chdir(tmpdir)
-    if grass.verbosity() == 0:
+    if grass.verbosity() <= 2:
         outdev = open(os.devnull, 'w')
     else:
         outdev = sys.stdout
@@ -533,7 +533,7 @@ def install_extension_other():
     
     os.chdir(os.path.join(tmpdir, options['extension']))
     
-    grass.message(_("Compiling <%s>...") % options['extension'])    
+    grass.message(_("Compiling..."))    
     if options['extension'] not in gui_list:
         ret = grass.call(makeCmd,
                          stdout = outdev)
@@ -548,7 +548,7 @@ def install_extension_other():
     if flags['i'] or options['extension'] in gui_list:
         return
     
-    grass.message(_("Installing <%s>...") % options['extension'])
+    grass.message(_("Installing..."))
     
     return grass.call(installCmd,
                       stdout = outdev)
