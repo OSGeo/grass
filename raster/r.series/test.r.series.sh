e476529a4eb28bfc9375317f1b6dc93d5c59eaf8
Soeren Gebbert <soerengebbert@googlemail.com>
2011-12-28 14:26:22 +0000

    Added new weighting option to r.series and created weighting tests and reference files.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@49946 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.series/test.r.series.sh
+++ raster/r.series/test.r.series.sh
@@ -13,9 +13,10 @@ r.mapcalc --o expr="prec_5 = 500"
 r.mapcalc --o expr="prec_6 = 600"
 
 TMP_FILE=`g.tempfile pid=1`
-# We create an input file containing errors. However, r.series should process the 
-# valid raster maps listed in this file.
-cat > $TMP_FILE << EOF
+TMP_FILE_WEIGHTS=`g.tempfile pid=2`
+# We create an input files containing empty lines. However, r.series should process the 
+# valid raster maps listed in the files.
+cat > "${TMP_FILE}" << EOF
 prec_1
 
 prec_2
@@ -31,6 +32,25 @@ prec_6
 
 EOF
 
+# The second file includes weights. The deafult weight pf 1.0 
+# must not be specified
+cat > "${TMP_FILE_WEIGHTS}" << EOF
+prec_1|3
+
+prec_2|1.5
+prec_3
+
+prec_4|0.75
+
+prec_5|0.6
+
+
+prec_6|0.5
+
+
+EOF
+
+
 # The first @test with map input and @precision=3
 r.series -z --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 output=test_1_prec_mean method=average
 r.series    --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 output=test_1_prec_max method=maximum
@@ -38,6 +58,7 @@ r.series -z --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 output=test_
 r.series    --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 output=test_1_prec_count method=count
 r.series -z --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 output=test_1_prec_range method=range
 r.series    --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 output=test_1_prec_sum method=sum
+
 #r.out.ascii --o input=test_1_prec_mean output=test_1_prec_mean.ref dp=3
 #r.out.ascii --o input=test_1_prec_max output=test_1_prec_max.ref dp=3
 #r.out.ascii --o input=test_1_prec_min output=test_1_prec_min.ref dp=3
@@ -45,13 +66,29 @@ r.series    --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 output=test_
 #r.out.ascii --o input=test_1_prec_range output=test_1_prec_range.ref dp=3
 #r.out.ascii --o input=test_1_prec_sum output=test_1_prec_sum.ref dp=3
 
+# Now @test the weighting option 
+r.series -z --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 weights=6,3,2,1.5,1.2,1 output=test_1_prec_mean_weight method=average
+r.series    --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 weights=6,3,2,1.5,1.2,1 output=test_1_prec_max_weight method=maximum
+r.series -z --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 weights=6,3,2,1.5,1.2,1 output=test_1_prec_min_weight method=minimum
+r.series    --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 weights=6,3,2,1.5,1.2,1 output=test_1_prec_count_weight method=count
+r.series -z --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 weights=6,3,2,1.5,1.2,1 output=test_1_prec_range_weight method=range
+r.series    --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 weights=6,3,2,1.5,1.2,1 output=test_1_prec_sum_weight method=sum
+
+#r.out.ascii --o input=test_1_prec_mean_weight output=test_1_prec_mean_weight.ref dp=3
+#r.out.ascii --o input=test_1_prec_max_weight output=test_1_prec_max_weight.ref dp=3
+#r.out.ascii --o input=test_1_prec_min_weight output=test_1_prec_min_weight.ref dp=3
+#r.out.ascii --o input=test_1_prec_count_weight output=test_1_prec_count_weight.ref dp=3
+#r.out.ascii --o input=test_1_prec_range_weight output=test_1_prec_range_weight.ref dp=3
+#r.out.ascii --o input=test_1_prec_sum_weight output=test_1_prec_sum_weight.ref dp=3
+
+
 # The second @test with file input and @precision=3
-r.series -z --o --v file=$TMP_FILE output=test_2_prec_mean method=average
-r.series    --o --v file=$TMP_FILE output=test_2_prec_max method=maximum
-r.series -z --o --v file=$TMP_FILE output=test_2_prec_min method=minimum
-r.series    --o --v file=$TMP_FILE output=test_2_prec_count method=count
-r.series -z --o --v file=$TMP_FILE output=test_2_prec_range method=range
-r.series    --o --v file=$TMP_FILE output=test_2_prec_sum method=sum
+r.series -z --o --v file="${TMP_FILE}" output=test_2_prec_mean method=average
+r.series    --o --v file="${TMP_FILE}" output=test_2_prec_max method=maximum
+r.series -z --o --v file="${TMP_FILE}" output=test_2_prec_min method=minimum
+r.series    --o --v file="${TMP_FILE}" output=test_2_prec_count method=count
+r.series -z --o --v file="${TMP_FILE}" output=test_2_prec_range method=range
+r.series    --o --v file="${TMP_FILE}" output=test_2_prec_sum method=sum
 
 #r.out.ascii --o input=test_2_prec_mean output=test_2_prec_mean.ref dp=3
 #r.out.ascii --o input=test_2_prec_max output=test_2_prec_max.ref dp=3
@@ -60,10 +97,24 @@ r.series    --o --v file=$TMP_FILE output=test_2_prec_sum method=sum
 #r.out.ascii --o input=test_2_prec_range output=test_2_prec_range.ref dp=3
 #r.out.ascii --o input=test_2_prec_sum output=test_2_prec_sum.ref dp=3
 
+# Now @test the weighting 
+r.series -z --o --v file="${TMP_FILE_WEIGHTS}" output=test_2_prec_mean_weight method=average
+r.series    --o --v file="${TMP_FILE_WEIGHTS}" output=test_2_prec_max_weight method=maximum
+r.series -z --o --v file="${TMP_FILE_WEIGHTS}" output=test_2_prec_min_weight method=minimum
+r.series    --o --v file="${TMP_FILE_WEIGHTS}" output=test_2_prec_count_weight method=count
+r.series -z --o --v file="${TMP_FILE_WEIGHTS}" output=test_2_prec_range_weight method=range
+r.series    --o --v file="${TMP_FILE_WEIGHTS}" output=test_2_prec_sum_weight method=sum
+
+#r.out.ascii --o input=test_2_prec_mean_weight output=test_2_prec_mean_weight.ref dp=3
+#r.out.ascii --o input=test_2_prec_max_weight output=test_2_prec_max_weight.ref dp=3
+#r.out.ascii --o input=test_2_prec_min_weight output=test_2_prec_min_weight.ref dp=3
+#r.out.ascii --o input=test_2_prec_count_weight output=test_2_prec_count_weight.ref dp=3
+#r.out.ascii --o input=test_2_prec_range_weight output=test_2_prec_range_weight.ref dp=3
+#r.out.ascii --o input=test_2_prec_sum_weight output=test_2_prec_sum_weight.ref dp=3
 
 # The third @test with multiple methods and outputs
 r.series    --o --v input=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 output=test_3_prec_mean,test_3_prec_max,test_3_prec_min method=average,maximum,minimum
-r.series -z --o --v file=$TMP_FILE output=test_4_prec_mean,test_4_prec_max,test_4_prec_min method=average,maximum,minimum
+r.series -z --o --v file="${TMP_FILE}" output=test_4_prec_mean,test_4_prec_max,test_4_prec_min method=average,maximum,minimum
 
 #r.out.ascii --o input=test_1_prec_mean output=test_3_prec_mean.ref dp=3
 #r.out.ascii --o input=test_1_prec_max output=test_3_prec_max.ref dp=3
