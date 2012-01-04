99108c614fe0af2dfc4f7966a9f87482e6fe6b50
Martin Landa <landa.martin@gmail.com>
2012-01-04 15:06:52 +0000

    g.extension: fix URL for WinGRASS (release/dev)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@50061 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/g.extension/g.extension.py
+++ scripts/g.extension/g.extension.py
@@ -647,7 +647,7 @@ def install_extension_win():
     ### TODO: do not use hardcoded url - http://wingrass.fsv.cvut.cz/grassXX/addonsX.X.X
     grass.message(_("Downloading precompiled GRASS Addons <%s>...") % options['extension'])
     url = "http://wingrass.fsv.cvut.cz/grass%s%s/addons" % (version[0], version[1])
-    if version[2][1:] != 'svn':
+    if version[2][-3:] != 'svn':
         url += '%s.%s.%s' % (version[0], version[1], version[2])
     grass.debug("url=%s" % url, 1)
     
