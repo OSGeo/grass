5e0222ec270245cc233a13aaa33f04e5e7261735
Hamish Bowman <hamish_b@yahoo.com>
2012-01-12 02:51:33 +0000

    basic collection of profile and md5sum notes. expect for formalize script and result files later
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@50140 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ vector/v.surf.rst/test_suite/profile.sh
@@ -0,0 +1,33 @@
+# nc2008/grass7 mapset
+g.region n=221230 s=220816 w=639090 e=639530 res=0.5 -p
+CMD="v.surf.rst in=lidar_test -z elev=omp_test.rst --o"
+time $CMD
+
+valgrind --tool=callgrind --trace-children=yes $CMD
+kcachegrind callgrind.out.1196
+
+
+x% of module time is spent in fn()
+
+
+
+ r.out.ascii omp_test.rst.g7omp6b dp=6 | md5sum
+32d2890afa57354d0d0e4a69b507e81e
+
+
+time $CMD
+=========
+
+orig serial run:
+res=2
+real    0m46.088s
+user    0m46.027s
+sys     0m0.064s
+res=0.5
+real    1m1.390s
+user    1m1.312s
+sys     0m0.080s
+
+--raster map not found?
+
+
