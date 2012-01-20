6047e83fb310b33972f6c7444934b91986b406ee
Markus Neteler <neteler@gmail.com>
2012-01-20 21:56:03 +0000

    typos discovered by Vincent Bain fixed
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@50335 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/core/render.py
+++ gui/wxpython/core/render.py
@@ -1326,7 +1326,7 @@ class Map(object):
         self.overlays.append(overlay)
         
         if l_render and command != '' and not overlay.Render():
-            raise GException(_("Unable render overlay <%s>.") % 
+            raise GException(_("Unable to render overlay <%s>.") % 
                              name)
         
         return self.overlays[-1]
@@ -1365,7 +1365,7 @@ class Map(object):
             overlay.SetOpacity(kargs['opacity'])
         
         if render and overlay.GetCmd() != [] and not overlay.Render():
-            raise GException(_("Unable render overlay <%s>") % 
+            raise GException(_("Unable to render overlay <%s>.") % 
                              name)
         
         return overlay
