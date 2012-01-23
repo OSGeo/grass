16e3b7936622d4a0cbf1dd085ffb833e23cc5f43
Martin Landa <landa.martin@gmail.com>
2012-01-23 13:06:34 +0000

    winGRASS: show version in menu items (to avoid mismatch when more versions is installed)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@50391 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/postinstall.bat
+++ mswindows/osgeo4w/postinstall.bat
@@ -6,8 +6,8 @@ textreplace -std -t "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@
 textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
 
 mkdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS GUI.lnk"   "%BATCH%" "-gui" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS Command Line.lnk"  "%BATCH%" "-text" \ "Launch GRASS GIS @VERSION@ in text mode" 1 "%ICON_CMD%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS @VERSION@ GUI.lnk"   "%BATCH%" "-gui" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS @VERSION@ Command Line.lnk"  "%BATCH%" "-text" \ "Launch GRASS GIS @VERSION@ in text mode" 1 "%ICON_CMD%" 
 
 xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk" "%BATCH%"  "-gui" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
 
