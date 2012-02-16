2b743926c10ac20e05401a1ed24c7b862a27d411
Hamish Bowman <hamish_b@yahoo.com>
2012-02-16 05:25:28 +0000

    add relocated MANPATH to startup routine
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@50825 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/init/grass.py
+++ lib/init/grass.py
@@ -341,6 +341,26 @@ def set_paths():
     # Set PYTHONPATH to find GRASS Python modules
     path_prepend(gfile('etc', 'python'), 'PYTHONPATH')
     
+    # set path for the GRASS man pages
+    grass_man_path = os.path.join(gisbase, 'docs', 'man')
+    man_path = os.getenv('MANPATH')
+    if man_path:
+	path_prepend(grass_man_path, 'MANPATH')
+    else:
+	try:
+	    p = Popen(['manpath'], stdout = subprocess.PIPE)
+	    s = p.stdout.read()
+	    p.wait()
+	    sys_man_path = s.strip()
+	except:
+	    pass
+
+	if sys_man_path:
+	    os.environ['MANPATH'] = sys_man_path
+	    path_prepend(grass_man_path, 'MANPATH')
+	else:
+	    os.environ['MANPATH'] = grass_man_path
+
     # Add .py (Python) to list of executable extensions to search for in MS-Windows PATH
     if windows:
 	path_append('.PY', 'PATHEXT')
