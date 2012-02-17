565b6579813665d4dc02b645d04e1f6609c389f3
Martin Landa <landa.martin@gmail.com>
2012-02-17 20:56:42 +0000

    g.extension: simplify wingrass-addons url
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@50847 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/g.extension/g.extension.py
+++ scripts/g.extension/g.extension.py
@@ -652,9 +652,6 @@ def install_extension_win(name):
     ### TODO: do not use hardcoded url - http://wingrass.fsv.cvut.cz/grassXX/addonsX.X.X
     grass.message(_("Downloading precompiled GRASS Addons <%s>...") % options['extension'])
     url = "http://wingrass.fsv.cvut.cz/grass%s%s/addons" % (version[0], version[1])
-    
-    if version[2][-3:] != 'svn':
-        url += '%s.%s.%s' % (version[0], version[1], version[2])
     grass.debug("url=%s" % url, 1)
     
     try:
