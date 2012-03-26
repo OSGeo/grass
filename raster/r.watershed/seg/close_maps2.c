530b30fd782bcf691f852560a7608d58e2413fd2
Markus Metz <markus.metz.giswork@gmail.com>
2012-03-26 14:19:20 +0000

    r.watershed: add safety check
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@51162 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.watershed/seg/close_maps2.c
+++ raster/r.watershed/seg/close_maps2.c
@@ -33,9 +33,14 @@ int close_array_seg(void)
 	max = n_basins;
 	G_debug(1, "%d basins created", max);
 	Rast_init_colors(&colors);
-	Rast_make_random_colors(&colors, 1, max);
+	if (max > 0)
+	    Rast_make_random_colors(&colors, 1, max);
+	else {
+	    G_warning(_("No basins were created. Verify threshold and region settings."));
+	    Rast_make_random_colors(&colors, 1, 2);
+	}
 
-	if (max < 1000) {
+	if (max < 1000 && max > 0) {
 	    Rast_set_c_color((CELL) 0, 0, 0, 0, &colors);
 	    r = 1;
 	    incr = 0;
