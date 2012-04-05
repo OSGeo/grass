4f4e194e226f57dbbdfe8f9a38fb1590f7adab19
Soeren Gebbert <soerengebbert@googlemail.com>
2012-04-05 21:10:55 +0000

    Using new naming scheme
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@51279 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ temporal/t.rast.series/test.t.rast.series.sh
@@ -0,0 +1,27 @@
+# We need to set a specific region in the
+# @preprocess step of this test. 
+# The region setting should work for UTM and LL test locations
+g.region s=0 n=80 w=0 e=120 b=0 t=50 res=10 res3=10 -p3
+
+r.mapcalc --o expr="prec_1 = 100"
+r.mapcalc --o expr="prec_2 = 200"
+r.mapcalc --o expr="prec_3 = 300"
+r.mapcalc --o expr="prec_4 = 400"
+r.mapcalc --o expr="prec_5 = 500"
+r.mapcalc --o expr="prec_6 = 600"
+
+# @test
+t.create --v --o type=strds temporaltype=absolute output=precip_abs title="A test" descr="A test"
+
+t.register --v type=rast input=precip_abs maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="1 months"
+
+t.rast.series --o input=precip_abs method=average output=prec_average where="start_time > '2001-03-01'"
+t.rast.series --o -t input=precip_abs method=maximum output=prec_max order=start_time
+t.rast.series --o -t input=precip_abs method=sum output=prec_sum
+
+t.unregister --v type=rast maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6
+t.remove --v type=strds input=precip_abs
+
+r.info prec_average
+t.info type=rast input=prec_max
+t.info type=rast input=prec_sum
