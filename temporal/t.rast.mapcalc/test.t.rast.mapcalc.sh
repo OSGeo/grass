3f38084a2fe1337e82fc8252d1c1e75336854ea9
Soeren Gebbert <soerengebbert@googlemail.com>
2012-04-05 21:02:22 +0000

    Using new naming scheme
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@51275 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ temporal/t.rast.mapcalc/test.t.rast.mapcalc.sh
@@ -0,0 +1,37 @@
+# Test for t.rast.mapcalc 
+
+# We need to set a specific region in the
+# @preprocess step of this test. We generate
+# raster with r.mapcalc and create several space time raster datasets
+# The region setting should work for UTM and LL test locations
+g.region s=0 n=80 w=0 e=120 b=0 t=50 res=10 res3=10 -p3
+
+r.mapcalc --o expr="prec_1 = rand(0, 550)"
+r.mapcalc --o expr="prec_2 = rand(0, 450)"
+r.mapcalc --o expr="prec_3 = rand(0, 320)"
+r.mapcalc --o expr="prec_4 = rand(0, 510)"
+r.mapcalc --o expr="prec_5 = rand(0, 300)"
+r.mapcalc --o expr="prec_6 = rand(0, 650)"
+
+t.create --o type=strds temporaltype=absolute output=precip_abs1 title="A test" descr="A test"
+t.create --o type=strds temporaltype=absolute output=precip_abs2 title="A test" descr="A test"
+t.register -i type=rast input=precip_abs1 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="3 months"
+t.register type=rast input=precip_abs2 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6
+
+t.info precip_abs1
+t.info precip_abs2
+
+# The first @test
+t.rast.mapcalc --o --v -n inputs=precip_abs1,precip_abs2 output=precip_abs3 \
+           expression=" precip_abs1 + precip_abs2" base=new_prec \
+           method=equal nprocs=6
+t.info type=strds input=precip_abs3
+
+t.rast.mapcalc --o --v -s inputs=precip_abs1,precip_abs2,precip_abs3 output=precip_abs4 \
+           expression=" (precip_abs1 + precip_abs2) / precip_abs3@PERMANENT " base=new_prec \
+           method=equal nprocs=6
+t.info type=strds input=precip_abs4
+
+t.unregister type=rast maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6
+t.unregister type=rast maps=new_prec_1,new_prec_2,new_prec_3,new_prec_4,new_prec_5,new_prec_6
+t.remove type=strds input=precip_abs1,precip_abs2,precip_abs3,precip_abs4
