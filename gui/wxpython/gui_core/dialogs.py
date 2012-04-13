40f0a7fb016f15af18ee583d50a21a5dbb4a6b5b
Martin Landa <landa.martin@gmail.com>
2012-04-13 18:21:13 +0000

    wxGUI: import/link - report a message if no layers selected
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@51423 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_core/dialogs.py
+++ gui/wxpython/gui_core/dialogs.py
@@ -1759,6 +1759,11 @@ class GdalImportDialog(ImportDialog):
         """!Import/Link data (each layes as separate vector map)"""
         self.commandId = -1
         data = self.list.GetLayers()
+        if not data:
+            GMessage(_("No layers selected. Operation canceled."),
+                     parent = self)
+            return
+        
         dsn  = self.dsnInput.GetDsn()
         ext  = self.dsnInput.GetFormatExt()
         
