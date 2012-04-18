9fca8c824f2e39f85fa94913dd60ad38dbc89a6b
Markus Metz <markus.metz.giswork@gmail.com>
2012-04-18 17:05:00 +0000

    v.out.vtk: port -n flag to trunk
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@51473 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.out.vtk/local_proto.h
+++ vector/v.out.vtk/local_proto.h
@@ -23,7 +23,7 @@ extern double y_extent;
 
 /*Write the vtk output */
 int write_vtk(FILE * ascii, struct Map_info *, int layer, int *types,
-	      int typenum, int dp, double scale);
+	      int typenum, int dp, double scale, int numatts, int labels );
 /*Write the VTK header */
 int write_vtk_head(FILE * ascii, struct Map_info *Map);
 
