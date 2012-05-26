2f15d01a1dfd1e2ba9d73a89b3872825456beb5c
Soeren Gebbert <soerengebbert@googlemail.com>
2012-05-26 12:43:48 +0000

    Using temporal information to scale the z-axis of space time voxel cubes.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@51779 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- temporal/t.rast.to.rast3/test.t.rast.to.rast3.sh
+++ temporal/t.rast.to.rast3/test.t.rast.to.rast3.sh
@@ -3,7 +3,7 @@
 # We need to set a specific region in the
 # @preprocess step of this test.
 # The region setting should work for UTM and LL test locations
-g.region s=0 n=80 w=0 e=120 b=0 t=1 res=10 res3=1 -p3
+g.region s=0 n=80 w=0 e=120 b=0 t=1 res=10 res3=10 -p3
 
 r.mapcalc --o expr="prec_1 = 100"
 r.mapcalc --o expr="prec_2 = 200"
@@ -18,14 +18,49 @@ r.mapcalc --o expr="prec_6 = 600"
 t.create --o type=strds temporaltype=absolute output=precip_abs title="A test" descr="A test"
 t.create --o type=strds temporaltype=relative output=precip_rel title="A test" descr="A test"
 
-t.register --o --v -i type=rast input=precip_abs maps=prec_1,prec_2,prec_3 start="2001-01-01" increment="1 months"
+t.register --o --v -i type=rast input=precip_abs maps=prec_1,prec_2,prec_3 start="2001-01-01" increment="3 years"
 t.info type=strds input=precip_abs
 
 t.rast.to.rast3 --o input=precip_abs output=precipitation
 t.info type=rast3d input=precipitation
 r3.info precipitation
 
-t.register --o --v -i type=rast input=precip_rel maps=prec_4,prec_5,prec_6 start=0 increment=100 unit=years
+t.register --o --v -i type=rast input=precip_abs maps=prec_1,prec_2,prec_3 start="2001-01-01" increment="2 months"
+t.info type=strds input=precip_abs
+
+t.rast.to.rast3 --o input=precip_abs output=precipitation
+t.info type=rast3d input=precipitation
+r3.info precipitation
+
+t.register --o --v -i type=rast input=precip_abs maps=prec_1,prec_2,prec_3 start="2001-01-01" increment="8 days"
+t.info type=strds input=precip_abs
+
+t.rast.to.rast3 --o input=precip_abs output=precipitation
+t.info type=rast3d input=precipitation
+r3.info precipitation
+
+t.register --o --v -i type=rast input=precip_abs maps=prec_1,prec_2,prec_3 start="2001-01-01" increment="6 hours"
+t.info type=strds input=precip_abs
+
+t.rast.to.rast3 --o input=precip_abs output=precipitation
+t.info type=rast3d input=precipitation
+r3.info precipitation
+
+t.register --o --v -i type=rast input=precip_abs maps=prec_1,prec_2,prec_3 start="2001-01-01" increment="30 minutes"
+t.info type=strds input=precip_abs
+
+t.rast.to.rast3 --o input=precip_abs output=precipitation
+t.info type=rast3d input=precipitation
+r3.info precipitation
+
+t.register --o --v -i type=rast input=precip_abs maps=prec_1,prec_2,prec_3 start="2001-01-01" increment="1 seconds"
+t.info type=strds input=precip_abs
+
+t.rast.to.rast3 --o input=precip_abs output=precipitation
+t.info type=rast3d input=precipitation
+r3.info precipitation
+
+t.register --o --v -i type=rast input=precip_rel maps=prec_4,prec_5,prec_6 start=1000 increment=100 unit=years
 t.info type=strds input=precip_rel
 
 t.rast.to.rast3 --o input=precip_rel output=precipitation
