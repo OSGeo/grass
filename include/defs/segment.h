d1ca578821996b3cd1970b475ed5f1b8938d1c35
Markus Metz <markus.metz.giswork@gmail.com>
2012-06-03 12:59:34 +0000

    segment lib: update headers
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@51949 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/defs/segment.h
+++ include/defs/segment.h
@@ -1,23 +1,16 @@
 #ifndef GRASS_SEGMENTDEFS_H
 #define GRASS_SEGMENTDEFS_H
 
-int segment_address(const SEGMENT *, off_t, off_t, int *, int *);
-int segment_address_fast(const SEGMENT *, off_t, off_t, int *, int *);
-int segment_address_slow(const SEGMENT *, off_t, off_t, int *, int *);
+int segment_open(SEGMENT *, char *, off_t, off_t, int, int, int, int);
+int segment_close(SEGMENT *);
 int segment_flush(SEGMENT *);
 int segment_format(int, off_t, off_t, int, int, int);
 int segment_format_nofill(int, off_t, off_t, int, int, int);
 int segment_get(SEGMENT *, void *, off_t, off_t);
 int segment_get_row(const SEGMENT *, void *, off_t);
 int segment_init(SEGMENT *, int, int);
-int segment_pagein(SEGMENT *, int);
-int segment_pageout(SEGMENT *, int);
 int segment_put(SEGMENT *, const void *, off_t, off_t);
 int segment_put_row(const SEGMENT *, const void *, off_t);
 int segment_release(SEGMENT *);
-int segment_seek(const SEGMENT *, int, int);
-int segment_seek_fast(const SEGMENT *, int, int);
-int segment_seek_slow(const SEGMENT *, int, int);
-int segment_setup(SEGMENT *);
 
 #endif /* GRASS_SEGMENTDEFS_H */
