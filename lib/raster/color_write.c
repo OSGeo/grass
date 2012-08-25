29b6f4a80da3f674baa58ff03bb7086799636a42
Markus Metz <markus.metz.giswork@gmail.com>
2012-08-25 11:03:08 +0000

    fix GRASS_EPSILON usage
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@52885 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/raster/color_write.c
+++ lib/raster/color_write.c
@@ -246,10 +246,16 @@ static void format_min(char *str, double dval)
     double dtmp;
 
     sprintf(str, "%.15g", dval);
+    /* Note that G_trim_decimal() does not trim e.g. 1.0000000e-20 */
     G_trim_decimal(str);
+
     sscanf(str, "%lf", &dtmp);
-    if (dtmp != dval) {  /* if  no zeros after decimal point were trimmed */
-	sprintf(str, "%.15g", dval - GRASS_EPSILON);
+    if (dtmp != dval) {  /* if no zeros after decimal point were trimmed */
+	/* lower dval by fraction of GRASS_EPSILON */
+	if (dval > 0)
+	    sprintf(str, "%.15g", dval * (1 - GRASS_EPSILON));
+	else
+	    sprintf(str, "%.15g", dval * (1 + GRASS_EPSILON));
     }
 }
 
@@ -259,9 +265,14 @@ static void format_max(char *str, double dval)
     double dtmp;
 
     sprintf(str, "%.15g", dval);
+    /* Note that G_trim_decimal() does not trim e.g. 1.0000000e-20 */
     G_trim_decimal(str);
     sscanf(str, "%lf", &dtmp);
     if (dtmp != dval) {  /* if  no zeros after decimal point were trimmed */
-	sprintf(str, "%.15g", dval + GRASS_EPSILON);
+	/* increase dval by fraction of GRASS_EPSILON */
+	if (dval > 0)
+	    sprintf(str, "%.15g", dval * (1 + GRASS_EPSILON));
+	else
+	    sprintf(str, "%.15g", dval * (1 - GRASS_EPSILON));
     }
 }
