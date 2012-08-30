457ffa2c62fa06063a3ed4c35867f06f0bc458e9
Anna Petrášová <kratochanna@gmail.com>
2012-08-30 15:45:48 +0000

    wxGUI/forms: check if event is not None
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@52994 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_core/forms.py
+++ gui/wxpython/gui_core/forms.py
@@ -1812,11 +1812,12 @@ class CmdPanel(wx.Panel):
 
         self.Layout()
 
-        # skip is needed for wx.Notebook on Windows
-        event.Skip()
-        # this is needed for dialogs launched from layer manager
-        # event is somehow propagated?
-        event.StopPropagation()
+        if event:
+            # skip is needed for wx.Notebook on Windows
+            event.Skip()
+            # this is needed for dialogs launched from layer manager
+            # event is somehow propagated?
+            event.StopPropagation()
 
     def OnColorChange(self, event):
         myId = event.GetId()
