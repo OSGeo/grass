OSGeo/grass

commit 5e9c79715df08494e50ccc54a5b781e4555c2d11

wxGUI: add right mapset to map names in command line (merge from releasebranch, https://trac.osgeo.org/grass/changeset/53241)
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@53243 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/core/utils.py
@@ -132,30 +132,34 @@ def GetLayerNameFromCmd(dcmd, fullyQualified = False, param = None,
         if len(params) < 1:
             return mapname, False
 
-        mapname = params[0][2]
-        mapset = ''
-        if fullyQualified and '@' not in mapname:
-            if layerType in ('raster', 'vector', '3d-raster', 'rgb', 'his'):
-                try:
-                    if layerType in ('raster', 'rgb', 'his'):
-                        findType = 'cell'
-                    else:
-                        findType = layerType
-                    mapset = grass.find_file(mapname, element = findType)['mapset']
-                except AttributeError, e: # not found
-                    return '', False
-                if not mapset:
-                    found = False
-            else:
-                mapset = grass.gisenv()['MAPSET']
+        # need to add mapset for all maps
+        mapsets = {}
+        for i, p, v in params:
+            mapname = v
+            mapset = ''
+            if fullyQualified and '@' not in mapname:
+                if layerType in ('raster', 'vector', '3d-raster', 'rgb', 'his'):
+                    try:
+                        if layerType in ('raster', 'rgb', 'his'):
+                            findType = 'cell'
+                        else:
+                            findType = layerType
+                        mapset = grass.find_file(mapname, element = findType)['mapset']
+                    except AttributeError, e: # not found
+                        return '', False
+                    if not mapset:
+                        found = False
+                else:
+                    mapset = grass.gisenv()['MAPSET']
+            mapsets[i] = mapset
             
         # update dcmd
         for i, p, v in params:
             if p == 'layer':
                 continue
             dcmd[i] = p + '=' + v
-            if mapset:
-                dcmd[i] += '@' + mapset
+            if i in mapsets and mapsets[i]:
+                dcmd[i] += '@' + mapsets[i]
         
         maps = list()
         ogr = False

commit 037e0261faa388e646509d1afa239b093cf66541

wxGUI: fix bug introduced by r53243
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@53261 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/core/utils.py
@@ -135,6 +135,8 @@ def GetLayerNameFromCmd(dcmd, fullyQualified = False, param = None,
         # need to add mapset for all maps
         mapsets = {}
         for i, p, v in params:
+            if p == 'layer':
+                continue
             mapname = v
             mapset = ''
             if fullyQualified and '@' not in mapname: