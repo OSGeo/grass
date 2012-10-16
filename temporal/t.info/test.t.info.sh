d1ab0ec971948c9e50ee1c32c92252d39ae0c4a4
Soeren Gebbert <soerengebbert@googlemail.com>
2012-10-16 15:03:44 +0000

    Added more documentation and corrected tests
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@53429 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- temporal/t.info/test.t.info.sh
+++ temporal/t.info/test.t.info.sh
@@ -15,6 +15,8 @@ v.random --o -z output=lidar_abs_1 n=20 zmin=0 zmax=100 column=height
 v.random --o -z output=lidar_abs_2 n=20 zmin=0 zmax=100 column=height
 
 # The first @test
+t.info -s
+t.info -sg
 
 t.create --o type=strds temporaltype=absolute output=precip_abs1 title="A test" descr="A test"
 t.create --o type=strds temporaltype=absolute output=precip_abs2 title="A test" descr="A test"
@@ -25,11 +27,11 @@ t.create --o type=str3ds temporaltype=absolute output=precip_abs2 title="A test"
 t.create --o type=stvds temporaltype=absolute output=lidar_abs_ds1 title="A test" descr="A test"
 t.create --o type=stvds temporaltype=absolute output=lidar_abs_ds2 title="A test" descr="A test"
 
-t.register type=rast -i input=precip_abs1 maps=prec_1,prec_2 start="2001-01-01" increment="20 years"
+t.register --o type=rast -i input=precip_abs1 maps=prec_1,prec_2 start="2001-01-01" increment="20 years"
 t.info type=strds input=precip_abs1
 t.info -g type=strds input=precip_abs1
 
-t.register type=rast -i input=precip_abs2 maps=prec_1,prec_2 start="2001-01-01" increment="20 years"
+t.register --o type=rast -i input=precip_abs2 maps=prec_1,prec_2 start="2001-01-01" increment="20 years"
 t.info type=strds input=precip_abs2
 t.info -g type=strds input=precip_abs2
 t.info type=rast input=prec_1
@@ -38,11 +40,11 @@ t.info type=rast input=prec_2
 t.info -g type=rast input=prec_2
 
 
-t.register type=rast3d -i input=precip_abs1 maps=prec_1,prec_2 start="2001-01-01" increment="20 years"
+t.register --o type=rast3d -i input=precip_abs1 maps=prec_1,prec_2 start="2001-01-01" increment="20 years"
 t.info type=str3ds input=precip_abs1
 t.info -g type=str3ds input=precip_abs1
 
-t.register type=rast3d -i input=precip_abs2 maps=prec_1,prec_2 start="2001-01-01" increment="20 years"
+t.register --o type=rast3d -i input=precip_abs2 maps=prec_1,prec_2 start="2001-01-01" increment="20 years"
 t.info type=str3ds input=precip_abs2
 t.info -g type=str3ds input=precip_abs2
 t.info type=rast3 input=prec_1
@@ -50,11 +52,11 @@ t.info -g type=rast3 input=prec_1
 t.info type=rast3 input=prec_2
 t.info -g type=rast3 input=prec_2
 
-t.register type=vect --v -i input=lidar_abs_ds1 maps=lidar_abs_1,lidar_abs_2 start="2001-01-01" increment="20 years"
+t.register --o type=vect --v -i input=lidar_abs_ds1 maps=lidar_abs_1,lidar_abs_2 start="2001-01-01" increment="20 years"
 t.info type=stvds input=lidar_abs_ds1
 t.info -g type=stvds input=lidar_abs_ds1
 
-t.register type=vect --v -i input=lidar_abs_ds2 maps=lidar_abs_1,lidar_abs_2 start="2001-01-01" increment="20 years"
+t.register --o type=vect --v -i input=lidar_abs_ds2 maps=lidar_abs_1,lidar_abs_2 start="2001-01-01" increment="20 years"
 t.info type=stvds input=lidar_abs_ds2
 t.info -g type=stvds input=lidar_abs_ds2
 t.info type=vect input=lidar_abs_1
