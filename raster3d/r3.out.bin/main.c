d251a6f701e73bbf5fc48d0018e941c520dc1d68
Martin Landa <landa.martin@gmail.com>
2012-11-14 17:33:40 +0000

    r3.in/out.bin: cosmetics - fix keywords and description
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@53819 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster3d/r3.out.bin/main.c
+++ raster3d/r3.out.bin/main.c
@@ -202,7 +202,7 @@ int main(int argc, char *argv[]) {
 	G_gisinit(argv[0]);
 
 	module = G_define_module();
-	G_add_keyword(_("3D raster"));
+	G_add_keyword(_("raster3d"));
 	G_add_keyword(_("export"));
 	module->description = _("Exports a GRASS 3D raster map to a binary array.");
 
