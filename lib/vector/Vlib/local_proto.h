491eed2fdf5d127b13792572c328622ad76a61c5
Martin Landa <landa.martin@gmail.com>
2012-11-28 09:01:36 +0000

    vlib/PostGIS Topo: implement deleting faces (work in progress)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@54085 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/vector/Vlib/local_proto.h
+++ lib/vector/Vlib/local_proto.h
@@ -4,6 +4,7 @@
 #include <grass/vector.h>
 
 void V2__add_line_to_topo_nat(struct Map_info *, int ,
-                              const struct line_pnts *, const struct line_cats *);
+                              const struct line_pnts *, const struct line_cats *,
+                              int (*external_routine) (const struct Map_info *, int));
 
 #endif /* PG_LOCAL_PROTO_H__ */
