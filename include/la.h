842fcdc602220025bd5008529f5d3f58c8278701
Markus Neteler <neteler@gmail.com>
2012-12-08 13:11:42 +0000

    added to make Ubuntu happy (from https://bugs.launchpad.net/ubuntu/+source/gcc-3.4/+bug/61623/comments/3)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@54247 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/la.h
+++ include/la.h
@@ -31,6 +31,10 @@
 #include <stdio.h>
 
 #ifdef HAVE_G2C_H
+typedef int __g77_integer;
+typedef unsigned int __g77_uinteger;
+typedef long int __g77_longint;
+typedef unsigned long int __g77_ulongint;
 #include <g2c.h>
 #else /* for gcc4+ */
 typedef int integer;
