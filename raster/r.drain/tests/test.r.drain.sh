60db2a969e288f3ac1c667a37f4ee835302208d6
Markus Neteler <neteler@gmail.com>
2012-12-20 13:00:26 +0000

    trying FP map to no avail
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@54361 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.drain/tests/test.r.drain.sh
+++ raster/r.drain/tests/test.r.drain.sh
@@ -11,8 +11,13 @@ d.mon wx0
 sleep 2
 d.rast.num testascii
 
-r.drain testascii out=drain coord=638442.5,220548.5
-d.rast -o drain
+r.drain input=testascii output=drain coord=638442.5,220548.5
+d.rast drain
+
+d.erase
+r.mapcalc "testascii_float = float(testascii)"
+r.drain input=testascii_float output=drain coord=638442.5,220548.5
+d.rast drain
 
 # for statistical test results:
 # - verify number of resulting pixels (r.univar -g)
