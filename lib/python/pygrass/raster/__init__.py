8ad0b8215aa79911583c20c462aa75e35865ab52
Pietro Zambelli <peter.zamb@gmail.com>
2013-01-25 13:24:13 +0000

    Fix Attribute error on RasterNumpy class
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@54768 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/pygrass/raster/__init__.py
+++ lib/python/pygrass/raster/__init__.py
@@ -548,7 +548,7 @@ class RasterNumpy(np.memmap, RasterAbstractBase):
 
     def __init__(self, name, *args, **kargs):
         ## Private attribute `_fd` that return the file descriptor of the map
-        #self._fd = None
+        self._fd = None
         rows, cols = self.rows, self.cols
         RasterAbstractBase.__init__(self, name)
         self._rows, self._cols = rows, cols
