c823f10284c573ff327e6bf509b88ced73b3b1bc
Anna Petrášová <kratochanna@gmail.com>
2013-02-03 09:39:53 +0000

    wxNviz: set minimum line width to 1 (merge from release branch, r54853)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@54855 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/nviz/tools.py
+++ gui/wxpython/nviz/tools.py
@@ -1223,7 +1223,7 @@ class NvizToolWindow(FN.FlatNotebook):
         
         width = wx.SpinCtrl(parent = panel, id = wx.ID_ANY, size = (65, -1),
                             initial = 1,
-                            min = 0,
+                            min = 1,
                             max = 100)
         width.SetValue(1)
         self.win['vector']['lines']['width'] = width.GetId()
