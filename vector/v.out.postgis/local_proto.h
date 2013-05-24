9830c8f631498ed001b17f1c2f1b798ec3dbc7df
Martin Landa <landa.martin@gmail.com>
2013-05-24 11:45:07 +0000

    v.out.postgis: add 'type' parameter                update manual - add note about 3D data support
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@56397 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.out.postgis/local_proto.h
+++ vector/v.out.postgis/local_proto.h
@@ -4,7 +4,7 @@
 #include <grass/vector.h>
 
 struct params {
-    struct Option *input, *layer, *dsn, *olayer, *opts, *olink;
+    struct Option *input, *layer, *dsn, *olayer, *opts, *olink, *type;
 };
 
 struct flags {
