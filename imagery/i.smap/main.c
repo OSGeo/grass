baae5ca743cd0000bbe22672c40e6b9699397b5f
Markus Neteler <neteler@gmail.com>
2013-05-26 18:31:09 +0000

    keyword improvements
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@56425 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.smap/main.c
+++ imagery/i.smap/main.c
@@ -38,6 +38,7 @@ int main(int argc, char *argv[])
     G_add_keyword(_("imagery"));
     G_add_keyword(_("classification"));
     G_add_keyword(_("supervised"));
+    G_add_keyword(_("segmentation"));
     G_add_keyword(_("SMAP"));
     module->description =
 	_("Performs contextual image classification "
