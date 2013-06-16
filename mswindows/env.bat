573a43543ff674764d074cb5a549c2cbdda232cb
Hamish Bowman <hamish_b@yahoo.com>
2013-06-16 02:41:01 +0000

    use locally installed python.exe by default to avoid out-of-sync breakage vs .grass's PYTHONPATH, tcl/tk no more
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@56719 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/env.bat
+++ mswindows/env.bat
@@ -1,6 +1,6 @@
 rem Environmental variables for GRASS stand-alone installer
 
-set GRASS_PYTHON=python
+set GRASS_PYTHON=%GISBASE%\extrabin\python.exe
 set GRASS_PROJSHARE=%GISBASE%\proj
 set GRASS_HTML_BROWSER=explorer
 
@@ -11,4 +11,4 @@ set GEOTIFF_CSV=%GISBASE%\share\epsg_csv
 
 set PATH=%GISBASE%\msys\bin;%PATH%
 set PATH=%GISBASE%\extrabin;%GISBASE%\extralib;%PATH%
-set PATH=%GISBASE%\tcl-tk\bin;%GISBASE%\sqlite\bin;%PATH%
+set PATH=%GISBASE%\sqlite\bin;%PATH%
