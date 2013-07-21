19aaa3a50c490752c23ca5f1257dbe875b911c84
Glynn Clements <glynn@gclements.plus.com>
2013-07-21 15:59:08 +0000

    Add format_arg attribute to G_gettext()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@57238 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/defs/glocale.h
+++ include/defs/glocale.h
@@ -2,6 +2,6 @@
 #define GRASS_GLOCALEDEFS_H
 
 extern void G_init_locale(void);
-extern char *G_gettext(const char *, const char *);
+extern char *G_gettext(const char *, const char *) __attribute__((format_arg (2)));
 
 #endif
