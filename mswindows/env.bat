fe8f7386c71e9df7a00faeab77156d4d58689f57
Martin Landa <landa.martin@gmail.com>
2013-08-01 16:15:31 +0000

    wingrass: major clean up of standalone installer
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@57344 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/env.bat
+++ mswindows/env.bat
@@ -1,14 +1,14 @@
 rem Environmental variables for GRASS stand-alone installer
 
-set GRASS_PYTHON=%GISBASE%\extrabin\python.exe
-set GRASS_PROJSHARE=%GISBASE%\proj
 set GRASS_HTML_BROWSER=explorer
 
+set GRASS_PYTHON=%GISBASE%\extrabin\python.exe
 set PYTHONHOME=%GISBASE%\Python27
+
+set GRASS_PROJSHARE=%GISBASE%\proj
 set GDAL_DATA=%GISBASE%\share\gdal
-set PROJ_LIB=%GISBASE%\proj
 set GEOTIFF_CSV=%GISBASE%\share\epsg_csv
 
 set PATH=%GISBASE%\msys\bin;%PATH%
 set PATH=%GISBASE%\extrabin;%GISBASE%\extralib;%PATH%
-set PATH=%GISBASE%\sqlite\bin;%PATH%
+set PATH=%GISBASE%\bin;%PATH%
