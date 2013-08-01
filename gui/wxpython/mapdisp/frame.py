dd41b3793731eacadbdc20d22e7352ece9544b7e
Štěpán Turek <stepan.turek@seznam.cz>
2013-08-01 16:17:05 +0000

    calling VDigitWindow - removed tree parameter (not yet used in constructor)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@57345 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/mapdisp/frame.py
+++ gui/wxpython/mapdisp/frame.py
@@ -249,8 +249,7 @@ class MapFrame(SingleMapFrame):
             from vdigit.mapwindow import VDigitWindow
             self.MapWindowVDigit = VDigitWindow(parent = self, giface = self._giface,
                                                 id = wx.ID_ANY, frame = self,
-                                                Map = self.Map, tree = self.tree,
-                                                lmgr = self._layerManager)
+                                                Map = self.Map, lmgr = self._layerManager)
             self.MapWindowVDigit.Show()
             self._mgr.AddPane(self.MapWindowVDigit, wx.aui.AuiPaneInfo().CentrePane().
                           Dockable(False).BestSize((-1,-1)).Name('vdigit').
