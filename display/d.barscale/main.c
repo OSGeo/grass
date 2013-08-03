b1c1fe602c499cc0ee12b8cf9c05ba924152ad63
Hamish Bowman <hamish_b@yahoo.com>
2013-08-03 01:23:58 +0000

    make at= label more relevant to this module
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@57380 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.barscale/main.c
+++ display/d.barscale/main.c
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
     coords->options = "0-100";
     coords->required = YES;
     coords->label =
-	_("The screen coordinates for top-left corner of label");
+	_("Screen coordinates of the rectangle's top-left corner");
     coords->description = _("(0,0) is lower-left of frame");
 
     fg_color_opt = G_define_standard_option(G_OPT_C_FG);
