78c07873c826769277819eca0a93a3b1d3d2c8d0
Markus Neteler <neteler@gmail.com>
2013-08-04 21:20:24 +0000

    i.smap: keep supervised classification keyword
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@57409 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.smap/main.c
+++ imagery/i.smap/main.c
@@ -37,6 +37,7 @@ int main(int argc, char *argv[])
     module = G_define_module();
     G_add_keyword(_("imagery"));
     G_add_keyword(_("classification"));
+    G_add_keyword(_("supervised classification"));
     G_add_keyword(_("segmentation"));
     G_add_keyword(_("SMAP"));
     module->description =
