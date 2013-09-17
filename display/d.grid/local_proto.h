279fdc855f7c7b779c68fcfdbbdd2bea8b9c402b
Hamish Bowman <hamish_b@yahoo.com>
2013-09-17 02:07:28 +0000

    add direction= option to allow rendering only lines of latitude or longitude (merge https://trac.osgeo.org/grass/ticket/1372 from devbr6); fix text & tick placement (https://trac.osgeo.org/grass/ticket/1442), and try to work-around missing border/grid edge
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@57714 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.grid/local_proto.h
+++ display/d.grid/local_proto.h
@@ -1,9 +1,13 @@
 #include <grass/gprojects.h>
 
+#define DIRN_BOTH 0 
+#define DIRN_LAT  1
+#define DIRN_LON  2
+
 /* plot.c */
-int plot_grid(double, double, double, int, int, int, int, int, double);
+int plot_grid(double, double, double, int, int, int, int, int, double, int);
 int plot_geogrid(double, struct pj_info, struct pj_info, int, int, int, int,
-		 int, double);
+		 int, double, int);
 void init_proj(struct pj_info *, struct pj_info *, int);
 void get_ll_bounds(double *, double *, double *, double *, struct Cell_head,
 		   struct pj_info, struct pj_info);
@@ -12,7 +16,7 @@ void check_coords(double, double, double *, double *, int, struct Cell_head,
 float get_heading(double, double);
 
 /* plotborder.c */
-int plot_border(double, double, double);
+int plot_border(double, double, double, int);
 
 #define MARK_GRID     0
 #define MARK_CROSS    1
