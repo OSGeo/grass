90d85ac1ade2fffecdc1bade4dd2074e5e4afdc6
Huidae Cho <grass4u@gmail.com>
2013-10-20 12:19:10 +0000

    Used to do too much preprocessing. Now, it optionally takes the masked topidx map only for generating the topographic index statistics file. This change reduces user confusion and simplifies GUI.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@58073 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.topmodel/topmodel.c
+++ raster/r.topmodel/topmodel.c
@@ -1,23 +1,40 @@
+#include <math.h>
 #include <grass/gis.h>
 #include <grass/raster.h>
+#include <grass/spawn.h>
 #include "global.h"
 
+void create_topidxstats(char *topidx, int ntopidxclasses, char *outtopidxstats)
+{
+    char input[GPATH_MAX];
+    char output[GPATH_MAX];
+    char nsteps[32];
 
-double get_lambda(void)
+    sprintf(input, "input=%s", topidx);
+    sprintf(output, "output=%s", outtopidxstats);
+    sprintf(nsteps, "nsteps=%d", ntopidxclasses);
+
+    G_message("Creating topographic index statistics file...");
+    G_verbose_message("r.stats -Anc %s %s %s ...", input, output, nsteps);
+
+    if (G_spawn("r.stats", "r.stats", "-Anc", input, output, nsteps, NULL) != 0)
+	G_fatal_error("r.stats failed");
+}
+
+double calculate_lambda(void)
 {
     int i;
     double retval;
 
 
     retval = 0.0;
-    for (i = 1; i < misc.nidxclass; i++)
-	retval += idxstats.Aatb_r[i] *
-	    (idxstats.atb[i] + idxstats.atb[i - 1]) / 2.0;
+    for (i = 1; i < misc.ntopidxclasses; i++)
+	retval += topidxstats.Aatb_r[i] *
+	    (topidxstats.atb[i] + topidxstats.atb[i - 1]) / 2.0;
 
 
     return retval;
 }
-
 
 void initialize(void)
 {
@@ -25,7 +42,7 @@ void initialize(void)
     double A1, A2;
 
 
-    misc.lambda = get_lambda();
+    misc.lambda = calculate_lambda();
     misc.lnTe = params.lnTe + log(input.dt);
     misc.vch = params.vch * input.dt;
     misc.vr = params.vr * input.dt;
@@ -37,16 +54,16 @@ void initialize(void)
     for (i = 1; i < params.nch; i++)
 	misc.tch[i] = misc.tch[0] + (params.d[i] - params.d[0]) / misc.vr;
 
-    misc.nreach = (int)misc.tch[params.nch - 1];
-    if ((double)misc.nreach < misc.tch[params.nch - 1])
-	misc.nreach++;
-    misc.ndelay = (int)misc.tch[0];
+    misc.nreaches = (int)misc.tch[params.nch - 1];
+    if ((double)misc.nreaches < misc.tch[params.nch - 1])
+	misc.nreaches++;
+    misc.ndelays = (int)misc.tch[0];
 
-    misc.nreach -= misc.ndelay;
+    misc.nreaches -= misc.ndelays;
 
-    misc.Ad = (double *)G_malloc(misc.nreach * sizeof(double));
-    for (i = 0; i < misc.nreach; i++) {
-	t = misc.ndelay + i + 1;
+    misc.Ad = (double *)G_malloc(misc.nreaches * sizeof(double));
+    for (i = 0; i < misc.nreaches; i++) {
+	t = misc.ndelays + i + 1;
 	if (t > misc.tch[params.nch - 1]) {
 	    misc.Ad[i] = 1.0;
 	}
@@ -66,47 +83,46 @@ void initialize(void)
 
     A1 = misc.Ad[0];
     misc.Ad[0] *= params.A;
-    for (i = 1; i < misc.nreach; i++) {
+    for (i = 1; i < misc.nreaches; i++) {
 	A2 = misc.Ad[i];
 	misc.Ad[i] = A2 - A1;
 	A1 = A2;
 	misc.Ad[i] *= params.A;
     }
 
-    misc.Srz = (double **)G_malloc(input.ntimestep * sizeof(double *));
-    misc.Suz = (double **)G_malloc(input.ntimestep * sizeof(double *));
-    for (i = 0; i < input.ntimestep; i++) {
-	misc.Srz[i] = (double *)G_malloc(misc.nidxclass * sizeof(double));
-	misc.Suz[i] = (double *)G_malloc(misc.nidxclass * sizeof(double));
+    misc.Srz = (double **)G_malloc(input.ntimesteps * sizeof(double *));
+    misc.Suz = (double **)G_malloc(input.ntimesteps * sizeof(double *));
+    for (i = 0; i < input.ntimesteps; i++) {
+	misc.Srz[i] = (double *)G_malloc(misc.ntopidxclasses * sizeof(double));
+	misc.Suz[i] = (double *)G_malloc(misc.ntopidxclasses * sizeof(double));
     }
 
-    for (i = 0; i < misc.nidxclass; i++) {
+    for (i = 0; i < misc.ntopidxclasses; i++) {
 	misc.Srz[0][i] = params.Sr0;
 	misc.Suz[0][i] = 0.0;
     }
 
-    misc.S_mean = (double *)G_malloc(input.ntimestep * sizeof(double));
+    misc.S_mean = (double *)G_malloc(input.ntimesteps * sizeof(double));
     misc.S_mean[0] = -params.m * log(misc.qs0 / misc.qss);
 
-    misc.Qt = (double *)G_malloc(input.ntimestep * sizeof(double));
-    for (i = 0; i < input.ntimestep; i++)
+    misc.Qt = (double *)G_malloc(input.ntimesteps * sizeof(double));
+    for (i = 0; i < input.ntimesteps; i++)
 	misc.Qt[i] = 0.0;
 
-    for (i = 0; i < misc.ndelay; i++)
+    for (i = 0; i < misc.ndelays; i++)
 	misc.Qt[i] = misc.qs0 * params.A;
 
     A1 = 0.0;
-    for (i = 0; i < misc.nreach; i++) {
+    for (i = 0; i < misc.nreaches; i++) {
 	A1 += misc.Ad[i];
-	misc.Qt[misc.ndelay + i] = misc.qs0 * (params.A - A1);
+	misc.Qt[misc.ndelays + i] = misc.qs0 * (params.A - A1);
     }
 
 
     return;
 }
 
-
-void implement(void)
+void calculate_flows(void)
 {
     int i, j, k;
     double Aatb_r;
@@ -114,38 +130,38 @@ void implement(void)
     double _qo, _qv;
 
 
-    misc.S = (double **)G_malloc(input.ntimestep * sizeof(double *));
-    misc.Ea = (double **)G_malloc(input.ntimestep * sizeof(double *));
-    misc.ex = (double **)G_malloc(input.ntimestep * sizeof(double *));
+    misc.S = (double **)G_malloc(input.ntimesteps * sizeof(double *));
+    misc.Ea = (double **)G_malloc(input.ntimesteps * sizeof(double *));
+    misc.ex = (double **)G_malloc(input.ntimesteps * sizeof(double *));
 
-    misc.qt = (double **)G_malloc(input.ntimestep * sizeof(double *));
-    misc.qo = (double **)G_malloc(input.ntimestep * sizeof(double *));
-    misc.qv = (double **)G_malloc(input.ntimestep * sizeof(double *));
+    misc.qt = (double **)G_malloc(input.ntimesteps * sizeof(double *));
+    misc.qo = (double **)G_malloc(input.ntimesteps * sizeof(double *));
+    misc.qv = (double **)G_malloc(input.ntimesteps * sizeof(double *));
 
-    misc.qs = (double *)G_malloc(input.ntimestep * sizeof(double));
-    misc.f = (double *)G_malloc(input.ntimestep * sizeof(double));
-    misc.fex = (double *)G_malloc(input.ntimestep * sizeof(double));
+    misc.qs = (double *)G_malloc(input.ntimesteps * sizeof(double));
+    misc.f = (double *)G_malloc(input.ntimesteps * sizeof(double));
+    misc.fex = (double *)G_malloc(input.ntimesteps * sizeof(double));
 
-    for (i = 0; i < input.ntimestep; i++) {
-	misc.S[i] = (double *)G_malloc(misc.nidxclass * sizeof(double));
-	misc.Ea[i] = (double *)G_malloc(misc.nidxclass * sizeof(double));
-	misc.ex[i] = (double *)G_malloc(misc.nidxclass * sizeof(double));
+    for (i = 0; i < input.ntimesteps; i++) {
+	misc.S[i] = (double *)G_malloc(misc.ntopidxclasses * sizeof(double));
+	misc.Ea[i] = (double *)G_malloc(misc.ntopidxclasses * sizeof(double));
+	misc.ex[i] = (double *)G_malloc(misc.ntopidxclasses * sizeof(double));
 
-	misc.qt[i] = (double *)G_malloc((misc.nidxclass + 1) *
+	misc.qt[i] = (double *)G_malloc((misc.ntopidxclasses + 1) *
 					sizeof(double));
-	misc.qo[i] = (double *)G_malloc((misc.nidxclass + 1) *
+	misc.qo[i] = (double *)G_malloc((misc.ntopidxclasses + 1) *
 					sizeof(double));
-	misc.qv[i] = (double *)G_malloc((misc.nidxclass + 1) *
+	misc.qv[i] = (double *)G_malloc((misc.ntopidxclasses + 1) *
 					sizeof(double));
 
-	misc.qt[i][misc.nidxclass] = 0.0;
-	misc.qo[i][misc.nidxclass] = 0.0;
-	misc.qv[i][misc.nidxclass] = 0.0;
+	misc.qt[i][misc.ntopidxclasses] = 0.0;
+	misc.qo[i][misc.ntopidxclasses] = 0.0;
+	misc.qv[i][misc.ntopidxclasses] = 0.0;
 	misc.qs[i] = 0.0;
 
 	if (params.infex) {
 	    misc.f[i] = input.dt *
-		get_f((i + 1) * input.dt, input.R[i] / input.dt);
+		calculate_f((i + 1) * input.dt, input.R[i] / input.dt);
 	    misc.fex[i] = input.R[i] - misc.f[i];
 	    R = misc.f[i];
 	}
@@ -156,7 +172,7 @@ void implement(void)
 	}
 
 	if (i) {
-	    for (j = 0; j < misc.nidxclass; j++) {
+	    for (j = 0; j < misc.ntopidxclasses; j++) {
 		misc.Srz[i][j] = misc.Srz[i - 1][j];
 		misc.Suz[i][j] = misc.Suz[i - 1][j];
 	    }
@@ -164,13 +180,13 @@ void implement(void)
 
 	misc.qs[i] = misc.qss * exp(-misc.S_mean[i] / params.m);
 
-	for (j = 0; j < misc.nidxclass; j++) {
-	    Aatb_r = (idxstats.Aatb_r[j] +
-		      (j < misc.nidxclass - 1 ? idxstats.Aatb_r[j + 1]
+	for (j = 0; j < misc.ntopidxclasses; j++) {
+	    Aatb_r = (topidxstats.Aatb_r[j] +
+		      (j < misc.ntopidxclasses - 1 ? topidxstats.Aatb_r[j + 1]
 		       : 0.0)) / 2.0;
 
 	    misc.S[i][j] = misc.S_mean[i] +
-		params.m * (misc.lambda - idxstats.atb[j]);
+		params.m * (misc.lambda - topidxstats.atb[j]);
 	    if (misc.S[i][j] < 0.0)
 		misc.S[i][j] = 0.0;
 
@@ -202,7 +218,7 @@ void implement(void)
 		_qv *= Aatb_r;
 	    }
 	    misc.qv[i][j] = _qv;
-	    misc.qv[i][misc.nidxclass] += misc.qv[i][j];
+	    misc.qv[i][misc.ntopidxclasses] += misc.qv[i][j];
 
 	    misc.Ea[i][j] = 0.0;
 	    if (input.Ep[i] > 0.0) {
@@ -216,7 +232,7 @@ void implement(void)
 	    _qo = 0.0;
 	    if (j > 0) {
 		if (misc.ex[i][j] > 0.0)
-		    _qo = idxstats.Aatb_r[j] *
+		    _qo = topidxstats.Aatb_r[j] *
 			(misc.ex[i][j - 1] + misc.ex[i][j]) / 2.0;
 		else if (misc.ex[i][j - 1] > 0.0)
 		    _qo = Aatb_r * misc.ex[i][j - 1] /
@@ -224,24 +240,24 @@ void implement(void)
 			 misc.ex[i][j]) * misc.ex[i][j - 1] / 2.0;
 	    }
 	    misc.qo[i][j] = _qo;
-	    misc.qo[i][misc.nidxclass] += misc.qo[i][j];
+	    misc.qo[i][misc.ntopidxclasses] += misc.qo[i][j];
 
 	    misc.qt[i][j] = misc.qo[i][j] + misc.qs[i];
 	}
-	misc.qo[i][misc.nidxclass] += misc.fex[i];
-	misc.qt[i][misc.nidxclass] = misc.qo[i][misc.nidxclass] + misc.qs[i];
+	misc.qo[i][misc.ntopidxclasses] += misc.fex[i];
+	misc.qt[i][misc.ntopidxclasses] = misc.qo[i][misc.ntopidxclasses] + misc.qs[i];
 
 	misc.S_mean[i] = misc.S_mean[i] +
-	    misc.qs[i] - misc.qv[i][misc.nidxclass];
+	    misc.qs[i] - misc.qv[i][misc.ntopidxclasses];
 
-	if (i + 1 < input.ntimestep)
+	if (i + 1 < input.ntimesteps)
 	    misc.S_mean[i + 1] = misc.S_mean[i];
 
-	for (j = 0; j < misc.nreach; j++) {
-	    k = i + j + misc.ndelay;
-	    if (k > input.ntimestep - 1)
+	for (j = 0; j < misc.nreaches; j++) {
+	    k = i + j + misc.ndelays;
+	    if (k > input.ntimesteps - 1)
 		break;
-	    misc.Qt[k] += misc.qt[i][misc.nidxclass] * misc.Ad[j];
+	    misc.Qt[k] += misc.qt[i][misc.ntopidxclasses] * misc.Ad[j];
 	}
     }
 
@@ -249,9 +265,8 @@ void implement(void)
     return;
 }
 
-
-/* Object function for hydrograph suggested by Servet and Dezetter(1991) */
-double get_Em(void)
+/* Objective function for hydrograph suggested by Servet and Dezetter(1991) */
+double calculate_Em(void)
 {
     int i;
     double Em, numerator, denominator;
@@ -259,14 +274,14 @@ double get_Em(void)
 
     misc.Qobs_mean = 0.0;
     numerator = 0.0;
-    for (i = 0; i < input.ntimestep; i++) {
+    for (i = 0; i < input.ntimesteps; i++) {
 	misc.Qobs_mean += misc.Qobs[i];
 	numerator += pow(misc.Qobs[i] - misc.Qt[i], 2.0);
     }
-    misc.Qobs_mean /= input.ntimestep;
+    misc.Qobs_mean /= input.ntimesteps;
 
     denominator = 0.0;
-    for (i = 0; i < input.ntimestep; i++)
+    for (i = 0; i < input.ntimesteps; i++)
 	denominator += pow(misc.Qobs[i] - misc.Qobs_mean, 2.0);
 
     if (denominator == 0.0) {
@@ -277,29 +292,27 @@ double get_Em(void)
 	Em = 1.0 - numerator / denominator;
     }
 
-
     return Em;
 }
 
-
-void others(void)
+void calculate_others(void)
 {
     int i;
 
 
     misc.Qt_mean = 0.0;
-    for (i = 0; i < input.ntimestep; i++) {
+    for (i = 0; i < input.ntimesteps; i++) {
 	misc.Qt_mean += misc.Qt[i];
 	if (!i || misc.Qt_peak < misc.Qt[i]) {
 	    misc.Qt_peak = misc.Qt[i];
 	    misc.tt_peak = i + 1;
 	}
     }
-    misc.Qt_mean /= input.ntimestep;
+    misc.Qt_mean /= input.ntimesteps;
 
-    if (file.Qobs) {
-	misc.Em = get_Em();
-	for (i = 0; i < input.ntimestep; i++) {
+    if (file.qobs) {
+	misc.Em = calculate_Em();
+	for (i = 0; i < input.ntimesteps; i++) {
 	    if (!i || misc.Qobs_peak < misc.Qobs[i]) {
 		misc.Qobs_peak = misc.Qobs[i];
 		misc.tobs_peak = i + 1;
@@ -307,17 +320,14 @@ void others(void)
 	}
     }
 
-
     return;
 }
 
-
-void topmodel(void)
+void run_topmodel(void)
 {
     initialize();
-    implement();
-    others();
-
+    calculate_flows();
+    calculate_others();
 
     return;
 }
