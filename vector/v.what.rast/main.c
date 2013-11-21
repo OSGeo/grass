67057d35058a1c877dd03c1be843dd775a7b1c6a
Hamish Bowman <hamish_b@yahoo.com>
2013-11-21 08:34:40 +0000

    fixes for print flag: vect map doesn't need to be in current mapset, and doesn't need to have a DB
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@58261 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.what.rast/main.c
+++ vector/v.what.rast/main.c
@@ -124,19 +124,18 @@ int main(int argc, char *argv[])
 
     field = Vect_get_field_number(&Map, opt.field->answer);
 
-    /* FIXME: if the print flag is used then a database doesn't
-	need to exist and the map doesn't need to be in the
-	current mapset. Topology should be optional too. */
     Fi = Vect_get_field(&Map, field);
-    if (Fi == NULL)
-	G_fatal_error(_("Database connection not defined for layer %d"),
-		      field);
+    if (!print_flag->answer) {
+	if (Fi == NULL)
+	    G_fatal_error(_("Database connection not defined for layer %d"),
+			  field);
 
-    /* Open driver */
-    driver = db_start_driver_open_database(Fi->driver, Fi->database);
-    if (driver == NULL) {
-	G_fatal_error(_("Unable to open database <%s> by driver <%s>"),
-		      Fi->database, Fi->driver);
+	/* Open driver */
+	driver = db_start_driver_open_database(Fi->driver, Fi->database);
+	if (driver == NULL) {
+	    G_fatal_error(_("Unable to open database <%s> by driver <%s>"),
+			  Fi->database, Fi->driver);
+	}
     }
 
     /* Open raster */
@@ -223,9 +222,11 @@ int main(int argc, char *argv[])
 	point_cnt++;
     }
 
-    Vect_set_db_updated(&Map);
-    Vect_hist_command(&Map);
-    Vect_set_db_updated(&Map);
+    if (!print_flag->answer) {
+	Vect_set_db_updated(&Map);
+	Vect_hist_command(&Map);
+	Vect_set_db_updated(&Map); /* again? */
+    }
     Vect_close(&Map);
 
     G_debug(1, "Read %d vector points", point_cnt);
@@ -516,7 +517,10 @@ int main(int argc, char *argv[])
     if (print_flag->answer) {
 	dupl_cnt = 0;
 
-	G_message("%s|value", Fi->key);
+	if(Fi)
+	    G_message("%s|value", Fi->key);
+	else
+	    G_message("cat|value");
 
 	for (point = 0; point < point_cnt; point++) {
 	    if (cache[point].count > 1) {
