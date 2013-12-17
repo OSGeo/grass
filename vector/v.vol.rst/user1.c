17b47773051a253af78d80ed244fefe13a65c402
Anna Petrášová <kratochanna@gmail.com>
2013-12-17 15:21:38 +0000

    v.vol.rst: fixed masking with 2D raster: mask rows were intentionally flipped, reason unknown, it does not seem to be related to changes for https://trac.osgeo.org/grass/ticket/1389 (major g3d lib review in G7) because the same problem is in G64.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@58478 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.vol.rst/user1.c
+++ vector/v.vol.rst/user1.c
@@ -67,7 +67,7 @@ int INPUT(struct Map_info *In, char *column, char *scol, char *wheresql)
     struct quadruple *point;
     double x, y, z, w, nz = 0., sm;
     double c1, c2, c3, c4, c5, c6, nsg;
-    int i, j, k = 0, a, irev, cfmask;
+    int i, j, k = 0, a, cfmask;
     int ddisk = 0;
     double deltx, delty, deltz;
     int first_time = 1;
@@ -368,13 +368,12 @@ int INPUT(struct Map_info *In, char *column, char *scol, char *wheresql)
 	cellmask = Rast_allocate_c_buf();
 	cfmask = Rast_open_old(maskmap, mapsetm);
 	for (i = 0; i < nsizr; i++) {
-	    irev = nsizr - i - 1;
 	    Rast_get_c_row(cfmask, cellmask, i);
 	    for (j = 0; j < nsizc; j++) {
 		if ((cellmask[j] == 0) || Rast_is_c_null_value(&cellmask[j]))
-		    BM_set(bitmask, j, irev, 0);
+		    BM_set(bitmask, j, i, 0);
 		else
-		    BM_set(bitmask, j, irev, 1);
+		    BM_set(bitmask, j, i, 1);
 	    }
 	}
 	G_message("bitmap mask created");
@@ -441,7 +440,7 @@ int OUTGR()
 	cnt = 0;
 	for (iarc = 0; iarc < nsizl; iarc++) {
 
-	    for (y = nsizr - 1; y >= 0; y--) {	/* changed by AV */
+	    for (y = 0; y < nsizr; y++) {
 		for (x = 0; x < nsizc; x++) {
 		    if (maskmap != NULL)
 			bmask = BM_get(bitmask, x, y);
@@ -491,7 +490,7 @@ int OUTGR()
 	cnt = 0;
 	for (iarc = 0; iarc < nsizl; iarc++) {
 
-	    for (y = nsizr - 1; y >= 0; y--) {	/* changed by AV */
+	    for (y = 0; y < nsizr; y++) {
 		for (x = 0; x < nsizc; x++) {
 		    if (maskmap != NULL)
 			bmask = BM_get(bitmask, x, y);
@@ -541,7 +540,7 @@ int OUTGR()
 	cnt = 0;
 	for (iarc = 0; iarc < nsizl; iarc++) {
 
-	    for (y = nsizr - 1; y >= 0; y--) {	/* changed by AV */
+	    for (y = 0; y < nsizr; y++) {	/* changed by AV */
 		for (x = 0; x < nsizc; x++) {
 		    if (maskmap != NULL)
 			bmask = BM_get(bitmask, x, y);
@@ -591,7 +590,7 @@ int OUTGR()
 	cnt = 0;
 	for (iarc = 0; iarc < nsizl; iarc++) {
 
-	    for (y = nsizr - 1; y >= 0; y--) {	/* changed by AV */
+	    for (y = 0; y < nsizr; y++) {	/* changed by AV */
 		for (x = 0; x < nsizc; x++) {
 		    if (maskmap != NULL)
 			bmask = BM_get(bitmask, x, y);
@@ -641,7 +640,7 @@ int OUTGR()
 	cnt = 0;
 	for (iarc = 0; iarc < nsizl; iarc++) {
 
-	    for (y = nsizr - 1; y >= 0; y--) {	/* changed by AV */
+	    for (y = 0; y < nsizr; y++) {
 		for (x = 0; x < nsizc; x++) {
 		    if (maskmap != NULL)
 			bmask = BM_get(bitmask, x, y);
@@ -691,7 +690,7 @@ int OUTGR()
 	cnt = 0;
 	for (iarc = 0; iarc < nsizl; iarc++) {
 
-	    for (y = nsizr - 1; y >= 0; y--) {	/* changed by AV */
+	    for (y = 0; y < nsizr; y++) {
 		for (x = 0; x < nsizc; x++) {
 		    if (maskmap != NULL)
 			bmask = BM_get(bitmask, x, y);
@@ -741,7 +740,7 @@ int OUTGR()
 	cnt = 0;
 	for (iarc = 0; iarc < nsizl; iarc++) {
 
-	    for (y = nsizr - 1; y >= 0; y--) {	/* changed by AV */
+	    for (y = 0; y < nsizr; y++) {
 		for (x = 0; x < nsizc; x++) {
 		    if (maskmap != NULL)
 			bmask = BM_get(bitmask, x, y);
