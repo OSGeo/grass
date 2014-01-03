ede6c946be930f45ab60c864fbedf1566cc97504
Anna Petrášová <kratochanna@gmail.com>
2014-01-03 03:39:48 +0000

    wxGUI example application update to work with current gui
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@58590 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- doc/gui/wxpython/example/dialogs.py
+++ doc/gui/wxpython/example/dialogs.py
@@ -6,53 +6,54 @@
 Classes:
  - dialogs::ExampleMapDialog
 
-(C) 2006-2011 by the GRASS Development Team
+(C) 2011-2014 by the GRASS Development Team
 This program is free software under the GNU General Public
 License (>=v2). Read the file COPYING that comes with GRASS
 for details.
 
-@author Anna Kratochvilova <kratochanna gmail.com>
+@author Anna Petrasova <kratochanna gmail.com>
 """
 
 import wx
 
-from core               import globalvar
-from gui_core.dialogs   import SimpleDialog, GroupDialog
-from gui_core           import gselect
+from core import globalvar
+from gui_core.dialogs import SimpleDialog
+from gui_core import gselect
+from core.utils import _
+
 
 class ExampleMapDialog(SimpleDialog):
     """!Dialog for adding raster map.
-    
+
     Dialog can be easily changed to enable to choose vector, imagery groups or other elements.
     """
-    def __init__(self, parent, title = _("Choose raster map"), id = wx.ID_ANY):
+    def __init__(self, parent, title=_("Choose raster map")):
         """!Calls super class constructor.
-        
+
         @param parent gui parent
         @param title dialog window title
         @param id id
         """
         SimpleDialog.__init__(self, parent, title)
-        
+
         # here is the place to determine element type
-        self.element = gselect.Select(parent = self.panel, type = 'raster',
-                                      size = globalvar.DIALOG_GSELECT_SIZE)
-        
+        self.element = gselect.Select(parent=self.panel, type='raster',
+                                      size=globalvar.DIALOG_GSELECT_SIZE)
+
         self._layout()
 
         self.SetMinSize(self.GetSize())
 
     def _layout(self):
         """!Do layout"""
-        self.dataSizer.Add(item = wx.StaticText(parent = self.panel, id = wx.ID_ANY,
-                                                label = _("Name of raster map:")),
-                           proportion = 0, flag = wx.ALL, border = 1)
-        self.dataSizer.Add(self.element, proportion = 0,
-                           flag = wx.EXPAND | wx.ALL, border = 1)
+        self.dataSizer.Add(item=wx.StaticText(parent=self.panel,
+                                              label=_("Name of raster map:")),
+                           proportion=0, flag=wx.ALL, border=1)
+        self.dataSizer.Add(self.element, proportion=0,
+                           flag=wx.EXPAND | wx.ALL, border=1)
         self.panel.SetSizer(self.sizer)
         self.sizer.Fit(self)
-        
+
     def GetRasterMap(self):
         """!Returns selected raster map"""
         return self.element.GetValue()
-
