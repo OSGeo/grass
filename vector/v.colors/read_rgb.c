b9eb6a54194f9d9b7a9942d72865d8dca62b1d92
Martin Landa <landa.martin@gmail.com>
2014-02-21 20:06:37 +0000

    v.colors: fix segfault when converting RGB values into color table
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59116 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.colors/read_rgb.c
+++ vector/v.colors/read_rgb.c
@@ -8,7 +8,7 @@
 void rgb2colr(const struct Map_info *Map, int layer, const char *rgb_column,
 	      struct Colors *colors)
 {
-    int i, ret;
+    int i, ret, nskipped;
     int red, grn, blu;
     const char *rgb;
     
@@ -39,21 +39,30 @@ void rgb2colr(const struct Map_info *Map, int layer, const char *rgb_column,
     Rast_init_colors(colors);
     
     cv = NULL;
+    nskipped = 0;
     for (i = 0; i < cvarr.n_values; i++) {
 	cv = &(cvarr.value[i]);
 	rgb = db_get_string(cv->val.s);
 	G_debug(3, "cat = %d RGB = %s", cv->cat, rgb);
 
+        if (!rgb) {
+            nskipped++;
+            continue;
+        }
+        
 	ret = G_str_to_color(rgb, &red, &grn, &blu);
 	if (ret != 1) {
-	    G_warning(_("Invalid RGB value '%s'"), rgb);
+	    G_debug(3, "Invalid RGB value '%s'", rgb);
+            nskipped++;
 	    continue;
 	}
 
 	Rast_add_c_color_rule((const CELL*) &(cv->cat), red, grn, blu,
 			      (const CELL*) &(cv->cat), red, grn, blu, colors);
-
     }
+
+    if (nskipped > 0)
+        G_warning(_("%d invalid RGB color values skipped"), nskipped);
     
     db_close_database_shutdown_driver(driver);
 }
