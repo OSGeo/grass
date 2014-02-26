60178caaad1d8b1afb728155728884ce4a263e0c
Markus Neteler <neteler@gmail.com>
2014-02-26 08:06:47 +0000

    r.li.daemon: tell user which map has been generated
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59143 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.li/r.li.daemon/daemon.c
+++ raster/r.li/r.li.daemon/daemon.c
@@ -175,6 +175,7 @@ int calculateIndex(char *file, rli_func *f,
 	Rast_short_history(output, "raster", &history);
 	Rast_command_history(&history);
 	Rast_write_history(output, &history);
+	G_message(_("Raster map <%s> created."), output);
     } else {
 	/* text file output */
 	G_message("Result written to ASCII file <%s>", out);
