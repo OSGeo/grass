531fef2ab71365a7469740557b20748f33dbc306
Maris Nartiss <maris.gis@gmail.com>
2014-03-01 17:55:31 +0000

    Introduce ngettext support and a new macro _n for it
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59156 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/defs/glocale.h
+++ include/defs/glocale.h
@@ -3,5 +3,6 @@
 
 extern void G_init_locale(void);
 extern char *G_gettext(const char *, const char *) __attribute__((format_arg (2)));
+extern char *G_ngettext(const char *, const char *, const char *, unsigned long int) __attribute__((format_arg (2), format_arg (3)));
 
 #endif
