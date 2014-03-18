1e6f6dd7fbeaf021f54d3b2de1381d6dea541fb7
Markus Neteler <neteler@gmail.com>
2014-03-18 07:13:50 +0000

    colors: register NDWI
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59279 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/core/utils.py
+++ gui/wxpython/core/utils.py
@@ -1010,7 +1010,7 @@ command2ltype = {'d.rast'         : 'raster',
                  'd.histogram'    : 'histogram',
                  'd.colortable'   : 'colortable',
                  'd.graph'        : 'graph',
-                 'd.out.file'     : 'export'
+                 'd.text'         : 'text'
                  }
 ltype2command = {}
 for (cmd, ltype) in command2ltype.items():
