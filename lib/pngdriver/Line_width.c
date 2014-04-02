ed600c7103e8a9b0e2d1ce3b61a9fb3e9f56b82a
Hamish Bowman <hamish_b@yahoo.com>
2014-04-02 03:42:22 +0000

    bugfix: line width wasn't working in the PNG driver
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59547 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/pngdriver/Line_width.c
+++ lib/pngdriver/Line_width.c
@@ -1,9 +1,7 @@
 #include <math.h>
 #include "pngdriver.h"
 
-int linewidth;
-
 void PNG_Line_width(double width)
 {
-    linewidth = (width < 0 ? 0 : (int) floor(width + 0.5));
+    png.linewidth = (width < 0 ? 0 : (int) floor(width + 0.5));
 }
