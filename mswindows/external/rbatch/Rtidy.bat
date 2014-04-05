2d1c0c8f79a6af336fe6227c1109b921cfa9bc98
Helmut Kudrnovsky <hkmyricaria@gmail.com>
2014-04-05 20:43:48 +0000

    update Windows batchfiles for use with R: part 2 - delete old files
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59584 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/external/rbatch/Rtidy.bat
+++
@@ -1,8 +0,0 @@
-#Rscript %0.bat %*
-# usage: Rtidy sourcefile.R > sourcefile.Rtidy.R
-# based on 3.1 of R Extensions Manual
-options(keep.source = FALSE)
-source(commandArgs(TRUE))
-dump(ls(all = TRUE), file = stdout())
-q("no")
-
