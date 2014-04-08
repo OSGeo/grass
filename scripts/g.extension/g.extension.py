bc31ca4b9248ddad70f617ccd2065f98480cb48a
Martin Landa <landa.martin@gmail.com>
2014-04-08 17:43:16 +0000

    g.extension: sync'ed with relbr70
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59644 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/g.extension/g.extension.py
+++ scripts/g.extension/g.extension.py
@@ -688,9 +688,11 @@ def install_extension_xml(url, mlist):
 
 # install extension on MS Windows
 def install_extension_win(name):
-    ### TODO: do not use hardcoded url - http://wingrass.fsv.cvut.cz/grassXX/addonsX.X.X
+    ### do not use hardcoded url - http://wingrass.fsv.cvut.cz/grassXX/addonsX.X.X
     grass.message(_("Downloading precompiled GRASS Addons <%s>...") % options['extension'])
-    url = "http://wingrass.fsv.cvut.cz/grass%s%s/addons" % (version[0], version[1])
+    url = "http://wingrass.fsv.cvut.cz/grass%(major)s%(minor)s/addons/grass-%(major)s.%(minor)s.%(patch)s/" % \
+        { 'major' : version[0], 'minor' : version[1], 'patch' : version[2]}
+    
     grass.debug("url=%s" % url, 1)
 
     try:
