147acc3d0134e8b182b776175916627752ae9d9d
Markus Neteler <neteler@gmail.com>
2014-04-11 13:05:21 +0000

    manual: use shorter url
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@59693 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/pygrass/modules/shortcuts.py
+++ lib/python/pygrass/modules/shortcuts.py
@@ -50,7 +50,7 @@ class MetaModule(object):
         return self.cls('%s.%s' % (self.prefix, name.replace('_', '.')))
 
 
-# http://grass.osgeo.org/grass70/manuals/html70_user/full_index.html
+# http://grass.osgeo.org/grass70/manuals/full_index.html
 #[ d.* | db.* | g.* | i.* | m.* | ps.* | r.* | r3.* | t.* | v.* ]
 #
 #  d.*	display commands
