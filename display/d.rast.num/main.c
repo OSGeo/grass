OSGeo/grass

commit aa59013bba24015577d9916f7dd7ff0abd724658

Expose plural forms for translations
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59255 15284696-431f-4ddb-bdfa-cd5b030d7da7

display/d.rast.num/main.c
@@ -172,11 +172,13 @@ int main(int argc, char **argv)
     /* number of rows and cols in window */
 
     if ((nrows > 75) || (ncols > 75)) {
-	G_warning(_("Current region size: %d rows X %d cols\n"
+        /* GTC %s will be replaced by strings "X rows" and "Y cols" */
+        G_warning(_("Current region size: %s X %s\n"
 		    "Your current region setting may be too large. "
 		    "Cells displayed on your graphics window may be too "
 		    "small for cell category number to be visible."),
-		  nrows, ncols);
+		  _n("%d row", "%d rows", nrows), 
+          _n("%d col", "%d cols", ncols));
     }
     if ((nrows > 200) || (ncols > 200)) {
 	G_fatal_error(_("Aborting (region larger then 200 rows X 200 cols is not allowed)"));

raster/r.out.ppm/main.c
@@ -97,7 +97,8 @@ int main(int argc, char *argv[])
     /*G_get_set_window (&w); *//* 10/99 MN: check for current region */
     G_get_window(&w);
 
-    G_message(_("rows = %d, cols = %d"), w.rows, w.cols);
+    G_message("%s, %s", _n("row = %d", "rows = %d", w.rows), 
+        _n("col = %d", "cols = %d", w.cols));
 
     /* open raster map for reading */
     cellfile = Rast_open_old(rast->answer, "");

raster/r.out.ppm3/main.c
@@ -91,7 +91,8 @@ int main(int argc, char **argv)
 
     G_get_window(&w);
 
-    G_message(_("rows = %d, cols = %d"), w.rows, w.cols);
+    G_message("%s, %s", _n("row = %d", "rows = %d", w.rows), 
+        _n("col = %d", "cols = %d", w.cols));
 
     /* open raster map for reading */
     for (i = 0; i < 3; i++) {

raster/r.thin/io.c
@@ -98,7 +98,10 @@ int open_file(char *name)
 
     n_rows = Rast_window_rows();
     n_cols = Rast_window_cols();
-    G_message(_("File %s -- %d rows X %d columns"), name, n_rows, n_cols);
+    /* GTC First is the file name, second and third - number of rows and cols */
+    G_message(_("File %s -- %s X %s"), name, 
+        _n("%d row", "%d rows", n_rows), 
+        _n("%d column", "%d columns", n_cols));
     n_cols += (PAD << 1);
 
     /* copy raster map into our read/write file */
@@ -158,9 +161,15 @@ int close_file(char *name)
 
     row_count = n_rows - (PAD << 1);
     col_count = n_cols - (PAD << 1);
-    G_message(_("Output file %d rows X %d columns"), row_count, col_count);
-    G_message(_("Window %d rows X %d columns"), Rast_window_rows(),
-	      Rast_window_cols());
+    /* GTC %s will be replaced with number of rows and columns */
+    G_message(_("Output file %s X %s"), 
+        _n("%d row", "%d rows", row_count), 
+        _n("%d column", "%d columns", col_count));
+    
+    /* GTC %s will be replaced with number of rows and columns */
+    G_message(_("Window %s X %s"), 
+        _n("%d row", "%d rows", Rast_window_rows()), 
+        _n("%d column", "%d columns", Rast_window_cols()));
 
     for (row = 0, k = PAD; row < row_count; row++, k++) {
 	buf = get_a_row(k);

display/d.linegraph/main.c
@@ -306,12 +306,19 @@ int main(int argc, char **argv)
 	    }
 	}
 	if ((i > 0) && (in[0].num_pnts != in[i].num_pnts)) {
-	    G_warning(_("Y input file <%s> contains %s data points than the X input file"),
-		      in[i].name,
-		      ((in[i].num_pnts < in[0].num_pnts) ? "fewer" : "more"));
-
+        if (in[i].num_pnts < in[0].num_pnts) {
+            G_warning(_("Y input file <%s> contains fewer data points than the X input file"),
+		      in[i].name);
+        }
+        else {
+            G_warning(_("Y input file <%s> contains more data points than the X input file"),
+		      in[i].name);
+        }
+        
 	    if (in[i].num_pnts > in[0].num_pnts)
-		G_message(_("The last %d point(s) will be ignored"),
+		G_message(_n("The last point will be ignored", 
+                     "The last %d points will be ignored",
+                     (in[i].num_pnts - in[0].num_pnts)),
 			  (in[i].num_pnts - in[0].num_pnts));
 	}
     }

display/d.vect/main.c
@@ -386,7 +386,7 @@ int main(int argc, char **argv)
 	chcat = 1;
 	ret = Vect_str_to_cat_list(cat_opt->answer, Clist);
 	if (ret > 0)
-	    G_warning(_("%d errors in cat option"), ret);
+	    G_warning(_n("%d error in cat option", "%d errors in cat option", ret), ret);
     }
     
     type = Vect_option_to_types(type_opt);

raster/r.clump/main.c
@@ -107,7 +107,7 @@ int main(int argc, char *argv[])
     Rast_command_history(&hist);
     Rast_write_history(OUTPUT, &hist);
 
-    G_done_msg(_("%d clumps."), range.max);
+    G_done_msg(_n("%d clump.", "%d clumps.", range.max), range.max);
 
     exit(EXIT_SUCCESS);
 }

raster/r.cost/main.c
@@ -578,7 +578,7 @@ int main(int argc, char *argv[])
 	if (npoints < 1)
 	    G_fatal_error(_("No start points found in vector map <%s>"), Vect_get_full_name(&In));
         else
-            G_verbose_message(_("%d points found"), npoints);
+            G_verbose_message(_n("%d point found", "%d points found", npoints), npoints);
         
 	Vect_close(&In);
     }

commit 0fd13286ec02033255d85b46ee0c662ce889ca68

Fix wrong message plural form handling introduced in r59255
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59713 15284696-431f-4ddb-bdfa-cd5b030d7da7

display/d.rast.num/main.c
@@ -67,6 +67,7 @@ int main(int argc, char **argv)
     } flg;
     RASTER_MAP_TYPE map_type, inmap_type;
     double t, b, l, r;
+    char *tmpstr1, *tmpstr2;
 
     /* Initialize the GIS calls */
     G_gisinit(argv[0]);
@@ -172,13 +173,17 @@ int main(int argc, char **argv)
     /* number of rows and cols in window */
 
     if ((nrows > 75) || (ncols > 75)) {
+        G_asprintf(&tmpstr1, _n("%d row", "%d rows", nrows), nrows);
+        G_asprintf(&tmpstr1, _n("%d col", "%d cols", ncols), ncols);
         /* GTC %s will be replaced by strings "X rows" and "Y cols" */
         G_warning(_("Current region size: %s X %s\n"
 		    "Your current region setting may be too large. "
 		    "Cells displayed on your graphics window may be too "
 		    "small for cell category number to be visible."),
-		  _n("%d row", "%d rows", nrows), 
-          _n("%d col", "%d cols", ncols));
+		    tmpstr1, tmpstr2);
+        G_free(tmpstr1);
+        G_free(tmpstr2); 
+          
     }
     if ((nrows > 200) || (ncols > 200)) {
 	G_fatal_error(_("Aborting (region larger then 200 rows X 200 cols is not allowed)"));

raster/r.out.ppm/main.c
@@ -45,6 +45,7 @@ int main(int argc, char *argv[])
     struct Cell_head w;
     FILEDESC cellfile = 0;
     FILE *fp;
+    char *tmpstr1, *tmpstr2;
 
 
     G_gisinit(argv[0]);
@@ -97,8 +98,11 @@ int main(int argc, char *argv[])
     /*G_get_set_window (&w); *//* 10/99 MN: check for current region */
     G_get_window(&w);
 
-    G_message("%s, %s", _n("row = %d", "rows = %d", w.rows), 
-        _n("col = %d", "cols = %d", w.cols));
+    G_asprintf(&tmpstr1, _n("row = %d", "rows = %d", w.rows), w.rows);
+    G_asprintf(&tmpstr2, _n("column = %d", "columns = %d", w.cols), w.cols);
+    G_message("%s, %s", tmpstr1, tmpstr2);
+    G_free(tmpstr1);
+    G_free(tmpstr2); 
 
     /* open raster map for reading */
     cellfile = Rast_open_old(rast->answer, "");

raster/r.out.ppm3/main.c
@@ -49,6 +49,7 @@ int main(int argc, char **argv)
     unsigned char *dummy;
     int row, col;
     int i;
+    char *tmpstr1, *tmpstr2;
 
     G_gisinit(argv[0]);
 
@@ -91,8 +92,12 @@ int main(int argc, char **argv)
 
     G_get_window(&w);
 
-    G_message("%s, %s", _n("row = %d", "rows = %d", w.rows), 
-        _n("col = %d", "cols = %d", w.cols));
+    G_asprintf(&tmpstr1, _n("row = %d", "rows = %d", w.rows), w.rows);
+    /* GTC Raster columns */
+    G_asprintf(&tmpstr2, _n("column = %d", "columns = %d", w.cols), w.cols);
+    G_message("%s, %s", tmpstr1, tmpstr2);
+    G_free(tmpstr1);
+    G_free(tmpstr2); 
 
     /* open raster map for reading */
     for (i = 0; i < 3; i++) {

raster/r.thin/io.c
@@ -87,6 +87,7 @@ int open_file(char *name)
     int cell_file, buf_len;
     int i, row;
     CELL *buf;
+    char *tmpstr1, *tmpstr2;
 
     /* open raster map */
     cell_file = Rast_open_old(name, "");
@@ -98,10 +99,16 @@ int open_file(char *name)
 
     n_rows = Rast_window_rows();
     n_cols = Rast_window_cols();
-    /* GTC First is the file name, second and third - number of rows and cols */
-    G_message(_("File %s -- %s X %s"), name, 
-        _n("%d row", "%d rows", n_rows), 
-        _n("%d column", "%d columns", n_cols));
+    
+    /* GTC Count of raster rows */
+    G_asprintf(&tmpstr1, _n("%d row", "%d rows", n_rows), n_rows);
+    /* GTC Count of raster columns */
+    G_asprintf(&tmpstr2, _n("%d column", "%d columns", n_cols), n_cols);
+    /* GTC First argument is the raster map name, second and third - a string representing number of rows and cols */
+    G_message(_("Raster map <%s> - %s X %s"), name, tmpstr1, tmpstr2);
+    G_free(tmpstr1);
+    G_free(tmpstr2);
+    
     n_cols += (PAD << 1);
 
     /* copy raster map into our read/write file */
@@ -156,20 +163,30 @@ int close_file(char *name)
     int cell_file, row, k;
     int row_count, col_count;
     CELL *buf;
+    char *tmpstr1, *tmpstr2;
 
     cell_file = Rast_open_c_new(name);
 
     row_count = n_rows - (PAD << 1);
     col_count = n_cols - (PAD << 1);
-    /* GTC %s will be replaced with number of rows and columns */
-    G_message(_("Output file %s X %s"), 
-        _n("%d row", "%d rows", row_count), 
-        _n("%d column", "%d columns", col_count));
     
+    /* GTC Count of raster rows */
+    G_asprintf(&tmpstr1, _n("%d row", "%d rows", row_count), row_count);
+    /* GTC Count of raster columns */
+    G_asprintf(&tmpstr2, _n("%d column", "%d columns", col_count), col_count);
     /* GTC %s will be replaced with number of rows and columns */
-    G_message(_("Window %s X %s"), 
-        _n("%d row", "%d rows", Rast_window_rows()), 
-        _n("%d column", "%d columns", Rast_window_cols()));
+    G_message(_("Output map %s X %s"), tmpstr1, tmpstr2);
+    G_free(tmpstr1);
+    G_free(tmpstr2); 
+    
+    /* GTC Count of window rows */
+    G_asprintf(&tmpstr1, _n("%d row", "%d rows", Rast_window_rows()), Rast_window_rows());
+    /* GTC Count of window columns */
+    G_asprintf(&tmpstr2, _n("%d column", "%d columns", Rast_window_cols()), Rast_window_cols());
+    /* GTC %s will be replaced with number of rows and columns */
+    G_message(_("Window %s X %s"), tmpstr1, tmpstr2);
+    G_free(tmpstr1);
+    G_free(tmpstr2); 
 
     for (row = 0, k = PAD; row < row_count; row++, k++) {
 	buf = get_a_row(k);