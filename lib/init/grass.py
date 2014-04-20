5d30ff8b6023e6d5a48881c56f8284bd61a80a6b
Martin Landa <landa.martin@gmail.com>
2014-04-20 20:24:09 +0000

    grass.py: show revision number only for dev versions
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59851 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/init/grass.py
+++ lib/init/grass.py
@@ -1002,16 +1002,17 @@ def show_banner():
 
 def say_hello():
     sys.stderr.write(_("Welcome to GRASS %s") % grass_version)
-    try:
-        filerev = open(os.path.join(gisbase, 'etc', 'VERSIONNUMBER'))
-        linerev = filerev.readline().rstrip('\n')
-        filerev.close()
-        
-        revision = linerev.split(' ')[1]
-        sys.stderr.write(' (' + revision + ')')
-    except:
-        pass
-
+    if grass_version.endswith('svn'):
+        try:
+            filerev = open(os.path.join(gisbase, 'etc', 'VERSIONNUMBER'))
+            linerev = filerev.readline().rstrip('\n')
+            filerev.close()
+            
+            revision = linerev.split(' ')[1]
+            sys.stderr.write(' (' + revision + ')')
+        except:
+            pass
+    
 def show_info():
     sys.stderr.write(
 r"""
