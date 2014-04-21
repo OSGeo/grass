da9d42a0507574d0012431d92d597e8cde219466
Martin Landa <landa.martin@gmail.com>
2014-04-21 11:55:01 +0000

    g.gui: rename -u flag to -d        manual tuning
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@59873 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/g.gui/main.c
+++ general/g.gui/main.c
@@ -35,10 +35,11 @@ int main(int argc, char *argv[])
 
     module = G_define_module();
     G_add_keyword(_("general"));
-    G_add_keyword(_("user interface"));
     G_add_keyword(_("gui"));
-    module->description =
+    G_add_keyword(_("user interface"));
+    module->label =
 	_("Launches a GRASS graphical user interface (GUI) session.");
+    module->description = _("And updates default user interface settings.");
 
     type = G_define_option();
     type->key = "ui";
@@ -61,7 +62,7 @@ int main(int argc, char *argv[])
     rc_file->description = _("Name of workspace file to load on start-up (valid only for wxGUI)");
 
     update = G_define_flag();
-    update->key = 'u';
+    update->key = 'd';
     update->description = _("Update default user interface settings");
     update->guisection = _("Default");
 
