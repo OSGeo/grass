e1885983ef7343b3b451397a6041c4846cd3442d
Soeren Gebbert <soerengebbert@googlemail.com>
2014-05-11 00:33:35 +0000

    Added offset option to aggregation modules and tests. More tests that check correct error handling in case r.mapcalc fails in spatio-temporal processing.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@60174 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- temporal/t.rast.aggregate.ds/test.t.rast.aggregate.ds.sh
+++ temporal/t.rast.aggregate.ds/test.t.rast.aggregate.ds.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 # Aggregate a dataset by a second one
 
+export GRASS_OVERWRITE=1
+
 # We need to set a specific region in the
 # @preprocess step of this test. 
 # The region setting should work for UTM and LL test locations
 g.region s=0 n=80 w=0 e=120 b=0 t=50 res=10 res3=10 -p3
 
-r.mapcalc --o expr="prec_1 = 100"
-r.mapcalc --o expr="prec_2 = 150"
-r.mapcalc --o expr="prec_3 = 250"
-r.mapcalc --o expr="prec_4 = 250"
-r.mapcalc --o expr="prec_5 = 150"
-r.mapcalc --o expr="prec_6 = 100"
+r.mapcalc expr="prec_1 = 100"
+r.mapcalc expr="prec_2 = 150"
+r.mapcalc expr="prec_3 = 250"
+r.mapcalc expr="prec_4 = 250"
+r.mapcalc expr="prec_5 = 150"
+r.mapcalc expr="prec_6 = 100"
 
-v.random --o -z output=soil_1 n=20 zmin=0 zmax=100 column=height
-v.random --o -z output=soil_2 n=20 zmin=0 zmax=100 column=height
-v.random --o -z output=soil_3 n=20 zmin=0 zmax=100 column=height
+v.random -z output=soil_1 n=20 zmin=0 zmax=100 column=height
+v.random -z output=soil_2 n=20 zmin=0 zmax=100 column=height
+v.random -z output=soil_3 n=20 zmin=0 zmax=100 column=height
 
 n1=`g.tempfile pid=1 -d` 
 
@@ -25,15 +27,18 @@ soil_2|2001-05-01|2001-07-01
 soil_3|2001-08-01|2001-12-01
 EOF
 
-t.create --o type=stvds temporaltype=absolute output=soil_abs1 title="A test" descr="A test"
+t.create type=stvds temporaltype=absolute output=soil_abs1 title="A test" descr="A test"
 t.register type=vect input=soil_abs1 file="${n1}"
 
-t.create --o type=strds temporaltype=absolute output=precip_abs1 title="A test" descr="A test"
-t.register -i type=rast input=precip_abs1 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-03-01 00:00:00" increment="1 months"
+t.create type=strds temporaltype=absolute output=precip_abs1 title="A test" descr="A test"
+t.register -i type=rast input=precip_abs1 \
+    maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 \
+    start="2001-03-01 00:00:00" increment="1 months"
 
 # The @test
 
-t.rast.aggregate.ds --o --v input=precip_abs1 output=precip_abs2 base=prec_sum type=stvds sample=soil_abs1 method=sum sampling=start,during
+t.rast.aggregate.ds --v input=precip_abs1 output=precip_abs2 base=prec_sum \
+    type=stvds sample=soil_abs1 method=sum sampling=start,during offset=100
 t.info type=strds input=precip_abs2
 t.rast.list input=precip_abs2 method=deltagap
 
@@ -41,6 +46,5 @@ t.rast.list input=precip_abs2 method=deltagap
 t.unregister type=vect maps=soil_1,soil_2,soil_3
 t.remove type=stvds input=soil_abs1
 
-t.unregister type=rast maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6
-t.remove type=strds input=precip_abs1,precip_abs2
+t.remove -rf type=strds input=precip_abs1,precip_abs2
 g.remove vect=soil_1,soil_2,soil_3
--- temporal/t.register/test.t.register.raster.sh
+++ temporal/t.register/test.t.register.raster.sh
@@ -4,6 +4,8 @@
 # The raster maps will be registered in different space time raster
 # datasets.
 
+export GRASS_OVERWRITE=1
+
 # We need to set a specific region in the
 # @preprocess step of this test. We generate
 # raster with r.mapcalc and create several space time raster datasets
@@ -11,81 +13,81 @@
 # The region setting should work for UTM and LL test locations
 g.region s=0 n=80 w=0 e=120 b=0 t=50 res=10 res3=10 -p3
 
-r.mapcalc --o expr="prec_1 = rand(0, 550)"
-r.mapcalc --o expr="prec_2 = rand(0, 450)"
-r.mapcalc --o expr="prec_3 = rand(0, 320)"
-r.mapcalc --o expr="prec_4 = rand(0, 510)"
-r.mapcalc --o expr="prec_5 = rand(0, 300)"
-r.mapcalc --o expr="prec_6 = rand(0, 650)"
+r.mapcalc  expr="prec_1 = rand(0, 550)"
+r.mapcalc  expr="prec_2 = rand(0, 450)"
+r.mapcalc  expr="prec_3 = rand(0, 320)"
+r.mapcalc  expr="prec_4 = rand(0, 510)"
+r.mapcalc  expr="prec_5 = rand(0, 300)"
+r.mapcalc  expr="prec_6 = rand(0, 650)"
 
 # The first @test
 # We create the space time raster inputs and register the raster maps with absolute time interval
 
-t.create --o type=strds temporaltype=absolute output=precip_abs1 title="A test" descr="A test"
-t.create --o type=strds temporaltype=absolute output=precip_abs2 title="A test" descr="A test"
-t.create --o type=strds temporaltype=absolute output=precip_abs3 title="A test" descr="A test"
-t.create --o type=strds temporaltype=absolute output=precip_abs4 title="A test" descr="A test"
-t.create --o type=strds temporaltype=absolute output=precip_abs5 title="A test" descr="A test"
-t.create --o type=strds temporaltype=absolute output=precip_abs6 title="A test" descr="A test"
-t.create --o type=strds temporaltype=absolute output=precip_abs7 title="A test" descr="A test"
+t.create  type=strds temporaltype=absolute output=precip_abs1 title="A test" descr="A test"
+t.create  type=strds temporaltype=absolute output=precip_abs2 title="A test" descr="A test"
+t.create  type=strds temporaltype=absolute output=precip_abs3 title="A test" descr="A test"
+t.create  type=strds temporaltype=absolute output=precip_abs4 title="A test" descr="A test"
+t.create  type=strds temporaltype=absolute output=precip_abs5 title="A test" descr="A test"
+t.create  type=strds temporaltype=absolute output=precip_abs6 title="A test" descr="A test"
+t.create  type=strds temporaltype=absolute output=precip_abs7 title="A test" descr="A test"
 
-t.register --o -i input=precip_abs1 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="1 seconds"
+t.register  -i input=precip_abs1 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="1 seconds"
 t.info type=strds input=precip_abs1
 t.info -g type=strds input=precip_abs1
 r.info map=prec_1
 t.rast.list input=precip_abs1
 t.topology input=precip_abs1
 
-t.register --o -i input=precip_abs2 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="20 seconds, 5 minutes"
+t.register  -i input=precip_abs2 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="20 seconds, 5 minutes"
 t.info type=strds input=precip_abs2
 t.info -g type=strds input=precip_abs2
 r.info map=prec_1
 t.rast.list input=precip_abs2
 t.topology input=precip_abs2
 
-t.register --o -i input=precip_abs3 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="8 hours"
+t.register  -i input=precip_abs3 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="8 hours"
 t.info -g type=strds input=precip_abs3
 r.info map=prec_1
 t.rast.list input=precip_abs3
 t.topology input=precip_abs3
 
-t.register --o input=precip_abs4 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="3 days"
+t.register  input=precip_abs4 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="3 days"
 t.info -g type=strds input=precip_abs4
 r.info map=prec_1
 t.rast.list input=precip_abs4
 t.topology input=precip_abs4
 
-t.register --o input=precip_abs5 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="4 weeks"
+t.register  input=precip_abs5 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="4 weeks"
 t.info -g type=strds input=precip_abs5
 r.info map=prec_1
 t.rast.list input=precip_abs5
 t.topology input=precip_abs5
 
-t.register --o input=precip_abs6 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-08-01" increment="2 months"
+t.register  input=precip_abs6 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-08-01" increment="2 months"
 t.info -g type=strds input=precip_abs6
 r.info map=prec_1
 t.rast.list input=precip_abs6
 t.topology input=precip_abs6
 
-t.register --o input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="20 years, 3 months, 1 days, 4 hours"
+t.register  input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="20 years, 3 months, 1 days, 4 hours"
 t.info -g type=strds input=precip_abs7
 r.info map=prec_1
 t.rast.list input=precip_abs7
 t.topology input=precip_abs7
 # Register with different valid time again
-t.register --o input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="99 years, 9 months, 9 days, 9 hours"
+t.register  input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="99 years, 9 months, 9 days, 9 hours"
 t.info -g type=strds input=precip_abs7
 r.info map=prec_1
 t.rast.list input=precip_abs7
 t.topology input=precip_abs7
 # Register with different valid time again creating an interval
-t.register --o -i input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="99 years, 9 months, 9 days, 9 hours"
+t.register  -i input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="99 years, 9 months, 9 days, 9 hours"
 t.info -g type=strds input=precip_abs7
 r.info map=prec_1
 t.rast.list input=precip_abs7
 t.topology input=precip_abs7
 
-t.register --o input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" end="2002-01-01"
+t.register  input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" end="2002-01-01"
 t.info -g type=strds input=precip_abs7
 r.info map=prec_1
 t.rast.list input=precip_abs7
@@ -93,7 +95,7 @@ t.topology input=precip_abs7
 
 # Check for correct errors
 # Increment format error
-t.register -i --o input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="months"
+t.register -i  input=precip_abs7 maps=prec_1,prec_2,prec_3,prec_4,prec_5,prec_6 start="2001-01-01" increment="months"
 
 
 t.unregister type=rast maps=prec_1,prec_2,prec_3
