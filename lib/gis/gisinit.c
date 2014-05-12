0a006a80b65a8ab147ba02fc041624632f647342
Huidae Cho <grass4u@gmail.com>
2014-05-12 03:21:22 +0000

    Added a space between sentences
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@60208 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/gisinit.c
+++ lib/gis/gisinit.c
@@ -49,7 +49,7 @@ void G__gisinit(const char *version, const char *pgm)
 
     if (strcmp(version, GIS_H_VERSION) != 0)
 	G_fatal_error(_("Module built against version %s but "
-			"trying to use version %s."
+			"trying to use version %s. "
 			"You need to rebuild GRASS GIS or untangle multiple installations."),
                         version, GIS_H_VERSION);
 
