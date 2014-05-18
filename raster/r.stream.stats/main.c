fb2d1d32d7c752f36a56f38af480902d397fc192
Huidae Cho <grass4u@gmail.com>
2014-05-18 14:43:46 +0000

    r.stream.*: option descriptions merged from https://trac.osgeo.org/grass/changeset/60308
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@60317 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.stream.stats/main.c
+++ raster/r.stream.stats/main.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
 
     in_stm_opt = G_define_standard_option(G_OPT_R_INPUT);
     in_stm_opt->key = "stream_rast";
-    in_stm_opt->description = _("Name for input raster map with stream network");
+    in_stm_opt->description = _("Name of input raster map with stream network");
 
     in_dir_opt = G_define_standard_option(G_OPT_R_INPUT);
     in_dir_opt->key = "direction";
