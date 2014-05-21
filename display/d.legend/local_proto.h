b539caa4e66c4f07c3dbdd109ecefad14a38bbde
Hamish Bowman <hamish_b@yahoo.com>
2014-05-21 02:24:08 +0000

    add support for histogram sidebars (https://trac.osgeo.org/grass/ticket/1049); add support for 3D raster map legends (https://trac.osgeo.org/grass/ticket/2083, thanks Vaclav); display map data units, if known [merge from trunk]
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@60379 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.legend/local_proto.h
+++ display/d.legend/local_proto.h
@@ -1,5 +1,34 @@
-/* main.c */
-char *G_chop(char *s);
+#define MAP_TYPE_RASTER2D 1
+#define MAP_TYPE_RASTER3D 2
+/* possibles for the future:
+#define MAP_TYPE_VECTOR 3
+#define MAP_TYPE_RULES 4
+*/
 
-/* box.c */
-int get_legend_box(int *, int *, int *, int *);
+struct stat_node
+{
+    long int cat;               /* cell-file category value */
+    long int stat;              /* statistic: number of cells with that cat */
+    struct stat_node *next;     /* pointer to next stat_node in list */
+};
+
+struct stat_list
+{
+    struct stat_node *ptr;      /* pointer to first stat_node in list */
+    long int count,             /* number of stat_nodes in list 
+                                   (not counting null cells) */
+      null_stat,                /* stats for null cell */
+      maxstat,                  /* max. statistic in list */
+      minstat,                  /* min. statistic in list */
+      sumstat,                  /* sum of all statistics in list */
+      maxcat,                   /* max. cell-file category value in list */
+      mincat;                   /* min. cell-file category value in list */
+};
+
+
+/* histogram.c */
+void draw_histogram(const char *, int, int, int, int, int, int, int, int);
+
+/* get_stats.c */
+void get_stats(const char *, struct stat_list *, int, int);
+void run_stats(const char *, int, const char *, int);
