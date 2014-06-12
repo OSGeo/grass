61e9364c6b2c6bbe5070a56c05aa6c5ebd9d6eea
Yann Chemin <yann.chemin@gmail.com>
2014-06-12 11:03:48 +0000

    better temporary fix
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@60797 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.albedo/bb_alb_landsat8.c
+++ imagery/i.albedo/bb_alb_landsat8.c
@@ -3,14 +3,24 @@
  * Temporary until a publication creates an algorithm
 -* chan5 is OLI Band 6 (1.57-1.65) 
  * chan7 is OLI band 7 (2.11-2.29)
+ * 
+ * Temporary better fix than weighted average
+ * ------------------------------------------
+ * r.regression.multi
+ * mapx=LC81270512014115LGN00.toar.1,LC81270512014115LGN00.toar.2,
+ * LC81270512014115LGN00.toar.3,LC81270512014115LGN00.toar.4,
+ * LC81270512014115LGN00.toar.5,LC81270512014115LGN00.toar.6,
+ * LC81270512014115LGN00.toar.7
+ * mapy=MCD43_2014113 
+ * 
  */
-double bb_alb_landsat8(double bluechan, double greenchan, double redchan,
-		      double nirchan, double chan5, double chan7)
+double bb_alb_landsat8(double shortbluechan, double bluechan, double greenchan, double redchan,
+              double nirchan, double chan5, double chan7)
 {
     double result;
 
-    result =
-	(0.06 * bluechan + 0.06 * greenchan + 0.03 * redchan +
-	 0.03 * nirchan + 0.08 * chan5 + 0.18 * chan7)/0.44;
+    result = 0.058674+shortbluechan*2.153642+bluechan*(-2.242688)+
+    greenchan*(-0.520669)+redchan*0.622670+nirchan*0.129979+
+    chan5*(-0.047970)+chan7*0.152228;
     return result;
 }
