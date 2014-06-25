1eae5aa9f281bbe54321a42a8949d9e7f6fe3237
Huidae Cho <grass4u@gmail.com>
2014-06-25 13:03:21 +0000

    r.to.vect: Removed unnecessary variables
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@60966 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.to.vect/lines.c
+++ raster/r.to.vect/lines.c
@@ -495,13 +495,10 @@ static int update_list(int count)
 	    v_list[col - 1].right = start_line(1);
 	break;
     default:
-	{
-	    double x, y;
-	    x = Rast_col_to_easting((double)(col - .5), &cell_head);
-	    y = Rast_row_to_northing((double)(row + .5), &cell_head);
-	    G_message(_("Crowded cell at (%f, %f): row %d, col %d, count %d"),
-		      x, y, row, col - 1, count);
-	}
+	G_message(_("Crowded cell at (%f, %f): row %d, col %d, count %d"),
+		  Rast_col_to_easting((double)col - .5, &cell_head),
+		  Rast_row_to_northing((double)row + .5, &cell_head),
+		  row, col - 1, count);
 	G_fatal_error(_("Raster map is not thinned properly.\nPlease run r.thin."));
     }				/* switch count */
 
