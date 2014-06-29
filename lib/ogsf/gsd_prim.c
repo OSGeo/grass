ebc13fd12d7ca2472e5cf71c8792c46891bfb8ec
Huidae Cho <grass4u@gmail.com>
2014-06-29 10:23:06 +0000

    libogsf: Avoid double calling the same function with the same argument. Change the prototypes of gsd_*buffer()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@61052 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/ogsf/gsd_prim.c
+++ lib/ogsf/gsd_prim.c
@@ -433,7 +433,7 @@ int gsd_getshademodel(void)
 /*!
    \brief ADD
  */
-void gsd_bothbuffer(void)
+void gsd_bothbuffers(void)
 {
     /* OGLXXX frontbuffer: other possibilities include GL_FRONT_AND_BACK */
     glDrawBuffer(GL_FRONT_AND_BACK);
@@ -447,10 +447,10 @@ void gsd_bothbuffer(void)
 
    \param bool non-zero for enable otherwise disable front buffer
  */
-void gsd_frontbuffer(int bool)
+void gsd_frontbuffer(void)
 {
     /* OGLXXX frontbuffer: other possibilities include GL_FRONT_AND_BACK */
-    glDrawBuffer((bool) ? GL_FRONT : GL_BACK);
+    glDrawBuffer(GL_FRONT);
 
     return;
 }
@@ -461,10 +461,10 @@ void gsd_frontbuffer(int bool)
 
    \param bool non-zero for enable otherwise disable back buffer
  */
-void gsd_backbuffer(int bool)
+void gsd_backbuffer(void)
 {
     /* OGLXXX backbuffer: other possibilities include GL_FRONT_AND_BACK */
-    glDrawBuffer((bool) ? GL_BACK : GL_FRONT);
+    glDrawBuffer(GL_BACK);
     return;
 }
 
