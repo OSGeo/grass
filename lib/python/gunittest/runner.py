34d546a8e3a37d5df64ddd5a12addb61da3e006e
Vaclav Petras <wenzeslaus@gmail.com>
2014-07-24 22:23:56 +0000

    gunittest: multi test result class and key-value report for one test file and their aggregation (aggregation not well tested)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@61392 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/gunittest/runner.py
+++ lib/python/gunittest/runner.py
@@ -18,7 +18,7 @@ a template. It is not expected that something will left.
 import sys
 import time
 
-from unittest import result
+from unittest.result import TestResult
 from unittest.signals import registerResult
 
 __unittest = True
@@ -40,7 +40,7 @@ class _WritelnDecorator(object):
         self.write('\n') # text-mode streams translate to \r\n if needed
 
 
-class TextTestResult(result.TestResult):
+class TextTestResult(TestResult):
     """A test result class that can print formatted text results to a stream.
 
     Used by TextTestRunner.
@@ -49,11 +49,16 @@ class TextTestResult(result.TestResult):
     separator2 = '-' * 70
 
     def __init__(self, stream, descriptions, verbosity):
-        super(TextTestResult, self).__init__(stream, descriptions, verbosity)
-        self.stream = stream
+        super(TextTestResult, self).__init__(
+            stream=stream, descriptions=descriptions, verbosity=verbosity)
+        self.stream = _WritelnDecorator(stream)
         self.showAll = verbosity > 1
         self.dots = verbosity == 1
         self.descriptions = descriptions
+
+        self.start_time = None
+        self.end_time = None
+        self.time_taken = None
 
     def getDescription(self, test):
         doc_first_line = test.shortDescription()
@@ -126,35 +131,310 @@ class TextTestResult(result.TestResult):
     def printErrorList(self, flavour, errors):
         for test, err in errors:
             self.stream.writeln(self.separator1)
-            self.stream.writeln("%s: %s" % (flavour,self.getDescription(test)))
+            self.stream.writeln("%s: %s" % (flavour,
+                                            self.getDescription(test)))
             self.stream.writeln(self.separator2)
             self.stream.writeln("%s" % err)
 
+    def setTimes(self, start_time, end_time, time_taken):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.time_taken = time_taken
+
+    def stopTestRun(self):
+        super(TextTestResult, self).stopTestRun()
+        self.printErrors()
+        self.stream.writeln(self.separator2)
+        run = self.testsRun
+        self.stream.write("Ran %d test%s" % (run, run != 1 and "s" or ""))
+        if self.time_taken:
+            self.stream.write(" in %.3fs" % (self.time_taken))
+        self.stream.writeln()
+
+        expectedFails = unexpectedSuccesses = skipped = 0
+        results = map(len, (self.expectedFailures,
+                            self.unexpectedSuccesses,
+                            self.skipped))
+        expectedFails, unexpectedSuccesses, skipped = results
+
+        infos = []
+        if not self.wasSuccessful():
+            self.stream.write("FAILED")
+            failed, errored = map(len, (self.failures, self.errors))
+            if failed:
+                infos.append("failures=%d" % failed)
+            if errored:
+                infos.append("errors=%d" % errored)
+        else:
+            self.stream.write("OK")
+        if skipped:
+            infos.append("skipped=%d" % skipped)
+        if expectedFails:
+            infos.append("expected_failures=%d" % expectedFails)
+        if unexpectedSuccesses:
+            infos.append("unexpected_successes=%d" % unexpectedSuccesses)
+        if infos:
+            self.stream.writeln(" (%s)" % (", ".join(infos),))
+        else:
+            self.stream.write("\n")
+
+
+class KeyValueTestResult(TestResult):
+    """A test result class that can print formatted text results to a stream.
+
+    Used by TextTestRunner.
+    """
+    separator1 = '=' * 70
+    separator2 = '-' * 70
+
+    def __init__(self, stream, test_type=None):
+        super(KeyValueTestResult, self).__init__(
+            stream=stream, descriptions=None, verbosity=None)
+        self._stream = _WritelnDecorator(stream)
+
+        self.start_time = None
+        self.end_time = None
+        self.time_taken = None
+
+        if test_type:
+            self.test_type = test_type
+        else:
+            self.test_type = 'not-specified'
+
+        self._grass_modules = []
+
+    def setTimes(self, start_time, end_time, time_taken):
+        self.start_time = start_time
+        self.end_time = end_time
+        self.time_taken = time_taken
+
+    def stopTest(self, test):
+        super(KeyValueTestResult, self).stopTest(test)
+        if hasattr(test, 'grass_modules'):
+            self._grass_modules.extend(test.grass_modules)
+
+    def stopTestRun(self):
+        super(KeyValueTestResult, self).stopTestRun()
+        infos = []
+
+        run = self.testsRun
+        # TODO: name should be included by test file caller
+        # from inspect import getsourcefile
+        # from os.path import abspath
+        # abspath(getsourcefile(lambda _: None))
+        # not writing name is a good option
+        # infos.append("name=%s" % 'unknown')
+
+        infos.append("time=%.3fs" % (self.time_taken))
+#            'date={rundate}\n'
+#            'date={runtime}\n'
+#            'date={start_datetime}\n'
+#            'date={end_datetime}\n'
+
+        results = map(len, (self.expectedFailures,
+                            self.unexpectedSuccesses,
+                            self.skipped))
+        expectedFails, unexpectedSuccesses, skipped = results
+
+        infos.append("status=%s" % ('failed' if self.wasSuccessful() else 'passed'))
+
+        infos.append("total=%d" % (run))
+        failed, errored = map(len, (self.failures, self.errors))
+        infos.append("failures=%d" % failed)
+        infos.append("errors=%d" % errored)
+        infos.append("skipped=%d" % skipped)
+
+        # TODO: document this: if not supported by view,
+        # expected_failures should be counted as failures and vice versa
+        # or both add to skipped as unclear?
+        infos.append("expected_failures=%d" % expectedFails)
+        infos.append("unexpected_successes=%d" % unexpectedSuccesses)
+
+        # TODO: include each module just once? list good and bad modules?
+        infos.append("modules=%s" % ','.join(self._grass_modules))
+        
+        # module, modules?, c, c++?, python
+        # TODO: include also type modules?
+        # TODO: include also C++ code?
+        # TODO: distinguish C and Python modules?
+        infos.append("test_type=%s" % (self.test_type))
+
+        self._stream.write('\n'.join(infos))
+        self._stream.write('\n')
+        self._stream.flush()
+
+
+class MultiTestResult(TestResult):
+    # descriptions and verbosity unused
+    # included for compatibility with unittest's TestResult
+    # where are also unused, so perhaps we can remove them
+    # stream set to None and not included in interface, it would not make sense
+    def __init__(self, results, forgiving=False,
+                 descriptions=None, verbosity=None):
+        super(MultiTestResult, self).__init__(
+            descriptions=descriptions, verbosity=verbosity, stream=None)
+        self._results = results
+        self._forgiving = forgiving
+
+    def startTest(self, test):
+        super(MultiTestResult, self).startTest(test)
+        for result in self._results:
+            try:
+                result.startTest(test)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def stopTest(self, test):
+        """Called when the given test has been run"""
+        super(MultiTestResult, self).stopTest(test)
+        for result in self._results:
+            try:
+                result.stopTest(test)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def addSuccess(self, test):
+        super(MultiTestResult, self).addSuccess(test)
+        for result in self._results:
+            try:
+                result.addSuccess(test)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def addError(self, test, err):
+        super(MultiTestResult, self).addError(test, err)
+        for result in self._results:
+            try:
+                result.addSuccess(test)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def addFailure(self, test, err):
+        super(MultiTestResult, self).addFailure(test, err)
+        for result in self._results:
+            try:
+                result.addSuccess(test)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def addSkip(self, test, reason):
+        super(MultiTestResult, self).addSkip(test, reason)
+        for result in self._results:
+            try:
+                result.addSuccess(test)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def addExpectedFailure(self, test, err):
+        super(MultiTestResult, self).addExpectedFailure(test, err)
+        for result in self._results:
+            try:
+                result.addSuccess(test)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def addUnexpectedSuccess(self, test):
+        super(MultiTestResult, self).addUnexpectedSuccess(test)
+        for result in self._results:
+            try:
+                result.addSuccess(test)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def printErrors(self):
+        "Called by TestRunner after test run"
+        super(MultiTestResult, self).printErrors()
+        for result in self._results:
+            try:
+                result.printErrors()
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def startTestRun(self):
+        """Called once before any tests are executed.
+
+        See startTest for a method called before each test.
+        """
+        super(MultiTestResult, self).startTestRun()
+        for result in self._results:
+            try:
+                result.startTestRun()
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    def stopTestRun(self):
+        """Called once after all tests are executed.
+
+        See stopTest for a method called after each test.
+        """
+        super(MultiTestResult, self).stopTestRun()
+        for result in self._results:
+            try:
+                result.stopTestRun()
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
+    # TODO: better would be to pass start at the beginning
+    # alternative is to leave counting time on class
+    # TODO: document: we expect all grass classes to have setTimes
+    # TODO: alternatively, be more forgiving for non-unittest methods
+    def setTimes(self, start_time, end_time, time_taken):
+        for result in self._results:
+            try:
+                result.setTimes(start_time, end_time, time_taken)
+            except AttributeError:
+                if self._forgiving:
+                    pass
+                else:
+                    raise
+
 
 class GrassTestRunner(object):
-    """A test runner class that displays results in textual form.
-
-    It prints out the names of tests as they are run, errors as they
-    occur, and a summary of the results at the end of the test run.
-    """
-    resultclass = TextTestResult
-
     def __init__(self, stream=sys.stderr, descriptions=True, verbosity=1,
-                 failfast=False, buffer=False, resultclass=None):
+                 failfast=False, buffer=False, result=None):
         self.stream = _WritelnDecorator(stream)
         self.descriptions = descriptions
         self.verbosity = verbosity
         self.failfast = failfast
         self.buffer = buffer
-        if resultclass is not None:
-            self.resultclass = resultclass
-
-    def _makeResult(self):
-        return self.resultclass(self.stream, self.descriptions, self.verbosity)
+        self._result = result
 
     def run(self, test):
         "Run the given test case or test suite."
-        result = self._makeResult()
+        result = self._result
         registerResult(result)
         result.failfast = self.failfast
         result.buffer = self.buffer
@@ -165,47 +445,13 @@ class GrassTestRunner(object):
         try:
             test(result)
         finally:
+            stopTime = time.time()
+            timeTaken = stopTime - startTime
+            setTimes = getattr(result, 'setTimes', None)
+            if setTimes is not None:
+                setTimes(startTime, stopTime, timeTaken)
             stopTestRun = getattr(result, 'stopTestRun', None)
             if stopTestRun is not None:
                 stopTestRun()
-        stopTime = time.time()
-        timeTaken = stopTime - startTime
-        result.printErrors()
-        if hasattr(result, 'separator2'):
-            self.stream.writeln(result.separator2)
-        run = result.testsRun
-        self.stream.writeln("Ran %d test%s in %.3fs" %
-                            (run, run != 1 and "s" or "", timeTaken))
-        self.stream.writeln()
 
-        expectedFails = unexpectedSuccesses = skipped = 0
-        try:
-            results = map(len, (result.expectedFailures,
-                                result.unexpectedSuccesses,
-                                result.skipped))
-        except AttributeError:
-            pass
-        else:
-            expectedFails, unexpectedSuccesses, skipped = results
-
-        infos = []
-        if not result.wasSuccessful():
-            self.stream.write("FAILED")
-            failed, errored = map(len, (result.failures, result.errors))
-            if failed:
-                infos.append("failures=%d" % failed)
-            if errored:
-                infos.append("errors=%d" % errored)
-        else:
-            self.stream.write("OK")
-        if skipped:
-            infos.append("skipped=%d" % skipped)
-        if expectedFails:
-            infos.append("expected failures=%d" % expectedFails)
-        if unexpectedSuccesses:
-            infos.append("unexpected successes=%d" % unexpectedSuccesses)
-        if infos:
-            self.stream.writeln(" (%s)" % (", ".join(infos),))
-        else:
-            self.stream.write("\n")
         return result
