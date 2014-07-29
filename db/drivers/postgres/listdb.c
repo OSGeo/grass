6d0d50fa040124c51bbb1c0fc9795f4d9f6fe5c1
Markus Neteler <neteler@gmail.com>
2014-07-29 05:02:12 +0000

    db pg driver: debug msg backport
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@61444 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/drivers/postgres/listdb.c
+++ db/drivers/postgres/listdb.c
@@ -42,8 +42,9 @@ int db__driver_list_databases(dbString * dbpath, int npaths,
         return DB_FAILED;
     }
     
-    G_debug(3, "host = %s, port = %s, options = %s, tty = %s",
-	    pgconn.host, pgconn.port, pgconn.options, pgconn.tty);
+    G_debug(1, "db = %s, user = %s, pass = %s, host = %s, port = %s, options = %s, tty = %s",
+	    pgconn.dbname, pgconn.user, pgconn.password, pgconn.host,
+            pgconn.port, pgconn.options, pgconn.tty);
 
     db_get_login("pg", NULL, &user, &passwd);
     G_debug(1, "user = %s, passwd = %s", user, passwd ? "xxx" : "");
