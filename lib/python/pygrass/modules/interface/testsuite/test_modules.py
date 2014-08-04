508489c054a7789337cbb45afe0a48d07b51c85f
Pietro Zambelli <peter.zamb@gmail.com>
2014-08-04 12:44:03 +0000

    pygrass: exclude modules: g.parser and g.gui.* from the testsuite
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@61511 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/pygrass/modules/interface/testsuite/test_modules.py
+++ lib/python/pygrass/modules/interface/testsuite/test_modules.py
@@ -4,13 +4,14 @@ Created on Tue Jun 24 09:43:53 2014
 
 @author: pietro
 """
+from fnmatch import fnmatch
 from grass.gunittest import TestCase, test
 
 from grass.script.core import get_commands
 from grass.pygrass.modules.interface import Module
 
 
-SKIP = []
+SKIP = ["g.parser", ]
 
 
 class TestModulesMeta(type):
@@ -21,7 +22,8 @@ class TestModulesMeta(type):
                 Module(cmd)
             return test
 
-        cmds = [c for c in sorted(list(get_commands()[0])) if c not in SKIP]
+        cmds = [c for c in sorted(list(get_commands()[0]))
+                if c not in SKIP and not fnmatch(c, "g.gui.*")]
         for cmd in cmds:
             test_name = "test__%s" % cmd.replace('.', '_')
             dict[test_name] = gen_test(cmd)
