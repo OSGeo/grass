9aebf64da37d0fe51209fc8d1b6bb201e734cd77
Luca Delucchi <lucadeluge@gmail.com>
2014-08-04 21:54:45 +0000

    v.mkgrid: added support to lines output through -l flag (ticket #394)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@61519 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.mkgrid/local_proto.h
+++ vector/v.mkgrid/local_proto.h
@@ -2,6 +2,6 @@
 void rotate(double *, double *, double, double, double);
 
 /* write_grid.c */
-int write_grid(struct grid_description *, struct Map_info *, int);
+int write_grid(struct grid_description *, struct Map_info *, int, int);
 int write_vect(double, double, double, double, struct Map_info *,
-	       struct line_pnts *);
+	       struct line_pnts *, int);
