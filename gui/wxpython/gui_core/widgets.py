b378489e191290048d36fdc289b52f5ea98d26e5
Anna Petrášová <kratochanna@gmail.com>
2014-08-06 20:03:22 +0000

    wxGUI/search: fix finding next match on Mac - workaround for Enter resulting in EVT_TEXT (merge from trunk, r61554)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@61555 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_core/widgets.py
+++ gui/wxpython/gui_core/widgets.py
@@ -843,6 +843,7 @@ class SearchModuleWidget(wx.Panel):
         self._results = [] # list of found nodes
         self._resultIndex = -1
         self._searchKeys = ['description', 'keywords', 'command']
+        self._oldValue = ''
         
         self.moduleSelected = Signal('SearchModuleWidget.moduleSelected')
         self.showSearchResult = Signal('SearchModuleWidget.showSearchResult')
@@ -913,6 +914,11 @@ class SearchModuleWidget(wx.Panel):
     def OnSearchModule(self, event):
         """!Search module by keywords or description"""
         value = self._search.GetValue()
+        if value == self._oldValue:
+            event.Skip()
+            return
+        self._oldValue = value
+
         if len(value) <= 2:
             if len(value) == 0: # reset
                 commands = self._searchModule(keys=['command'], value='')
