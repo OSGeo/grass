c4a214bb80dd1bd9be596afba3731be420accd82
Markus Metz <markus.metz.giswork@gmail.com>
2014-09-16 20:05:31 +0000

    move RGBA_Color to display.h
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@62002 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/display.h
+++ include/display.h
@@ -5,6 +5,18 @@
 #include <grass/raster.h>
 #include <grass/symbol.h>
 
+struct color_rgba
+{
+    unsigned char r, g, b, a;	/* red, green, blue, and alpha */
+};
+
+typedef struct color_rgba RGBA_Color;
+
+/* RGBA color alpha presets */
+#define RGBA_COLOR_OPAQUE     255
+#define RGBA_COLOR_TRANSPARENT  0
+#define RGBA_COLOR_NONE         0
+
 enum clip_mode
 {
     M_NONE,
