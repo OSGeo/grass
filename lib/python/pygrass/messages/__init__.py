9e94f0cd8d1c82600050266aeccad268a3152262
Soeren Gebbert <soerengebbert@googlemail.com>
2014-09-17 17:52:37 +0000

    pygrass messages: Better string length libgis restriction handling
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@62021 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/pygrass/messages/__init__.py
+++ lib/python/pygrass/messages/__init__.py
@@ -75,7 +75,11 @@ def message_server(lock, conn):
             libgis.G_debug(1, "Stop messenger server")
             sys.exit()
 
-        message = data[1]
+        message = data[1]            
+        # libgis limitation
+        if isinstance(message,  type(" ")):
+            if len(message) >= 2000:
+                message = message[:1999]
 
         if message_type == "PERCENT":
             n = int(data[1])
@@ -85,40 +89,19 @@ def message_server(lock, conn):
         elif message_type == "DEBUG":
             level = data[1]
             message = data[2]
-            # libgis limitation
-            if len(message) >= 2000:
-                message = message[:1999]
             libgis.G_debug(level, message)
         elif message_type == "VERBOSE":
-            # libgis limitation
-            if len(message) >= 2000:
-                message = message[:1999]
             libgis.G_verbose_message(message)
         elif message_type == "INFO":
-            # libgis limitation
-            if len(message) >= 2000:
-                message = message[:1999]
             libgis.G_message(message)
         elif message_type == "IMPORTANT":
-            # libgis limitation
-            if len(message) >= 2000:
-                message = message[:1999]
             libgis.G_important_message(message)
         elif message_type == "WARNING":
-            # libgis limitation
-            if len(message) >= 2000:
-                message = message[:1999]
             libgis.G_warning(message)
         elif message_type == "ERROR":
-            # libgis limitation
-            if len(message) >= 2000:
-                message = message[:1999]
             libgis.G_important_message("ERROR: %s"%message)
         # This is for testing only
         elif message_type == "FATAL":
-            # libgis limitation
-            if len(message) >= 2000:
-                message = message[:1999]
             libgis.G_fatal_error(message)
 
         lock.release()
