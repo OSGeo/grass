e464bed84b0ad8c7cc34642a0342ca3c3e141e93
Anna Petrášová <kratochanna@gmail.com>
2014-10-09 09:11:48 +0000

    g.list/remove: do not add mapset to the selected name in GUI
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@62220 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/g.list/main.c
+++ general/g.list/main.c
@@ -93,7 +93,7 @@ int main(int argc, char *argv[])
     opt.pattern->type = TYPE_STRING;
     opt.pattern->required = NO;
     opt.pattern->multiple = NO;
-    opt.pattern->gisprompt = "old,element,element";
+    opt.pattern->gisprompt = "new,element,element";
     opt.pattern->description = _("Map name search pattern (default: all)");
     opt.pattern->guisection = _("Pattern");
 
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
     opt.exclude->type = TYPE_STRING;
     opt.exclude->required = NO;
     opt.exclude->multiple = NO;
-    opt.exclude->gisprompt = "old,element,element";
+    opt.exclude->gisprompt = "new,element,element";
     opt.exclude->description = _("Map name exclusion pattern (default: none)");
     opt.exclude->guisection = _("Pattern");
 
