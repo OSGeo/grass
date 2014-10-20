96ab73bef93c3f6949fa874257b5de077eeb987e
Huidae Cho <grass4u@gmail.com>
2014-10-20 14:40:27 +0000

    g.remove: names= and ignore= accept fully qualified names
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@62303 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ general/g.remove/construct_pattern.c
@@ -0,0 +1,52 @@
+#include <string.h>
+#include <grass/gis.h>
+#include <grass/glocale.h>
+
+char *construct_pattern(char **names)
+{
+    char *pattern, *p;
+    int i, len, found_illegal_names;
+    const char *mapset;
+    char xname[GNAME_MAX], xmapset[GMAPSET_MAX];
+
+    len = 0;
+    for (i = 0; names[i]; i++) {
+	if ((p = strchr(names[i], '@')))
+	    len += p - names[i];
+	else
+	    len += strlen(names[i]);
+    }
+    len += i; /* # names - 1 commas + \0 */
+
+    pattern = p = (char *)G_malloc(len);
+
+    mapset = G_mapset();
+    found_illegal_names = 0;
+
+    for (i = 0; names[i]; i++) {
+	char *name;
+
+	name = names[i];
+	if (G_name_is_fully_qualified(name, xname, xmapset)) {
+	    if (strcmp(xmapset, mapset) != 0)
+		G_fatal_error(_("%s: Cannot remove or exclude files not in "
+				"the current mapset."), name);
+	    name = xname;
+	}
+
+	if (G_legal_filename(name) == -1)
+	    found_illegal_names = 1;
+
+	if (!i)
+	    strcpy(p, name);
+	else
+	    sprintf(p++, ",%s", name);
+	p += strlen(name);
+    }
+
+    if (found_illegal_names)
+	G_fatal_error(_("Illegal filenames not allowed in the names or ignore "
+			"option."));
+
+    return pattern;
+}
