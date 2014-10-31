d7c75172dc2aa811087675080062ce6079ce9ff3
Markus Neteler <neteler@gmail.com>
2014-10-31 15:04:24 +0000

    i.modis.qc: comment unproductive flag checking for MOD09CMG (trunk, https://trac.osgeo.org/grass/changeset/62503)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@62504 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.modis.qc/mod09CMGa.c
+++ imagery/i.modis.qc/mod09CMGa.c
@@ -1,8 +1,8 @@
 /* MODLAND QA Bits 500m long int bits[0-1]
  * 00 -> class 0: Corrected product produced at ideal quality -- all bands
- * 01 -> class 1: Corrected product produced at less than idel quality -- some or all bands
+ * 01 -> class 1: Corrected product produced at less than ideal quality -- some or all bands
  * 10 -> class 2: Corrected product NOT produced due to cloud effect -- all bands
- * 11 -> class 3: Corrected product NOT produced due to other reasons -- some or all bands mayb be fill value (Note that a value of [11] overrides a value of [01])
+ * 11 -> class 3: Corrected product NOT produced due to other reasons -- some or all bands maybe fill value (Note that a value of [11] overrides a value of [01])
  */  
 
 #include <grass/raster.h>
