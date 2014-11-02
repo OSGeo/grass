OSGeo/grass

commit 184211a27373750e840ea79245093b7d212747a3

wxGUI: check named region name for illegal characters only when saving the region
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@61356 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/gui_core/dialogs.py
@@ -509,7 +509,8 @@ class SavedRegion(wx.Dialog):
         
         box.Add(item=self._selection, proportion=0, flag=wx.ALIGN_CENTRE | wx.ALL, border=5)
         self._selection.SetFocus()
-        self._selection.Bind(wx.EVT_TEXT, self.OnRegion)
+        if self.loadsave == 'save':
+            self._selection.Bind(wx.EVT_TEXT, self.OnRegion)
         
         sizer.Add(item = box, proportion = 0, flag = wx.GROW | wx.ALIGN_CENTER_VERTICAL | wx.ALL,
                   border = 5)

commit 4a706f6d6918ed5ae63060ce972633fb7bdc71d7

wxGUI: fix zoom to saved region, broken by r61356
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@62541 15284696-431f-4ddb-bdfa-cd5b030d7da7

gui/wxpython/gui_core/dialogs.py
@@ -510,8 +510,7 @@ class SavedRegion(wx.Dialog):
         
         box.Add(item=self._selection, proportion=0, flag=wx.ALIGN_CENTRE | wx.ALL, border=5)
         self._selection.SetFocus()
-        if self.loadsave == 'save':
-            self._selection.Bind(wx.EVT_TEXT, self.OnRegion)
+        self._selection.Bind(wx.EVT_TEXT, self.OnRegion)
         
         sizer.Add(item = box, proportion = 0, flag = wx.GROW | wx.ALIGN_CENTER_VERTICAL | wx.ALL,
                   border = 5)
@@ -538,6 +537,8 @@ class SavedRegion(wx.Dialog):
 
     def OnRegion(self, event):
         value = self._selection.GetValue()
+        if '@' in value:
+            value = value.rsplit('@', 1)[0]
         if not grass.legal_name(value):
             GMessage(parent=self,
                      message=_("Name cannot begin with '.' "