c4e202b19c8a4bd6299fc4eb76175cdd7d437826
Martin Landa <landa.martin@gmail.com>
2014-11-16 12:53:20 +0000

    wxGUI: fix 'File -> Run model'        (merge https://trac.osgeo.org/grass/changeset/62757 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@62758 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/lmgr/frame.py
+++ gui/wxpython/lmgr/frame.py
@@ -510,7 +510,7 @@ class GMFrame(wx.Frame):
         
         self.model = Model()
         self.model.LoadModel(filename)
-        self.model.Run(log = self._goutput, onDone = self.OnDone, parent = self)
+        self.model.Run(log = self.GetLogWindow(), onDone = self.OnDone, parent = self)
         
         dlg.Destroy()
         
