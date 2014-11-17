d92d97c99f87b6234eacdab3be83053912e86f16
Luca Delucchi <lucadeluge@gmail.com>
2014-11-17 08:48:04 +0000

    manual: cliccable topics refs, anchors for keyword page, pep8 cleanup
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@62762 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- man/build_topics.py
+++ man/build_topics.py
@@ -7,7 +7,6 @@
 import os
 import sys
 import glob
-import string
 from build_html import *
 
 path = sys.argv[1]
@@ -15,23 +14,24 @@ year = os.getenv("VERSION_DATE")
 
 keywords = {}
 
-htmlfiles = glob.glob1(path,'*.html')
+htmlfiles = glob.glob1(path, '*.html')
 
 for fname in htmlfiles:
-    fil = open(os.path.join(path,fname))
+    fil = open(os.path.join(path, fname))
     # TODO maybe move to Python re (regex)
-    lines=fil.readlines()
+    lines = fil.readlines()
     try:
         index_keys = lines.index('<h2>KEYWORDS</h2>\n')+1
         index_desc = lines.index('<h2>NAME</h2>\n')+1
     except:
-        continue 
+        continue
     try:
         key = lines[index_keys].split(',')[1].strip().capitalize().replace(' ', '_')
+        key = key.split('>')[1].split('<')[0]
     except:
         continue
     try:
-        desc = lines[index_desc].split('-',1)[1].strip()
+        desc = lines[index_desc].split('-', 1)[1].strip()
     except:
         desc.strip()
     if key not in keywords.keys():
@@ -40,23 +40,24 @@ for fname in htmlfiles:
     elif fname not in keywords[key]:
         keywords[key][fname] = desc
 
-topicsfile = open(os.path.join(path,'topics.html'),'w')
+topicsfile = open(os.path.join(path, 'topics.html'), 'w')
 topicsfile.write(header1_tmpl.substitute(title = "GRASS GIS " \
                         "%s Reference Manual: Topics index" % grass_version))
 topicsfile.write(headertopics_tmpl)
+
 for key, values in sorted(keywords.iteritems()):
-    topicsfile.writelines([moduletopics_tmpl.substitute(key = key.lower(), 
-                                                        name = key.replace('_', ' '))])
-    keyfile = open(os.path.join(path,'topic_%s.html' % key.lower()),'w')
+    topicsfile.writelines([moduletopics_tmpl.substitute(key=key.lower(),
+                                                        name=key.replace('_', ' '))])
+    keyfile = open(os.path.join(path, 'topic_%s.html' % key.lower()), 'w')
     keyfile.write(header1_tmpl.substitute(title = "GRASS GIS " \
-                        "%s Reference Manual: Topic %s" % (grass_version, 
+                        "%s Reference Manual: Topic %s" % (grass_version,
                                                     key.replace('_', ' '))))
-    keyfile.write(headerkey_tmpl.substitute(keyword = key.replace('_', ' ')))
+    keyfile.write(headerkey_tmpl.substitute(keyword=key.replace('_', ' ')))
     for mod, desc in sorted(values.iteritems()):
-        keyfile.write(desc1_tmpl.substitute(cmd = mod, desc = desc, 
-                                            basename = mod.replace('.html','')))
+        keyfile.write(desc1_tmpl.substitute(cmd=mod, desc=desc,
+                                            basename=mod.replace('.html', '')))
     keyfile.write("</table>\n")
     write_html_footer(keyfile, "index.html", year)
 topicsfile.write("</ul>\n")
-write_html_footer(topicsfile, "index.html", year)  
+write_html_footer(topicsfile, "index.html", year)
 topicsfile.close()
