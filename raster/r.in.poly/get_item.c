17938ad63b37205f767fdd2b9d07342447e4e1b9
Anna Petrášová <kratochanna@gmail.com>
2014-11-19 04:15:42 +0000

    r.in.poly: add support for FCELL, DCELL output map and fix writing zeros and nulls
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@62809 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.in.poly/get_item.c
+++ raster/r.in.poly/get_item.c
@@ -1,8 +1,9 @@
 #include <grass/gis.h>
 #include <grass/raster.h>
 #include <grass/glocale.h>
+#include "format.h"
 
-int get_item(FILE * fd, int *type, long *cat, double **x, double **y,
+int get_item(FILE * fd, int format, int *type, int *cat_int, double *cat_double, double **x, double **y,
 	     int *count, struct Categories *labels)
 {
     static double *X = NULL;
@@ -13,8 +14,10 @@ int get_item(FILE * fd, int *type, long *cat, double **x, double **y,
     char east[256], north[256];
     double e, n;
     long offset;
+    FCELL cat_float_tmp;
 
-    *cat = 0;
+    *cat_int = 0;
+    *cat_double = 0;
     *count = 0;
     *type = 0;
 
@@ -63,14 +66,32 @@ int get_item(FILE * fd, int *type, long *cat, double **x, double **y,
 
 	/* if we found a cat (and optionally a label), read them and continue to scan */
 	if (*buf == '=') {
-	    if (sscanf(buf + 1, "%ld", cat) != 1)
+	    if (format == USE_FCELL || format == USE_DCELL) {
+		if (sscanf(buf + 1, "%lf", cat_double) != 1)
+		    continue;
+		/* probably change this as G_getl2() doesn't store the new line (?) */
+		if (sscanf(buf + 1, "%lf%[^\n]", cat_double, lbl) == 2) {
+		    G_strip(lbl);
+		    if (format == USE_FCELL) {
+			cat_float_tmp = (FCELL) *cat_double;
+			Rast_set_f_cat(&cat_float_tmp, &cat_float_tmp, lbl, labels);
+		    }
+		    else {
+			Rast_set_d_cat((DCELL*) cat_double, (DCELL *) cat_double, lbl, labels);
+		    }
+		}
 		continue;
-	    /* probably change this as G_getl2() doesn't store the new line (?) */
-	    if (sscanf(buf + 1, "%ld%[^\n]", cat, lbl) == 2) {
-		G_strip(lbl);
-		Rast_set_c_cat((CELL*) cat, (CELL *) cat, lbl, labels);
 	    }
-	    continue;
+	    else {
+		if (sscanf(buf + 1, "%d", cat_int) != 1)
+		    continue;
+		/* probably change this as G_getl2() doesn't store the new line (?) */
+		if (sscanf(buf + 1, "%d%[^\n]", cat_int, lbl) == 2) {
+		    G_strip(lbl);
+		    Rast_set_c_cat((CELL*) cat_int, (CELL *) cat_int, lbl, labels);
+		}
+		continue;
+	    }
 	}
 	if (sscanf(buf, "%s %s", east, north) != 2) {
 	    G_warning(_("Illegal coordinate <%s, %s>, skipping."), east, north);
