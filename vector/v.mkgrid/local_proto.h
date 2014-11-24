3f77a63dcd74b0e63491eabc46a60cb7f1aa2245
Markus Metz <markus.metz.giswork@gmail.com>
2014-11-24 20:53:44 +0000

    v.mkgrid: simplify user interface, optimze rotation, add hexagon creation
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@62894 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.mkgrid/local_proto.h
+++ vector/v.mkgrid/local_proto.h
@@ -1,7 +1,10 @@
 /* rotate.c */
+void set_angle(double);
 void rotate(double *, double *, double, double, double);
 
 /* write_grid.c */
 int write_grid(struct grid_description *, struct Map_info *, int, int);
-int write_vect(double, double, double, double, struct Map_info *,
-	       struct line_pnts *, int);
+
+/* hexgrid.c */
+int hexgrid(struct grid_description *, struct Map_info *, int, int);
+
--- vector/v.mkgrid/rotate.c
+++ vector/v.mkgrid/rotate.c
@@ -1,16 +1,31 @@
-#include<math.h>
+#include <math.h>
+
+static double sa = 0.0;
+static double ca = 1.0;
+
+void set_angle(double angle)
+{
+    if (angle != 0) {
+	sa = sin(angle);
+	ca = cos(angle);
+    }
+}
+
 /* rotates x and y about the origin (xo,yo) by angle radians */
 void rotate(double *x, double *y, double xo, double yo, double angle)
 {
     double tmpx, tmpy;
+
+    if (angle == 0)
+	return;
 
     /* first translate */
     tmpx = *x - xo;
     tmpy = *y - yo;
 
     /* now rotate */
-    *x = tmpx * cos(angle) - tmpy * sin(angle);
-    *y = tmpx * sin(angle) + tmpy * cos(angle);
+    *x = tmpx * ca - tmpy * sa;
+    *y = tmpx * sa + tmpy * ca;
 
     /* now translate back */
     *x += xo;
