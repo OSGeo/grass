15f03427eac870ae808513f70c9a933e5658e309
Martin Landa <landa.martin@gmail.com>
2014-11-26 09:19:35 +0000

    i.his.rgb: use standardized options (https://trac.osgeo.org/grass/ticket/2409)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@62962 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.his.rgb/main.c
+++ imagery/i.his.rgb/main.c
@@ -54,27 +54,27 @@ int main(int argc, char **argv)
 
     /* Define the different options */
     opt_hue = G_define_standard_option(G_OPT_R_INPUT);
-    opt_hue->key = "hue_input";
+    opt_hue->key = "hue";
     opt_hue->description = _("Name of input raster map (hue)");
 
     opt_int = G_define_standard_option(G_OPT_R_INPUT);
-    opt_int->key = "intensity_input";
+    opt_int->key = "intensity";
     opt_int->description = _("Name of input raster map (intensity)");
 
     opt_sat = G_define_standard_option(G_OPT_R_INPUT);
-    opt_sat->key = "saturation_input";
+    opt_sat->key = "saturation";
     opt_sat->description = _("Name of input raster map (saturation)");
 
     opt_red = G_define_standard_option(G_OPT_R_OUTPUT);
-    opt_red->key = "red_output";
+    opt_red->key = "red";
     opt_red->description = _("Name for output raster map (red)");
 
     opt_green = G_define_standard_option(G_OPT_R_OUTPUT);
-    opt_green->key = "green_output";
+    opt_green->key = "green";
     opt_green->description = _("Name for output raster map (green)");
 
     opt_blue = G_define_standard_option(G_OPT_R_OUTPUT);
-    opt_blue->key = "blue_output";
+    opt_blue->key = "blue";
     opt_blue->description = _("Name for output raster map (blue)");
 
     if (G_parser(argc, argv))
