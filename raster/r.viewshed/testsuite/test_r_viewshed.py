22ab2cfe45f7a6d98d1e4042382f71f03f86137e
Anna Petrášová <kratochanna@gmail.com>
2014-11-29 02:59:03 +0000

    r.viewshed: rename option - update testsuite
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63277 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.viewshed/testsuite/test_r_viewshed.py
+++ raster/r.viewshed/testsuite/test_r_viewshed.py
@@ -9,7 +9,7 @@ class TestViewshed(grass.gunittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.use_temp_region()
-        cls.runModule('g.region', rast='elevation')
+        cls.runModule('g.region', raster='elevation')
 
     @classmethod
     def tearDownClass(cls):
@@ -18,7 +18,7 @@ class TestViewshed(grass.gunittest.TestCase):
     def tearDown(cls):
         """Remove viewshed map after each test method"""
         # TODO: eventually, removing maps should be handled through testing framework fucntions
-        cls.runModule('g.remove', flags='f', type='rast',
+        cls.runModule('g.remove', flags='f', type='raster',
                       name=cls.viewshed)
 
     def test_limits(self):
@@ -26,7 +26,7 @@ class TestViewshed(grass.gunittest.TestCase):
         obs_elev = '1.72'
         self.assertModule('r.viewshed', input='elevation',
             coordinates=(634720,216180), output=self.viewshed,
-            obs_elev=obs_elev)
+            observer_elevation=obs_elev)
         self.assertRasterMinMax(map=self.viewshed, refmin=0, refmax=180,
                                 msg="Viewing angle above the ground must be between 0 and 180 deg")
 
@@ -35,13 +35,13 @@ class TestViewshed(grass.gunittest.TestCase):
         # test e flag
         self.assertModule('r.viewshed', input='elevation', flags='e',
                           coordinates=(634720, 216180), output=self.viewshed,
-                          obs_elev=obs_elev)
+                          observer_elevation=obs_elev)
         minmax = 'null_cells=1963758\nmin=-24.98421\nmax=43.15356'
         self.assertRasterFitsUnivar(raster=self.viewshed, reference=minmax, precision=1e-5)
         # test b flag (#1788)
         self.assertModule('r.viewshed', input='elevation', flags='b',
                           coordinates=(634720, 216180), output=self.viewshed,
-                          obs_elev=obs_elev, overwrite=True)
+                          observer_elevation=obs_elev, overwrite=True)
         minmax = 'min=0\nmax=1\ndatatype=CELL'
         self.assertRasterFitsInfo(raster=self.viewshed, reference=minmax,
                                   msg="Values of binary output must be 0 or 1")
@@ -55,7 +55,7 @@ class TestViewshed(grass.gunittest.TestCase):
         obs_elev = '500000'
         self.assertModule('r.viewshed', input='elevation',
             coordinates=(634720,216180), output=self.viewshed,
-            obs_elev=obs_elev)
+            observer_elevation=obs_elev)
         self.assertRasterMinMax(map=self.viewshed, refmin=0, refmax=180,
             msg="Viewing angle above the ground must be between 0 and 180 deg")
 
@@ -67,7 +67,7 @@ class TestViewshedAgainstReference(grass.gunittest.TestCase):
 
         g.region n=216990 s=215520 w=633730 e=635950 res=10 -p
         r.out.ascii elevation out=data/elevation.ascii
-        r.viewshed input=elevation output=lake_viewshed coordinates=634720,216180 obs_elev=1.72
+        r.viewshed input=elevation output=lake_viewshed coordinates=634720,216180 observer_elevation=1.72
         r.out.ascii lake_viewshed out=data/lake_viewshed.ascii
 
     """
@@ -91,10 +91,10 @@ class TestViewshedAgainstReference(grass.gunittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.del_temp_region()
-        cls.runModule('g.remove', flags='f', type='rast',
+        cls.runModule('g.remove', flags='f', type='raster',
                       name=cls.elevation)
         if cls.to_remove:
-            cls.runModule('g.remove', flags='f', type='rast',
+            cls.runModule('g.remove', flags='f', type='raster',
                           name=','.join(cls.to_remove))
 
     def test_viewshed(self):
@@ -107,7 +107,7 @@ class TestViewshedAgainstReference(grass.gunittest.TestCase):
         self.to_remove.append(ref_viewshed)
 
         self.assertModule('r.viewshed', input=self.elevation,
-                          coordinates=(634720,216180), output=viewshed, obs_elev=obs_elev)
+                          coordinates=(634720,216180), output=viewshed, observer_elevation=obs_elev)
         self.to_remove.append(viewshed)
 
         # check we have expected values
