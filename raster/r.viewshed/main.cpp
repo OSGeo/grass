0e09826dc75ff8c8c79e5dea04e8c5acbb7cd483
Anna Petrášová <kratochanna@gmail.com>
2014-11-29 02:58:40 +0000

    r.viewshed: rename options
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63276 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.viewshed/main.cpp
+++ raster/r.viewshed/main.cpp
@@ -493,7 +493,7 @@ parse_args(int argc, char *argv[], int *vpRow, int *vpCol,
     struct Option *obsElevOpt;
 
     obsElevOpt = G_define_option();
-    obsElevOpt->key = "obs_elev";
+    obsElevOpt->key = "observer_elevation";
     obsElevOpt->type = TYPE_DOUBLE;
     obsElevOpt->required = NO;
     obsElevOpt->key_desc = "value";
@@ -505,7 +505,7 @@ parse_args(int argc, char *argv[], int *vpRow, int *vpCol,
     struct Option *tgtElevOpt;
 
     tgtElevOpt = G_define_option();
-    tgtElevOpt->key = "tgt_elev";
+    tgtElevOpt->key = "target_elevation";
     tgtElevOpt->type = TYPE_DOUBLE;
     tgtElevOpt->required = NO;
     tgtElevOpt->key_desc = "value";
@@ -517,7 +517,7 @@ parse_args(int argc, char *argv[], int *vpRow, int *vpCol,
     struct Option *maxDistOpt;
 
     maxDistOpt = G_define_option();
-    maxDistOpt->key = "max_dist";
+    maxDistOpt->key = "max_distance";
     maxDistOpt->type = TYPE_DOUBLE;
     maxDistOpt->required = NO;
     maxDistOpt->key_desc = "value";
