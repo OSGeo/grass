680467867de156d7d719cd880bf7c023ae570948
Vaclav Petras <wenzeslaus@gmail.com>
2014-12-12 22:00:16 +0000

    d.mon: use GPATH_MAX rather than PATH_MAX (probably typo in https://trac.osgeo.org/grass/changeset/63457, breaks compilation on Mac OS X 10.10)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@63515 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.mon/start.c
+++ display/d.mon/start.c
@@ -49,7 +49,7 @@ void start(const char *name, const char *output)
     if (!strchr(output_name, HOST_DIRSEP)) { /* relative path */
         char *ptr;
         
-        if (!getcwd(output_path, PATH_MAX))
+        if (!getcwd(output_path, GPATH_MAX))
             G_fatal_error(_("Unable to get current working directory"));
         ptr = output_path + strlen(output_path) - 1;
         if (*(ptr++) != HOST_DIRSEP) {
