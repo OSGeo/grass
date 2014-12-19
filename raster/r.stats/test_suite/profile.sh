8f4fbbf911839a70d3df79672661561114efe357
Markus Neteler <neteler@gmail.com>
2014-12-19 23:48:16 +0000

    some shell test scripts updated (trunk, https://trac.osgeo.org/grass/changeset/63634)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@63635 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.stats/test_suite/profile.sh
+++ raster/r.stats/test_suite/profile.sh
@@ -1,11 +1,16 @@
-#spearfish
-MAP=elevation.10m
-g.region raster=$MAP
+# NC dataset, http://grass.osgeo.org/download/sample-data/
+
+OUTFILE=callgrind.out.1196
+MAP=elevation
+export GRASS_OVERWRITE=1
+
+g.region raster=$MAP -p
+
 CMD="r.stats -a $MAP"
 
 time $CMD
 
-valgrind --tool=callgrind --trace-children=yes $CMD
+valgrind --tool=callgrind --callgrind-out-file=$OUTFILE --trace-children=yes $CMD
 
-kcachegrind callgrind.out.12345
-
+# http://kcachegrind.sourceforge.net
+kcachegrind $OUTFILE
--- raster3d/r3.neighbors/test_suite/test.r3.neighbors.sh
+++ raster3d/r3.neighbors/test_suite/test.r3.neighbors.sh
@@ -11,25 +11,25 @@ r3.mapcalc  expr="test_neighbor_null = null()"
 # First @test with float values with @precision=2
 r3.neighbors input=test_neighbor_float output=test_neighbor_float_average \
     method=average window=3,3,3 
-r3.out.ascii dp=2 input=test_neighbor_float_average output=test_neighbor_float_average.txt
+r3.out.ascii precision=2 input=test_neighbor_float_average output=test_neighbor_float_average.txt
 
 r3.neighbors input=test_neighbor_float output=test_neighbor_float_sum \
     method=sum window=3,3,3 
-r3.out.ascii dp=2 input=test_neighbor_float_sum output=test_neighbor_float_sum.txt
+r3.out.ascii precision=2 input=test_neighbor_float_sum output=test_neighbor_float_sum.txt
 
 # Second @test with double values
 r3.neighbors input=test_neighbor_double output=test_neighbor_double_average \
     method=average window=3,3,3 
-r3.out.ascii dp=2 input=test_neighbor_double_average output=test_neighbor_double_average.txt
+r3.out.ascii precision=2 input=test_neighbor_double_average output=test_neighbor_double_average.txt
 
 r3.neighbors input=test_neighbor_double output=test_neighbor_double_sum \
     method=sum window=3,3,3 
-r3.out.ascii dp=2 input=test_neighbor_double_sum output=test_neighbor_double_sum.txt
+r3.out.ascii precision=2 input=test_neighbor_double_sum output=test_neighbor_double_sum.txt
 
 # Third @test with null values
 
 r3.neighbors input=test_neighbor_null output=test_neighbor_null_sum \
     method=sum window=3,3,3 
-r3.out.ascii dp=2 input=test_neighbor_null_sum output=test_neighbor_null_sum.txt
+r3.out.ascii precision=2 input=test_neighbor_null_sum output=test_neighbor_null_sum.txt
 
 
