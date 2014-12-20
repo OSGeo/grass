91ed7ca8f5cdadad796acfd43c2909a216a82c0f
Markus Neteler <neteler@gmail.com>
2014-12-20 17:03:16 +0000

    temporal: renamed parameters extrdir/workdir -> directory for consistency (trac https://trac.osgeo.org/grass/ticket/2409) (trunk, https://trac.osgeo.org/grass/changeset/63650)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@63651 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- temporal/t.vect.export/t.vect.export.py
+++ temporal/t.vect.export/t.vect.export.py
@@ -70,7 +70,7 @@ def main():
     _input = options["input"]
     output = options["output"]
     compression = options["compression"]
-    workdir = options["directory"]
+    directory = options["directory"]
     where = options["where"]
     _format = options["format"]
 
@@ -78,7 +78,7 @@ def main():
     tgis.init()
     # Export the space time raster dataset
     tgis.export_stds(
-        _input, output, compression, workdir, where, _format, "stvds")
+        _input, output, compression, directory, where, _format, "stvds")
 
 ############################################################################
 if __name__ == "__main__":
