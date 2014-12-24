OSGeo/grass

commit 19feda82b85cdc03fb1ac0a02705d061f170a974

v.net modules: alayer -> arc_layer and nlayer -> node_layer (#2409) v.net: type -> arc_type (#2409) libgis: update renamed_options (v.net) - #2409 v.net: ccats -> center_cats (#2409) v.net: ncolumn -> node_column (#2409) v.net modules: afcolumn -> arc_column and abcolumn -> arc_backward_column (#2409) v.net.steiner: tcats -> terminal_cats and nsp -> npoints (#2409) v.net.visibility: coordinate -> coordinates and vis -> visibility (#2409) (merge 63717-63718,63721-63727 from trunk)
git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@63730 15284696-431f-4ddb-bdfa-cd5b030d7da7

vector/v.net.alloc/main.c
@@ -72,44 +72,45 @@ int main(int argc, char **argv)
     map = G_define_standard_option(G_OPT_V_INPUT);
     output = G_define_standard_option(G_OPT_V_OUTPUT);
 
+    afield_opt = G_define_standard_option(G_OPT_V_FIELD);
+    afield_opt->key = "arc_layer";
+    afield_opt->answer = "1";
+    afield_opt->label = _("Arc layer");
+
     type_opt = G_define_standard_option(G_OPT_V_TYPE);
+    type_opt->key "arc_type";
     type_opt->options = "line,boundary";
     type_opt->answer = "line,boundary";
     type_opt->description = _("Arc type");
 
-    afield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    afield_opt->key = "alayer";
-    afield_opt->answer = "1";
-    afield_opt->label = _("Arc layer");
-
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    nfield_opt->key = "nlayer";
+    nfield_opt->key = "node_layer";
     nfield_opt->answer = "2";
     nfield_opt->label = _("Node layer");
 
     afcol = G_define_option();
-    afcol->key = "afcolumn";
+    afcol->key = "arc_column";
     afcol->type = TYPE_STRING;
     afcol->required = NO;
     afcol->description = _("Arc forward/both direction(s) cost column (number)");
     afcol->guisection = _("Cost");
 
     abcol = G_define_option();
-    abcol->key = "abcolumn";
+    abcol->key = "arc_backward_column";
     abcol->type = TYPE_STRING;
     abcol->required = NO;
     abcol->description = _("Arc backward direction cost column (number)");
     abcol->guisection = _("Cost");
 
     ncol = G_define_option();
-    ncol->key = "ncolumn";
+    ncol->key = "node_column";
     ncol->type = TYPE_STRING;
     ncol->required = NO;
     ncol->description = _("Node cost column (number)");
     ncol->guisection = _("Cost");
 
     term_opt = G_define_standard_option(G_OPT_V_CATS);
-    term_opt->key = "ccats";
+    term_opt->key = "center_cats";
     term_opt->required = YES;
     term_opt->description =
 	_("Categories of centers (points on nodes) to which net "

vector/v.net.allpairs/main.c
@@ -69,13 +69,13 @@ int main(int argc, char *argv[])
     map_out = G_define_standard_option(G_OPT_V_OUTPUT);
 
     afield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    afield_opt->key = "alayer";
+    afield_opt->key = "arc_layer";
     afield_opt->answer = "1";
     afield_opt->label = _("Arc layer");
     afield_opt->guisection = _("Selection");
 
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    nfield_opt->key = "nlayer";
+    nfield_opt->key = "node_layer";
     nfield_opt->answer = "2";
     nfield_opt->label = _("Node layer");
     nfield_opt->guisection = _("Selection");
@@ -87,20 +87,20 @@ int main(int argc, char *argv[])
     where_opt->guisection = _("Selection");
 
     afcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    afcol->key = "afcolumn";
+    afcol->key = "arc_column";
     afcol->required = NO;
     afcol->description =
 	_("Arc forward/both direction(s) cost column (number)");
     afcol->guisection = _("Cost");
 
     abcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    abcol->key = "abcolumn";
+    abcol->key = "arc_backward_column";
     abcol->required = NO;
     abcol->description = _("Arc backward direction cost column (number)");
     abcol->guisection = _("Cost");
 
     ncol = G_define_option();
-    ncol->key = "ncolumn";
+    ncol->key = "node_column";
     ncol->type = TYPE_STRING;
     ncol->required = NO;
     ncol->description = _("Node cost column (number)");

vector/v.net.bridge/main.c
@@ -55,32 +55,32 @@ int main(int argc, char *argv[])
     map_out = G_define_standard_option(G_OPT_V_OUTPUT);
 
     afield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    afield_opt->key = "alayer";
+    afield_opt->key = "arc_layer";
     afield_opt->answer = "1";
     afield_opt->label = _("Arc layer");
     afield_opt->guisection = _("Cost");
 
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    nfield_opt->key = "nlayer";
+    nfield_opt->key = "node_layer";
     nfield_opt->answer = "2";
     nfield_opt->label = _("Node layer");
     nfield_opt->guisection = _("Cost");
 
     afcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    afcol->key = "afcolumn";
+    afcol->key = "arc_column";
     afcol->required = NO;
     afcol->description =
 	_("Arc forward/both direction(s) cost column (number)");
     afcol->guisection = _("Cost");
 
     abcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    abcol->key = "abcolumn";
+    abcol->key = "arc_backward_column";
     abcol->required = NO;
     abcol->description = _("Arc backward direction cost column (number)");
     abcol->guisection = _("Cost");
 
     ncol = G_define_option();
-    ncol->key = "ncolumn";
+    ncol->key = "node_column";
     ncol->type = TYPE_STRING;
     ncol->required = NO;
     ncol->description = _("Node cost column (number)");

vector/v.net.centrality/main.c
@@ -104,13 +104,13 @@ int main(int argc, char *argv[])
     map_in = G_define_standard_option(G_OPT_V_INPUT);
 
     afield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    afield_opt->key = "alayer";
+    afield_opt->key = "arc_layer";
     afield_opt->answer = "1";
     afield_opt->label = _("Arc layer");
     afield_opt->guisection = _("Cost");
 
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    nfield_opt->key = "nlayer";
+    nfield_opt->key = "node_layer";
     nfield_opt->answer = "2";
     nfield_opt->label = _("Node layer");
     nfield_opt->guisection = _("Cost");
@@ -123,20 +123,20 @@ int main(int argc, char *argv[])
     where_opt->guisection = _("Selection");
 
     afcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    afcol->key = "afcolumn";
+    afcol->key = "arc_column";
     afcol->required = NO;
     afcol->description =
 	_("Arc forward/both direction(s) cost column (number)");
     afcol->guisection = _("Cost");
 
     abcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    abcol->key = "abcolumn";
+    abcol->key = "arc_backward_column";
     abcol->required = NO;
     abcol->description = _("Arc backward direction cost column (number)");
     abcol->guisection = _("Cost");
 
     ncol = G_define_option();
-    ncol->key = "ncolumn";
+    ncol->key = "node_column";
     ncol->type = TYPE_STRING;
     ncol->required = NO;
     ncol->description = _("Node cost column (number)");

vector/v.net.components/main.c
@@ -78,32 +78,32 @@ int main(int argc, char *argv[])
     map_in = G_define_standard_option(G_OPT_V_INPUT);
 
     afield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    afield_opt->key = "alayer";
+    afield_opt->key = "arc_layer";
     afield_opt->answer = "1";
     afield_opt->label = _("Arc layer");
     afield_opt->guisection = _("Cost");
 
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    nfield_opt->key = "nlayer";
+    nfield_opt->key = "node_layer";
     nfield_opt->answer = "2";
     nfield_opt->label = _("Node layer");
     nfield_opt->guisection = _("Cost");
 
     afcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    afcol->key = "afcolumn";
+    afcol->key = "arc_column";
     afcol->required = NO;
     afcol->description =
 	_("Arc forward/both direction(s) cost column (number)");
     afcol->guisection = _("Cost");
 
     abcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    abcol->key = "abcolumn";
+    abcol->key = "arc_backward_column";
     abcol->required = NO;
     abcol->description = _("Arc backward direction cost column (number)");
     abcol->guisection = _("Cost");
 
     ncol = G_define_option();
-    ncol->key = "ncolumn";
+    ncol->key = "node_column";
     ncol->type = TYPE_STRING;
     ncol->required = NO;
     ncol->description = _("Node cost column (number)");

vector/v.net.connectivity/main.c
@@ -59,26 +59,26 @@ int main(int argc, char *argv[])
     map_in = G_define_standard_option(G_OPT_V_INPUT);
 
     afield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    afield_opt->key = "alayer";
+    afield_opt->key = "arc_layer";
     afield_opt->answer = "1";
     afield_opt->label = _("Arc layer");
     afield_opt->guisection = _("Cost");
 
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    nfield_opt->key = "nlayer";
+    nfield_opt->key = "node_layer";
     nfield_opt->answer = "2";
     nfield_opt->label = _("Node layer");
     nfield_opt->guisection = _("Cost");
 
     afcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    afcol->key = "afcolumn";
+    afcol->key = "arc_column";
     afcol->required = NO;
     afcol->description =
 	_("Arc forward/both direction(s) cost column (number)");
     afcol->guisection = _("Cost");
 
     abcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    abcol->key = "abcolumn";
+    abcol->key = "arc_backward_column";
     abcol->required = NO;
     abcol->description = _("Arc backward direction cost column (number)");
     abcol->guisection = _("Cost");
@@ -86,7 +86,7 @@ int main(int argc, char *argv[])
     map_out = G_define_standard_option(G_OPT_V_OUTPUT);
 
     ncol = G_define_standard_option(G_OPT_DB_COLUMN);
-    ncol->key = "ncolumn";
+    ncol->key = "node_column";
     ncol->required = NO;
     ncol->description = _("Node cost column (number)");
     ncol->guisection = _("Cost");

vector/v.net.distance/main.c
@@ -75,19 +75,20 @@ int main(int argc, char *argv[])
     map_out = G_define_standard_option(G_OPT_V_OUTPUT);
 
     afield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    afield_opt->key = "alayer";
+    afield_opt->key = "arc_layer";
     afield_opt->answer = "1";
     afield_opt->label = _("Arc layer");
     afield_opt->guisection = _("Cost");
 
     atype_opt = G_define_standard_option(G_OPT_V_TYPE);
+    atype_opt->key = "arc_type";
     atype_opt->options = "line,boundary";
     atype_opt->answer = "line,boundary";
     atype_opt->label = _("Arc type");
     atype_opt->guisection = _("Cost");
 
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    nfield_opt->key = "nlayer";
+    nfield_opt->key = "node_layer";
     nfield_opt->answer = "2";
     nfield_opt->label = _("Node layer");
     nfield_opt->guisection = _("Cost");
@@ -132,20 +133,20 @@ int main(int argc, char *argv[])
     wheret_opt->guisection = _("To");
 
     afcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    afcol->key = "afcolumn";
+    afcol->key = "arc_column";
     afcol->required = NO;
     afcol->description =
 	_("Arc forward/both direction(s) cost column (number)");
     afcol->guisection = _("Cost");
 
     abcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    abcol->key = "abcolumn";
+    abcol->key = "arc_backward_column";
     abcol->required = NO;
     abcol->description = _("Arc backward direction cost column (number)");
     abcol->guisection = _("Cost");
 
     ncol = G_define_standard_option(G_OPT_DB_COLUMN);
-    ncol->key = "ncolumn";
+    ncol->key = "node_column";
     ncol->required = NO;
     ncol->description = _("Node cost column (number)");
     ncol->guisection = _("Cost");

vector/v.net.flow/main.c
@@ -65,13 +65,13 @@ int main(int argc, char *argv[])
     map_in = G_define_standard_option(G_OPT_V_INPUT);
 
     afield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    afield_opt->key = "alayer";
+    afield_opt->key = "arc_layer";
     afield_opt->answer = "1";
     afield_opt->label = _("Arc layer");
     afield_opt->guisection = _("Cost");
 
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
-    nfield_opt->key = "nlayer";
+    nfield_opt->key = "node_layer";
     nfield_opt->answer = "2";
     nfield_opt->label = _("Node layer");
     nfield_opt->guisection = _("Cost");
@@ -84,20 +84,20 @@ int main(int argc, char *argv[])
 	_("Name for output vector map containing a minimum cut");
 
     afcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    afcol->key = "afcolumn";
+    afcol->key = "arc_column";
     afcol->required = NO;
     afcol->description =
 	_("Arc forward/both direction(s) cost column (number)");
     afcol->guisection = _("Cost");
 
     abcol = G_define_standard_option(G_OPT_DB_COLUMN);
-    abcol->key = "abcolumn";
+    abcol->key = "arc_backward_column";
     abcol->required = NO;
     abcol->description = _("Arc backward direction cost column (number)");
     abcol->guisection = _("Cost");
 
     ncol = G_define_standard_option(G_OPT_DB_COLUMN);
-    ncol->key = "ncolumn";
+    ncol->key = "node_column";
     ncol->required = NO;
     ncol->description = _("Node cost column (number)");
     ncol->guisection = _("Cost");

commit 03aeaa0a80f02ef9d63873eadc9d0d49a32492d3

fix compilation issue introduced in r63730
git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@63735 15284696-431f-4ddb-bdfa-cd5b030d7da7

vector/v.net.alloc/main.c
@@ -78,7 +78,7 @@ int main(int argc, char **argv)
     afield_opt->label = _("Arc layer");
 
     type_opt = G_define_standard_option(G_OPT_V_TYPE);
-    type_opt->key "arc_type";
+    type_opt->key = "arc_type";
     type_opt->options = "line,boundary";
     type_opt->answer = "line,boundary";
     type_opt->description = _("Arc type");