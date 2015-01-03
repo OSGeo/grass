97c0a08f964d575bbf8ae5b5c4ead2a5b3cd54d3
Martin Landa <landa.martin@gmail.com>
2015-01-03 12:43:36 +0000

    g.version: fix segfault on unknown svn revision/date            (merge r63940-1 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@63942 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/g.version/main.c
+++ general/g.version/main.c
@@ -126,10 +126,13 @@ int main(int argc, char *argv[])
 	if (G_number_of_tokens(rev_ver) == tokens_expected &&
 	    G_number_of_tokens(rev_time) == tokens_expected) {
 	    if (shell->answer) {
+                const char *p;
+                p = strstr(rev_ver[1], " ");
 		fprintf(stdout, "libgis_revision=%s\n",
-			strstr(rev_ver[1], " ") + 1);
+			p ? p + 1 : "00000");
+                p = strstr(rev_time[1], " ");
 		fprintf(stdout, "libgis_date=\"%s\"\n",
-			strstr(rev_time[1], " ") + 1);
+			p ? p + 1 : "?");
 	    }
 	    else {
 		fprintf(stdout, "libgis %s\nlibgis %s\n", rev_ver[1],
