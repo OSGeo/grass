cdf053dc0f9766417c0e142332d19615608b24ce
Martin Landa <landa.martin@gmail.com>
2015-01-08 19:08:36 +0000

    remove old_vector from element list remove v.convert        (merge r63930:63931 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@64007 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.convert/local_proto.h
+++
@@ -1,7 +0,0 @@
-int read_dig(FILE *, struct Map_info *, struct Line **, int, int);
-int read_att(FILE *, struct Categ **);
-double ldist(double, double, struct Line *);
-int old2new(char *, char *, int);
-int new2old(char *, char *);
-char dig_old_to_new_type(char);
-int attributes(char *in, struct Map_info *Out);
