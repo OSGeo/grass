a828ee9a2f03284561b13d8c89429a234280de2a
Markus Neteler <neteler@gmail.com>
2015-01-08 20:29:00 +0000

    r.distance: Added -n flag to report null as * and sort by distance parameter backported; minor sync to trunk
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@64010 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.distance/parse.c
+++ raster/r.distance/parse.c
@@ -4,28 +4,30 @@
  * MODULE:       r.distance
  *
  * AUTHOR(S):    Michael Shapiro - CERL
+ *               Sort/reverse sort by distance by Huidae Cho
  *
- * PURPOSE:      Locates the closest points between objects in two
+ * PURPOSE:      Locates the closest points between objects in two 
  *               raster maps.
  *
- * COPYRIGHT:    (C) 2003 by the GRASS Development Team
+ * COPYRIGHT:    (C) 2003-2014 by the GRASS Development Team
  *
- *               This program is free software under the GNU General Public
- *               License (>=v2). Read the file COPYING that comes with GRASS
- *               for details.
+ *               This program is free software under the GNU General
+ *               Public License (>=v2). Read the file COPYING that
+ *               comes with GRASS for details.
  *
  ***************************************************************************/
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "defs.h"
 #include <grass/gis.h>
 #include <grass/glocale.h>
 
 void parse(int argc, char *argv[], struct Parms *parms)
 {
-    struct Option *maps, *fs;
-    struct Flag *labels, *overlap;
+    struct Option *maps, *fs, *sort;
+    struct Flag *labels, *overlap, *null;
     const char *name, *mapset;
 
     maps = G_define_standard_option(G_OPT_R_MAPS);
@@ -34,6 +36,19 @@ void parse(int argc, char *argv[], struct Parms *parms)
 
     fs = G_define_standard_option(G_OPT_F_SEP);
     fs->answer = ":";		/* colon is default output fs */
+
+    sort = G_define_option();
+    sort->key = "sort";
+    sort->type = TYPE_STRING;
+    sort->required = NO;
+    sort->multiple = NO;
+    sort->label = _("Sort output by distance");
+    sort->description = _("Default: sorted by categories");
+    sort->options = "asc,desc";
+    G_asprintf((char **)&(sort->descriptions),
+               "asc;%s;desc;%s",
+               _("Sort by distance in ascending order"),
+               _("Sort by distance in descending order"));
 
     labels = G_define_flag();
     labels->key = 'l';
@@ -44,6 +59,10 @@ void parse(int argc, char *argv[], struct Parms *parms)
     overlap->description =
 	_("Report zero distance if rasters are overlapping");
 
+    null = G_define_flag();
+    null->key = 'n';
+    null->description = _("Report null objects as *");
+
     if (G_parser(argc, argv))
 	exit(EXIT_FAILURE);
 
@@ -51,6 +70,8 @@ void parse(int argc, char *argv[], struct Parms *parms)
     mapset = parms->map1.mapset = G_find_raster2(name, "");
     if (mapset == NULL)
 	G_fatal_error(_("Raster map <%s> not found"), name);
+    if (Rast_map_type(name, mapset) != CELL_TYPE)
+	G_fatal_error(_("Raster map <%s> is not CELL"), name);
 
     parms->map1.fullname = G_fully_qualified_name(name, mapset);
 
@@ -58,10 +79,17 @@ void parse(int argc, char *argv[], struct Parms *parms)
     mapset = parms->map2.mapset = G_find_raster2(name, "");
     if (mapset == NULL)
 	G_fatal_error(_("Raster map <%s> not found"), name);
+    if (Rast_map_type(name, mapset) != CELL_TYPE)
+	G_fatal_error(_("Raster map <%s> is not CELL"), name);
 
     parms->map2.fullname = G_fully_qualified_name(name, mapset);
 
     parms->labels = labels->answer ? 1 : 0;
-    parms->fs = fs->answer;
+    parms->fs = G_option_to_separator(fs);
     parms->overlap = overlap->answer ? 1 : 0;
+    parms->null = null->answer ? 1 : 0;
+    if (sort->answer)
+	parms->sort = strcmp(sort->answer, "asc") == 0 ? 1 : 2;
+    else
+	parms->sort = 0;
 }
