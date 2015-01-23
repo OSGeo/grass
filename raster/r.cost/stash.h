c53ce626731105f125725f242b19f0c7e4d3c75f
Markus Metz <markus.metz.giswork@gmail.com>
2015-01-23 19:39:26 +0000

    r.cost, r.walk: change percent_memory to memory (in MB)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@64288 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.cost/stash.h
+++ raster/r.cost/stash.h
@@ -1,25 +1,14 @@
 
-/****************************************************************************
- *
- * MODULE:       r.cost
- *
- * AUTHOR(S):    Antony Awaida - IESL - M.I.T.
- *               James Westervelt - CERL
- *               Pierre de Mouveaux <pmx audiovu com>
- *               Eric G. Miller <egm2 jps net>
- *
- * PURPOSE:      Outputs a raster map layer showing the cumulative cost
- *               of moving between different geographic locations on an
- *               input raster map layer whose cell category values
- *               represent cost.
- *
- * COPYRIGHT:    (C) 2006-2009 by the GRASS Development Team
- *
- *               This program is free software under the GNU General Public
- *               License (>=v2). Read the file COPYING that comes with GRASS
- *               for details.
- *
- ***************************************************************************/
+/***************************************************************/
+/*                                                             */
+/*       stash.h     in    ~/src/Gcost                         */
+/*                                                             */
+/*       This header file declares the global variables and    */
+/*       the structures that are to be used for command        */
+/*       line processing.                                      */
+/*                                                             */
+
+/***************************************************************/
 
 #ifndef __STASH_H__
 #define __STASH_H__
