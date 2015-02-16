c919a2006757a477db661f15e6477e7afa2a4fb5
Markus Metz <markus.metz.giswork@gmail.com>
2015-02-16 19:48:39 +0000

    v.net: initialize z, sync to trunk
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@64657 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.net/connect.c
+++ vector/v.net/connect.c
@@ -53,6 +53,7 @@ int connect_arcs(struct Map_info *In, struct Map_info *Pnts,
     
 
     /* go thorough all points in point map and write a new arcs if missing */
+    pz = 0.0;
     while ((type = Vect_read_next_line(Pnts, Points, Cats)) >= 0) {
 	if (type != GV_POINT)
 	    continue;
@@ -141,6 +142,7 @@ int connect_arcs(struct Map_info *In, struct Map_info *Pnts,
     Vect_destroy_cats_struct(Cats);
     Vect_destroy_cats_struct(Cline);
     Vect_destroy_cats_struct(Cnew);
+    Vect_destroy_list(exclude_list);
 
     return narcs;
 }
