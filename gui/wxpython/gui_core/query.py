6980cc8fd6f9b308759dbe0b90f6f7cddde0046a
Anna Petrášová <kratochanna@gmail.com>
2015-03-09 19:11:10 +0000

    wxGUI: revert r64818 as not working fix, see #2617
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@64822 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_core/query.py
+++ gui/wxpython/gui_core/query.py
@@ -17,7 +17,6 @@ import os
 import wx
 
 from core.utils import _
-from core.gcmd import DecodeString
 from gui_core.treeview import TreeListView
 from core.treemodel import TreeModel, DictNode
 
@@ -182,16 +181,12 @@ def QueryTreeBuilder(data, column):
     """
     def addNode(parent, data, model):
         for k, v in data.iteritems():
-            if type(k) == str:
-                k = DecodeString(k)
             if isinstance(v, dict):
                 node = model.AppendNode(parent=parent, label=k)
                 addNode(parent=node, data=v, model=model)
             else:
                 if not isinstance(v, basestring):
                     v = str(v)
-                elif type(v) == str:
-                    v = DecodeString(v)
                 node = model.AppendNode(parent=parent, label=k,
                                         data={column: v})
 
