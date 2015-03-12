99437632b28563efade035b2c986024769f9f828
Markus Neteler <neteler@gmail.com>
2015-03-12 22:27:26 +0000

    msg cosmetics
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@64843 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/init/grass.py
+++ lib/init/grass.py
@@ -1191,7 +1191,7 @@ def grep(string,list):
 def print_params():
     plat = gfile(gisbase, 'include', 'Make', 'Platform.make')
     if not os.path.exists(plat):
-        fatal(_("Please install GRASS development package"))
+        fatal(_("Please install the GRASS GIS development package"))
     fileplat = open(plat)
     linesplat = fileplat.readlines()
     fileplat.close()
