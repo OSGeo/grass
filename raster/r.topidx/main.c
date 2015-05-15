a4059ead5f6f5017342be1d529a41c3c7ad155a2
Huidae Cho <grass4u@gmail.com>
2015-05-15 15:42:52 +0000

    r.topidx: indent
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65249 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.topidx/main.c
+++ raster/r.topidx/main.c
@@ -51,7 +51,7 @@ int main(int argc, char **argv)
 	exit(EXIT_FAILURE);
 
     /* Make sure that the current projection is not lat/long */
-    if ((G_projection() == PROJECTION_LL))
+    if (G_projection() == PROJECTION_LL)
 	G_fatal_error(_("Lat/Long location is not supported by %s. Please reproject map first."),
 		      G_program_name());
 
