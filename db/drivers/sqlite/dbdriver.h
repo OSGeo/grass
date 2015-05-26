8cd96bbc3896c3b5a157f770c10de2c5644a8878
Martin Landa <landa.martin@gmail.com>
2015-05-26 16:45:48 +0000

    sqlite driver: implement db__driver_drop_table()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65329 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/drivers/sqlite/dbdriver.h
+++ db/drivers/sqlite/dbdriver.h
@@ -8,6 +8,8 @@ int db__driver_create_table();
 int db__driver_close_cursor();
 int db__driver_open_database();
 int db__driver_close_database();
+int db__driver_create_database();
+int db__driver_delete_database();
 int db__driver_describe_table();
 int db__driver_init();
 int db__driver_finish();
@@ -17,14 +19,18 @@ int db__driver_commit_transaction();
 int db__driver_fetch();
 int db__driver_get_num_rows();
 int db__driver_create_index();
+int db__driver_list_databases();
 int db__driver_list_tables();
 int db__driver_open_select_cursor();
+int db__driver_drop_table();
 
 #define	init_dbdriver() do{\
 db_driver_create_table = db__driver_create_table;\
 db_driver_close_cursor = db__driver_close_cursor;\
 db_driver_open_database = db__driver_open_database;\
 db_driver_close_database = db__driver_close_database;\
+db_driver_create_database = db__driver_create_database;\
+db_driver_delete_database = db__driver_delete_database;\
 db_driver_describe_table = db__driver_describe_table;\
 db_driver_init = db__driver_init;\
 db_driver_finish = db__driver_finish;\
@@ -34,8 +40,10 @@ db_driver_commit_transaction = db__driver_commit_transaction;\
 db_driver_fetch = db__driver_fetch;\
 db_driver_get_num_rows = db__driver_get_num_rows;\
 db_driver_create_index = db__driver_create_index;\
+db_driver_list_databases = db__driver_list_databases;\
 db_driver_list_tables = db__driver_list_tables;\
 db_driver_open_select_cursor = db__driver_open_select_cursor;\
+db_driver_drop_table = db__driver_drop_table;\
 }while(0)
 
 #endif
