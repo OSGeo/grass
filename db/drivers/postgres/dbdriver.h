1aeef60b3fc4a085be02b0f24f53f7519b7de5c6
Martin Landa <landa.martin@gmail.com>
2015-05-26 18:00:25 +0000

    pg driver: implement db__driver_drop_table
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65333 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/drivers/postgres/dbdriver.h
+++ db/drivers/postgres/dbdriver.h
@@ -8,6 +8,8 @@ int db__driver_create_table();
 int db__driver_close_cursor();
 int db__driver_open_database();
 int db__driver_close_database();
+int db__driver_create_database();
+int db__driver_delete_database();
 int db__driver_describe_table();
 int db__driver_init();
 int db__driver_finish();
@@ -21,12 +23,15 @@ int db__driver_list_databases();
 int db__driver_list_tables();
 int db__driver_grant_on_table();
 int db__driver_open_select_cursor();
+int db__driver_drop_table();
 
 #define	init_dbdriver() do{\
 db_driver_create_table = db__driver_create_table;\
 db_driver_close_cursor = db__driver_close_cursor;\
 db_driver_open_database = db__driver_open_database;\
 db_driver_close_database = db__driver_close_database;\
+db_driver_create_database = db__driver_create_database;\
+db_driver_delete_database = db__driver_delete_database;\
 db_driver_describe_table = db__driver_describe_table;\
 db_driver_init = db__driver_init;\
 db_driver_finish = db__driver_finish;\
@@ -40,6 +45,7 @@ db_driver_list_databases = db__driver_list_databases;\
 db_driver_list_tables = db__driver_list_tables;\
 db_driver_grant_on_table = db__driver_grant_on_table;\
 db_driver_open_select_cursor = db__driver_open_select_cursor;\
+db_driver_drop_table = db__driver_drop_table;\
 }while(0)
 
 #endif
