837c823d89880a5e1761dd936044bf0c97ba5a24
Martin Landa <landa.martin@gmail.com>
2015-07-21 19:10:22 +0000

    fix https://trac.osgeo.org/grass/ticket/2713 (GUI hangs when adding vector map from cli)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65765 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/lmgr/frame.py
+++ gui/wxpython/lmgr/frame.py
@@ -685,7 +685,7 @@ class GMFrame(wx.Frame):
             # add layer into layer tree
             lname, found = GetLayerNameFromCmd(command, fullyQualified = True,
                                                layerType = layertype)
-            self.GetLayerTree().AddLayer(ltype = layertype, lchecked=True,
+            self.GetLayerTree().AddLayer(ltype = layertype, lchecked = False,
                                          lname = lname,
                                          lcmd = command)
 
