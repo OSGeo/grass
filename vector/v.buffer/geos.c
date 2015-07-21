OSGeo/grass

commit 0f1ce564f69a9ba48ded9e0259d4ef15d9b97df9

fix v.buffer to be compiled with GEOS < 3.2 (see https://trac.osgeo.org/grass/changeset/65710)
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65715 15284696-431f-4ddb-bdfa-cd5b030d7da7

vector/v.buffer/geos.c
@@ -158,6 +158,7 @@ int geos_buffer(struct Map_info *In, struct Map_info *Out,
      * A value of 8 gives less than 2% max error in the buffer distance.
      * For a max error of < 1%, use QS = 12.
      * For a max error of < 0.1%, use QS = 18. */
+#ifdef GEOS_3_3
     if (flat || no_caps) {
         GEOSBufferParams* geos_params = GEOSBufferParams_create();
         GEOSBufferParams_setEndCapStyle(geos_params,
@@ -166,9 +167,10 @@ int geos_buffer(struct Map_info *In, struct Map_info *Out,
         OGeom = GEOSBufferWithParams(IGeom, geos_params, da);
         GEOSBufferParams_destroy(geos_params);
     }
-    else {
+    else
+#else
         OGeom = GEOSBuffer(IGeom, da, 12);
-    }
+#endif
     
     if (!OGeom) {
 	G_fatal_error(_("Buffering failed"));

vector/v.buffer/local_proto.h
@@ -1,3 +1,6 @@
+#if (GEOS_VERSION_MAJOR >= 3 && GEOS_VERSION_MINOR >= 3)
+#define GEOS_3_3
+#endif
 
 struct buf_contours
 {

vector/v.buffer/main.c
@@ -474,6 +474,12 @@ int main(int argc, char *argv[])
 
 #ifdef HAVE_GEOS
     initGEOS(G_message, G_fatal_error);
+
+    /* check required version for -s/-c flag */
+#ifndef GEOS_3_3
+        G_warning(_("Flags -%c/%c ignored by this version, GEOS >= 3.3 is required"),
+                  's', 'c');
+#endif
 #endif
     if(!use_geos && (da < 0. || db < 0.)) {
 	G_warning(_("Negative distances for internal buffers are not supported "

commit 41f433d3a3c5b34d8b63c40264ca3e039dcb2a7b

v.buffer: fix bug introduced in r65715
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65759 15284696-431f-4ddb-bdfa-cd5b030d7da7

vector/v.buffer/geos.c
@@ -147,7 +147,7 @@ int geos_buffer(struct Map_info *In, struct Map_info *Out,
     GEOSGeometry *IGeom = NULL;
     GEOSGeometry *OGeom = NULL;
     
-    G_debug(3, "geos_buffer()");
+    G_debug(3, "geos_buffer(): id=%d", id);
 
     if (type == GV_AREA)
 	IGeom = Vect_read_area_geos(In, id);
@@ -167,13 +167,15 @@ int geos_buffer(struct Map_info *In, struct Map_info *Out,
         OGeom = GEOSBufferWithParams(IGeom, geos_params, da);
         GEOSBufferParams_destroy(geos_params);
     }
-    else
-#else
+    else {
         OGeom = GEOSBuffer(IGeom, da, 12);
+    }
+#else
+    OGeom = GEOSBuffer(IGeom, da, 12);
 #endif
     
     if (!OGeom) {
-	G_fatal_error(_("Buffering failed"));
+        G_fatal_error(_("Buffering failed (feature %d)"), id);
     }
     
     geom2ring(OGeom, Out, Buf, si, Cats, arr_bc, buffers_count, arr_bc_alloc);