4873a215698c74f5488cc1b522ccbbd770fa4d45
Soeren Gebbert <soerengebbert@googlemail.com>
2015-08-14 09:04:03 +0000

    pygrass vector: Added mapinfo pointer checks and some alive() methods
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@65929 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/pygrass/errors.py
+++ lib/python/pygrass/errors.py
@@ -16,3 +16,15 @@ def must_be_open(method):
         else:
             get_msgr().warning(_("The map is close!"))
     return wrapper
+
+
+def mapinfo_must_be_set(method):
+
+    @wraps(method)
+    def wrapper(self, *args, **kargs):
+        if self.c_mapinfo:
+            return method(self, *args, **kargs)
+        else:
+            raise GrassError(_("The self.c_mapinfo pointer must be "\
+                                 "correctly initiated"))
+    return wrapper
