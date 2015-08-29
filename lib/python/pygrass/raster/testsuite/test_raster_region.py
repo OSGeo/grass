b4ba130155ca1f708510b513a34cb8465a54c6e4
Soeren Gebbert <soerengebbert@googlemail.com>
2015-08-29 22:01:27 +0000

    pygrass raster: Added computational raster region setting test
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@66062 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/pygrass/raster/testsuite/test_raster_region.py
+++ lib/python/pygrass/raster/testsuite/test_raster_region.py
@@ -4,6 +4,7 @@ from grass.gunittest.main import test
 from unittest import skip
 
 from grass.pygrass.raster import RasterRow
+from grass.pygrass.raster import raster2numpy
 from grass.pygrass.gis.region import Region
 
 class RasterRowRegionTestCase(TestCase):
@@ -77,6 +78,36 @@ class RasterRowRegionTestCase(TestCase):
         self.assertItemsEqual(rast[5].tolist()[2:6], [14.,24.,34.,44.])
         
         rast.close()
+
+    def test_resampling_to_numpy(self):
+        
+        region = Region()
+        region.ewres = 0.1
+        region.nsres = 0.1
+        region.adjust()
+        region.set_raster_region()
+        
+        a = raster2numpy(self.name)
+        
+        self.assertEqual(len(a), 400)
+        
+        region.ewres = 1
+        region.nsres = 1
+        region.adjust()
+        region.set_raster_region()
+        
+        a = raster2numpy(self.name)
+        
+        self.assertEqual(len(a), 40)
+
+        region.ewres = 5
+        region.nsres = 5
+        region.adjust()
+        region.set_raster_region()
+        
+        a = raster2numpy(self.name)
+        
+        self.assertEqual(len(a), 8)
 
 if __name__ == '__main__':
     test()
