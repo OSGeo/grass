efc0c4b62f156a408c61540ebbf7052adf5cde21
Markus Neteler <neteler@gmail.com>
2015-09-12 18:04:42 +0000

    temporal separator parameter: use label to re-enable standard description
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@66179 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- temporal/t.vect.db.select/t.vect.db.select.py
+++ temporal/t.vect.db.select/t.vect.db.select.py
@@ -30,7 +30,7 @@
 #%end
 
 #%option G_OPT_F_SEP
-#% description: Field separator character between the output columns
+#% label: Field separator character between the output columns
 #%end
 
 #%option G_OPT_V_FIELD
