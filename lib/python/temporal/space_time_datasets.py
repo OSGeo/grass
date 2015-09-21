4b415a528bfc7be495d143837d5af7dfe8d90842
Moritz Lennert <mlennert@club.worldonline.be>
2015-09-21 11:15:53 +0000

    added missing import of array library (fix https://trac.osgeo.org/grass/ticket/2688)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@66267 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/temporal/space_time_datasets.py
+++ lib/python/temporal/space_time_datasets.py
@@ -11,6 +11,7 @@ for details.
 import getpass
 from abstract_map_dataset import *
 from abstract_space_time_dataset import *
+import grass.script.array as garray
 
 ###############################################################################
 
