bcfbae1fe5a39b0be02592e5786163a313b4516c
Markus Neteler <neteler@gmail.com>
2015-10-16 08:20:28 +0000

    v.import: added snap option, default 1e-13 (trunk, r66510)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@66513 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/v.import/v.import.py
+++ scripts/v.import/v.import.py
@@ -56,6 +56,14 @@
 #% descriptions: Overrides encoding interpretation, useful when importing ESRI Shapefile
 #% guisection: Output
 #%end
+#%option
+#% key: snap
+#% type: double
+#% label: Snapping threshold for boundaries (map units)
+#% description: '-1' for no snap
+#% answer: 1e-13
+#% guisection: Output
+#%end
 #%flag
 #% key: f
 #% description: List supported OGR formats and exit
@@ -136,6 +144,7 @@ def main():
         vopts['layer'] = layers
     if output:
         vopts['output'] = output
+    vopts['snap'] = options['snap']
     try:
         grass.run_command('v.in.ogr', input=OGRdatasource,
                           location=TMPLOC, flags='i', quiet=True, **vopts)
