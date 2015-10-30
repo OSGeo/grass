e13464f96220614334e168cf2110534ad808e57b
Markus Neteler <neteler@gmail.com>
2015-10-30 11:03:11 +0000

    minor comment fix
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@66670 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/temporal/lib/connect.c
+++ lib/temporal/lib/connect.c
@@ -99,8 +99,9 @@ static char *get_mapset_connection_name(const char *mapset, int contype)
     ret = G_mapset_permissions2(gisdbase, location, mapset);
     switch (ret) {
     case 0: /* Check if the mapset exists and user is owner */
-        /* We usppress this warning, since G_mapset_permission does not
-         * check the access privilegs to the mapset of a stranger.
+        /* We suppress this warning, since G_mapset_permission2() does not
+         * properly check the access privileges to the mapset of a different user.
+         * TODO: develop a dedicated G_mapset_permission3() for that
         G_warning(_("You are not the owner of mapset <%s>"),
                       mapset);
         */
