7f92d7b61e8a70561e444dfe9d2d88cfec6161e3
Markus Neteler <neteler@gmail.com>
2015-11-16 22:34:42 +0000

    t.rast.import/t.vect.import: minor description sync to trunk
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@66854 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- temporal/t.vect.import/t.vect.import.py
+++ temporal/t.vect.import/t.vect.import.py
@@ -73,7 +73,8 @@
 
 #%flag
 #% key: o
-#% description: Override projection (use location's projection)
+#% label: Override projection check (use current location's projection)
+#% description: Assume that the dataset has same projection as the current location
 #%end
 
 #%flag
