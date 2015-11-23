54017d667d73b02057f29418064371a7be1d4317
Martin Landa <landa.martin@gmail.com>
2015-11-23 20:12:02 +0000

    v.in.e00: input is a binary file           (merge https://trac.osgeo.org/grass/changeset/66903 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@66904 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/v.in.e00/v.in.e00.py
+++ scripts/v.in.e00/v.in.e00.py
@@ -25,8 +25,9 @@
 #% description: Imports E00 file into a vector map.
 #% keyword: vector
 #% keyword: import
+#% keyword: E00
 #%end
-#%option G_OPT_F_INPUT
+#%option G_OPT_F_BIN_INPUT
 #% description: Name of input E00 file
 #%end
 #%option G_OPT_V_TYPE
