ab26c25bb9d4c3e0d432e06d7bc8d666fe9e589e
Martin Landa <landa.martin@gmail.com>
2015-12-05 11:52:56 +0000

    preremove: define batch variable
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@67019 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -1,7 +1,9 @@
+set BATCH=%OSGEO4W_ROOT%\bin\@GRASS_EXECUTABLE@.bat
+
 del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@.lnk"
 del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 
-del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@.bat
+del "%BATCH%"
 del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
 del "%OSGEO4W_ROOT%"\apps\msys\bin\libintl3.dll
 del "%OSGEO4W_ROOT%"\apps\msys\bin\libiconv2.dll
