fe4107b2eb55513198d0a3e10c44ba315a0609f4
Martin Landa <landa.martin@gmail.com>
2015-12-05 11:34:56 +0000

    winGRASS: remove ununsed shell startup script           (merge r66921:2, r66924, r66956, r66961 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67018 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -5,7 +5,6 @@ rmdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@"
 del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 
 del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@.bat
-del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@
 del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
 del "%OSGEO4W_ROOT%"\apps\msys\bin\libintl3.dll
 del "%OSGEO4W_ROOT%"\apps\msys\bin\libiconv2.dll
