4743f40661627471bfd5e1126bf53835c78d6a00
Martin Landa <landa.martin@gmail.com>
2015-12-12 14:21:11 +0000

    update mklib.sh for 64bit builds        (merge https://trac.osgeo.org/grass/changeset/67089 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67090 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/mklibs.sh
+++ mswindows/osgeo4w/mklibs.sh
@@ -2,12 +2,22 @@
 
 set -e
 
+if [ -z "$OSGEO4W_POSTFIX" ]; then
+    OSGEO4W_POSTFIX=""
+fi
+
+if [ "$OSGEO4W_POSTFIX" != "64" ]; then
+    MACHINE=x86
+else
+    MACHINE=x64
+fi
+
 PROGRAMFILES="/c/Program Files (x86)"
 VSDIR="$PROGRAMFILES/Microsoft Visual Studio 12.0"
 PATH="$VSDIR/Common7/IDE:$PATH"
 PATH="$VSDIR/VC/bin:$PATH"
 PATH="$VSDIR/Common7/Tools:$PATH"
-PATH="$PATH:/c/OSGeo4W/bin"
+PATH="$PATH:/c/OSGeo4W${OSGEO4W_POSTFIX}/bin"
 export PATH
 
 [ -d mswindows/osgeo4w/vc ] || mkdir mswindows/osgeo4w/vc
@@ -28,8 +38,7 @@ for dllfile in "$@"; do
 		sed -nf mswindows/osgeo4w/mklibs.sed |
 		egrep -v "^[ 	]*(_IMPORT_DESCRIPTOR_.*|_NULL_IMPORT_DESCRIPTOR)$" >mswindows/osgeo4w/vc/${defname%$VERSION}
 
-	cd mswindows/osgeo4w/vc
-	lib -nologo -def:${defname} -subsystem:windows -machine:x86
-	lib -nologo $libname || exit
-	cd ../../..
+	(cd mswindows/osgeo4w/vc ;
+	    lib -nologo -def:${defname} -subsystem:windows -machine:x86 ;
+	    lib -nologo $libname || exit)
 done
