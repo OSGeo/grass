2888c3993570ada82169056ee8603f43fc2c35d7
Martin Landa <landa.martin@gmail.com>
2015-12-12 14:03:22 +0000

    wingrass: update preremove script for mingw32-w64           (merge https://trac.osgeo.org/grass/changeset/67068 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67087 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -6,6 +6,16 @@ del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 
 del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@.bat
 del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
-del "%OSGEO4W_ROOT%"\apps\msys\bin\libintl3.dll
-del "%OSGEO4W_ROOT%"\apps\msys\bin\libiconv2.dll
-del "%OSGEO4W_ROOT%"\apps\msys\bin\regex2.dll
+
+del "%OSGEO4W_ROOT%"\bin\libintl-8.dll
+del "%OSGEO4W_ROOT%"\bin\libiconv-2.dll
+del "%OSGEO4W_ROOT%"\bin\libfontconfig-1.dll
+if exist "%OSGEO4W_ROOT%"\bin\libgcc_s_seh-1.dll "%OSGEO4W_ROOT%"\bin\libgcc_s_seh-1.dll
+if exist "%OSGEO4W_ROOT%"\bin\libgcc_s_dw2-1.dll "%OSGEO4W_ROOT%"\bin\libgcc_s_dw2-1.dll
+del "%OSGEO4W_ROOT%"\bin\libwinpthread-1.dll
+del "%OSGEO4W_ROOT%"\bin\libexpat-1.dll
+del "%OSGEO4W_ROOT%"\bin\libfreetype-6.dll
+del "%OSGEO4W_ROOT%"\bin\libbz2-1.dll
+del "%OSGEO4W_ROOT%"\bin\libharfbuzz-0.dll
+del "%OSGEO4W_ROOT%"\bin\libglib-2.0-0.dll
+del "%OSGEO4W_ROOT%"\bin\libpng16-16.dll
