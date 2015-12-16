e116f44ecf63f5ef43b9c54b93f1355bd8d30f48
Martin Landa <landa.martin@gmail.com>
2015-12-16 12:53:14 +0000

    wingrass: cleanup preremove.bat file           (merge https://trac.osgeo.org/grass/changeset/67167 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67168 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -1,21 +1,5 @@
-del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS @VERSION@ GUI.lnk"
-del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS @VERSION@ Command Line.lnk"
-rmdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
-
+del "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@.lnk"
 del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 
-del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@.bat
+del "%OSGEO4W_ROOT%\bin\@GRASS_EXECUTABLE@.bat"
 del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
-
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libintl-8.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libiconv-2.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libfontconfig-1.dll
-if exist "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libgcc_s_seh-1.dll "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libgcc_s_seh-1.dll
-if exist "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libgcc_s_dw2-1.dll "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libgcc_s_dw2-1.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libwinpthread-1.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libexpat-1.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libfreetype-6.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libbz2-1.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libharfbuzz-0.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libglib-2.0-0.dll
-del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libpng16-16.dll
