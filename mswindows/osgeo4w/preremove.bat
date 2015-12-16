c287e75254ac26f251c89eef4b2b4bf572d31609
Martin Landa <landa.martin@gmail.com>
2015-12-16 11:33:47 +0000

    wingrass: move extra libs from bin to lib directory           (merge r67165 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67166 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -7,15 +7,15 @@ del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@.bat
 del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
 
-del "%OSGEO4W_ROOT%"\bin\libintl-8.dll
-del "%OSGEO4W_ROOT%"\bin\libiconv-2.dll
-del "%OSGEO4W_ROOT%"\bin\libfontconfig-1.dll
-if exist "%OSGEO4W_ROOT%"\bin\libgcc_s_seh-1.dll "%OSGEO4W_ROOT%"\bin\libgcc_s_seh-1.dll
-if exist "%OSGEO4W_ROOT%"\bin\libgcc_s_dw2-1.dll "%OSGEO4W_ROOT%"\bin\libgcc_s_dw2-1.dll
-del "%OSGEO4W_ROOT%"\bin\libwinpthread-1.dll
-del "%OSGEO4W_ROOT%"\bin\libexpat-1.dll
-del "%OSGEO4W_ROOT%"\bin\libfreetype-6.dll
-del "%OSGEO4W_ROOT%"\bin\libbz2-1.dll
-del "%OSGEO4W_ROOT%"\bin\libharfbuzz-0.dll
-del "%OSGEO4W_ROOT%"\bin\libglib-2.0-0.dll
-del "%OSGEO4W_ROOT%"\bin\libpng16-16.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libintl-8.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libiconv-2.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libfontconfig-1.dll
+if exist "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libgcc_s_seh-1.dll "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libgcc_s_seh-1.dll
+if exist "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libgcc_s_dw2-1.dll "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libgcc_s_dw2-1.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libwinpthread-1.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libexpat-1.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libfreetype-6.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libbz2-1.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libharfbuzz-0.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libglib-2.0-0.dll
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\lib\libpng16-16.dll
