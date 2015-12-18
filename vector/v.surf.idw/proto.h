f2bad03d6fcc7853a46309652df94d598fa2e5f0
Anna Petrášová <kratochanna@gmail.com>
2015-12-18 04:10:41 +0000

    v.surf.idw: fix serious bugs - wrong choice of nearest points when using indexing, missing sqrt, see #2820
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@67211 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.surf.idw/proto.h
+++ vector/v.surf.idw/proto.h
@@ -2,5 +2,5 @@
 void read_sites(const char *, const char *, const char *, int);
 
 void newpoint(double, double, double, int);
-void calculate_distances(int, int, double, double, int *);
+void calculate_distances(int, int, double, double, int *, int *);
 void calculate_distances_noindex(double, double);
