bc4542d1f3c9ea9066558af0efb8004e44834cfb
Martin Landa <landa.martin@gmail.com>
2015-12-22 14:50:09 +0000

    fix g.mapset -s issue (set_gui_path)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@67326 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/modules/mapsets_picker.py
+++ gui/wxpython/modules/mapsets_picker.py
@@ -2,6 +2,9 @@
 
 import wx
 
+from grass.script.setup import set_gui_path
+set_gui_path()
+
 from core.gcmd import RunCommand
 from core.utils import _
 from gui_core.preferences import MapsetAccess
