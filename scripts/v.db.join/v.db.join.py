6ec0cfceaf7b3fc44c2cc18015e67b3dc8f3554d
Martin Landa <landa.martin@gmail.com>
2015-12-29 09:24:41 +0000

    v.db.join: fix db.describe and mysql (https://trac.osgeo.org/grass/ticket/2792)            (merge r67376:67377 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67411 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/v.db.join/v.db.join.py
+++ scripts/v.db.join/v.db.join.py
@@ -123,8 +123,18 @@ def main():
 	colname = col[0]
 	if colname == column:
 	    continue
-	# Sqlite 3 does not support the precision number any more
-	if len(col) > 2 and driver != "sqlite":
+        
+        use_len = False
+	if len(col) > 2:
+            use_len = True
+            # Sqlite 3 does not support the precision number any more
+            if driver == "sqlite":
+                use_len = False
+            # MySQL - expect format DOUBLE PRECISION(M,D), see #2792
+            elif driver == "mysql" and col[1] == 'DOUBLE PRECISION':
+                use_len = False
+        
+        if use_len:
 	    coltype = "%s(%s)" % (col[1], col[2])
 	else:
 	    coltype = "%s" % col[1]
