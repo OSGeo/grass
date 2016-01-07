149a06630e084215aee3c5d3bd7048dec24ccbb3
Martin Landa <landa.martin@gmail.com>
2016-01-07 10:58:00 +0000

    v.in.ogr doesn't use information from dblogin file about external PostgreSQL server (https://trac.osgeo.org/grass/ticket/2385)          experimental implementation containing duplicated code (TODO: will be probably moved to Vlib)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@67510 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.external/local_proto.h
+++ vector/v.external/local_proto.h
@@ -13,6 +13,9 @@ struct _flags {
 void parse_args(int, char **,
 		struct _options *, struct _flags*);
 
+/* dsn.c */
+char *get_datasource_name(const char *, int);
+
 /* list.c */
 void list_formats();
 int list_layers(FILE *, const char *, const char *, int, int);
