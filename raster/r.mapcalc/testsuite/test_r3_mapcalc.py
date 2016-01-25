7aa3930e275ef933c8bd639059826b0c9dcfafeb
Maris Nartiss <maris.gis@gmail.com>
2016-01-25 13:23:34 +0000

    r.mapcalc: Introduce new variables to access current region dimensions in number of cells
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@67664 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.mapcalc/testsuite/test_r3_mapcalc.py
+++ raster/r.mapcalc/testsuite/test_r3_mapcalc.py
@@ -51,6 +51,14 @@ class TestBasicOperations(TestCase):
             expression='diff_e_e = 3 * x() * y() * z() - 3 * x() * y() * z()')
         self.to_remove.append('diff_e_e')
         self.assertRaster3dMinMax('diff_e_e', refmin=0, refmax=0)
+    
+    def test_nrows_ncols_ndepths_sum(self):
+        """Test if sum of nrows, ncols and ndepths matches one
+        expected from current region settigs"""
+        self.assertModule('r3.mapcalc',
+            expression='nrows_ncols_ndepths_sum = nrows() + ncols() + ndepths()')
+        self.to_remove.append('nrows_ncols_ndepths_sum')
+        self.assertRasterMinMax('nrows_ncols_ndepths_sum', refmin=2160, refmax=2160)
 
 
 if __name__ == '__main__':
