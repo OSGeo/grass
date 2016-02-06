4e572e40a2ebb8390658d3b1fbcff0cf04704308
Martin Landa <landa.martin@gmail.com>
2016-02-06 18:10:33 +0000

    r.external: added -t flag for listing bands             (merge https://trac.osgeo.org/grass/changeset/67284 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_0@67744 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.external/list.c
+++ raster/r.external/list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include <grass/gis.h>
+#include <grass/gprojects.h>
 #include <grass/glocale.h>
 
 #include <grass/gis.h>
@@ -43,3 +44,50 @@ void list_formats(void)
 		pszRWFlag, GDALGetDriverLongName(hDriver));
     }
 }
+
+void list_bands(struct Cell_head *cellhd, GDALDatasetH hDS)
+{
+    struct Cell_head loc_wind;
+    struct Key_Value *proj_info = NULL, *proj_units = NULL;
+    struct Key_Value *loc_proj_info = NULL, *loc_proj_units = NULL;
+    int n_bands, i_band, proj_same;
+    GDALRasterBandH hBand;
+    GDALDataType gdal_type;
+
+    if (GPJ_wkt_to_grass(cellhd, &proj_info,
+                         &proj_units, GDALGetProjectionRef(hDS), 0) < 0) {
+        proj_same = 0;
+    }
+    else {
+
+        G_get_default_window(&loc_wind);
+        if (loc_wind.proj != PROJECTION_XY) {
+            loc_proj_info = G_get_projinfo();
+            loc_proj_units = G_get_projunits();
+        }
+
+
+        if (loc_wind.proj != cellhd->proj ||
+            (G_compare_projections
+             (loc_proj_info, loc_proj_units, proj_info, proj_units)) < 0) {
+            proj_same = 0;
+        }
+        else {
+            proj_same = 1;
+        }
+
+    }
+
+    n_bands = GDALGetRasterCount(hDS);
+
+    for (i_band = 1; i_band <= n_bands; i_band++) {
+
+        hBand = GDALGetRasterBand(hDS, i_band);
+        gdal_type = GDALGetRasterDataType(hBand);
+
+        fprintf(stdout, "%d,%s,%d\n", i_band, GDALGetDataTypeName(gdal_type),
+                proj_same);
+
+    }
+}
+
