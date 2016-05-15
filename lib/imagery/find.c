2a60040a5c2f8c738dcce0fd86892c354c0bfc74
Maris Nartiss <maris.gis@gmail.com>
2016-05-15 10:53:08 +0000

    Reindent I_find
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@68431 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/imagery/find.c
+++ lib/imagery/find.c
@@ -21,7 +21,7 @@
 int I_find_group(const char *group)
 {
     if (group == NULL || *group == 0)
-	return 0;
+        return 0;
 
     return G_find_file2("group", group, G_mapset()) != NULL;
 }
@@ -29,9 +29,9 @@ int I_find_group(const char *group)
 int I_find_group_file(const char *group, const char *file)
 {
     if (!I_find_group(group))
-	return 0;
+        return 0;
     if (file == NULL || *file == 0)
-	return 0;
+        return 0;
 
     return G_find_file2_misc("group", file, group, G_mapset()) != NULL;
 }
@@ -41,9 +41,9 @@ int I_find_subgroup(const char *group, const char *subgroup)
     char element[GNAME_MAX];
 
     if (!I_find_group(group))
-	return 0;
+        return 0;
     if (subgroup == NULL || *subgroup == 0)
-	return 0;
+        return 0;
 
     sprintf(element, "subgroup/%s", subgroup);
 
@@ -51,16 +51,16 @@ int I_find_subgroup(const char *group, const char *subgroup)
 }
 
 int I_find_subgroup_file(const char *group, const char *subgroup,
-			 const char *file)
+                         const char *file)
 {
     char element[GNAME_MAX * 2];
 
     if (!I_find_group(group))
-	return 0;
+        return 0;
     if (subgroup == NULL || *subgroup == 0)
-	return 0;
+        return 0;
     if (file == NULL || *file == 0)
-	return 0;
+        return 0;
 
     sprintf(element, "subgroup/%s/%s", subgroup, file);
 
