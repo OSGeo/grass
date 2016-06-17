3147062cbdc2fbb2e0ebd5c3f13ab722136e7809
Martin Landa <landa.martin@gmail.com>
2016-06-17 11:14:41 +0000

    wxGUI: catch also UnicodeDecodeError when reading current directory as GISDBASE
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@68704 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/core/utils.py
+++ gui/wxpython/core/utils.py
@@ -614,7 +614,7 @@ def GetListOfLocations(dbase):
                     listOfLocations.append(os.path.basename(location))
             except:
                 pass
-    except UnicodeEncodeError as e:
+    except (UnicodeEncodeError, UnicodeDecodeError) as e:
         raise e
 
     ListSortLower(listOfLocations)
