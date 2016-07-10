d59b6e06162b08ed56cc396e707eace3c5680242
Vaclav Petras <wenzeslaus@gmail.com>
2016-07-10 00:16:06 +0000

    pythonlib: use isinstance to distinguish string types (fixes r65787)
    isinstance(bytes) seems to better distinguish if we can/need to decode/encode
    a string. hasattr with decode/encode fails in Python 2 because all are
    present everywhere (see below).
    
    This enables Python modules to be executed with non-ascii characters
    as option values (test which uses UTF-8 included). This also asks for locale
    only when needed.
    
    In Python 2:
    >>> print(hasattr(b"", "decode"), hasattr("", "decode"), hasattr(u"", "decode"))
    (True, True, True)
    >>> print(hasattr(b"", "encode"), hasattr("", "encode"), hasattr(u"", "encode"))
    (True, True, True)
    >>> print(isinstance(b"", bytes), isinstance("", bytes), isinstance(u"", bytes))
    (True, True, False)
    
    In Python 3:
    >>> print(hasattr(b"", "decode"), hasattr("", "decode"), hasattr(u"", "decode"))
    True False False
    >>> print(hasattr(b"", "encode"), hasattr("", "encode"), hasattr(u"", "encode"))
    False True True
    >>> print(isinstance(b"", bytes), isinstance("", bytes), isinstance(u"", bytes))
    True False False
    
    
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@68927 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/script/testsuite/test_start_command_functions.py
+++ lib/python/script/testsuite/test_start_command_functions.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+# the utf-8 is important because we do use the characters
 """Tests of start_command function family (location independent)"""
 
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
 
-from grass.script.core import start_command, PIPE
+from grass.script.core import start_command, PIPE, run_command
 
 
 class TestPythonKeywordsInParameters(TestCase):
@@ -37,6 +39,29 @@ class TestPythonKeywordsInParameters(TestCase):
         returncode = proc.poll()
         self.assertEquals(returncode, 1)
         self.assertIn(b'raster', stderr)
+
+
+class TestPythonModuleWithUnicodeParameters(TestCase):
+    """Tests if unicode works in parameters of Python modules
+
+    This in fact tests also the `parser()` function (original motivation
+    for this tests).
+
+    Using g.search.module because it takes any option values.
+    """
+
+    def test_python_module_ascii(self):
+        """This tests if Python module works"""
+        run_command('g.search.modules', keyword=b'Priserny kun')
+
+    def test_python_module_czech_nonascii(self):
+        """This likely fails on non-UTF-8 systems (i.e. MS Win)"""
+        run_command('g.search.modules', keyword=b'Příšerný kůň')
+
+    def test_python_module_czech_unicode(self):
+        """This likely fails on non-UTF-8 systems (i.e. MS Win)"""
+        run_command('g.search.modules', keyword=u'Příšerný kůň')
+
 
 if __name__ == '__main__':
     test()
