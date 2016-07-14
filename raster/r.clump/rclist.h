9a8139d08788f29b144e80268f77007815847a93
Markus Metz <markus.metz.giswork@gmail.com>
2016-07-14 20:54:01 +0000

    r.clump: +minimum size of clumps add rclist
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@68970 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ raster/r.clump/rclist.h
@@ -0,0 +1,20 @@
+/* row/col list */
+
+struct rc
+{
+    struct rc *next;
+    int row;
+    int col;
+};
+
+struct rclist
+{
+    struct rc *tail, *head;
+};
+
+/* rclist.c */
+void rclist_init(struct rclist *);
+void rclist_add(struct rclist *, int, int);
+int rclist_drop(struct rclist *, struct rc *);
+void rclist_destroy(struct rclist *);
+
