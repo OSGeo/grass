0343bed35a456b051b169dbdc392444780af01d6
Anna Petrášová <kratochanna@gmail.com>
2016-08-01 08:21:57 +0000

    d.barscale: various GSoC improvements, author Adam Laza, merge from trunk https://trac.osgeo.org/grass/changeset/68502, https://trac.osgeo.org/grass/changeset/68507, https://trac.osgeo.org/grass/changeset/68950
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_2@69051 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.northarrow/options.h
+++ display/d.northarrow/options.h
@@ -2,7 +2,7 @@
 /* globals */
 extern int fg_color;
 extern int bg_color;
-extern int do_background;
+extern int text_color;
 
 /* draw_n_arrow.c */
-int draw_n_arrow(double, double, double, char *, double);
+int draw_n_arrow(double, double, double, char *, int, double, char *, double);
