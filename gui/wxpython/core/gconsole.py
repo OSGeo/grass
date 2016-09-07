a40171805e667078c1b692b669ff6b06edaa9990
Martin Landa <landa.martin@gmail.com>
2016-09-07 18:51:20 +0000

    Pressing Run button in GUI dialogs open a new dialog (see https://trac.osgeo.org/grass/ticket/3135)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_2@69395 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/core/gconsole.py
+++ gui/wxpython/core/gconsole.py
@@ -44,7 +44,7 @@ from gui_core.forms import GUI
 from core.debug import Debug
 from core.settings import UserSettings
 from core.giface import Notification
-
+from gui_core.widgets import FormNotebook
 
 wxCmdOutput, EVT_CMD_OUTPUT = NewEvent()
 wxCmdProgress, EVT_CMD_PROGRESS = NewEvent()
@@ -532,8 +532,11 @@ class GConsole(wx.EvtHandler):
                             pymodule.main(self._giface)
                             return
 
-                    if hasParams and command[0] != 'v.krige':
-                        # no arguments given
+                    # no arguments given
+                    if hasParams and \
+                       not isinstance(self._guiparent, FormNotebook) and \
+                       command[0] != 'v.krige':
+                        # also parent must be checked, see #3135 for details
                         try:
                             GUI(parent=self._guiparent,
                                 giface=self._giface).ParseCommand(command)
