b10b80a1d72c2e013aae2527316958bdd49983cf
Anna Petrášová <kratochanna@gmail.com>
2016-09-28 02:43:08 +0000

    GUI/vdigit: fix snapping units, see #2848 (merge from trunk r69578, r69579, r69580)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_2@69594 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/core/settings.py
+++ gui/wxpython/core/settings.py
@@ -394,7 +394,8 @@ class Settings:
                 # snapping
                 'snapping': {
                     'value': 10,
-                    'units': 'screen pixels'
+                    'unit': 0,  # new
+                    'units': 'screen pixels'  # old for backwards comp.
                 },
                 'snapToVertex': {
                     'enabled': True
