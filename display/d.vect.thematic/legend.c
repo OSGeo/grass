941f31894a11af813ac3350aae026aabb0f39682
Anna Petrášová <kratochanna@gmail.com>
2016-10-16 03:11:40 +0000

    d.legend.vect: add new column to legend file to be able to distinguish line/fill color by d.vect and primary/secondary color by d.vect.thematic
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@69697 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.vect.thematic/legend.c
+++ display/d.vect.thematic/legend.c
@@ -28,7 +28,7 @@ void write_into_legend_file(const char *legfile, const char *icon, const char *t
         fd = fopen(legfile, "a");
 
     /* Title */
-    fprintf(fd, "|||||%s\n", title);
+    fprintf(fd, "||||||%s\n", title);
 
     /* First line */
     if (stats_min > breakpoints[0]){
@@ -37,16 +37,16 @@ void write_into_legend_file(const char *legfile, const char *icon, const char *t
     else{
         fprintf(fd, "%.2f - %.2f|",stats_min, breakpoints[0]);
     }
-    fprintf(fd, "%s|%d|%d:%d:%d|%d:%d:%d|%d|%s|%d\n",
-            icon, size, bcolor.r, bcolor.g, bcolor.b,
-            colors[0].r, colors[0].g, colors[0].b, default_width,
+    fprintf(fd, "%s|%d|ps|%d:%d:%d|%d:%d:%d|%d|%s|%d\n",
+            icon, size, colors[0].r, colors[0].g, colors[0].b,
+            bcolor.r, bcolor.g, bcolor.b, default_width,
             topo, frequencies[0]);
     /* Middle lines */
     for (i = 1; i < nbreaks; i++){
-        fprintf(fd, "%.2f - %.2f|%s|%d|%d:%d:%d|%d:%d:%d|%d|%s|%d\n",
+        fprintf(fd, "%.2f - %.2f|%s|%d|ps|%d:%d:%d|%d:%d:%d|%d|%s|%d\n",
                 breakpoints[i-1],breakpoints[i],
-                icon, size, bcolor.r, bcolor.g, bcolor.b,
-                colors[i].r, colors[i].g, colors[i].b, default_width,
+                icon, size, colors[i].r, colors[i].g, colors[i].b,
+                bcolor.r, bcolor.g, bcolor.b, default_width,
                 topo, frequencies[i]);
     }
     /* Last one */
@@ -56,9 +56,9 @@ void write_into_legend_file(const char *legfile, const char *icon, const char *t
     else {
         fprintf(fd, "%.2f - %.2f|", breakpoints[nbreaks - 1], stats_max);
     }
-    fprintf(fd, "%s|%d|%d:%d:%d|%d:%d:%d|%d|%s|%d\n",
-            icon, size, bcolor.r, bcolor.g, bcolor.b,
-            colors[nbreaks].r, colors[nbreaks].g, colors[nbreaks].b, default_width,
+    fprintf(fd, "%s|%d|ps|%d:%d:%d|%d:%d:%d|%d|%s|%d\n",
+            icon, size, colors[nbreaks].r, colors[nbreaks].g, colors[nbreaks].b,
+            bcolor.r, bcolor.g, bcolor.b, default_width,
             topo, frequencies[nbreaks]);
 
     fclose(fd);
