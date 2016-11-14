d231261d02feda048dfe9f5752f992fcd44152fb
Maris Nartiss <maris.gis@gmail.com>
2016-11-14 14:04:06 +0000

    Do not fiddle with button size as default works better (shows full length translated strings on the button)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@69827 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_core/dialogs.py
+++ gui/wxpython/gui_core/dialogs.py
@@ -2349,7 +2349,6 @@ class QuitDialog(wx.Dialog):
         self.btnClose.SetFocus()
         self.btnQuit = wx.Button(parent=self.panel, id=wx.ID_YES,
                                  label=_("Quit GRASS GIS"))
-        self.btnQuit.SetMinSize((130, self.btnQuit.GetSize()[1]))
         self.btnQuit.SetForegroundColour(wx.Colour(35, 142, 35))
 
         self.btnClose.Bind(wx.EVT_BUTTON, self.OnClose)
