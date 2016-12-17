bfa280c332ece614b1a8a08afd8f3e98751e85fe
Yann Chemin <yann.chemin@gmail.com>
2016-12-17 11:11:35 +0000

    Added mod09GA definitions
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@70082 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ imagery/i.modis.qc/mod09GAa.c
@@ -0,0 +1,22 @@
+/* MODLAND QA Bits 500m long int bits[0-1]
+ * 00 -> class 0: Corrected product produced at ideal quality -- all bands
+ * 01 -> class 1: Corrected product produced at less than idel quality -- some or all bands
+ * 10 -> class 2: Corrected product NOT produced due to cloud effect -- all bands
+ * 11 -> class 3: Corrected product NOT produced due to other reasons -- some or all bands mayb be fill value (Note that a value of [11] overrides a value of [01])
+ */  
+
+#include <grass/raster.h>
+
+CELL mod09GAa(CELL pixel) 
+{
+    CELL qctemp;
+
+    /* Select bit 0 and 1 (right-side).
+     * hexadecimal "0x03" => binary "11" 
+     * this will set all other bits to null */
+    qctemp = pixel & 0x03;
+
+    return qctemp;
+}
+
+
---
+++ imagery/i.modis.qc/mod09GAsg.c
@@ -0,0 +1,18 @@
+/* Internal Fire Algorithm Flag unsigned int bits[11]
+ * 0 -> class 0: No fire 
+ * 1 -> class 1: Fire
+ */  
+
+#include <grass/raster.h>
+
+CELL mod09GAsg(CELL pixel) 
+{
+    CELL qctemp;
+
+    pixel >>= 11;
+    qctemp = pixel & 0x01;
+
+    return qctemp;
+}
+
+
