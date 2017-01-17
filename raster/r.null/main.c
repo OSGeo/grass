8b9062d04a80da23f1cfe103a078502ca61d7e5f
Markus Metz <markus.metz.giswork@gmail.com>
2017-01-17 21:42:37 +0000

    r.null: backport trunk r70338
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_2@70390 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.null/main.c
+++ raster/r.null/main.c
@@ -196,9 +196,26 @@ int main(int argc, char *argv[])
     }
 
     if (recreate) {
-	int in_fd;
-
 	/* (un-)compress NULL file */
+	int in_fd;
+	char *nullcompr = getenv("GRASS_COMPRESS_NULLS");
+        int donullcompr = (nullcompr && atoi(nullcompr)) ? 1 : 0;
+
+	G_debug(1, "NULL compression is currently %s", donullcompr ? "enabled" : "disabled");
+
+	if (donullcompr) {
+	    if (G_find_file2_misc("cell_misc", "nullcmpr", name, mapset)) {
+		G_message(_("The NULL file is already compressed, nothing to do."));
+		exit(EXIT_SUCCESS);
+	    }
+	}
+	else {
+	    if (G_find_file2_misc("cell_misc", "null", name, mapset)) {
+		G_message(_("The NULL file is already uncompressed, nothing to do."));
+		exit(EXIT_SUCCESS);
+	    }
+	}
+
 	null_bits = Rast__allocate_null_bits(cellhd.cols);
 	Rast__init_null_bits(null_bits, cellhd.cols);
 
