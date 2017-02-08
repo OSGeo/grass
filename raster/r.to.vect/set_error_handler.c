3ea1871ffa98e40e632e0d6f89d56a6f57ee7054
Huidae Cho <grass4u@gmail.com>
2017-02-08 17:48:13 +0000

    Call the DB error handler before the vector handler to avoid busy database warnings
    Reproduce this issue in the North Carolina sample dataset:
    	r.to.vect input=aspect output=aspect type=line
    
    Any unthinned input raster will produce the same issue.
    
    
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@70506 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ raster/r.to.vect/set_error_handler.c
@@ -0,0 +1,31 @@
+#include <grass/gis.h>
+#include <grass/vector.h>
+
+struct handler_input
+{
+    struct Map_info *Map;
+    dbDriver **driver;
+};
+
+static void error_handler(void *p)
+{
+    const struct handler_input *input = (const struct handler_input *)p;
+
+    if (input->driver && *input->driver)
+	db_close_database_shutdown_driver(*(input->driver));
+    if (input->Map) {
+	if (input->Map->open == VECT_OPEN_CODE)
+	    Vect_close(input->Map);
+	Vect_delete(input->Map->name);
+    }
+}
+
+void set_error_handler(struct Map_info *Map, dbDriver **driver)
+{
+    struct handler_input *input = G_malloc(sizeof(struct handler_input));
+
+    input->Map = Map;
+    input->driver = driver;
+
+    G_add_error_handler(error_handler, input);
+}
