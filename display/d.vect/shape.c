4e01e99a808dcf51d8dfa56e45a2bb1b1b3a5ac3
Martin Landa <landa.martin@gmail.com>
2017-02-10 09:30:00 +0000

    d.vect: use Vect_get_map_box1() on level1 (see https://trac.osgeo.org/grass/ticket/3244)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@70524 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.vect/shape.c
+++ display/d.vect/shape.c
@@ -187,8 +187,18 @@ int display_shape(struct Map_info *Map, int type, struct cat_list *Clist, const 
 	    z_style = NULL;
         }
 	else {
-	    Vect_get_map_box(Map, &box);
-	    Rast_make_fp_colors(&zcolors, z_style, box.B, box.T);
+            int ret;
+
+            ret = 0;
+            if (Vect_level(Map) > 1)
+                ret = Vect_get_map_box(Map, &box);
+            else
+                ret = Vect_get_map_box1(Map, &box);
+
+            if (ret == 1)
+                Rast_make_fp_colors(&zcolors, z_style, box.B, box.T);
+            else
+                G_warning(_("Unable to colorize features, unknown map bounding box"));
 	}
     }
 
