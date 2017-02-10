857141e6f4659f0c8cfc8c3a0c2531ecc3493e84
Martin Landa <landa.martin@gmail.com>
2017-02-10 09:29:17 +0000

    Vect_get_map_box1(): print message on verbose level (cosmetics)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@70523 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/vector/Vlib/box.c
+++ lib/vector/Vlib/box.c
@@ -426,6 +426,8 @@ int Vect_get_map_box1(struct Map_info *Map, struct bound_box *Box)
     
     Points = Vect_new_line_struct();
     Vect_rewind(Map);
+    G_verbose_message(_("Topology not available for vector map <%s>. "
+                        "Registering primitives..."), Vect_get_full_name(Map));
     while (TRUE) {
         /* register line */
         type = Vect_read_next_line(Map, Points, NULL);
