2d4bea7e082bd7f6acf66f978852f49b9db07a38
Martin Landa <landa.martin@gmail.com>
2017-02-17 07:14:04 +0000

    broken links to source code and history in add-ons user manuals (see https://trac.osgeo.org/grass/ticket/3224)        (merge https://trac.osgeo.org/grass/changeset/70546 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_2@70605 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/mkhtml.py
+++ tools/mkhtml.py
@@ -307,10 +307,18 @@ curdir = os.path.abspath(os.path.curdir)
 pgmdir = curdir.replace(topdir, '').lstrip('/')
 if os.getenv('SOURCE_URL', ''):
     # addons
-    pgmname = os.path.basename(pgmdir)
-    classname = index_names[pgmname[:pgmname.find('.')]]
-    url_source = urlparse.urljoin('{0}{1}/'.format(os.environ['SOURCE_URL'], classname),
-                                  pgmname)
+    url_source = ''
+    for prefix in index_names.keys():
+        cwd = os.getcwd()
+        idx = cwd.find('{0}{1}.'.format(os.path.sep, prefix))
+        if idx > -1:
+            pgmname = cwd[idx+1:]
+            classname = index_names[prefix]
+            url_source = urlparse.urljoin('{0}{1}/'.format(
+                    os.environ['SOURCE_URL'], classname),
+                    pgmname
+            )
+            break
 else:
     url_source = urlparse.urljoin(source_url, pgmdir)
 if index_name:
