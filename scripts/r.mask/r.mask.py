1b0c4fef256b7eeed753f85b9764634025083d57
Markus Neteler <neteler@gmail.com>
2017-03-02 17:45:27 +0000

    r.mask: fix msg typo
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@70721 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- scripts/r.mask/r.mask.py
+++ scripts/r.mask/r.mask.py
@@ -94,7 +94,7 @@ def main():
     invert = flags['i']
 
     if not remove and not raster and not vector:
-        grass.fatal(_("Either parameter <raster> ot parameter <vector> is required"))
+        grass.fatal(_("Either parameter <raster> or parameter <vector> is required"))
 
     mapset = grass.gisenv()['MAPSET']
     exists = bool(grass.find_file('MASK', element='cell', mapset=mapset)['file'])
