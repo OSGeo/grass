07f5411aa1e0f8af7ff908e823558979d4fe52f6
Martin Landa <landa.martin@gmail.com>
2017-04-28 10:36:07 +0000

    vlib: skip build message when using PostGIS Topology (cosmetics)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@70976 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/vector/Vlib/copy.c
+++ lib/vector/Vlib/copy.c
@@ -96,8 +96,6 @@ int Vect_copy_map_lines_field(struct Map_info *In, int field,
         Vect_rewind(In);
         Vect_set_constraint_type(In, GV_POINT | GV_LINES);
         type = Vect_read_next_line(In, NULL, NULL);
-        if (!(type & (GV_POINTS | GV_LINES)))
-            G_fatal_error(_("Unsupported feature type %d"), type);
             
         /* create feature table with given feature type */
         if (0 > Vect_write_line(Out, type, NULL, NULL)) {
