06b8d2edc6dde48e71520d40ed171251f91c56e3
Anna Petrášová <kratochanna@gmail.com>
2017-07-08 13:36:39 +0000

    pythonlib: fix https://trac.osgeo.org/grass/ticket/3367 - creating gif files
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@71243 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/imaging/images2gif.py
+++ lib/python/imaging/images2gif.py
@@ -334,8 +334,8 @@ class GifWriter:
             Y = np.argwhere(diff.sum(1))
             # Get rect coordinates
             if X.size and Y.size:
-                x0, x1 = X[0], X[-1] + 1
-                y0, y1 = Y[0], Y[-1] + 1
+                x0, x1 = int(X[0]), int(X[-1] + 1)
+                y0, y1 = int(Y[0]), int(Y[-1] + 1)
             else:  # No change ... make it minimal
                 x0, x1 = 0, 2
                 y0, y1 = 0, 2
