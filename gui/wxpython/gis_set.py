6687ebc413dccb5b75b0ef1be9c92162f1078f52
Anna Petrášová <kratochanna@gmail.com>
2017-07-21 19:30:25 +0000

    wxGUI: fix importing vector during location creation when the vector file name does not match the layer name (merge from trunk, r71285)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_2@71286 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gis_set.py
+++ gui/wxpython/gis_set.py
@@ -583,7 +583,7 @@ class GRASSStartup(wx.Frame):
 
         wx.BeginBusyCursor()
         wx.Yield()
-        if mapName in vectors:
+        if vectors:
             # vector detected
             returncode, error = RunCommand(
                 'v.in.ogr', input=filePath, output=mapName, flags='e',
