12c702387271b6ef1348fd5788dfa889fd497bf2
Anna Petrášová <kratochanna@gmail.com>
2017-08-19 20:54:53 +0000

    r.sim: fix when openmp is not available
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@71419 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.sim/r.sim.sediment/main.c
+++ raster/r.sim/r.sim.sediment/main.c
@@ -375,7 +375,11 @@ int main(int argc, char *argv[])
       threads, abs(threads));
       threads = abs(threads);
     }
+#if defined(_OPENMP)
     omp_set_num_threads(threads);
+#else
+    threads = 1;
+#endif
     G_message(_("Number of threads: %d"), threads);
 
     /*      sscanf(parm.nwalk->answer, "%d", &wp.maxwa); */
