6e624687932be2435223068094d87961360df469
Moritz Lennert <mlennert@club.worldonline.be>
2017-08-26 13:46:31 +0000

    gui_core: add label in StaticWrapText widget used by SearchModuleWidget (fix for https://trac.osgeo.org/grass/ticket/3402)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@71439 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_core/widgets.py
+++ gui/wxpython/gui_core/widgets.py
@@ -1088,7 +1088,7 @@ class SearchModuleWidget(wx.Panel):
 
         if self._showTip:
             self._searchTip = StaticWrapText(parent=self, id=wx.ID_ANY,
-                                             size=(-1, 35))
+                                  label="Choose a module", size=(-1, 35))
 
         if self._showChoice:
             self._searchChoice = wx.Choice(parent=self, id=wx.ID_ANY)
@@ -1199,7 +1199,7 @@ class SearchModuleWidget(wx.Panel):
         """Reset widget"""
         self._search.SetValue('')
         if self._showTip:
-            self._searchTip.SetLabel('')
+            self._searchTip.SetLabel('Choose a module')
 
 
 class ManageSettingsWidget(wx.Panel):
