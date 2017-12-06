01ef447e0a188865dca262e62f58b951718e6808
Markus Neteler <neteler@gmail.com>
2017-12-06 14:31:49 +0000

    temporal: update header to standard statement
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_4@71900 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- temporal/t.list/t.list.py
+++ temporal/t.list/t.list.py
@@ -6,11 +6,17 @@
 # AUTHOR(S): Soeren Gebbert
 #
 # PURPOSE: List space time datasets and maps registered in the temporal database
-# COPYRIGHT: (C) 2011-2014, Soeren Gebbert and the GRASS Development Team
+# COPYRIGHT: (C) 2011-2017, Soeren Gebbert and the GRASS Development Team
 #
-# This program is free software under the GNU General Public
-# License (version 2). Read the file COPYING that comes with GRASS
-# for details.
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
 #############################################################################
 
