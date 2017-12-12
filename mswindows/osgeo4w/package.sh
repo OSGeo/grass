670aea556a2b9d041da3dabd66e594cd80976005
Helmut Kudrnovsky <hkmyricaria@gmail.com>
2017-12-12 22:26:09 +0000

    disable lidar modules: see https://trac.osgeo.org/osgeo4w/ticket/550 and https://trac.osgeo.org/osgeo4w/ticket/539
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_4@71921 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/package.sh
+++ mswindows/osgeo4w/package.sh
@@ -155,11 +155,13 @@ if ! [ -f mswindows/osgeo4w/configure-stamp ]; then
 
 	if [ "$MINGW_POSTFIX" = "64" ]; then
 	    conf_host=x86_64-w64-mingw32
-            # see https://trac.osgeo.org/osgeo4w/ticket/539#ticket
-            # LAS support only enabled on 64bit
-            conf_opts="--with-liblas=$PWD/mswindows/osgeo4w/liblas-config"
+            # https://trac.osgeo.org/osgeo4w/ticket/550
+            # LAS support hopefully only temporarily disabled on 64bit
+            conf_opts=
 	else
 	    conf_host=i386-w64-mingw32
+            # https://trac.osgeo.org/osgeo4w/ticket/539
+            #  LAS support hopefully only temporarily disabled on 32bit
             conf_opts=
 	fi
 	
