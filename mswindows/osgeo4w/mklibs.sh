3ec763e8ec162381fa228aea85ebc35a9423bbeb
Helmut Kudrnovsky <hkmyricaria@gmail.com>
2017-12-25 21:16:21 +0000

    mklibs.sh: add example $PATH for MS Visual Studio 2017 Community 64 bit
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@71969 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/mklibs.sh
+++ mswindows/osgeo4w/mklibs.sh
@@ -14,8 +14,12 @@ fi
 
 PROGRAMFILES="/c/Program Files (x86)"
 VSDIR="$PROGRAMFILES/Microsoft Visual Studio 14.0"
+# add example for MS Visual Studio 2017 Community 64 bit
+# VSDIR="$PROGRAMFILES/Microsoft Visual Studio/2017/Community"
 PATH="$VSDIR/Common7/IDE:$PATH"
 PATH="$VSDIR/VC/bin:$PATH"
+# add example for MS Visual Studio 2017 Community 64 bit
+# PATH="$VSDIR/VC/Tools/MSVC/14.12.25827/bin/Hostx64/x64:$PATH"
 PATH="$VSDIR/Common7/Tools:$PATH"
 PATH="$PATH:/c/OSGeo4W${OSGEO4W_POSTFIX}/bin"
 export PATH
