88502d54654e462bc30a88e1f44ebe3a78afa7e8
Markus Metz <markus.metz.giswork@gmail.com>
2018-03-20 23:28:47 +0000

    d.grid: adjust PROJ 5 usage
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@72452 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.grid/local_proto.h
+++ display/d.grid/local_proto.h
@@ -11,9 +11,9 @@ int plot_geogrid(double, struct pj_info, struct pj_info, int, int, int, int,
                  int, int, double, int, int);
 void init_proj(struct pj_info *, struct pj_info *, int);
 void get_ll_bounds(double *, double *, double *, double *, struct Cell_head,
-                   struct pj_info, struct pj_info);
+                   struct pj_info, struct pj_info, int);
 void check_coords(double, double, double *, double *, int, struct Cell_head,
-                  struct pj_info, struct pj_info);
+                  struct pj_info, struct pj_info, int);
 float get_heading(double, double);
 
 /* plotborder.c */
