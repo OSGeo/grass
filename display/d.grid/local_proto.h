9804043e4887ba656fede27523de7176fb61e85d
Markus Metz <markus.metz.giswork@gmail.com>
2018-03-23 09:22:34 +0000

    d.grid: use new GRASS API for coordinate transformation
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@72511 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.grid/local_proto.h
+++ display/d.grid/local_proto.h
@@ -7,13 +7,13 @@
 /* plot.c */
 int plot_grid(double, double, double, int, int, int, int, int, int, double,
               int);
-int plot_geogrid(double, struct pj_info, struct pj_info, int, int, int, int,
-                 int, int, double, int, int);
-void init_proj(struct pj_info *, struct pj_info *, int);
+int plot_geogrid(double, struct pj_info *, struct pj_info *, struct pj_info *,
+                 int, int, int, int, int, int, double, int);
+void init_proj(struct pj_info *, struct pj_info *, struct pj_info *, int);
 void get_ll_bounds(double *, double *, double *, double *, struct Cell_head,
-                   struct pj_info, struct pj_info, int);
+                   struct pj_info *, struct pj_info *, struct pj_info *);
 void check_coords(double, double, double *, double *, int, struct Cell_head,
-                  struct pj_info, struct pj_info, int);
+                  struct pj_info *, struct pj_info *, struct pj_info *);
 float get_heading(double, double);
 
 /* plotborder.c */
