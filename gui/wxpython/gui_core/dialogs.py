fe83b05fb92b86ffc472611d51b06a0ca53e4913
Anna Petrášová <kratochanna@gmail.com>
2018-06-17 19:51:21 +0000

    wxGUI: replace deprecated checkbox event method (merge from trunk, see #3151)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_4@72855 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_core/dialogs.py
+++ gui/wxpython/gui_core/dialogs.py
@@ -916,7 +916,7 @@ class GroupDialog(wx.Dialog):
         self._checkGSellAll()
 
     def OnSubgSelAll(self, event):
-        check = event.Checked()
+        check = event.IsChecked()
         for item in range(self.subgListBox.GetCount()):
             self.CheckSubgItem(item, check)
             self.dataChanged = True
@@ -924,7 +924,7 @@ class GroupDialog(wx.Dialog):
         event.Skip()
 
     def OnGSelAll(self, event):
-        check = event.Checked()
+        check = event.IsChecked()
         if not check:
             self.gLayerBox.DeselectAll()
         else:
