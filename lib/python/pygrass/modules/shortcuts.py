589c4bdd79de18896538d1efce28eb1071b5c6d2
Pietro Zambelli <peter.zamb@gmail.com>
2018-08-03 05:50:27 +0000

    pygrass - shortcuts: Fix use of python reserved words
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@73029 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/pygrass/modules/shortcuts.py
+++ lib/python/pygrass/modules/shortcuts.py
@@ -34,6 +34,17 @@ class MetaModule(object):
        >>> what.inputs.map = 'elevation'
        >>> what.inputs.coordinates = [640000,220500]          # doctest: +SKIP
        >>> what.run()                                         # doctest: +SKIP
+       >>> v = MetaModule('v')
+       >>> v.import                                      # doctest: +ELLIPSIS
+       Traceback (most recent call last):
+         File ".../doctest.py", line 1315, in __run
+          compileflags, 1) in test.globs
+         File "<doctest grass.pygrass.modules.shortcuts.MetaModule[16]>", line 1
+          v.import
+               ^
+       SyntaxError: invalid syntax
+       >>> v.import_
+       Module('v.import')
     """
     def __init__(self, prefix, cls=None):
         self.prefix = prefix
@@ -44,7 +55,8 @@ class MetaModule(object):
                 for mod in fnmatch.filter(_CMDS, "%s.*" % self.prefix)]
 
     def __getattr__(self, name):
-        return self.cls('%s.%s' % (self.prefix, name.replace('_', '.')))
+        return self.cls('%s.%s' % (self.prefix,
+                                   name.strip('_').replace('_', '.')))
 
 
 # http://grass.osgeo.org/grass75/manuals/full_index.html
