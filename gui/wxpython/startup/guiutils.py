bdbba2ae7173e2d6fc7530a2b4f253ea92139bfa
Vaclav Petras <wenzeslaus@gmail.com>
2018-08-24 03:05:13 +0000

    wxGUI/startup: file to separate GUI-dependent utils (now only db/loc/mapset setting)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@73153 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ gui/wxpython/startup/guiutils.py
@@ -0,0 +1,24 @@
+"""
+@package startup.guiutils
+
+@brief General GUI-dependent utilities for GUI startup of GRASS GIS
+
+(C) 2018 by Vaclav Petras the GRASS Development Team
+
+This program is free software under the GNU General Public License
+(>=v2). Read the file COPYING that comes with GRASS for details.
+
+@author Vaclav Petras <wenzeslaus gmail com>
+
+This is for code which depend on something from GUI (wx or wxGUI). 
+"""
+
+
+from core.gcmd import RunCommand
+
+
+def SetSessionMapset(database, location, mapset):
+    """Sets database, location and mapset for the current session"""
+    RunCommand("g.gisenv", set="GISDBASE=%s" % database)
+    RunCommand("g.gisenv", set="LOCATION_NAME=%s" % location)
+    RunCommand("g.gisenv", set="MAPSET=%s" % mapset)
