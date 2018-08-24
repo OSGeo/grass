3bc688807688dd4fd15a16bfb9853459d8783e5b
Vaclav Petras <wenzeslaus@gmail.com>
2018-08-24 03:45:40 +0000

    wxGUI/startup: new mapset dialog as a separate class and reuse create mapset method
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@73154 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/startup/guiutils.py
+++ gui/wxpython/startup/guiutils.py
@@ -14,7 +14,13 @@ This is for code which depend on something from GUI (wx or wxGUI).
 """
 
 
+import wx
+
+import grass.script as gs
+
 from core.gcmd import RunCommand
+from gui_core.dialogs import TextEntryDialog
+from gui_core.widgets import GenericValidator
 
 
 def SetSessionMapset(database, location, mapset):
@@ -22,3 +28,29 @@ def SetSessionMapset(database, location, mapset):
     RunCommand("g.gisenv", set="GISDBASE=%s" % database)
     RunCommand("g.gisenv", set="LOCATION_NAME=%s" % location)
     RunCommand("g.gisenv", set="MAPSET=%s" % mapset)
+
+
+
+class NewMapsetDialog(TextEntryDialog):
+    def __init__(self, parent=None, default=None,
+                 validation_failed_handler=None, help_hanlder=None):
+        if help_hanlder:
+            style = wx.OK | wx.CANCEL | wx.HELP
+        else:
+            style = wx.OK | wx.CANCEL
+        if validation_failed_handler:
+            validator=GenericValidator(
+                gs.legal_name, validation_failed_handler)
+        else:
+            validator = None
+        TextEntryDialog.__init__(
+            self, parent=parent,
+            message=_("Name for the new mapset:"),
+            caption=_("Create new mapset"),
+            defaultValue=default,
+            validator=validator,
+            style=style
+        )
+        if help_hanlder:
+            help_button = self.FindWindowById(wx.ID_HELP)
+            help_button.Bind(wx.EVT_BUTTON, help_hanlder)
