08a362e02e72a03f6f4854bf0b4714210620dd61
Soeren Gebbert <soerengebbert@googlemail.com>
2018-10-21 18:04:26 +0000

    pygrass library: Fixed doctests in _gunittest/gmodules
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@73587 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/python/gunittest/gmodules.py
+++ lib/python/gunittest/gmodules.py
@@ -39,7 +39,7 @@ class SimpleModule(Module):
     >>> str(colors.outputs.stdout)
     ''
     >>> colors.outputs.stderr.strip()
-    "Color table for raster map <test_a> set to 'rules'"
+    u"Color table for raster map <test_a> set to 'rules'"
     """
     def __init__(self, cmd, *args, **kargs):
         for banned in ['stdout_', 'stderr_', 'finish_', 'run_']:
