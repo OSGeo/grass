8df08897b3cd4d038453a9ba86667dd3d45aecc0
Martin Landa <landa.martin@gmail.com>
2019-01-28 09:56:40 +0000

    wingrass: change instalation target, see #3732 (g76: merge r74027:74029 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_7_6@74038 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/postinstall.bat
+++ mswindows/osgeo4w/postinstall.bat
@@ -1,7 +1,7 @@
-set ICON=%OSGEO4W_ROOT%\apps\grass\grass-@VERSION@\gui\icons\grass_osgeo.ico
+set ICON=%OSGEO4W_ROOT%\apps\grass\grass@POSTFIX@\gui\icons\grass_osgeo.ico
 set BATCH=%OSGEO4W_ROOT%\bin\@GRASS_EXECUTABLE@.bat
 textreplace -std -t "%BATCH%"
-textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
+textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@POSTFIX@\etc\fontcap
 
 xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@.lnk"       "%BATCH%"  "--gui" \ "Launch GRASS GIS @VERSION@" 1 "%ICON%" 
 xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk" "%BATCH%"  "--gui" \ "Launch GRASS GIS @VERSION@" 1 "%ICON%" 
@@ -10,7 +10,7 @@ rem run g.mkfontcap outside a GRASS session during
 rem an OSGeo4W installation for updating paths to fonts
 
 rem set gisbase
-set GISBASE=%OSGEO4W_ROOT%\apps\grass\grass-@VERSION@
+set GISBASE=%OSGEO4W_ROOT%\apps\grass\grass@POSTFIX@
 
 rem set path to freetype dll and its dependencies
 set FREETYPEBASE=%OSGEO4W_ROOT%\bin;%OSGEO4W_ROOT%\apps\msys\bin;%GISBASE%\lib
