9c748a74235c84cf4e1d3f42aeba9b6a3645608e
Markus Metz <markus.metz.giswork@gmail.com>
2019-01-30 08:41:15 +0000

    postgres driver: create index if not exists for PG server versions 9.5+
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@74042 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/drivers/postgres/index.c
+++ db/drivers/postgres/index.c
@@ -34,6 +34,8 @@ int db__driver_create_index(dbIndex * index)
 	db_append_string(&sql, " unique");
 
     db_append_string(&sql, " index ");
+    if (PQserverVersion(pg_conn) >= 905000)
+	db_append_string(&sql, " if not exists ");
     db_append_string(&sql, db_get_index_name(index));
     db_append_string(&sql, " on ");
 
