4d2b0603e536183ed83f85450bad4489f49d962d
Martin Landa <landa.martin@gmail.com>
2019-03-17 19:45:18 +0000

    v.to.points: new option use=start/end [news]
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@74283 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.to.points/local_proto.h
+++ vector/v.to.points/local_proto.h
@@ -1,6 +1,8 @@
 #ifndef __LOCAL_PROTO_H
 #define GV_NODE   1
 #define GV_VERTEX 2
+#define GV_START  3
+#define GV_END    4
 
 void write_point(struct Map_info *, double, double, double,
 		 int, double, dbDriver *, struct field_info *);
