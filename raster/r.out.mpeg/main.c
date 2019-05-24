78e9da1af300035ab1dbe4135ab79d1068aadd58
Markus Metz <markus.metz.giswork@gmail.com>
2019-05-24 22:04:38 +0200

    r.out.mpeg fails because the output file name is empty
    This fix copies the output file name to the correct variable, as in GRASS 6

--- raster/r.out.mpeg/main.c
+++ raster/r.out.mpeg/main.c
@@ -6,6 +6,8 @@
  *               Brad Douglas <rez touchofmadness.com>, Markus Neteler <neteler itc.it>,
  *               Glynn Clements <glynn gclements.plus.com>, Hamish Bowman <hamish_b yahoo.com>,
  *               Jan-Oliver Wagner <jan intevation.de>, Paul Kelly <paul-grass stjohnspoint.co.uk>
+ *               Paolo Zatelli <paolo.zatelli unitn.it>
+ *
  * PURPOSE:      combines a series of GRASS raster maps into a single MPEG-1
  * COPYRIGHT:    (C) 1999-2006, 2011 by the GRASS Development Team
  *
@@ -141,6 +143,9 @@ int main(int argc, char **argv)
 	exit(EXIT_FAILURE);
 
     parse_command(viewopts, vfiles, &numviews, &frames);
+
+    /* output file */
+    strcpy(outfile, out->answer);
 
     r_out = 0;
     if (conv->answer)
