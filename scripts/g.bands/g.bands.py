738961a3dc89ad78a9e006d8082f94bb94ef637f
Martin Landa <landa.martin@gmail.com>
2019-05-26 16:45:55 +0200

    move BandReader to a separate module (will by used by PyGRASS) add BandReader.find_filename() see #10

--- scripts/g.bands/g.bands.py
+++ scripts/g.bands/g.bands.py
@@ -31,103 +31,13 @@
 #% multiple: no
 #%end
 
-import os
 import sys
-import json
-import glob
-from collections import OrderedDict
 
 import grass.script as gs
 
-class BandReaderError(Exception):
-    def __init__(self, msg):
-        gs.fatal(msg)
-
-class BandReader:
-    def __init__(self):
-        json_files = glob.glob(
-            os.path.join(os.environ['GISBASE'], 'etc', 'g.bands', '*.json')
-        )
-        if not json_files:
-            raise BandReaderError("No band definitions found")
-        self.config = OrderedDict()
-        for json_file in json_files:
-            with open(json_file) as fd:
-                config = json.load(
-                    fd,
-                    object_pairs_hook=OrderedDict
-                )
-                # check if configuration is valid
-                self._check_config(config)
-
-                # check configuration intersection
-                config_intersection = list(set(config.keys()) & set(self.config.keys()))
-                if config_intersection:
-                    raise BandReaderError("Band identifiers intersection: {}".format(
-                        ','.join(config_intersection)
-                    ))
-                # merge configs
-                self.config.update(config)
-
-    @staticmethod
-    def _check_config(config):
-        for items in config.values():
-            for item in ('shortcut', 'bands'):
-                if item not in items.keys():
-                    raise BandReaderError(
-                        "Invalid band definition: <{}> is missing".format(item
-                ))
-            if len(items['bands']) < 1:
-                raise BandReaderError(
-                    "Invalid band definition: no bands defined"
-                )
-
-    @staticmethod
-    def _print_band(band, item):
-        indent = 4
-        print ('{}band: {}'.format(
-            ' ' * indent, band
-        ))
-        for k, v in item[band].items():
-            print ('{}{}: {}'.format(' ' * indent * 2, k, v))
-
-    def print_info(self, shortcut=None, band=None):
-        found = False
-        for item in self.config.values():
-            if shortcut and item['shortcut'] != shortcut:
-                continue
-
-            found = True
-            if band and band not in item['bands']:
-                raise BandReaderError(
-                    "Band <{}> not found in <{}>".format(
-                        band, shortcut
-                    ))
-
-            # print generic information
-            for subitem in item.keys():
-                if subitem == 'bands':
-                    # bands item is processed bellow
-                    continue
-                print ('{}: {}'.format(
-                    subitem, item[subitem]
-                ))
-
-            # print detailed band information
-            if band:
-                self._print_band(band, item['bands'])
-            else:
-                for iband in item['bands']:
-                    self._print_band(iband, item['bands'])
-
-        # raise error when defined shortcut not found
-        if shortcut and not found:
-            raise BandReaderError(
-                "<{}> not found".format(shortcut)
-            )
-
 def main():
-    reader = BandReader()
+    gs.utils.set_path('g.bands')
+    from reader import BandReader
 
     band = None
     kwargs = {}
@@ -138,6 +48,7 @@ def main():
     else:
         kwargs['shortcut'] = options['bands']
 
+    reader = BandReader()
     reader.print_info(**kwargs)
 
 if __name__ == "__main__":
---
+++ scripts/g.bands/reader.py
@@ -0,0 +1,129 @@
+import os
+import sys
+import json
+import glob
+from collections import OrderedDict
+
+import grass.script as gs
+
+class BandReaderError(Exception):
+    def __init__(self, msg):
+        gs.fatal(msg)
+
+class BandReader:
+    """Band references reader"""
+    def __init__(self):
+        self._json_files = glob.glob(
+            os.path.join(os.environ['GISBASE'], 'etc', 'g.bands', '*.json')
+        )
+        if not self._json_files:
+            raise BandReaderError("No band definitions found")
+
+        self._read_config()
+
+    def _read_config(self):
+        """Read configuration"""
+        self.config = dict()
+        for json_file in self._json_files:
+            with open(json_file) as fd:
+                config = json.load(
+                    fd,
+                    object_pairs_hook=OrderedDict
+                )
+            # check if configuration is valid
+            self._check_config(config)
+
+            self.config[os.path.basename(json_file)] = config
+
+    @staticmethod
+    def _check_config(config):
+        """Check if config is valid
+
+        :todo: check shortcut uniqueness
+
+        :param dict config: configuration to be validated
+        """
+        for items in config.values():
+            for item in ('shortcut', 'bands'):
+                if item not in items.keys():
+                    raise BandReaderError(
+                        "Invalid band definition: <{}> is missing".format(item
+                ))
+            if len(items['bands']) < 1:
+                raise BandReaderError(
+                    "Invalid band definition: no bands defined"
+                )
+
+    @staticmethod
+    def _print_band(band, item):
+        indent = 4
+        print ('{}band: {}'.format(
+            ' ' * indent, band
+        ))
+        for k, v in item[band].items():
+            print ('{}{}: {}'.format(' ' * indent * 2, k, v))
+
+    def print_info(self, shortcut=None, band=None):
+        """Prints band reference information to stdout.
+
+        Can be filtered by shortcut or band identifier.
+
+        :param str shortcut: shortcut to filter (eg. S2A) or None
+        :param str band: band (eg. 1) or None
+        """
+        found = False
+        for root in self.config.values():
+            for item in root.values():
+                if shortcut and item['shortcut'] != shortcut:
+                    continue
+
+                found = True
+                if band and band not in item['bands']:
+                    raise BandReaderError(
+                        "Band <{}> not found in <{}>".format(
+                            band, shortcut
+                        ))
+
+                # print generic information
+                for subitem in item.keys():
+                    if subitem == 'bands':
+                        # bands item is processed bellow
+                        continue
+                    print ('{}: {}'.format(
+                        subitem, item[subitem]
+                    ))
+
+                # print detailed band information
+                if band:
+                    self._print_band(band, item['bands'])
+                else:
+                    for iband in item['bands']:
+                        self._print_band(iband, item['bands'])
+
+        # raise error when defined shortcut not found
+        if shortcut and not found:
+            raise BandReaderError(
+                "<{}> not found".format(shortcut)
+            )
+
+    def find_filename(self, band_reference):
+        """Find filename by band reference.
+
+        :param str band_reference: band reference identifier to search for (eg. S2A_1)
+
+        :return str: file basename if found or None
+        """
+        try:
+            shortcut, band = band_reference.split('_')
+        except ValueError:
+            raise BandReaderError("Invalid band identifier <{}>".fomat(
+                band_identifier
+            ))
+
+        for filename, config in self.config.items():
+            for root in config.keys():
+                if config[root]['shortcut'] == shortcut and \
+                   band in config[root]['bands'].keys():
+                    return filename
+
+        return None
