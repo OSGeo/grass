188fc2b6940e870638698f8c644d808559c559bc
Markus Metz <33666869+metzm@users.noreply.github.com>
2019-06-07 19:44:38 +0200

    v.voronoi for areas (#26)
    v.voronoi
    
    Fix for area skeletons and voronoi diagrams for areas.
    
    Numerical stability of v.voronoi has been improved but is not perfect.

--- vector/v.voronoi/defs.h
+++ vector/v.voronoi/defs.h
@@ -1,7 +1,7 @@
 
 extern struct Cell_head Window;
 extern struct bound_box Box;
-extern struct Map_info In, Out;
+extern struct Map_info In, Out, Out2;
 extern int Type;
 extern int Field;
 extern int in_area;
