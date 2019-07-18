3b4c55e51b068ae63ea135f6c9310b92f471f07d
Martin Landa <landa.martin@gmail.com>
2019-07-18 18:50:47 +0200

    g.bands: add testsuite (first three tests)

---
+++ scripts/g.bands/testsuite/test_g_bands.py
@@ -0,0 +1,42 @@
+import os
+
+from grass.gunittest.case import TestCase
+from grass.gunittest.main import test
+from grass.gunittest.gmodules import SimpleModule, call_module
+
+class TestBandsSystemDefined(TestCase):
+    @staticmethod
+    def _number_of_bands(**kwargs):
+        gbands = call_module('g.bands', **kwargs)
+        return len(gbands.rstrip(os.linesep).split(os.linesep))
+
+    def test_number_system_defined(self):
+        # test number of valid band identifiers
+        #
+        # get number of valid band identifiers by g.bands
+        nbands = self._number_of_bands()
+
+        # get number of valid band identifiers by Bands lib
+        from grass.bands import BandReader
+        nbands_ref = len(BandReader().get_bands())
+
+        self.assertEqual(nbands, nbands_ref)
+
+    def test_number_s2a(self):
+        # test number of S2A band identifiers (hardcoded, no changes expected)
+        #
+        nbands = self._number_of_bands(band='S2A')
+
+        self.assertEqual(nbands, 13)
+
+
+    def test_number_s2a_1(self):
+        # test if S2A_1 is defined (lower + upper case)
+        #
+        band = 'S2A_1'
+        for iband in [band, band.lower()]:
+            nbands = self._number_of_bands(band=iband)
+            self.assertEqual(nbands, 1)
+
+if __name__ == '__main__':
+    test()
