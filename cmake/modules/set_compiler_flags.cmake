eae5f0bbffe0e930d3bac0b1adcaff98a4b9bdc0
Rashad Kanavath <mohammedrashadkm@gmail.com>
2019-12-30 21:50:16 +0100

    CMake: reorganize cmake functions (2)

---
+++ cmake/modules/set_compiler_flags.cmake
@@ -0,0 +1,13 @@
+macro(set_compiler_flags)
+if(MSVC)
+  set(GRASS_C_FLAGS "/D_CRT_SECURE_NO_WARNINGS")
+  if(CMAKE_C_FLAGS)
+    set(CMAKE_C_FLAGS "${GRASS_C_FLAGS} ${CMAKE_C_FLAGS}")
+  endif()
+
+  set(GRASS_CXX_FLAGS "/D_CRT_SECURE_NO_WARNINGS")
+  if(CMAKE_CXX_FLAGS)
+    set(CMAKE_CXX_FLAGS "${GRASS_CXX_FLAGS} ${CMAKE_CXX_FLAGS}")
+  endif()
+endif()
+endmacro()
