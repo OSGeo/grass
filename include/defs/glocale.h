b1af94a513b228141453867ce2fe94f446375ae4
Rashad Kanavath <mohammedrashadkm@gmail.com>
2019-12-30 13:21:13 +0100

    include grass/gis.h on msvc for __atribute fix

--- include/defs/glocale.h
+++ include/defs/glocale.h
@@ -1,6 +1,8 @@
 #ifndef GRASS_GLOCALEDEFS_H
 #define GRASS_GLOCALEDEFS_H
-
+#if defined(_MSC_VER)
+#include "grass/gis.h"
+#endif
 extern void G_init_locale(void);
 extern char *G_gettext(const char *, const char *) __attribute__((format_arg (2)));
 extern char *G_ngettext(const char *, const char *, const char *, unsigned long int) __attribute__((format_arg (2), format_arg (3)));
