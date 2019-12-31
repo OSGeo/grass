cd8d72f16f95dd74cca86d470ad291efdc094c0e
Rashad Kanavath <mohammedrashadkm@gmail.com>
2019-12-31 07:29:33 +0100

    avoid touch  .stamp files and depend on generated pyc file

--- cmake/modules/copy_python_file.cmake
+++ cmake/modules/copy_python_file.cmake
@@ -11,20 +11,20 @@ function(copy_python_file py_file py_module_name py_DIR_PATH install_dest)
 	 
   set(output_path ${CMAKE_CURRENT_BINARY_DIR}/${py_DIR_PATH})
   file(COPY ${py_file_REALPATH} DESTINATION ${output_path})
-  set(copied_py_file ${output_path}/${py_file_NAME})
+  set(copied_file_path ${output_path}/${py_file_NAME})
 
-  set(stamp_file_path ${output_path}/CMakeFiles/${py_file_NAME}.stamp)
+  set(pyc_file_path "${copied_file_path}c")
   add_custom_command(
-    OUTPUT ${stamp_file_path}
-    COMMAND ${PYTHON_EXECUTABLE} -t -m py_compile ${copied_py_file}
-    COMMAND ${CMAKE_COMMAND} -E touch  ${stamp_file_path}
-    COMMENT "Generating ${copied_py_file}c"
+    OUTPUT ${pyc_file_path}
+    COMMAND ${PYTHON_EXECUTABLE} -t -m py_compile ${copied_file_path}
+    COMMENT "Generating ${pyc_file_path}"
     VERBATIM)
-  
+
+  set_source_files_properties("${pyc_file_path}" GENERATED)
+
   add_custom_target(${py_module_name}.${py_file_NAME} ALL
-    DEPENDS ${stamp_file_path})
-    
-  set_source_files_properties("${copied_py_file}c" GENERATED)
-  install(FILES ${copied_py_file} DESTINATION ${install_dest})
+    DEPENDS ${pyc_file_path})
+
+  install(FILES ${copied_file_path} DESTINATION ${install_dest})
 
 endfunction()
