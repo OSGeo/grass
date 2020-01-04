b1236ea35e7f5d44e61c29f0b589fafdfe3e0ae3
Rashad Kanavath <mohammedrashadkm@gmail.com>
2020-01-04 01:49:08 +0100

    wrapper scripts to build html docs

---
+++ cmake/run_python.bat
@@ -0,0 +1,13 @@
+@echo off
+set MODULE_TOPDIR=@module_top_dir@
+set GISBASE=@bin_dir@
+set GISRC=@bin_dir@\etc\config\rc
+set PATH=@bin_dir@\bin;@bin_dir@\scripts;%PATH%
+set PYTHONPATH=@bin_dir@\etc\python;@bin_dir@\gui\wxpython;%PYTHONPATH%
+set LC_ALL=C
+set LANG=C
+set LANGUAGE=C
+set VERSION_NUMBER=@GRASS_VERSION_NUMBER@
+set VERSION_DATE=@VERSION_DATE@
+"@PYTHON_EXECUTABLE@" %*
+
