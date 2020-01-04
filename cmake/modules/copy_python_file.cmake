5acb30826612978555270d01a0789aae8c543a0a
Rashad Kanavath <mohammedrashadkm@gmail.com>
2020-01-04 01:47:48 +0100

    fix copy_python_file (used in gui/wxpython, lib/python)

--- cmake/modules/copy_python_file.cmake
+++ cmake/modules/copy_python_file.cmake
@@ -1,30 +1,26 @@
-function(copy_python_file py_file py_module_name py_DIR_PATH install_dest)
-  # A single case where setup.py from cmake binary directory is added. 
-  # For this corner case we need to check for IS_ABSOLUTE first!s
-  if(NOT IS_ABSOLUTE ${py_file})
-    get_filename_component(py_file_REALPATH ${py_DIR_PATH}/${py_file} REALPATH)
-  else()
-	set(py_file_REALPATH ${py_file})
-  endif()
-
-  get_filename_component(py_file_NAME ${py_file_REALPATH} NAME)
-	 
-  set(output_path ${CMAKE_CURRENT_BINARY_DIR}/${py_DIR_PATH})
-  file(COPY ${py_file_REALPATH} DESTINATION ${output_path})
+function(copy_python_file py_file install_dest)
+  get_filename_component(py_file_NAME ${py_file} NAME)
+  set(output_path ${CMAKE_BINARY_DIR}/${install_dest})
   set(copied_file_path ${output_path}/${py_file_NAME})
-
+   add_custom_command(OUTPUT ${copied_file_path}
+    COMMAND ${CMAKE_COMMAND} -E copy ${py_file} ${output_path}
+	DEPENDS g.parser
+    COMMENT "Copy ${output_path}/${py_file_NAME}"
+    VERBATIM)
   set(pyc_file_path "${copied_file_path}c")
   add_custom_command(
     OUTPUT ${pyc_file_path}
     COMMAND ${PYTHON_EXECUTABLE} -t -m py_compile ${copied_file_path}
+	DEPENDS ${copied_file_path}
     COMMENT "Generating ${pyc_file_path}"
     VERBATIM)
 
-  set_source_files_properties("${pyc_file_path}" GENERATED)
-
-  add_custom_target(${py_module_name}.${py_file_NAME} ALL
+  #set_source_files_properties("${pyc_file_path}" GENERATED)
+  string(REPLACE "/" "_" targ_name ${install_dest})
+  #message(FATAL_ERROR "targ_name=${targ_name}")
+  add_custom_target(${targ_name}_${py_file_NAME} ALL
     DEPENDS ${pyc_file_path})
-
+SET_TARGET_PROPERTIES (${targ_name}_${py_file_NAME} PROPERTIES FOLDER copy)
   install(FILES ${copied_file_path} DESTINATION ${install_dest})
 
 endfunction()
