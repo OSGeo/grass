1517395810649adc25e064dbe527a4d90a34c96a
Huidae Cho <grass4u@gmail.com>
2020-01-07 03:22:34 -0500

    Add 2020 to copyright

--- mswindows/crosscompile.sh
+++ mswindows/crosscompile.sh
@@ -5,7 +5,7 @@
 # MODULE:	crosscompile.sh
 # AUTHOR(S):	Huidae Cho <grass4u gmail.com>
 # PURPOSE:	Builds a cross-compiled portable package of GRASS GIS
-# COPYRIGHT:	(C) 2019 by Huidae Cho and the GRASS Development Team
+# COPYRIGHT:	(C) 2019, 2020 by Huidae Cho and the GRASS Development Team
 #
 #		This program is free software under the GNU General Public
 #		License (>=v2). Read the file COPYING that comes with GRASS
