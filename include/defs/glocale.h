17b6ea37857db5d1ff39be1dd2b7a3bd665c4f9e
Rashad Kanavath <mohammedrashadkm@gmail.com>
2020-02-18 01:12:32 +0100

    msvc: __atribute__  is gcc only

--- include/defs/glocale.h
+++ include/defs/glocale.h
@@ -1,6 +1,12 @@
 #ifndef GRASS_GLOCALEDEFS_H
 #define GRASS_GLOCALEDEFS_H
 
+#if defined(_MSC_VER)
+#ifndef __attribute__
+#define __attribute__(x)
+#endif
+#endif
+
 extern void G_init_locale(void);
 extern char *G_gettext(const char *, const char *) __attribute__((format_arg (2)));
 extern char *G_ngettext(const char *, const char *, const char *, unsigned long int) __attribute__((format_arg (2), format_arg (3)));
