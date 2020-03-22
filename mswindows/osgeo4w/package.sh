daff3ab9c83eeb94b091a713ff79cb95da50161c
Martin Landa <landa.martin@gmail.com>
2020-03-23 00:04:59 +0100

    revert changes in package.sh, see #445

--- mswindows/osgeo4w/package.sh
+++ mswindows/osgeo4w/package.sh
@@ -113,7 +113,11 @@ read PATCH <&3
 export VERSION=${MAJOR}.${MINOR}.${PATCH}
 export POSTFIX=${MAJOR}${MINOR}
 
-GRASS_EXECUTABLE=grass${MAJOR}${MINOR}
+if [[ "$PATCH" == *svn* ]] ; then
+    GRASS_EXECUTABLE=grass${MAJOR}${MINOR}svn
+else
+    GRASS_EXECUTABLE=grass${MAJOR}${MINOR}
+fi
 
 if [ -f mswindows/osgeo4w/package.log ]; then
     i=0
