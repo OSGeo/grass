c33bb5ecd4fe52f6406ae4ddffd316d80435f6fb
Markus Metz <33666869+metzm@users.noreply.github.com>
2020-03-23 08:34:07 +0100

    i.ortho.photo (#401)
    Add optional correction for panorama cameras, adjust for earth curvature
    
    * i.ortho.elev
    
    improve error messages, set mapset from target if not given
    
    * i.ortho.photo
    
    Omega and Phi are swapped in the description but correct in the code
    
    * i.ortho
    
    support panorama corrections
    
    * i.ortho photo2image
    
    InsertColumnInfo -> InsertColumn
    
    * i.ortho
    
    support orthorectification without GCPs if camera position and angles are known
    
    * i.ortho.transform
    
    fix for orthorectification
    
    * i.ortho.photo
    
    fix g.gui.image2target for wxpython 4 (phoenix)
    
    * i.ortho.target
    
    XY and ll locations are not supported
    
    * i.ortho.init
    
    explain importance of standard deviations for the improvement of the orthorectification parameters
    
    * i.ortho.photo
    
    lib: report number of iterations needed for the refinement of orthorectification parameters
    
    * i.ortho.photo
    
    refine panorama correction
    add correction for earth curvature
    
    * i.ortho.photo
    
    I_compute_ortho_equations() must be run in the target location for earth curvature adjustments

--- gui/wxpython/image2target/ii2t_manager.py
+++ gui/wxpython/image2target/ii2t_manager.py
@@ -2241,7 +2241,7 @@ class GCPList(ListCtrl,
                         _('Forward error'),
                         _('Backward error')):
                 info.SetText(lbl)
-                self.InsertColumnInfo(idx_col, info)
+                self.InsertColumn(idx_col, info)
                 idx_col += 1
 
     def LoadData(self):
