ca73967c7303e05881ca7698e5218b9c325af4e3
Martin Landa <landa.martin@gmail.com>
2020-03-29 18:12:01 +0200

    libgis_revision: use quotes only when string contrains a space

--- general/g.version/main.c
+++ general/g.version/main.c
@@ -150,7 +150,12 @@ int main(int argc, char *argv[])
 
 	if (*rev_ver && *rev_time) {
 	    if (shell->answer) {
-		fprintf(stdout, "libgis_revision=%s\n", rev_ver);
+                fprintf(stdout, "libgis_revision=");
+                if (strstr(rev_ver, " ") != NULL)
+                    fprintf(stdout, "\"%s\"", rev_ver);
+                else
+                    fprintf(stdout, "%s", rev_ver);
+                fprintf(stdout, "\n");
 		fprintf(stdout, "libgis_date=\"%s\"\n", rev_time);
 	    }
 	    else {
