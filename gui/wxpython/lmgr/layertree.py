912583c1031a00ac22b0b2df18116c7a0ce8221a
Vaclav Petras <wenzeslaus@gmail.com>
2020-04-16 15:44:46 -0400

    wxGUI/lmgr: Lazy import specialized dialogs and work without import numpy (#511)
    This follows other lazy imports in lmgr.frame and potentially speeds up GUI startup by
    lazy importing wxplot.histogram and SaveWMSLayerDialog.
    
    More importantly, this avoids numpy import in wx.lib.plot.plotcanvas used by wxplot.histogram.
    So, now GUI should start even without numpy installed with message in terminal from wxnviz.
    wxnviz should be the only place requiring numpy on startup and thus it is the only place proving
    this message about the missing numpy dependency.
    
    This also removes unused import of core.giface.Notification (replaced by use of two empty lines after imports (code style)).

--- gui/wxpython/lmgr/layertree.py
+++ gui/wxpython/lmgr/layertree.py
@@ -37,18 +37,16 @@ from gui_core.dialogs import SqlQueryFrame, SetOpacityDialog, TextEntryDialog
 from gui_core.forms import GUI
 from mapdisp.frame import MapFrame
 from core.render import Map
-from wxplot.histogram import HistogramPlotFrame
 from core.utils import GetLayerNameFromCmd, ltype2command
 from core.debug import Debug
 from core.settings import UserSettings, GetDisplayVectSettings
 from vdigit.main import haveVDigit
 from core.gcmd import GWarning, GError, RunCommand
 from icons.icon import MetaIcon
-from web_services.dialogs import SaveWMSLayerDialog
 from gui_core.widgets import MapValidator
 from gui_core.wrap import Menu, GenBitmapButton, TextCtrl, NewId
 from lmgr.giface import LayerManagerGrassInterfaceForMapDisplay
-from core.giface import Notification
+
 
 TREE_ITEM_HEIGHT = 25
 
@@ -898,6 +896,7 @@ class LayerTree(treemixin.DragAndDrop, CT.CustomTreeCtrl):
     def OnSaveWs(self, event):
         """Show dialog for saving web service layer into GRASS vector/raster layer"""
         mapLayer = self.GetLayerInfo(self.layer_selected, key='maplayer')
+        from web_services.dialogs import SaveWMSLayerDialog
         dlg = SaveWMSLayerDialog(parent=self, layer=mapLayer,
                                  giface=self._gifaceForDisplay)
         dlg.CentreOnScreen()
@@ -1136,6 +1135,8 @@ class LayerTree(treemixin.DragAndDrop, CT.CustomTreeCtrl):
                              "raster map. No map name defined."))
             return
 
+        # lazy import to reduce dependecies and startup
+        from wxplot.histogram import HistogramPlotFrame
         win = HistogramPlotFrame(parent=self, giface=self._giface, rasterList=rasterList)
         win.CentreOnScreen()
         win.Show()
