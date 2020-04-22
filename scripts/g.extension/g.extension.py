edad5d30d7647adbcf843c0a6547a212e3c8dbfe
Vaclav Petras <wenzeslaus@gmail.com>
2020-04-21 22:28:01 -0400

    Do not assign a lambda expression, use a def (Flake8 E731)

--- scripts/g.extension/g.extension.py
+++ scripts/g.extension/g.extension.py
@@ -1160,7 +1160,9 @@ def fix_newlines(directory):
     # skip binary files
     # see https://stackoverflow.com/a/7392391
     textchars = bytearray({7,8,9,10,12,13,27} | set(range(0x20, 0x100)) - {0x7f})
-    is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
+
+    def is_binary_string(bytes):
+        return bool(bytes.translate(None, textchars))
 
     for root, unused, files in os.walk(directory):
         for name in files:
