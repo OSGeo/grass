e671d1e3568113b450bb5605ac3abf1a21f28e35
Vaclav Petras <wenzeslaus@gmail.com>
2020-04-30 22:25:32 -0400

    Comparison to None should be 'if cond is not None:' (E711)

--- temporal/t.vect.observe.strds/t.vect.observe.strds.py
+++ temporal/t.vect.observe.strds/t.vect.observe.strds.py
@@ -64,7 +64,7 @@ class Sample(object):
     def __init__(self, start = None, end = None, raster_names = None):
         self.start = start
         self.end = end
-        if raster_names != None:
+        if raster_names is not None:
             self.raster_names = raster_names
         else:
             self.raster_names = []
