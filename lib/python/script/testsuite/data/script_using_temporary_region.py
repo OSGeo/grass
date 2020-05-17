b79a5d7e27ed38132c82d853f1230d690215396b
Vaclav Petras <wenzeslaus@gmail.com>
2020-05-16 23:53:07 -0400

    Create maps and check their existence
    Return codes of bg procs are not propagated, so something is needed to track processes.
    Using maps created at the end of each Python script to do that.
    Parametrize the Bash script to make it reusable also for a potential nesting region correctness test.
    Also be more robust when the Python script is not available (zero returned on some platforms).

--- lib/python/script/testsuite/data/script_using_temporary_region.py
+++ lib/python/script/testsuite/data/script_using_temporary_region.py
@@ -8,7 +8,7 @@ import platform
 import grass.script as gs
 
 
-def call_use_temp_region(script, size, remaining, nesting):
+def call_use_temp_region(script, size, remaining, nesting, map_name=None):
     pid = os.getpid()
     node = platform.node()
     gs.message(
@@ -26,7 +26,12 @@ def call_use_temp_region(script, size, remaining, nesting):
     gs.run_command("g.region", rows=size, cols=size)
     if remaining:
         nesting += 1
-        subprocess.check_call([sys.executable, script, remaining, str(nesting)])
+        call = [sys.executable, script, remaining, str(nesting)]
+        if map_name:
+            call.append(map_name)
+        subprocess.check_call(call)
+    if map_name:
+        gs.run_command("r.mapcalc", expression="{map_name}_size_{size}_nesting_{nesting} = 1".format(**locals()))
 
 
 def main():
@@ -37,12 +42,19 @@ def main():
         size = 100
         remaining = None
         nesting = 0
-    elif len(sys.argv) < 3:
+        map_name = None
+    elif len(sys.argv) not in [3, 4]:
         gs.fatal(
-            "Usage: <script name> <size[,size,...]> <nesting level>\n"
-            "<script name>      The name of this file ({})\n"
-            "<size[,size,...]>  Remaining region sizes\n"
-            "<nesting level>    Nesting level of this call".format(this_file)
+            "Usage: <script name> <size[,size,...]> <nesting level> [<map name>]\n"
+            "  <script name>      The name of this file ({name})\n"
+            "  <size[,size,...]>  Remaining region sizes\n"
+            "  <nesting level>    Nesting level of this call\n"
+            "  <map name>         Base name of maps to generate (optional)\n"
+            "Examples:\n"
+            "  {name} 100 0\n"
+            "  {name} 100 1\n"
+            "  {name} 100,200 0\n"
+            "  {name} 100,200 0 test_raster".format(name=this_file)
         )
     else:
         argument = sys.argv[1]
@@ -53,8 +65,12 @@ def main():
         else:
             remaining = None
         nesting = int(sys.argv[2])
+        if len(sys.argv) == 4:
+            map_name = sys.argv[3]
+        else:
+            map_name = None
     call_use_temp_region(
-        script=this_file, size=size, remaining=remaining, nesting=nesting
+        script=this_file, size=size, remaining=remaining, nesting=nesting, map_name=map_name
     )
 
 
