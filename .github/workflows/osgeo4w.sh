2b20fd9da9ca6dcd763dbf1d59e87c3ecb138210
Huidae Cho <grass4u@gmail.com>
2020-06-04 16:00:37 -0400

    Define PROJ_LIB

--- .github/workflows/osgeo4w.sh
+++ .github/workflows/osgeo4w.sh
@@ -8,15 +8,7 @@
 # stop on errors
 set -e
 
-cd "$1"
-
-export PATH=/mingw64/bin:/c/OSGeo4W64/bin:/usr/bin
-
-osgeo4w_root_msys=/c/OSGeo4W64
-arch=x86_64-w64-mingw32
-bit=64
-
-cat $osgeo4w_root_msys/bin/nc-config
+test -d "$1" && cd "$1"
 
 # start
 echo "Started compilation: `date`"
@@ -24,22 +16,23 @@ echo
 
 # compile
 
+osgeo4w_path=/c/OSGeo4W64
+arch=x86_64-w64-mingw32
 grass_src=`pwd`
-tmp=`dirname $0`; grass_build_scripts=`realpath $tmp`
 
-export MINGW_CHOST=$arch
-export PATH="/mingw$bit/bin:$PATH"
+export PATH=/mingw64/bin:/c/OSGeo4W64/bin:/usr/bin
+export PROJ_LIB=/$osgeo4w_path/share/proj
 
-OSGEO4W_ROOT_MSYS=$osgeo4w_root_msys \
+OSGEO4W_ROOT_MSYS=$osgeo4w_path \
 ./configure \
---host=$MINGW_CHOST \
---with-includes=$osgeo4w_root_msys/include \
---with-libs="$osgeo4w_root_msys/lib $osgeo4w_root_msys/bin" \
+--host=$arch \
+--with-includes=$osgeo4w_path/include \
+--with-libs="$osgeo4w_path/lib $osgeo4w_path/bin" \
 --with-nls \
---with-freetype-includes=$osgeo4w_root_msys/include/freetype2 \
+--with-freetype-includes=$osgeo4w_path/include/freetype2 \
 --with-bzlib \
 --with-geos=$grass_src/mswindows/osgeo4w/geos-config \
---with-netcdf=$osgeo4w_root_msys/bin/nc-config \
+--with-netcdf=$osgeo4w_path/bin/nc-config \
 --with-gdal=$grass_src/mswindows/osgeo4w/gdal-config \
 --with-liblas=$grass_src/mswindows/osgeo4w/liblas-config \
 --with-opengl=windows
@@ -48,18 +41,18 @@ make clean default
 
 # package
 
-opt_path=$osgeo4w_root_msys/opt
+opt_path=$osgeo4w_path/opt
 grass_path=$opt_path/grass
 version=`sed -n '/^INST_DIR[ \t]*=/{s/^.*grass//; p}' include/Make/Platform.make`
 date=`date +%Y%m%d`
-grass_zip=$grass_src/grass$version-$arch-osgeo4w$bit-$date.zip
+grass_zip=$grass_src/grass$version-$arch-osgeo4w64-$date.zip
 
 test -d $grass_path && rm -rf $grass_path
 test -d $opt_path || mkdir -p $opt_path
 cp -a dist.$arch $grass_path
 rm -f $grass_path/grass$version.tmp $grass_path/etc/fontcap
 cp -a bin.$arch/grass$version.py $grass_path/etc
-cp -a `ldd dist.$arch/lib/*.dll | awk '/mingw'$bit'/{print $3}' |
+cp -a `ldd dist.$arch/lib/*.dll | awk '/mingw64/{print $3}' |
 	sort -u | grep -v 'lib\(crypto\|ssl\)'` $grass_path/lib
 
 (
@@ -89,10 +82,9 @@ sed -e 's/^\(call "%~dp0\)\(.*\)$/\1\\..\\..\\bin\2/' \
 ) > $grass_path/grass$version.bat
 unix2dos $grass_path/grass$version.bat
 
-cd $osgeo4w_root_msys/..
-osgeo4w_basename=`basename $osgeo4w_root_msys`
+cd $osgeo4w_path/..
+osgeo4w_basename=`basename $osgeo4w_path`
 zip -r $grass_zip $osgeo4w_basename -x "$osgeo4w_basename/var/*" "*/__pycache__/*"
-ls -al
 
 echo
 echo "Completed compilation: `date`"
