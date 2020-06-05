5bc58adbbff7b198f2b4d2b841c37dc2939346aa
Huidae Cho <grass4u@gmail.com>
2020-06-05 16:39:50 -0400

    Use call instead of cmd /c

--- .github/workflows/test_simple.bat
+++ .github/workflows/test_simple.bat
@@ -1,4 +1,4 @@
 @echo off
 set PATH=%PATH%;%1
 
-cmd /c grass79 --tmp-location EPSG:4326 --exec g.region res=0.1 -p
+call grass79 --tmp-location EPSG:4326 --exec g.region res=0.1 -p
--- .github/workflows/test_thorough.bat
+++ .github/workflows/test_thorough.bat
@@ -1,7 +1,7 @@
 @echo off
 set PATH=%PATH%;%1
 
-cmd /c grass79 --tmp-location XY --exec g.extension g.download.location
-cmd /c grass79 --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
+call grass79 --tmp-location XY --exec g.extension g.download.location
+call grass79 --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
 
-cmd /c grass79 --tmp-location XY --exec python3 -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
+call grass79 --tmp-location XY --exec python3 -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
