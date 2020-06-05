9e50a0c66e28ce679f0f0a9afd2210163e43f884
Huidae Cho <grass4u@gmail.com>
2020-06-05 00:35:15 -0400

    OK, runs are separate sessions

--- .github/workflows/test_simple.bat
+++ .github/workflows/test_simple.bat
@@ -1,2 +1,1 @@
-echo %PATH%
-rem grass79 --tmp-location EPSG:4326 --exec g.region res=0.1 -p
+grass79 --tmp-location EPSG:4326 --exec g.region res=0.1 -p
--- .github/workflows/test_thorough.bat
+++ .github/workflows/test_thorough.bat
@@ -1,6 +1,4 @@
-echo %PATH%
+grass79 --tmp-location XY --exec g.extension g.download.location
+grass79 --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
 
-rem grass79 --tmp-location XY --exec g.extension g.download.location
-rem grass79 --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
-
-rem grass79 --tmp-location XY --exec python3 -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
+grass79 --tmp-location XY --exec python3 -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
