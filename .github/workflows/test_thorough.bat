acf8bc9a895e26e41fe9887070cdb30291a32380
Huidae Cho <grass4u@gmail.com>
2020-06-05 16:31:23 -0400

    Back to LF; use cmd /c

--- .github/workflows/test_thorough.bat
+++ .github/workflows/test_thorough.bat
@@ -1,9 +1,7 @@
 @echo off
 set PATH=%PATH%;%1
 
-echo %USERPROFILE%
+cmd /c grass79 --tmp-location XY --exec g.extension g.download.location
+cmd /c grass79 --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
 
-grass79 --tmp-location XY --exec g.extension g.download.location
-grass79 --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
-
-grass79 --tmp-location XY --exec python3 -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
+cmd /c grass79 --tmp-location XY --exec python3 -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
