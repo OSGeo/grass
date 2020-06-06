141d117f9094b74b981199e04932e7d827b2f928
Huidae Cho <grass4u@gmail.com>
2020-06-05 21:47:54 -0400

    Just directly call grass.bat; PATH changes after each grass.bat call

--- .github/workflows/test_simple.bat
+++ .github/workflows/test_simple.bat
@@ -1,5 +1,1 @@
-@echo off
-set PATH=%PATH%;%*
-echo %PATH%
-
-call grass79 --tmp-location EPSG:4326 --exec g.region res=0.1 -p
+call %1\grass79 --tmp-location EPSG:4326 --exec g.region res=0.1 -p
--- .github/workflows/test_thorough.bat
+++ .github/workflows/test_thorough.bat
@@ -1,7 +1,3 @@
-@echo off
-set PATH=%PATH%;%1
-
-call grass79 --tmp-location XY --exec g.extension g.download.location
-call grass79 --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
-
-call grass79 --tmp-location XY --exec python3 -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
+call %1\grass79 --tmp-location XY --exec g.extension g.download.location
+call %1\grass79 --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
+call %1\grass79 --tmp-location XY --exec python3 -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
