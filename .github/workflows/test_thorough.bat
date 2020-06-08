6af11b38c4c1493d1272bcf75356943ad300f8a1
Huidae Cho <grass4u@gmail.com>
2020-06-07 23:26:46 -0400

    Success rate for test_thorough.bat 60%

--- .github/workflows/test_thorough.bat
+++ .github/workflows/test_thorough.bat
@@ -3,4 +3,4 @@ set python=C:\OSGeo4W64\bin\python3
 
 call %grass% --tmp-location XY --exec g.extension g.download.location
 call %grass% --tmp-location XY --exec g.download.location url=http://fatra.cnr.ncsu.edu/data/nc_spm_full_v2alpha2.tar.gz dbase=%USERPROFILE%
-call %grass% --tmp-location XY --exec %python% -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 50
+call %grass% --tmp-location XY --exec %python% -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 60
