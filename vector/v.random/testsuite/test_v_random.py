32b09b0deaa3761821b0cb423ec2bde381132fff
JosefPudil <pepa.pudil@seznam.cz>
2020-06-10 13:41:43 +0200

    Initial tests for v.random
    1) Checking if number of points equals 100
    2) Checking if all points are in the polygon boundary state
    3) Checking if all points are in the polygon boundary state

---
+++ vector/v.random/testsuite/test_v_random.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from grass.gunittest.case import TestCase
+from grass.gunittest.main import test
+
+output = 'test01'
+output2 = 'test02'
+npoints = 100
+state = 'boundary_state'
+zmin=10
+zmax=120
+
+class Test_v_random(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.use_temp_region()
+        cls.runModule('g.region', vector=state)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.del_temp_region()
+
+    #Checking if number of points equals 100
+    def test_num_points(self):
+        self.assertModule('v.random', output=output, npoints=npoints,
+                          overwrite=True)
+
+        topology = dict(points=npoints)
+        self.assertVectorFitsTopoInfo(vector=output, reference=topology)
+    #Checking if the map is 3D and number of points is 100
+    def test_num_points_3D(self):
+        self.assertModule('v.random', output=output, npoints=npoints,
+                          zmin=zmin, zmax=zmax,
+                          overwrite=True, flags='z')
+
+        topology = dict(points=npoints, map3d=1)
+        self.assertVectorFitsTopoInfo(vector=output, reference=topology)
+    #Checking if all points are in the polygon boundary state
+    def test_restrict(self):
+        self.assertModule('v.random', output=output, npoints=npoints,
+                          restrict=state, overwrite=True)
+        self.assertModule('v.clip', input=output, clip=state,
+                          overwrite=True, output=output2)
+        self.assertVectorInfoEqualsVectorInfo(output,output2,precision=0.01)
+
+if __name__ == '__main__':
+    test()
