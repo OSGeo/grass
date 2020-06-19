8183109f304d440727d2f6058212367a3c894432
ninsbl <stefan.blumentrath@nina.no>
2020-06-19 22:58:59 +0200

    apply grass_indent.sh

--- raster/r.neighbors/readweights.c
+++ raster/r.neighbors/readweights.c
@@ -13,15 +13,15 @@ void read_weights(const char *filename)
 
     ncb.weights = G_malloc(ncb.nsize * sizeof(DCELL *));
     for (i = 0; i < ncb.nsize; i++)
-	ncb.weights[i] = G_malloc(ncb.nsize * sizeof(DCELL));
+        ncb.weights[i] = G_malloc(ncb.nsize * sizeof(DCELL));
 
     if (!fp)
-	G_fatal_error(_("Unable to open weights file %s"), filename);
+        G_fatal_error(_("Unable to open weights file %s"), filename);
 
     for (i = 0; i < ncb.nsize; i++)
-	for (j = 0; j < ncb.nsize; j++)
-	    if (fscanf(fp, "%lf", &ncb.weights[i][j]) != 1)
-		G_fatal_error(_("Error reading weights file %s"), filename);
+        for (j = 0; j < ncb.nsize; j++)
+            if (fscanf(fp, "%lf", &ncb.weights[i][j]) != 1)
+                G_fatal_error(_("Error reading weights file %s"), filename);
 
     fclose(fp);
 }
@@ -29,38 +29,39 @@ void read_weights(const char *filename)
 double gaussian(double factor, double squared_distance)
 {
     double sigma2 = factor * factor;
-	return exp(-squared_distance/(2*sigma2))/(2*M_PI*sigma2);
+
+    return exp(-squared_distance / (2 * sigma2)) / (2 * M_PI * sigma2);
 }
 
 double exponential(double factor, double squared_distance)
 {
-	return exp(factor*sqrt(squared_distance));
+    return exp(factor * sqrt(squared_distance));
 }
 
-void compute_weights(const char * function_type, double factor)
+void compute_weights(const char *function_type, double factor)
 {
-		int i, j;
-		double (*weight)(double, double);
+    int i, j;
+    double (*weight) (double, double);
 
-        if(!strcmp(function_type, "gaussian"))
-        {
-                weight = gaussian;
-         }
-         else if (!strcmp(function_type, "exponential"))
-         {
-                weight = exponential;
+    if (!strcmp(function_type, "gaussian")) {
+        weight = gaussian;
+    }
+    else if (!strcmp(function_type, "exponential")) {
+        weight = exponential;
+    }
+
+
+    ncb.weights = G_malloc(ncb.nsize * sizeof(DCELL *));
+    for (i = 0; i < ncb.nsize; i++)
+        ncb.weights[i] = G_malloc(ncb.nsize * sizeof(DCELL));
+
+    for (i = 0; i < ncb.nsize; i++) {
+        double y = i - ncb.dist;
+
+        for (j = 0; j < ncb.nsize; j++) {
+            double x = j - ncb.dist;
+
+            ncb.weights[i][j] = weight(factor, x * x + y * y);
         }
-
-
-        ncb.weights = G_malloc(ncb.nsize * sizeof(DCELL *));
-        for (i = 0; i < ncb.nsize; i++)
-		ncb.weights[i] = G_malloc(ncb.nsize * sizeof(DCELL));
-
-        for (i = 0; i < ncb.nsize; i++) {
-		double y = i - ncb.dist;
-		for (j = 0; j < ncb.nsize; j++) {
-	        double x = j - ncb.dist;
-            ncb.weights[i][j] = weight(factor, x*x+y*y);
-	    }
-	    }
+    }
 }
