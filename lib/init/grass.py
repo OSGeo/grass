dd48123cbcb8af6ce7e8305412a6959c75a9d18f
Vaclav Petras <wenzeslaus@gmail.com>
2020-07-03 14:08:18 -0400

    init: Start in last used mapset with --gui like --text
    When the grass executable is started with --text, it uses the last used mapset.
    Now also grass --gui behaves the same way and starts the main GUI without using the startup
    window first to ask about the mapset.
    
    The checks for starting in mapset are different because --text assumes it is interactive
    and can just fail and give optins to the user while --gui needs to provide a GUI way.
    As a result, --gui shows the statup window when the mapset is not usable for starting
    (both use the ask-for-permission paradigm).

--- lib/init/grass.py
+++ lib/init/grass.py
@@ -1182,6 +1182,24 @@ def load_gisrc(gisrc, gisrcrc):
     return mapset_settings
 
 
+def can_start_in_mapset(gisrc, force_lock_removal=False):
+    """Check if a mapset from a gisrc file is usable for new session"""
+    mapset_settings = MapsetSettings()
+    kv = read_gisrc(gisrc)
+    mapset_settings.gisdbase = kv.get('GISDBASE')
+    mapset_settings.location = kv.get('LOCATION_NAME')
+    mapset_settings.mapset = kv.get('MAPSET')
+    if not mapset_settings.is_valid():
+        return False
+    if not is_mapset_valid(mapset_settings.full_mapset):
+        return False
+    if not is_mapset_users(mapset_settings.full_mapset):
+        return False
+    if not force_lock_removal and is_mapset_locked(mapset_settings.full_mapset):
+        return False
+    return True
+
+
 # load environmental variables from grass_env_file
 def load_env(grass_env_file):
     if not os.access(grass_env_file, os.R_OK):
@@ -1426,6 +1444,24 @@ def set_language(grass_config_dir):
         gettext.install('grasslibs', gpath('locale'), codeset=encoding)
     else:
         gettext.install('grasslibs', gpath('locale'))
+
+
+def is_mapset_users(mapset_path):
+    """Check if the mapset belongs to the user"""
+    if os.environ.get("GRASS_SKIP_MAPSET_OWNER_CHECK", None):
+        # Mapset just needs to be accessible for writting.
+        return os.access(mapset_path, os.W_OK)
+    # Mapset needs to be owned by user.
+    stat_info = os.stat(mapset_path)
+    mapset_uid = stat_info.st_uid
+    return mapset_uid == os.getuid()
+
+
+def is_mapset_locked(mapset_path):
+    """Check if the mapset is locked"""
+    lock_name = '.gislock'
+    lockfile = os.path.join(mapset_path, lock_name)
+    return os.path.exists(lockfile)
 
 
 # TODO: grass_gui parameter is a hack and should be removed, see below
@@ -2286,9 +2322,10 @@ def main():
 
     # Parsing argument to get LOCATION
     if not params.mapset and not params.tmp_location:
+        last_mapset_usable = can_start_in_mapset(gisrc, params.force_gislock_removal)
         # Try interactive startup
         # User selects LOCATION and MAPSET if not set
-        if not set_mapset_interactive(grass_gui):
+        if not last_mapset_usable and not set_mapset_interactive(grass_gui):
             # No GUI available, update gisrc file
             fatal(_("<{0}> requested, but not available. Run GRASS in text "
                     "mode (--text) or install missing package (usually "
