2f8cf8f24c106d14beb856d19fe3f135c0b68763
nila <n_larsson@yahoo.com>
2020-07-21 15:58:12 +0200

    wxGUI: avoid flush clipboard if not opened
    Fixes #786

--- gui/wxpython/gui_core/goutput.py
+++ gui/wxpython/gui_core/goutput.py
@@ -692,7 +692,8 @@ class GStc(stc.StyledTextCtrl):
         """The clipboard contents can be preserved after
         the app has exited"""
 
-        wx.TheClipboard.Flush()
+        if wx.TheClipboard.IsOpened():
+            wx.TheClipboard.Flush()
         evt.Skip()
 
     def AddTextWrapped(self, txt, wrap=None):
