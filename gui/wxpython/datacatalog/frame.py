939739c71c1dff819a478f495f7ae780c05c736e
Anna Petrasova <kratochanna@gmail.com>
2020-07-28 23:13:52 -0400

    wxGUI/datacatalog: change ReloadTreeItems to use node-specific reloads (#847)
    The change is done in order to reload only nodes that are needed to be reloaded, e.g. when mapset is deleted, we don't need to reload entire tree including all dbs.
    
    Includes fixes in treeview and treemodel.

--- gui/wxpython/datacatalog/frame.py
+++ gui/wxpython/datacatalog/frame.py
@@ -52,7 +52,7 @@ class DataCatalogFrame(wx.Frame):
 
         # tree
         self.tree = DataCatalogTree(parent=self.panel, giface=self._giface)
-        self.tree.InitTreeItems()
+        self.tree.ReloadTreeItems()
         self.tree.UpdateCurrentDbLocationMapsetNode()
         self.tree.ExpandCurrentMapset()
         self.tree.changeMapset.connect(lambda mapset:
