6780c43fccd7031cc7fb4afea2fe5ce26a07647b
Brad ReDacted <jawayetti@gmail.com>
2020-08-30 00:13:54 -0700

    r.proj: Removed extra commented error. Added define around OpenMP code.

--- raster/r.proj/main.c
+++ raster/r.proj/main.c
@@ -253,11 +253,8 @@ int main(int argc, char **argv)
     setname = imapset->answer ? imapset->answer : G_store(G_mapset());
     if (strcmp(inlocation->answer, G_location()) == 0 &&
         (!indbase->answer || strcmp(indbase->answer, G_gisdbase()) == 0))
-#if 0
-	G_fatal_error(_("Input and output locations can not be the same"));
-#else
 	G_warning(_("Input and output locations are the same"));
-#endif
+
     G_get_window(&outcellhd);
 
     if (gprint_bounds->answer && !print_bounds->answer)
@@ -526,7 +523,9 @@ int main(int argc, char **argv)
 	/* parallelization does not always work,
 	 * segfaults in the interpolation functions 
 	 * can happen */
+#if defined(_OPENMP)
         #pragma omp parallel for schedule (static)
+#endif
 #endif
 
 	for (col = 0; col < outcellhd.cols; col++) {
