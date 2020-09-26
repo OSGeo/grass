442e3688be17de2c6427a13d0705fe91e523c745
Denis Ovsienko <denis@ovsienko.info>
2020-09-26 19:51:07 +0100

    r.geomorphon: Fix a typo in a comment. [skip ci]

--- raster/r.geomorphon/main.c
+++ raster/r.geomorphon/main.c
@@ -11,7 +11,7 @@
 *		This technology is currently capable of "experimental" stage.
 *
 * COPYRIGHT:	(C) 2002,2012 by the GRASS Development Team
-*		(C) Scientific idea of geomotrphon copyrighted to authors.
+*		(C) Scientific idea of geomorphon copyrighted to authors.
 *
 *		This program is free software under the GNU General Public
 *		License (>=v2). Read the file COPYING that comes with GRASS
