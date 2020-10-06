58be0f94f145346303a040b82a426ba8580ba9ac
Markus Metz <33666869+metzm@users.noreply.github.com>
2020-10-06 21:08:47 +0200

    libproj: fix crs modification with PROJ6+ (#1007)
    fix crs modification with PROJ6+ in lib/proj
    fix usage of over switch in g.region and  [r|v].proj

--- raster/r.proj/main.c
+++ raster/r.proj/main.c
@@ -317,9 +317,12 @@ int main(int argc, char **argv)
 	G_fatal_error(_("Unable to get projection info of input map"));
 
     /* apparently the +over switch must be set in the input projection,
-     * not the output latlon projection */
+     * not the output latlon projection
+     * TODO: for PROJ 6+, the +over switch must be added to the
+     * transformation pipeline if authority:name or WKt are used as
+     * crs definition */
     if (curr_proj == PROJECTION_LL)
-	G_set_key_value("+over", "defined", in_proj_info);
+	G_set_key_value("over", "defined", in_proj_info);
 
     if ((in_unit_info = G_get_projunits()) == NULL)
 	G_fatal_error(_("Unable to get projection units of input map"));
