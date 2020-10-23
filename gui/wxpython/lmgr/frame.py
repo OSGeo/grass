5ceecf208051cdfc93041041e5166c52ef0d052c
Anna Petrasova <kratochanna@gmail.com>
2020-10-22 23:28:22 -0400

    catalog: first load and display tree with locations for responsiveness, then load everything

--- gui/wxpython/lmgr/frame.py
+++ gui/wxpython/lmgr/frame.py
@@ -413,6 +413,7 @@ class GMFrame(wx.Frame):
             FN.EVT_FLATNOTEBOOK_PAGE_CLOSING,
             self.OnCBPageClosing)
 
+        wx.CallAfter(self.datacatalog.LoadItems)
         return self.notebook
 
     def AddNvizTools(self, firstTime):
@@ -649,8 +650,6 @@ class GMFrame(wx.Frame):
         page = event.GetSelection()
         if page == self.notebook.GetPageIndexByName('output'):
             wx.CallAfter(self.goutput.ResetFocus)
-        elif page == self.notebook.GetPageIndexByName('catalog'):
-            wx.CallAfter(self.datacatalog.LoadItems)
         self.SetStatusText('', 0)
 
         event.Skip()
