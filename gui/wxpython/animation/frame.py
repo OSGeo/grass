89f015f1f6de3c27d671f8a83e62d2d26fbb7432
nila <n_larsson@yahoo.com>
2020-11-30 10:08:28 +0100

    wxGUI: drop show preferences dialog as modal

--- gui/wxpython/animation/frame.py
+++ gui/wxpython/animation/frame.py
@@ -314,7 +314,7 @@ class AnimationFrame(wx.Frame):
                 lambda: self.controller.UpdateAnimations())
             dlg.CenterOnParent()
 
-        self.dialogs['preferences'].ShowModal()
+        self.dialogs['preferences'].Show()
 
     def OnHelp(self, event):
         RunCommand('g.manual',
