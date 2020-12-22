d794f02e2ce1ec37245cee294a809b41f483ebe0
Tomas Zigo <50632337+tmszi@users.noreply.github.com>
2020-12-22 17:38:51 +0100

    wxGUI/tplot: fix export csv file (#1206)

--- gui/wxpython/tplot/frame.py
+++ gui/wxpython/tplot/frame.py
@@ -796,6 +796,7 @@ class TplotFrame(wx.Frame):
 
     def OnRedraw(self, event=None):
         """Required redrawing."""
+        self.init()
         self.csvpath = self.csvButton.GetValue()
         self.header = self.headerCheck.IsChecked()
         if (os.path.exists(self.csvpath) and not self.overwrite):
@@ -809,7 +810,6 @@ class TplotFrame(wx.Frame):
                        message=_("Please change name of output CSV file or "))
                 return
             dlg.Destroy()
-        self.init()
         datasetsR = self.datasetSelectR.GetValue().strip()
         datasetsV = self.datasetSelectV.GetValue().strip()
 
