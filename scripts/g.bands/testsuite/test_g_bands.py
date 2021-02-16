d851bdeeb3f1849acef09e8e8b616d11bb0725d6
Vaclav Petras <wenzeslaus@gmail.com>
2021-02-15 22:36:01 -0500

    Apply Black also to the tests

--- scripts/g.bands/testsuite/test_g_bands.py
+++ scripts/g.bands/testsuite/test_g_bands.py
@@ -4,10 +4,11 @@ from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
 from grass.gunittest.gmodules import call_module
 
+
 class TestBandsSystemDefined(TestCase):
     @staticmethod
     def _number_of_bands(**kwargs):
-        gbands = call_module('g.bands', **kwargs)
+        gbands = call_module("g.bands", **kwargs)
         return len(gbands.rstrip(os.linesep).split(os.linesep))
 
     def test_number_system_defined(self):
@@ -18,6 +19,7 @@ class TestBandsSystemDefined(TestCase):
 
         # get number of valid band identifiers by Bands lib
         from grass.bandref import BandReferenceReader
+
         nbands_ref = len(BandReferenceReader().get_bands())
 
         self.assertEqual(nbands, nbands_ref)
@@ -25,18 +27,18 @@ class TestBandsSystemDefined(TestCase):
     def test_number_s2(self):
         # test number of S2 band identifiers (hardcoded, no changes expected)
         #
-        nbands = self._number_of_bands(band='S2')
+        nbands = self._number_of_bands(band="S2")
 
         self.assertEqual(nbands, 13)
-
 
     def test_number_s2_1(self):
         # test if S2_1 is defined (lower + upper case)
         #
-        band = 'S2_1'
+        band = "S2_1"
         for iband in [band, band.lower()]:
             nbands = self._number_of_bands(band=iband)
             self.assertEqual(nbands, 1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test()
--- scripts/i.band/testsuite/test_i_band.py
+++ scripts/i.band/testsuite/test_i_band.py
@@ -4,6 +4,7 @@ from grass.gunittest.gmodules import SimpleModule, call_module
 
 from grass.pygrass.raster import RasterRow
 
+
 class TestBandsSystemDefined(TestCase):
     # note that full NC dataset is needed
     raster_map = "lsat7_2002_10"
@@ -17,28 +18,28 @@ class TestBandsSystemDefined(TestCase):
 
     def test_band_ref_assign_not_current_mapset(self):
         # it is assumed that we are not in PERMANENT mapset
-        module = SimpleModule('i.band', map=self.raster_map + '@PERMANENT',
-                              band=self.band_ref)
+        module = SimpleModule(
+            "i.band", map=self.raster_map + "@PERMANENT", band=self.band_ref
+        )
         self.assertModuleFail(module)
 
     def test_band_ref_assign(self):
         # copy raster map to the current mapset
-        call_module('g.copy',
-                    raster='{m}@PERMANENT,{m}'.format(m=self.raster_map))
+        call_module("g.copy", raster="{m}@PERMANENT,{m}".format(m=self.raster_map))
 
-        module = SimpleModule('i.band', map=self.raster_map,
-                              band=self.band_ref)
+        module = SimpleModule("i.band", map=self.raster_map, band=self.band_ref)
         self.assertModule(module)
 
         # check also using pygrass
         self.assertEqual(self.read_band_ref(), self.band_ref)
 
     def test_band_ref_dissociate(self):
-        module = SimpleModule('i.band', flags='r', map=self.raster_map)
+        module = SimpleModule("i.band", flags="r", map=self.raster_map)
         self.assertModule(module)
 
         # check also using pygrass
         self.assertEqual(self.read_band_ref(), None)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     test()
