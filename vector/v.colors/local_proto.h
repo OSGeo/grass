b1432e242c60bd681574f26611b9a8ad905b5f6a
Huidae Cho <grass4u@gmail.com>
2021-02-23 06:01:11 -0500

    v.colors: raster= with use=attr (#1393)
    * v.colors: raster= with use=attr
    
    * Remove trailing whitespaces

--- vector/v.colors/local_proto.h
+++ vector/v.colors/local_proto.h
@@ -9,8 +9,10 @@ void load_colors(struct Colors *, const char *, DCELL, DCELL, int);
 
 /* scan_attr.c */
 int scan_attr(const struct Map_info *, int, const char *, const char *,
-	      const char *, const struct FPRange *, struct Colors *);
-void color_rules_to_cats(dbCatValArray *, int, struct Colors *, struct Colors *);  
+	      const char *, const struct FPRange *, struct Colors *,
+	      struct Colors *);
+void color_rules_to_cats(dbCatValArray *, int, struct Colors *,
+			 struct Colors *);
 
 /* scan_cats.c */
 void scan_cats(const struct Map_info *, int, const char *, const char *,
@@ -18,11 +20,10 @@ void scan_cats(const struct Map_info *, int, const char *, const char *,
 
 /* scan_z.c */
 void scan_z(struct Map_info *, int, const char *, const char *,
-            const struct FPRange *, struct Colors *);
+	    const struct FPRange *, struct Colors *);
 
 /* write_rgb.c */
 void write_rgb_values(const struct Map_info *, int, const char *,
 		      struct Colors *);
 /* read_rgb.c */
-void rgb2colr(const struct Map_info *, int, const char *,
-	      struct Colors *);
+void rgb2colr(const struct Map_info *, int, const char *, struct Colors *);
--- vector/v.colors/scan_z.c
+++ vector/v.colors/scan_z.c
@@ -13,21 +13,21 @@ void scan_z(struct Map_info *Map, int field,
     double zmin = 0, zmax = 0;
     struct line_pnts *Points;
     struct line_cats *Cats;
-    
+
     struct Colors vcolors;
-    
+
     dbCatValArray cvarr;
 
     Points = Vect_new_line_struct();
     Cats = Vect_new_cats_struct();
-    
+
     items_alloc = 0;
     db_CatValArray_init(&cvarr);
     cvarr.ctype = DB_C_TYPE_DOUBLE;
 
     Vect_set_constraint_field(Map, field);
     Vect_set_constraint_type(Map, GV_POINTS); /* points, centroids or kernels only) */
-        
+
     G_message(_("Reading features..."));
     line = i = found = 0;
     while(TRUE) {
@@ -38,10 +38,10 @@ void scan_z(struct Map_info *Map, int field,
 	    break; /* EOF */
 
 	G_progress(++line, 1e4);
-        
+
         if (Vect_cat_get(Cats, field, &cat) == -1)
             continue; /* skip features without category */
-        
+
         /* add item into cat-value array */
         if (i >= items_alloc) {
             items_alloc += 1000;
@@ -58,17 +58,17 @@ void scan_z(struct Map_info *Map, int field,
 	found = 1;
     }
     G_progress(1, 1);
-    
+
     /* sort array by z-coordinate */
     db_CatValArray_sort_by_value(&cvarr);
-    
+
     if (range) {
 	if (!found || (range->min >= zmin && range->min <= zmax))
 	    zmin = range->min;
 	else
 	    G_warning(_("Min value (%f) is out of range %f,%f"),
 		      range->min, zmin, zmax);
-	
+
 	if (!found || (range->max <= zmax && range->max >= zmin))
 	    zmax = range->max;
 	else
