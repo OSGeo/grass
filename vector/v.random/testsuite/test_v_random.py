e7429eb5fb6a48275bc419f4c9901f04efd5bf7d
Sunveer Singh <33540878+SunveerSingh@users.noreply.github.com>
2021-03-22 23:51:04 +0530

    Update test_v_random.py

--- vector/v.random/testsuite/test_v_random.py
+++ vector/v.random/testsuite/test_v_random.py
@@ -3,53 +3,60 @@
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
 
-output = 'test01'
-output2 = 'test02'
-npoints = 100
-state = 'boundary_state'
-zmin=10
-zmax=120
 
-class Test_v_random(TestCase):
+class TestVRandom(TestCase):
+    output = "test01"
+    output2 = "test02"
+    npoints = 100
+    state = "nc_state"
+    zmin = 10
+    zmax = 120
 
     @classmethod
     def setUpClass(cls):
         cls.use_temp_region()
-        cls.runModule('g.region', vector=state)
+        cls.runModule("g.region", vector=cls.state)
 
     @classmethod
     def tearDownClass(cls):
         cls.del_temp_region()
 
     def tearDown(cls):
-        cls.runModule('g.remove', type='vector', flags='f', name=cls.output)
-        cls.runModule('g.remove', type='vector', flags='f', name=cls.output2)
+        cls.runModule("g.remove", type="vector", flags="f", name=cls.output)
+        cls.runModule("g.remove", type="vector", flags="f", name=cls.output2)
 
-
-
-    #Checking if number of points equals 100
+    # Checking if number of points equals 100
     def test_num_points(self):
-        self.assertModule('v.random', output=output, npoints=npoints)
+        self.assertModule("v.random", output=self.output, npoints=self.npoints)
 
-        topology = dict(points=npoints)
-        self.assertVectorFitsTopoInfo(vector=output, reference=topology)
+        topology = dict(points=self.npoints)
+        self.assertVectorFitsTopoInfo(vector=self.output, reference=topology)
 
-    #Checking if the map is 3D and number of points is 100
+    # Checking if the map is 3D and number of points is 100
     def test_num_points_3D(self):
-        self.assertModule('v.random', output=output, npoints=npoints,
-                          zmin=zmin, zmax=zmax, flags='z')
+        self.assertModule(
+            "v.random",
+            output=self.output,
+            npoints=self.npoints,
+            zmin=self.zmin,
+            zmax=self.zmax,
+            flags="z",
+        )
 
-        topology = dict(points=npoints, map3d=1)
-        self.assertVectorFitsTopoInfo(vector=output, reference=topology)
+        topology = dict(points=self.npoints, map3d=1)
+        self.assertVectorFitsTopoInfo(vector=self.output, reference=topology)
 
-    #Checking if all points are in the polygon boundary state
+    # Checking if all points are in the polygon boundary state
     def test_restrict(self):
-        self.assertModule('v.random', output=output, npoints=npoints,
-                          restrict=state)
-        self.assertModule('v.clip', input=output, clip=state, output=output2)
+        self.assertModule(
+            "v.random", output=self.output, npoints=self.npoints, restrict=self.state
+        )
+        self.assertModule(
+            "v.clip", input=self.output, clip=self.state, output=self.output2
+        )
 
-        self.assertVectorInfoEqualsVectorInfo(output,output2,precision=0.01)
+        self.assertVectorInfoEqualsVectorInfo(self.output, self.output2, precision=0.01)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test()
