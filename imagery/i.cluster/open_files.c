f60d79577bb5a068b500f8597da0956568560aa9
Māris Nartišs <maris.gis@gmail.com>
2021-04-16 12:56:57 +0300

    i.cluster: introduce band references into new and old signature files

--- imagery/i.cluster/open_files.c
+++ imagery/i.cluster/open_files.c
@@ -7,7 +7,7 @@
 
 int open_files(void)
 {
-    char *name, *mapset;
+    char *name, *mapset, **err, *bandref;
     FILE *fd;
     int n, missing;
 
@@ -15,6 +15,7 @@ int open_files(void)
     I_free_group_ref(&ref);
     I_get_subgroup_ref(group, subgroup, &ref);
 
+    bandrefs = (char **)G_malloc(ref.nfiles * sizeof(char **));
     missing = 0;
     for (n = 0; n < ref.nfiles; n++) {
 	name = ref.file[n].name;
@@ -23,7 +24,12 @@ int open_files(void)
 	    missing = 1;
 	    G_warning(_("Raster map <%s> do not exists in subgroup <%s>"),
 		      G_fully_qualified_name(name, mapset), subgroup);
-	}
+        }
+        bandref = Rast_read_bandref(ref.file[n].name, ref.file[n].mapset);
+        if (!bandref)
+            G_fatal_error(_("Raster map <%s@%s> lacks band reference"),
+                            ref.file[n].name, ref.file[n].mapset);
+        bandrefs[n] = G_store(bandref);
     }
     if (missing)
 	G_fatal_error(_("No raster maps found"));
@@ -47,9 +53,9 @@ int open_files(void)
 	cellfd[n] = Rast_open_old(name, mapset);
     }
 
-    I_init_signatures(&in_sig, ref.nfiles);
     if (insigfile) {
-	fd = I_fopen_signature_file_old(group, subgroup, insigfile);
+    I_init_signatures(&in_sig, 0);
+	fd = I_fopen_signature_file_old(insigfile);
 	if (fd == NULL)
 	    G_fatal_error(_("Unable to open seed signature file <%s>"),
 			  insigfile);
@@ -63,6 +69,15 @@ int open_files(void)
 	if (in_sig.nsigs > 255)
 	    G_fatal_error(_("<%s> has too many signatures (limit is 255)"),
 			  insigfile);
+
+        err = I_sort_signatures_by_bandref(&in_sig, &ref);
+        if (err)
+            G_fatal_error(_("Signature – group member band reference mismatch.\n"
+                "Extra signatures for bands: %s\n"
+                "Imagery group bands without signatures: %s"),
+                err[0] ? err[0] : _("none"),
+                err[1] ? err[1] : _("none")
+            );
 
 	maxclass = in_sig.nsigs;
     }
