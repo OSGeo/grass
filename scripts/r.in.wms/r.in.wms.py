2c071cc5907508aa096541cd1f9348bc04fbd14e
Markus <neteler@gmail.com>
2021-04-29 15:17:49 +0200

    split on comma

--- scripts/r.in.wms/r.in.wms.py
+++ scripts/r.in.wms/r.in.wms.py
@@ -6,7 +6,7 @@ AUTHOR(S): Stepan Turek <stepan.turek AT seznam.cz>
 
 PURPOSE:   Downloads and imports data from WMS/WMTS/NASA OnEarth server.
 
-COPYRIGHT: (C) 2012 Stepan Turek, and by the GRASS Development Team
+COPYRIGHT: (C) 2012-2021 Stepan Turek, and by the GRASS Development Team
 
 This program is free software under the GNU General Public License
 (>=v2). Read the file COPYING that comes with GRASS for details.
@@ -130,6 +130,16 @@ This program is free software under the GNU General Public License
 # % required: no
 # %end
 
+#%option
+#% key: gdal_createopt
+#% type: string
+#% required: no
+#% multiple: yes
+#% label: GDAL creation option(s) to pass to the output format driver
+#% description: In the form of "NAME=VALUE", separate multiple entries with a comma
+#% guisection: Request
+#%end
+
 # %option
 # % key: region
 # % type: string
@@ -236,7 +246,11 @@ def main():
         grass.debug("Using GDAL WMS driver")
         from wms_gdal_drv import WMSGdalDrv
 
-        wms = WMSGdalDrv()
+        if options["gdal_createopt"]:
+            create_options = options["gdal_createopt"].split(",")
+        else:
+            create_options = None
+        wms = WMSGdalDrv(create_options)
 
     if flags["c"]:
         wms.GetCapabilities(options)
