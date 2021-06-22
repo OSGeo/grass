56e17c5c5c54a23e28bef72d808b4bf1022d98fb
chaedri <caitlin.haedrich@gmail.com>
2021-06-22 19:41:58 -0400

    Edits suggested in PR

--- python/grass/jupyter/display.py
+++ python/grass/jupyter/display.py
@@ -13,36 +13,29 @@
 
 import os
 import grass.script as gs
+from IPython.display import Image
 
 
 class GrassRenderer:
-    """
-    The grassRenderer class creates and displays GRASS maps in Jupyter
-    Notebooks.
-    """
+    """The grassRenderer class creates and displays GRASS maps in 
+    Jupyter Notebooks."""
 
     def __init__(self, width=600, height=400, text_size=12):
         """Initiates an instance of the GrassRenderer class."""
-        self.height = height
-        self.width = width
         os.environ["GRASS_RENDER_WIDTH"] = str(width)
         os.environ["GRASS_RENDER_HEIGHT"] = str(height)
         os.environ["GRASS_TEXT_SIZE"] = str(text_size)
         gs.run_command("d.erase")
 
-    def d_rast(self, raster, color="greys"):
+    def d_rast(self, raster, **kwargs):
         """Adds a raster to the display"""
-        gs.run_command("r.colors", map=raster, color=color)
-        gs.run_command("d.rast", map=raster)
+        # gs.run_command("r.colors", map=raster, color=color)
+        gs.run_command("d.rast", map=raster, **kwargs)
 
-    def d_vect(self, vector, fill="grey", color="black", width=2):
+    def d_vect(self, vector, **kwargs):
         """Adds a vector to the display"""
-        gs.run_command(
-            "d.vect", map=vector, fill_color=fill, color=color, width=str(width)
-        )
+        gs.run_command("d.vect", map=vector, **kwargs)
 
     def show(self):
-        """Displays a png image of the map (non-interactive)"""
-        from IPython.display import Image
-
+        """Displays a PNG image of the map (non-interactive)"""
         return Image("map.png")
