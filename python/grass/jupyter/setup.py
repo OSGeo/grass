008c2a128b54dc829779ca34599698c696581b90
chaedri <caitlin.haedrich@gmail.com>
2021-06-30 16:05:01 -0400

    Deleted display_settings function

--- python/grass/jupyter/setup.py
+++ python/grass/jupyter/setup.py
@@ -21,7 +21,6 @@ def _set_notebook_defaults():
     """
     This function sets several GRASS environment variables that are
     important for GRASS to run smoothly in Jupyter.
-
     It also allows GRASS to overwrite existing maps of the same name.
     """
     # We want functions to raise exceptions and see standard output of
@@ -37,7 +36,6 @@ def init(path, location, mapset):
     """
     This function initiates a GRASS session and sets GRASS
     environment variables.
-
     Inputs:
         path - path to grass databases
         location - name of GRASS location
@@ -47,28 +45,3 @@ def init(path, location, mapset):
     gsetup.init(os.environ["GISBASE"], path, location, mapset)
     # Set GRASS env. variables
     _set_notebook_defaults()
-
-
-def display_settings(font="sans", driver="cairo"):
-    """
-    This function sets the display settings for a GRASS session
-    in Jupyter Notebooks.
-
-    Example Usage: display_settings(font="sans", driver="cairo")
-
-    Inputs:
-        font - specifies the font as either the name of a font from
-        $GISBASE/etc/fontcap (or alternative fontcap file specified by
-        GRASS_FONT_CAP), or alternatively the full path to a FreeType
-        font file.
-
-        driver - tell teh display library which driver to use
-            Possible values: "cairo", "png", "ps", "html"
-    """
-    # Set display font
-    os.environ["GRASS_FONT"] = font
-
-    # Set display modeules to render to a file (named map.png by
-    # default).
-    os.environ["GRASS_RENDER_IMMEDIATE"] = driver
-    os.environ["GRASS_RENDER_FILE_READ"] = "TRUE"
