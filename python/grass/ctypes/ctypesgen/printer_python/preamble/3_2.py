1dd3d9dfb73f9c1b44985fd7efe79ce7d88ad67a
nilason <n_larsson@yahoo.com>
2021-06-30 05:36:38 +0200

    patch for ctypesgen POINTER

--- python/grass/ctypes/ctypesgen/printer_python/preamble/3_2.py
+++ python/grass/ctypes/ctypesgen/printer_python/preamble/3_2.py
@@ -13,6 +13,20 @@ for t in _int_types:
 del t
 del _int_types
 
+def POINTER(obj):
+    p = ctypes.POINTER(obj)
+
+    # Convert None to a real NULL pointer to work around bugs
+    # in how ctypes handles None on 64-bit platforms
+    if not isinstance(p.from_param, classmethod):
+        def from_param(cls, x):
+            if x is None:
+                return cls()
+            else:
+                return x
+        p.from_param = classmethod(from_param)
+
+    return p
 
 class UserString:
     def __init__(self, seq):
