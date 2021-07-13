0f8351f643334115106d61096bf4fd1860623015
aaronsms <aaronsms@u.nus.edu>
2021-07-13 23:26:55 +0800

    Refactor input to use pointers

--- raster/r.neighbors/local_proto.h
+++ raster/r.neighbors/local_proto.h
@@ -1,15 +1,17 @@
+#include <grass/gis.h>
+
 /* bufs.c */
 extern int allocate_bufs(void);
-extern int rotate_bufs(void);
+extern int rotate_bufs(int);
 
 /* gather */
 extern void circle_mask(void);
 extern void weights_mask(void);
-extern int gather(DCELL *, int);
-extern int gather_w(DCELL *, DCELL(*)[2], int);
+extern int gather(DCELL *, int, int);
+extern int gather_w(DCELL *, DCELL(*)[2], int, int);
 
 /* readcell.c */
-extern int readcell(int, int, int, int);
+extern int readcell(int, int, int, int, int);
 
 /* divr_cats.c */
 extern int divr_cats(void);
@@ -25,3 +27,7 @@ extern void read_weights(const char *);
 extern double gaussian(double, double);
 extern double exponential(double, double);
 extern void compute_weights(const char *, double);
+
+/* parallel.c */
+extern int segment_open(void);
+extern void segment_close(void);
