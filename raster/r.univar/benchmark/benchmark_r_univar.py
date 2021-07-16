950298d015c91cb0ab9a53ba849c77c1c020bc06
aaronsms <aaronsms@u.nus.edu>
2021-07-16 14:52:24 +0800

    Add benchmark script

--- raster/r.univar/benchmark/benchmark_r_univar.py
+++ raster/r.univar/benchmark/benchmark_r_univar.py
@@ -5,54 +5,41 @@
 
 from grass.pygrass.modules import Module
 from subprocess import DEVNULL
-import os
 
-ITERATION = 5
-UP_TO_THREADS = 12
-
-
-def benchmark(module):
-    term_size = os.get_terminal_size()
-    print(module.get_bash())
-
-    min_ave = float("inf")
-    min_t = 1
-    for t in range(1, UP_TO_THREADS + 1):
-        print("\u2500" * term_size.columns)
-        print(f"Benchmark with {t} thread(s)...\n")
-        sum = 0
-        for _ in range(ITERATION):
-            module(nprocs=t).run()
-            print(f"{module.time}s")
-            sum += module.time
-
-        ave = sum / ITERATION
-        if t == 1:
-            serial_ave = ave
-        if ave < min_ave:
-            min_ave = ave
-            min_t = t
-        print(f"\nResult - {ave}s")
-
-    print("\u2500" * term_size.columns)
-    print(f"\nSerial average time - {serial_ave}s")
-    print(f"Best average time - {min_ave}s ({min_t} threads)\n")
+import grass.benchmark as bm
 
 
 def main():
-    multiple_maps_parallel = Module(
-        "r.univar", map=["elevation"] * 10, flags="g", stdout_=DEVNULL
-    )
-    multiple_maps_with_zone_parallel = Module(
-        "r.univar",
-        map=["elevation"] * 10,
-        zones="basin_50K",
-        flags="g",
-        stdout_=DEVNULL,
-    )
+    results = []
 
-    benchmark(multiple_maps_parallel)
-    benchmark(multiple_maps_with_zone_parallel)
+    benchmark(7071, "r.univar_50M", results)
+    benchmark(10000, "r.univar_100M", results)
+    benchmark(14142, "r.univar_200M", results)
+    benchmark(20000, "r.univar_400M", results)
+
+    bm.nprocs_plot(results)
+
+
+def benchmark(length, label, results):
+    fractal = "benchmark_fractal"
+    output = "benchmark_r_univar_nprocs"
+
+    generate_fractal(rows=length, cols=length, fname=fractal)
+    module = Module(
+        "r.univar",
+        map=[fractal] * 5,
+        run_=False,
+        stdout_=DEVNULL,
+        overwrite=True,
+    )
+    results.append(bm.benchmark_nprocs(module, label=label, max_nprocs=16, repeat=3))
+    Module("g.remove", quiet=True, flags="f", type="raster", name=fractal)
+    Module("g.remove", quiet=True, flags="f", type="raster", name=output)
+
+
+def generate_fractal(rows, cols, fname):
+    Module("g.region", flags="p", s=0, n=rows, w=0, e=cols, res=1)
+    Module("r.surf.fractal", output=fname)
 
 
 if __name__ == "__main__":
