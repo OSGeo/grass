3130a47d7c15080ddfb03ae706e7880ebd8153d6
Vaclav Petras <wenzeslaus@gmail.com>
2021-07-24 22:48:09 -0400

    libpython: Add plot nprocs to benchmark CLI (#1761)
    * Adds new subcommand plot nprocs.
    * Uses more general plot title.
    * Allows user to select results by label.
    * Prefixes can replace labels instead of prefixing them.
    * Adds test for plot nprocs (uses nprocs with r.univar, so it is skipped in 8.0).

--- python/grass/benchmark/testsuite/test_benchmark_cli.py
+++ python/grass/benchmark/testsuite/test_benchmark_cli.py
@@ -12,26 +12,68 @@
 
 """Tests of grass.benchmark CLI"""
 
+import sys
 from pathlib import Path
 from subprocess import DEVNULL
 
-from grass.benchmark import benchmark_resolutions, save_results_to_file
+import grass
+from grass.benchmark import (
+    benchmark_nprocs,
+    benchmark_resolutions,
+    save_results_to_file,
+)
 from grass.benchmark.app import main as benchmark_main
 from grass.gunittest.case import TestCase
 from grass.gunittest.main import test
 from grass.pygrass.modules import Module
 
 
+def remove_file(path):
+    """Remove filename if exists"""
+    if sys.version_info < (3, 8):
+        try:
+            Path(path).unlink()
+        except FileNotFoundError:
+            pass
+    else:
+        Path(path).unlink(missing_ok=True)
+
+
 class TestBenchmarkCLI(TestCase):
     """Tests that benchmarkin CLI works"""
 
-    def test_plot_workflow(self):
-        """Test that plot workflow runs"""
+    json_filename = "plot_test.json"
+    png_filename1 = "plot_test1.png"
+    png_filename2 = "plot_test2.png"
+
+    def tearDown(self):
+        """Remove test files"""
+        remove_file(self.json_filename)
+        remove_file(self.png_filename1)
+        remove_file(self.png_filename2)
+
+    def test_plot_nprocs_workflow(self):
+        """Test that plot nprocs workflow runs"""
         label = "Standard output"
         repeat = 4
-        json_filename = "plot_test.json"
-        png_filename = "plot_test.png"
-        png_filename_resolutions = "plot_test_resolutions.png"
+        # The benchmark part has only Python API, not CLI.
+        try:
+            result = benchmark_nprocs(
+                module=Module("r.univar", map="elevation", stdout_=DEVNULL, run_=False),
+                label=label,
+                repeat=repeat,
+                max_nprocs=3,
+            )
+        except grass.exceptions.ParameterError:
+            self.skipTest("r.univar without nprocs parameter")
+        save_results_to_file([result], self.json_filename)
+        benchmark_main(["plot", "nprocs", self.json_filename, self.png_filename1])
+        self.assertTrue(Path(self.png_filename1).is_file())
+
+    def test_plot_cells_workflow(self):
+        """Test that plot cells workflow runs"""
+        label = "Standard output"
+        repeat = 4
         # The benchmark part has only Python API, not CLI.
         result = benchmark_resolutions(
             module=Module("r.univar", map="elevation", stdout_=DEVNULL, run_=False),
@@ -39,13 +81,13 @@ class TestBenchmarkCLI(TestCase):
             repeat=repeat,
             resolutions=[1000, 500],
         )
-        save_results_to_file([result], json_filename)
-        benchmark_main(["plot", "cells", json_filename, png_filename])
-        self.assertTrue(Path(png_filename).is_file())
+        save_results_to_file([result], self.json_filename)
+        benchmark_main(["plot", "cells", self.json_filename, self.png_filename1])
+        self.assertTrue(Path(self.png_filename1).is_file())
         benchmark_main(
-            ["plot", "cells", "--resolutions", json_filename, png_filename_resolutions]
+            ["plot", "cells", "--resolutions", self.json_filename, self.png_filename2]
         )
-        self.assertTrue(Path(png_filename_resolutions).is_file())
+        self.assertTrue(Path(self.png_filename2).is_file())
 
 
 if __name__ == "__main__":
