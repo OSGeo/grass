0c177063c26ad3fd6588b71aca036c0e10916ee6
ninsbl <stefan.blumentrath@nina.no>
2021-07-27 23:08:20 +0200

    remove temporary printing

--- python/grass/gunittest/invoker.py
+++ python/grass/gunittest/invoker.py
@@ -198,10 +198,6 @@ class GrassTestFilesInvoker(object):
             #                command is used to control an if, elif, while, or
             #                until; or if the command is the left hand operand
             #                of an '&&' or '||' operator.
-            print("PATH", os.environ["PATH"])
-            print("sh", shutil.which("sh"))
-            print("sh.exe", shutil.which("sh.exe"))
-            print("mod_path", module.abs_file_path)
             p = subprocess.Popen(
                 ["sh", "-e", "-x", module.abs_file_path],
                 cwd=cwd,
