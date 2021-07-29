c06532e6639ad6c635f9b80658b11222e8345d3b
Alex Barreto <axbarreto@lavabit.com>
2021-07-29 09:40:29 -0400

    wxGUI: refactoring: build GUI tools' status bars based on wx.StatusBar widget (#1689)
    Builds status bars based on wx.StatusBar for SwipeMapFrame, IClassMapFrame, Image2Target Frame, Photo2Image Frame, and GCP manager MapFrame.
    Some general methods related to a status bar and toolbars moved to gui_core class MapFrameBase.

--- gui/wxpython/image2target/ii2t_mapdisplay.py
+++ gui/wxpython/image2target/ii2t_mapdisplay.py
@@ -110,6 +110,11 @@ class MapPanel(SingleMapPanel):
 
         # create statusbar and its manager
         self.statusbar = self.CreateStatusbar(statusbarItems)
+<<<<<<< HEAD
+=======
+
+        self.statusbarManager.SetMode(8)  # goto GCP
+>>>>>>> 03a790ad9a (wxGUI: refactoring: build GUI tools' status bars based on wx.StatusBar widget (#1689))
 
         #
         # Init map display (buffered DC & set default cursor)
@@ -240,13 +245,19 @@ class MapPanel(SingleMapPanel):
         # default toolbar
         if name == "map":
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 03a790ad9a (wxGUI: refactoring: build GUI tools' status bars based on wx.StatusBar widget (#1689))
             if "map" not in self.toolbars:
                 self.toolbars["map"] = MapToolbar(
                     self, self._toolSwitcher, self._giface
                 )
+<<<<<<< HEAD
 =======
             self.toolbars["map"] = MapToolbar(self, self._toolSwitcher, self._giface)
 >>>>>>> 953489b535 (wxGUI: fix layout flag assert in wms dialog (#1764))
+=======
+>>>>>>> 03a790ad9a (wxGUI: refactoring: build GUI tools' status bars based on wx.StatusBar widget (#1689))
 
             self._mgr.AddPane(
                 self.toolbars["map"],
