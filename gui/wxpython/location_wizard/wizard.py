6e466281c1eb82e0f883e96c33dc92361bc1b728
ocsmit <ocsmit@protonmail.com>
2021-08-02 16:32:23 -0400

    Set vertical size of title text based on font pixel height

--- gui/wxpython/location_wizard/wizard.py
+++ gui/wxpython/location_wizard/wizard.py
@@ -97,6 +97,8 @@ class TitledPage(WizardPageSimple):
 
     def __init__(self, parent, title):
         self.page = WizardPageSimple.__init__(self, parent)
+        font = wx.Font(13, wx.SWISS, wx.NORMAL, wx.BOLD)
+        font_height = font.GetPixelSize()[1]
 
         # page title
         self.title = StaticText(
@@ -104,9 +106,9 @@ class TitledPage(WizardPageSimple):
             id=wx.ID_ANY,
             label=title,
             style=wx.ALIGN_CENTRE_HORIZONTAL,
-            size=(-1, 30),
+            size=(-1, font_height),
         )
-        self.title.SetFont(wx.Font(13, wx.SWISS, wx.NORMAL, wx.BOLD))
+        self.title.SetFont(font)
         # main sizers
         self.pagesizer = wx.BoxSizer(wx.VERTICAL)
 
