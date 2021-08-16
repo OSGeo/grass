f1dde90606944eb9086310bc11a5ac9c9a068836
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2021-08-17 00:13:49 +0200

    wrap python in shell

--- .github/workflows/build_osgeo4w.sh
+++ .github/workflows/build_osgeo4w.sh
@@ -126,15 +126,15 @@ unix2dos $bin/grass$ver.bat
 
 if [ "$UNITTEST" ]; then
     cp $bin/grass$ver.bat $bin/grass.bat
-    # for f in $src/dist.$arch/bin/*.bat
-    # do
-    #     bat_exe=$(echo $f | sed 's/\.bat//g')
-    #     ln -s "$f" "$bat_exe"
-    #     #unix2dos "$bat_exe"
-    #     #chmod ugo+x "$bat_exe"
-    #     file "$bat_exe"
-    #     #cat "$bat_exe"
-    # done
+    for f in $src/dist.$arch/bin/*.bat
+    do
+        bat_exe=$(echo $f | sed 's/\.bat//g')
+        echo "#!/bin/bash\n/c/osgeo4w/bin/python3 ${bat_exe}.py \$@" > "$bat_exe"
+        # unix2dos "$bat_exe"
+        chmod ugo+x "$bat_exe"
+        file "$bat_exe"
+        # cat "$bat_exe"
+    done
 fi
 
 test $package -eq 0 && exit
