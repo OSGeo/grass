b752963e0d3ef33d57ce4d4f428ffb46e0585743
ninsbl <stefan.blumentrath@nina.no>
2021-08-18 22:24:03 +0200

    temporarily deactivate errors

--- .github/workflows/test_simple.sh
+++ .github/workflows/test_simple.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # fail on non-zero return code from a subprocess
-set -e
+# set -e
 
 grass --tmp-location EPSG:4326 --exec g.region res=0.1 -p
+grass --tmp-location EPSG:4326 --exec which t.create
