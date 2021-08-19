ae331762e7d1245f2d2229863af674abba8022f0
ninsbl <stefan.blumentrath@nina.no>
2021-08-19 17:42:38 +0200

    print path and skip

--- .github/workflows/test_simple.sh
+++ .github/workflows/test_simple.sh
@@ -3,6 +3,8 @@
 # fail on non-zero return code from a subprocess
 set -e
 
-grass --tmp-location EPSG:4326 --exec g.region res=0.1 -p
-grass --tmp-location EPSG:4326 --exec which t.create
-grass --tmp-location EPSG:4326 --exec t.create --help
+echo $PATH
+which grass
+#grass --tmp-location EPSG:4326 --exec g.region res=0.1 -p
+#grass --tmp-location EPSG:4326 --exec which t.create
+#grass --tmp-location EPSG:4326 --exec t.create --help
