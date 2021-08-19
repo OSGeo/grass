a21614e8d955ff28da36ff0bdf36a858794daaba
ninsbl <stefan.blumentrath@nina.no>
2021-08-19 08:46:33 +0200

    test python module

--- .github/workflows/test_simple.bat
+++ .github/workflows/test_simple.bat
@@ -1,4 +1,5 @@
 set grass=%1
 
 call %grass% --tmp-location EPSG:4326 --exec g.region res=0.1 -p
-call %grass% --tmp-location EPSG:4326 --exec where t.create
+call %grass% --tmp-location EPSG:4326 --exec C:/Windows/System32/where.exe t.create
+call %grass% --tmp-location EPSG:4326 --exec t.create --help
--- .github/workflows/test_simple.sh
+++ .github/workflows/test_simple.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # fail on non-zero return code from a subprocess
-# set -e
+set -e
 
 grass --tmp-location EPSG:4326 --exec g.region res=0.1 -p
 grass --tmp-location EPSG:4326 --exec which t.create
+grass --tmp-location EPSG:4326 --exec t.create --help
