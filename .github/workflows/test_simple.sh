c927a6a24858f7579fcb6d50377b0ff9a9ccf52e
ninsbl <stefan.blumentrath@nina.no>
2021-08-20 10:04:59 +0200

    check content of grass.py and skip

--- .github/workflows/test_simple.sh
+++ .github/workflows/test_simple.sh
@@ -5,6 +5,11 @@ set -e
 
 echo $PATH
 which grass
-grass --tmp-location EPSG:4326 --exec g.region res=0.1 -p
-grass --tmp-location EPSG:4326 --exec which t.create
-grass --tmp-location EPSG:4326 --exec t.create --help
+grass_exe=$(which grass)
+
+echo $grass_exe
+cat $grass_exe
+
+# grass --tmp-location EPSG:4326 --exec g.region res=0.1 -p
+# grass --tmp-location EPSG:4326 --exec which t.create
+# grass --tmp-location EPSG:4326 --exec t.create --help
