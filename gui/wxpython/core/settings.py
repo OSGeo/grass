ced772c7f378373399b32c78deb091a25044c898
Anna Petrasova <kratochanna@gmail.com>
2021-08-21 22:56:57 -0400

    wxGUI: fix settings when item added to settings but it doesn't exist yet locally (#1811)

--- gui/wxpython/core/settings.py
+++ gui/wxpython/core/settings.py
@@ -26,6 +26,7 @@ import sys
 import copy
 import wx
 import json
+import collections.abc
 
 from core import globalvar
 from core.gcmd import GException, GError
@@ -900,9 +901,19 @@ class Settings:
 
         :param settings: dict where to store settings (None for self.userSettings)
         """
+
+        def update_nested_dict_by_dict(dictionary, update):
+            """Recursively update nested dictionary by another nested dictionary"""
+            for key, value in update.items():
+                if isinstance(value, collections.abc.Mapping):
+                    update_nested_dict_by_dict(dictionary.get(key, {}), value)
+                else:
+                    dictionary[key] = value
+
         try:
             with open(self.filePath, "r") as f:
-                settings.update(json.load(f, object_hook=settings_JSON_decode_hook))
+                update = json.load(f, object_hook=settings_JSON_decode_hook)
+                update_nested_dict_by_dict(settings, update)
         except json.JSONDecodeError as e:
             sys.stderr.write(
                 _("Unable to read settings file <{path}>:\n{err}").format(
