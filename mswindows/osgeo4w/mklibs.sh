f823bfe7792105eeb29b2d969cccf819916c11df
ninsbl <stefan.blumentrath@nina.no>
2021-08-23 22:30:47 +0200

    apply patches from jef-n/osgeo4w

--- mswindows/osgeo4w/mklibs.sh
+++ mswindows/osgeo4w/mklibs.sh
@@ -1,30 +1,13 @@
 #!/bin/sh
 
 set -e
-
-if [ -z "$OSGEO4W_POSTFIX" ]; then
-    OSGEO4W_POSTFIX=""
-fi
-
-if [ "$OSGEO4W_POSTFIX" != "64" ]; then
-    MACHINE=x86
-else
-    MACHINE=x64
-fi
-
-PROGRAMFILES="/c/Program Files (x86)"
-VSDIR="$PROGRAMFILES/Microsoft Visual Studio 14.0"
-# add example for MS Visual Studio 2017 Community 64 bit
-# VSDIR="$PROGRAMFILES/Microsoft Visual Studio/2017/Community"
-PATH="$VSDIR/Common7/IDE:$PATH"
-PATH="$VSDIR/VC/bin:$PATH"
-# add example for MS Visual Studio 2017 Community 64 bit
-# PATH="$VSDIR/VC/Tools/MSVC/14.12.25827/bin/Hostx64/x64:$PATH"
-PATH="$VSDIR/Common7/Tools:$PATH"
-PATH="$PATH:/c/OSGeo4W${OSGEO4W_POSTFIX}/bin"
-export PATH
+set -x
 
 [ -d mswindows/osgeo4w/vc ] || mkdir mswindows/osgeo4w/vc
+
+if [ -n "$VCPATH" ]; then
+	PATH=$PATH:$VCPATH
+fi
 
 for dllfile in "$@"; do
 	dlldir=${dllfile%/*}
@@ -43,6 +26,7 @@ for dllfile in "$@"; do
 		egrep -v "^[	 ]*(_+IMPORT_DESCRIPTOR_.*|_+NULL_IMPORT_DESCRIPTOR)$" >mswindows/osgeo4w/vc/${defname%$VERSION}
 
 	(cd mswindows/osgeo4w/vc ;
-	    lib -nologo -def:${defname} -subsystem:windows -machine:${MACHINE}
+	    lib -nologo -def:${defname} -subsystem:windows -machine:x64
 	    lib -nologo $libname || exit)
 done
+
