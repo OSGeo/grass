a1157b6ef048d40cd0d756d58916271d78d3d210
ninsbl <stefan.blumentrath@nina.no>
2021-08-23 21:50:05 +0200

    test binary and python

--- .github/workflows/test_simple.sh
+++ .github/workflows/test_simple.sh
@@ -3,4 +3,19 @@
 # fail on non-zero return code from a subprocess
 set -e
 
+echo $PATH
+# Test if grass executable is found
+which grass
+grass_exe=$(which grass)
+
+which python3
+python3 -c "import sys; print(sys.executable, sys.version)"
+
+echo $grass_exe
+
+# Test execution of binary command
 grass --tmp-location EPSG:4326 --exec g.region res=0.1 -p
+# Test if python modules without extension are found
+grass --tmp-location EPSG:4326 --exec which t.create
+# Test if python modules can be called
+grass --tmp-location EPSG:4326 --exec t.create --help
