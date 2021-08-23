2bc85549c819cf999192a8cde2b0aca8239185a0
ninsbl <stefan.blumentrath@nina.no>
2021-08-23 22:31:09 +0200

    apply patches from jef-n/osgeo4w

--- lib/gis/gisinit.c
+++ lib/gis/gisinit.c
@@ -49,12 +49,14 @@ void G__gisinit(const char *version, const char *pgm)
 
     G_set_program_name(pgm);
 
+#ifndef _WIN32
     /* verify version of GRASS headers (and anything else in include) */
     if (strcmp(version, GIS_H_VERSION) != 0)
 	G_fatal_error(_("Module built against version %s but "
 			"trying to use version %s. "
 			"You need to rebuild GRASS GIS or untangle multiple installations."),
                         version, GIS_H_VERSION);
+#endif
     
     /* Make sure location and mapset are set */
     G_location_path();
