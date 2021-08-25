9190b07661972aad5174c7b30a015470621b53f5
Anna Petrasova <kratochanna@gmail.com>
2021-08-24 23:12:01 -0400

    wxGUI/vnet: fix removing notebook page, wx layout assert error, missing giface (#1822)

--- gui/wxpython/vnet/dialogs.py
+++ gui/wxpython/vnet/dialogs.py
@@ -582,9 +582,9 @@ class VNETDialog(wx.Dialog):
                 name="resultDbMgr",
             )
         elif not haveDbMgr:
-            self.notebook.RemovePage(
-                page=self.notebook.GetPageIndexByName("resultDbMgr")
-            )
+            page = self.notebook.GetPageIndexByName("resultDbMgr")
+            if page != -1:
+                self.notebook.RemovePage(page=page)
 
     def OnPageChanged(self, event):
         """Tab switched"""
