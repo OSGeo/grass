c9fc2722b292cda6f559e230d91b2a98c1427f31
Vaclav Petras <wenzeslaus@gmail.com>
2021-08-30 21:33:15 -0400

    Include dependencies and extended Python version info using GRASS_PYTHON

--- .github/workflows/print_versions.sh
+++ .github/workflows/print_versions.sh
@@ -6,5 +6,8 @@
 set -e
 
 grass --version
-grass --tmp-location XY --exec g.version
+grass --tmp-location XY --exec g.version -e
+# Detailed Python version info (in one line thanks to echo)
+grass --tmp-location XY --exec bash -c "echo Python: \$(\$GRASS_PYTHON -c 'import sys; print(sys.version)')"
+python3 --version
 python --version
