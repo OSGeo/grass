21d294c08ac1e2a7c797b4cd62595d7da762a912
Vaclav Petras <wenzeslaus@gmail.com>
2021-08-31 15:29:44 -0400

    jupyter: Return session from from init
    * Calling grass.script.setup.init needs to paired with a call to finish.
    * Explicit call to finish would have to be commented out in most notebooks with notes about nothing working once this is called, but an explanation of the need to call it at some point (confusing).
    * atexit does not or may not work in Jupyter (IPython kernel). It does not for me locally and the Internet is full of various notes about not working in certain cases or versions.
    * Returned session object uses weakref.finalize technique (used, e.g., in the Python standard library temporary directory object) to call the finish procedure.
    * The session object takes care of switching the mapset although there is nothing in the object needed to do that since the state is global anyway.
    * User needs to hold result of init, e.g., s = init(...), otherwise the session will end immediately.

--- python/grass/jupyter/setup.py
+++ python/grass/jupyter/setup.py
@@ -12,6 +12,7 @@
 #           for details.
 
 import os
+import weakref
 
 import grass.script as gs
 import grass.script.setup as gsetup
@@ -33,10 +34,85 @@ def _set_notebook_defaults():
     os.environ["GRASS_OVERWRITE"] = "1"
 
 
+class _JupyterGlobalSession:
+    """Represents a global GRASS session for Jupyter Notebooks.
+
+    Do not create objects of this class directly. Use the standalone *init* function
+    and an object will be returned to you, e.g.:
+
+    >>> import grass.jupyter as gj
+    >>> session = gj.init(...)
+
+    An object ends the session when it is destroyed or when the *finish* method is
+    called explicitely.
+
+    Notably, only the mapset is closed, but the libraries and GRASS modules
+    remain on path.
+    """
+
+    def __init__(self):
+        self._finalizer = weakref.finalize(self, gsetup.finish)
+
+    def switch_mapset(self, arg):
+        """Switch to a mapset provided as a name or path.
+
+        The *arg* positional-only parameter can be either name of a mapset in the
+        current location or a full path to a mapset.
+
+        Raises ValueError if the mapset does not exist or CalledModuleError if
+        call to the underlying the g.mapset module fails (e.g., when mapset is
+        invalid).
+        """
+        # The method could be a function, but this is more general (would work even for
+        # a non-global session) and users need to keep the reference anyway.
+        # pylint: disable=no-self-use
+        # Functions needed only here.
+        # pylint: disable=import-outside-toplevel
+        from grass.grassdb.checks import mapset_exists
+        from grass.grassdb.manage import split_mapset_path
+
+        if mapset_exists(arg):
+            path, location, mapset = split_mapset_path(arg)
+            # TODO: Requires direct session file modification.
+            # g.mapset locks the mapset which is not how init and finish behave.
+            gs.run_command("g.mapset", dbase=path, location=location, mapset=mapset)
+            return
+        gisenv = gs.gisenv()
+        if mapset_exists(
+            path=gisenv["GISDBASE"], location=gisenv["LOCATION_NAME"], mapset=arg
+        ):
+            gs.run_command("g.mapset", mapset=arg)
+            return
+        raise ValueError(_("Mapset '{}' does not exist").format(arg))
+
+    def finish(self):
+        """Close the session, i.e., close the opened mapset.
+
+        Subsequent calls to GRASS GIS modules will fail because there will be
+        no current (opened) mapset anymore.
+
+        The finish procedure is done automatically when process finishes or the object
+        is destroyed.
+        """
+        self._finalizer()
+
+
 def init(path, location=None, mapset=None, grass_path=None):
     """
     This function initiates a GRASS session and sets GRASS
     environment variables.
+
+    Calling this function returns an object which represents the session.
+
+    >>> import grass.jupyter as gj
+    >>> session = gj.init(...)
+
+    When the object is destroyed, the session is ended. Therefore, it is necessary
+    to keep a reference to the object as long as the session should remain active.
+    In a notebook, this is normally achieved by simply assigning the function return
+    value to a variable. If you see ``GISRC - variable not set`` after calling
+    a GRASS module, you know you have forgot to assign the result to a variable.
+
 
     :param str path: path to grass databases
     :param str location: name of GRASS location
@@ -46,3 +122,4 @@ def init(path, location=None, mapset=None, grass_path=None):
     gsetup.init(path, location=location, mapset=mapset, grass_path=grass_path)
     # Set GRASS env. variables
     _set_notebook_defaults()
+    return _JupyterGlobalSession()
