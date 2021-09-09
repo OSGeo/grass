699a4882a06adc3df27e56a83d826aad4250ba86
Martin Landa <landa.martin@gmail.com>
2021-09-09 09:54:24 +0200

    message improvements
    Co-authored-by: Markus Metz <33666869+metzm@users.noreply.github.com>

--- python/grass/temporal/open_stds.py
+++ python/grass/temporal/open_stds.py
@@ -58,7 +58,7 @@ def open_old_stds(name, type, dbif=None):
             if band_ref not in BandReferenceReader().get_bands():
                 msgr.fatal(
                     "Unknown <{}> band reference. "
-                    "Run g.bands module to list supported bands.".format(band_ref)
+                    "Check available band names in the current strds .".format(band_ref)
                 )
         except ValueError:
             msgr.fatal("Invalid name of the space time dataset. Only one dot allowed.")
