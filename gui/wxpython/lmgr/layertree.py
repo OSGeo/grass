6bd316c67cc9fb5cd15f62fe427a797910c5fbf5
Tomas Zigo <tomas.zigo@slovanet.sk>
2021-09-29 19:13:57 +0200

    wxGUI/lmgr: add layer menu items for check/uncheck selected layer(s)

--- gui/wxpython/lmgr/layertree.py
+++ gui/wxpython/lmgr/layertree.py
@@ -459,6 +459,8 @@ class LayerTree(treemixin.DragAndDrop, CT.CustomTreeCtrl):
                 "export-pg",
                 "export-attr",
                 "pack",
+                "check",
+                "uncheck",
             ):
                 self.popupID[key] = NewId()
 
@@ -477,6 +479,25 @@ class LayerTree(treemixin.DragAndDrop, CT.CustomTreeCtrl):
         if ltype != "command" and numSelected == 1:
             self.popupMenu.Append(self.popupID["rename"], _("Rename"))
             self.Bind(wx.EVT_MENU, self.OnRenameLayer, id=self.popupID["rename"])
+
+        item = wx.MenuItem(
+            self.popupMenu,
+            id=self.popupID["check"],
+            text=_("Check selected layer(s)"),
+        )
+        self.popupMenu.AppendItem(item)
+        self.Bind(
+            wx.EVT_MENU, self.OnCheckUncheckSelectedLayer, id=self.popupID["check"]
+        )
+        item = wx.MenuItem(
+            self.popupMenu,
+            id=self.popupID["uncheck"],
+            text=_("Uncheck selected layer(s)"),
+        )
+        self.popupMenu.AppendItem(item)
+        self.Bind(
+            wx.EVT_MENU, self.OnCheckUncheckSelectedLayer, id=self.popupID["uncheck"]
+        )
 
         # when multiple maps are selected of different types
         # we cannot zoom or change region
@@ -1333,6 +1354,15 @@ class LayerTree(treemixin.DragAndDrop, CT.CustomTreeCtrl):
                         "@" + mapset, "@" + newname
                     )
                     self.SetItemText(item, newlabel)
+
+    def OnCheckUncheckSelectedLayer(self, event):
+        """Check/uncheck selected layer(s)"""
+        check = wx.CHK_CHECKED
+        if event.GetId() == self.popupID["uncheck"]:
+            check = wx.CHK_UNCHECKED
+        self.hitCheckbox = True
+        for layer in self.GetSelections():
+            self.CheckItem(layer, checked=check)
 
     def AddLayer(
         self,
