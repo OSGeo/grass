e55fa5eb5dfd3c231c4b129bf324b111aa7adcae
ninsbl <stefan.blumentrath@nina.no>
2021-10-02 19:22:30 +0200

    some more OSGeo4W adjustments

--- mswindows/osgeo4w/mklibs.sh
+++ mswindows/osgeo4w/mklibs.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+if [ "$CI" ] ; then
+	DUMPBIN_PATH=$(cygpath -ua "C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/")
+	export PATH="${DUMPBIN_PATH}:$PATH"
+	# dumpbin in GH actions does not support options starting with "-"
+	DUMPBIN_EXPORT="/EXPORTS"
+else
+	DUMPBIN_EXPORT="-exports"
+fi
+
 [ -d mswindows/osgeo4w/vc ] || mkdir mswindows/osgeo4w/vc
 
 if [ -n "$VCPATH" ]; then
@@ -20,7 +29,7 @@ for dllfile in "$@"; do
 
  	echo "$dllfile => $dllname"
 
-	(cd $dlldir; dumpbin -exports $dllfile) |
+	(cd $dlldir; dumpbin "$DUMPBIN_EXPORT" $dllfile) |
 		sed -nf mswindows/osgeo4w/mklibs.sed |
 		egrep -v "^[	 ]*(_+IMPORT_DESCRIPTOR_.*|_+NULL_IMPORT_DESCRIPTOR)$" >mswindows/osgeo4w/vc/${defname%$VERSION}
 
@@ -28,4 +37,3 @@ for dllfile in "$@"; do
 	    lib -nologo -def:${defname} -subsystem:windows -machine:x64
 	    lib -nologo $libname || exit)
 done
-
--- mswindows/osgeo4w/package.sh
+++ mswindows/osgeo4w/package.sh
@@ -178,6 +178,11 @@ if ! [ -f mswindows/osgeo4w/configure-stamp ]; then
 		--with-nls \
 		--with-zstd \
 		--with-odbc \
+        --with-netcdf=${OSGEO4W_ROOT_MSYS}/bin/nc-config \
+		--with-blas \
+		--with-lapack-includes=/mingw64/include/lapack \
+		--with-openmp \
+		--with-wxwidgets \
 	    --with-cairo \
 		--with-cairo-includes=$OSGEO4W_ROOT_MSYS/include \
 		--with-cairo-ldflags="-L$PWD/mswindows/osgeo4w/lib -lcairo -lfontconfig" \
@@ -234,6 +239,19 @@ if [ -n "$PACKAGE_PATCH" ]; then
     unix2dos etc/preremove/grass${PACKAGE_POSTFIX}.bat
 
     # copy dependencies (TODO: to be reduced)
+    blas_dll=../msys64/mingw64/bin/libblas.dll
+    lapack_dll=../msys64/mingw64/bin/liblapack.dll
+    lapack_gcc=../msys64/mingw64/bin/libgcc_s_dw2-1.dll
+    omp=../msys64/mingw64/bin/libomp.dll
+
+    cp -uv $DLLS apps/grass/grass$POSTFIX/bin
+    if [ -f "$blas_dll" ] ; then echo "Copying libblas.dll"; fi
+    if [ -f "$blas_dll" ] ; then cp -uv "$blas_dll" apps/grass/grass$POSTFIX/bin/ ; fi
+
+    if [ -f "$lapack_dll" ] ; then echo "Copying liblapack.dll"; fi
+    if [ -f "$lapack_dll" ] ; then cp -uv "$lapack_dll" apps/grass/grass$POSTFIX/bin/ ; fi
+    if [ -f "$lapack_gcc" ] ; then cp -uv "$lapack_gcc" apps/grass/grass$POSTFIX/bin/ ; fi
+    if [ -f "$omp" ] ; then cp -uv "$omp" apps/grass/grass$POSTFIX/bin/ ; fi
     cp -uv $DLLS apps/grass/grass$POSTFIX/bin
     cp -uv /mingw64/etc/fonts/fonts.conf apps/grass/grass$POSTFIX/etc
 
