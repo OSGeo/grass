1b5f6a74c1e061aa6f96abce83ce5e909b6e64c6
ninsbl <stefan.blumentrath@nina.no>
2021-10-06 00:19:29 +0200

    add back dumpbin path

--- mswindows/osgeo4w/mklibs.sh
+++ mswindows/osgeo4w/mklibs.sh
@@ -3,6 +3,8 @@
 set -e
 
 if [ "$CI" ] ; then
+	DUMPBIN_PATH=$(cygpath -ua "C:/Program Files (x86)\Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/")
+	export PATH="${DUMPBIN_PATH}:$PATH"
 	# dumpbin in GH actions does not support options starting with "-"
 	DUMPBIN_EXPORT="/EXPORTS"
 else
