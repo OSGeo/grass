bd33f423d0b64d45f15ba2c6fbef9c776c6dbe45
Wolf Bergenheim <wolf@bergenheim.net>
2021-10-10 18:58:45 +0200

    Fixes Rast_log_colors to use GRASS_EPSILON instead of 1.
    Also fix Rast_abs_log_colors to actually work so that the color ramp
    is symmetrical around 0.

--- lib/raster/color_xform.c
+++ lib/raster/color_xform.c
@@ -190,14 +190,14 @@ void Rast_log_colors(struct Colors *dst, struct Colors *src, int samples)
 
     Rast_get_d_color_range(&min, &max, src);
     /* Make sure that the log makes sense, floor values < 1.0 to 1. */
-    if (min < 1.0) {
-	lmin = 0.0;
+    if (min < GRASS_EPSILON) {
+	lmin = GRASS_EPSILON;
     }
     else {
 	lmin = log(min);
     }
-    if (max < 1.0) {
-	lmax = 1.0;
+    if (max < GRASS_EPSILON) {
+	lmax = GRASS_EPSILON;
     }
     else {
 	lmax = log(max);
@@ -258,19 +258,18 @@ void Rast_abs_log_colors(struct Colors *dst, struct Colors *src, int samples)
     Rast_init_colors(dst);
 
     Rast_get_d_color_range(&min, &max, src);
-
     /* Make sure that the log makes sense, floor values < 1.0 to 1. */
     absmin = fabs(min);
     absmax = fabs(max);
     amax = MAX(absmin, absmax);
     amin = MIN(absmin, absmax);
-    if (amin < 1.0) {
+    if (amin < GRASS_EPSILON) {
 	lmin = 0.0;
     }
     else {
 	lmin = log(amin);
     }
-    if (amax < 1.0) {
+    if (amax < GRASS_EPSILON) {
 	lmax = 1.0;
     }
     else {
@@ -280,26 +279,25 @@ void Rast_abs_log_colors(struct Colors *dst, struct Colors *src, int samples)
     lamax = MAX(lmin, lmax);
     lamin = MIN(lmin, lmax);
 
-    Rast_get_default_color(&red, &grn, &blu, src);
-    Rast_set_default_color(red, grn, blu, dst);
-
     Rast_get_null_value_color(&red, &grn, &blu, src);
     Rast_set_null_value_color(red, grn, blu, dst);
+    Rast_get_default_color(&red, &grn, &blu, src);
+    Rast_set_default_color(red, grn, blu, dst);
 
     for (i = 0; i <= samples; i++) {
 	int red2, grn2, blu2;
 	double lx;
 	DCELL x, y;
 
-	y = amin + (amax - amin) * i / samples;
+	y = min + (max - min) * i / samples;
 	Rast_get_d_color(&y, &red2, &grn2, &blu2, src);
 
 	if (i == 0)
-	    x = amin;
+	    x = 0;
 	else if (i == samples)
 	    x = amax;
 	else {
-	    lx = lamin + (lamax - lamin) * i / samples;
+	    lx = lamax * i / samples;
 	    x = exp(lx);
 	}
 
@@ -310,8 +308,8 @@ void Rast_abs_log_colors(struct Colors *dst, struct Colors *src, int samples)
 				  &x1, red2, grn2, blu2, dst);
 	    x0 = -x0;
 	    x1 = -x1;
-	    Rast_add_d_color_rule(&x0, red, grn, blu,
-				  &x1, red2, grn2, blu2, dst);
+	    Rast_add_d_color_rule(&x1, red, grn, blu,
+				  &x0, red2, grn2, blu2, dst);
 	}
 
 	prev = x;
