c08a31556e542092a539ed7107f92fd7d880beef
Martin Landa <landa.martin@gmail.com>
2021-12-10 13:19:43 +0100

    cosmetics

--- lib/temporal/SQL/update_strds_metadata_template_v3.sql
+++ lib/temporal/SQL/update_strds_metadata_template_v3.sql
@@ -7,9 +7,9 @@
 -- SPACETIME_REGISTER_TABLE is a placeholder for specific stds map register table name (SQL compliant)
 -- SPACETIME_ID is a placeholder for specific stds id: name@mapset
 
--- Update the number of semantic labels
+-- Update the number of registered semantic labels
 UPDATE strds_metadata SET number_of_semantic_labels =
        (SELECT count(distinct semantic_label) FROM raster_metadata WHERE
        raster_metadata.id IN
-    		(SELECT id FROM SPACETIME_REGISTER_TABLE)
+               (SELECT id FROM SPACETIME_REGISTER_TABLE)
        ) WHERE id = 'SPACETIME_ID';
