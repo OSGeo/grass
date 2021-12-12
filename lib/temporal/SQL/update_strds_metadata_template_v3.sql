598f1e9c49277f8e56f31858eab5ec1dd7c035cd
Martin Landa <landa.martin@gmail.com>
2021-12-12 16:58:02 +0100

    TGIS: number_of_bands -> number_of_semantic_labels (#2011)

--- lib/temporal/SQL/update_strds_metadata_template_v3.sql
+++ lib/temporal/SQL/update_strds_metadata_template_v3.sql
@@ -7,8 +7,8 @@
 -- SPACETIME_REGISTER_TABLE is a placeholder for specific stds map register table name (SQL compliant)
 -- SPACETIME_ID is a placeholder for specific stds id: name@mapset
 
--- Update the number of registered bands
-UPDATE strds_metadata SET number_of_bands =
+-- Update the number of registered semantic labels
+UPDATE strds_metadata SET number_of_semantic_labels =
        (SELECT count(distinct semantic_label) FROM raster_metadata WHERE
        raster_metadata.id IN
                (SELECT id FROM SPACETIME_REGISTER_TABLE)
