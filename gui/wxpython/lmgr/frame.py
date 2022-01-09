0f7658822c4eb9e97e2ab4da10b4ef273f9412b2
Tomas Zigo <50632337+tmszi@users.noreply.github.com>
2022-01-09 06:16:03 +0100

    wxGUI/main_window: fix close map notebook page (#2072)
    Only in single window mode.
    
    * wxGUI/main_window: fix getting correct map notebook page index
    
    To avoid incorrectly closing the map notebook page, if the pages
    of the map notebook change position by dragging and dropping.
    
    * wxGUI/mapdisp: fix checking pgnum_dict variable if it's None
    
    When the map display notebook page has a vector/raster map
    displayed and the page is closed, and pressed the Cancel button
    in the Close Map Display dialog, canCloseDisplayCallback function
    return None not dict value.
    
    * wxGUI/lmgr: actual closed map display notebook layers tree page is stored in the dict
    
    Fixes closing the map display notebook layers tree page in
    multi window mode.
    
    Co-authored-by: Linda Kladivova <49241681+lindakladivova@users.noreply.github.com>

--- gui/wxpython/lmgr/frame.py
+++ gui/wxpython/lmgr/frame.py
@@ -515,14 +515,20 @@ class GMFrame(wx.Frame):
         page = self.currentPage
 
         def CanCloseDisplay(askIfSaveWorkspace):
-            """Callback to check if user wants to close display"""
-            pgnum = self.notebookLayers.GetPageIndex(page)
-            name = self.notebookLayers.GetPageText(pgnum)
+            """Callback to check if user wants to close display.
+
+            :return dict/None pgnum_dict/None: dict "layers" key represent
+                                               map display notebook layers
+                                               tree page index
+            """
+            pgnum_dict = {}
+            pgnum_dict["layers"] = self.notebookLayers.GetPageIndex(page)
+            name = self.notebookLayers.GetPageText(pgnum_dict["layers"])
             caption = _("Close Map Display {}").format(name)
             if not askIfSaveWorkspace or (
                 askIfSaveWorkspace and self.workspace_manager.CanClosePage(caption)
             ):
-                return pgnum
+                return pgnum_dict
             return None
 
         mapdisplay.canCloseDisplayCallback = CanCloseDisplay
@@ -851,12 +857,19 @@ class GMFrame(wx.Frame):
 
         event.Skip()
 
-    def _closePageNoEvent(self, page_index):
-        """Close page and destroy map display without
-        generating notebook page closing event"""
+    def _closePageNoEvent(self, pgnum_dict):
+        """Close page and destroy map display without generating notebook
+        page closing event.
+
+        :param dict pgnum_dict: dict "layers" key represent map display
+                                notebook layers tree page index
+        """
         self.notebookLayers.Unbind(FN.EVT_FLATNOTEBOOK_PAGE_CLOSING)
-        self.notebookLayers.DeletePage(page_index)
-        self.notebookLayers.Bind(FN.EVT_FLATNOTEBOOK_PAGE_CLOSING, self.OnCBPageClosing)
+        self.notebookLayers.DeletePage(pgnum_dict["layers"])
+        self.notebookLayers.Bind(
+            FN.EVT_FLATNOTEBOOK_PAGE_CLOSING,
+            self.OnCBPageClosing,
+        )
 
     def _switchPageHandler(self, event, notification):
         self._switchPage(notification=notification)
