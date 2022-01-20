91d67c903de4378fa7bb0d9f4d575025b4675c92
chaedri <caitlin.haedrich@gmail.com>
2022-01-20 13:29:25 -0500

    Renders all images to temp dir

--- python/grass/jupyter/timeseries.py
+++ python/grass/jupyter/timeseries.py
@@ -12,7 +12,11 @@
 #           for details.
 
 import tempfile
+import os
 import grass.script as gs
+from .display import GrassRenderer
+#from grass.animation import temporal_manager as tm
+#import grass.temporal as tgis
 
 from .region import RegionManagerFor2D, RegionManagerFor3D
 import ipywidgets as widgets
@@ -22,42 +26,74 @@ class timeseries:
     """timeseries creates visualization of time-space raster and
     vector dataset in Jupyter Notebooks"""
 
-    def __init__(self, map, basemap, overlay):
-        self.map = map
+    def __init__(self, timeseries, type="strds", basemap=None, overlay=None):
+        self.timeseries = timeseries
         self.basemap = basemap
         self.overlay = overlay
-        # Check that map ends with tsrds or tsvds
+        self.etype = type # element type - convention from temporal manager
 
-        # self.type == type #tsrds or tsvds
+        # Check that map is time space dataset
+        #self.validateTimeseriesName()
+        test = gs.read_command("t.list", where=f"name LIKE '{timeseries}'")
+        if not test:
+             raise AttributeError(_(f"Could not find space time raster or vector dataset named {timeseries}"))
+
+        # Create a temporary directory for our PNG images
+        self._tmpdir = tempfile.TemporaryDirectory()
+        print(self._tmpdir.name)
+
+    #def d_legend(self, **kwargs):
+        # this function should construct a legend command that can
+        # be called in the render_layers loop below
+
+    # THIS IS FROM gui/wxpython/animation/util.py but I couldn't figure out how
+    # to import that...
+    # CANNOT IMPORT grass.temporal
+    # def validateTimeseriesName(self):
+    #     """Check if space time dataset exists and completes missing mapset.
+    #
+    #     Raises GException if dataset doesn't exist."""
+    #     trastDict = tgis.tlist_grouped(self.etype)
+    #     if self.timeseries.find("@") >= 0:
+    #         nameShort, mapset = self.timeseries.split("@", 1)
+    #         if nameShort in trastDict[mapset]:
+    #             return self.timeseries
+    #         else:
+    #             raise GException(_(f"Space time dataset {self.timeseries} not found."))
+    #
+    #     mapsets = tgis.get_tgis_c_library_interface().available_mapsets()
+    #     for mapset in mapsets:
+    #         if mapset in trastDict.keys():
+    #             if self.timeseries in trastDict[mapset]:
+    #                 return self.timeseries + "@" + mapset
+    #     raise GException(_(f"Space time dataset {self.timeseries} not found."))
 
     def render_layers(self):
-        if self.type == "tsrds":
-            lyrList = gs.read_command("t.rast.list", input=self.map)
-        if self.type == "tsvds":
-            lyrList = gs.read_command("t.vect.list", input=self.map)
+        # NOT SURE IF THIS WILL ALWAYS WORK
+        # The maps key is found in the command history of the t.info output
+        # If, somehow, there was no "t.register" with map list in the command history,
+        # I think this would fail.
+        renderlist = gs.parse_command("t.info", input=self.timeseries, flags="h")["maps"][1:-1].split(",")
 
-        lyrList = str(lyrList)
-        lyrList = lyrList.split("\r\n")
-
-        names = []
-        for line in lyrList[1:-1]:
-            line = line.split("|")
-            names.append(line[0])
-
-        # TODO: add tempdirectory here for filenames
         filenames = []
-        for name in names:
-            filename = "{}.png".format(name)
+        for name in renderlist:
+            filename= os.path.join(self._tmpdir.name, "{}.png".format(name))
             filenames.append(filename)
-            img = gj.GrassRenderer(filename=filename)
-            if self.background:
-                img.d_rast(map=self.background)
-            if self.type == "tsrds":
+            img = GrassRenderer(filename=filename)
+            if self.basemap:
+                img.d_rast(map=self.basemap)
+            # THIS IS A BAD WAY OF DOING THIS
+            try:
                 img.d_rast(map=name)
-            if self.type == "tsvds":
+            except CalledModuleError:
                 img.d._vect(map=name)
             if self.overlay:
                 img.d_vect(map=self.overlay)
+            # THIS SHOULD CHANGE WITH d_legend completion
+            info = gs.parse_command("t.info", input="precip_sum", flags="g")
+            min_min = info["min_min"]
+            max_max = info["max_max"]
+            img.d_legend(raster=name, range=f"{min_min}, {max_max}")
 
     def timeslider(self):
 
