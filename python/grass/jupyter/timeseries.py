227cad6962e4e9914f9e7ca31d8b1d7b3063468d
chaedri <caitlin.haedrich@gmail.com>
2022-01-24 14:33:55 -0500

    Black formatting

--- python/grass/jupyter/timeseries.py
+++ python/grass/jupyter/timeseries.py
@@ -13,17 +13,11 @@
 
 import tempfile
 import os
-import datetime
 import grass.script as gs
 from .display import GrassRenderer
-#from grass.animation import temporal_manager as tm
-#import grass.temporal as tgis
-
-from .region import RegionManagerFor2D, RegionManagerFor3D
 
 
-
-class timeseries:
+class TimeSeries:
     """timeseries creates visualization of time-space raster and
     vector dataset in Jupyter Notebooks"""
 
@@ -39,36 +33,49 @@ class timeseries:
         # Check that map is time space dataset
         test = gs.read_command("t.list", where=f"name LIKE '{timeseries}'")
         if not test:
-             raise NameError(_(f"Could not find space time raster or vector dataset named {timeseries}"))
-
-        # Check datatype
+            raise NameError(
+                _(
+                    f"Could not find space time raster or vector dataset named {timeseries}"
+                )
+            )
 
         # Create a temporary directory for our PNG images
         self._tmpdir = tempfile.TemporaryDirectory()
-        print(self._tmpdir.name)
 
     def d_legend(self, **kwargs):
         self._legend = True
         self._legend_kwargs = kwargs
 
-
     def render_layers(self):
         if self.type == "strds":
-            renderlist = gs.read_command("t.rast.list", input=self.timeseries, columns="name", flags="u").strip().split("\n")
+            renderlist = (
+                gs.read_command(
+                    "t.rast.list", input=self.timeseries, columns="name", flags="u"
+                )
+                .strip()
+                .split("\n")
+            )
         elif self.type == "stvds":
-            renderlist = gs.read_command("t.vect.list", input=self.timeseries, columns="name", flags="u").strip().split("\n")
+            renderlist = (
+                gs.read_command(
+                    "t.vect.list", input=self.timeseries, columns="name", flags="u"
+                )
+                .strip()
+                .split("\n")
+            )
         else:
-            raise NameError(_(f"Dataset {self.timeseries} is not data type 'strds' or 'stvds'"))
+            raise NameError(
+                _(f"Dataset {self.timeseries} is not data type 'strds' or 'stvds'")
+            )
 
         filenames = []
         for name in renderlist:
-            filename= os.path.join(self._tmpdir.name, "{}.png".format(name))
+            filename = os.path.join(self._tmpdir.name, "{}.png".format(name))
             filenames.append(filename)
             img = GrassRenderer(filename=filename)
             if self.basemap:
                 img.d_rast(map=self.basemap)
             if self.type == "strds":
-                print(name)
                 img.d_rast(map=name)
             elif self.type == "stvds":
                 img.d_vect(map=name)
@@ -79,25 +86,46 @@ class timeseries:
                 info = gs.parse_command("t.info", input="precip_sum", flags="g")
                 min_min = info["min_min"]
                 max_max = info["max_max"]
-                img.d_legend(raster=name, range=f"{min_min}, {max_max}", **self._legend_kwargs)
+                img.d_legend(
+                    raster=name, range=f"{min_min}, {max_max}", **self._legend_kwargs
+                )
 
         self._filenames = filenames
 
-    def timeslider(self):
+    def TimeSlider(self):
         # Lazy Imports
         import ipywidgets as widgets
         from IPython.display import Image
 
         # create list of date/times for labels
         if self.type == "strds":
-            dates = gs.read_command("t.rast.list", input=self.timeseries, columns="start_time", flags="u").strip().split("\n")
+            dates = (
+                gs.read_command(
+                    "t.rast.list",
+                    input=self.timeseries,
+                    columns="start_time",
+                    flags="u",
+                )
+                .strip()
+                .split("\n")
+            )
         elif self.type == "stvds":
-            dates = gs.read_command("t.vect.list", input=self.timeseries, columns="start_time", flags="u").strip().split("\n")
-
+            dates = (
+                gs.read_command(
+                    "t.vect.list",
+                    input=self.timeseries,
+                    columns="start_time",
+                    flags="u",
+                )
+                .strip()
+                .split("\n")
+            )
+        # Dictionary of dates and associated image filename
         value_dict = {dates[i]: self._filenames[i] for i in range(len(dates))}
 
         def view_image(date):
             return Image(value_dict[date])
 
+        # This creates a dropdown menu for dates since they are a string
+        # In the future, it should create a SelectionSlider instead
         widgets.interact(view_image, date=dates)
-
