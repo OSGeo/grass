fd7f160e1c8297beaa56168a691c5fdaeb1ca71c
lindakladivova <l.kladivova@seznam.cz>
2022-01-25 11:28:09 -0600

    black

--- gui/wxpython/mapdisp/frame.py
+++ gui/wxpython/mapdisp/frame.py
@@ -1560,9 +1560,7 @@ class MapPanel(SingleMapPanel):
         from mapdisp.properties import MapDisplayPropertiesDialog
 
         dlg = MapDisplayPropertiesDialog(
-            parent=self,
-            giface=self._giface,
-            mapDispProperties=self.mapWindowProperties
+            parent=self, giface=self._giface, mapDispProperties=self.mapWindowProperties
         )
         dlg.CenterOnParent()
         dlg.Show()
