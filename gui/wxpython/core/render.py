f52b36d7369532c91d818f7372508a6c118af6f9
Anna Petrasova <kratochanna@gmail.com>
2022-01-31 00:20:18 -0500

    wxGUI: remove unnecessary printing of invalid map size

--- gui/wxpython/core/render.py
+++ gui/wxpython/core/render.py
@@ -1018,9 +1018,6 @@ class Map(object):
             self.width = int(size[0])
             self.height = int(size[1])
             if self.width < 1 or self.height < 1:
-                sys.stderr.write(
-                    _("Invalid map size %d,%d\n") % (self.width, self.height)
-                )
                 raise ValueError
         except ValueError:
             self.width = 640
