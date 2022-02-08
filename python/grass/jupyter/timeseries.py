48b2b0ae3d46b49f96b59ce315342ccf8efa666b
chaedri <caitlin.haedrich@gmail.com>
2022-02-08 18:28:34 -0500

    docstrings, string parsing and minor edits

--- python/grass/jupyter/timeseries.py
+++ python/grass/jupyter/timeseries.py
@@ -13,24 +13,56 @@
 
 import tempfile
 import os
+import platform
 import grass.script as gs
 from .display import GrassRenderer
 
+# Probably needs a better name
+def parse_csv_str(string):
+    if platform.system() == "Windows":
+        out = string.strip().split("\r\n")
+    else:
+        out = string.strip().split("\n")
+    return out
+
 
 class TimeSeries:
-    """timeseries creates visualization of time-space raster and
-    vector dataset in Jupyter Notebooks"""
+    """Creates visualizations of time-space raster and
+    vector datasets in Jupyter Notebooks
+
+    Basic usage::
+    >>> img = TimeSeries("series_name")
+    >>> img.d_legend() #Add legend
+    >>> img.render_layers() #Render Layers
+    >>> img.time_slider() #Create TimeSlider
+    >>> img.animate()
+
+    This class of grass.jupyter is experimental and under development.
+    The API can change at anytime.
+    """
 
     def __init__(self, timeseries, etype="strds", basemap=None, overlay=None):
+        """Creates an instance of the TimeSeries visualizations class.
+
+        :param str timeseries: name of space-time dataset
+        :param str etype: element type, strds (space-time raster dataset) or stvds (space-time vector dataset)
+        :param str basemap: name of raster to use as basemap/background for visuals
+        :param str overlay: name of vector to add to visuals
+        """
         self.timeseries = timeseries
-        self.basemap = basemap
-        self.overlay = overlay
         self._legend = False
         self._etype = etype  # element type, borrowing convention from tgis
         self._legend_kwargs = None
         self._filenames = []
         self._file_date_dict = {}
         self._date_file_dict = {}
+
+        # TODO: Improve basemap and overlay method
+        # Currently does not support multiple basemaps or overlays
+        # (i.e. if you wanted to have two vectors rendered, like
+        # roads and streams, this isn't possible - you can only have one
+        self.basemap = basemap
+        self.overlay = overlay
 
         # Check that map is time space dataset
         test = gs.read_command("t.list", where=f"name LIKE '{timeseries}'")
@@ -47,55 +79,58 @@ class TimeSeries:
 
         # create list of date/times
         if self._etype == "strds":
-            self._dates = (
+            self._dates = parse_csv_str(
                 gs.read_command(
                     "t.rast.list",
                     input=self.timeseries,
                     columns="start_time",
                     flags="u",
                 )
-                .strip()
-                .split("\n")
             )
         elif self._etype == "stvds":
-            self._dates = (
+            self._dates = parse_csv_str(
                 gs.read_command(
                     "t.vect.list",
                     input=self.timeseries,
                     columns="start_time",
                     flags="u",
                 )
-                .strip()
-                .split("\n")
             )
 
     def d_legend(self, **kwargs):
+        """Wrapper for d.legend. Passes keyword arguments to d.legend in
+        render_layers method.
+        """
         self._legend = True
         self._legend_kwargs = kwargs
 
     def render_layers(self):
+        """Renders map for each time-step in space-time dataset and save to PNG
+        image in temporary directory.
+
+        Must be run before creating a visualization (i.e. time_slider or animate).
+
+        Can be time-consuming to run with large space-time datasets.
+        """
         # Create List of layers to Render
         if self._etype == "strds":
-            renderlist = (
+            renderlist = parse_csv_str(
                 gs.read_command(
                     "t.rast.list", input=self.timeseries, columns="name", flags="u"
                 )
-                .strip()
-                .split("\n")
             )
         elif self._etype == "stvds":
-            renderlist = (
+            renderlist = parse_csv_str(
                 gs.read_command(
                     "t.vect.list", input=self.timeseries, columns="name", flags="u"
                 )
-                .strip()
-                .split("\n")
             )
         else:
             raise NameError(
                 _(f"Dataset {self.timeseries} is not element type 'strds' or 'stvds'")
             )
 
+        # Start counter for matching datetime stamp with filename
         i = 0
         for name in renderlist:
             # Create image file
@@ -121,13 +156,21 @@ class TimeSeries:
                 img.d_legend(
                     raster=name, range=f"{min_min}, {max_max}", **self._legend_kwargs
                 )
+            # Add 1 to counter
             i = i + 1
 
-    def time_slider(self):
+    def time_slider(self, slider_width="60%"):
+        """
+        Create interactive timeline slider.
+
+        param str slider_width: width of datetime selection slider as a
+                                percentage (%) or pixels (px)
+        """
         # Lazy Imports
         import ipywidgets as widgets
         from IPython.display import Image
 
+        # Datetime selection slider
         slider = widgets.SelectionSlider(
             options=self._dates,
             value=self._dates[0],
@@ -136,12 +179,14 @@ class TimeSeries:
             continuous_update=True,
             orientation="horizontal",
             readout=True,
-            layout=widgets.Layout(width="80%"),
+            layout=widgets.Layout(width=slider_width),
         )
 
+        # Display image associated with datetime
         def view_image(date):
             return Image(self._file_date_dict[date])
 
+        # Return interact widget with image and slider
         widgets.interact(view_image, date=slider)
 
     def animate(
