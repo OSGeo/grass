af20745947d5e225805679643eccece01699c3cb
Linda Kladivova <49241681+lindakladivova@users.noreply.github.com>
2022-02-09 22:21:25 +0100

    wxGUI: Use Map Display settings in other Map Display applications (#2155)

--- gui/wxpython/image2target/ii2t_mapdisplay.py
+++ gui/wxpython/image2target/ii2t_mapdisplay.py
@@ -102,11 +102,8 @@ class MapPanel(SingleMapPanel):
             sb.SbCoordinates,
             sb.SbRegionExtent,
             sb.SbCompRegionExtent,
-            sb.SbShowRegion,
-            sb.SbResolution,
             sb.SbDisplayGeometry,
             sb.SbMapScale,
-            sb.SbProjection,
             sbgcp.SbGoToGCP,
             sbgcp.SbRMSError,
         ]
@@ -114,7 +111,7 @@ class MapPanel(SingleMapPanel):
         # create statusbar and its manager
         self.statusbar = self.CreateStatusbar(statusbarItems)
 
-        self.statusbarManager.SetMode(8)  # goto GCP
+        self.statusbarManager.SetMode(5)  # goto GCP
 
         #
         # Init map display (buffered DC & set default cursor)
