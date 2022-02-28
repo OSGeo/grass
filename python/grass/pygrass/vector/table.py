d853d40a4a371b11ad0fbe92820e1900cc4e4342
Maximilian Stahlberg <maximilian.stahlberg@tu-berlin.de>
2022-02-28 07:29:16 +0100

    PyGRASS: Distinguish unsupported and unknown database drivers.

--- python/grass/pygrass/vector/table.py
+++ python/grass/pygrass/vector/table.py
@@ -45,6 +45,7 @@ if sys.version_info.major >= 3:
 test_vector_name = "table_doctest_map"
 
 DRIVERS = ("sqlite", "pg")
+UNSUPPORTED_DRIVERS = ("ogr", "dbf")
 
 
 def get_path(path, vect_name=None):
@@ -714,12 +715,18 @@ class Link(object):
         return decode(self.c_fieldinfo.contents.driver)
 
     def _set_driver(self, driver):
-        if driver not in DRIVERS:
+        if driver in DRIVERS:
+            self.c_fieldinfo.contents.driver = ReturnString(driver)
+        elif driver in UNSUPPORTED_DRIVERS:
             raise NotImplementedError(
                 "The database driver %s is not supported by PyGRASS, "
                 "use: %s." % (driver, ", ".join(DRIVERS))
             )
-        self.c_fieldinfo.contents.driver = ReturnString(driver)
+        else:
+            raise ValueError(
+                "The database driver %s is not known to PyGRASS, "
+                "use: %s." % (driver, ", ".join(DRIVERS))
+            )
 
     driver = property(
         fget=_get_driver,
