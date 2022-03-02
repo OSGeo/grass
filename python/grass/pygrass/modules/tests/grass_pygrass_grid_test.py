ca4d98114cf0c1472bc9be889d6a60f52dc830e9
Vaclav Petras <wenzeslaus@gmail.com>
2022-03-01 23:17:17 -0500

    Add test for accepting mapset prefix and deleting mapsets

--- python/grass/pygrass/modules/tests/grass_pygrass_grid_test.py
+++ python/grass/pygrass/modules/tests/grass_pygrass_grid_test.py
@@ -97,7 +97,7 @@ def test_tiling_schemes(tmp_path, width, height):
 
 @pytest.mark.parametrize("overlap", [0, 1, 2, 5])
 def test_overlaps(tmp_path, overlap):
-    """Check that init function return value works as a context manager"""
+    """Check that overlap accepts different values"""
     location = "test"
     gs.core._create_location_xy(tmp_path, location)  # pylint: disable=protected-access
     with grass_setup.init(tmp_path / location):
@@ -126,3 +126,48 @@ def test_overlaps(tmp_path, overlap):
 
         info = gs.raster_info("slope")
         assert info["min"] > 0
+
+
+@pytest.mark.parametrize("clean", [True, False])
+def test_cleans(tmp_path, clean):
+    """Check that temporary mapsets are cleaned when appropriate"""
+    location = "test"
+    mapset_prefix = "abc"
+    gs.core._create_location_xy(tmp_path, location)  # pylint: disable=protected-access
+    with grass_setup.init(tmp_path / location):
+        gs.run_command("g.region", s=0, n=50, w=0, e=50, res=1)
+        surface = "surface"
+        gs.run_command("r.surf.fractal", output=surface)
+
+        def run_grid_module():
+            # modules/shortcuts calls get_commands which requires GISBASE.
+            # pylint: disable=import-outside-toplevel
+            from grass.pygrass.modules.grid import GridModule
+
+            grid = GridModule(
+                "r.slope.aspect",
+                width=10,
+                height=5,
+                overlap=0,
+                processes=max_processes(),
+                elevation=surface,
+                slope="slope",
+                aspect="aspect",
+                mapset_prefix=mapset_prefix,
+            )
+            grid.run(clean=clean)
+
+        run_in_subprocess(run_grid_module)
+
+        path = tmp_path / location
+        prefixed = 0
+        for item in path.iterdir():
+            if item.is_dir():
+                if clean:
+                    # We know right away something is wrong.
+                    assert not item.name.startswith(mapset_prefix), "Mapset not cleaned"
+                else:
+                    # We need to see if there is at least one prefixed mapset.
+                    prefixed += int(item.name.startswith(mapset_prefix))
+        if not clean:
+            assert prefixed, "Not even one prefixed mapset"
