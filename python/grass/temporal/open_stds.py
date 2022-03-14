9ad643371ed59a8dcbf67f18571090ac8f1d2a4b
Markus Metz <33666869+metzm@users.noreply.github.com>
2022-03-14 18:26:22 +0100

    TGIS: enhance mapset access management (#1924)
    * tgis: enhance mapset access management

--- python/grass/temporal/open_stds.py
+++ python/grass/temporal/open_stds.py
@@ -78,8 +78,8 @@ def open_old_stds(name, type, dbif=None):
     if not sp.is_in_db(dbif):
         dbif.close()
         msgr.fatal(
-            _("Space time %(sp)s dataset <%(name)s> not found")
-            % {"sp": sp.get_new_map_instance(None).get_type(), "name": name}
+            _("Space time %(sp)s dataset <%(id)s> not found")
+            % {"sp": sp.get_new_map_instance(None).get_type(), "id": id}
         )
     # Read content from temporal database
     sp.select(dbif)
