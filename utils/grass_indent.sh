ac263b7448f7d1cc0a445ac1572080f8614d81e3
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2022-03-18 09:49:44 +0100

    [FreeBSD] Use gnu indent

--- utils/grass_indent.sh
+++ utils/grass_indent.sh
@@ -15,12 +15,17 @@
 
 # TODO: replace short flags by long ones to improve readability
 
+GP=
+
+if [ `uname` == FreeBSD ] ; then
+ GP=g
+fi
 
 if [ $# -lt 1 ] ; then
  echo "No files specified (give file name(s) as parameter)"
  exit 1
 else
- indent -npro -bad -bap -bbb -br -bli0 -bls -cli0 -ncs -fc1 -hnl -i4 \
+ ${GP}indent -npro -bad -bap -bbb -br -bli0 -bls -cli0 -ncs -fc1 -hnl -i4 \
       -nbbo -nbc -nbfda -nbfde -ncdb -ncdw -nce -nfca -npcs -nprs \
       -npsl -nsc -nsob -saf -sai -saw -sbi0 -ss --no-tabs "$@"
 
