aad1e8240ec549365642d260139f369dbe081f56
Markus Neteler <neteler@gmail.com>
2022-04-02 13:53:30 +0200

    fix missing newline; expand abbreviated parameter names

--- raster/r.external/testsuite/data/generate_data.sh
+++ raster/r.external/testsuite/data/generate_data.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-# This script requires the GRASS 7 North Caroline location
+# This script requires the GRASS 7 North Carolina location
 
 export GRASS_OVERWRITE=1
 
 # 2D output
-g.region rast=elevation -p
+g.region raster=elevation -p
 g.region res=100 -p
 
-r.out.gdal in=elevation out=elevation.tif format=GTiff
-r.out.gdal in=elevation out=elevation.tiff format=GTiff
-r.out.gdal in=elevation out=elevation.asc format=AAIGrid
-r.out.gdal in=elevation out=elevation.nc format=netCDF
+r.out.gdal input=elevation output=elevation.tif format=GTiff
+r.out.gdal input=elevation output=elevation.tiff format=GTiff
+r.out.gdal input=elevation output=elevation.asc format=AAIGrid
+r.out.gdal input=elevation output=elevation.nc format=netCDF
 
 # 3D output
 g.region b=0 t=5 res3=100 tbres=1 -p3
 
 r.to.rast3 input=elevation output=elevation3d
-r3.out.netcdf input=elevation3d out=elevation3d.nc
+r3.out.netcdf input=elevation3d output=elevation3d.nc
 
