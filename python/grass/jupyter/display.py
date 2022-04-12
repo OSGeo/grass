41ab3d4a40a7d64659a2cae38488ad49346bb756
Anna Petrasova <kratochanna@gmail.com>
2022-04-12 17:14:23 -0400

    grass.jupyter: Automatically determine image size based on region (#2285)
    Using the aspect ratio of the computational region, automatically determine 2D rendered image dimensions. Defaults are now None and one or both values can be specified. Missing values are taken from defaults and modified to match aspect ratio of the computational region extent. Resulting images in notebooks are now smaller without white (empty) areas around the data. As a result, the image of the displayed data is often significantly bigger.

--- python/grass/jupyter/display.py
+++ python/grass/jupyter/display.py
@@ -47,8 +47,8 @@ class GrassRenderer:
 
     def __init__(
         self,
-        height=400,
-        width=600,
+        height=None,
+        width=None,
         filename=None,
         env=None,
         font="sans",
@@ -82,8 +82,8 @@ class GrassRenderer:
         else:
             self._env = os.environ.copy()
         # Environment Settings
-        self._env["GRASS_RENDER_WIDTH"] = str(width)
-        self._env["GRASS_RENDER_HEIGHT"] = str(height)
+        self._env["GRASS_RENDER_WIDTH"] = str(width) if width else "600"
+        self._env["GRASS_RENDER_HEIGHT"] = str(height) if height else "400"
         self._env["GRASS_FONT"] = font
         self._env["GRASS_RENDER_TEXT_SIZE"] = str(text_size)
         self._env["GRASS_RENDER_IMMEDIATE"] = renderer
@@ -117,7 +117,13 @@ class GrassRenderer:
         self._env["GRASS_LEGEND_FILE"] = str(self._legend_file)
 
         # rendering region setting
-        self._region_manager = RegionManagerFor2D(use_region, saved_region, self._env)
+        self._region_manager = RegionManagerFor2D(
+            use_region=use_region,
+            saved_region=saved_region,
+            width=width,
+            height=height,
+            env=self._env,
+        )
 
     @property
     def filename(self):
@@ -146,6 +152,7 @@ class GrassRenderer:
         # Check module is from display library then run
         if module[0] == "d":
             self._region_manager.set_region_from_command(module, **kwargs)
+            self._region_manager.adjust_rendering_size_from_region()
             gs.run_command(module, env=self._env, **kwargs)
         else:
             raise ValueError("Module must begin with letter 'd'.")
