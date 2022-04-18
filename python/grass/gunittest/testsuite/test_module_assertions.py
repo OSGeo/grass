c4e052d568fe86c2cea44733a84a32e6b4e7775b
adinayyu <87903802+adinayyu@users.noreply.github.com>
2022-04-18 19:20:26 +0530

    Update test_module_assertions.py
    Added result when tests were run on jupyterlab binder

--- python/grass/gunittest/testsuite/test_module_assertions.py
+++ python/grass/gunittest/testsuite/test_module_assertions.py
@@ -1,4 +1,3 @@
-# use %%python here to run this code in jupyterlab binder
 import copy
 import subprocess
 
@@ -31,28 +30,18 @@ class TestModuleAssertions(TestCase):
 
     def test_runModule(self):
         """Correct and incorrect Module used in runModule()"""
-        """Run PyGRASS module. Runs the module and raises an exception if the module ends with non-zero return code. 
-        Usually, this is the same as testing the return code and raising exception but by using this method, you give testing framework more control over the execution, error handling and storing of output.
-        In terms of testing framework, this function causes a common error, not a test failure."""
         self.runModule(self.rinfo)
         self.assertTrue(self.rinfo.outputs["stdout"].value)
         self.assertRaises(CalledModuleError, self.runModule, self.rinfo_wrong)
 
     def test_assertModule(self):
         """Correct and incorrect Module used in assertModule()"""
-        """Run PyGRASS module in controlled way and assert non-zero return code.
-        You should use this method to invoke module you are testing. 
-        By using this method, you give testing framework more control over the execution, error handling and storing of output.
-        It will not print module stdout and stderr, instead it will always store them for further examination. Streams are stored separately.
-        This method is not suitable for testing error states of the module. """
         self.assertModule(self.rinfo)
         self.assertTrue(self.rinfo.outputs["stdout"].value)
         self.assertRaises(self.failureException, self.assertModule, self.rinfo_wrong)
 
     def test_assertModuleFail(self):
         """Correct and incorrect Module used in assertModuleFail()"""
-        """Test that module fails with a non-zero return code.
-        Works like assertModule() but expects module to fail."""
         self.assertModuleFail(self.rinfo_wrong)
         stderr = self.rinfo_wrong.outputs["stderr"].value
         self.assertTrue(stderr)
@@ -74,28 +63,18 @@ class TestSimpleModuleAssertions(TestCase):
 
     def test_runModule(self):
         """Correct and incorrect SimpleModule used in runModule()"""
-        """Run PyGRASS module. Runs the module and raises an exception if the module ends with non-zero return code. 
-        Usually, this is the same as testing the return code and raising exception but by using this method, you give testing framework more control over the execution, error handling and storing of output.
-        In terms of testing framework, this function causes a common error, not a test failure."""
         self.runModule(self.rinfo)
         self.assertTrue(self.rinfo.outputs["stdout"].value)
         self.assertRaises(CalledModuleError, self.runModule, self.rinfo_wrong)
 
     def test_assertModule(self):
         """Correct and incorrect SimpleModule used in assertModule()"""
-        """Run PyGRASS module in controlled way and assert non-zero return code.
-        You should use this method to invoke module you are testing. 
-        By using this method, you give testing framework more control over the execution, error handling and storing of output.
-        It will not print module stdout and stderr, instead it will always store them for further examination. Streams are stored separately.
-        This method is not suitable for testing error states of the module. """
         self.assertModule(self.rinfo)
         self.assertTrue(self.rinfo.outputs["stdout"].value)
         self.assertRaises(self.failureException, self.assertModule, self.rinfo_wrong)
 
     def test_assertModuleFail(self):
         """Correct and incorrect SimpleModule used in assertModuleFail()"""
-        """Test that module fails with a non-zero return code.
-        Works like assertModule() but expects module to fail."""
         self.assertModuleFail(self.rinfo_wrong)
         stderr = self.rinfo_wrong.outputs["stderr"].value
         self.assertTrue(stderr)
@@ -105,3 +84,58 @@ class TestSimpleModuleAssertions(TestCase):
 
 if __name__ == "__main__":
     test()
+    
+"""
+north=228500
+south=215000
+east=645000
+west=630000
+nsres=10
+ewres=10
+rows=1350
+cols=1500
+cells=2025000
+datatype=FCELL
+ncats=255
+ERROR: Raster map <does_not_exists> not found
+ERROR: Raster map <does_not_exists> not found
+north=228500
+south=215000
+east=645000
+west=630000
+nsres=10
+ewres=10
+rows=1350
+cols=1500
+cells=2025000
+datatype=FCELL
+ncats=255
+north=228500
+south=215000
+east=645000
+west=630000
+nsres=10
+ewres=10
+rows=1350
+cols=1500
+cells=2025000
+datatype=FCELL
+ncats=255
+ERROR: Raster map <does_not_exists> not found
+ERROR: Raster map <does_not_exists> not found
+north=228500
+south=215000
+east=645000
+west=630000
+nsres=10
+ewres=10
+rows=1350
+cols=1500
+cells=2025000
+datatype=FCELL
+ncats=255
+......
+----------------------------------------------------------------------
+Ran 6 tests in 1.076s
+OK
+"""
