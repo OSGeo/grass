bb05c4ab43894f68cd4aea29041e3d1324634b7a
Vaclav Petras <wenzeslaus@gmail.com>
2022-05-20 10:54:16 -0400

    checks: Indent mask-related code
    A whitespace-only change after running grass_indent.sh.

--- raster/r.report/maskinfo.c
+++ raster/r.report/maskinfo.c
@@ -16,10 +16,10 @@ char *maskinfo(void)
 
     results = NULL;
     if (G_find_raster("MASK", G_mapset()) == NULL)
-	return "none";
+        return "none";
     if (Rast_get_reclass("MASK", G_mapset(), &reclass) <= 0) {
-	sprintf(text, "MASK in %s", G_mapset());
-	return append(results, text);
+        sprintf(text, "MASK in %s", G_mapset());
+        return append(results, text);
     }
 
     sprintf(text, "%s in %s", reclass.name, reclass.mapset);
@@ -27,15 +27,15 @@ char *maskinfo(void)
     next = 0;
     first = 1;
     do {
-	next = reclass_text(text, &reclass, next);
-	if (*text == 0)
-	    break;
-	if (first) {
-	    first = 0;
-	    results = append(results, ", categories");
-	}
-	results = append(results, " ");
-	results = append(results, text);
+        next = reclass_text(text, &reclass, next);
+        if (*text == 0)
+            break;
+        if (first) {
+            first = 0;
+            results = append(results, ", categories");
+        }
+        results = append(results, " ");
+        results = append(results, text);
     }
     while (next >= 0);
     Rast_free_reclass(&reclass);
@@ -54,21 +54,21 @@ static int reclass_text(char *text, struct Reclass *reclass, int next)
 
     first = -1;
     for (i = next; i < n; i++) {
-	if (reclass->table[i]) {
-	    if (first < 0)
-		first = i;
-	}
-	else if (first >= 0) {
-	    do_text(text, (long)(first + reclass->min),
-		    (long)(i - 1 + reclass->min));
-	    first = -1;
-	    if (strlen(text) > 60)
-		return i;
-	}
+        if (reclass->table[i]) {
+            if (first < 0)
+                first = i;
+        }
+        else if (first >= 0) {
+            do_text(text, (long)(first + reclass->min),
+                    (long)(i - 1 + reclass->min));
+            first = -1;
+            if (strlen(text) > 60)
+                return i;
+        }
     }
     if (first >= 0)
-	do_text(text, (long)(first + reclass->min),
-		(long)(i - 1 + reclass->min));
+        do_text(text, (long)(first + reclass->min),
+                (long)(i - 1 + reclass->min));
     return -1;
 }
 
@@ -77,12 +77,12 @@ static int do_text(char *text, long first, long last)
     char work[40];
 
     if (*text)
-	strcat(text, " ");
+        strcat(text, " ");
 
     if (first == last)
-	sprintf(work, "%ld", first);
+        sprintf(work, "%ld", first);
     else
-	sprintf(work, "%ld-%ld", first, last);
+        sprintf(work, "%ld-%ld", first, last);
 
     strcat(text, work);
 
@@ -92,7 +92,7 @@ static int do_text(char *text, long first, long last)
 static char *append(char *results, char *text)
 {
     if (results == NULL)
-	return G_store(text);
+        return G_store(text);
     results = G_realloc(results, strlen(results) + strlen(text) + 1);
     strcat(results, text);
     return results;
