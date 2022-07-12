OSGeo/grass

commit 45ff0ead5acfbcb574a4326b54538b28821a2aea

validate provided urls
git-svn-id: https://svn.osgeo.org/grass/grass/trunk@74288 15284696-431f-4ddb-bdfa-cd5b030d7da7

scripts/g.extension/g.extension.py
@@ -1679,6 +1679,38 @@ def resolve_source_code(url=None, name=None):
                    .format(version=version[0],
                            module_class=module_class, module_name=name)
         return 'official', trac_url
+
+    # Check if URL can be found
+    # Catch corner case if local URL is given starting with file://
+    url = url[6:] if url.startswith('file://') else url
+    if not os.path.exists(url):
+        url_validated = False
+        if url.startswith('http'):
+            try:
+                open_url = urlopen(url)
+                open_url.close()
+                url_validated = True
+            except:
+                pass
+        else:
+            try:
+                open_url = urlopen('http://' + url)
+                open_url.close()
+                url_validted = True
+            except:
+                pass
+            try:
+                open_url = urlopen('https://' + url)
+                open_url.close()
+                url_validated = True
+            except:
+                pass
+
+        if not url_validated:
+            grass.fatal(_('Cannot open URL: {}'.format(url)))
+
+
+    # Handle local URLs
     if os.path.isdir(url):
         return 'dir', os.path.abspath(url)
     elif os.path.exists(url):
@@ -1687,6 +1719,7 @@ def resolve_source_code(url=None, name=None):
         for suffix in extract_tar.supported_formats:
             if url.endswith('.' + suffix):
                 return suffix, os.path.abspath(url)
+    # Handle remote URLs
     else:
         source, resolved_url = resolve_known_host_service(url)
         if source:

commit 5a181bff8cad6753296c2b5df8f9a11e50a30654

g.extension: Add details to URL error message
The URL validation introduced in 45ff0ead5acfbcb574a4326b54538b28821a2aea does not include the original error message details in the user-visible error message. This adds the details and also fixes the formatting which was done on the regular string literal instead of the translated string.

scripts/g.extension/g.extension.py
@@ -2494,29 +2494,31 @@ def resolve_source_code(url=None, name=None, branch=None, fork=False):
     url = url[6:] if url.startswith("file://") else url
     if not os.path.exists(url):
         url_validated = False
+        message = None
         if url.startswith("http"):
             try:
                 open_url = urlopen(url)
                 open_url.close()
                 url_validated = True
-            except URLError:
-                pass
+            except URLError as error:
+                message = error
         else:
             try:
                 open_url = urlopen("http://" + url)
                 open_url.close()
                 url_validated = True
-            except URLError:
-                pass
+            except URLError as error:
+                message = error
             try:
                 open_url = urlopen("https://" + url)
                 open_url.close()
                 url_validated = True
-            except URLError:
-                pass
-
+            except URLError as error:
+                message = error
         if not url_validated:
-            grass.fatal(_("Cannot open URL: {}".format(url)))
+            grass.fatal(
+                _("Cannot open URL <{url}>: {error}").format(url=url, error=message)
+            )
 
     # Handle local URLs
     if os.path.isdir(url):