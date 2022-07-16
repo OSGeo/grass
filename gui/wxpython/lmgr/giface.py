75d4881bbb65f852bf6bf9ec3520f1593e3f85ec
Tomas Zigo <50632337+tmszi@users.noreply.github.com>
2022-07-16 18:15:07 +0200

    wxGUI/lmgr: fix launch GCP Manager if not map display is activated (#2464)

--- gui/wxpython/lmgr/giface.py
+++ gui/wxpython/lmgr/giface.py
@@ -58,10 +58,11 @@ class LayerList(object):
 
     def __iter__(self):
         """Iterates over the contents of the list."""
-        item = self._tree.GetFirstChild(self._tree.root)[0]
-        while item and item.IsOk():
-            yield Layer(item, self._tree.GetPyData(item))
-            item = self._tree.GetNextItem(item)
+        if self._tree:
+            item = self._tree.GetFirstChild(self._tree.root)[0]
+            while item and item.IsOk():
+                yield Layer(item, self._tree.GetPyData(item))
+                item = self._tree.GetNextItem(item)
 
     def __getitem__(self, index):
         """Select a layer from the LayerList using the index."""
