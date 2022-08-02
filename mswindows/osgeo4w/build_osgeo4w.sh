17ca3c9aa4ad9bf452288758e893dce4546e571a
ninsbl <stefan.blumentrath@gmx.de>
2022-08-02 21:10:46 +0200

    try to deactivate fontconfig

--- mswindows/osgeo4w/build_osgeo4w.sh
+++ mswindows/osgeo4w/build_osgeo4w.sh
@@ -67,7 +67,7 @@ export ARCH=x86_64-w64-mingw32
     --with-odbc \
     --with-cairo \
     --with-cairo-includes=${OSGEO4W_ROOT_MSYS}/include \
-    --with-cairo-ldflags="-L${SRC}/mswindows/osgeo4w/lib -lcairo -lfontconfig" \
+    --with-cairo-ldflags="-L${SRC}/mswindows/osgeo4w/lib -lcairo" # -lfontconfig" \ # temporary dectivate fontconfig on osgeo4w
     --with-opengl=windows \
     --with-bzlib \
     --with-liblas=${SRC}/mswindows/osgeo4w/liblas-config \
