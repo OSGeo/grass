f784646e12dc7e2f60c1b87bf1241aa8f3479b1d
Tomas Zigo <tomas.zigo@slovanet.sk>
2022-09-09 12:40:50 +0200

    man: allow register all addons keywords in main Keywords Index (#2529)

--- man/build_keywords.py
+++ man/build_keywords.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# generates keywords.html
-# (c) 2013 by the GRASS Development Team, Luca Delucchi
+"""
+Generates keywords.html file for core and optionally for addons modules.
+
+Usage:
+
+Generate core modules keywords HTML page
+
+python man/build_keywords.py <path_to_core_modules_html_man_files>
+
+Generate core modules and optionally inject addons keywords HTML page
+
+python man/build_keywords.py <dir_path_to_core_modules_html_man_files>
+    <dir_path_to_addons_modules_html_man_files>
+
+@author Luca Delucchi
+@author Tomas Zigo <tomas.zigo slovanet.sk> - inject addons modules keywords
+"""
 
 import os
 import sys
@@ -13,18 +28,45 @@ blacklist = ['Display', 'Database', 'General', 'Imagery', 'Misc', 'Postscript',
              'Raster', 'Raster3D', 'Temporal', 'Vector']
 
 path = sys.argv[1]
+addons_path = None
+if len(sys.argv) >= 3:
+    addons_path = sys.argv[2]
+
 year = os.getenv("VERSION_DATE")
 
 keywords = {}
 
-htmlfiles = glob.glob1(path, '*.html')
+htmlfiles = glob.glob(os.path.join(path, "*.html"))
+if addons_path:
+    addons_man_files = glob.glob(os.path.join(addons_path, "*.html"))
+    htmlfiles.extend(addons_man_files)
 
 char_list = {}
 
-for fname in htmlfiles:
-    fil = open(os.path.join(path, fname))
+
+def get_module_man_html_file_path(module):
+    """Get module manual HTML file path
+
+    :param str module: module manual HTML file name e.g. v.surf.rst.html
+
+    :return str module_path: core/addon module manual HTML file path
+    """
+    if addons_path and module in ",".join(addons_man_files):
+        module_path = os.path.join(addons_path, module)
+        module_path = module_path.replace(
+            os.path.commonpath([path, module_path]),
+            ".",
+        )
+    else:
+        module_path = os.path.join(path, module)
+    return module_path
+
+
+for html_file in htmlfiles:
+    fname = os.path.basename(html_file)
+    with open(html_file) as f:
+        lines = f.readlines()
     # TODO maybe move to Python re (regex)
-    lines = fil.readlines()
     # remove empty lines
     lines = [x for x in lines if x != '\n']
     try:
@@ -82,10 +124,12 @@ for key in sortedKeys:
     keyword_line = '<dt><b><a name="%s" class="urlblack">%s</a></b></dt>' \
                    '<dd>' % (key, key)
     for value in sorted(keywords[key]):
-        keyword_line += ' <a href="%s">%s</a>,' % (value,
-                                                   value.replace('.html', ''))
-    keyword_line = keyword_line.rstrip(',')
-    keyword_line += '</dd>\n'
+        keyword_line += ' <a href="{url}">{val}</a>,'.format(
+            url=get_module_man_html_file_path(value),
+            val=value.replace(".html", "")
+        )
+    keyword_line = keyword_line.rstrip(",")
+    keyword_line += "</dd>\n"
     keywordsfile.write(keyword_line)
 keywordsfile.write("</dl>\n")
 # create toc
