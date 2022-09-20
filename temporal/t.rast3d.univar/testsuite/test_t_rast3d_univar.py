d0218b5e928fa2ba0a1097d5b83c564abd77dfef
ninsbl <stefan.blumentrath@gmx.de>
2022-09-20 22:38:26 +0200

    add test for zones

--- temporal/t.rast3d.univar/testsuite/test_t_rast3d_univar.py
+++ temporal/t.rast3d.univar/testsuite/test_t_rast3d_univar.py
@@ -19,6 +19,12 @@ class TestRasterUnivar(TestCase):
         cls.use_temp_region()
         cls.runModule("g.region", s=0, n=80, w=0, e=120, b=0, t=50, res=1, res3=1)
 
+        cls.runModule(
+            "r3.mapcalc",
+            expression="zones = int(if(row() <= 5, 1, if(row() >= 20, 3, 2)))",
+            overwrite=True,
+        )
+
         cls.runModule("r3.mapcalc", expression="a_1 = 100", overwrite=True)
         cls.runModule("r3.mapcalc", expression="a_2 = 200", overwrite=True)
         cls.runModule("r3.mapcalc", expression="a_3 = 300", overwrite=True)
@@ -48,6 +54,7 @@ class TestRasterUnivar(TestCase):
     def tearDownClass(cls):
         """Remove the temporary region"""
         cls.runModule("t.remove", flags="df", type="str3ds", inputs="A")
+        cls.runModule("g.remove", flags="f", type="raster_3d", name="zones")
         cls.del_temp_region()
 
     def test_1(self):
@@ -161,6 +168,45 @@ a_4@testing|2001-10-01 00:00:00|2002-01-01 00:00:00|400|400|400|400|0|0|0|192000
     def test_6_error_handling_no_input(self):
         # No input
         self.assertModuleFail("t.rast3d.univar", output="out.txt")
+
+    def test_7(self):
+        """Test use of zones"""
+
+        t_rast_univar = SimpleModule(
+            "t.rast3d.univar",
+            input="A",
+            where="start_time >= '2001-01-01'",
+            zones="zones",
+            overwrite=True,
+            verbose=True,
+        )
+        self.runModule("g.region", res=1)
+        self.assertModule(t_rast_univar)
+
+        print(t_rast_univar.outputs.stdout.split("\n"))
+
+        univar_text = """id|start|end|zone|mean|min|max|mean_of_abs|stddev|variance|coeff_var|sum|null_cells|cells|non_null_cells
+a_1@PERMANENT|2001-01-01 00:00:00|2001-04-01 00:00:00|1|100|100|100|100|0|0|0|3000000|0|30000|30000
+a_1@PERMANENT|2001-01-01 00:00:00|2001-04-01 00:00:00|2|100|100|100|100|0|0|0|8400000|0|84000|84000
+a_1@PERMANENT|2001-01-01 00:00:00|2001-04-01 00:00:00|3|100|100|100|100|0|0|0|36600000|0|366000|366000
+a_2@PERMANENT|2001-04-01 00:00:00|2001-07-01 00:00:00|1|200|200|200|200|0|0|0|6000000|0|30000|30000
+a_2@PERMANENT|2001-04-01 00:00:00|2001-07-01 00:00:00|2|200|200|200|200|0|0|0|16800000|0|84000|84000
+a_2@PERMANENT|2001-04-01 00:00:00|2001-07-01 00:00:00|3|200|200|200|200|0|0|0|73200000|0|366000|366000
+a_3@PERMANENT|2001-07-01 00:00:00|2001-10-01 00:00:00|1|300|300|300|300|0|0|0|9000000|0|30000|30000
+a_3@PERMANENT|2001-07-01 00:00:00|2001-10-01 00:00:00|2|300|300|300|300|0|0|0|25200000|0|84000|84000
+a_3@PERMANENT|2001-07-01 00:00:00|2001-10-01 00:00:00|3|300|300|300|300|0|0|0|109800000|0|366000|366000
+a_4@PERMANENT|2001-10-01 00:00:00|2002-01-01 00:00:00|1|400|400|400|400|0|0|0|12000000|0|30000|30000
+a_4@PERMANENT|2001-10-01 00:00:00|2002-01-01 00:00:00|2|400|400|400|400|0|0|0|33600000|0|84000|84000
+a_4@PERMANENT|2001-10-01 00:00:00|2002-01-01 00:00:00|3|400|400|400|400|0|0|0|146400000|0|366000|366000
+"""
+
+        for ref, res in zip(
+            univar_text.split("\n"), t_rast_univar.outputs.stdout.split("\n")
+        ):
+            if ref and res:
+                ref_line = ref.split("|", 1)[1]
+                res_line = res.split("|", 1)[1]
+                self.assertLooksLike(ref_line, res_line)
 
 
 if __name__ == "__main__":
