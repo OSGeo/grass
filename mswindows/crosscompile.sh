7a5ad84f7d6c351ae8c4e7b84c1e28942cd32fd4
Francisco J. Solis <siscomagma@gmail.com>
2022-10-09 10:21:46 -0500

    configure: remove unused wxwidgets compilation configuration option (#2593)
    The configuration option wxwidget has been removed
    
    Refs: #2578

--- mswindows/crosscompile.sh
+++ mswindows/crosscompile.sh
@@ -131,7 +131,6 @@ LDFLAGS="-lcurses" \
 ./configure \
 --with-nls \
 --with-readline \
---with-wxwidgets \
 --with-freetype-includes=$freetype_include \
 --with-bzlib \
 --with-postgres \
@@ -189,7 +188,6 @@ PKG_CONFIG=$mxe_bin-pkg-config \
 --host=$arch \
 --with-nls \
 --with-readline \
---with-wxwidgets \
 --with-freetype-includes=$mxe_shared/include/freetype2 \
 --with-bzlib \
 --with-postgres \
