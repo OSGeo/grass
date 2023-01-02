7dd1daebc6cded4277c733037527fcc598c1c933
Markus Neteler <neteler@osgeo.org>
2023-01-02 11:45:54 +0100

    update year
    Co-authored-by: Edouard Choinière <27212526+echoix@users.noreply.github.com>

--- cmake/modules/build_program_in_subdir.cmake
+++ cmake/modules/build_program_in_subdir.cmake
@@ -1,7 +1,7 @@
 # AUTHOR(S): Rashad Kanavath <rashad km gmail>
 # PURPOSE: 	 CMake macro to build a grass executable (program) under sub directory
 #            which is passed as argument to macro
-# COPYRIGHT: (C) 2000 by the GRASS Development Team
+# COPYRIGHT: (C) 2020 by the GRASS Development Team
 #   	    	 This program is free software under the GPL (>=v2)
 #   	    	 Read the file COPYING that comes with GRASS for details.
 
