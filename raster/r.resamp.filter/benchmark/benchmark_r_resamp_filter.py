c5924d6dc9c236cd6f2d6523440df9a1aefbb250
Anna Petrasova <kratochanna@gmail.com>
2023-01-03 13:59:41 -0500

    add banchmark, performance section to manual page, improve benchmark

--- raster/r.resamp.filter/benchmark/benchmark_r_resamp_filter.py
+++ raster/r.resamp.filter/benchmark/benchmark_r_resamp_filter.py
@@ -19,7 +19,7 @@ def main():
     benchmark(14142, "r.resamp.filter_200M", results)
     benchmark(20000, "r.resamp.filter_400M", results)
 
-    bm.nprocs_plot(results)
+    bm.nprocs_plot(results, filename="benchmark.svg")
 
 
 def benchmark(size, label, results):
@@ -27,24 +27,25 @@ def benchmark(size, label, results):
     output = "benchmark_r_resamp_filter_nprocs"
 
     generate_map(rows=size, cols=size, fname=reference)
+    Module("g.region", flags="pa", s=0, n=size, w=0, e=size, res=1)
     module = Module(
         "r.resamp.filter",
         input=reference,
-        filter=["lanczos3", "sinc"],
+        filter=["gauss", "box"],
         output=output,
-        radius=[10, 10],
+        radius=[6, 9],
         memory=500,
         run_=False,
         stdout_=DEVNULL,
         overwrite=True,
     )
-    results.append(bm.benchmark_nprocs(module, label=label, max_nprocs=16, repeat=3))
+    results.append(bm.benchmark_nprocs(module, label=label, max_nprocs=8, repeat=3))
     Module("g.remove", quiet=True, flags="f", type="raster", name=reference)
     Module("g.remove", quiet=True, flags="f", type="raster", name=output)
 
 
 def generate_map(rows, cols, fname):
-    Module("g.region", flags="p", s=0, n=rows, w=0, e=cols, res=1)
+    Module("g.region", flags="pa", s=0, n=rows, w=0, e=cols, res=3)
     # Generate using r.random.surface if r.surf.fractal fails
     try:
         print("Generating reference map using r.surf.fractal...")
