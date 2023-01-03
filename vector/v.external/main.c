95e195424ce24c69d4e20b39bca5eab6b9412ecc
Nicklas Larsson <n_larsson@yahoo.com>
2023-01-03 20:28:45 +0100

    fix -Wstrict-prototypes warnings in vector

--- vector/v.external/main.c
+++ vector/v.external/main.c
@@ -95,7 +95,7 @@ int main(int argc, char *argv[])
 
     if (flags.format->answer) {
         /* list formats */
-        list_formats(stdout);
+        list_formats();
         exit(EXIT_SUCCESS);
     }
 
