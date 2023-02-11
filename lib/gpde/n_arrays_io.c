f6b8ac9a2274c335819d40cd9c72dd484f7d861e
Brad ReDacted <brad.redacted@outlook.com>
2023-02-11 13:53:13 -0800

    libgpde: fix error handing parameters (#2596)
    Co-authored-by: Nicklas Larsson <n_larsson@yahoo.com>

--- lib/gpde/n_arrays_io.c
+++ lib/gpde/n_arrays_io.c
@@ -353,7 +353,7 @@ N_array_3d *N_read_rast3d_to_array_3d(char *name, N_array_3d *array, int mask)
 
     /* Close files and exit */
     if (!Rast3d_close(map))
-        Rast3d_fatal_error(map, NULL, 0, _("Error closing g3d file"));
+        Rast3d_fatal_error(_("Error closing g3d file <%s>"), name);
 
     return data;
 }
@@ -454,7 +454,7 @@ void N_write_array_3d_to_rast3d(N_array_3d *array, char *name, int mask)
         Rast3d_fatal_error("Error flushing tiles with Rast3d_flush_all_tiles");
     /* Close files and exit */
     if (!Rast3d_close(map))
-        Rast3d_fatal_error(map, NULL, 0, _("Error closing g3d file"));
+        Rast3d_fatal_error(_("Error closing g3d file <%s>"), name);
 
     return;
 }
