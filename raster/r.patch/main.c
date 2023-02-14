4acec22336d90cae3d955424e141e1e81810ee0d
Anna Petrasova <kratochanna@gmail.com>
2023-02-14 14:01:38 -0500

    fix formatting

--- raster/r.patch/main.c
+++ raster/r.patch/main.c
@@ -113,10 +113,10 @@ int main(int argc, char *argv[])
                     "threads setting."));
     nprocs = 1;
 #endif
-     if (nprocs > 1 && G_find_raster("MASK", G_mapset()) != NULL) {
-         G_warning(_("Parallel processing disabled due to active MASK."));
-         nprocs = 1;
-     }
+    if (nprocs > 1 && G_find_raster("MASK", G_mapset()) != NULL) {
+        G_warning(_("Parallel processing disabled due to active MASK."));
+        nprocs = 1;
+    }
 
     use_zero = (zeroflag->answer);
     no_support = (nosupportflag->answer);
