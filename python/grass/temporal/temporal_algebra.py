eb1a90028901e5e28defafe4955d34f6f52750ae
ninsbl <stefan.blumentrath@gmx.de>
2023-02-21 13:45:54 +0100

    linting

--- python/grass/temporal/temporal_algebra.py
+++ python/grass/temporal/temporal_algebra.py
@@ -3336,20 +3336,20 @@ class TemporalAlgebraParser(object):
         """
         if self.run:
             # Check input stds.
-            maplist = self.check_stds(t[3])
-            resultlist = []
+            map_list = self.check_stds(t[3])
+            result_list = []
 
             # Get increment format.
             t_neighbour = int(t[5])
 
-            max_index = len(maplist)
-            for map_i in maplist:
+            max_index = len(map_list)
+            for map_i in map_list:
                 # Get map index and temporal extent.
-                map_index = maplist.index(map_i)
+                map_index = map_list.index(map_i)
                 new_index = map_index + t_neighbour
                 if new_index < max_index and new_index >= 0:
                     # Get neighbouring map and set temporal extent.
-                    map_n = maplist[new_index]
+                    map_n = map_list[new_index]
                     map_i_t_extent = map_i.get_temporal_extent()
                     # Create r.mapcalc expression string for the operation.
                     tfuncdict = self.get_temporal_func_dict(map_n)
@@ -3360,12 +3360,12 @@ class TemporalAlgebraParser(object):
                     map_new.cmd_list = str(tfuncdict[t[1].upper()])
 
                     # Append map with updated command list to result list.
-                    resultlist.append(map_new)
+                    result_list.append(map_new)
 
-            t[0] = resultlist
+            t[0] = result_list
 
         if self.debug:
-            print(t[0], t[1], t[2], t[3])
+            print(str(t[3]), "* = ", str(t[0]), "(", str(t[3]), ",", str(t[4]), ")")
 
     def p_error(self, t):
         # Handle errors.
