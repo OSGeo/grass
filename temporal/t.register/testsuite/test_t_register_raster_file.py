8d736bbfd8685702ac025aab9c2fa1c79b651ef3
ninsbl <stefan.blumentrath@gmx.de>
2023-03-02 00:10:06 +0100

    check register result

--- temporal/t.register/testsuite/test_t_register_raster_file.py
+++ temporal/t.register/testsuite/test_t_register_raster_file.py
@@ -21,7 +21,10 @@ The region setting should work for UTM and LL test locations
 
 """
 
+from datetime import datetime
+
 import grass.script as gs
+import grass.temporal as tgis
 
 from grass.gunittest.case import TestCase
 from grass.gunittest.gmodules import SimpleModule
@@ -31,6 +34,9 @@ class TestRasterUnivar(TestCase):
     @classmethod
     def setUpClass(cls):
         """Initiate the temporal GIS and set the region"""
+
+        tgis.init()
+        # cls.mapset = gs.gisenv()["MAPSET"]
         cls.use_temp_region()
         cls.runModule(
             "g.region", s=0, n=80, w=0, e=120, b=0, t=50, res=10, res3=10, flags="p3"
@@ -89,8 +95,32 @@ class TestRasterUnivar(TestCase):
         )
         self.assertModule(register_module)
 
-        # maplist = SimpleModule("t.rast.list",
-        # input="precip_abs8")
+        strds = tgis.open_old_stds("precip_abs8", type="strds")
+
+        self.assertEqual(strds.metadata.get_number_of_maps(), 6)
+        start, end = strds.get_absolute_time()
+        self.assertEqual(start, datetime(2001, 1, 1))
+        self.assertEqual(end, datetime(2001, 6, 1))
+        self.assertEqual(strds.check_temporal_topology(), True)
+        self.assertEqual(strds.get_granularity(), "1 month")
+
+        print(gs.read_command("t.info", type="strds", input="precip_abs8"))
+        t_rast_list = SimpleModule("t.rast.list", input="precip_abs8")
+        t_rast_list.run()
+
+        # Check registered raster maps
+        ref_str = "...".join(
+            [
+                "name|mapset|start_time|end_time",
+                "prec_1|...|2001-01-01 00:00:00|None",
+                "prec_2|...|2001-02-01 00:00:00|None",
+                "prec_3|...|2001-03-01 00:00:00|None",
+                "prec_4|...|2001-04-01 00:00:00|None",
+                "prec_5|...|2001-05-01 00:00:00|None",
+                "prec_6|...|2001-06-01 00:00:00|None",
+            ]
+        )
+        self.assertLooksLike(str(t_rast_list.outputs.stdout), ref_str)
 
     def test_with_file_and_no_increment(self):
 
@@ -108,8 +138,78 @@ class TestRasterUnivar(TestCase):
         )
         self.assertModule(register_module)
 
-        # t.info type=strds input=precip_abs8
-        # t.rast.list input=precip_abs8
+        strds = tgis.open_old_stds("precip_abs8", type="strds")
+
+        self.assertEqual(strds.metadata.get_number_of_maps(), 6)
+        start, end = strds.get_absolute_time()
+        self.assertEqual(start, datetime(2001, 1, 1))
+        self.assertEqual(end, datetime(2001, 1, 1))
+        self.assertEqual(strds.check_temporal_topology(), False)
+        self.assertEqual(strds.get_granularity(), None)
+
+        print(gs.read_command("t.info", type="strds", input="precip_abs8"))
+        t_rast_list = SimpleModule("t.rast.list", input="precip_abs8")
+        t_rast_list.run()
+
+        # Check registered raster maps
+        ref_str = "...".join(
+            [
+                "name|mapset|start_time|end_time",
+                "prec_1|...|2001-01-01 00:00:00|None",
+                "prec_2|...|2001-01-01 00:00:00|None",
+                "prec_3|...|2001-01-01 00:00:00|None",
+                "prec_4|...|2001-01-01 00:00:00|None",
+                "prec_5|...|2001-01-01 00:00:00|None",
+                "prec_6|...|2001-01-01 00:00:00|None",
+            ]
+        )
+        self.assertLooksLike(str(t_rast_list.outputs.stdout), ref_str)
+
+    def test_with_file_increment_and_intervall(self):
+
+        tmp_file = gs.tempfile()
+        with open(tmp_file, "w") as register_file:
+            register_file.write("prec_1\nprec_2\nprec_3\nprec_4\nprec_5\nprec_6")
+
+        register_module = SimpleModule(
+            "t.register",
+            flags="i",
+            input="precip_abs8",
+            start="2001-01-01",
+            increment="1 months",
+            file=tmp_file,
+            overwrite=True,
+            verbose=True,
+        )
+        self.assertModule(register_module)
+
+        print(gs.read_command("t.info", type="strds", input="precip_abs8"))
+        strds = tgis.open_old_stds("precip_abs8", type="strds")
+
+        self.assertEqual(strds.metadata.get_number_of_maps(), 6)
+        start, end = strds.get_absolute_time()
+        self.assertEqual(start, datetime(2001, 1, 1))
+        self.assertEqual(end, datetime(2001, 7, 1))
+        self.assertEqual(strds.check_temporal_topology(), True)
+        self.assertEqual(strds.get_granularity(), "1 month")
+
+        print(gs.read_command("t.info", type="strds", input="precip_abs8"))
+        t_rast_list = SimpleModule("t.rast.list", input="precip_abs8")
+        t_rast_list.run()
+
+        # Check registered raster maps
+        ref_str = "\n".join(
+            [
+                "name|mapset|start_time|end_time",
+                "prec_1|...|2001-01-01 00:00:00|2001-02-01 00:00:00",
+                "prec_2|...|2001-02-01 00:00:00|2001-03-01 00:00:00",
+                "prec_3|...|2001-03-01 00:00:00|2001-04-01 00:00:00",
+                "prec_4|...|2001-04-01 00:00:00|2001-05-01 00:00:00",
+                "prec_5|...|2001-05-01 00:00:00|2001-06-01 00:00:00",
+                "prec_6|...|2001-06-01 00:00:00|2001-07-01 00:00:00",
+            ]
+        )
+        self.assertLooksLike(str(t_rast_list.outputs.stdout), ref_str)
 
     def test_with_start_in_file(self):
 
@@ -137,8 +237,32 @@ class TestRasterUnivar(TestCase):
         )
         self.assertModule(register_module)
 
-        # t.info type=strds input=precip_abs8
-        # t.rast.list input=precip_abs8
+        strds = tgis.open_old_stds("precip_abs8", type="strds")
+
+        self.assertEqual(strds.metadata.get_number_of_maps(), 6)
+        start, end = strds.get_absolute_time()
+        self.assertEqual(start, datetime(2001, 1, 1))
+        self.assertEqual(end, datetime(2001, 6, 1))
+        self.assertEqual(strds.check_temporal_topology(), True)
+        self.assertEqual(strds.get_granularity(), "1 month")
+
+        print(gs.read_command("t.info", type="strds", input="precip_abs8"))
+        t_rast_list = SimpleModule("t.rast.list", input="precip_abs8")
+        t_rast_list.run()
+
+        # Check registered raster maps
+        ref_str = "...".join(
+            [
+                "name|mapset|start_time|end_time",
+                "prec_1|...|2001-01-01 00:00:00|None",
+                "prec_2|...|2001-02-01 00:00:00|None",
+                "prec_3|...|2001-03-01 00:00:00|None",
+                "prec_4|...|2001-04-01 00:00:00|None",
+                "prec_5|...|2001-05-01 00:00:00|None",
+                "prec_6|...|2001-06-01 00:00:00|None",
+            ]
+        )
+        self.assertLooksLike(str(t_rast_list.outputs.stdout), ref_str)
 
     def test_with_start_in_file_and_increment(self):
 
@@ -167,8 +291,8 @@ class TestRasterUnivar(TestCase):
         )
         self.assertModuleFail(register_module)
 
-        # t.info type=strds input=precip_abs8
-        # t.rast.list input=precip_abs8
+        # print(gs.read_command("t.info", type="strds", input="precip_abs8"))
+        # print(gs.read_command("t.rast.list", input="precip_abs8"))
 
     def test_with_start_and_end_in_file_and_interval(self):
 
@@ -197,8 +321,8 @@ class TestRasterUnivar(TestCase):
         )
         self.assertModuleFail(register_module)
 
-        # t.info type=strds input=precip_abs8
-        # t.rast.list input=precip_abs8
+        # print(gs.read_command("t.info", type="strds", input="precip_abs8"))
+        # print(gs.read_command("t.rast.list", input="precip_abs8"))
 
     def test_with_mapset_and_semantic_label(self):
 
@@ -220,16 +344,39 @@ class TestRasterUnivar(TestCase):
 
         register_module = SimpleModule(
             "t.register",
-            flags="i",
             input="precip_abs8",
             file=tmp_file,
             overwrite=True,
             verbose=True,
         )
-        self.assertModuleFail(register_module)
+        self.assertModule(register_module)
 
-        # t.info type=strds input=precip_abs8
-        # t.rast.list input=precip_abs8
+        strds = tgis.open_old_stds("precip_abs8", type="strds")
+
+        self.assertEqual(strds.metadata.get_number_of_maps(), 6)
+        start, end = strds.get_absolute_time()
+        self.assertEqual(start, datetime(2001, 1, 1))
+        self.assertEqual(end, datetime(2002, 7, 1))
+        self.assertEqual(strds.check_temporal_topology(), True)
+        self.assertEqual(strds.get_granularity(), "3 months")
+
+        print(gs.read_command("t.info", type="strds", input="precip_abs8"))
+        t_rast_list = SimpleModule("t.rast.list", input="precip_abs8")
+        t_rast_list.run()
+
+        # Check registered raster maps
+        ref_str = "...".join(
+            [
+                "name|mapset|start_time|end_time",
+                "prec_1|...|2001-01-01 00:00:00|2001-04-01 00:00:00",
+                "prec_2|...|2001-04-01 00:00:00|2001-07-01 00:00:00",
+                "prec_3|...|2001-07-01 00:00:00|2001-10-01 00:00:00",
+                "prec_4|...|2001-10-01 00:00:00|2002-01-01 00:00:00",
+                "prec_5|...|2002-01-01 00:00:00|2002-04-01 00:00:00",
+                "prec_6|...|2002-04-01 00:00:00|2002-07-01 00:00:00",
+            ]
+        )
+        self.assertLooksLike(str(t_rast_list.outputs.stdout), ref_str)
 
 
 if __name__ == "__main__":
