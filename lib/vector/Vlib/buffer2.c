785ab26638ab8ef47f2831129f9e0667e42bc220
Nicklas Larsson <n_larsson@yahoo.com>
2023-03-11 12:47:28 +0100

    update fix of buffer2.c

--- lib/vector/Vlib/buffer2.c
+++ lib/vector/Vlib/buffer2.c
@@ -1023,9 +1023,11 @@ void Vect_line_buffer2(const struct line_pnts *Points, double da, double db,
 
     Vect_line_prune((struct line_pnts *)Points);
 
-    if (Points->n_points == 1)
-        return; /* Vect_point_buffer2(Points->x[0], Points->y[0], da, db,
-                   dalpha, round, tol, oPoints)*/
+    if (Points->n_points == 1) {
+        Vect_point_buffer2(Points->x[0], Points->y[0], da, db, dalpha, round,
+                           tol, oPoints);
+        return;
+    }
 
     /* initializations */
     tPoints = Vect_new_line_struct();
