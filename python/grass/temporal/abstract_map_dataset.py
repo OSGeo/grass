cb41621cf37e680e2b557ecf7bbd692844165978
ninsbl <stefan.blumentrath@gmx.de>
2023-03-18 23:37:53 +0100

    split function

--- python/grass/temporal/abstract_map_dataset.py
+++ python/grass/temporal/abstract_map_dataset.py
@@ -166,26 +166,19 @@ class AbstractMapDataset(AbstractDataset):
         return self.base.get_map_id()
 
     @staticmethod
-    def build_id(name, mapset, layer=None, check_mapset_element=None):
-        """Convenient method to build the unique identifier
-
-        Existing layer and mapset definitions in the name
-        string will be reused
-
-        If check_mapset_element is given, but no mapset, the function will
-        try to get the correct mapset by looking for an element of the
-        given element type with the given name. If the combination is
-        not found on the current search path, it will fail and throw an error.
+    def split_name(name, layer=None, mapset=None):
+        """Convenient method to split a map name into three potentially
+        contained parts: map name, map layer and mapset. For the layer and
+        mapset, default keyword arguments can be given if not present in
+        the name. Layer and mapset present in the name will overwrite
+        the keyword arguments.
 
         :param name: The name of the map
-        :param mapset: The mapset in which the map is located
         :param layer: The layer of the vector map, use None in case no
                       layer exists
-        :param check_mapset_element: A mapset element type to be passed
-                                     to g.findfile, e.g. "cell", "vector"
-                                     "raster3d"
+        :param mapset: The mapset in which the map is located
 
-        :return: the id of the map as "name(:layer)@mapset" while layer is
+        :return: tuple of three elements name, layer, mapset e(:layer)@mapset" while layer is
                  optional
         """
 
@@ -197,16 +190,69 @@ class AbstractMapDataset(AbstractDataset):
         if name.find(":") >= 0:
             name, layer = name.split(":")[0:2]
 
-        if check_mapset_element and not mapset:
-            result = gs.find_file(element=check_mapset_element, name=name)
+        return name, layer, mapset
+
+    @staticmethod
+    def build_id_from_search_path(name, element):
+        """Convenient method to build the unique identifier while
+        checking the current seach path for the correct mapset.
+
+        Existing mapset definitions in the name string will be reused.
+
+        If an element type is given and the mapset is not specified in
+        the name, the function will try to get the correct mapset by
+        searching for a map with the given name and of the given element
+        type on the current search path. If the combination is not found
+        on the current search path, it will fail and throw an error.
+
+        :param name: The name of the map
+        :param element: A mapset element type to be passed to g.findfile,
+                        e.g. "cell", "vector", "raster3d"
+
+        :return: the id of the map as "name(:layer)@mapset" where layer is
+                 optional
+        """
+
+        # Split given name into relevant parts
+        name, layer, mapset = AbstractMapDataset.split_name(name)
+
+        # Identify mapset of map with the given name of given element type
+        if element and not mapset:
+            result = gs.find_file(element=element, name=name)
             if result["mapset"]:
                 mapset = result["mapset"]
             else:
                 gs.fatal(
-                    _("{type} map <{map_name}> not found on search path").format(
-                        type=check_mapset_element, map_name=name
-                    )
+                    _(
+                        "Map <{map_name}> of element tpye '{element}' not found on search path"
+                    ).format(element=element, map_name=name)
                 )
+
+        if layer is not None:
+            return f"{name}:{layer}@{mapset}"
+        else:
+            return f"{name}@{mapset}"
+
+    @staticmethod
+    def build_id(name, mapset, layer=None):
+        """Convenient method to build the unique identifier
+
+        Existing layer and mapset definitions in the name
+        string will be reused
+
+        :param name: The name of the map
+        :param mapset: The mapset in which the map is located
+        :param layer: The layer of the vector map, use None in case no
+                      layer exists
+
+        :return: the id of the map as "name(:layer)@mapset" where layer is
+                 optional
+        """
+
+        # Split given name into relevant parts
+        name, layer, mapset = AbstractMapDataset.split_name(
+            name, layer=layer, mapset=mapset
+        )
 
         if layer is not None:
             return f"{name}:{layer}@{mapset}"
