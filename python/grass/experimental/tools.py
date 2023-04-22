82f5894cf324700e9e7642f4e49cfaedd587d8e8
Vaclav Petras <wenzeslaus@gmail.com>
2023-04-22 14:33:29 -0400

    Allow to specify stdin and use a new instance of Tools itself to execute with that stdin

--- python/grass/experimental/tools.py
+++ python/grass/experimental/tools.py
@@ -9,7 +9,10 @@ class ExecutedTool:
     def __init__(self, name, kwargs, stdout, stderr):
         self._name = name
         self._stdout = stdout
-        self._decoded_stdout = gs.decode(self._stdout)
+        if self._stdout:
+            self._decoded_stdout = gs.decode(self._stdout)
+        else:
+            self._decoded_stdout = ""
 
     @property
     def text(self):
@@ -45,8 +48,14 @@ class SubExecutor:
     """use as tools().params(a="x", b="y").g_region()"""
 
     # a and b would be overwrite or stdin
-
     # Can support other envs or all PIPE and encoding read command supports
+    def __init__(self, *, tools, env, stdin=None):
+        self._tools = tools
+        self._env = env
+        self._stdin = stdin
+
+    def run(self, name, /, **kwargs):
+        pass
 
 
 class Tools:
@@ -60,6 +69,7 @@ class Tools:
         verbose=False,
         superquiet=False,
         freeze_region=False,
+        stdin=None,
     ):
         # TODO: fix region, so that external g.region call in the middle
         # is not a problem
@@ -85,6 +95,7 @@ class Tools:
             self._env["GRASS_VERBOSE"] = "1"
         if verbose:
             self._env["GRASS_VERBOSE"] = "3"
+        self._set_stdin(stdin)
 
     # These could be public, not protected.
     def _freeze_region(self):
@@ -93,6 +104,10 @@ class Tools:
 
     def _overwrite(self):
         self._env["GRASS_OVERWRITE"] = "1"
+
+    def _set_stdin(self, stdin, /):
+        print("_set_stdin", stdin)
+        self._stdin = stdin
 
     @property
     def env(self):
@@ -107,10 +122,23 @@ class Tools:
         :param str module: name of GRASS module
         :param `**kwargs`: named arguments passed to run_command()"""
         # alternatively use dev null as default or provide it as convenient settings
-        kwargs["stdout"] = gs.PIPE
-        kwargs["stderr"] = gs.PIPE
-        process = gs.pipe_command(name, env=self._env, **kwargs)
-        stdout, stderr = process.communicate()
+        stdout_pipe = gs.PIPE
+        stderr_pipe = gs.PIPE
+        if self._stdin:
+            stdin_pipe = gs.PIPE
+            stdin = gs.utils.encode(self._stdin)
+        else:
+            stdin_pipe = None
+            stdin = None
+        process = gs.start_command(
+            name,
+            env=self._env,
+            **kwargs,
+            stdin=stdin_pipe,
+            stdout=stdout_pipe,
+            stderr=stderr_pipe,
+        )
+        stdout, stderr = process.communicate(input=stdin)
         stderr = gs.utils.decode(stderr)
         returncode = process.poll()
         # TODO: instead of printing, do exception right away
@@ -131,6 +159,11 @@ class Tools:
                 sys.stderr.write(stderr)
             gs.handle_errors(returncode, stdout, [name], kwargs)
         return ExecutedTool(name=name, kwargs=kwargs, stdout=stdout, stderr=stderr)
+        # executor = SubExecutor(tools=self, env=self._env, stdin=self._stdin)
+        # return executor.run(name, **kwargs)
+
+    def feed_input_to(self, stdin, /):
+        return Tools(env=self._env, stdin=stdin)
 
     def __getattr__(self, name):
         """Parse attribute to GRASS display module. Attribute should be in
@@ -192,8 +225,27 @@ def _test():
     tools.g_region(res=500)  # we would do this for another computation elsewhere
     print(independent_computation.g_region(flags="g").keyval["ewres"])
 
-    tools_pro = Tools(session=session, freeze_region=True, superquiet=True)
+    tools_pro = Tools(
+        session=session, freeze_region=True, overwrite=True, superquiet=True
+    )
+    # gs.feed_command("v.in.ascii",
+    #    input="-", output="point", separator=",",
+    #    stdin="13.45,29.96,200", overwrite=True)
     tools_pro.r_slope_aspect(elevation="elevation", slope="slope")
+    tools_pro.feed_input_to("13.45,29.96,200").v_in_ascii(
+        input="-", output="point", separator=","
+    )
+    print(tools_pro.v_info(map="point", flags="t").keyval["points"])
+
+    # try:
+    tools_pro.feed_input_to("13.45,29.96,200").v_in_ascii(
+        input="-",
+        output="point",
+        format="xstandard",
+    )
+    # except gs.CalledModuleError as error:
+    #    print("Exception text:")
+    #    print(error)
 
 
 if __name__ == "__main__":
