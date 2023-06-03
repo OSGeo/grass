5921f71929051944b8bae05b1505c4bddd30d831
Loïc Bartoletti <loic.bartoletti@oslandia.com>
2023-06-03 09:44:26 +0200

    v.out.ogr/create.c: remove GDAL < 2.2 code

--- vector/v.out.ogr/create.c
+++ vector/v.out.ogr/create.c
@@ -20,11 +20,7 @@ void create_ogr_layer(const char *dsn, const char *format, const char *layer,
     }
 
     /* create datasource */
-#if GDAL_VERSION_NUM >= 2020000
     hDS = GDALCreate(hDriver, dsn, 0, 0, 0, GDT_Unknown, papszDSCO);
-#else
-    hDS = OGR_Dr_CreateDataSource(hDriver, dsn, papszDSCO);
-#endif
     if (hDS == NULL) {
         G_fatal_error(_("Creation of output OGR datasource <%s> failed"), dsn);
     }
@@ -33,11 +29,7 @@ void create_ogr_layer(const char *dsn, const char *format, const char *layer,
 
     /* create layer */
     /* todo: SRS */
-#if GDAL_VERSION_NUM >= 2020000
     hLayer = GDALDatasetCreateLayer(hDS, layer, NULL, wkbtype, papszLCO);
-#else
-    hLayer = OGR_DS_CreateLayer(hDS, layer, NULL, wkbtype, papszLCO);
-#endif
     if (hLayer == NULL) {
         G_fatal_error(_("Creation of OGR layer <%s> failed"), layer);
     }
