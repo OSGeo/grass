befd0370f1bb443ab73befccf248a2f14f293255
Vaclav Petras <wenzeslaus@gmail.com>
2023-08-17 09:53:34 -0400

    Update usage of g.mapset to use project. Remove all caps MAPSET and LOCATION from g.mapset documentation.

--- gui/wxpython/core/utils.py
+++ gui/wxpython/core/utils.py
@@ -601,7 +601,7 @@ def GetListOfMapsets(dbase, location, selectable=False):
 
     if selectable:
         ret = RunCommand(
-            "g.mapset", read=True, flags="l", location=location, dbase=dbase
+            "g.mapset", read=True, flags="l", project=location, dbase=dbase
         )
 
         if not ret:
