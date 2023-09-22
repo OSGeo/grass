137cae05a705ed73ac66e0708645abaae1c9b00e
Tomas Zigo <50632337+tmszi@users.noreply.github.com>
2023-09-22 05:21:25 +0200

    wxGUI/iclass: fix showing scatter plots (#3169)
    Compatible with Matplotlib v3.

--- gui/wxpython/iscatt/plots.py
+++ gui/wxpython/iscatt/plots.py
@@ -1019,8 +1019,7 @@ def imshow(
     @author: Chris Beaumont <beaumont@hawaii.edu>
     """
 
-    if not axes._hold:
-        axes.cla()
+    axes.cla()
     if norm is not None:
         assert isinstance(norm, mcolors.Normalize)
     if aspect is None:
@@ -1069,7 +1068,7 @@ def imshow(
     # to tightly fit the image, regardless of dataLim.
     im.set_extent(im.get_extent())
 
-    axes.images.append(im)
+    axes.add_image(im)
     im._remove_method = lambda h: axes.images.remove(h)
 
     return im
