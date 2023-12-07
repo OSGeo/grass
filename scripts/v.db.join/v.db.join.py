9937208f056360fac841f6dd9019d4ec6d2b5ac7
Guido Riembauer <riembauer@mundialis.de>
2023-12-07 10:11:18 +0100

    use chunks

--- scripts/v.db.join/v.db.join.py
+++ scripts/v.db.join/v.db.join.py
@@ -103,19 +103,27 @@ def main():
 
     if not maptable:
         grass.fatal(
-            _("There is no table connected to this map. Unable to join any column.")
+            _(
+                "There is no table connected to this map. Unable to join any column."
+            )
         )
 
     # check if column is in map table
     if column not in grass.vector_columns(map, layer):
-        grass.fatal(_("Column <%s> not found in table <%s>") % (column, maptable))
+        grass.fatal(
+            _("Column <%s> not found in table <%s>") % (column, maptable)
+        )
 
     # describe other table
-    all_cols_ot = grass.db_describe(otable, driver=driver, database=database)["cols"]
+    all_cols_ot = grass.db_describe(otable, driver=driver, database=database)[
+        "cols"
+    ]
 
     # check if ocolumn is on other table
     if ocolumn not in [ocol[0] for ocol in all_cols_ot]:
-        grass.fatal(_("Column <%s> not found in table <%s>") % (ocolumn, otable))
+        grass.fatal(
+            _("Column <%s> not found in table <%s>") % (ocolumn, otable)
+        )
 
     # determine columns subset from other table
     if not scolumns:
@@ -132,11 +140,15 @@ def main():
                     cols_to_add.append(col_ot)
                     break
             if not found:
-                grass.warning(_("Column <%s> not found in table <%s>") % (scol, otable))
+                grass.warning(
+                    _("Column <%s> not found in table <%s>") % (scol, otable)
+                )
 
     # exclude columns from other table
     if ecolumns:
-        cols_to_add = list(filter(lambda col: col[0] not in ecolumns, cols_to_add))
+        cols_to_add = list(
+            filter(lambda col: col[0] not in ecolumns, cols_to_add)
+        )
 
     all_cols_tt = grass.vector_columns(map, int(layer)).keys()
     # This is used for testing presence (and potential name conflict) with
@@ -144,69 +156,68 @@ def main():
     # is SQL, so we lowercase the names here and in the test.
     all_cols_tt = [name.lower() for name in all_cols_tt]
 
-    # first create new columns in one v.db.addcolumn command
-    cols_added = []
-    for col in cols_to_add:
-        # skip the vector column which is used for join
-        colname = col[0]
-        if colname == column:
-            continue
+    # loop over chunks of columns instead of single columns
+    chunk_size = 500
+    chunked_cols = list(
+        cols_to_add[pos : pos + chunk_size]
+        for pos in range(0, len(cols_to_add), chunk_size)
+    )
+    for t, chunk in enumerate(chunked_cols):
+        last_idx = len(chunk) - 1
+        col_add_str = ""
+        cols_added = []
+        for i, col in enumerate(chunk):
+            # skip the vector column which is used for join
+            colname = col[0]
+            if colname == column:
+                continue
+            use_len = False
+            if len(col) > 2:
+                use_len = True
+                # Sqlite 3 does not support the precision number any more
+                if driver == "sqlite":
+                    use_len = False
+                # MySQL - expect format DOUBLE PRECISION(M,D), see #2792
+                elif driver == "mysql" and col[1] == "DOUBLE PRECISION":
+                    use_len = False
 
-        use_len = False
-        if len(col) > 2:
-            use_len = True
-            # Sqlite 3 does not support the precision number any more
-            if driver == "sqlite":
-                use_len = False
-            # MySQL - expect format DOUBLE PRECISION(M,D), see #2792
-            elif driver == "mysql" and col[1] == "DOUBLE PRECISION":
-                use_len = False
+            if use_len:
+                coltype = "%s(%s)" % (col[1], col[2])
+            else:
+                coltype = "%s" % col[1]
 
-        if use_len:
-            coltype = "%s(%s)" % (col[1], col[2])
-        else:
-            coltype = "%s" % col[1]
+            colspec = "%s %s" % (colname, coltype)
 
-        colspec = "%s %s" % (colname, coltype)
-        # add only the new column to the table
-        if colname.lower() not in all_cols_tt:
-            try:
-                grass.run_command(
-                    "v.db.addcolumn", map=map, columns=colspec, layer=layer
-                )
-            except CalledModuleError:
-                grass.fatal(_(f"Error creating column {colspec}"))
-            cols_added.append(colname)
+            # add only the new column to the table
+            if colname.lower() not in all_cols_tt:
+                col_add_str += colspec
+                if i < last_idx:
+                    col_add_str += ","
+                cols_added.append(colname)
+        cols_added_str = ",".join(cols_added)
+        try:
+            grass.run_command(
+                "v.db.addcolumn", map=map, columns=col_add_str, layer=layer
+            )
+        except CalledModuleError:
+            grass.fatal(_(f"Error creating columns {cols_added_str}"))
 
-    # loop to create a single SQL UPDATE statement
-    # break up into individual SQL statements if it gets too long
-    max_length = 10000
-    set_str_list = []
-    cols_list = []
-    cols_in_str = []
-    set_str = ""
-    last_idx = len(cols_added) - 1
-    for i, colname in enumerate(cols_added):
-        cur_set_str = (
-            f"{colname}=(SELECT {colname} FROM {otable} WHERE "
-            f"{otable}.{ocolumn}={maptable}.{column})"
-        )
-        set_str += cur_set_str
-        cols_in_str.append(colname)
-        if len(set_str) < max_length and i < last_idx:
-            set_str += ", "
-        else:
-            set_str_list.append(set_str)
-            set_str = ""
-            cols_list.append(cols_in_str)
-            cols_in_str = []
-
-    test_idx = 1
-    for set_str, cols_in_str in zip(set_str_list, cols_list):
-        cols_str = ",".join(cols_in_str)
+        # loop again to create a single SQL UPDATE statement
+        set_str = ""
+        last_idx = len(cols_added) - 1
+        for i, colname in enumerate(cols_added):
+            cur_set_str = (
+                f"{colname}=(SELECT {colname} FROM {otable} WHERE "
+                f"{otable}.{ocolumn}={maptable}.{column})"
+            )
+            set_str += cur_set_str
+            if i < last_idx:
+                set_str += ","
         update_str = f"UPDATE {maptable} SET {set_str};"
         grass.debug(update_str, 1)
-        grass.verbose(_(f"Updating columns {cols_str} of vector map {map}..."))
+        grass.verbose(
+            _(f"Updating columns {cols_added_str} of vector map {map}...")
+        )
         try:
             grass.write_command(
                 "db.execute",
@@ -216,9 +227,8 @@ def main():
                 driver=driver,
             )
         except CalledModuleError:
-            grass.fatal(_(f"Error filling columns {cols_str}"))
+            grass.fatal(_(f"Error filling columns {cols_added_str}"))
 
-        test_idx += 1
     # write cmd history
     grass.vector_history(map)
 
