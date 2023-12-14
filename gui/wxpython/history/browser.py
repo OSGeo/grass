e58999eedb07bcaf37085042ab0a218157325b59
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-12-14 18:52:30 +0100

    wxGUI/history: fix execute r/r3.mapcalc module command

--- gui/wxpython/history/browser.py
+++ gui/wxpython/history/browser.py
@@ -20,6 +20,7 @@ import re
 
 from core import globalvar
 from core.gcmd import GError, GException
+from core.utils import split
 from gui_core.forms import GUI
 from gui_core.treeview import CTreeView
 from history.tree import HistoryBrowserTree
@@ -87,8 +88,86 @@ class HistoryBrowser(wx.Panel):
             return None
         return selection[0]
 
+    def _parseMapcalcCmd(self, command):
+        """Parse r.mapcalc/r3.mapcalc module command
+
+        Examples:
+
+        r.mapcalc foo = 1 -> r.mapcalc expression=foo=1
+
+        r.mapcalc foo=1 -> r.mapcalc expression=foo=1
+
+        r.mapcalc expression=foo=1 -> r.mapcalc expression=foo=1
+
+        r.mapcalc expression=foo = 1 -> r.mapcalc expression=foo=1
+
+        r.mapcalc expression="foo = 1" -> r.mapcalc expression="foo=1"
+
+        r.mapcalc expression="foo=1" -> r.mapcalc expression="foo=1"
+
+        r.mapcalc expression='foo = 1' -> r.mapcalc expression='foo=1'
+
+        r.mapcalc expression='foo=1' -> r.mapcalc expression='foo=1'
+
+
+        :param str command: r.mapcalc command string
+
+        :return str command: parsed r.mapcalc command string
+        """
+        flags = []
+        others_params_args = []
+        expression_param = "expression="
+        command = command.split()
+
+        for arg in command[:]:
+            if arg.startswith("-"):
+                flags.append(command.pop(command.index(arg)))
+            elif "region=" in arg or "file=" in arg or "seed=" in arg:
+                others_params_args.append(command.pop(command.index(arg)))
+
+        if expression_param not in "".join(command):
+            expression_param_arg = f"{expression_param}{''.join(command[1:])}"
+        else:
+            expression_param_arg = f"{''.join(command[1:])}"
+
+        command = " ".join(
+            [
+                command[0],
+                *flags,
+                expression_param_arg,
+                *others_params_args,
+            ]
+        )
+        return command
+
     def _refreshTree(self):
         self._tree.SetModel(self._model.GetModel())
+
+    def _replaceModuleCmdSpecialFlags(self, command):
+        """Replace module command special flags short version with
+        full version
+
+        Flags:
+
+        --o -> --overwrite
+        --q -> --quiet
+        --v -> --verbose
+
+        :param str command: module command string
+
+        :return str: module command string with replaced flags
+        """
+        flags_regex = re.compile(r"(--o(\s+|$))|(--q(\s+|$))|(--v(\s+|$))")
+        replace = {
+            "--o": "--overwrite ",
+            "--q": "--quiet ",
+            "--v": "--verbose ",
+        }
+        return re.sub(
+            flags_regex,
+            lambda flag: replace[flag.group().strip()],
+            command,
+        )
 
     def UpdateHistoryModelFromScratch(self):
         """Update the model from scratch and refresh the tree"""
@@ -110,15 +189,18 @@ class HistoryBrowser(wx.Panel):
         node = node or self._getSelectedNode()
         if node:
             command = node.data["command"]
-            lst = re.split(r"\s+", command)
             if (
                 globalvar.ignoredCmdPattern
                 and re.compile(globalvar.ignoredCmdPattern).search(command)
                 and "--help" not in command
                 and "--ui" not in command
             ):
-                self.runIgnoredCmdPattern.emit(cmd=lst)
+                self.runIgnoredCmdPattern.emit(cmd=split(command))
                 return
+            if re.compile(r"^r[3]?\.mapcalc").search(command):
+                command = self._parseMapcalcCmd(command)
+            command = self._replaceModuleCmdSpecialFlags(command)
+            lst = split(command)
             try:
                 GUI(parent=self, giface=self._giface).ParseCommand(lst)
             except GException as e:
