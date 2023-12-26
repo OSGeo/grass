c9a00709e9b5035b91fa8333e51a21c07587ae53
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2023-12-26 15:44:15 +0000

    Use `yield from` when a simple yield in a for loop is used. Supported since Python 3.3.
    See https://docs.astral.sh/ruff/rules/yield-in-for-loop/

--- python/grass/pygrass/modules/grid/grid.py
+++ python/grass/pygrass/modules/grid/grid.py
@@ -44,8 +44,7 @@ def select(parms, ptype):
         par = parms[k]
         if par.type == ptype or par.typedesc == ptype and par.value:
             if par.multiple:
-                for val in par.value:
-                    yield val
+                yield from par.value
             else:
                 yield par.value
 
