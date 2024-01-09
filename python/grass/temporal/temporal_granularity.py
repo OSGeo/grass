89d7b7df9d3df572d1d864b99a6f876e0e1e24da
ninsbl <stefan.blumentrath@gmx.de>
2024-01-09 22:11:10 +0100

    address test-failures

--- python/grass/temporal/temporal_granularity.py
+++ python/grass/temporal/temporal_granularity.py
@@ -253,6 +253,8 @@ def compute_relative_time_granularity(maps):
 
     """
 
+    if maps is None:
+        return None
     if issubclass(maps[0].__class__, AbstractMapDataset):
         get_time_tuple = _get_map_time_tuple
     else:
