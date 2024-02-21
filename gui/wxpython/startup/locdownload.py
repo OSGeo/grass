cfce0ec1226c0e003e40ae5ab075c5c83ac55c9e
Markus Neteler <neteler@osgeo.org>
2024-02-21 23:34:17 +0100

    wxGUI: fix URL to Piemonte (Italy) dataset (#3443)
    The dataset belongs to the book
    
    L. Casagrande, P. Cavallini, A. Frigeri, A. Furieri, I. Marchesini, M. Neteler, 2012: GIS Open Source. GRASS GIS, Quantum GIS e Spatialite.
    ISBN 9788857901497. 224 pages, Dario Flaccovio Editore S.r.l.
    
    The dataset URL is now updated to the new download space:
    
    https://grass.osgeo.org/sampledata/grassdata_piemonte_utm32n_wgs84_grass7.tar.gz
    
    (size: 291MB)
    
    Fixes #3292

--- gui/wxpython/startup/locdownload.py
+++ gui/wxpython/startup/locdownload.py
@@ -59,7 +59,7 @@ LOCATIONS = [
     },
     {
         "label": "Piemonte, Italy data set",
-        "url": "http://geodati.fmach.it/gfoss_geodata/libro_gfoss/grassdata_piemonte_utm32n_wgs84_grass7.tar.gz",
+        "url": "https://grass.osgeo.org/sampledata/grassdata_piemonte_utm32n_wgs84_grass7.tar.gz",
     },
     {
         "label": "Slovakia 3D precipitation voxel data set",
