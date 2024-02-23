1879b0c911132d68f4ce2a196ca65e01feb179ba
Paulo van Breugel <paulo@ecodiv.earth>
2024-02-23 04:14:38 +0100

    v.dissolve: Allow all features dissolving line features (#3433)
    * Allows all types to be processed through the underlying modules, not just areas.
    * Resolves issue with dissolving line features reported in #3420.
    * Makes statements in documentation more generic, rather than referring to dissolving areas only.
    * Improves documentation overall, esp. parts about attribute aggregation.
    
    Closes #3420

--- scripts/v.dissolve/v.dissolve.py
+++ scripts/v.dissolve/v.dissolve.py
@@ -6,9 +6,9 @@
 #               Markus Neteler for column support
 #               Converted to Python by Glynn Clements
 #               Vaclav Petras <wenzeslaus gmail com> (aggregate statistics)
-# PURPOSE:      Dissolve common boundaries between areas with common cat
-#                 (frontend to v.extract -d)
-# COPYRIGHT:    (c) 2006-2023 Hamish Bowman, and the GRASS Development Team
+# PURPOSE:      Dissolve adjacent or overlapping features
+#               with common cat (frontend to v.extract -d)
+# COPYRIGHT:    (c) 2006-2024 Hamish Bowman, and the GRASS Development Team
 #               This program is free software under the GNU General Public
 #               License (>=v2). Read the file COPYING that comes with GRASS
 #               for details.
@@ -16,7 +16,7 @@
 #############################################################################
 
 # %module
-# % description: Dissolves boundaries between adjacent areas sharing a common category number or attribute.
+# % description: Dissolves adjacent or overlaping features sharing a common category number or attribute.
 # % keyword: vector
 # % keyword: dissolve
 # % keyword: area
@@ -31,7 +31,7 @@
 # % guisection: Dissolving
 # %end
 # %option G_OPT_DB_COLUMN
-# % description: Name of attribute column used to dissolve common boundaries
+# % description: Name of attribute column used to dissolve features
 # % guisection: Dissolving
 # %end
 # %option G_OPT_V_OUTPUT
@@ -595,7 +595,6 @@ def main():
             flags="d",
             input=input_vector,
             output=output,
-            type="area",
             layer=layer,
         )
     else:
@@ -640,7 +639,6 @@ def main():
                 flags="d",
                 input=tmpfile,
                 output=output,
-                type="area",
                 layer=layer,
             )
             if columns_to_aggregate:
