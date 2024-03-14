87924f0701b2deeda0a66b92c04e8c332e4193f1
Huidae Cho <grass4u@gmail.com>
2024-03-14 04:53:00 -0600

    Revert to current D_open_driver()

--- display/d.redraw/main.c
+++ display/d.redraw/main.c
@@ -49,15 +49,7 @@ int main(int argc, char **argv)
         G_fatal_error(_("No graphics device selected. Use d.mon to select "
                         "graphics device."));
 
-    if (D_open_driver() < 0) {
-        /* first time it runs, add d.redraw to cmd file and exit; this module
-         * will be rerun by the monitor with the display extent (D_open_driver()
-         * will return 0 then) and use that information to constrain rendering
-         */
-        D_close_driver();
-        exit(EXIT_SUCCESS);
-    }
-
+    D_open_driver();
     D_close_driver();
 
     G_temp_element(element);
