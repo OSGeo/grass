f22bf295966bbff4b57ee70368b835f56a34b330
Maryam Abkar <mankara@ncsu.edu>
2024-03-20 12:50:50 -0400

    fixed formatting warning by black python formatter

--- utils/gitlog2changelog.py
+++ utils/gitlog2changelog.py
@@ -27,7 +27,7 @@ if len(sys.argv) > 1:
     git_command.append(rev_range)
 
 # Execute git log with the desired command line options.
-process = subprocess.Popen(git_command, stdout=subprocess.PIPE, encoding='utf8')
+process = subprocess.Popen(git_command, stdout=subprocess.PIPE, encoding="utf8")
 fin = process.stdout
 
 # Create a ChangeLog file in the current directory.
