8df7495d2081e5447f2e63f046c32ca873d75762
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-03-22 09:48:26 -0400

    CI(windows): Increase required passing tests percentage to 80% (#3517)

--- .github/workflows/test_thorough.bat
+++ .github/workflows/test_thorough.bat
@@ -2,4 +2,4 @@ set grass=%1
 set python=%2
 
 call %grass% --tmp-location XY --exec g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=%USERPROFILE%
-call %grass% --tmp-location XY --exec %python% -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 60
+call %grass% --tmp-location XY --exec %python% -m grass.gunittest.main --grassdata %USERPROFILE% --location nc_spm_full_v2alpha2 --location-type nc --min-success 80
