b3bd7953da277ac95c5b63b000e026958fd1395e
Vaclav Petras <wenzeslaus@gmail.com>
2024-03-22 13:39:07 -0400

    wxGUI: Minimal changes to rename location to project (#2993)
    This renames location to project in user messages in wxGUI.
    This does not change any documentation or variable names. It does not change names outside of wxGUI.
    This changes only location to project. It leaves mapset as is because there seems to be much higher consensus about renaming location to project than about renaming mapset in general.
    See also https://github.com/OSGeo/grass/issues/3121
    
    Co-authored-by: Caitlin Haedrich
    Co-authored-by: Veronica Andreo <veroandreo@gmail.com>
    Co-authored-by: Anna Petrasova <kratochanna@gmail.com>
    Co-authored-by: Markus Neteler <neteler@osgeo.org>

--- gui/wxpython/startup/locdownload.py
+++ gui/wxpython/startup/locdownload.py
@@ -40,27 +40,27 @@ from gui_core.wrap import Button, StaticText
 # TODO: labels (and descriptions) translatable?
 LOCATIONS = [
     {
-        "label": "Complete NC location",
+        "label": "Complete North Carolina dataset",
         "url": "https://grass.osgeo.org/sampledata/north_carolina/nc_spm_08_grass7.tar.gz",
     },
     {
-        "label": "Basic NC location",
+        "label": "Basic North Carolina dataset",
         "url": "https://grass.osgeo.org/sampledata/north_carolina/nc_basic_spm_grass7.tar.gz",
     },
     {
-        "label": "World location in LatLong/WGS84",
+        "label": "World dataset in LatLong/WGS84",
         "url": "https://grass.osgeo.org/sampledata/worldlocation.tar.gz",
     },
     {
-        "label": "Spearfish (SD) location",
+        "label": "Spearfish (SD) dataset",
         "url": "https://grass.osgeo.org/sampledata/spearfish_grass70data-0.3.tar.gz",
     },
     {
-        "label": "Piemonte, Italy data set",
+        "label": "Piemonte, Italy dataset",
         "url": "https://grass.osgeo.org/sampledata/grassdata_piemonte_utm32n_wgs84_grass7.tar.gz",
     },
     {
-        "label": "Slovakia 3D precipitation voxel data set",
+        "label": "Slovakia 3D precipitation voxel dataset",
         "url": "https://grass.osgeo.org/sampledata/slovakia3d_grass7.tar.gz",
     },
     {
@@ -68,7 +68,7 @@ LOCATIONS = [
         "url": "https://grass.osgeo.org/sampledata/fire_grass6data.tar.gz",
     },
     {
-        "label": "GISMentors location, Czech Republic",
+        "label": "GISMentors dataset, Czech Republic",
         "url": "http://training.gismentors.eu/geodata/grass/gismentors.zip",
     },
     {
@@ -176,7 +176,7 @@ def download_location(url, name, database):
         directory = download_and_extract(source=url, reporthook=reporthook)
         destination = os.path.join(database, name)
         if not is_location_valid(directory):
-            return _("Downloaded location is not valid")
+            return _("Downloaded project is not valid")
         shutil.copytree(src=directory, dst=destination)
         try_rmdir(directory)
     except DownloadError as error:
@@ -221,10 +221,10 @@ class LocationDownloadPanel(wx.Panel):
         self.database = database
         self.locations = locations
         self._abort_btn_label = _("Abort")
-        self._abort_btn_tooltip = _("Abort download location")
+        self._abort_btn_tooltip = _("Abort download")
 
         self.label = StaticText(
-            parent=self, label=_("Select sample location to download:")
+            parent=self, label=_("Select sample project to download:")
         )
 
         choices = []
@@ -289,7 +289,7 @@ class LocationDownloadPanel(wx.Panel):
         self.SetMinSize(self.GetBestSize())
 
     def _change_download_btn_label(
-        self, label=_("Do&wnload"), tooltip=_("Download selected location")
+        self, label=_("Do&wnload"), tooltip=_("Download selected project")
     ):
         """Change download button label/tooltip"""
         if self.parent.download_button:
@@ -321,7 +321,9 @@ class LocationDownloadPanel(wx.Panel):
         destination = os.path.join(self.database, dirname)
         if os.path.exists(destination):
             self._error(
-                _("Location named <%s> already exists," " download canceled") % dirname
+                _(
+                    "Project name {name} already exists in {path}, download canceled"
+                ).format(name=dirname, path=self.database)
             )
             self._change_download_btn_label()
             return
@@ -335,8 +337,8 @@ class LocationDownloadPanel(wx.Panel):
                 self._last_downloaded_location_name = dirname
                 self._warning(
                     _(
-                        "Download completed. The downloaded sample data is listed "
-                        "in the location/mapset tabs upon closing of this window"
+                        "Download completed. The downloaded sample data is available "
+                        "now in the data tree"
                     )
                 )
             self._change_download_btn_label()
@@ -376,7 +378,9 @@ class LocationDownloadPanel(wx.Panel):
         destination = os.path.join(self.database, dirname)
         if os.path.exists(destination):
             self._warning(
-                _("Location named <%s> already exists," " rename it first") % dirname
+                _("Project named {name} already exists, rename it first").format(
+                    name=dirname
+                )
             )
             self.parent.download_button.SetLabel(label=_("Download"))
             return
@@ -429,15 +433,15 @@ class LocationDownloadDialog(wx.Dialog):
     Contains the panel and Cancel button.
     """
 
-    def __init__(self, parent, database, title=_("Location Download")):
+    def __init__(self, parent, database, title=_("Dataset Download")):
         """
-        :param database: database to download the location to
+        :param database: database to download the project (location) to
         :param title: window title if the default is not appropriate
         """
         wx.Dialog.__init__(self, parent=parent, title=title)
         cancel_button = Button(self, id=wx.ID_CANCEL)
         self.download_button = Button(parent=self, id=wx.ID_ANY, label=_("Do&wnload"))
-        self.download_button.SetToolTip(_("Download selected location"))
+        self.download_button.SetToolTip(_("Download selected dataset"))
         self.panel = LocationDownloadPanel(parent=self, database=database)
         cancel_button.Bind(wx.EVT_BUTTON, self.OnCancel)
         self.Bind(wx.EVT_CLOSE, self.OnCancel)
@@ -480,7 +484,7 @@ class LocationDownloadDialog(wx.Dialog):
             # running thread
             dlg = wx.MessageDialog(
                 parent=self,
-                message=_("Do you want to cancel location download?"),
+                message=_("Do you want to cancel dataset download?"),
                 caption=_("Abort download"),
                 style=wx.YES_NO | wx.NO_DEFAULT | wx.ICON_QUESTION | wx.CENTRE,
             )
