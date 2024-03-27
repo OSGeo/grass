0be4fe0b2a04a29ecaab73a63f5fe157033502c1
Māris Nartišs <maris.gis@gmail.com>
2024-03-27 02:15:23 +0200

    Document that GRASS jupyter enables overwrite by default (#3516)
    * Document that GRASS jupyter enables overwrite by default
    
    * Update python/grass/jupyter/__init__.py
    
    Explain rationale for defaulting to overwrite
    
    Co-authored-by: Vaclav Petras <wenzeslaus@gmail.com>
    
    Improve syntax
    
    Co-authored-by: Vaclav Petras <wenzeslaus@gmail.com>
    
    * Shorten line length of lines commited directly from GH
    
    ---------
    
    Co-authored-by: Vaclav Petras <wenzeslaus@gmail.com>

--- python/grass/jupyter/__init__.py
+++ python/grass/jupyter/__init__.py
@@ -50,6 +50,14 @@ To use existing data, we start a GRASS session in an existing mapset::
 
 >>> gj.init("grassdata/nc_basic_spm_grass7/user1")
 
+.. note::
+    Contrary to typical command line / GUI module usage, grass.jupyter
+    enables output overwrite by default to align with behaviour of other
+    Python packages and to allow repeated executions of the same cells and
+    of the whole notebook. The default command line behaviour can be
+    restored by setting GRASS_OVERWRITE environmental variable to "0" after
+    `gj.init()` call: `os.environ["GRASS_OVERWRITE"] = "0"`.
+
 All classes and functions for interaction in notebooks are now available under *gj*,
 for example we can display a map with a selected raster and vector::
 
