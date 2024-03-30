7306341ebe2dbd80d7863de935fb6042717aa94e
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-03-30 12:07:50 +0000

    test: Quote variables in test_thorough.sh

--- .github/workflows/test_thorough.sh
+++ .github/workflows/test_thorough.sh
@@ -4,9 +4,9 @@
 set -e
 
 grass --tmp-location XY --exec \
-    g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path=$HOME
+    g.download.location url=https://grass.osgeo.org/sampledata/north_carolina/nc_spm_full_v2alpha2.tar.gz path="$HOME"
 
 grass --tmp-location XY --exec \
     python3 -m grass.gunittest.main \
-    --grassdata $HOME --location nc_spm_full_v2alpha2 --location-type nc \
-    --min-success 100 $@
+    --grassdata "$HOME" --location nc_spm_full_v2alpha2 --location-type nc \
+    --min-success 100 "$@"
