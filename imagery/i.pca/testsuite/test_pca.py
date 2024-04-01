57141c931542af9862d151e180d964dfdd2ef1b9
HamedElgizery <hamedashraf2004@gmail.com>
2024-04-01 20:16:51 +0200

    Add tests for r.univar results [i.pca] & save expected results in files

--- imagery/i.pca/testsuite/test_pca.py
+++ imagery/i.pca/testsuite/test_pca.py
@@ -1,11 +1,13 @@
+import os
+
 from grass.gunittest.case import TestCase
-import time
 
 class TestReport(TestCase):
     
     @classmethod
     def setUpClass(cls):
         """Use temporary region settings"""
+        cls.data_path = "data"
         cls.runModule("g.region", raster="lsat7_2002_10@PERMANENT")
         cls.use_temp_region()
     
@@ -17,9 +19,22 @@ class TestReport(TestCase):
         """Testing pca sample"""
         self.assertModule("i.pca", input="lsat7_2002_10,lsat7_2002_20,lsat7_2002_30,lsat7_2002_40,lsat7_2002_50,lsat7_2002_70", output="lsat7_2002_pca")
         
-        values = "north=228513\nsouth=214975.5\neast=645012\nwest=629992.5\nnsres=28.5\newres=28.5\nrows=475\ncols=527\ncells=250325\ndatatype=CELL\nncats=0\ncomments=\"Eigen values, (vectors), and [percent importance]:PC1   4334.35 ( 0.2824, 0.3342, 0.5092,-0.0087, 0.5264, 0.5217) [83.04%]PC2    588.31 ( 0.2541, 0.1885, 0.2923,-0.7428,-0.5110,-0.0403) [11.27%]PC3    239.22 ( 0.3801, 0.3819, 0.2681, 0.6238,-0.4000,-0.2980) [ 4.58%]PC4     32.85 ( 0.1752,-0.0191,-0.4053, 0.1593,-0.4435, 0.7632) [ 0.63%]PC5     20.73 (-0.6170,-0.2514, 0.6059, 0.1734,-0.3235, 0.2330) [ 0.40%]PC6      4.08 (-0.5475, 0.8021,-0.2282,-0.0607,-0.0208, 0.0252) [ 0.08%]i.pca input=\"lsat7_2002_10,lsat7_2002_20,lsat7_2002_30,lsat7_2002_40\,lsat7_2002_50,lsat7_2002_70\" output=\"lsat7_2002_pca\" rescale=0,255 \\percent=99\""
 
-        self.assertRasterFitsInfo(raster="lsat7_2002_pca.1", reference=values, precision=0)
+        # Asserting the results givien from r.info
+        with open(os.path.join(self.data_path, "lsat7_2002_pca_info.out")) as ref_output:
+            values = ref_output.read()
+            self.assertRasterFitsInfo(raster="lsat7_2002_pca.1", reference=values, precision=0)
+            self.assertRasterFitsInfo(raster="lsat7_2002_pca.2", reference=values, precision=0)
+            self.assertRasterFitsInfo(raster="lsat7_2002_pca.3", reference=values, precision=0)
+            self.assertRasterFitsInfo(raster="lsat7_2002_pca.4", reference=values, precision=0)
+            self.assertRasterFitsInfo(raster="lsat7_2002_pca.5", reference=values, precision=0)
+            self.assertRasterFitsInfo(raster="lsat7_2002_pca.6", reference=values, precision=0)
+
+        # Asserting the results givien from r.univar
+        for i in range(1, 7):
+            with open(os.path.join(self.data_path, "lsat7_2002_pca_{}_univar.out".format(i))) as ref_output:
+                values = ref_output.read()
+                self.assertModuleKeyValue("r.univar", flags="eg", map="lsat7_2002_pca.{}".format(i), reference=values, precision=0, sep="=")
 
 
 
