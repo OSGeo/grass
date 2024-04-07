ace2374dc925fbd0c183a1447eecbddafc4bebcd
Corey White <ctwhite48@gmail.com>
2024-04-07 15:08:19 -0400

    Removed unneeded logic

--- general/g.mapsets/list.c
+++ general/g.mapsets/list.c
@@ -15,16 +15,7 @@ void list_available_mapsets(const char **mapset_name, int nmapsets,
     for (n = 0; n < nmapsets; n++) {
         fprintf(stdout, "%s", mapset_name[n]);
         if (n < nmapsets - 1) {
-            if (strcmp(fs, "newline") == 0)
-                fprintf(stdout, "\n");
-            else if (strcmp(fs, "space") == 0)
-                fprintf(stdout, " ");
-            else if (strcmp(fs, "comma") == 0)
-                fprintf(stdout, ",");
-            else if (strcmp(fs, "tab") == 0)
-                fprintf(stdout, "\t");
-            else
-                fprintf(stdout, "%s", fs);
+            fprintf(stdout, "%s", fs);
         }
     }
     fprintf(stdout, "\n");
@@ -41,16 +32,7 @@ void list_accessible_mapsets(const char *fs)
         /* match each mapset to its numeric equivalent */
         fprintf(stdout, "%s", name);
         if (G_get_mapset_name(n + 1)) {
-            if (strcmp(fs, "newline") == 0)
-                fprintf(stdout, "\n");
-            else if (strcmp(fs, "space") == 0)
-                fprintf(stdout, " ");
-            else if (strcmp(fs, "comma") == 0)
-                fprintf(stdout, ",");
-            else if (strcmp(fs, "tab") == 0)
-                fprintf(stdout, "\t");
-            else
-                fprintf(stdout, "%s", fs);
+            fprintf(stdout, "%s", fs);
         }
     }
     fprintf(stdout, "\n");
