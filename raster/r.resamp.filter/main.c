dddbc066de9a30b4313c2b7ac40dfdd895293ed6
Anna Petrasova <kratochanna@gmail.com>
2024-04-09 16:05:13 -0400

    raster: disable parallelization when MASK active (#3561)

--- raster/r.resamp.filter/main.c
+++ raster/r.resamp.filter/main.c
@@ -494,7 +494,10 @@ int main(int argc, char *argv[])
                     "threads setting."));
     nprocs = 1;
 #endif
-
+    if (nprocs > 1 && G_find_raster("MASK", G_mapset()) != NULL) {
+        G_warning(_("Parallel processing disabled due to active MASK."));
+        nprocs = 1;
+    }
     if (parm.radius->answer) {
         if (parm.x_radius->answer || parm.y_radius->answer)
             G_fatal_error(_("%s= and %s=/%s= are mutually exclusive"),
--- raster/r.resamp.interp/main.c
+++ raster/r.resamp.interp/main.c
@@ -132,7 +132,10 @@ int main(int argc, char *argv[])
                     "threads setting."));
     threads = 1;
 #endif
-
+    if (threads > 1 && G_find_raster("MASK", G_mapset()) != NULL) {
+        G_warning(_("Parallel processing disabled due to active MASK."));
+        threads = 1;
+    }
     bufrows = atoi(memory->answer) * (((1 << 20) / sizeof(DCELL)) / dst_w.cols);
     /* set the output buffer rows to be at most covering the entire map */
     if (bufrows > dst_w.rows) {
