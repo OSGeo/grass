6dd6d465261e373accd36ac217eacd0adc43096e
Anna Petrasova <kratochanna@gmail.com>
2024-04-17 10:45:26 -0400

    t.vect.import: rename location parameter to project (#3610)
    Applies to t.rast.import also.

--- temporal/t.rast.import/t.rast.import.py
+++ temporal/t.rast.import/t.rast.import.py
@@ -26,7 +26,7 @@
 # % keyword: import
 # % keyword: raster
 # % keyword: time
-# % keyword: create location
+# % keyword: create project
 # %end
 
 # %option G_OPT_F_INPUT
@@ -68,9 +68,9 @@
 # %end
 
 # %option
-# % key: location
+# % key: project
 # % type: string
-# % description: Create a new location and import the data into it. Do not run this module in parallel or interrupt it when a new location should be created
+# % description: Create a new project and import the data into it. Do not run this module in parallel or interrupt it when a new project should be created
 # % required: no
 # % multiple: no
 # %end
@@ -90,18 +90,18 @@
 
 # %flag
 # % key: e
-# % description: Extend location extents based on new dataset
+# % description: Extend project extents based on new dataset
 # %end
 
 # %flag
 # % key: o
-# % label: Override projection check (use current location's projection)
-# % description: Assume that the dataset has same projection as the current location
+# % label: Override projection check (use current projects's CRS)
+# % description: Assume that the dataset has same coordinate reference system as the current location
 # %end
 
 # %flag
 # % key: c
-# % description: Create the location specified by the "location" parameter and exit. Do not import the space time raster datasets.
+# % description: Create the project specified by the "project" parameter and exit. Do not import the space time raster datasets.
 # %end
 
 import grass.script as grass
@@ -117,7 +117,7 @@ def main():
     directory = options["directory"]
     title = options["title"]
     descr = options["description"]
-    location = options["location"]
+    location = options["project"]
     base = options["basename"]
     memory = options["memory"]
     set_current_region = flags["r"]
