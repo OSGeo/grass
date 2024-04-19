bf81ce054ce6d26d5d7091119b1d5b21cac9dccb
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-04-19 18:41:46 +0200

    v.db.dropcolumn: fix enclosing column name with SQL standard double quotes
    Enable to use column name which is reserved DB backend keyword.

--- scripts/v.db.dropcolumn/v.db.dropcolumn.py
+++ scripts/v.db.dropcolumn/v.db.dropcolumn.py
@@ -96,11 +96,11 @@ def main():
                 # see db_sqltype_name() for type names
                 if f[1] == "CHARACTER":
                     # preserve field length for sql type "CHARACTER"
-                    coltypes.append("%s %s(%s)" % (f[0], f[1], f[2]))
+                    coltypes.append(f'"{f[0]}" {f[1]}({f[2]})')
                 else:
-                    coltypes.append("%s %s" % (f[0], f[1]))
+                    coltypes.append(f'"{f[0]}" {f[1]}')
 
-            colnames = ", ".join(colnames)
+            colnames = ", ".join([f'"{col}"' for col in colnames])
             coltypes = ", ".join(coltypes)
 
             cmds = [
