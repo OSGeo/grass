f0717a695ac21926373d87e93587d65d22e6862c
Huidae Cho <grass4u@gmail.com>
2024-04-21 02:46:55 -0600

    Extend copy_python_files_in_subdir to copy on build event

--- cmake/modules/copy_python_files_in_subdir.cmake
+++ cmake/modules/copy_python_files_in_subdir.cmake
@@ -9,13 +9,33 @@ COPYRIGHT:  (C) 2020 by the GRASS Development Team
 #]]
 
 function(copy_python_files_in_subdir dir_name dst_prefix)
+  cmake_parse_arguments(G "PRE_BUILD;PRE_LINK;POST_BUILD" "TARGET" "" ${ARGN})
+
   file(GLOB PY_FILES "${CMAKE_CURRENT_SOURCE_DIR}/${dir_name}/*.py")
-  string(REPLACE "/" "_" targ_name ${dir_name})
-  add_custom_target(
-    python_${targ_name}
-    COMMAND ${CMAKE_COMMAND} -E make_directory
-            ${GISBASE}/${dst_prefix}/${dir_name}/
-    COMMAND ${CMAKE_COMMAND} -E copy ${PY_FILES}
-            ${GISBASE}/${dst_prefix}/${dir_name}/)
-  set_target_properties(python_${targ_name} PROPERTIES FOLDER lib/python)
+
+  if(DEFINED G_TARGET)
+    if(${G_PRE_BUILD})
+      set(BUILD PRE_BUILD)
+    elseif(${G_PRE_LINK})
+      set(BUILD PRE_LINK)
+    else()
+      set(BUILD POST_BUILD)
+    endif()
+    add_custom_command(
+      TARGET ${G_TARGET}
+      ${BUILD}
+      COMMAND ${CMAKE_COMMAND} -E make_directory
+              ${GISBASE}/${dst_prefix}/${dir_name}/
+      COMMAND ${CMAKE_COMMAND} -E copy ${PY_FILES}
+              ${GISBASE}/${dst_prefix}/${dir_name}/)
+  else()
+    string(REPLACE "/" "_" targ_name ${dir_name})
+    add_custom_target(
+      python_${targ_name}
+      COMMAND ${CMAKE_COMMAND} -E make_directory
+              ${GISBASE}/${dst_prefix}/${dir_name}/
+      COMMAND ${CMAKE_COMMAND} -E copy ${PY_FILES}
+              ${GISBASE}/${dst_prefix}/${dir_name}/)
+    set_target_properties(python_${targ_name} PROPERTIES FOLDER lib/python)
+  endif()
 endfunction()
