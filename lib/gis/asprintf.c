571fdb2ad46675785de1c9274def586b22b6976b
Huidae Cho <grass4u@gmail.com>
2024-04-22 21:57:43 -0600

    libgis: G_vasprintf bug fix

--- lib/gis/asprintf.c
+++ lib/gis/asprintf.c
@@ -43,23 +43,21 @@ int G_vasprintf(char **out, const char *fmt, va_list ap)
 #ifdef HAVE_ASPRINTF
     return vasprintf(out, fmt, ap);
 #else
+    va_list aq;
     size_t size = strlen(fmt) + 50;
     char *buf = G_malloc(size);
     int count;
 
-    for (;;) {
-        /* BUG: according to man vsnprintf,
-         * va_start() should be called immediately before vsnprintf(),
-         * and va_end() immediately after vsnprintf()
-         * otherwise there will be memory corruption */
-        count = vsnprintf(buf, size, fmt, ap);
-        if (count >= 0 && count < size)
-            break;
-        size *= 2;
-        buf = G_realloc(buf, size);
+    va_copy(aq, ap);
+    count = vsnprintf(buf, size, fmt, ap);
+    if (count++ >= size) {
+        buf = G_realloc(buf, count);
+        if (count - 1 == size)
+            buf[count] = '\0';
+        else
+            vsnprintf(buf, count, fmt, aq);
     }
-
-    buf = G_realloc(buf, count + 1);
+    va_end(aq);
     *out = buf;
 
     return count;
