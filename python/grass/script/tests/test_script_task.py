e8053367dcb87243dfa2b2a6ec2404d8bb9af4c6
Martin Landa <landa.martin@gmail.com>
2024-05-11 17:30:31 +0200

    black applied
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- python/grass/script/tests/test_script_task.py
+++ python/grass/script/tests/test_script_task.py
@@ -4,7 +4,8 @@ from grass.script.task import grassTask as gtask
 
 def test_mapcalc_simple_e_name():
     gt = gtask("r.mapcalc.simple")
-    assert gt.get_param('e')['name'] == 'e'
+    assert gt.get_param("e")["name"] == "e"
+
 
 def test_mapcalc_simple_expession_name():
     gt = gtask("r.mapcalc.simple")
