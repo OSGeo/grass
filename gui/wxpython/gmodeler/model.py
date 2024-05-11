4850cd4460d5d9c55c1292d57c72b183d8d5e563
Martin Landa <landa.martin@gmail.com>
2024-05-11 17:43:11 +0200

    graphical modeler: export variables to Python

--- gui/wxpython/gmodeler/model.py
+++ gui/wxpython/gmodeler/model.py
@@ -3170,6 +3170,12 @@ class WritePythonFile(WriteScriptFile):
         self._writePython()
 
     def _getStandardizedOption(self, string):
+        """Return GRASS standardized option based on specified string.
+
+        :param string: input string to be converted
+
+        :return: GRASS standardized option as a string or None if not converted
+        """
         if string == "raster":
             return "G_OPT_R_MAP"
         elif string == "vector":
@@ -3183,7 +3189,7 @@ class WritePythonFile(WriteScriptFile):
         elif string == "region":
             return "G_OPT_M_REGION"
 
-        return ""
+        return None
 
     def _writePython(self):
         """Write model to file"""
@@ -3227,7 +3233,8 @@ class WritePythonFile(WriteScriptFile):
 
         modelItems = self.model.GetItems(ModelAction)
         for item in modelItems:
-            for flag in item.GetParameterizedParams()["flags"]:
+            parametrizedParams = item.GetParameterizedParams()
+            for flag in parametrizedParams["flags"]:
                 if flag["label"]:
                     desc = flag["label"]
                 else:
@@ -3251,7 +3258,7 @@ class WritePythonFile(WriteScriptFile):
                     self.fd.write("# % answer: False\n")
                 self.fd.write("# %end\n")
 
-            for param in item.GetParameterizedParams()["params"]:
+            for param in parametrizedParams["params"]:
                 if param["label"]:
                     desc = param["label"]
                 else:
@@ -3277,6 +3284,29 @@ class WritePythonFile(WriteScriptFile):
                 if param["value"]:
                     self.fd.write("# % answer: {}\n".format(param["value"]))
                 self.fd.write("# %end\n")
+
+        # variables
+        for vname, vdesc in self.model.GetVariables().items():
+            self.fd.write("# %option")
+            optionType = self._getStandardizedOption(vdesc["type"])
+            if optionType:
+                self.fd.write(" {}".format(optionType))
+            self.fd.write("\n")
+            self.fd.write(
+                r"""# % key: {param_name}
+# % description: {description}
+# % required: yes
+""".format(
+                    param_name=vname,
+                    description=vdesc["description"],
+                )
+            )
+            if optionType is None and vdesc["type"]:
+                self.fd.write("# % type: {}\n".format(vdesc["type"]))
+
+            if vdesc["value"]:
+                self.fd.write("# % answer: {}\n".format(vdesc["value"]))
+            self.fd.write("# %end\n")
 
         # import modules
         self.fd.write(
@@ -3326,8 +3356,11 @@ def cleanup():
             self.fd.write("    pass\n")
 
         self.fd.write("\ndef main(options, flags):\n")
+        modelVars = self.model.GetVariables()
         for item in self.model.GetItems(ModelAction):
-            self._writeItem(item, variables=item.GetParameterizedParams())
+            modelParams = item.GetParameterizedParams()
+            modelParams["vars"] = modelVars
+            self._writeItem(item, variables=modelParams)
 
         self.fd.write("    return 0\n")
 
@@ -3402,6 +3435,15 @@ if __name__ == "__main__":
                 if name in parameterizedParams:
                     foundVar = True
                     value = 'options["{}"]'.format(self._getParamName(name, item))
+                else:
+                    # check for variables
+                    for var in variables["vars"]:
+                        pattern = re.compile("%" + var)
+                        if pattern.search(value):
+                            foundVar = True
+                            value = pattern.sub("{options['" + var + "']}", value)
+                    if foundVar:
+                        value = 'f"' + value + '"'
 
                 if (
                     foundVar
