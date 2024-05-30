fe3de57f821eabf8d4ae3e953860e1c47c209130
Stefan Blumentrath <stefan.blumentrath@gmx.de>
2024-05-30 13:18:58 +0200

    flake8

--- python/grass/pygrass/modules/interface/module.py
+++ python/grass/pygrass/modules/interface/module.py
@@ -14,8 +14,6 @@ from .typedict import TypeDict
 from .read import GETFROMTAG, DOC
 from .env import G_debug
 
-from itertools import zip_longest
-
 
 def _get_bash(self, *args, **kargs):
     return self.get_bash()
