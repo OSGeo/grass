47d256ba9d5e488d32bea1c244ae7a853558a5c9
Luís de Sousa <luis.de.sousa@protonmail.ch>
2024-06-17 11:32:07 +0200

    Closes #2187

--- vector/v.out.ogr/testsuite/test_v_out_ogr.py
+++ vector/v.out.ogr/testsuite/test_v_out_ogr.py
@@ -3,8 +3,10 @@
 @author Luís Moreira de Sousa
 """
 
+import grass.script as gs
 from grass.gunittest.case import TestCase
 from pathlib import Path
+from shutil import rmtree
 
 
 class TestOgrExport(TestCase):
@@ -14,6 +16,15 @@ class TestOgrExport(TestCase):
 
     # Result of import tests
     temp_import = "test_ogr_import_map"
+
+    # Temporary location
+    temp_location = "temp_location"
+
+    # Temporary Homolosine map
+    temp_54052 = "temp_rand.gpkg"
+
+    # Temporary environment file
+    session_file = ""
 
     # Column on which to test v.univar
     univar_col = "PERIMETER"
@@ -42,17 +53,35 @@ skewness=4.86561
         """Use temporary region settings"""
         cls.use_temp_region()
 
+
     @classmethod
     def tearDownClass(cls):
         """!Remove the temporary region"""
         cls.del_temp_region()
 
+
     def tearDown(self):
+
+        # Remove maps in temp mapset
         self.runModule(
-            "g.remove", type="vector", flags="f", pattern="%s*" % self.temp_import
+            "g.remove", type="vector", flags="f", 
+            pattern="%s*" % self.temp_import
         )
+
+        # Remove temporary files
         for p in Path(".").glob("%s*" % self.test_map):
             p.unlink()
+        for p in Path(".").glob("%s*" % self.temp_54052):
+            p.unlink()
+        if len(self.session_file) > 0:
+            Path(self.session_file).unlink()
+
+        # Remove temporary location    
+        env = gs.parse_command("g.gisenv")
+        rmtree("%s/%s" % (env["GISDBASE"].replace('\'', '').replace(';', ''),
+                          self.temp_location),
+               ignore_errors=True)
+
 
     def test_1(self):
         self.assertModule(
@@ -102,6 +131,44 @@ skewness=4.86561
             reference=self.univar_string,
             column=self.univar_col,
             precision=1e-8,
+        )
+
+    def test_3(self):
+
+        # Record original location to return later
+        env_orig = gs.parse_command("g.gisenv")
+
+        # Create new location with CRS without EPSG code
+        gs.run_command(
+            "g.proj",
+            wkt="ESRI54052.wkt",
+            location=self.temp_location,
+            flags="c"
+        )
+
+        self.session_file, env_new = gs.core.create_environment(
+            env_orig["GISDBASE"].replace('\'', '').replace(';', ''),
+            self.temp_location,
+            "PERMANENT"
+        )
+
+        # Creates a random layer to test output
+        gs.run_command(
+            "v.random",
+            output="temp_rand",
+            npoints=3,
+            env=env_new
+        )
+
+        # Test output of a vector with non-EPSG CRS
+        gs.run_command(
+            "v.out.ogr",
+            input="temp_rand",
+            output=self.temp_54052,
+            format="GPKG",
+            dsco="a_srs=ESRI:54052",
+            env=env_new
+
         )
 
 
