cfe7f32aa04206884e441293efa605bba39f3d4c
Corey White <smortopahri@gmail.com>
2024-06-18 15:18:53 -0400

    Update vector/v.ppa/main.c
    Co-authored-by: Loïc Bartoletti <lbartoletti@users.noreply.github.com>

--- vector/v.ppa/main.c
+++ vector/v.ppa/main.c
@@ -274,7 +274,7 @@ void calculate_f_function(struct Point *points, int n, const char *output_file)
     G_message(_("Max distance: %f"), max_dist);
 
     fprintf(fp, "Distance,F-value\n");
-    for (double d = 0; d <= max_dist; d += max_dist / 100) {
+    for (double d = 0.0; d <= max_dist; d += max_dist / 100.0) {
         double f_value = 0.0;
 
 #pragma omp parallel for reduction(+ : f_value)
