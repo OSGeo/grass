5c2652b5806953854c6947e7f943313094d0470c
Ivan Mincik <ivan.mincik@gmail.com>
2024-06-25 10:17:54 +0200

    nix: improve nix development environment
    * inherit build dependencies from grass package
    * add `dev-help` function

--- flake.nix
+++ flake.nix
@@ -19,58 +19,34 @@
         packages.grass = pkgs.callPackage ./package.nix { };
 
         devShells.default = pkgs.mkShell {
-          buildInputs =
-            # from package nativeBuildInputs
-            with pkgs; [
-              bison
-              flex
-              gdal # for `gdal-config`
-              geos # for `geos-config`
-              netcdf # for `nc-config`
-              pkg-config
-            ] ++ (with python3Packages; [ pytest python-dateutil numpy wxPython_4_2 ])
+          inputsFrom = [ self'.packages.grass ];
 
-            # from package buildInputs
-            ++ [
-              blas
-              cairo
-              ffmpeg
-              fftw
-              freetype
-              gdal
-              geos
-              lapack
-              libGLU
-              libpng
-              libsvm
-              libtiff
-              libxml2
-              netcdf
-              pdal
-              postgresql
-              proj
-              readline
-              sqlite
-              wxGTK32
-              zlib
-              zstd
-            ] ++ lib.optionals stdenv.isDarwin [ libiconv ];
+          # additional packages
+          buildInputs = with pkgs.python3Packages; [
+            pytest
+          ];
 
           shellHook = ''
-            echo -e "\nWelcome to a GRASS development environment !"
-            echo "Build GRASS using following commands:"
-            echo
-            echo " 1.  ./configure --prefix=\$(pwd)/app"
-            echo " 2.  make -j$(nproc)"
-            echo " 3.  make install"
-            echo
-            echo "Run tests:"
-            echo
-            echo " 1.  export PYTHONPATH=\$(app/bin/grass --config python_path):\$PYTHONPATH"
-            echo " 2.  export LD_LIBRARY_PATH=\$(app/bin/grass --config path)/lib:\$LD_LIBRARY_PATH"
-            echo " 3.  pytest"
-            echo
-            echo "Note: run 'nix flake update' from time to time to update dependencies."
+            function dev-help {
+              echo -e "\nWelcome to a GRASS development environment !"
+              echo "Build GRASS using following commands:"
+              echo
+              echo " 1.  ./configure --prefix=\$(pwd)/app"
+              echo " 2.  make -j$(nproc)"
+              echo " 3.  make install"
+              echo
+              echo "Run tests:"
+              echo
+              echo " 1.  export PYTHONPATH=\$(app/bin/grass --config python_path):\$PYTHONPATH"
+              echo " 2.  export LD_LIBRARY_PATH=\$(app/bin/grass --config path)/lib:\$LD_LIBRARY_PATH"
+              echo " 3.  pytest"
+              echo
+              echo "Note: run 'nix flake update' from time to time to update dependencies."
+              echo
+              echo "Run 'dev-help' to see this message again."
+            }
+
+            dev-help
           '';
         };
       };
