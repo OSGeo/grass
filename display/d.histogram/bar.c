695e550164dfe8cd35ee77aa87174bd526a9be0c
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2024-06-28 15:44:04 -0400

    d.histogram: Initialize range_dmin and range_dmax to fix uninitialize value warnings in bar.c (#3923)
    The change satisfies the warning and does not change the behavior. The initial values (garbage or 0) are actually not used. `is_fp` conditions are used to set the values and drive their use (so that the initial values are not used).

--- display/d.histogram/bar.c
+++ display/d.histogram/bar.c
@@ -39,7 +39,7 @@ int bar(struct stat_list *dist_stats, /* list of distribution statistics */
     int draw = YES;
     long int bar_height; /* height, in pixels, of a histogram bar */
     CELL bar_color;      /* color/category number of a histogram bar */
-    DCELL dmax, range_dmin, range_dmax, dmin, dval;
+    DCELL dmax, range_dmin = 0, range_dmax = 0, dmin, dval;
     long int max_tics; /* maximum tics allowed on an axis */
     long int xoffset;  /* offset for x-axis */
     long int yoffset;  /* offset for y-axis */
