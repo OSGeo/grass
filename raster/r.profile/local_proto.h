5b5a70e968394c7b22787a43005af735a0c1ef9d
Kriti Birda <164247895+kritibirda26@users.noreply.github.com>
2024-07-02 15:24:52 +0530

    r.profile: add JSON support (#3872)
    Use parson to add json output format support to the r.profile module.

--- raster/r.profile/local_proto.h
+++ raster/r.profile/local_proto.h
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <grass/parson.h>
 #include <grass/gis.h>
 #include <grass/raster.h>
 
+enum OutputFormat { PLAIN, JSON };
+
 /* main.c */
 int do_profile(double, double, double, double, int, double, int, int, FILE *,
-               char *, const char *, double);
+               char *, const char *, double, enum OutputFormat, char *,
+               JSON_Array *);
 
 /* read_rast.c */
-int read_rast(double, double, double, int, int, RASTER_MAP_TYPE, FILE *,
-              char *);
+int read_rast(double, double, double, int, int, RASTER_MAP_TYPE, FILE *, char *,
+              enum OutputFormat, char *, JSON_Array *);
 
 /* input.c */
 int input(char *, char *, char *, char *, char *, FILE *);
--- raster/r.profile/read_rast.c
+++ raster/r.profile/read_rast.c
@@ -10,10 +10,12 @@
 #include <grass/gis.h>
 #include <grass/raster.h>
 #include <grass/glocale.h>
+#include <grass/parson.h>
 #include "local_proto.h"
 
 int read_rast(double east, double north, double dist, int fd, int coords,
-              RASTER_MAP_TYPE data_type, FILE *fp, char *null_string)
+              RASTER_MAP_TYPE data_type, FILE *fp, char *null_string,
+              enum OutputFormat format, char *name, JSON_Array *array)
 {
     static DCELL *dcell;
     static int cur_row = -1;
@@ -22,6 +24,13 @@ int read_rast(double east, double north, double dist, int fd, int coords,
     static struct Cell_head window;
     int row, col;
     int outofbounds = FALSE;
+    JSON_Object *object;
+    JSON_Value *value;
+
+    if (format == JSON) {
+        value = json_value_init_object();
+        object = json_object(value);
+    }
 
     if (!dcell) {
         Rast_set_c_null_value(&nullcell, 1);
@@ -44,18 +53,54 @@ int read_rast(double east, double north, double dist, int fd, int coords,
         cur_row = row;
     }
 
-    if (coords)
-        fprintf(fp, "%f %f", east, north);
+    switch (format) {
+    case JSON:
+        if (coords) {
+            json_object_set_number(object, "easting", east);
+            json_object_set_number(object, "northing", north);
+        }
+        json_object_set_number(object, "distance", dist);
+        break;
+    case PLAIN:
+        if (coords)
+            fprintf(fp, "%f %f", east, north);
 
-    fprintf(fp, " %f", dist);
+        fprintf(fp, " %f", dist);
+        break;
+    }
 
-    if (outofbounds || Rast_is_d_null_value(&dcell[col]))
-        fprintf(fp, " %s", null_string);
+    if (outofbounds || Rast_is_d_null_value(&dcell[col])) {
+        switch (format) {
+        case JSON:
+            json_object_set_null(object, name);
+            break;
+        case PLAIN:
+            fprintf(fp, " %s", null_string);
+            break;
+        }
+    }
     else {
-        if (data_type == CELL_TYPE)
-            fprintf(fp, " %d", (int)dcell[col]);
-        else
-            fprintf(fp, " %f", dcell[col]);
+        if (data_type == CELL_TYPE) {
+            int dvalue = (int)dcell[col];
+            switch (format) {
+            case JSON:
+                json_object_set_number(object, name, dvalue);
+                break;
+            case PLAIN:
+                fprintf(fp, " %d", dvalue);
+                break;
+            }
+        }
+        else {
+            switch (format) {
+            case JSON:
+                json_object_set_number(object, name, dcell[col]);
+                break;
+            case PLAIN:
+                fprintf(fp, " %f", dcell[col]);
+                break;
+            }
+        }
     }
 
     if (clr) {
@@ -65,11 +110,26 @@ int read_rast(double east, double north, double dist, int fd, int coords,
             Rast_get_c_color(&nullcell, &red, &green, &blue, &colors);
         else
             Rast_get_d_color(&dcell[col], &red, &green, &blue, &colors);
-
-        fprintf(fp, " %03d:%03d:%03d", red, green, blue);
+        switch (format) {
+        case JSON:
+            json_object_set_number(object, "red", red);
+            json_object_set_number(object, "green", green);
+            json_object_set_number(object, "blue", blue);
+            break;
+        case PLAIN:
+            fprintf(fp, " %03d:%03d:%03d", red, green, blue);
+            break;
+        }
     }
 
-    fprintf(fp, "\n");
+    switch (format) {
+    case JSON:
+        json_array_append_value(array, value);
+        break;
+    case PLAIN:
+        fprintf(fp, "\n");
+        break;
+    }
 
     return 0;
 }
