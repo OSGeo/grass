5716f1eea02624703b200caaead12a35dbdc9c62
Kriti Birda <workwithkritibirda@gmail.com>
2024-07-10 16:57:11 +0530

    add test and documentation

--- general/g.region/testsuite/test_g_region.py
+++ general/g.region/testsuite/test_g_region.py
@@ -3,6 +3,8 @@
 @author Anna Petrasova
 """
 
+import json
+
 from grass.gunittest.case import TestCase
 from grass.gunittest.gmodules import call_module
 import grass.script as gs
@@ -45,6 +47,46 @@ class TestRegion(TestCase):
     def test_f_flag(self):
         line = call_module("g.region", flags="fglecn3", capture_stdout=True)
         self.assertEqual(1, len(line.splitlines()))
+
+    def test_format_json(self):
+        """Test json format"""
+        expected = {
+            "projection": "99 (Lambert Conformal Conic)",
+            "zone": 0,
+            "datum": "nad83",
+            "ellipsoid": "a=6378137 es=0.006694380022900787",
+            "region": {
+                "north": 320000,
+                "south": 10000,
+                "west": 120000,
+                "east": 935000,
+                "ns-res": 500,
+                "ns-res3": 1000,
+                "ew-res": 500,
+                "ew-res3": 1000,
+            },
+            "top": 500,
+            "bottom": -500,
+            "tbres": 100,
+            "rows": 620,
+            "rows3": 310,
+            "cols": 1630,
+            "cols3": 815,
+            "depths": 10,
+            "cells": 1010600,
+            "cells3": 2526500,
+        }
+
+        output = call_module("g.region", flags="p", format="json")
+        output_json = json.loads(output)
+
+        expected_ellps = expected.pop("ellipsoid").split(" ")
+        received_ellps = output_json.pop("ellipsoid").split(" ")
+        self.assertEqual(expected_ellps[0], received_ellps[0])
+        self.assertAlmostEqual(
+            float(expected_ellps[1][3:]), float(received_ellps[1][3:]), places=6
+        )
+        self.assertDictEqual(expected, output_json)
 
 
 if __name__ == "__main__":
