34c737dd4515f631b7a0c14145a0f18d4517f13a
ninsbl <stefan.blumentrath@gmx.de>
2024-07-16 00:07:22 +0200

    fix direct rng call

--- python/grass/pygrass/vector/testsuite/test_vector3d.py
+++ python/grass/pygrass/vector/testsuite/test_vector3d.py
@@ -21,10 +21,10 @@ def generate_coordinates(number, bbox=None, with_z=False):
     """Return 2 or 3 random arrays of coordinates"""
     rng = np.random.default_rng()
     bbox = Region() if bbox is None else bbox
-    x = bbox.south + (bbox.north - bbox.south) * rng(number)
-    y = bbox.west + (bbox.east - bbox.west) * rng(number)
+    x = bbox.south + (bbox.north - bbox.south) * rng.random(number)
+    y = bbox.west + (bbox.east - bbox.west) * rng.random(number)
     if with_z:
-        z = rng(number) * 1000
+        z = rng.random(number) * 1000
         return x, y, z
     return x, y
 
