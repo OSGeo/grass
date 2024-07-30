OSGeo/grass

commit f99780c11f8d4d1633e469fb32da2b6c75852b4f

style: Fix collapsible-else-if (PLR5501) (#4045)
* style: Fix collapsible-else-if (PLR5501)

Ruff rule: https://docs.astral.sh/ruff/rules/collapsible-else-if/

* style: Fix collapsible-else-if (PLR5501)

Ruff rule: https://docs.astral.sh/ruff/rules/collapsible-else-if/

* style: Fix collapsible-else-if (PLR5501)

Ruff rule: https://docs.astral.sh/ruff/rules/collapsible-else-if/

* Apply suggestions from code review

Co-authored-by: Stefan Blumentrath <stefan.blumentrath@gmx.de>

* style: Combine if conditions in wxpython/gmodeler/model.py

* style: Simplify if conditions in wxpython/gui_core

---------

Co-authored-by: Stefan Blumentrath <stefan.blumentrath@gmx.de>

gui/wxpython/gui_core/toolbars.py
@@ -173,13 +173,10 @@ class ToolbarController:
             if isinstance(tool[0], tuple):
                 if tool[0][0] == "":  # separator
                     continue
-                else:
-                    internal_label = tool[0][0]
-            else:
-                if tool[0] == "":  # separator
-                    continue
-                else:
-                    internal_label = tool[0]
+                internal_label = tool[0][0]
+            elif tool[0] == "":  # separator
+                continue
+            internal_label = tool[0]
 
             label = vars(self.widget)[internal_label]
             if enable:

gui/wxpython/vdigit/toolbars.py
@@ -672,16 +672,12 @@ class VDigitToolbar(BaseToolbar):
         """
         self._enableTool(self.redo, enable)
 
-    def _enableTool(self, tool, enable):
+    def _enableTool(self, tool, enable: bool):
         if not self.FindById(tool):
             return
 
-        if enable:
-            if self.GetToolEnabled(tool) is False:
-                self.EnableTool(tool, True)
-        else:
-            if self.GetToolEnabled(tool) is True:
-                self.EnableTool(tool, False)
+        if self.GetToolEnabled(tool) is not bool(enable):
+            self.EnableTool(tool, bool(enable))
 
     def GetAction(self, type="desc"):
         """Get current action info"""

commit e56c7cddd7a23b612cd491592c4ac355ca1bac61

gui: fix broken if-else statement in EnableLongHelp() (#4106)
Fixes a regression introduced by
https://github.com/OSGeo/grass/commit/f99780c11f8d4d1633e469fb32da2b6c75852b4f

Closes #4091

gui/wxpython/gui_core/toolbars.py
@@ -174,9 +174,10 @@ class ToolbarController:
                 if tool[0][0] == "":  # separator
                     continue
                 internal_label = tool[0][0]
-            elif tool[0] == "":  # separator
-                continue
-            internal_label = tool[0]
+            else:
+                if tool[0] == "":  # separator
+                    continue
+                internal_label = tool[0]
 
             label = vars(self.widget)[internal_label]
             if enable: