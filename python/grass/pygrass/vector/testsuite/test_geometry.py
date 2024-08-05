b43173f99f0d6803a4c93686082be60c5f11f178
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-08-05 01:42:24 +0000

    style: Ignore remaining comparison-with-itself (PLR0124) in test_eq
    Ruff rule: https://docs.astral.sh/ruff/rules/comparison-with-itself/
    
    1 instance solved

--- python/grass/pygrass/vector/testsuite/test_geometry.py
+++ python/grass/pygrass/vector/testsuite/test_geometry.py
@@ -86,7 +86,7 @@ class PointTestCase(TestCase):
         point1 = Point(1, 0)
         self.assertFalse(point0 == point1)
         self.assertFalse(point0 == (1, 0))
-        self.assertTrue(point0 == point0)
+        self.assertTrue(point0 == point0)  # noqa: PLR0124
         self.assertTrue(point0 == (0, 0))
 
     def test_repr(self):
