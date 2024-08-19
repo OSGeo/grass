44736a54729c9f809e08ce42cf641a8bc30ca46f
Martin Landa <landa.martin@gmail.com>
2024-08-19 20:54:09 +0200

    Update man/build_keywords.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- man/build_keywords.py
+++ man/build_keywords.py
@@ -147,7 +147,7 @@ def build_keywords(ext):
             man_file_path = get_module_man_file_path(value, addons_man_files)
             if ext == "html":
                 keyword_line += (
-                    f' <a href="{man_file_path}">' f'{value.replace(".{ext}", "")}</a>,'
+                    f' <a href="{man_file_path}">{value.replace(".{ext}", "")}</a>,'
                 )
             else:
                 keyword_line += f' [{value.rsplit(".", 1)[0]}]({man_file_path}),'
