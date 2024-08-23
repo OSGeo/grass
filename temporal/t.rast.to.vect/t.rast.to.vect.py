4587ef76b4bb41d1e101a63f40e7f8dfacdcf55f
Makiko Shukunobe <mshukuno@users.noreply.github.com>
2024-08-22 23:33:47 -0400

    Checks: Fix flake8 F841 (local variable assigned to but never used) in /temporal/t.rast.to.vect (#4206)

--- temporal/t.rast.to.vect/t.rast.to.vect.py
+++ temporal/t.rast.to.vect/t.rast.to.vect.py
@@ -163,7 +163,7 @@ def main(options, flags):
         return
 
     # Check the new stvds
-    new_sp = tgis.check_new_stds(output, "stvds", dbif=dbif, overwrite=overwrite)
+    tgis.check_new_stds(output, "stvds", dbif=dbif, overwrite=overwrite)
 
     # Setup the flags
     flags = ""
@@ -189,6 +189,7 @@ def main(options, flags):
         type=method,
         overwrite=overwrite,
         quiet=True,
+        column=column,
     )
 
     # The module queue for parallel execution, except if attribute tables should
