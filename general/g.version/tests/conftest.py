8126c2979ea96ff5ee7a210832e0b8259fa007dc
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2024-08-23 17:50:04 -0400

    Update general/g.version/tests/conftest.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- general/g.version/tests/conftest.py
+++ general/g.version/tests/conftest.py
@@ -9,7 +9,6 @@ def grass_session(tmp_path_factory):
     """Set up a GRASS session for the tests."""
     tmp_path = tmp_path_factory.mktemp("grass_session")
     location = "test_location"
-    
     gs.core._create_location_xy(tmp_path, location)
 
     with gs.setup.init(tmp_path / location, env=os.environ.copy()) as session:
