641fa5faf0c32c5c9068c6e57a8cb4cbc434e448
Shubham Vasudeo Desai <sdesai8@vclvm176-51.vcl.ncsu.edu>
2024-08-26 15:41:32 -0400

    add pylint comment

--- general/g.version/tests/conftest.py
+++ general/g.version/tests/conftest.py
@@ -9,7 +9,7 @@ def grass_session(tmp_path_factory):
     """Set up a GRASS session for the tests."""
     tmp_path = tmp_path_factory.mktemp("grass_session")
     location = "test_location"
-    gs.core._create_location_xy(tmp_path, location)
+    gs.core._create_location_xy(tmp_path, location)  # pylint: disable=protected-access
 
     with gs.setup.init(tmp_path / location, env=os.environ.copy()) as session:
         yield SimpleNamespace(session=session)
