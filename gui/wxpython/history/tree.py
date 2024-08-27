OSGeo/grass

commit 02e45e93a87c87a61915bbc9c3997fd5ec57fbc3

small suggestion

gui/wxpython/history/tree.py
@@ -274,7 +274,10 @@ class HistoryBrowserTree(CTreeView):
         """Reload the model of a specific node."""
         self._model.SortChildren(node)
         self._orig_model = copy.deepcopy(self._model)
-        self.RefreshItems()
+        if node == self._model.root:
+            self.RefreshItems()
+        else:
+            self.RefreshNode(node, recursive=True)
 
     def _populateDayItem(self, day_node, entry):
         """
@@ -536,7 +539,7 @@ class HistoryBrowserTree(CTreeView):
             status=status,
             timestamp=datetime_timestamp,
         )
-        self._reloadNode(first_node)
+        self._reloadNode(first_node.parent)
 
         # Show command info in info panel
         self.infoPanel.showCommandInfo(command_info)

commit acfbdd891145c90de10050cfc8222be04070ac91

wxGUI: fix adding and removing from history for new mapset (#4228)
Bug introduced in 02e45e9 when trying to avoid calling complete tree refresh every time.

gui/wxpython/history/tree.py
@@ -505,6 +505,7 @@ class HistoryBrowserTree(CTreeView):
                 },
             )
             self._model.SortChildren(self._model.root)
+            self.RefreshItems()
 
         # Populate today's node by executed command
         command_node = self._populateDayItem(today_node, entry)
@@ -621,9 +622,9 @@ class HistoryBrowserTree(CTreeView):
         selected_day = selected_command.parent
         if selected_day and len(selected_day.children) == 0:
             self._model.RemoveNode(selected_day)
-
-        # Reload day node
-        self._reloadNode(selected_day)
+            self._reloadNode(self._model.root)
+        else:
+            self._reloadNode(selected_day)
         self.showNotification.emit(message=_("<{}> removed").format(command))
 
     def OnItemSelected(self, node):