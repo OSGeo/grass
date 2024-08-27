3b42db105861063c5d867cb4029313d897584c98
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-08-27 02:40:57 +0000

    man/build_rest: Use context manager for get_desc

--- man/build_rest.py
+++ man/build_rest.py
@@ -333,25 +333,25 @@ def write_rest_footer(f, index_url):
 
 
 def get_desc(cmd):
-    f = open(cmd)
-    while True:
-        line = f.readline()
-        if not line:
-            return ""
-        if "NAME" in line:
-            break
+    with Path(cmd).open() as f:
+        while True:
+            line = f.readline()
+            if not line:
+                return ""
+            if "NAME" in line:
+                break
 
-    while True:
-        line = f.readline()
-        if not line:
-            return ""
-        if "SYNOPSIS" in line:
-            break
-        if "*" in line:
-            sp = line.split("-", 1)
-            if len(sp) > 1:
-                return sp[1].strip()
-            return None
+        while True:
+            line = f.readline()
+            if not line:
+                return ""
+            if "SYNOPSIS" in line:
+                break
+            if "*" in line:
+                sp = line.split("-", 1)
+                if len(sp) > 1:
+                    return sp[1].strip()
+                return None
 
     return ""
 
