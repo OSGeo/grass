848b74eb81c09afbeee09c0c1c42eb4bbf39f894
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-09-08 18:20:38 +0000

    style: Fix List index lookup in `enumerate()` loop (PLR1736) in __writeNvizState

--- gui/wxpython/core/workspace.py
+++ gui/wxpython/core/workspace.py
@@ -1611,12 +1611,12 @@ class WriteWorkspaceFile:
             for idx, plane in enumerate(constants):
                 self.file.write("%s<plane>\n" % (" " * self.indent))
                 self.indent += 4
-                self.__writeTagWithValue("height", constants[idx]["constant"]["value"])
+                self.__writeTagWithValue("height", plane["constant"]["value"])
                 self.__writeTagWithValue(
-                    "fine_resolution", constants[idx]["constant"]["resolution"]
+                    "fine_resolution", plane["constant"]["resolution"]
                 )
                 self.__writeTagWithValue(
-                    "color", constants[idx]["constant"]["color"], format="s"
+                    "color", plane["constant"]["color"], format="s"
                 )
                 self.indent -= 4
                 self.file.write("%s</plane>\n" % (" " * self.indent))
