8c23c2ea61ff96ab8d161dacf6102bcf73f49cda
Makiko Shukunobe <mshukuno@users.noreply.github.com>
2024-09-12 11:49:18 -0400

    Checks: flake8 F841 (local variable assigned to but never used) fixes in the temporal directory (#4229)

--- temporal/t.rast.what/t.rast.what.py
+++ temporal/t.rast.what/t.rast.what.py
@@ -466,7 +466,6 @@ def one_point_per_col_output(
     for count in range(len(output_files)):
         file_name = output_files[count]
         gs.verbose(_("Transforming r.what output file %s" % (file_name)))
-        map_list = output_time_list[count]
         in_file = open(file_name, "r")
         lines = in_file.readlines()
 
