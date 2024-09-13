OSGeo/grass

commit f99780c11f8d4d1633e469fb32da2b6c75852b4f

style: Fix collapsible-else-if (PLR5501) (#4045)
* style: Fix collapsible-else-if (PLR5501)

Ruff rule: https://docs.astral.sh/ruff/rules/collapsible-else-if/

* style: Fix collapsible-else-if (PLR5501)

Ruff rule: https://docs.astral.sh/ruff/rules/collapsible-else-if/

* style: Fix collapsible-else-if (PLR5501)

Ruff rule: https://docs.astral.sh/ruff/rules/collapsible-else-if/

* Apply suggestions from code review

Co-authored-by: Stefan Blumentrath <stefan.blumentrath@gmx.de>

* style: Combine if conditions in wxpython/gmodeler/model.py

* style: Simplify if conditions in wxpython/gui_core

---------

Co-authored-by: Stefan Blumentrath <stefan.blumentrath@gmx.de>

gui/wxpython/gmodeler/model.py
@@ -947,12 +947,10 @@ class ModelObject:
 
         result = []
         for rel in self.rels:
-            if fdir == "from":
-                if rel.GetFrom() == self:
-                    result.append(rel)
-            else:
-                if rel.GetTo() == self:
-                    result.append(rel)
+            if fdir == "from" and rel.GetFrom() == self:
+                result.append(rel)
+            elif rel.GetTo() == self:
+                result.append(rel)
 
         return result
 
@@ -1040,10 +1038,7 @@ class ModelAction(ModelObject, ogl.DividedShape):
         if cmd:
             self.task = GUI(show=None).ParseCommand(cmd=cmd)
         else:
-            if task:
-                self.task = task
-            else:
-                self.task = None
+            self.task = task or None
 
         self.propWin = None
 

commit 2c1df9f89b308b6f0bb3df45763d92ff00c986a6

GUI: gmodeler fix adding tool (#4309)
* revert 2cf98da

* fix regression introduced in f99780c

gui/wxpython/gmodeler/model.py
@@ -941,8 +941,9 @@ class ModelObject:
 
         result = []
         for rel in self.rels:
-            if fdir == "from" and rel.GetFrom() == self:
-                result.append(rel)
+            if fdir == "from":
+                if rel.GetFrom() == self:
+                    result.append(rel)
             elif rel.GetTo() == self:
                 result.append(rel)
 
@@ -1577,7 +1578,7 @@ class ModelDataSingle(ModelData, ogl.EllipseShape):
         :param width, height: dimension of the shape
         :param x, y: position of the shape
         """
-        ogl.EllipseShape(self, width, height)
+        ogl.EllipseShape.__init__(self, width, height)  # noqa: PLC2801, C2801
         if self.parent.GetCanvas():
             self.SetCanvas(self.parent.GetCanvas())
 
@@ -1592,7 +1593,7 @@ class ModelDataSeries(ModelData, ogl.CompositeShape):
         :param width, height: dimension of the shape
         :param x, y: position of the shape
         """
-        ogl.CompositeShape(self)
+        ogl.CompositeShape.__init__(self)  # noqa: PLC2801, C2801
         if self.parent.GetCanvas():
             self.SetCanvas(self.parent.GetCanvas())
 