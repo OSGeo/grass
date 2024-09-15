873a4e13d7b0ad4a292dbb54c2c7b592b4a71e4f
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-09-15 18:30:30 +0000

    Add Edouard Choinière (echoix) to the authors

--- python/grass/gunittest/loader.py
+++ python/grass/gunittest/loader.py
@@ -6,7 +6,7 @@ This program is free software under the GNU General Public
 License (>=v2). Read the file COPYING that comes with GRASS GIS
 for details.
 
-:authors: Vaclav Petras
+:authors: Vaclav Petras, Edouard Choinière
 """
 
 from __future__ import annotations
