34ad40d5b17564c734d28893e9a6d8b9d7692d4b
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-09-18 19:45:55 -0400

    grass.gunittest: Add xfail_windows decorator for Windows expected failures

--- python/grass/gunittest/utils.py
+++ python/grass/gunittest/utils.py
@@ -14,6 +14,9 @@ import os
 from pathlib import Path
 import shutil
 import sys
+from unittest import expectedFailure
+import warnings
+
 
 
 def ensure_dir(directory):
@@ -80,3 +83,16 @@ def safe_repr(obj, short=False):
     if not short or len(result) < _MAX_LENGTH:
         return result
     return result[:_MAX_LENGTH] + " [truncated]..."
+
+
+def xfail_windows(test_item):
+    """Marks a test as an expected failure or error only on Windows
+    
+    Equivalent to applying @unittest.expectedFailure only when running
+    on Windows.
+    """
+    if not sys.platform.startswith("win"):
+        return lambda func: func
+    warnings.warn("Once the test is fixed and passing, remove the"
+        "@xfail_windows decorator")
+    return expectedFailure(test_item)
