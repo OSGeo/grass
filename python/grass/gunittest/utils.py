87c317db35aba3a04283daa52423c374162fbe69
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-09-18 21:03:45 -0400

    Update warning stacklevel to 3

--- python/grass/gunittest/utils.py
+++ python/grass/gunittest/utils.py
@@ -92,6 +92,6 @@ def xfail_windows(test_item):
     """
     if not sys.platform.startswith("win"):
         return lambda func: func
-    # warnings.warn("Once the test is fixed and passing, remove the "
-    #    "@xfail_windows decorator", stacklevel=2)
+    warnings.warn("Once the test is fixed and passing, remove the "
+        "@xfail_windows decorator", stacklevel=3)
     return expectedFailure(test_item)
