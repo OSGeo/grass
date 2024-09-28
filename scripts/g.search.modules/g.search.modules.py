8d2b1f8dd983314cca7826f4f7490a9998d68e88
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-09-28 16:49:09 -0400

    Update g.search.modules.py
    Co-authored-by: Stefan Blumentrath <stefan.blumentrath@gmx.de>

--- scripts/g.search.modules/g.search.modules.py
+++ scripts/g.search.modules/g.search.modules.py
@@ -311,7 +311,7 @@ def _exact_search(keyword, module_keywords):
     :param module_keywords: comma separated list of keywords
     """
     module_keywords = module_keywords.split(",")
-    return any(keyword == current for current in module_keywords)
+    return keyword in module_keywords
 
 
 def _manpage_search(pattern, name):
