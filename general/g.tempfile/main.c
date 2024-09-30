e8f16d22380aaeede1403601fd5dae3a069360fa
ninsbl <stefan.blumentrath@gmx.de>
2024-09-30 22:47:41 +0200

    use g_mkdir

--- general/g.tempfile/main.c
+++ general/g.tempfile/main.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
     /* create tempfile so next run of this program will create a unique name */
     if (!dry_run->answer) {
         if (directory->answer)
-            mkdir(tempfile, 0777);
+            G_mkdir(tempfile);
         else
             close(creat(tempfile, 0666));
     }
