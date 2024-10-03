aa1c32a59a029d0f78c00452f9085a0bac379144
Arohan Ajit <arohanajit232@gmail.com>
2024-10-02 21:46:53 -0400

    wxGUI: Fixed F841 in dialogs.py, frame.py (#4431)
    * fixed 841
    
    * removed dlgSize comments

--- gui/wxpython/iscatt/frame.py
+++ gui/wxpython/iscatt/frame.py
@@ -221,9 +221,6 @@ class ScatterPlotsPanel(scrolled.ScrolledPanel):
 
         self.Bind(aui.EVT_AUI_PANE_CLOSE, self.OnPlotPaneClosed)
 
-        dlgSize = (-1, 400)
-        # self.SetBestSize(dlgSize)
-        # self.SetInitialSize(dlgSize)
         self.SetAutoLayout(1)
         # fix goutput's pane size (required for Mac OSX)
         # if self.gwindow:
