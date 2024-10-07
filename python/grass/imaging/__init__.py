1c13ab0d8feec43c8112a0095b1d8fb6908c347e
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-10-07 03:00:43 +0000

    grass.imaging: Fix unused-import (F401) by defining __all__ in __init__

--- python/grass/imaging/__init__.py
+++ python/grass/imaging/__init__.py
@@ -2,3 +2,14 @@ from grass.imaging.images2gif import readGif, writeGif
 from grass.imaging.images2swf import readSwf, writeSwf
 from grass.imaging.images2avi import readAvi, writeAvi
 from grass.imaging.images2ims import readIms, writeIms
+
+__all__ = [
+    "readAvi",
+    "readGif",
+    "readIms",
+    "readSwf",
+    "writeAvi",
+    "writeGif",
+    "writeIms",
+    "writeSwf",
+]
