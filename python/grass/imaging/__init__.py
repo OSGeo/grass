d2becc99f385798681bf0cdc874bfe2b77b4a493
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-10-07 03:01:10 +0000

    grass.imaging: Apply isort

--- python/grass/imaging/__init__.py
+++ python/grass/imaging/__init__.py
@@ -1,7 +1,7 @@
-from grass.imaging.images2gif import readGif, writeGif
-from grass.imaging.images2swf import readSwf, writeSwf
 from grass.imaging.images2avi import readAvi, writeAvi
+from grass.imaging.images2gif import readGif, writeGif
 from grass.imaging.images2ims import readIms, writeIms
+from grass.imaging.images2swf import readSwf, writeSwf
 
 __all__ = [
     "readAvi",
