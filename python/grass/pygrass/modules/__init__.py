cdc43805e3b46a5f3c173e9002cc93252beef974
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-10-07 02:57:08 +0000

    grass.pygrass.modules: Fix unused-import (F401) by defining __all__ in __init__

--- python/grass/pygrass/modules/__init__.py
+++ python/grass/pygrass/modules/__init__.py
@@ -1,2 +1,9 @@
 from grass.pygrass.modules.interface import Module, MultiModule, ParallelModuleQueue
 from grass.pygrass.modules import shortcuts
+
+__all__ = [
+    "Module",
+    "MultiModule",
+    "ParallelModuleQueue",
+    "shortcuts",
+]
