6a6aa63f7aaa055ec7e5583a7e225d963f855202
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-10-07 02:57:50 +0000

    grass.pygrass.modules: Apply isort

--- python/grass/pygrass/modules/__init__.py
+++ python/grass/pygrass/modules/__init__.py
@@ -1,5 +1,5 @@
-from grass.pygrass.modules.interface import Module, MultiModule, ParallelModuleQueue
 from grass.pygrass.modules import shortcuts
+from grass.pygrass.modules.interface import Module, MultiModule, ParallelModuleQueue
 
 __all__ = [
     "Module",
