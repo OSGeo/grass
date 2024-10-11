b77a09d8101d5966c9306477432b48090fe837f2
Shubham Vasudeo Desai <sdesai8@vclvm176-239.vcl.ncsu.edu>
2024-10-11 16:26:52 -0400

    requested changes

--- imagery/i.ortho.photo/lib/find_init.c
+++ imagery/i.ortho.photo/lib/find_init.c
@@ -4,20 +4,19 @@
  * Find the a camera initial file in the current group (if it exists)
  **************************************************************/
 #include <grass/gis.h>
+#include <grass/glocale.h>
 
 int I_find_initial(char *group)
 {
-    char element[GNAME_MAX];
-    int file_exists;
+    char element[GNAME_MAX + 6];
 
     if (group == NULL || *group == 0)
         return 0;
 
     if (snprintf(element, GNAME_MAX, "group/%s", group) >= GNAME_MAX) {
-        G_warning("Group name truncated");
+        G_warning(_("Group name <%s> truncated"), group);
         return 0;
     }
 
-    file_exists = G_find_file(element, "INIT_EXP", G_mapset()) != NULL;
-    return file_exists;
+    return G_find_file(element, "INIT_EXP", G_mapset()) != NULL;
 }
