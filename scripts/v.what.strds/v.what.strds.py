1412791a95abbfa294977bcf1c427a036555c686
Arohan Ajit <arohanajit@Mac.lan>
2024-10-15 10:59:53 -0400

    fixed e722 in v.what.shrds

--- scripts/v.what.strds/v.what.strds.py
+++ scripts/v.what.strds/v.what.strds.py
@@ -225,7 +225,7 @@ def main():
     pymap = Vector(output)
     try:
         pymap.open("r")
-    except:
+    except (FileNotFoundError, CalledModuleError):
         dbif.close()
         gs.fatal(_("Unable to create vector map <%s>") % output)
 
