90ba33975fe30595e35628f055455727ab2e9789
rohannallamadge <rohannallamadge@gmail.com>
2024-10-17 15:51:55 +0530

    grass.jupyter: Add _repr_pretty_ for better Jupyter/Quarto integration

--- python/grass/jupyter/map3d.py
+++ python/grass/jupyter/map3d.py
@@ -232,6 +232,16 @@ class Map3D:
     def show(self):
         """Displays a PNG image of map"""
         # Lazy import to avoid an import-time dependency on IPython.
-        from IPython.display import Image  # pylint: disable=import-outside-toplevel
+        from IPython.display import Image, display  # pylint: disable=import-outside-toplevel
 
         return Image(self._filename)
+    def _repr_pretty_(self, p, cycle):
+        """Pretty representation of the object in IPython/Jupyter."""
+        # Lazy import to avoid an import-time dependency on IPython.
+        from IPython.display import Image, display  # pylint: disable=import-outside-toplevel
+
+        if cycle:
+            p.text("Map(...)")
+        else:
+            display(Image(self._filename))
+
