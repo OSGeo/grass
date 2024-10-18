dc0caa57e1e3a16476d6c24a8ba085155de32b94
Arohan Ajit <arohanajit@Mac.lan>
2024-10-18 11:35:19 -0400

    fixed 722

--- gui/wxpython/modules/import_export.py
+++ gui/wxpython/modules/import_export.py
@@ -495,7 +495,7 @@ class GdalImportDialog(ImportDialog):
             if nBandsStr:
                 try:
                     nBands = int(nBandsStr.rstrip("\n"))
-                except:
+                except (ValueError, TypeError):
                     pass
             if nBands < 0:
                 GWarning(_("Unable to determine number of raster bands"), parent=self)
