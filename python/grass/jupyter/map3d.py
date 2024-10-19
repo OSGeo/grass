05d9b90515c9e9ce05c00c80a882a7a27adacfd5
rohannallamadge <rohannallamadge@gmail.com>
2024-10-19 22:47:06 +0530

    style: reformat import statements as suggested by black

--- python/grass/jupyter/map3d.py
+++ python/grass/jupyter/map3d.py
@@ -232,6 +232,8 @@ class Map3D:
     def show(self):
         """Displays a PNG image of map"""
         # Lazy import to avoid an import-time dependency on IPython.
-        from IPython.display import Image, display  # pylint: disable=import-outside-toplevel
-
+        from IPython.display import (
+        Image,
+        display,
+        )  # pylint: disable=import-outside-toplevel
         display(Image(self._filename))
