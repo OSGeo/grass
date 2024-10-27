17969161d3251186e939bd33e8d7f027fededacc
Shubham Vasudeo Desai <sdesai8@vclvm176-77.vcl.ncsu.edu>
2024-10-27 16:25:32 -0400

    fix resource leak for cbuf

--- raster/r.watershed/seg/close_maps.c
+++ raster/r.watershed/seg/close_maps.c
@@ -326,6 +326,7 @@ int close_maps(void)
 
         Rast_close(fd);
         G_free(afbuf);
+        G_free(cbuf);
 
         Rast_init_colors(&colors);
         Rast_make_aspect_colors(&colors, -8, 8);
