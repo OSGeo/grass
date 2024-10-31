91b928468dbc74f39033f2bd6c0373a5880670c3
Arohan Ajit <arohanajit@Mac.lan>
2024-10-31 14:01:55 -0400

    removed e722

--- scripts/r.in.wms/wms_drv.py
+++ scripts/r.in.wms/wms_drv.py
@@ -24,7 +24,7 @@ from time import sleep
 
 try:
     from osgeo import gdal
-except:
+except ImportError:
     gs.fatal(
         _(
             "Unable to load GDAL Python bindings (requires package 'python-gdal' "
