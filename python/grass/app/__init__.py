a34f0e888f0d5a88168e5fa6e87d3b8f4b3a2076
Arohan Ajit <arohanajit@Mac.lan>
2024-11-04 16:09:45 -0500

    experimental update

--- python/grass/app/__init__.py
+++ python/grass/app/__init__.py
@@ -1,8 +1,6 @@
 from .data import (
     get_possible_database_path,
     create_database_directory,
-    _get_startup_location_in_distribution,
-    _copy_startup_location,
     create_startup_location_in_grassdb,
     ensure_default_data_hierarchy,
     MapsetLockingException,
@@ -11,8 +9,6 @@ from .data import (
 
 __all__ = [
     "MapsetLockingException",
-    "_copy_startup_location",
-    "_get_startup_location_in_distribution",
     "create_database_directory",
     "create_startup_location_in_grassdb",
     "ensure_default_data_hierarchy",
