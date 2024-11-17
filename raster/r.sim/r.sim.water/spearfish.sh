859d6ed33c3ced7acd20227805e4b14b627eb0c6
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-11-17 01:20:06 +0000

    r.sim.water: Address shellcheck warnings and format with shfmt
    `shfmt -w -s -i 4 -ci -bn -sr .`

--- raster/r.sim/r.sim.water/spearfish.sh
+++ raster/r.sim/r.sim.water/spearfish.sh
@@ -29,28 +29,28 @@ v.random --o output=observation_points n=5 -z
 
 echo "r.sim.water --o -t elevation=${dem} dx=${output}_dx dy=${output}_dy rain=${output}_rain man=${output}_manin infil=${output}_infil depth=${output}_depth disch=${output}_disch err=${output}_err outwalk=${output}_walker niter=5 outiter=1 observation=observation_points logfile=simwe.log"
 r.sim.water --o -t elevation=${dem} dx=${output}_dx dy=${output}_dy \
-            rain=${output}_rain man=${output}_manin infil=${output}_infil \
-            depth=${output}_depth disch=${output}_disch err=${output}_err \
-            outwalk=${output}_walker niter=5 outiter=1 observation=observation_points \
-            logfile=simwe.log
+    rain=${output}_rain man=${output}_manin infil=${output}_infil \
+    depth=${output}_depth disch=${output}_disch err=${output}_err \
+    outwalk=${output}_walker niter=5 outiter=1 observation=observation_points \
+    logfile=simwe.log
 
 echo "Plotting the observation points with R. Result in Rplots.pdf"
-R --vanilla -e "df = read.table(\"simwe.log\", header=1); par(mfrow=c(3,2)); plot(df\$STEP, df\$CAT0001, type=\"l\"); plot(df\$STEP, df\$CAT0002, type=\"l\"); plot(df\$STEP, df\$CAT0003, type=\"l\"); plot(df\$STEP, df\$CAT0004, type=\"l\");plot(df\$STEP, df\$CAT0005, type=\"l\")"
+R --vanilla -e 'df = read.table("simwe.log", header=1); par(mfrow=c(3,2)); plot(df$STEP, df$CAT0001, type="l"); plot(df$STEP, df$CAT0002, type="l"); plot(df$STEP, df$CAT0003, type="l"); plot(df$STEP, df$CAT0004, type="l");plot(df$STEP, df$CAT0005, type="l")'
 
 echo "Export of manning, soil and gradients"
 r.out.vtk elevation=${dem} input=${output}_manin,soils vectormaps=${output}_dx,${output}_dy,${output}_null output=manning_soils_gradient.vtk null=0.0
 
 echo "Build topology and exporting walker vector points for each time step"
-for i in `g.list vect | grep ${output}` ; do
-	v.build $i
-	echo "v.out.vtk input=$i output=$i.vtk"
-	v.out.vtk input=$i output=$i.vtk
+for i in $(g.list vect | grep ${output}); do
+    v.build "$i"
+    echo "v.out.vtk input=$i output=$i.vtk"
+    v.out.vtk input="$i" output="$i".vtk
 done
 
 echo "Exporting the raster maps for each time step"
-for i in `g.list rast | grep ${output}` ; do
-	echo "r.out.vtk elevation=${dem} input=$i output=$i.vtk null=0.0"
-	r.out.vtk elevation=${dem} input=$i output=$i.vtk null=0.0
+for i in $(g.list rast | grep ${output}); do
+    echo "r.out.vtk elevation=${dem} input=$i output=$i.vtk null=0.0"
+    r.out.vtk elevation=${dem} input="$i" output="$i".vtk null=0.0
 done
 
 echo "Now open paraview from this command line and load the vtk files as time series using the File->Open menu entry"
