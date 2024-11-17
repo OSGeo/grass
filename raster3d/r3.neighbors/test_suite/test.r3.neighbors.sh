46870ae21d23e142bbe2725f89f824d3b70339e6
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-11-17 01:54:02 +0000

    raster3d: Address shellcheck warnings and format with shfmt
    `shfmt -w -s -i 4 -ci -bn -sr .`

--- raster3d/r3.neighbors/test_suite/test.r3.neighbors.sh
+++ raster3d/r3.neighbors/test_suite/test.r3.neighbors.sh
@@ -4,9 +4,9 @@ export GRASS_OVERWRITE=1
 
 g.region w=0 e=30 s=0 n=30 b=0 t=30 res3=10
 
-r3.mapcalc  expr="test_neighbor_float  = float(3)"
-r3.mapcalc  expr="test_neighbor_double = double(3)"
-r3.mapcalc  expr="test_neighbor_null = null()"
+r3.mapcalc expr="test_neighbor_float  = float(3)"
+r3.mapcalc expr="test_neighbor_double = double(3)"
+r3.mapcalc expr="test_neighbor_null = null()"
 
 # First @test with float values with @precision=2
 r3.neighbors input=test_neighbor_float output=test_neighbor_float_average \
