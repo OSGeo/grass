a034543d4eee6045d7fd13f56ae36768068ccd79
Arohan Ajit <arohanajit@Mac.lan>
2024-11-16 20:49:12 -0500

    fixed import issue

--- gui/wxpython/web_services/widgets.py
+++ gui/wxpython/web_services/widgets.py
@@ -59,14 +59,15 @@ from gui_core.wrap import (
     TreeCtrl,
 )
 
+import grass.script as gs
+
 rinwms_path = os.path.join(os.getenv("GISBASE"), "etc", "r.in.wms")
 if rinwms_path not in sys.path:
     sys.path.append(rinwms_path)
 
 
 def init_imports():
-    global gs, WMSDriversInfo, Srs, Signal
-    import grass.script as gs
+    global WMSDriversInfo, Srs, Signal
     from wms_base import WMSDriversInfo
     from srs import Srs
     from grass.pydispatch.signal import Signal
