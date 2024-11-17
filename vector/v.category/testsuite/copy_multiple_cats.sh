7b6f4b6a872c1e92ff9c80e048156509c5c04394
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2024-11-17 03:29:49 +0000

    v.category: Address shellcheck warnings and format with shfmt
    `shfmt -w -s -i 4 -ci -bn -sr .`

--- vector/v.category/testsuite/copy_multiple_cats.sh
+++ vector/v.category/testsuite/copy_multiple_cats.sh
@@ -9,7 +9,7 @@ set -x
 
 echo "100|100|1" | v.in.ascii output=vcategory_test_1 input=-
 fid=$(v.edit --quiet layer=1 map=vcategory_test_1 tool=select cat=1)
-v.edit map=vcategory_test_1 layer=1 tool=catadd ids=${fid} cats=2
+v.edit map=vcategory_test_1 layer=1 tool=catadd ids="${fid}" cats=2
 v.category option=transfer layer=1,2 input=vcategory_test_1 output=vcategory_test_2
 
 expected="1/2"
