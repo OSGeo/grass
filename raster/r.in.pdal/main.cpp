b4752e4a8ef3b3ce36951b0b8ea38dd62d6eebef
Māris Nartišs <maris.gis@gmail.com>
2024-11-24 14:49:23 +0200

    r.in.pdal: update copyright notice

--- raster/r.in.pdal/main.cpp
+++ raster/r.in.pdal/main.cpp
@@ -4,12 +4,12 @@
  *
  * AUTHOR(S): Vaclav Petras
  *            Based on r.in.xyz and r.in.lidar by Markus Metz,
- *            Hamish Bowman, Volker Wichmann
+ *            Hamish Bowman, Volker Wichmann, Maris Nartiss
  *
  * PURPOSE:   Imports LAS LiDAR point clouds to a raster map using
  *            aggregate statistics.
  *
- * COPYRIGHT: (C) 2019-2021 by Vaclav Petras and the GRASS Development Team
+ * COPYRIGHT: (C) 2019-2024 by Vaclav Petras and the GRASS Development Team
  *
  *            This program is free software under the GNU General Public
  *            License (>=v2). Read the file COPYING that comes with
