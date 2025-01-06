1bd76e1a06022564d9824f71599b62f43400f8d4
Markus Neteler <neteler@gmail.com>
2025-01-06 23:32:16 +0100

    gui: Happy new year 2025
    `g.version` / wxGUI "About" dialog still point to 2024. This PR updates to 2025.
    
    I used the diff from last year's PR #3407 and updated it accordingly with a few extras.

--- python/grass/script/setup.py
+++ python/grass/script/setup.py
@@ -65,7 +65,7 @@ Usage::
     session.finish()
 
 
-(C) 2010-2024 by the GRASS Development Team
+(C) 2010-2025 by the GRASS Development Team
 This program is free software under the GNU General Public
 License (>=v2). Read the file COPYING that comes with GRASS
 for details.
