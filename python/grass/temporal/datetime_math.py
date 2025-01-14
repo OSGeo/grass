273d3aea84f196c510d48cdbce39a742e1a4de74
Arohan Ajit <arohanajit@arohan-8.local>
2025-01-14 15:02:02 -0500

    update

--- python/grass/temporal/datetime_math.py
+++ python/grass/temporal/datetime_math.py
@@ -21,7 +21,7 @@ try:
     from dateutil import parser
 
     has_dateutil = True
-except:
+except (ImportError, ModuleNotFoundError):  # More comprehensive exception handling
     has_dateutil = False
 
 
@@ -811,7 +811,7 @@ def check_datetime_string(time_string: str, use_dateutil: bool = True):
 
     try:
         return datetime.strptime(time_string, time_format)
-    except:
+    except (ValueError, TypeError):  # Handle both format and type errors
         return _("Unable to parse time string: %s") % time_string
 
 
