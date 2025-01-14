a6d9bb10c719c0416f836376de7a0726a0619c32
Arohan Ajit <arohanajit@arohan-8.local>
2025-01-14 15:06:00 -0500

    added error

--- python/grass/temporal/temporal_algebra.py
+++ python/grass/temporal/temporal_algebra.py
@@ -2857,7 +2857,7 @@ class TemporalAlgebraParser:
                         map_i.condition_value.append(boolname)
                     else:
                         map_i.condition_value = boolname
-                except (IndexError, AttributeError):
+                except (IndexError, AttributeError, SyntaxError):
                     self.msgr.fatal(
                         "Error: the given expression does not contain a correct time "
                         "difference object."
