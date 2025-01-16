8a9fb1ce0c95f025d28daab0f62dd4d3640b2081
Shubham Vasudeo Desai <sdesai8@vclvm178-92.vcl.ncsu.edu>
2025-01-16 11:31:55 -0500

    Fix Resource Leak issue

--- display/d.barscale/draw_scale.c
+++ display/d.barscale/draw_scale.c
@@ -634,8 +634,10 @@ int draw_scale(double east, double north, int length, int seg, int units,
     }
     D_stroke();
 
-    if (fontsize < 0)
+    if (fontsize < 0) {
+        G_free(label);
         return 0;
+    }    
 
     /* draw the distance + units text */
 
@@ -676,6 +678,7 @@ int draw_scale(double east, double north, int length, int seg, int units,
         D_pos_abs(x_pos + 5 - (tr - tl), y_pos + ysize / 2 + (tt - tb) / 2);
         D_text(label);
     }
+    G_free(label);
 
     return 0;
 }
