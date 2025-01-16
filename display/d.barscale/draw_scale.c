17e794bb780f994c1c88be19cc5bb7938b1ac090
Shubham Vasudeo Desai <sdesai8@vclvm178-92.vcl.ncsu.edu>
2025-01-16 12:07:35 -0500

    changes

--- display/d.barscale/draw_scale.c
+++ display/d.barscale/draw_scale.c
@@ -678,7 +678,6 @@ int draw_scale(double east, double north, int length, int seg, int units,
         D_pos_abs(x_pos + 5 - (tr - tl), y_pos + ysize / 2 + (tt - tb) / 2);
         D_text(label);
     }
-    G_free(label);
 
     return 0;
 }
