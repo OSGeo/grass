4f4207c911bb8e379d1c9cb585f66f1d0bb45618
Shubham Vasudeo Desai <sdesai8@vclvm178-92.vcl.ncsu.edu>
2025-01-22 13:09:30 -0500

    update

--- lib/vector/Vlib/open_ogr.c
+++ lib/vector/Vlib/open_ogr.c
@@ -148,13 +148,13 @@ int V2_open_old_ogr(struct Map_info *Map)
 
     G_debug(3, "V2_open_old_ogr(): name = %s mapset = %s", Map->name,
             Map->mapset);
-    char *map_name = NULL;
+    const char *map_name = NULL;
 
     if (Vect_open_fidx(Map, &(Map->fInfo.ogr.offset)) != 0) {
         map_name = Vect_get_full_name(Map);
         G_warning(_("Unable to open feature index file for vector map <%s>"),
                   map_name);
-        G_free(map_name);
+        G_free((void *)map_name);
         G_zero(&(Map->fInfo.ogr.offset), sizeof(struct Format_info_offset));
     }
 
@@ -271,10 +271,10 @@ int Vect_open_fidx(struct Map_info *Map, struct Format_info_offset *offset)
     dig_file_init(&fp);
     fp.file = G_fopen_old(elem, GV_FIDX_ELEMENT, Map->mapset);
     if (fp.file == NULL) {
-        char *map_name = Vect_get_full_name(Map);
+        const char *map_name = Vect_get_full_name(Map);
         G_debug(1, "unable to open fidx file for vector map <%s>",
                 map_name);
-        G_free(map_name);
+        G_free((void *)map_name);
         return -1;
     }
 
