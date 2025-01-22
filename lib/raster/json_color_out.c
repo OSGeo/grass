fbabf32052e1d4b5cda3be53b5e45a22019ae9f4
Nishant Bansal <103022832+NishantBansal2003@users.noreply.github.com>
2025-01-22 08:25:13 +0530

    v.colors.out: Add JSON support (#4964)

--- lib/raster/json_color_out.c
+++ lib/raster/json_color_out.c
@@ -99,24 +99,24 @@ static void set_color(int r, int g, int b, ColorFormat clr_frmt,
     case RGB:
         snprintf(color_string, sizeof(color_string), "rgb(%d, %d, %d)", r, g,
                  b);
-        G_json_object_set_string(color_object, "rgb", color_string);
+        G_json_object_set_string(color_object, "color", color_string);
         break;
 
     case HEX:
         snprintf(color_string, sizeof(color_string), "#%02X%02X%02X", r, g, b);
-        G_json_object_set_string(color_object, "hex", color_string);
+        G_json_object_set_string(color_object, "color", color_string);
         break;
 
     case HSV:
         rgb_to_hsv(r, g, b, &h, &s, &v);
         snprintf(color_string, sizeof(color_string), "hsv(%d, %d, %d)", (int)h,
                  (int)s, (int)v);
-        G_json_object_set_string(color_object, "hsv", color_string);
+        G_json_object_set_string(color_object, "color", color_string);
         break;
 
     case TRIPLET:
         snprintf(color_string, sizeof(color_string), "%d:%d:%d", r, g, b);
-        G_json_object_set_string(color_object, "triplet", color_string);
+        G_json_object_set_string(color_object, "color", color_string);
         break;
     }
 }
