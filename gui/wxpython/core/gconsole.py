99ddf21fb0abc10c3114e729667b999e1560ca91
Vaclav Petras <wenzeslaus@gmail.com>
2025-01-27 13:22:31 -0500

    Prefering standard API over RunCmd for API-like calls. giface is for running tools based on direct user input.

--- gui/wxpython/core/gconsole.py
+++ gui/wxpython/core/gconsole.py
@@ -856,7 +856,6 @@ class GConsole(wx.EvtHandler):
             for p in task.get_options()["flags"]:
                 if p.get("name") == "r" and p.get("value"):
                     action = "delete"
-            # TODO: Use giface here.
             mask_full_name = gs.parse_command("r.mask.status", format="json")["name"]
             mask_name, mask_mapset = mask_full_name.split("@", maxsplit=1)
             gisenv = gs.gisenv()
