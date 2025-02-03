c3f43c3f4b74f6a30fa7ed0a32a25602a0f004e1
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2025-02-03 15:57:25 -0500

    v.to.3d: Fix Resource Leak issue in trans3.c (#5028)

--- vector/v.to.3d/trans3.c
+++ vector/v.to.3d/trans3.c
@@ -148,6 +148,7 @@ void trans3d(struct Map_info *In, struct Map_info *Out, int type,
 
     Vect_destroy_line_struct(Points);
     Vect_destroy_cats_struct(Cats);
+    Vect_destroy_field_info(Fi);
 }
 
 int srch(const void *pa, const void *pb)
