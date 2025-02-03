d09b87ce7f342e397a4f33c2618c6cebff89a285
Shubham Vasudeo Desai <sdesai8@vclvm178-177.vcl.ncsu.edu>
2025-02-03 14:15:14 -0500

    remove check

--- lib/nviz/exag.c
+++ lib/nviz/exag.c
@@ -64,8 +64,7 @@ int Nviz_get_exag_height(double *val, double *min, double *max)
 
     G_debug(1, "Nviz_get_exag_height(): value = %f min = %f max = %f", *val,
             min ? *min : 0.0, max ? *max : 0.0);
-    if (nsurfs > 0)
-        G_free(surf_list);
+    G_free(surf_list);
 
     return 1;
 }
@@ -95,9 +94,7 @@ double Nviz_get_exag(void)
 
     if (exag == 0.0)
         exag = 1.0;
-
-    if (nsurfs > 0)
-        G_free(surf_list);
+    G_free(surf_list);
 
     G_debug(1, "Nviz_get_exag(): value = %f", exag);
     return exag;
