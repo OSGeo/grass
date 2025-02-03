46d633dab15ab3e7bbbce0a4993281ce9cb16b51
Shubham Vasudeo Desai <sdesai8@vclvm178-177.vcl.ncsu.edu>
2025-02-03 12:39:28 -0500

    changes

--- display/d.vect.chart/plot.c
+++ display/d.vect.chart/plot.c
@@ -27,8 +27,6 @@ int plot(int ctype, struct Map_info *Map, int type, int field, char *columns,
     dbTable *table;
     dbColumn *column;
 
-    Points = Vect_new_line_struct();
-    Cats = Vect_new_cats_struct();
     db_init_string(&sql);
 
     Fi = Vect_get_field(Map, field);
@@ -40,11 +38,11 @@ int plot(int ctype, struct Map_info *Map, int type, int field, char *columns,
     if (driver == NULL) {
         G_warning(_("Unable to open database <%s> by driver <%s>"),
                   Fi->database, Fi->driver);
-        Vect_destroy_line_struct(Points);
-        Vect_destroy_cats_struct(Cats);
         Vect_destroy_field_info(Fi);
         return 1;
     }
+    Points = Vect_new_line_struct();
+    Cats = Vect_new_cats_struct();
     db_set_error_handler_driver(driver);
 
     val =
