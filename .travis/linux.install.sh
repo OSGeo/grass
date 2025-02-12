631cecb17797ef020053a320404890b5f713d72e
Vaclav Petras <wenzeslaus@gmail.com>
2025-02-12 16:04:12 -0500

    ci: Remove Travis-CI
    As explained in #4313, OSGeo ends its Travis subscription. Travis is not a crucial component of our CI, so we are removing it.
    
    Additionally, travis became problematic after the Markdown documentation build was enabled because Travis limits size of the log and terminates the build if it hits the limit.

--- .travis/linux.install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Author: Ivan Mincik, ivan.mincik@gmail.com
-
-set -e
-
-sudo apt-get update -y
-    sudo apt-get install -y wget git gawk findutils
-    xargs -a <(awk '! /^ *(#|$)/' ".github/workflows/apt.txt") -r -- \
-        sudo apt-get install -y --no-install-recommends --no-install-suggests
