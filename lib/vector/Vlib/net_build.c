OSGeo/grass

commit fa0dbb002c0e89e19e9a1a215febd10910903db3

lib/vector/Vlib: Fix Resource Leak issue in net_build.c (#5038)

lib/vector/Vlib/net_build.c
@@ -666,6 +666,8 @@ int Vect_net_ttb_build_graph(struct Map_info *Map, int ltype, int afield,
     dglInitializeSPCache(gr, &(Map->dgraph.spCache));
 
     G_message(_("Graph was built"));
+    Vect_destroy_field_info(Fi);
+    db_free_column(Column);
     return 0;
 }
 
@@ -1061,6 +1063,8 @@ int Vect_net_build_graph(struct Map_info *Map, int ltype, int afield,
     dglInitializeSPCache(gr, &(Map->dgraph.spCache));
 
     G_message(_("Graph was built"));
+    Vect_destroy_field_info(Fi);
+    db_free_column(Column);
 
     return 0;
 }

commit 4b92908f56d5950ab16d5ae0a229753f7dd9cf72

lib/vector/Vlib: fix segfault introduced with #5038

lib/vector/Vlib/net_build.c
@@ -60,12 +60,12 @@ int Vect_net_ttb_build_graph(struct Map_info *Map, int ltype, int afield,
     dglGraph_s *gr;
     dglInt32_t opaqueset[16] = {360000, 0, 0, 0, 0, 0, 0, 0,
                                 0,      0, 0, 0, 0, 0, 0, 0};
-    struct field_info *Fi;
+    struct field_info *Fi = NULL;
     dbDriver *driver = NULL;
     dbDriver *ttbdriver = NULL;
     dbHandle handle;
     dbString stmt;
-    dbColumn *Column;
+    dbColumn *Column = NULL;
     dbCatValArray fvarr, bvarr;
     int fctype = 0, bctype = 0, nrec, nturns;
 
@@ -666,8 +666,13 @@ int Vect_net_ttb_build_graph(struct Map_info *Map, int ltype, int afield,
     dglInitializeSPCache(gr, &(Map->dgraph.spCache));
 
     G_message(_("Graph was built"));
-    Vect_destroy_field_info(Fi);
-    db_free_column(Column);
+    if (Fi) {
+        Vect_destroy_field_info(Fi);
+    }
+    if (Column) {
+        db_free_column(Column);
+    }
+
     return 0;
 }
 
@@ -709,11 +714,11 @@ int Vect_net_build_graph(struct Map_info *Map, int ltype, int afield,
     dglInt32_t dgl_cost;
     dglInt32_t opaqueset[16] = {360000, 0, 0, 0, 0, 0, 0, 0,
                                 0,      0, 0, 0, 0, 0, 0, 0};
-    struct field_info *Fi;
+    struct field_info *Fi = NULL;
     dbDriver *driver = NULL;
     dbHandle handle;
     dbString stmt;
-    dbColumn *Column;
+    dbColumn *Column = NULL;
     dbCatValArray fvarr, bvarr;
     int fctype = 0, bctype = 0, nrec;
 
@@ -1063,8 +1068,12 @@ int Vect_net_build_graph(struct Map_info *Map, int ltype, int afield,
     dglInitializeSPCache(gr, &(Map->dgraph.spCache));
 
     G_message(_("Graph was built"));
-    Vect_destroy_field_info(Fi);
-    db_free_column(Column);
+    if (Fi) {
+        Vect_destroy_field_info(Fi);
+    }
+    if (Column) {
+        db_free_column(Column);
+    }
 
     return 0;
 }