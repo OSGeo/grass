c8d6ab9eab6cfba5685493ecad0ebc4a22c75725
Huidae Cho <grass4u@gmail.com>
2025-02-21 08:51:38 -0700

    Clean up msvc/unistd.h

--- msvc/unistd.h
+++ msvc/unistd.h
@@ -1,34 +1,20 @@
 #ifndef GRASS_MSVC_UNISTD_H
 #define GRASS_MSVC_UNISTD_H
 
-#include <windows.h>
 #include <io.h>
-#include <direct.h>
-#include <process.h>
-#include <time.h>
-#include <synchapi.h>
-
 #define access _access
 #define close  _close
 #define dup    _dup
 #define dup2   _dup2
 #define unlink _unlink
+#define isatty _isatty
+
+#include <direct.h>
 #define rmdir  _rmdir
 #define getcwd _getcwd
 #define chdir  _chdir
-#define isatty _isatty
+
+#include <process.h>
 #define getpid _getpid
-
-static inline int usleep(useconds_t usec)
-{
-    Sleep(usec / 1000); /* convert microseconds to milliseconds */
-    return 0;
-}
-
-static inline unsigned int sleep(unsigned int seconds)
-{
-    Sleep(seconds * 1000);
-    return 0;
-}
 
 #endif
