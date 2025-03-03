8ca405625015c6e4b69573698ac5ea919ae70a84
Vaclav Petras <wenzeslaus@gmail.com>
2025-03-03 12:23:29 -0500

    docs: Add captions to Markdown manual gallery (#5250)
    This adds simple captions to the images in the manual gallery in Markdown just like the custom HTML doc has.
    
    Additionally, this skips small images if PIL is available on the system (but does not fail and instead uses all if PIS is not available). It tests width only because the images are shown line by line and that's what we have in the style guide. This saves about 10 sec in the build (from 30 to 20 sec).
    
    It also sorts the images by the page name they link to which happens to put the display tools on top.

--- man/build_manual_gallery.py
+++ man/build_manual_gallery.py
@@ -19,6 +19,7 @@ from pathlib import Path
 import fnmatch
 import re
 from typing import TYPE_CHECKING
+import operator
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
@@ -91,6 +92,19 @@ header_graphical_index_tmpl = """\
 <hr class="header">
 <h2>GRASS GIS manual gallery</h2>
 """
+
+# The recommeded width from style guide.
+MIN_IMAGE_WIDTH = 600
+
+
+def image_width(filename):
+    """Get image size in pixels (width times height)"""
+    try:
+        from PIL import Image
+    except ImportError:
+        return None
+
+    return Image.open(filename).size[0]  # First element is width.
 
 
 def img_in_file(filename: str | os.PathLike[str], imagename: str, ext: str) -> bool:
@@ -167,6 +181,10 @@ def main(ext):
         if filename in img_blacklist:
             continue
         if file_matches(filename, img_patterns):
+            width = image_width(Path(man_dir, filename))
+            if width is not None and width < MIN_IMAGE_WIDTH:
+                # Skip small images.
+                continue
             for man_filename in man_files:
                 if img_in_file(Path(man_dir, man_filename), filename, ext):
                     img_files[filename] = man_filename
@@ -182,7 +200,7 @@ def main(ext):
         if ext == "html":
             output.write(header_graphical_index_tmpl)
             output.write('<ul class="img-list">\n')
-        for image, filename in sorted(img_files.items()):
+        for image, filename in sorted(img_files.items(), key=operator.itemgetter(1)):
             name = get_module_name(filename, ext)
             title = title_from_names(name, image)
             if ext == "html":
@@ -195,7 +213,9 @@ def main(ext):
                     "</li>\n".format(fn=filename, img=image, title=title, name=name)
                 )
             else:
-                output.write(f'[![{name}]({image} "{title}")]({filename})\n')
+                output.write(
+                    f'[![{name}]({image} "{title}")]({filename})  \n*{title}*\n\n'
+                )
         if ext == "html":
             output.write("</ul>")
         write_footer(output, f"index.{ext}", year, template=ext)
