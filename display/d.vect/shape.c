1f4583a5190439256c6f578035e3d6690c589ff5
Shubham Vasudeo Desai <sdesai8@vclvm177-155.vcl.ncsu.edu>
2025-03-03 22:31:47 -0500

    fix

--- display/d.vect/shape.c
+++ display/d.vect/shape.c
@@ -178,9 +178,7 @@ int display_shape(struct Map_info *Map, int type, struct cat_list *Clist,
         }
     }
 
-    if (open_db) {
-        db_close_database_shutdown_driver(driver);
-    }
+    
 
     if (z_style) {
         if (!Vect_is_3d(Map)) {
@@ -224,7 +222,10 @@ int display_shape(struct Map_info *Map, int type, struct cat_list *Clist,
         cats_colors_flag, default_width, width_scale, z_style ? &zcolors : NULL,
         rgb_column ? &cvarr_rgb : NULL, have_colors ? &colors : NULL,
         &cvarr_width, nrec_width, &cvarr_size, nrec_size, &cvarr_rot, nrec_rot);
-    Vect_destroy_field_info(fi);
+    if (open_db) {
+        db_close_database_shutdown_driver(driver);
+        Vect_destroy_field_info(fi);
+    }
 
     return stat;
 }
