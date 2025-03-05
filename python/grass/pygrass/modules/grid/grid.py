6c402a93ff512e1f6d4634a5d7ac306762f4c66b
Arohan Ajit <arohanajit232@gmail.com>
2025-03-05 10:30:26 -0500

    fixed macos test

--- python/grass/pygrass/modules/grid/grid.py
+++ python/grass/pygrass/modules/grid/grid.py
@@ -59,7 +59,7 @@ def copy_special_mapset_files(path_src, path_dst):
     :type path_dst: str
     """
     for fil in (fi for fi in Path(path_src).iterdir() if fi.name.isupper()):
-        sht.copy(os.path.join(path_src, fil.name), path_dst)
+        sht.copy(fil, os.path.join(path_dst, fil.name))
 
 
 def copy_mapset(mapset, path):
