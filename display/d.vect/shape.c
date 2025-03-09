58bfbc28c2f62644e976eb12887e364cb7ac665b
Shubham Vasudeo Desai <sdesai8@vclvm179-48.vcl.ncsu.edu>
2025-03-09 12:19:07 -0400

    fix

--- display/d.vect/shape.c
+++ display/d.vect/shape.c
@@ -178,10 +178,6 @@ int display_shape(struct Map_info *Map, int type, struct cat_list *Clist,
         }
     }
 
-    if (open_db) {
-        db_close_database_shutdown_driver(driver);
-    }
-
     if (z_style) {
         if (!Vect_is_3d(Map)) {
             G_warning(_("Vector map is not 3D. Unable to colorize features "
@@ -224,6 +220,10 @@ int display_shape(struct Map_info *Map, int type, struct cat_list *Clist,
         cats_colors_flag, default_width, width_scale, z_style ? &zcolors : NULL,
         rgb_column ? &cvarr_rgb : NULL, have_colors ? &colors : NULL,
         &cvarr_width, nrec_width, &cvarr_size, nrec_size, &cvarr_rot, nrec_rot);
+    if (open_db) {
+        db_close_database_shutdown_driver(driver);
+        Vect_destroy_field_info(fi);
+    }
 
     return stat;
 }
