dc19150f754aa7b26eba5b15a6891226fdff44a6
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2025-03-27 08:35:39 -0400

    d.barscale: Fix uninitialized Variable in draw_scale.c (#5450)

--- display/d.barscale/draw_scale.c
+++ display/d.barscale/draw_scale.c
@@ -66,8 +66,8 @@ int draw_scale(double east, double north, int length, int seg, int units,
     int i, incr;
     double x_pos, y_pos;
     double t, b, l, r;
-    double pt, pb, pl, pr; /* background box */
-    double tt, tb, tl, tr; /* text box */
+    double pt = 0.0, pb = 0.0, pl = 0.0, pr = 0.0; /* background box */
+    double tt, tb, tl, tr;                         /* text box */
     double xarr[5], yarr[5];
     double seg_len;
     const struct scale *scales = all_scales[use_feet];
