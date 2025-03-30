a161aac5e5d2fd9472761b73e3d8b39885c54921
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-03-30 21:43:10 +0000

    python: Fix raise-without-from-inside-except (B904)
    Rule contents: Within an `except` clause, raise exceptions with `raise ... from err` or `raise ... from None` to distinguish them from errors in exception handling

--- python/grass/gunittest/loader.py
+++ python/grass/gunittest/loader.py
@@ -180,7 +180,7 @@ def discover_modules(
                     raise ImportError(
                         "Cannot import module named %s in %s (%s)"
                         % (name, full, e.message)
-                    )
+                    ) from e
                     # alternative is to create TestClass which will raise
                     # see unittest.loader
             if add:
