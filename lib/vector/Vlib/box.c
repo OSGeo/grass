99e8872101a5f1e6eea4d4242213b6603c43ad61
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2025-04-02 16:00:45 -0400

    Update lib/vector/Vlib/box.c
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/vector/Vlib/box.c
+++ lib/vector/Vlib/box.c
@@ -422,7 +422,8 @@ int Vect_get_map_box1(struct Map_info *Map, struct bound_box *Box)
     Vect_rewind(Map);
     const char *map_name = Vect_get_full_name(Map);
     G_verbose_message(_("Topology not available for vector map <%s>. "
-                        "Registering primitives..."), map_name);
+                        "Registering primitives..."),
+                      map_name);
     G_free((void *)map_name);
     while (TRUE) {
         /* register line */
