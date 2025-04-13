4bf91eadefbf64ea7da0524dfbcacb1aabb9a82e
Vedant Patel <104895751+Kiraschild@users.noreply.github.com>
2025-04-12 20:28:28 -0400

    vector: Adding edge cases to v_delaunay_test.py

--- vector/v.delaunay/testsuite/v_delaunay_test.py
+++ vector/v.delaunay/testsuite/v_delaunay_test.py
@@ -21,12 +21,43 @@ class TestVDelaunay(TestCase):
             "1 1\n1 2\n2 1\n2 2\n11 11\n11 12\n12 11\n12 12\n50 50\n50 51\n51 50\n51 51\n100 100"
         )
 
+        self.collinear_points = self.create_temp_file("0 0\n10 10\n20 20\n30 30\n40 40")
+        self.three_points = self.create_temp_file("5 5\n5 10\n10 10")
+        self.duplicate_points = self.create_temp_file("5 5\n5 10\n10 10\n5 5\n5 10")
+
         self.runModule(
             "v.in.ascii",
             input=self.temp_points,
             format="point",
             separator="space",
             output="points",
+            overwrite=True,
+        )
+
+        self.runModule(
+            "v.in.ascii",
+            input=self.collinear_points,
+            format="point",
+            separator="space",
+            output="collinear_points",
+            overwrite=True,
+        )
+
+        self.runModule(
+            "v.in.ascii",
+            input=self.three_points,
+            format="point",
+            separator="space",
+            output="three_points",
+            overwrite=True,
+        )
+
+        self.runModule(
+            "v.in.ascii",
+            input=self.duplicate_points,
+            format="point",
+            separator="space",
+            output="duplicate_points",
             overwrite=True,
         )
 
@@ -53,15 +84,58 @@ class TestVDelaunay(TestCase):
         )
 
     def test_triangulation(self):
+        """tests the basic triangulation done by v.delaunay"""
         topology_before = vector_info_topo("test_points")
         self.assertModule(
             "v.delaunay", input="test_points", output="output_delaunay", overwrite=True
         )
         topology_after = vector_info_topo("output_delaunay")
         self.assertVectorExists("output_delaunay")
+        self.assertGreater(topology_after["areas"], 0)
         self.assertGreater(topology_after["areas"], topology_before["areas"])
 
+        # Checking for if the triangulation created a planar graph or not
+        vertices = topology_after["nodes"]
+        edges = topology_after["boundaries"]
+        faces = topology_after["areas"]
+        self.assertEqual(vertices - edges + faces, 1, "Invalid topology")
+
+    def test_collinear_points(self):
+        """Tests how v.delaunay reacts to collinear points. Expected 0 areas in output map"""
+        self.assertModule(
+            "v.delaunay",
+            input="collinear_points",
+            output="output_collinear",
+            overwrite=True,
+        )
+        self.assertVectorExists("output_collinear")
+        topology = vector_info_topo("output_collinear")
+        self.assertEqual(topology["areas"], 0)
+
+    def test_minimum_points(self):
+        """Testing v.delaunay for minimum points i.e. 3"""
+        self.assertModule(
+            "v.delaunay", input="three_points", output="output_3points", overwrite=True
+        )
+        self.assertVectorExists("output_3points")
+        topology = vector_info_topo("output_3points")
+        self.assertEqual(topology["areas"], 1)
+
+    def test_duplicate_points(self):
+        """Testing the effect that duplicate points would have"""
+        self.assertModule(
+            "v.delaunay",
+            input="duplicate_points",
+            output="output_duplicate",
+            overwrite=True,
+        )
+        self.assertVectorExists("output_duplicate")
+        topology = vector_info_topo("output_duplicate")
+        self.assertEqual(topology["nodes"], 3, "Should remove duplicates")
+        self.assertEqual(topology["areas"], 1, "Should create one triangle")
+
     def test_r_flag(self):
+        """Tests the effect of r flag"""
         self.runModule("g.region", n=15, s=0, e=15, w=0)
 
         self.assertModule(
@@ -76,6 +150,7 @@ class TestVDelaunay(TestCase):
         self.assertEqual(info["nodes"], 8)
 
     def test_l_flag(self):
+        """Tests the effect of l flag"""
         self.assertModule(
             "v.delaunay", input="test_points", output="output_delaunay", overwrite=True
         )
@@ -90,6 +165,7 @@ class TestVDelaunay(TestCase):
             flags="l",
             overwrite=True,
         )
+
         topology_l_flag = vector_info_topo("output_l_flag")
         self.assertGreater(topology_l_flag["lines"], 0)
         self.assertEqual(topology_l_flag["areas"], 0)
