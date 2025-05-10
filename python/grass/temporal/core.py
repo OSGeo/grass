OSGeo/grass

commit 3b1d8897390fc38a964d7a82f2a4cb0588aa4461

temporal: Fix bare except statements with specific error handling (#4805)
Co-authored-by: Edouard Choinière <27212526+echoix@users.noreply.github.com>

python/grass/temporal/core.py
@@ -52,7 +52,7 @@ except ImportError:
 try:
     import psycopg2
     import psycopg2.extras
-except:
+except ImportError:
     pass
 
 import atexit
@@ -414,7 +414,7 @@ def get_tgis_metadata(dbif=None):
         statement = "SELECT * FROM tgis_metadata;\n"
         dbif.execute(statement)
         rows = dbif.fetchall()
-    except:
+    except Exception:
         rows = None
 
     if connection_state_changed:
@@ -1501,7 +1501,7 @@ class DBConnection:
                 self.cursor.execute(statement, args)
             else:
                 self.cursor.execute(statement)
-        except:
+        except (sqlite3.Error, psycopg2.Error):
             if connected:
                 self.close()
             self.msgr.error(_("Unable to execute :\n %(sql)s") % {"sql": statement})
@@ -1544,7 +1544,7 @@ class DBConnection:
             else:
                 self.cursor.execute(statement)
             self.connection.commit()
-        except:
+        except (sqlite3.Error, psycopg2.Error):
             if connected:
                 self.close()
             self.msgr.error(

.flake8
@@ -48,9 +48,6 @@ per-file-ignores =
     # TODO: Is this really needed?
     python/grass/pygrass/vector/__init__.py: E402
     python/grass/pygrass/vector/__init__.py: E402
-    python/grass/temporal/abstract_space_time_dataset.py: E722
-    python/grass/temporal/c_libraries_interface.py: E722
-    python/grass/temporal/core.py: E722
     python/grass/temporal/datetime_math.py: E722
     python/grass/temporal/spatial_topology_dataset_connector.py: E722
     python/grass/temporal/temporal_algebra.py: E722

python/grass/temporal/abstract_space_time_dataset.py
@@ -19,6 +19,7 @@ import uuid
 from abc import ABCMeta, abstractmethod
 from datetime import datetime
 from pathlib import Path
+from grass.exceptions import FatalError
 
 from .abstract_dataset import AbstractDataset, AbstractDatasetComparisonKeyStartTime
 from .core import (
@@ -2463,7 +2464,9 @@ class AbstractSpaceTimeDataset(AbstractDataset):
             try:
                 dbif.execute(sql, (map_id,), mapset=self.base.mapset)
                 row = dbif.fetchone(mapset=self.base.mapset)
-            except:
+            except FatalError:
+                raise
+            except Exception:
                 self.msgr.warning(_("Error in register table request"))
                 raise
 

commit 813c4202c31a6eb497d54e94b0864daa1de02c36

grass.temporal: fix error handling (#5631)
The problem is caused by #4805, where the psycopg2.Error is used, even when the package is not available.

I also removed try except for sqlite3 since it has been part of standard Python package since Python 2.5.

python/grass/temporal/core.py
@@ -44,16 +44,17 @@ from .c_libraries_interface import CLibrariesInterface
 
 # Import all supported database backends
 # Ignore import errors since they are checked later
-try:
-    import sqlite3
-except ImportError:
-    pass
+
+import sqlite3
+
 # Postgresql is optional, existence is checked when needed
 try:
     import psycopg2
     import psycopg2.extras
+
+    db_errors = (sqlite3.Error, psycopg2.Error)
 except ImportError:
-    pass
+    db_errors = (sqlite3.Error,)
 
 import atexit
 from datetime import datetime
@@ -1500,7 +1501,7 @@ class DBConnection:
                 self.cursor.execute(statement, args)
             else:
                 self.cursor.execute(statement)
-        except (sqlite3.Error, psycopg2.Error):
+        except db_errors:
             if connected:
                 self.close()
             self.msgr.error(_("Unable to execute :\n %(sql)s") % {"sql": statement})
@@ -1543,7 +1544,7 @@ class DBConnection:
             else:
                 self.cursor.execute(statement)
             self.connection.commit()
-        except (sqlite3.Error, psycopg2.Error):
+        except db_errors:
             if connected:
                 self.close()
             self.msgr.error(