ff5cb6e07a5e5a7774a002ab313318f0aaafc79e
pesekon2 <pesej.ondrek@gmail.com>
2025-05-22 22:51:01 +0200

    Python: pygrass.vector.table.DBlinks - improve error messages

--- python/grass/pygrass/vector/table.py
+++ python/grass/pygrass/vector/table.py
@@ -955,11 +955,16 @@ class DBlinks:
         """
         nlinks = self.num_dblinks()
         if nlinks == 0:
-            raise IndexError
+            raise IndexError(_("No links available."))
         if indx < 0:
             indx += nlinks
         if indx > nlinks:
-            raise IndexError
+            raise IndexError(
+                _(
+                    "Index out of bounds. The maximum index allowed is "
+                    "{max_link_indx}, the queried index is {indx}"
+                ).format(max_link_indx=nlinks - 1, indx=indx)
+            )
         c_fieldinfo = libvect.Vect_get_dblink(self.c_mapinfo, indx)
         return Link(c_fieldinfo=c_fieldinfo) if c_fieldinfo else None
 
