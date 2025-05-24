722c50de094e34fc4d42375383e3dc2a14e7ebd3
Huidae Cho <grass4u@gmail.com>
2025-05-24 13:37:46 -0600

    MSVC: Use the same M_PI_4 value to avoid redefinition warnings

--- include/grass/gis.h
+++ include/grass/gis.h
@@ -160,7 +160,11 @@ static const char *GRASS_copyright UNUSED = "GRASS GNU GPL licensed Software";
 #define M_PI_2 1.57079632679489661923 /* pi/2 */
 
 #undef M_PI_4
+#ifdef _MSC_VER
+#define M_PI_4 0.785398163397448309616 /* pi/4 */
+#else
 #define M_PI_4 0.78539816339744830962 /* pi/4 */
+#endif
 
 #undef M_R2D
 #define M_R2D 57.295779513082320877 /* 180/pi */
