a384da16e289372e81a28cef812f680ac7d80d45
Nishant Bansal <nishant.bansal.282003@gmail.com>
2025-05-30 23:48:29 +0530

    apply suggestions from code review
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- general/g.mapset/tests/conftest.py
+++ general/g.mapset/tests/conftest.py
@@ -10,22 +10,21 @@ TEST_MAPSETS = ["PERMANENT", "test1", "test2", "test3"]
 def simple_dataset(tmp_path_factory):
     """Set up a GRASS session for the tests."""
     tmp_path = tmp_path_factory.mktemp("simple_dataset")
-    location = "test"
+    project = "test"
 
-    # Create a test location
-    gs.create_project(tmp_path, location)
+    # Create a test project
+    gs.create_project(tmp_path, project)
 
     # Initialize the GRASS session
-    with gs.setup.init(tmp_path / location):
+    with gs.setup.init(tmp_path / project):
         # Create Mock Mapsets
         for mapset in TEST_MAPSETS:
-            gs.run_command("g.mapset", project=location, mapset=mapset, flags="c")
+            gs.run_command("g.mapset", project=project, mapset=mapset, flags="c")
 
         # Set current mapset to test1
-        gs.run_command("g.mapset", project=location, mapset="test1")
+        gs.run_command("g.mapset", project=project, mapset="test1")
 
         yield SimpleNamespace(
             mapsets=TEST_MAPSETS,
-            project=location,
             current_mapset="test1",
         )
