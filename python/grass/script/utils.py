2f48774cda921708255c4be421b87ff6a82dd897
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-06-09 22:35:31 +0000

    docs: Fill in docstring for grass.script.utils.text_to_string()

--- python/grass/script/utils.py
+++ python/grass/script/utils.py
@@ -248,6 +248,10 @@ def encode(string: AnyStr, encoding: str | None = None) -> bytes:
 def text_to_string(text: AnyStr, encoding: str | None = None) -> str:
     """Convert text to str. Useful when passing text into environments,
     in Python 2 it needs to be bytes on Windows, in Python 3 in needs unicode.
+
+    :param text: The text to convert to string
+    :param encoding: The encoding to be used to decode the text that will be converted
+    :returns: A (unicode) string
     """
     return decode(text, encoding=encoding)
 
