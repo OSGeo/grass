4154e291e4559d4db879eaf05392186251309925
Shubham Vasudeo Desai <sdesai8@vclvm176-36.vcl.ncsu.edu>
2025-06-16 17:46:46 -0400

    Use Goto

--- lib/vector/Vlib/ascii.c
+++ lib/vector/Vlib/ascii.c
@@ -84,10 +84,8 @@ int Vect_read_ascii(FILE *ascii, struct Map_info *Map)
                 continue;
             }
             G_warning(_("Error reading ASCII file: (bad type) [%s]"), buff);
-            G_free(xarray);
-            G_free(yarray);
-            G_free(zarray);
-            return -1;
+            n_lines = -1;
+            goto cleanup;
         }
         if (ctype == '#') {
             G_debug(2, "a2b: Skipping commented line");
@@ -125,10 +123,8 @@ int Vect_read_ascii(FILE *ascii, struct Map_info *Map)
             break;
         default: {
             G_warning(_("Error reading ASCII file: (unknown type) [%s]"), buff);
-            G_free(xarray);
-            G_free(yarray);
-            G_free(zarray);
-            return -1;
+            n_lines = -1;
+            goto cleanup;
         }
         }
         G_debug(5, "feature type = %d", type);
@@ -161,27 +157,21 @@ int Vect_read_ascii(FILE *ascii, struct Map_info *Map)
                 if (sscanf(buff, " %s %s %lf", east_str, north_str, z) < 2) {
                     G_warning(_("Error reading ASCII file: (bad point) [%s]"),
                               buff);
-                    G_free(xarray);
-                    G_free(yarray);
-                    G_free(zarray);
-                    return -1;
+                    n_lines = -1;
+                    goto cleanup;
                 }
                 else {
                     if (!G_scan_easting(east_str, x, G_projection())) {
                         G_warning(_("Unparsable longitude value: [%s]"),
                                   east_str);
-                        G_free(xarray);
-                        G_free(yarray);
-                        G_free(zarray);
-                        return -1;
+                        n_lines = -1;
+                        goto cleanup;
                     }
                     if (!G_scan_northing(north_str, y, G_projection())) {
                         G_warning(_("Unparsable latitude value: [%s]"),
                                   north_str);
-                        G_free(xarray);
-                        G_free(yarray);
-                        G_free(zarray);
-                        return -1;
+                        n_lines = -1;
+                        goto cleanup;
                     }
                 }
             }
@@ -225,10 +215,8 @@ int Vect_read_ascii(FILE *ascii, struct Map_info *Map)
 
             if (sscanf(buff, "%d%d", &catn, &cat) != 2) {
                 G_warning(_("Error reading categories: [%s]"), buff);
-                G_free(xarray);
-                G_free(yarray);
-                G_free(zarray);
-                return -1;
+                n_lines = -1;
+                goto cleanup;
             }
 
             Vect_cat_set(Cats, catn, cat);
@@ -241,18 +229,14 @@ int Vect_read_ascii(FILE *ascii, struct Map_info *Map)
         if (0 >
             Vect_copy_xyz_to_pnts(Points, xarray, yarray, zarray, n_points)) {
             G_warning(_("Unable to copy points"));
-            G_free(xarray);
-            G_free(yarray);
-            G_free(zarray);
-            return -1;
+            n_lines = -1;
+            goto cleanup;
         }
 
         if (type > 0) {
             if (-1 == Vect_write_line(Map, type, Points, Cats)) {
-                G_free(xarray);
-                G_free(yarray);
-                G_free(zarray);
-                return -1;
+                n_lines = -1;
+                goto cleanup;
             }
             n_lines++;
         }
@@ -262,7 +246,7 @@ int Vect_read_ascii(FILE *ascii, struct Map_info *Map)
         G_warning(_("Vector map <%s> is 2D. %d 3D features (faces or kernels) "
                     "skipped."),
                   Vect_get_name(Map), nskipped_3d);
-
+cleanup:
     Vect_destroy_line_struct(Points);
     Vect_destroy_cats_struct(Cats);
     G_free(xarray);
