79c6f78a576855104f8fec724d6f1b25a924c642
Nishant Bansal <nishant.bansal.282003@gmail.com>
2025-06-18 23:58:33 +0530

    update tests to use single mapset
    Signed-off-by: Nishant Bansal <nishant.bansal.282003@gmail.com>

--- general/g.list/tests/g_list_test.py
+++ general/g.list/tests/g_list_test.py
@@ -3,71 +3,31 @@ import grass.script as gs
 
 def test_default_output(simple_dataset):
     """Test default output of g.list."""
-    # Current mapset only
     actual = gs.read_command("g.list", type="all", env=simple_dataset.env).splitlines()
     expected = ["raster_test_1", "vector_test_1"]
-    assert actual == expected
-
-    # All mapsets
-    actual = gs.read_command(
-        "g.list", type="all", mapset="*", env=simple_dataset.env
-    ).splitlines()
-    expected = [
-        "raster_test_1",
-        "raster_test_2",
-        "vector_test_1",
-        "vector_test_2",
-    ]
     assert actual == expected
 
 
 def test_data_types_output(simple_dataset):
     """Test g.list with t flag."""
-    # Current mapset only
     actual = gs.read_command(
         "g.list", type="all", flags="t", env=simple_dataset.env
     ).splitlines()
     expected = ["raster/raster_test_1", "vector/vector_test_1"]
     assert actual == expected
 
-    # All mapsets
-    actual = gs.read_command(
-        "g.list", type="all", flags="t", mapset="*", env=simple_dataset.env
-    ).splitlines()
-    expected = [
-        "raster/raster_test_1",
-        "raster/raster_test_2",
-        "vector/vector_test_1",
-        "vector/vector_test_2",
-    ]
-    assert actual == expected
-
 
 def test_full_map_output(simple_dataset):
     """Test g.list with m flag."""
-    # Current mapset only
     actual = gs.read_command(
         "g.list", type="all", flags="m", env=simple_dataset.env
     ).splitlines()
     expected = ["raster_test_1@test_1", "vector_test_1@test_1"]
     assert actual == expected
 
-    # All mapsets
-    actual = gs.read_command(
-        "g.list", type="all", flags="m", mapset="*", env=simple_dataset.env
-    ).splitlines()
-    expected = [
-        "raster_test_1@test_1",
-        "raster_test_2@test_2",
-        "vector_test_1@test_1",
-        "vector_test_2@test_2",
-    ]
-    assert actual == expected
-
 
 def test_human_readable_output(simple_dataset):
     """Test g.list with p flag."""
-    # Current mapset only
     actual = gs.read_command(
         "g.list", type="all", flags="p", env=simple_dataset.env
     ).splitlines()
@@ -84,31 +44,9 @@ def test_human_readable_output(simple_dataset):
     for line in expected:
         assert line in actual, f"Expected line not found: {line}"
 
-    # All mapsets
-    actual = gs.read_command(
-        "g.list", type="all", flags="p", mapset="*", env=simple_dataset.env
-    ).splitlines()
-    expected = [
-        "raster files available in mapset <test_1>:",
-        "raster_test_1",
-        "raster files available in mapset <test_2>:",
-        "raster_test_2",
-        "no 3D raster files available in current mapset",
-        "vector files available in mapset <test_1>:",
-        "vector_test_1",
-        "vector files available in mapset <test_2>:",
-        "vector_test_2",
-        "no label files available in current mapset",
-        "no region definition files available in current mapset",
-        "no imagery group files available in current mapset",
-    ]
-    for line in expected:
-        assert line in actual, f"Expected line not found: {line}"
-
 
 def test_verbose_listing_output(simple_dataset):
     """Test g.list with f flag."""
-    # Current mapset only
     actual = gs.read_command(
         "g.list", type="all", flags="f", env=simple_dataset.env
     ).splitlines()
@@ -119,29 +57,6 @@ def test_verbose_listing_output(simple_dataset):
         "vector files available in mapset <test_1>:",
         "vector_test_1      Vector title test_1",
         "no vector files available in mapset <PERMANENT>",
-        "no 3D raster files available in current mapset",
-        "no label files available in current mapset",
-        "no region definition files available in current mapset",
-        "no imagery group files available in current mapset",
-    ]
-    for line in expected:
-        assert line in actual, f"Expected line not found: {line}"
-
-    # All mapsets
-    actual = gs.read_command(
-        "g.list", type="all", flags="f", mapset="*", env=simple_dataset.env
-    ).splitlines()
-    expected = [
-        "raster files available in mapset <test_1>:",
-        "raster_test_1      Raster title test_1",
-        "no raster files available in mapset <PERMANENT>",
-        "raster files available in mapset <test_2>:",
-        "raster_test_2      Raster title test_2",
-        "vector files available in mapset <test_1>:",
-        "vector_test_1      Vector title test_1",
-        "no vector files available in mapset <PERMANENT>",
-        "vector files available in mapset <test_2>:",
-        "vector_test_2      Vector title test_2",
         "no 3D raster files available in current mapset",
         "no label files available in current mapset",
         "no region definition files available in current mapset",
