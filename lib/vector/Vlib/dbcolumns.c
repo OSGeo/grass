aa699892729ce8663f89208cba1c67d6c706bdde
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2025-06-19 16:29:02 -0400

    Update lib/vector/Vlib/dbcolumns.c
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/vector/Vlib/dbcolumns.c
+++ lib/vector/Vlib/dbcolumns.c
@@ -122,7 +122,7 @@ const char *Vect_get_column_types(struct Map_info *Map, int field)
     db_set_handle(&handle, fi->database, NULL);
     if (db_open_database(driver, &handle) != DB_OK) {
         goto cleanup;
-    } 
+    }
     db_init_string(&table_name);
     db_set_string(&table_name, fi->table);
     if (db_describe_table(driver, &table_name, &table) != DB_OK) {
