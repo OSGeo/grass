39988d7842f75483a960a86cf3d27881e49fc108
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-06-22 02:37:14 +0000

    g.access: Replace sentences constructed by parts with translatable sentences

--- general/g.access/exp_perms.c
+++ general/g.access/exp_perms.c
@@ -3,32 +3,55 @@
 
 char *explain_perms(int group, int other, int will)
 {
+    static const int show_read = 0;
     static char buf[256];
-    char *who;
-    char *verb;
-    char *read;
+    char *msg = "Cannot explain access permissions for mapset %s";
 
-    verb = _("have");
-    read = _("read ");
-    read = ""; /* remove this to have "read" appear */
+    msg = _("Cannot explain access permissions for mapset %s");
+
     if (group && other) {
-        who = _("Everyone");
-        verb = _("has");
+        if (show_read) {
+            msg = will ? _("Everyone will have read access to mapset %s")
+                       : _("Everyone now has read access to mapset %s");
+        }
+        else {
+            msg = will ? _("Everyone will have access to mapset %s")
+                       : _("Everyone now has access to mapset %s");
+        }
     }
     else if (group) {
-        who = _("Only users in your group");
+        if (show_read) {
+            msg = will ? _("Only users in your group will have read access to "
+                           "mapset %s")
+                       : _("Only users in your group now have read access to "
+                           "mapset %s");
+        }
+        else {
+            msg = will ? _("Only users in your group will have access to "
+                           "mapset %s")
+                       : _("Only users in your group now have access to mapset "
+                           "%s");
+        }
     }
     else if (other) {
-        who = _("Only users outside your group");
+        if (show_read) {
+            msg = will ? _("Only users outside your group will have read "
+                           "access to mapset %s")
+                       : _("Only users outside your group now have read access "
+                           "to mapset %s");
+        }
+        else {
+            msg = will ? _("Only users outside your group will have access to "
+                           "mapset %s")
+                       : _("Only users outside your group now have access to "
+                           "mapset %s");
+        }
     }
     else {
-        who = _("Only you");
-        read = "";
+        msg = will ? _("Only you will have access to mapset %s")
+                   : _("Only you now have access to mapset %s");
     }
-    if (will)
-        verb = _("have");
 
-    snprintf(buf, sizeof(buf), _("%s %s %s %saccess to mapset %s"), who,
-             will ? _("will") : _("now"), verb, read, G_mapset());
+    snprintf(buf, sizeof(buf), msg, G_mapset());
     return buf;
 }
