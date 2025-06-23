842fd682ac55ff476b39c7ddf7cd0835d6641f13
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-06-23 13:43:58 -0400

    Update exp_perms.c

--- general/g.access/exp_perms.c
+++ general/g.access/exp_perms.c
@@ -7,24 +7,24 @@ char *explain_perms(int group, int other, int will)
     char *msg = _("Cannot explain access permissions for mapset <%s>");
 
     if (group && other) {
-        msg = will ? _("Everyone will have access to mapset %s")
-                   : _("Everyone now has access to mapset %s");
+        msg = will ? _("Everyone will have access to mapset <%s>")
+                   : _("Everyone now has access to mapset <%s>");
     }
     else if (group) {
         msg = will ? _("Only users in your group will have access to "
-                       "mapset %s")
+                       "mapset <%s>")
                    : _("Only users in your group now have access to mapset "
-                       "%s");
+                       "<%s>");
     }
     else if (other) {
         msg = will ? _("Only users outside your group will have access to "
-                       "mapset %s")
+                       "mapset <%s>")
                    : _("Only users outside your group now have access to "
-                       "mapset %s");
+                       "mapset <%s>");
     }
     else {
-        msg = will ? _("Only you will have access to mapset %s")
-                   : _("Only you now have access to mapset %s");
+        msg = will ? _("Only you will have access to mapset <%s>")
+                   : _("Only you now have access to mapset <%s>");
     }
 
     snprintf(buf, sizeof(buf), msg, G_mapset());
