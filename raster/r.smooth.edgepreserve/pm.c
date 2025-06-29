99487ae1a6440475b69fe725bc8371e3880bce10
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-06-29 19:27:34 -0400

    r.smooth.edgepreserve: Fix OpenMP parallel for form for MSVC build (#5956)
    * r.smooth.edgepreserve: Fix OpenMP parallel for form for MSVC
    
    With MSVC, `#pragma omp parallel for` doesn't support defining the loop variable inside the for construct. It seems limited to C99 language rules. So, define the loop variables outside of the for loops.
    
    * Initialize int col only once
    
    * Update pm.c

--- raster/r.smooth.edgepreserve/pm.c
+++ raster/r.smooth.edgepreserve/pm.c
@@ -75,8 +75,10 @@ void pm(const struct PM_params *pm_params, struct Row_cache *row_cache)
             rc = rb;
             rb = row_cache->get(prow + 1, row_cache);
 
+            int col;
+            int pcol;
 #pragma omp parallel for
-            for (int pcol = 1; pcol <= pm_params->ncols; pcol++) {
+            for (pcol = 1; pcol <= pm_params->ncols; pcol++) {
                 // N
                 gradients[0][pcol - 1] =
                     (ra[pcol] - rc[pcol]) * pm_params->vert_cor;
@@ -121,9 +123,9 @@ void pm(const struct PM_params *pm_params, struct Row_cache *row_cache)
 
             /* Calculate diffusivity coefficient */
             if (pm_params->conditional == 3) {
-                /* Black et al. 1998 Tukey's biweight function */
+/* Black et al. 1998 Tukey's biweight function */
 #pragma omp parallel for
-                for (int col = 0; col < pm_params->ncols; col++) {
+                for (col = 0; col < pm_params->ncols; col++) {
                     /* Both variations of impact of scale are two ways
                      * how to read 17th formula of Black et al. */
                     if (pm_params->preserve) {
@@ -167,7 +169,7 @@ void pm(const struct PM_params *pm_params, struct Row_cache *row_cache)
             else if (pm_params->conditional == 1) {
 /* Perona & Malik 1st diffusivity function = exponential */
 #pragma omp parallel for
-                for (int col = 0; col < pm_params->ncols; col++) {
+                for (col = 0; col < pm_params->ncols; col++) {
 #pragma GCC unroll 8
                     for (int i = 0; i < 8; i++) {
                         divs[i][col] =
@@ -181,7 +183,7 @@ void pm(const struct PM_params *pm_params, struct Row_cache *row_cache)
             else if (pm_params->conditional == 2) {
 /* Perona & Malik 2nd diffusivity function = quadratic */
 #pragma omp parallel for
-                for (int col = 0; col < pm_params->ncols; col++) {
+                for (col = 0; col < pm_params->ncols; col++) {
 #pragma GCC unroll 8
                     for (int i = 0; i < 8; i++) {
                         divs[i][col] =
@@ -194,7 +196,7 @@ void pm(const struct PM_params *pm_params, struct Row_cache *row_cache)
 
 /* Calculate new values and add padding */
 #pragma omp parallel for
-            for (int col = 0; col < pm_params->ncols; col++) {
+            for (col = 0; col < pm_params->ncols; col++) {
                 di[col] = divs[0][col] + divs[1][col] + divs[2][col] +
                           divs[3][col] + divs[4][col] + divs[5][col] +
                           divs[6][col] + divs[7][col];
