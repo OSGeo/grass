79cf950a0ff8b78e54326be9b4c109247f619ed6
Vaclav Petras <wenzeslaus@gmail.com>
2025-07-29 11:16:24 -0400

    Document the support class

--- python/grass/tools/support.py
+++ python/grass/tools/support.py
@@ -32,6 +32,8 @@ import grass.script.array as garray
 
 
 class ParameterConverter:
+    """Converts parameter values to strings and facilitates flow of the data."""
+
     def __init__(self):
         self._numpy_inputs = {}
         self._numpy_outputs = []
@@ -41,6 +43,21 @@ class ParameterConverter:
         self.temporary_rasters = []
 
     def process_parameters(self, kwargs):
+        """Converts high level parameter values to strings.
+
+        Converts io.StringIO to dash and stores the string in the *stdin* attribute.
+        Replaces NumPy arrays by temporary raster names and stores the arrays.
+        Replaces NumPy array types by temporary raster names.
+
+        Temporary names are accessible in the *temporary_rasters* attribute and need
+        to be cleaned.
+        The functions *translate_objects_to_data* and *translate_data_to_objects*
+        need to be called before and after the computation to do the translations
+        from NumPy arrays to GRASS data and from GRASS data to NumPy arrays.
+
+        Simple type conversions from numbers and iterables to strings are expected to
+        be done by lower level code.
+        """
         for key, value in kwargs.items():
             if isinstance(value, np.ndarray):
                 name = gs.append_uuid("tmp_serialized_input_array")
@@ -56,6 +73,7 @@ class ParameterConverter:
                 self.stdin = value.getvalue()
 
     def translate_objects_to_data(self, kwargs, env):
+        """Convert NumPy arrays to GRASS data"""
         for name, value in self._numpy_inputs.values():
             map2d = garray.array(env=env)
             map2d[:] = value
@@ -63,6 +81,11 @@ class ParameterConverter:
             self.temporary_rasters.append(name)
 
     def translate_data_to_objects(self, kwargs, env):
+        """Convert GRASS data to NumPy arrays
+
+        Returns True if there is one or more output arrays, False otherwise.
+        The arrays are stored in the *result* attribute.
+        """
         output_arrays = []
         for name, value in self._numpy_outputs:
             output_array = garray.array(name, env=env)
