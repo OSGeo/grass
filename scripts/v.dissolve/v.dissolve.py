5649ddcd29988da0808805681e2eaba023fa5f71
Vaclav Petras <wenzeslaus@gmail.com>
2025-07-31 04:43:01 -0400

    grass.tools: Use Tools in v.dissolve
    This uses grass.tools in the v.dissolve tool for whatever was previously done by the grass.script.run_command functions. There is no strong reason to do this migration except to test grass.tool more and create more examples, but that's a good reason. Nice thing is that simple calls once the Tools object is created are shorter by one line. Tools objects need to be created. (I opted for creating them again in functions as opposed to passing one to all functions.)
    
    To keep the behavior close to the original, some of the Tools objects do not capture stdout and stderr so that it is printed as it would be with run_command. This means that progress messages will be printed from the underlying tools and error messages will be part of that output as well. (This choice might have been different for a newly written tool or for different parts of a tool.) Places which capture stdin for further processing are kept with the defaults, so stderr is captured and will be part of an exception, but will not be visible otherwise (but was previously visible).
    
    Places which use tool wrappers from grass.script are left as is. Replacement of the convenience wrappers is waiting for JSON outputs of specific tools, so the current change is only focusing on run_command and friends.
    
    Running the pytest test takes the same time with the old and new code (20-29s depending on external factors but consistent for both versions).

--- scripts/v.dissolve/v.dissolve.py
+++ scripts/v.dissolve/v.dissolve.py
@@ -76,11 +76,11 @@
 """Dissolve geometries and aggregate attribute values"""
 
 import atexit
-import json
-import subprocess
+import io
 from collections import defaultdict
 
 import grass.script as gs
+from grass.tools import Tools
 from grass.exceptions import CalledModuleError
 
 
@@ -212,21 +212,19 @@ def updates_to_sql(table, updates):
 
 def update_columns(output_name, output_layer, updates, add_columns):
     """Update attribute values based on a list of updates"""
+    tools = Tools(capture_output=False)
     if add_columns:
-        gs.run_command(
-            "v.db.addcolumn",
+        tools.v_db_addcolumn(
             map=output_name,
             layer=output_layer,
             columns=",".join(add_columns),
         )
     db_info = gs.vector_db(output_name)[int(output_layer)]
     sql = updates_to_sql(table=db_info["table"], updates=updates)
-    gs.write_command(
-        "db.execute",
-        input="-",
+    tools.db_execute(
+        input=io.StringIO(sql),
         database=db_info["database"],
         driver=db_info["driver"],
-        stdin=sql,
     )
 
 
@@ -413,15 +411,13 @@ def aggregate_attributes_sql(
         select_columns = columns_to_aggregate
         column_types = None
 
-    data = json.loads(
-        gs.read_command(
-            "v.db.select",
-            map=input_name,
-            layer=input_layer,
-            columns=",".join([column] + select_columns),
-            group=column,
-            format="json",
-        )
+    tools = Tools()
+    data = tools.v_db_select(
+        map=input_name,
+        layer=input_layer,
+        columns=",".join([column] + select_columns),
+        group=column,
+        format="json",
     )
     # We added the group column to the select, so we need to skip it here.
     select_column_names = [item["name"] for item in data["info"]["columns"]][1:]
@@ -474,15 +470,13 @@ def aggregate_attributes_univar(
             "must be the same"
         )
         raise ValueError(msg)
-    records = json.loads(
-        gs.read_command(
-            "v.db.select",
-            map=input_name,
-            layer=input_layer,
-            columns=column,
-            group=column,
-            format="json",
-        )
+    tools = Tools()
+    records = tools.v_db_select(
+        map=input_name,
+        layer=input_layer,
+        columns=column,
+        group=column,
+        format="json",
     )["records"]
     columns = defaultdict(list)
     for agg_column, method, result in zip(
@@ -501,15 +495,12 @@ def aggregate_attributes_univar(
         where = column_value_to_where(column, value, quote=quote_column)
         # for i, aggregate_column in enumerate(columns_to_aggregate):
         for aggregate_column, methods_results in columns.items():
-            stats = json.loads(
-                gs.read_command(
-                    "v.db.univar",
-                    map=input_name,
-                    column=aggregate_column,
-                    format="json",
-                    where=where,
-                )
-            )["statistics"]
+            stats = tools.v_db_univar(
+                map=input_name,
+                column=aggregate_column,
+                format="json",
+                where=where,
+            )
             for method, result_column in methods_results:
                 updates.append(
                     {
@@ -522,14 +513,13 @@ def aggregate_attributes_univar(
 
 
 def cleanup(name):
-    """Remove temporary vector silently"""
-    gs.run_command(
-        "g.remove",
+    """Remove temporary vector silently (outputs are captured)"""
+    tools = Tools()
+    tools.g_remove(
         flags="f",
         type="vector",
         name=name,
         quiet=True,
-        stderr=subprocess.DEVNULL,
         errors="ignore",
     )
 
@@ -579,6 +569,7 @@ def main():
         methods=user_aggregate_methods,
         backend=aggregate_backend,
     )
+    tools = Tools(capture_output=False)
 
     # does map exist?
     if not gs.find_file(input_vector, element="vector")["file"]:
@@ -592,8 +583,7 @@ def main():
             )
             % ("column", layer)
         )
-        gs.run_command(
-            "v.extract",
+        tools.v_extract(
             flags="d",
             input=input_vector,
             output=output,
@@ -629,15 +619,13 @@ def main():
         atexit.register(cleanup, tmpfile)
 
         try:
-            gs.run_command(
-                "v.reclass",
+            tools.v_reclass(
                 input=input_vector,
                 output=tmpfile,
                 layer=layer,
                 column=column,
             )
-            gs.run_command(
-                "v.extract",
+            tools.v_extract(
                 flags="d",
                 input=tmpfile,
                 output=output,
