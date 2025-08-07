08a58887628d25c76018925264ce8d7052d04853
Jayneel Shah <80264736+jayneel-shah18@users.noreply.github.com>
2025-08-07 14:22:08 +0000

    bug fixes

--- scripts/i.in.spotvgt/i.in.spotvgt.py
+++ scripts/i.in.spotvgt/i.in.spotvgt.py
@@ -78,7 +78,7 @@ def create_VRT_file(projfile, vrtfile, infile):
     with open(projfile) as fh:
         for line in fh:
             f = line.rstrip("\r\n").split()
-            if f < 2:
+            if len(f) < 2:
                 continue
             kv[f[0]] = f[1]
 
@@ -179,7 +179,7 @@ def main():
 
     # clone current region
     # switch to a temporary region
-    gs.use_temp_region()
+    # gs.use_temp_region()
 
     gs.run_command("g.region", raster=name, quiet=True)
 
@@ -226,7 +226,7 @@ def main():
     if also:
         gs.message(_("Importing SPOT VGT NDVI quality map..."))
         gs.try_remove(vrtfile)
-        qname = spotname.replace("NDV", "SM")
+        qname = spotname.replace("NDV", "SM") + ".HDF"
         qfile = os.path.join(spotdir, qname)
         create_VRT_file(projfile, vrtfile, qfile)
 
@@ -238,22 +238,18 @@ def main():
             gs.fatal(_("An error occurred. Stop."))
 
         # some of the possible values:
-        rules = [
-            r + "\n"
-            for r in [
-                "8 50 50 50",
-                "11 70 70 70",
-                "12 90 90 90",
-                "60 grey",
-                "155 blue",
-                "232 violet",
-                "235 red",
-                "236 brown",
-                "248 orange",
-                "251 yellow",
-                "252 green",
-            ]
-        ]
+        rules = """8 50 50 50
+        11 70 70 70
+        12 90 90 90
+        60 grey
+        155 blue
+        232 violet
+        235 red
+        236 brown
+        248 orange
+        251 yellow
+        252 green
+        """
         gs.write_command("r.colors", map=smfile, rules="-", stdin=rules)
 
         gs.message(_("Imported SPOT VEGETATION SM quality map <%s>.") % smfile)
