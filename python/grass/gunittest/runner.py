d09644220a5605f1abdf015cdbf577808cfa1894
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-08-17 22:07:29 +0000

    grass.gunittest: Simplify GrassTestRunner to count test duration after calling the base run() implementation

--- python/grass/gunittest/runner.py
+++ python/grass/gunittest/runner.py
@@ -17,7 +17,6 @@ from __future__ import annotations
 import time
 import unittest
 from typing import TextIO
-import warnings
 
 import grass.gunittest.case
 import grass.gunittest.result
@@ -404,82 +403,11 @@ class GrassTestRunner(unittest.TextTestRunner):
 
     def run(self, test):
         """Run the given test case or test suite."""
-        result = self._makeResult()
-        unittest.registerResult(result)
-        result.failfast = self.failfast
-        result.buffer = self.buffer
-        result.tb_locals = self.tb_locals
-        with warnings.catch_warnings():
-            if self.warnings:
-                # if self.warnings is set, use it to filter all the warnings
-                warnings.simplefilter(self.warnings)
-                # if the filter is 'default' or 'always', special-case the
-                # warnings from the deprecated unittest methods to show them
-                # no more than once per module, because they can be fairly
-                # noisy.  The -Wd and -Wa flags can be used to bypass this
-                # only when self.warnings is None.
-                if self.warnings in ["default", "always"]:
-                    warnings.filterwarnings(
-                        "module",
-                        category=DeprecationWarning,
-                        message=r"Please use assert\w+ instead.",
-                    )
-            startTime = time.perf_counter()
-            startTestRun = getattr(result, "startTestRun", None)
-            if startTestRun is not None:
-                startTestRun()
-            try:
-                test(result)
-            finally:
-                stopTime = time.perf_counter()
-                timeTaken = stopTime - startTime
-                setTimes = getattr(result, "setTimes", None)
-                if setTimes is not None:
-                    setTimes(startTime, stopTime, timeTaken)
-                stopTestRun = getattr(result, "stopTestRun", None)
-                if stopTestRun is not None:
-                    stopTestRun()
-            stopTime = time.perf_counter()
+        startTime = time.perf_counter()
+        result = super().run(test)
+        stopTime = time.perf_counter()
         timeTaken = stopTime - startTime
-        result.printErrors()
-        if hasattr(result, "separator2"):
-            self.stream.writeln(result.separator2)
-        run = result.testsRun
-        self.stream.writeln(
-            "Ran %d test%s in %.3fs" % (run, (run != 1 and "s") or "", timeTaken)
-        )
-        self.stream.writeln()
-
-        expectedFails = unexpectedSuccesses = skipped = 0
-        try:
-            results = map(
-                len,
-                (result.expectedFailures, result.unexpectedSuccesses, result.skipped),
-            )
-        except AttributeError:
-            pass
-        else:
-            expectedFails, unexpectedSuccesses, skipped = results
-
-        infos = []
-        if not result.wasSuccessful():
-            self.stream.write("FAILED")
-            failed, errored = len(result.failures), len(result.errors)
-            if failed:
-                infos.append("failures=%d" % failed)
-            if errored:
-                infos.append("errors=%d" % errored)
-        else:
-            self.stream.write("OK")
-        if skipped:
-            infos.append("skipped=%d" % skipped)
-        if expectedFails:
-            infos.append("expected failures=%d" % expectedFails)
-        if unexpectedSuccesses:
-            infos.append("unexpected successes=%d" % unexpectedSuccesses)
-        if infos:
-            self.stream.writeln(" (%s)" % (", ".join(infos),))
-        else:
-            self.stream.write("\n")
-        self.stream.flush()
+        setTimes = getattr(result, "setTimes", None)
+        if setTimes is not None:
+            setTimes(startTime, stopTime, timeTaken)
         return result
