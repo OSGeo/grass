dbc7a67d1bf675577dd5e22710af778638a226f1
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2025-08-21 08:12:31 -0400

    Update raster/r.mapcalc/evaluate.c
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- raster/r.mapcalc/evaluate.c
+++ raster/r.mapcalc/evaluate.c
@@ -459,7 +459,7 @@ void execute(expr_list *ee)
 
     verbose = isatty(2);
     for (current_depth = 0; current_depth < depths; current_depth++) {
-      int row;
+        int row;
 #pragma omp parallel for default(shared) schedule(static, 1) private(i) ordered
         for (row = 0; row < rows; row++) {
             if (verbose)
