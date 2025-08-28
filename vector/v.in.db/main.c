946b3fbd51ee3f667c8445acb50450761afcac91
ShubhamDesai <42180509+ShubhamDesai@users.noreply.github.com>
2025-08-28 19:11:19 -0400

    Update vector/v.in.db/main.c
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- vector/v.in.db/main.c
+++ vector/v.in.db/main.c
@@ -121,8 +121,10 @@ int main(int argc, char *argv[])
         char name[GNAME_MAX], mapset[GMAPSET_MAX];
 
         if (!G_name_is_fully_qualified(outvect->answer, name, mapset)) {
-            if (G_strlcpy(name, outvect->answer, sizeof(name)) >= sizeof(name)) {
-                G_warning(_("Output vector name too long: <%s>"), outvect->answer);
+            if (G_strlcpy(name, outvect->answer, sizeof(name)) >=
+                sizeof(name)) {
+                G_warning(_("Output vector name too long: <%s>"),
+                          outvect->answer);
             }
             if (G_strlcpy(mapset, G_mapset(), sizeof(mapset)) >= sizeof(mapset)) {
                 G_warning(_("Mapset name too long: <%s>"), G_mapset());
