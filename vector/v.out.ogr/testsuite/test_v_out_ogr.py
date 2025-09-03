05fb80fdfc0fbcf9e9223cb73905e5a6293b04f0
Anna Petrasova <kratochanna@gmail.com>
2025-09-03 16:13:01 -0400

    v.out.ogr: fix fast method with no attributes

--- vector/v.out.ogr/testsuite/test_v_out_ogr.py
+++ vector/v.out.ogr/testsuite/test_v_out_ogr.py
@@ -36,15 +36,52 @@ kurtosis=33.681
 skewness=4.86561
 """
 
+    area_map = "test_area_map"
+    areas_string = """\
+VERTI:
+B  6
+ 11.6         29.55
+ 12.01        27.01
+ 15.25        26.68
+ 16.32        29.42
+ 13.49        31.06
+ 11.6         29.55
+C  1 1
+ 13.85        28.69
+ 1     1
+B  10
+ 18.49        34.1
+ 21.73        28.85
+ 28.24        33.28
+ 26.6         37.17
+ 23.9         34.92
+ 22.75        36.92
+ 18.86        37.17
+ 14.1         36.19
+ 14.19        32.54
+ 18.49        34.1
+C  1 1
+ 21.11        34.03
+ 1     2
+"""
+
     @classmethod
     def setUpClass(cls):
         """Use temporary region settings"""
         cls.use_temp_region()
+        cls.runModule(
+            "v.in.ascii",
+            input="-",
+            output=cls.area_map,
+            format="standard",
+            stdin=cls.areas_string,
+        )
 
     @classmethod
     def tearDownClass(cls):
         """Remove the temporary region"""
         cls.del_temp_region()
+        cls.runModule("g.remove", type="vector", flags="f", name=cls.area_map)
 
     def tearDown(self):
         self.runModule(
@@ -105,6 +142,50 @@ skewness=4.86561
             reference=self.univar_string,
             column=self.univar_col,
             precision=1e-8,
+        )
+
+    def test_geojson_format(self):
+        """Tests output to GeoJSON format"""
+
+        self.assertModule(
+            "v.out.ogr",
+            "Export to GeoJSON Format",
+            input=self.test_map,
+            output=f"{self.test_map}.json",
+            format="GeoJSON",
+        )
+
+        # Import back to verify
+        self.runModule(
+            "v.in.ogr",
+            input=f"{self.test_map}.json",
+            output=self.temp_import,
+        )
+
+        self.runModule("g.region", vector=self.temp_import)
+
+        self.assertVectorFitsUnivar(
+            map=self.temp_import,
+            reference=self.univar_string,
+            column=self.univar_col,
+            precision=1e-8,
+        )
+
+    def test_geojson_format_no_attributes(self):
+        """Tests output to GeoJSON format with no attributes"""
+        self.assertModule(
+            "v.out.ogr",
+            "Export to GeoJSON Format",
+            input=self.area_map,
+            output=f"{self.test_map}.json",
+            format="GeoJSON",
+        )
+
+        # Import back to verify
+        self.runModule(
+            "v.in.ogr",
+            input=f"{self.test_map}.json",
+            output=self.temp_import,
         )
 
 
