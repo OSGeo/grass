0b3f412d921ded2647fe92d55f9d0ec20f765e1e
Vaclav Petras <wenzeslaus@gmail.com>
2025-09-03 12:14:36 -0400

    Remove rasters which are copies of pack files

--- python/grass/experimental/tests/grass_tools_pack_test.py
+++ python/grass/experimental/tests/grass_tools_pack_test.py
@@ -12,6 +12,11 @@ def test_pack_input_output(xy_dataset_session, rows_raster_file3x3):
     assert os.path.exists(rows_raster_file3x3)
     tools.r_slope_aspect(elevation=rows_raster_file3x3, slope="file.grass_raster")
     assert os.path.exists("file.grass_raster")
+    assert not tools.g_findfile(element="raster", file="file", format="json")["name"]
+    assert not tools.g_findfile(
+        element="raster", file=rows_raster_file3x3.stem, format="json"
+    )["name"]
+    assert not tools.g_list(type="raster", format="json")
 
 
 def test_pack_input_output_with_name_and_parameter_call(
--- python/grass/experimental/tools.py
+++ python/grass/experimental/tools.py
@@ -26,6 +26,8 @@ class PackImporterExporter:
     def __init__(self, *, run_function, env=None):
         self._run_function = run_function
         self._env = env
+        self.input_rasters: list[tuple] = []
+        self.output_rasters: list[tuple] = []
 
     @classmethod
     def is_raster_pack_file(cls, value):
@@ -33,36 +35,36 @@ class PackImporterExporter:
 
     def modify_and_ingest_argument_list(self, args, parameters):
         # Uses parameters, but modifies the command, generates list of rasters and vectors.
-        self.input_rasters = []
         if "inputs" in parameters:
             for item in parameters["inputs"]:
                 if self.is_raster_pack_file(item["value"]):
-                    self.input_rasters.append(Path(item["value"]))
+                    inproject_name = Path(item["value"]).stem
+                    self.input_rasters.append((Path(item["value"]), inproject_name))
                     # No need to change that for the original kwargs.
                     # kwargs[item["param"]] = Path(item["value"]).stem
                     # Actual parameters to execute are now a list.
                     for i, arg in enumerate(args):
                         if arg.startswith(f"{item['param']}="):
-                            arg = arg.replace(item["value"], Path(item["value"]).stem)
+                            arg = arg.replace(item["value"], inproject_name)
                             args[i] = arg
-        self.output_rasters = []
         if "outputs" in parameters:
             for item in parameters["outputs"]:
                 if self.is_raster_pack_file(item["value"]):
-                    self.output_rasters.append(Path(item["value"]))
+                    inproject_name = Path(item["value"]).stem
+                    self.output_rasters.append((Path(item["value"]), inproject_name))
                     # kwargs[item["param"]] = Path(item["value"]).stem
                     for i, arg in enumerate(args):
                         if arg.startswith(f"{item['param']}="):
-                            arg = arg.replace(item["value"], Path(item["value"]).stem)
+                            arg = arg.replace(item["value"], inproject_name)
                             args[i] = arg
 
     def import_rasters(self):
-        for raster_file in self.input_rasters:
+        for raster_file, inproject_name in self.input_rasters:
             # Currently we override the projection check.
             self._run_function(
                 "r.unpack",
                 input=raster_file,
-                output=raster_file.stem,
+                output=inproject_name,
                 overwrite=True,
                 superquiet=True,
                 # flags="o",
@@ -71,14 +73,14 @@ class PackImporterExporter:
 
     def export_rasters(self):
         # Pack the output raster
-        for raster in self.output_rasters:
+        for raster_file, inproject_name in self.output_rasters:
             # Overwriting a file is a warning, so to avoid it, we delete the file first.
-            Path(raster).unlink(missing_ok=True)
+            Path(raster_file).unlink(missing_ok=True)
 
             self._run_function(
                 "r.pack",
-                input=raster.stem,
-                output=raster,
+                input=inproject_name,
+                output=raster_file,
                 flags="c",
                 overwrite=True,
                 superquiet=True,
@@ -89,6 +91,14 @@ class PackImporterExporter:
 
     def export_data(self):
         self.export_rasters()
+
+    def cleanup(self):
+        remove = [name for (unused, name) in self.input_rasters]
+        remove.extend([name for (unused, name) in self.output_rasters])
+        if remove:
+            self._run_function(
+                "g.remove", type="raster", name=remove, superquiet=True, flags="f"
+            )
 
 
 class Tools(grass.tools.Tools):
@@ -166,4 +176,5 @@ class Tools(grass.tools.Tools):
             **popen_options,
         )
         pack_importer_exporter.export_data()
+        pack_importer_exporter.cleanup()
         return result
