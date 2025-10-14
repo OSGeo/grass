7ef358280a5f4cb7ef2abe17eaf55e8bc17a420c
Shubham Vasudeo Desai <sdesai8@vclvm179-56.vcl.ncsu.edu>
2025-10-14 16:10:41 -0400

    Fix argument cannot be negative

--- raster/r.thin/io.c
+++ raster/r.thin/io.c
@@ -118,7 +118,11 @@ int open_file(char *name)
     work_file_name = G_tempfile();
 
     /* create the file and then open it for read and write */
-    close(creat(work_file_name, 0666));
+    int tmpfd = creat(work_file_name, 0666);
+    if (tmpfd < 0) {
+        G_fatal_error(_("Unable to create temporary file <%s>"), work_file_name);
+    }
+    close(tmpfd);
     if ((work_file = open(work_file_name, 2)) < 0) {
         unlink(work_file_name);
         G_fatal_error(_("Unable to create temporary file <%s> -- errno = %d"),
