bf53070dc64ab80a58c5f296bca4e2c6f318e8a9
sakirr <sakirahmed75531@gmail.com>
2026-01-28 11:25:49 +0000

    tests(t.rast.mapcalc): replace pytest with grass.gunittest in temporal tests

--- temporal/t.rast.mapcalc/testsuite/test_t_rast_mapcalc_basic.py
+++ temporal/t.rast.mapcalc/testsuite/test_t_rast_mapcalc_basic.py
@@ -8,42 +8,30 @@ for details.
 @author GRASS Development Team
 """
 
-import os
-
-import pytest
-
 import grass.script as gs
-from grass.exceptions import CalledModuleError
-from grass.tools import Tools
+from grass.gunittest.case import TestCase
+from grass.gunittest.gmodules import SimpleModule
+from grass.gunittest.main import test
 
 
-@pytest.fixture(scope="module")
-def session(tmp_path_factory):
-    """Pytest fixture to create a temporary GRASS project with STRDS.
-
-    This setup initializes the GRASS environment, sets the computational region,
-    creates a series of raster maps, and registers them in STRDS datasets.
-
-    Yields:
-        session: active GRASS session with the prepared project and STRDS.
-    """
-    tmp_path = tmp_path_factory.mktemp("t_rast_mapcalc_basic")
-
-    gs.create_project(tmp_path)
-
-    with gs.setup.init(tmp_path, env=os.environ.copy()) as session:
-        tools = Tools(session=session)
-        tools.g_region(s=0, n=80, w=0, e=120, b=0, t=50, res=10, res3=10)
+class TestRasterMapcalcBasic(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Initiate the temporal GIS and set the region"""
+        cls.use_temp_region()
+        cls.runModule("g.gisenv", set="TGIS_USE_CURRENT_MAPSET=1")
+        cls.runModule("g.region", s=0, n=80, w=0, e=120, b=0, t=50, res=10, res3=10)
 
         # Generate data using rand() to match original shell test
-        tools.r_mapcalc(expression="prec_1 = rand(0, 550)", overwrite=True)
-        tools.r_mapcalc(expression="prec_2 = rand(0, 450)", overwrite=True)
-        tools.r_mapcalc(expression="prec_3 = rand(0, 320)", overwrite=True)
-        tools.r_mapcalc(expression="prec_4 = rand(0, 510)", overwrite=True)
-        tools.r_mapcalc(expression="prec_5 = rand(0, 300)", overwrite=True)
-        tools.r_mapcalc(expression="prec_6 = rand(0, 650)", overwrite=True)
+        cls.runModule("r.mapcalc", expression="prec_1 = rand(0, 550)", overwrite=True)
+        cls.runModule("r.mapcalc", expression="prec_2 = rand(0, 450)", overwrite=True)
+        cls.runModule("r.mapcalc", expression="prec_3 = rand(0, 320)", overwrite=True)
+        cls.runModule("r.mapcalc", expression="prec_4 = rand(0, 510)", overwrite=True)
+        cls.runModule("r.mapcalc", expression="prec_5 = rand(0, 300)", overwrite=True)
+        cls.runModule("r.mapcalc", expression="prec_6 = rand(0, 650)", overwrite=True)
 
-        tools.t_create(
+        cls.runModule(
+            "t.create",
             type="strds",
             temporaltype="absolute",
             output="precip_abs1",
@@ -51,7 +39,8 @@ def session(tmp_path_factory):
             description="A test",
             overwrite=True,
         )
-        tools.t_create(
+        cls.runModule(
+            "t.create",
             type="strds",
             temporaltype="absolute",
             output="precip_abs2",
@@ -59,125 +48,144 @@ def session(tmp_path_factory):
             description="A test",
             overwrite=True,
         )
-        tools.t_register(
+        cls.runModule(
+            "t.register",
+            flags="i",
             type="raster",
             input="precip_abs1",
             maps="prec_1,prec_2,prec_3,prec_4,prec_5,prec_6",
             start="2001-01-01",
             increment="3 months",
-            flags="i",
             overwrite=True,
         )
-        tools.t_register(
+        cls.runModule(
+            "t.register",
             type="raster",
             input="precip_abs2",
             maps="prec_1,prec_2,prec_3,prec_4,prec_5,prec_6",
             overwrite=True,
         )
 
-        yield session
+    @classmethod
+    def tearDownClass(cls):
+        """Remove the temporary region"""
+        cls.runModule(
+            "t.remove",
+            flags="rf",
+            type="strds",
+            inputs="precip_abs1,precip_abs2,precip_abs3,precip_abs4",
+        )
+        cls.del_temp_region()
 
+    def test_basic_addition(self):
+        """Test basic addition of two STRDS with -n flag (register null maps)."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="n",
+            inputs="precip_abs1,precip_abs2",
+            output="precip_abs3",
+            expression=" precip_abs1 + precip_abs2",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            overwrite=True,
+        )
+        # Verify output STRDS was created and has expected number of maps
+        tinfo_string = """number_of_maps=6
+temporal_type=absolute
+name=precip_abs3"""
+        info = SimpleModule("t.info", type="strds", input="precip_abs3", flags="g")
+        self.assertModuleKeyValue(
+            module=info, reference=tinfo_string, precision=2, sep="="
+        )
 
-def test_basic_addition(session):
-    """Test basic addition of two STRDS with -n flag (register null maps)."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2",
-        output="precip_abs3",
-        expression=" precip_abs1 + precip_abs2",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="n",
-        overwrite=True,
-    )
-    # Verify output STRDS was created and has expected number of maps
-    strds_info = tools.t_info(type="strds", input="precip_abs3", flags="g").keyval
-    assert strds_info["number_of_maps"] == 6, (
-        f"Expected 6 maps, got {strds_info['number_of_maps']}"
-    )
-    assert strds_info["name"] == "precip_abs3"
-    assert strds_info["temporal_type"] == "absolute"
+    def test_division_with_three_inputs(self):
+        """Test division expression with three input STRDS using -s flag."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="s",
+            inputs="precip_abs1,precip_abs2,precip_abs3",
+            output="precip_abs4",
+            expression=" (precip_abs1 + precip_abs2) / precip_abs2",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            overwrite=True,
+        )
+        # Verify output STRDS was created and has expected number of maps
+        tinfo_string = """number_of_maps=6
+temporal_type=absolute
+name=precip_abs4"""
+        info = SimpleModule("t.info", type="strds", input="precip_abs4", flags="g")
+        self.assertModuleKeyValue(
+            module=info, reference=tinfo_string, precision=2, sep="="
+        )
 
-
-def test_division_with_three_inputs(session):
-    """Test division expression with three input STRDS using -s flag."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2,precip_abs3",
-        output="precip_abs4",
-        expression=" (precip_abs1 + precip_abs2) / precip_abs2",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="s",
-        overwrite=True,
-    )
-    # Verify output STRDS was created and has expected number of maps
-    strds_info = tools.t_info(type="strds", input="precip_abs4", flags="g").keyval
-    assert strds_info["number_of_maps"] == 6, (
-        f"Expected 6 maps, got {strds_info['number_of_maps']}"
-    )
-    assert strds_info["name"] == "precip_abs4"
-    assert strds_info["temporal_type"] == "absolute"
-
-
-def test_null_multiplication(session):
-    """Test multiplication with null() function using -s flag."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2",
-        output="precip_abs4",
-        expression=" (precip_abs1 + precip_abs2) * null()",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="s",
-        overwrite=True,
-    )
-    # Verify output STRDS was created
-    strds_info = tools.t_info(type="strds", input="precip_abs4", flags="g").keyval
-    assert strds_info["name"] == "precip_abs4"
-    # With null() multiplication, maps may be empty but STRDS should exist
-    assert "number_of_maps" in strds_info
-
-
-def test_null_multiplication_with_null_flag(session):
-    """Test multiplication with null() function using -sn flags."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2",
-        output="precip_abs4",
-        expression=" (precip_abs1 + precip_abs2) * null()",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="sn",
-        overwrite=True,
-    )
-    # Verify output STRDS was created
-    strds_info = tools.t_info(type="strds", input="precip_abs4", flags="g").keyval
-    assert strds_info["name"] == "precip_abs4"
-    # With -n flag, null maps should be registered
-    assert "number_of_maps" in strds_info
-
-
-def test_failure_on_missing_map(session):
-    """Test that the module fails when a required map is missing."""
-    tools = Tools(session=session)
-    # Remove a raster map that is required
-    tools.g_remove(flags="f", type="raster", name="prec_1", overwrite=True)
-    # This should fail
-    with pytest.raises(CalledModuleError):
-        tools.t_rast_mapcalc(
+    def test_null_multiplication(self):
+        """Test multiplication with null() function using -s flag."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="s",
             inputs="precip_abs1,precip_abs2",
             output="precip_abs4",
             expression=" (precip_abs1 + precip_abs2) * null()",
             basename="new_prec",
             method="equal",
             nprocs=5,
-            flags="sn",
             overwrite=True,
         )
-    # Recreate the map so other tests aren't affected
-    tools.r_mapcalc(expression="prec_1 = rand(0, 550)", overwrite=True)
+        # Verify output STRDS was created
+        info = SimpleModule("t.info", type="strds", input="precip_abs4", flags="g")
+        info.run()
+        self.assertEqual(info.returncode, 0)
+        strds_info = gs.parse_key_val(info.outputs.stdout, sep="=")
+        self.assertEqual(strds_info["name"], "precip_abs4")
+        # With null() multiplication, maps may be empty but STRDS should exist
+        self.assertIn("number_of_maps", strds_info)
+
+    def test_null_multiplication_with_null_flag(self):
+        """Test multiplication with null() function using -sn flags."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="sn",
+            inputs="precip_abs1,precip_abs2",
+            output="precip_abs4",
+            expression=" (precip_abs1 + precip_abs2) * null()",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            overwrite=True,
+        )
+        # Verify output STRDS was created
+        info = SimpleModule("t.info", type="strds", input="precip_abs4", flags="g")
+        info.run()
+        self.assertEqual(info.returncode, 0)
+        strds_info = gs.parse_key_val(info.outputs.stdout, sep="=")
+        self.assertEqual(strds_info["name"], "precip_abs4")
+        # With -n flag, null maps should be registered
+        self.assertIn("number_of_maps", strds_info)
+
+    def test_failure_on_missing_map(self):
+        """Test that the module fails when a required map is missing."""
+        # Remove a raster map that is required
+        self.runModule(
+            "g.remove", flags="f", type="raster", name="prec_1", overwrite=True
+        )
+        # This should fail
+        self.assertModuleFail(
+            "t.rast.mapcalc",
+            flags="sn",
+            inputs="precip_abs1,precip_abs2",
+            output="precip_abs4",
+            expression=" (precip_abs1 + precip_abs2) * null()",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            overwrite=True,
+        )
+        # Recreate the map so other tests aren't affected
+        self.runModule("r.mapcalc", expression="prec_1 = rand(0, 550)", overwrite=True)
+
+
+if __name__ == "__main__":
+    test()
--- temporal/t.rast.mapcalc/testsuite/test_t_rast_mapcalc_operators.py
+++ temporal/t.rast.mapcalc/testsuite/test_t_rast_mapcalc_operators.py
@@ -8,41 +8,42 @@ for details.
 @author GRASS Development Team
 """
 
-import os
-
-import pytest
-
 import grass.script as gs
-from grass.tools import Tools
+from grass.gunittest.case import TestCase
+from grass.gunittest.gmodules import SimpleModule
+from grass.gunittest.main import test
 
 
-@pytest.fixture(scope="module")
-def session(tmp_path_factory):
-    """Pytest fixture to create a temporary GRASS project with STRDS.
-
-    This setup initializes the GRASS environment, sets the computational region,
-    creates a series of raster maps, and registers them in STRDS datasets.
-
-    Yields:
-        session: active GRASS session with the prepared project and STRDS.
-    """
-    tmp_path = tmp_path_factory.mktemp("t_rast_mapcalc_operators")
-
-    gs.create_project(tmp_path)
-
-    with gs.setup.init(tmp_path, env=os.environ.copy()) as session:
-        tools = Tools(session=session)
-        tools.g_region(s=0, n=80, w=0, e=120, b=0, t=50, res=10, res3=10)
+class TestRasterMapcalcOperators(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Initiate the temporal GIS and set the region"""
+        cls.use_temp_region()
+        cls.runModule("g.gisenv", set="TGIS_USE_CURRENT_MAPSET=1")
+        cls.runModule("g.region", s=0, n=80, w=0, e=120, b=0, t=50, res=10, res3=10)
 
         # Generate data with -s flag (silent) to match original shell test
-        tools.r_mapcalc(expression="prec_1 = rand(0, 550)", flags="s", overwrite=True)
-        tools.r_mapcalc(expression="prec_2 = rand(0, 450)", flags="s", overwrite=True)
-        tools.r_mapcalc(expression="prec_3 = rand(0, 320)", flags="s", overwrite=True)
-        tools.r_mapcalc(expression="prec_4 = rand(0, 510)", flags="s", overwrite=True)
-        tools.r_mapcalc(expression="prec_5 = rand(0, 300)", flags="s", overwrite=True)
-        tools.r_mapcalc(expression="prec_6 = rand(0, 650)", flags="s", overwrite=True)
+        cls.runModule(
+            "r.mapcalc", expression="prec_1 = rand(0, 550)", flags="s", overwrite=True
+        )
+        cls.runModule(
+            "r.mapcalc", expression="prec_2 = rand(0, 450)", flags="s", overwrite=True
+        )
+        cls.runModule(
+            "r.mapcalc", expression="prec_3 = rand(0, 320)", flags="s", overwrite=True
+        )
+        cls.runModule(
+            "r.mapcalc", expression="prec_4 = rand(0, 510)", flags="s", overwrite=True
+        )
+        cls.runModule(
+            "r.mapcalc", expression="prec_5 = rand(0, 300)", flags="s", overwrite=True
+        )
+        cls.runModule(
+            "r.mapcalc", expression="prec_6 = rand(0, 650)", flags="s", overwrite=True
+        )
 
-        tools.t_create(
+        cls.runModule(
+            "t.create",
             type="strds",
             temporaltype="absolute",
             output="precip_abs1",
@@ -50,7 +51,8 @@ def session(tmp_path_factory):
             description="A test",
             overwrite=True,
         )
-        tools.t_create(
+        cls.runModule(
+            "t.create",
             type="strds",
             temporaltype="absolute",
             output="precip_abs2",
@@ -58,149 +60,166 @@ def session(tmp_path_factory):
             description="A test",
             overwrite=True,
         )
-        tools.t_register(
+        cls.runModule(
+            "t.register",
+            flags="i",
             type="raster",
             input="precip_abs1",
             maps="prec_1,prec_2,prec_3,prec_4,prec_5,prec_6",
             start="2001-01-01",
             increment="3 months",
-            flags="i",
             overwrite=True,
         )
-        tools.t_register(
+        cls.runModule(
+            "t.register",
             type="raster",
             input="precip_abs2",
             maps="prec_1,prec_2,prec_3,prec_4,prec_5,prec_6",
             overwrite=True,
         )
 
-        yield session
+    @classmethod
+    def tearDownClass(cls):
+        """Remove the temporary region"""
+        cls.runModule(
+            "t.remove",
+            flags="rf",
+            type="strds",
+            inputs="precip_abs1,precip_abs2,precip_abs3",
+        )
+        cls.del_temp_region()
+
+    def test_start_time_end_time_operators(self):
+        """Test start_time() and end_time() temporal operators."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="sn",
+            inputs="precip_abs1,precip_abs2",
+            output="precip_abs3",
+            expression="if(start_time() >= 0 && end_time() >= 0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            verbose=True,
+            overwrite=True,
+        )
+        # Verify output STRDS was created
+        tinfo_string = """number_of_maps=6
+temporal_type=absolute
+name=precip_abs3"""
+        info = SimpleModule("t.info", type="strds", input="precip_abs3", flags="g")
+        self.assertModuleKeyValue(
+            module=info, reference=tinfo_string, precision=2, sep="="
+        )
+
+    def test_start_time_components(self):
+        """Test start_year(), start_month(), start_day(), start_hour(), start_minute(), start_second() operators."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="sn",
+            inputs="precip_abs1,precip_abs2",
+            output="precip_abs3",
+            expression="if(start_year()>=0&&start_month()>=0&&start_day()>=0&&start_hour()>=0&&start_minute()>=0&&start_second()>=0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            verbose=True,
+            overwrite=True,
+        )
+        # Verify output STRDS was created
+        tinfo_string = """number_of_maps=6
+name=precip_abs3"""
+        info = SimpleModule("t.info", type="strds", input="precip_abs3", flags="g")
+        self.assertModuleKeyValue(
+            module=info, reference=tinfo_string, precision=2, sep="="
+        )
+
+    def test_end_time_components(self):
+        """Test end_year(), end_month(), end_day(), end_hour(), end_minute(), end_second() operators."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="sn",
+            inputs="precip_abs1,precip_abs2",
+            output="precip_abs3",
+            expression="if(end_year()>=0&&end_month()>=0&&end_day()>=0&&end_hour()>=0&&end_minute()>=0&&end_second()>=0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            verbose=True,
+            overwrite=True,
+        )
+        # Verify output STRDS was created
+        tinfo_string = """number_of_maps=6
+name=precip_abs3"""
+        info = SimpleModule("t.info", type="strds", input="precip_abs3", flags="g")
+        self.assertModuleKeyValue(
+            module=info, reference=tinfo_string, precision=2, sep="="
+        )
+
+    def test_start_doy_dow_operators(self):
+        """Test start_doy() and start_dow() temporal operators."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="sn",
+            inputs="precip_abs1,precip_abs2",
+            output="precip_abs3",
+            expression="if(start_doy() >= 0 && start_dow() >= 0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            verbose=True,
+            overwrite=True,
+        )
+        # Verify output STRDS was created
+        tinfo_string = """number_of_maps=6
+name=precip_abs3"""
+        info = SimpleModule("t.info", type="strds", input="precip_abs3", flags="g")
+        self.assertModuleKeyValue(
+            module=info, reference=tinfo_string, precision=2, sep="="
+        )
+
+    def test_end_doy_dow_operators(self):
+        """Test end_doy() and end_dow() temporal operators."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="sn",
+            inputs="precip_abs1,precip_abs2",
+            output="precip_abs3",
+            expression="if(end_doy() >= 0 && end_dow() >= 0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
+            basename="new_prec",
+            method="equal",
+            nprocs=5,
+            verbose=True,
+            overwrite=True,
+        )
+        # Verify output STRDS was created
+        tinfo_string = """number_of_maps=6
+name=precip_abs3"""
+        info = SimpleModule("t.info", type="strds", input="precip_abs3", flags="g")
+        self.assertModuleKeyValue(
+            module=info, reference=tinfo_string, precision=2, sep="="
+        )
+
+    def test_map_comparison_operator(self):
+        """Test comparison operator with map names."""
+        self.assertModule(
+            "t.rast.mapcalc",
+            flags="sn",
+            inputs="precip_abs1",
+            output="precip_abs3",
+            expression="if(precip_abs1 == prec_1, prec_1, null())",
+            basename="new_prec",
+            verbose=True,
+            overwrite=True,
+        )
+        # Verify output STRDS was created
+        info = SimpleModule("t.info", type="strds", input="precip_abs3", flags="g")
+        info.run()
+        self.assertEqual(info.returncode, 0)
+        strds_info = gs.parse_key_val(info.outputs.stdout, sep="=")
+        self.assertEqual(strds_info["name"], "precip_abs3")
+        self.assertIn("number_of_maps", strds_info)
 
 
-def test_start_time_end_time_operators(session):
-    """Test start_time() and end_time() temporal operators."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2",
-        output="precip_abs3",
-        expression="if(start_time() >= 0 && end_time() >= 0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="sn",
-        verbose=True,
-        overwrite=True,
-    )
-    # Verify output STRDS was created
-    strds_info = tools.t_info(type="strds", input="precip_abs3", flags="g").keyval
-    assert strds_info["name"] == "precip_abs3"
-    assert strds_info["number_of_maps"] == 6, (
-        f"Expected 6 maps, got {strds_info['number_of_maps']}"
-    )
-    assert strds_info["temporal_type"] == "absolute"
-
-
-def test_start_time_components(session):
-    """Test start_year(), start_month(), start_day(), start_hour(), start_minute(), start_second() operators."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2",
-        output="precip_abs3",
-        expression="if(start_year()>=0&&start_month()>=0&&start_day()>=0&&start_hour()>=0&&start_minute()>=0&&start_second()>=0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="sn",
-        verbose=True,
-        overwrite=True,
-    )
-    # Verify output STRDS was created
-    strds_info = tools.t_info(type="strds", input="precip_abs3", flags="g").keyval
-    assert strds_info["name"] == "precip_abs3"
-    assert strds_info["number_of_maps"] == 6, (
-        f"Expected 6 maps, got {strds_info['number_of_maps']}"
-    )
-
-
-def test_end_time_components(session):
-    """Test end_year(), end_month(), end_day(), end_hour(), end_minute(), end_second() operators."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2",
-        output="precip_abs3",
-        expression="if(end_year()>=0&&end_month()>=0&&end_day()>=0&&end_hour()>=0&&end_minute()>=0&&end_second()>=0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="sn",
-        verbose=True,
-        overwrite=True,
-    )
-    # Verify output STRDS was created
-    strds_info = tools.t_info(type="strds", input="precip_abs3", flags="g").keyval
-    assert strds_info["name"] == "precip_abs3"
-    assert strds_info["number_of_maps"] == 6, (
-        f"Expected 6 maps, got {strds_info['number_of_maps']}"
-    )
-
-
-def test_start_doy_dow_operators(session):
-    """Test start_doy() and start_dow() temporal operators."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2",
-        output="precip_abs3",
-        expression="if(start_doy() >= 0 && start_dow() >= 0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="sn",
-        verbose=True,
-        overwrite=True,
-    )
-    # Verify output STRDS was created
-    strds_info = tools.t_info(type="strds", input="precip_abs3", flags="g").keyval
-    assert strds_info["name"] == "precip_abs3"
-    assert strds_info["number_of_maps"] == 6, (
-        f"Expected 6 maps, got {strds_info['number_of_maps']}"
-    )
-
-
-def test_end_doy_dow_operators(session):
-    """Test end_doy() and end_dow() temporal operators."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1,precip_abs2",
-        output="precip_abs3",
-        expression="if(end_doy() >= 0 && end_dow() >= 0, (precip_abs1*td() + precip_abs2) / td(), null()) ",
-        basename="new_prec",
-        method="equal",
-        nprocs=5,
-        flags="sn",
-        verbose=True,
-        overwrite=True,
-    )
-    # Verify output STRDS was created
-    strds_info = tools.t_info(type="strds", input="precip_abs3", flags="g").keyval
-    assert strds_info["name"] == "precip_abs3"
-    assert strds_info["number_of_maps"] == 6, (
-        f"Expected 6 maps, got {strds_info['number_of_maps']}"
-    )
-
-
-def test_map_comparison_operator(session):
-    """Test comparison operator with map names."""
-    tools = Tools(session=session)
-    tools.t_rast_mapcalc(
-        inputs="precip_abs1",
-        output="precip_abs3",
-        expression="if(precip_abs1 == prec_1, prec_1, null())",
-        basename="new_prec",
-        flags="sn",
-        verbose=True,
-        overwrite=True,
-    )
-    # Verify output STRDS was created
-    strds_info = tools.t_info(type="strds", input="precip_abs3", flags="g").keyval
-    assert strds_info["name"] == "precip_abs3"
-    assert "number_of_maps" in strds_info
+if __name__ == "__main__":
+    test()
