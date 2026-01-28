654b1e809eaa1fabbf40125758dd2defe84804b7
Saurabh Singh <sureshsingh09120@gmail.com>
2026-01-28 11:42:04 +0530

    Verify actual values in r.colors tests

--- raster/r.colors/testsuite/test_r_colors.py
+++ raster/r.colors/testsuite/test_r_colors.py
@@ -14,7 +14,6 @@ class TestRColorsBasic(TestCase):
     def test_color_table_applied(self):
         """Test that color table is actually applied"""
         self.assertModule("r.colors", map="elevation", color="grey")
-        # Verify color table was applied using JSON output
         rules = gs.parse_command("r.colors.out", map="elevation", format="json")
         self.assertIsNotNone(rules, "Color table should be returned")
 
@@ -23,26 +22,21 @@ class TestRColorsBasic(TestCase):
         self.assertModule("r.colors", map="elevation", color="grey", flags="e")
         rules_eq = gs.read_command("r.colors.out", map="elevation")
 
-        # Apply without equalize
         self.assertModule("r.colors", map="elevation", color="grey")
         rules_normal = gs.read_command("r.colors.out", map="elevation")
 
-        # Rules should be different
         self.assertNotEqual(
             rules_eq, rules_normal, "Equalized colors should differ from normal"
         )
 
     def test_remove_color_table(self):
         """Test removing color table with -r flag"""
-        # First apply a non-default color table
         self.assertModule("r.colors", map="elevation", color="grey")
         rules_grey = gs.read_command("r.colors.out", map="elevation")
 
-        # Remove it (should revert to default viridis)
         self.assertModule("r.colors", map="elevation", flags="r")
         rules_after = gs.read_command("r.colors.out", map="elevation")
 
-        # After removal, should be different from grey
         self.assertNotEqual(
             rules_grey, rules_after, "Removed color should revert to default"
         )
@@ -50,7 +44,6 @@ class TestRColorsBasic(TestCase):
     def test_logarithmic_scaling(self):
         """Test logarithmic scaling flag"""
         self.assertModule("r.colors", map="elevation", color="viridis", flags="g")
-        # Verify it runs without error
         rules = gs.read_command("r.colors.out", map="elevation")
         self.assertGreater(len(rules), 0, "Should have color rules")
 
@@ -63,9 +56,7 @@ class TestRColorsRules(TestCase):
         rules = "0 blue\n100 green\n200 red"
         self.assertModule("r.colors", map="elevation", rules="-", stdin_=rules)
 
-        # Verify custom rules were applied (output is RGB values)
         output = gs.read_command("r.colors.out", map="elevation")
-        # Check for RGB values: blue=0:0:255, green=0:255:0, red=255:0:0
         self.assertIn("0:0:255", output, "Should contain blue RGB")
         self.assertIn("0:255:0", output, "Should contain green RGB")
         self.assertIn("255:0:0", output, "Should contain red RGB")
@@ -75,9 +66,10 @@ class TestRColorsRules(TestCase):
         rules = "0% blue\n50% yellow\n100% red"
         self.assertModule("r.colors", map="elevation", rules="-", stdin_=rules)
 
-        # Verify rules were applied
         output = gs.read_command("r.colors.out", map="elevation")
-        self.assertGreater(len(output), 0, "Should have color rules")
+        self.assertIn("0:0:255", output, "Should contain blue RGB at 0%")
+        self.assertIn("255:255:0", output, "Should contain yellow RGB at 50%")
+        self.assertIn("255:0:0", output, "Should contain red RGB at 100%")
 
 
 class TestRColorsOptions(TestCase):
@@ -85,29 +77,38 @@ class TestRColorsOptions(TestCase):
 
     def test_raster_option(self):
         """Test using another raster's color table"""
-        # First set a color table on elevation
         self.assertModule("r.colors", map="elevation", color="grey")
 
-        # Create another raster and copy elevation's colors
         self.runModule("r.mapcalc", expression="test_raster = elevation * 2")
         self.assertModule("r.colors", map="test_raster", raster="elevation")
 
-        # Cleanup
         self.runModule("g.remove", flags="f", type="raster", name="test_raster")
 
     def test_offset_option(self):
         """Test offset parameter"""
+        self.assertModule("r.colors", map="elevation", color="viridis")
+        rules_no_offset = gs.read_command("r.colors.out", map="elevation")
+
         self.assertModule("r.colors", map="elevation", color="viridis", offset=100)
-        # Verify it runs without error
-        rules = gs.read_command("r.colors.out", map="elevation")
-        self.assertGreater(len(rules), 0, "Should have color rules")
+        rules_with_offset = gs.read_command("r.colors.out", map="elevation")
+
+        self.assertNotEqual(
+            rules_no_offset,
+            rules_with_offset,
+            "Offset should change color rules",
+        )
 
     def test_scale_option(self):
         """Test scale parameter"""
+        self.assertModule("r.colors", map="elevation", color="viridis")
+        rules_no_scale = gs.read_command("r.colors.out", map="elevation")
+
         self.assertModule("r.colors", map="elevation", color="viridis", scale=2.0)
-        # Verify it runs without error
-        rules = gs.read_command("r.colors.out", map="elevation")
-        self.assertGreater(len(rules), 0, "Should have color rules")
+        rules_with_scale = gs.read_command("r.colors.out", map="elevation")
+
+        self.assertNotEqual(
+            rules_no_scale, rules_with_scale, "Scale should change color rules"
+        )
 
 
 if __name__ == "__main__":
