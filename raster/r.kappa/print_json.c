98e43f8ca161fd1b545d9b73869f5336ad261b19
pahalsrivastava <srivastavapahal5@gmail.com>
2026-01-28 10:49:12 +0530

    r.kappa: clean rewrite of JSON output using Parson library

--- raster/r.kappa/print_json.c
+++ raster/r.kappa/print_json.c
@@ -1,4 +1,3 @@
-#include <stdbool.h>
 #include <stdlib.h>
 #include <grass/gis.h>
 #include <grass/glocale.h>
@@ -8,19 +7,17 @@
 
 void print_json(void)
 {
-    FILE *fd;
-    JSON_Value *root_value;
-    JSON_Object *root_object;
-    JSON_Value *array_value;
-    JSON_Array *array;
-    char *json_text;
+    FILE *fd = NULL;
+    JSON_Value *root_value = NULL;
+    JSON_Object *root_object = NULL;
+    JSON_Value *array_value = NULL;
+    JSON_Array *array = NULL;
+    char *json_text = NULL;
+    bool first;
 
     if (output != NULL)
         fd = fopen(output, "w");
-    else
-        fd = stdout;
-
-    if (fd == NULL)
+    if (!fd)
         G_fatal_error(_("Cannot open file <%s> to write JSON output"), output);
 
     root_value = json_value_init_object();
@@ -28,12 +25,9 @@ void print_json(void)
 
     json_object_set_string(root_object, "reference", maps[0]);
     json_object_set_string(root_object, "classification", maps[1]);
-    json_object_set_number(root_object, "observations",
-                           (double)metrics->observations);
-    json_object_set_number(root_object, "correct",
-                           (double)metrics->correct);
-    json_object_set_number(root_object, "overall_accuracy",
-                           metrics->overall_accuracy);
+    json_object_set_number(root_object, "observations", (double)metrics->observations);
+    json_object_set_number(root_object, "correct", (double)metrics->correct);
+    json_object_set_number(root_object, "overall_accuracy", metrics->overall_accuracy);
 
     if (metrics->kappa == na_value)
         json_object_set_null(root_object, "kappa");
@@ -43,8 +37,7 @@ void print_json(void)
     if (metrics->kappa_variance == na_value)
         json_object_set_null(root_object, "kappa_variance");
     else
-        json_object_set_number(root_object, "kappa_variance",
-                               metrics->kappa_variance);
+        json_object_set_number(root_object, "kappa_variance", metrics->kappa_variance);
 
     array_value = json_value_init_array();
     array = json_value_get_array(array_value);
@@ -58,8 +51,7 @@ void print_json(void)
         JSON_Value *row_value = json_value_init_array();
         JSON_Array *row_array = json_value_get_array(row_value);
         for (int j = 0; j < ncat; j++)
-            json_array_append_number(
-                row_array, (double)metrics->matrix[ncat * i + j]);
+            json_array_append_number(row_array, (double)metrics->matrix[ncat * i + j]);
         json_array_append_value(array, row_value);
     }
     json_object_set_value(root_object, "matrix", array_value);
@@ -78,48 +70,34 @@ void print_json(void)
 
     array_value = json_value_init_array();
     array = json_value_get_array(array_value);
-    for (int i = 0; i < ncat; i++) {
-        if (metrics->producers_accuracy[i] == na_value)
-            json_array_append_null(array);
-        else
-            json_array_append_number(array,
-                                     metrics->producers_accuracy[i]);
-    }
+    for (int i = 0; i < ncat; i++)
+        metrics->producers_accuracy[i] == na_value
+            ? json_array_append_null(array)
+            : json_array_append_number(array, metrics->producers_accuracy[i]);
     json_object_set_value(root_object, "producers_accuracy", array_value);
-
     array_value = json_value_init_array();
     array = json_value_get_array(array_value);
-    for (int i = 0; i < ncat; i++) {
-        if (metrics->users_accuracy[i] == na_value)
-            json_array_append_null(array);
-        else
-            json_array_append_number(array,
-                                     metrics->users_accuracy[i]);
-    }
+    for (int i = 0; i < ncat; i++)
+        metrics->users_accuracy[i] == na_value
+            ? json_array_append_null(array)
+            : json_array_append_number(array, metrics->users_accuracy[i]);
     json_object_set_value(root_object, "users_accuracy", array_value);
-
     array_value = json_value_init_array();
     array = json_value_get_array(array_value);
-    for (int i = 0; i < ncat; i++) {
-        if (metrics->conditional_kappa[i] == na_value)
-            json_array_append_null(array);
-        else
-            json_array_append_number(array,
-                                     metrics->conditional_kappa[i]);
-    }
+    for (int i = 0; i < ncat; i++)
+        metrics->conditional_kappa[i] == na_value
+            ? json_array_append_null(array)
+            : json_array_append_number(array, metrics->conditional_kappa[i]);
     json_object_set_value(root_object, "conditional_kappa", array_value);
-
     if (metrics->mcc == na_value)
         json_object_set_null(root_object, "mcc");
     else
         json_object_set_number(root_object, "mcc", metrics->mcc);
-
     json_text = json_serialize_to_string_pretty(root_value);
     fprintf(fd, "%s\n", json_text);
-
     json_free_serialized_string(json_text);
     json_value_free(root_value);
 
-    if (output != NULL)
+    if (fd)
         fclose(fd);
 }
