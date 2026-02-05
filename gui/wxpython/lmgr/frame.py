bce91a2c0eaaf3bb6915a2afbd3f7bbb0717742c
lindakladivova <l.kladivova@seznam.cz>
2026-02-05 23:49:26 +0100

    changes in icons order in toolbar, button for Jupyter start always active, deletation of availability check - will be done after clicking on new buttons in Start dialog

--- gui/wxpython/lmgr/frame.py
+++ gui/wxpython/lmgr/frame.py
@@ -86,7 +86,6 @@ from startup.guiutils import (
 )
 from grass.grassdb.checks import is_first_time_user
 from grass.grassdb.history import Status
-from grass.workflows.server import is_wx_html2_available
 
 
 class GMFrame(wx.Frame):
@@ -804,36 +803,6 @@ class GMFrame(wx.Frame):
         )
         frame.CentreOnParent()
         frame.Show()
-
-    def OnShowJupyterInfo(self, event=None):
-        """Show information dialog when Jupyter Notebook is not available."""
-        if sys.platform.startswith("win"):
-            message = _(
-                "Jupyter Notebook is currently not included in the Windows GRASS build process.\n"
-                "This feature will be available in a future release. "
-                "You can use Jupyter Notebook externally."
-            )
-        elif not is_wx_html2_available():
-            message = _(
-                "Jupyter Notebook integration requires wxPython with the wx.html2 module enabled.\n\n"
-                "Your current wxPython / wxWidgets build does not provide wx.html2 support "
-                "(typically due to missing WebView / WebKit support).\n\n"
-                "Please install wxPython and wxWidgets with HTML2/WebView support enabled, "
-                "or use Jupyter Notebook externally."
-            )
-        else:
-            message = _(
-                "To use notebooks in GRASS, you need to have the Jupyter Notebook package installed. "
-                "For full functionality, we also recommend installing the visualization libraries "
-                "Folium and ipyleaflet. After installing these packages, please restart GRASS to enable this feature."
-            )
-
-        wx.MessageBox(
-            message=message,
-            caption=_("Jupyter Notebook not available"),
-            style=wx.OK | wx.ICON_INFORMATION,
-            parent=self,
-        )
 
     def OnPsMap(self, event=None, cmd=None):
         """Launch Cartographic Composer. See OnIClass documentation"""
