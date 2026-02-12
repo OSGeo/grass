f1ca64015de64c3009ce4ac9952559f77abf97df
HUN-sp <chopravinaykumarchopra@gmail.com>
2026-02-12 11:15:48 +0530

    r.resamp.stats: OpenMP parallelization with memory chunking

--- raster/r.resamp.stats/main.c
+++ raster/r.resamp.stats/main.c
@@ -2,8 +2,9 @@
  *
  * MODULE:       r.resamp.stats
  * AUTHOR(S):    Glynn Clements <glynn gclements.plus.com> (original
- *                 contributor)
+ *               contributor)
  *               Hamish Bowman <hamish_b yahoo.com>
+ *               Chopra (OpenMP parallelization)
  * PURPOSE:
  * COPYRIGHT:    (C) 2006-2007 by the GRASS Development Team
  *
@@ -11,14 +12,19 @@
  *               License (>=v2). Read the file COPYING that comes with GRASS
  *               for details.
  *
- *****************************************************************************/
+ ****************************************************************************/
 
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
+
 #include <grass/gis.h>
-#include <grass/raster.h>
 #include <grass/glocale.h>
+#include <grass/raster.h>
 #include <grass/stats.h>
 
 static const struct menu {
@@ -74,178 +80,338 @@ static int find_method(const char *name)
 }
 
 static int nulls;
-static int infile, outfile;
+static int outfile;
 static struct Cell_head dst_w, src_w;
-static DCELL *outbuf;
-static DCELL **bufs;
 static int method;
 static const void *closure;
 static int row_scale, col_scale;
 static double quantile;
 
+/* ----- GLOBAL PARALLEL VARIABLES ----- */
+static int nprocs;
+static int memory_mb;
+static int *in_fd;
+/* ------------------------------------- */
+
 static void resamp_unweighted(void)
 {
     stat_func *method_fn;
-    DCELL *values;
     int *col_map, *row_map;
     int row, col;
+    int i, t, k;
+    int chunk_size;
+    int row_start;
+    DCELL *chunk_buf;
+    DCELL **t_values;
+    DCELL ***t_bufs;
 
     method_fn = menu[method].method;
-
-    values = G_malloc(row_scale * col_scale * sizeof(DCELL));
 
     col_map = G_malloc((dst_w.cols + 1) * sizeof(int));
     row_map = G_malloc((dst_w.rows + 1) * sizeof(int));
 
+    /* Pre-calculate coordinate maps (serial) */
     for (col = 0; col <= dst_w.cols; col++) {
         double x = Rast_col_to_easting(col, &dst_w);
 
-        /* col_map[col] = (int)floor(Rast_easting_to_col(x, &src_w) + 0.5); */
         col_map[col] = (int)floor((x - src_w.west) / src_w.ew_res + 0.5);
     }
-
     for (row = 0; row <= dst_w.rows; row++) {
         double y = Rast_row_to_northing(row, &dst_w);
 
         row_map[row] = (int)floor(Rast_northing_to_row(y, &src_w) + 0.5);
     }
 
-    for (row = 0; row < dst_w.rows; row++) {
-        int maprow0 = row_map[row + 0];
-        int maprow1 = row_map[row + 1];
-        int count = maprow1 - maprow0;
-        int i;
+    /* Calculate chunk size based on memory_mb */
+    {
+        size_t row_size = dst_w.cols * sizeof(DCELL);
+        size_t total_mem_bytes = (size_t)memory_mb * 1024 * 1024;
 
-        G_percent(row, dst_w.rows, 4);
+        if (row_size > 0)
+            chunk_size = total_mem_bytes / row_size;
+        else
+            chunk_size = dst_w.rows;
+    }
+    if (chunk_size < nprocs)
+        chunk_size = nprocs;
+    if (chunk_size > dst_w.rows)
+        chunk_size = dst_w.rows;
 
-        for (i = 0; i < count; i++)
-            Rast_get_d_row(infile, bufs[i], maprow0 + i);
+    G_message(_("Parallel processing: Using %d threads, chunk size %d rows"),
+              nprocs, chunk_size);
 
-        for (col = 0; col < dst_w.cols; col++) {
-            int mapcol0 = col_map[col + 0];
-            int mapcol1 = col_map[col + 1];
-            int null = 0;
-            int n = 0;
-            int i, j;
+    /* Allocate the output chunk buffer */
+    chunk_buf = G_malloc((size_t)chunk_size * dst_w.cols * sizeof(DCELL));
 
-            for (i = maprow0; i < maprow1; i++)
-                for (j = mapcol0; j < mapcol1; j++) {
-                    DCELL *src = &bufs[i - maprow0][j];
-                    DCELL *dst = &values[n++];
+    /* Pre-allocate per-thread buffers */
+    t_values = G_malloc(nprocs * sizeof(DCELL *));
+    t_bufs = G_malloc(nprocs * sizeof(DCELL **));
+    for (t = 0; t < nprocs; t++) {
+        t_values[t] = G_malloc(row_scale * col_scale * sizeof(DCELL));
+        t_bufs[t] = G_malloc(row_scale * sizeof(DCELL *));
+        for (k = 0; k < row_scale; k++)
+            t_bufs[t][k] = Rast_allocate_d_input_buf();
+    }
 
-                    if (Rast_is_d_null_value(src)) {
-                        Rast_set_d_null_value(dst, 1);
-                        null = 1;
+    /* Main chunking loop */
+    for (row_start = 0; row_start < dst_w.rows; row_start += chunk_size) {
+        int current_chunk_rows = chunk_size;
+
+        if (row_start + current_chunk_rows > dst_w.rows)
+            current_chunk_rows = dst_w.rows - row_start;
+
+        G_percent(row_start, dst_w.rows, 2);
+
+/* PARALLEL REGION */
+#pragma omp parallel for private(row, col, i, t, k) schedule(static)
+        for (i = 0; i < current_chunk_rows; i++) {
+            int global_row = row_start + i;
+            int maprow0, maprow1, count;
+            int my_infile;
+            int mapcol0, mapcol1;
+            int null, n, r, c;
+            DCELL *values;
+            DCELL **local_bufs;
+            DCELL *out_row_ptr;
+
+#if defined(_OPENMP)
+            t = omp_get_thread_num();
+#else
+            t = 0;
+#endif
+
+            values = t_values[t];
+            local_bufs = t_bufs[t];
+            my_infile = in_fd[t];
+
+            maprow0 = row_map[global_row + 0];
+            maprow1 = row_map[global_row + 1];
+            count = maprow1 - maprow0;
+
+            /* Read input rows into pre-allocated per-thread buffers */
+            for (k = 0; k < count; k++)
+                Rast_get_d_row(my_infile, local_bufs[k], maprow0 + k);
+
+            out_row_ptr = &chunk_buf[(size_t)i * dst_w.cols];
+
+            for (col = 0; col < dst_w.cols; col++) {
+                mapcol0 = col_map[col + 0];
+                mapcol1 = col_map[col + 1];
+                null = 0;
+                n = 0;
+
+                for (r = 0; r < count; r++) {
+                    for (c = mapcol0; c < mapcol1; c++) {
+                        DCELL *src = &local_bufs[r][c];
+                        DCELL *dst = &values[n++];
+
+                        if (Rast_is_d_null_value(src)) {
+                            Rast_set_d_null_value(dst, 1);
+                            null = 1;
+                        }
+                        else {
+                            *dst = *src;
+                        }
                     }
-                    else
-                        *dst = *src;
                 }
 
-            if (null && nulls)
-                Rast_set_d_null_value(&outbuf[col], 1);
-            else
-                (*method_fn)(&outbuf[col], values, n, closure);
-        }
+                if (null && nulls)
+                    Rast_set_d_null_value(&out_row_ptr[col], 1);
+                else
+                    (*method_fn)(&out_row_ptr[col], values, n, closure);
+            }
+        } /* End parallel for */
 
-        Rast_put_d_row(outfile, outbuf);
+        /* Sequential write to disk */
+        for (i = 0; i < current_chunk_rows; i++)
+            Rast_put_d_row(outfile, &chunk_buf[(size_t)i * dst_w.cols]);
     }
+
+    /* Free per-thread buffers */
+    for (t = 0; t < nprocs; t++) {
+        for (k = 0; k < row_scale; k++)
+            G_free(t_bufs[t][k]);
+        G_free(t_bufs[t]);
+        G_free(t_values[t]);
+    }
+    G_free(t_bufs);
+    G_free(t_values);
+    G_free(chunk_buf);
     G_free(row_map);
     G_free(col_map);
-    G_free(values);
 }
 
 static void resamp_weighted(void)
 {
     stat_func_w *method_fn;
-
-    DCELL(*values)[2];
     double *col_map, *row_map;
     int row, col;
+    int i, t, k;
+    int chunk_size;
+    int row_start;
+    DCELL *chunk_buf;
+    DCELL(**t_values)[2];
+    DCELL ***t_bufs;
 
     method_fn = menu[method].method_w;
-
-    values = G_malloc(row_scale * col_scale * 2 * sizeof(DCELL));
 
     col_map = G_malloc((dst_w.cols + 1) * sizeof(double));
     row_map = G_malloc((dst_w.rows + 1) * sizeof(double));
 
+    /* Pre-calculate coordinate maps (serial) */
     for (col = 0; col <= dst_w.cols; col++) {
         double x = Rast_col_to_easting(col, &dst_w);
 
-        /* col_map[col] = Rast_easting_to_col(x, &src_w); */
         col_map[col] = (x - src_w.west) / src_w.ew_res;
     }
-
     for (row = 0; row <= dst_w.rows; row++) {
         double y = Rast_row_to_northing(row, &dst_w);
 
         row_map[row] = Rast_northing_to_row(y, &src_w);
     }
 
-    for (row = 0; row < dst_w.rows; row++) {
-        double y0 = row_map[row + 0];
-        double y1 = row_map[row + 1];
-        int maprow0 = (int)floor(y0);
-        int maprow1 = (int)ceil(y1);
-        int count = maprow1 - maprow0;
-        int i;
+    /* Calculate chunk size */
+    {
+        size_t row_size = dst_w.cols * sizeof(DCELL);
+        size_t total_mem_bytes = (size_t)memory_mb * 1024 * 1024;
 
-        G_percent(row, dst_w.rows, 4);
+        if (row_size > 0)
+            chunk_size = total_mem_bytes / row_size;
+        else
+            chunk_size = dst_w.rows;
+    }
+    if (chunk_size < nprocs)
+        chunk_size = nprocs;
+    if (chunk_size > dst_w.rows)
+        chunk_size = dst_w.rows;
 
-        for (i = 0; i < count; i++)
-            Rast_get_d_row(infile, bufs[i], maprow0 + i);
+    G_message(
+        _("Weighted parallel processing: Using %d threads, chunk size %d rows"),
+        nprocs, chunk_size);
 
-        for (col = 0; col < dst_w.cols; col++) {
-            double x0 = col_map[col + 0];
-            double x1 = col_map[col + 1];
-            int mapcol0 = (int)floor(x0);
-            int mapcol1 = (int)ceil(x1);
-            int null = 0;
-            int n = 0;
-            int i, j;
+    /* Allocate output buffer */
+    chunk_buf = G_malloc((size_t)chunk_size * dst_w.cols * sizeof(DCELL));
 
-            for (i = maprow0; i < maprow1; i++) {
-                double ky = (i == maprow0)       ? 1 - (y0 - maprow0)
-                            : (i == maprow1 - 1) ? 1 - (maprow1 - y1)
-                                                 : 1;
+    /* Pre-allocate per-thread buffers */
+    t_values = G_malloc(nprocs * sizeof(DCELL(*)[2]));
+    t_bufs = G_malloc(nprocs * sizeof(DCELL **));
+    for (t = 0; t < nprocs; t++) {
+        t_values[t] = G_malloc(row_scale * col_scale * 2 * sizeof(DCELL));
+        t_bufs[t] = G_malloc(row_scale * sizeof(DCELL *));
+        for (k = 0; k < row_scale; k++)
+            t_bufs[t][k] = Rast_allocate_d_input_buf();
+    }
 
-                for (j = mapcol0; j < mapcol1; j++) {
-                    double kx = (j == mapcol0)       ? 1 - (x0 - mapcol0)
-                                : (j == mapcol1 - 1) ? 1 - (mapcol1 - x1)
+    /* Main chunking loop */
+    for (row_start = 0; row_start < dst_w.rows; row_start += chunk_size) {
+        int current_chunk_rows = chunk_size;
+
+        if (row_start + current_chunk_rows > dst_w.rows)
+            current_chunk_rows = dst_w.rows - row_start;
+
+        G_percent(row_start, dst_w.rows, 2);
+
+/* PARALLEL REGION */
+#pragma omp parallel for private(row, col, i, t, k) schedule(static)
+        for (i = 0; i < current_chunk_rows; i++) {
+            int global_row = row_start + i;
+            double y0, y1;
+            int maprow0, maprow1, count;
+            int my_infile;
+            int mapcol0, mapcol1;
+            int null, n, r, c;
+            double x0, x1;
+            DCELL(*values)[2];
+            DCELL **local_bufs;
+            DCELL *out_row_ptr;
+
+#if defined(_OPENMP)
+            t = omp_get_thread_num();
+#else
+            t = 0;
+#endif
+
+            values = t_values[t];
+            local_bufs = t_bufs[t];
+            my_infile = in_fd[t];
+
+            y0 = row_map[global_row + 0];
+            y1 = row_map[global_row + 1];
+            maprow0 = (int)floor(y0);
+            maprow1 = (int)ceil(y1);
+            count = maprow1 - maprow0;
+
+            /* Read input rows into pre-allocated per-thread buffers */
+            for (k = 0; k < count; k++)
+                Rast_get_d_row(my_infile, local_bufs[k], maprow0 + k);
+
+            out_row_ptr = &chunk_buf[(size_t)i * dst_w.cols];
+
+            for (col = 0; col < dst_w.cols; col++) {
+                x0 = col_map[col + 0];
+                x1 = col_map[col + 1];
+                mapcol0 = (int)floor(x0);
+                mapcol1 = (int)ceil(x1);
+                null = 0;
+                n = 0;
+
+                for (r = maprow0; r < maprow1; r++) {
+                    /* Calculate Y overlap weight */
+                    double ky = (r == maprow0)       ? 1 - (y0 - maprow0)
+                                : (r == maprow1 - 1) ? 1 - (maprow1 - y1)
                                                      : 1;
 
-                    DCELL *src = &bufs[i - maprow0][j];
-                    DCELL *dst = &values[n++][0];
+                    for (c = mapcol0; c < mapcol1; c++) {
+                        /* Calculate X overlap weight */
+                        double kx = (c == mapcol0)       ? 1 - (x0 - mapcol0)
+                                    : (c == mapcol1 - 1) ? 1 - (mapcol1 - x1)
+                                                         : 1;
 
-                    if (Rast_is_d_null_value(src)) {
-                        Rast_set_d_null_value(&dst[0], 1);
-                        null = 1;
-                    }
-                    else {
-                        dst[0] = *src;
-                        dst[1] = kx * ky;
+                        DCELL *src = &local_bufs[r - maprow0][c];
+                        DCELL *dst = &values[n++][0];
+
+                        if (Rast_is_d_null_value(src)) {
+                            Rast_set_d_null_value(&dst[0], 1);
+                            null = 1;
+                        }
+                        else {
+                            dst[0] = *src;
+                            dst[1] = kx * ky; /* Store weight */
+                        }
                     }
                 }
+
+                if (null && nulls)
+                    Rast_set_d_null_value(&out_row_ptr[col], 1);
+                else
+                    (*method_fn)(&out_row_ptr[col], values, n, closure);
             }
+        } /* End parallel for */
 
-            if (null && nulls)
-                Rast_set_d_null_value(&outbuf[col], 1);
-            else
-                (*method_fn)(&outbuf[col], values, n, closure);
-        }
-
-        Rast_put_d_row(outfile, outbuf);
+        /* Sequential write to disk */
+        for (i = 0; i < current_chunk_rows; i++)
+            Rast_put_d_row(outfile, &chunk_buf[(size_t)i * dst_w.cols]);
     }
+
+    /* Free per-thread buffers */
+    for (t = 0; t < nprocs; t++) {
+        for (k = 0; k < row_scale; k++)
+            G_free(t_bufs[t][k]);
+        G_free(t_bufs[t]);
+        G_free(t_values[t]);
+    }
+    G_free(t_bufs);
+    G_free(t_values);
+    G_free(chunk_buf);
     G_free(row_map);
     G_free(col_map);
-    G_free(values);
 }
 
 int main(int argc, char *argv[])
 {
     struct GModule *module;
     struct {
-        struct Option *rastin, *rastout, *method, *quantile;
+        struct Option *rastin, *rastout, *method, *quantile, *nprocs, *memory;
     } parm;
     struct {
         struct Flag *nulls, *weight;
@@ -254,7 +420,7 @@ int main(int argc, char *argv[])
     char title[64];
     char buf_nsres[100], buf_ewres[100];
     struct Colors colors;
-    int row;
+    int i;
 
     G_gisinit(argv[0]);
 
@@ -263,6 +429,7 @@ int main(int argc, char *argv[])
     G_add_keyword(_("resample"));
     G_add_keyword(_("univariate statistics"));
     G_add_keyword(_("aggregation"));
+    G_add_keyword(_("parallel"));
     module->description =
         _("Resamples raster map layers to a coarser grid using aggregation.");
 
@@ -286,6 +453,9 @@ int main(int argc, char *argv[])
     parm.quantile->options = "0.0-1.0";
     parm.quantile->answer = "0.5";
 
+    parm.nprocs = G_define_standard_option(G_OPT_M_NPROCS);
+    parm.memory = G_define_standard_option(G_OPT_MEMORYMB);
+
     flag.nulls = G_define_flag();
     flag.nulls->key = 'n';
     flag.nulls->description = _("Propagate NULLs");
@@ -297,6 +467,17 @@ int main(int argc, char *argv[])
     if (G_parser(argc, argv))
         exit(EXIT_FAILURE);
 
+    /* --- PARALLEL SETUP --- */
+    nprocs = G_set_omp_num_threads(parm.nprocs);
+    nprocs = Rast_disable_omp_on_mask(nprocs);
+    if (nprocs < 1)
+        G_fatal_error(_("<%d> is not valid number of nprocs."), nprocs);
+
+    memory_mb = atoi(parm.memory->answer);
+    if (memory_mb <= 0)
+        memory_mb = 300;
+    /* ---------------------------- */
+
     nulls = flag.nulls->answer;
 
     method = find_method(parm.method->answer);
@@ -304,7 +485,7 @@ int main(int argc, char *argv[])
         G_fatal_error(_("Unknown method <%s>"), parm.method->answer);
 
     if (menu[method].method == c_quant) {
-        quantile = atoi(parm.quantile->answer);
+        quantile = atof(parm.quantile->answer);
         closure = &quantile;
     }
 
@@ -353,16 +534,10 @@ int main(int argc, char *argv[])
     row_scale = 2 + ceil(dst_w.ns_res / src_w.ns_res);
     col_scale = 2 + ceil(dst_w.ew_res / src_w.ew_res);
 
-    /* allocate buffers for input rows */
-    bufs = G_malloc(row_scale * sizeof(DCELL *));
-    for (row = 0; row < row_scale; row++)
-        bufs[row] = Rast_allocate_d_input_buf();
-
-    /* open old map */
-    infile = Rast_open_old(parm.rastin->answer, "");
-
-    /* allocate output buffer */
-    outbuf = Rast_allocate_d_output_buf();
+    /* open old map (multiple times for thread safety) */
+    in_fd = G_malloc(nprocs * sizeof(int));
+    for (i = 0; i < nprocs; i++)
+        in_fd[i] = Rast_open_old(parm.rastin->answer, "");
 
     /* open new map */
     outfile = Rast_open_new(parm.rastout->answer, DCELL_TYPE);
@@ -374,7 +549,10 @@ int main(int argc, char *argv[])
 
     G_percent(dst_w.rows, dst_w.rows, 2);
 
-    Rast_close(infile);
+    /* Close all input file descriptors */
+    for (i = 0; i < nprocs; i++)
+        Rast_close(in_fd[i]);
+    G_free(in_fd);
     Rast_close(outfile);
 
     /* record map metadata/history info */
