22f08695d28294c4161b5d3ecffa5a9c5235dbb6
lindakladivova <l.kladivova@seznam.cz>
2026-02-16 23:39:04 +0100

    better error handling and new lightweight panel for Jupyter running in external browser

--- gui/wxpython/jupyter_notebook/panel.py
+++ gui/wxpython/jupyter_notebook/panel.py
@@ -5,6 +5,7 @@
 
 Classes:
  - panel::JupyterPanel
+ - panel::JupyterBrowserPanel
 
 (C) 2026 by the GRASS Development Team
 
@@ -17,6 +18,7 @@ This program is free software under the GNU General Public License
 from pathlib import Path
 
 import wx
+import webbrowser
 
 from main_window.page import MainPageBase
 
@@ -26,6 +28,18 @@ from .toolbars import JupyterToolbar
 
 
 class JupyterPanel(wx.Panel, MainPageBase):
+    """Integrated Jupyter Notebook panel with embedded browser (requires wx.html2).
+
+    This panel provides a full-featured Jupyter Notebook interface embedded
+    directly in the GRASS GUI using wx.html2.WebView. Notebooks are displayed
+    in tabs within the GUI itself.
+
+    For a lightweight alternative that opens notebooks in an external browser
+    without requiring wx.html2, see JupyterBrowserPanel.
+
+    The Jupyter server is automatically stopped when this panel is closed.
+    """
+
     def __init__(
         self,
         parent,
@@ -38,18 +52,18 @@ class JupyterPanel(wx.Panel, MainPageBase):
         create_template=False,
         **kwargs,
     ):
-        """Jupyter main panel."""
         super().__init__(parent=parent, id=id, **kwargs)
         MainPageBase.__init__(self, dockable)
 
         self.parent = parent
         self._giface = giface
         self.statusbar = statusbar
-        self.workdir = workdir
-        self.SetName("Jupyter")
+        self.SetName("JupyterIntegrated")
 
+        # Create environment in integrated mode (requires wx.html2)
         self.env = JupyterEnvironment(
-            workdir=self.workdir, create_template=create_template, integrated=True
+            workdir=workdir,
+            create_template=create_template,
         )
 
         self.toolbar = JupyterToolbar(parent=self)
@@ -58,7 +72,6 @@ class JupyterPanel(wx.Panel, MainPageBase):
         self._layout()
 
     def _layout(self):
-        """Do layout"""
         sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(self.toolbar, proportion=0, flag=wx.EXPAND)
         sizer.Add(self.aui_notebook, proportion=1, flag=wx.EXPAND)
@@ -68,9 +81,17 @@ class JupyterPanel(wx.Panel, MainPageBase):
         sizer.Fit(self)
         self.Layout()
 
-    def SetUpNotebookInterface(self):
-        """Setup Jupyter notebook environment and load initial notebooks."""
+    def SetUpEnvironment(self):
+        """Setup integrated Jupyter notebook environment and load initial notebooks.
+
+        - Prepares notebook files in the working directory
+        - Starts the Jupyter server
+        - Loads all existing notebooks as tabs in the embedded browser
+
+        :return: bool: True if setup was successful, False otherwise
+        """
         try:
+            # Prepare files and start server
             self.env.setup()
         except RuntimeError as e:
             wx.MessageBox(
@@ -78,7 +99,7 @@ class JupyterPanel(wx.Panel, MainPageBase):
                 _("Startup Error"),
                 wx.ICON_ERROR,
             )
-            return
+            return False
 
         # Load notebook tabs in embedded AUI notebook
         for fname in self.env.directory.files:
@@ -90,22 +111,24 @@ class JupyterPanel(wx.Panel, MainPageBase):
                     _("Startup Error"),
                     wx.ICON_ERROR,
                 )
-                return
+                return False
+
             self.aui_notebook.AddPage(url=url, title=fname.name)
 
         self.SetStatusText(
             _("Jupyter server started at {url} (PID: {pid}), directory: {dir}").format(
-                url=self.env.server.server_url,
-                pid=self.env.server.pid,
-                dir=str(self.workdir),
+                url=self.env.server_url,
+                pid=self.env.pid,
+                dir=self.env.workdir,
             )
         )
+        return True
 
     def Switch(self, file_name):
-        """
-        Switch to existing notebook tab.
-        :param file_name: Name of the .ipynb file (e.g., 'example.ipynb') (str).
-        :return: True if the notebook was found and switched to, False otherwise.
+        """Switch to existing notebook tab.
+
+        :param file_name: Name of the .ipynb file (e.g., 'example.ipynb')
+        :return: True if the notebook was found and switched to, False otherwise
         """
         for i in range(self.aui_notebook.GetPageCount()):
             if self.aui_notebook.GetPageText(i) == file_name:
@@ -114,9 +137,9 @@ class JupyterPanel(wx.Panel, MainPageBase):
         return False
 
     def Open(self, file_name):
-        """
-        Open a Jupyter notebook to a new tab and switch to it.
-        :param file_name: Name of the .ipynb file (e.g., 'example.ipynb') (str).
+        """Open a Jupyter notebook in a new tab and switch to it.
+
+        :param file_name: Name of the .ipynb file (e.g., 'example.ipynb')
         """
         try:
             url = self.env.server.get_url(file_name)
@@ -130,9 +153,9 @@ class JupyterPanel(wx.Panel, MainPageBase):
             )
 
     def OpenOrSwitch(self, file_name):
-        """
-        Switch to .ipynb file if open, otherwise open it.
-        :param file_name: Name of the .ipynb file (e.g., 'example.ipynb') (str).
+        """Switch to .ipynb file if open, otherwise open it.
+
+        :param file_name: Name of the .ipynb file (e.g., 'example.ipynb')
         """
         if self.Switch(file_name):
             self.SetStatusText(_("File '{}' is already opened.").format(file_name), 0)
@@ -141,10 +164,10 @@ class JupyterPanel(wx.Panel, MainPageBase):
             self.SetStatusText(_("File '{}' opened.").format(file_name), 0)
 
     def Import(self, source_path, new_name=None):
-        """
-        Import a .ipynb file into a working directory and open it to a new tab.
-        :param source_path: Path to the source .ipynb file to be imported (Path).
-        :param new_name: Optional new name for the imported file (str).
+        """Import a .ipynb file into working directory and open it in a new tab.
+
+        :param source_path: Path to the source .ipynb file to be imported
+        :param new_name: Optional new name for the imported file
         """
         try:
             path = self.env.directory.import_file(source_path, new_name=new_name)
@@ -158,14 +181,11 @@ class JupyterPanel(wx.Panel, MainPageBase):
             )
 
     def OnImport(self, event=None):
-        """
-        Import an existing Jupyter notebook file into the working directory
-        and open it in the GUI.
-        - Prompts user to select a .ipynb file.
-        - If the selected file is already in the notebook directory:
-            - Switch to it or open it.
-        - If the file is from elsewhere:
-            - Import the notebook and open it (if needed, prompt for a new name).
+        """Import an existing Jupyter notebook file into the working directory.
+
+        Prompts user to select a .ipynb file:
+        - If the file is already in the notebook directory: switch to it or open it
+        - If the file is from elsewhere: import and open it (prompt for new name if needed)
         """
         # Open file dialog to select an existing Jupyter notebook file
         with wx.FileDialog(
@@ -180,7 +200,7 @@ class JupyterPanel(wx.Panel, MainPageBase):
 
             source_path = Path(dlg.GetPath())
             file_name = source_path.name
-            target_path = self.workdir / file_name
+            target_path = self.env.workdir / file_name
 
             # File is already in the working directory
             if source_path.resolve() == target_path.resolve():
@@ -247,10 +267,7 @@ class JupyterPanel(wx.Panel, MainPageBase):
                 )
 
     def OnCreate(self, event=None):
-        """
-        Prompt the user to create a new empty Jupyter notebook in the working directory,
-        and open it in the GUI.
-        """
+        """Create a new empty Jupyter notebook in the working directory and open it."""
         with wx.TextEntryDialog(
             self,
             message=_("Enter a name for the new notebook:"),
@@ -276,19 +293,15 @@ class JupyterPanel(wx.Panel, MainPageBase):
 
             # Open the newly created notebook in the GUI
             self.Open(path.name)
+            self.SetStatusText(_("New file '{}' created.").format(path.name), 0)
 
-    def SetStatusText(self, *args):
-        """Set text in the status bar."""
-        self.statusbar.SetStatusText(*args)
+    def OnCloseWindow(self, event=None):
+        """Cleanup when panel is being closed (called by parent notebook).
 
-    def GetStatusBar(self):
-        """Get statusbar"""
-        return self.statusbar
-
-    def OnCloseWindow(self, event):
-        """Prompt user, then stop server and close panel."""
+        This method is called by mainnotebook when the tab is being closed.
+        """
         confirm = wx.MessageBox(
-            _("Do you really want to close this window and stop the Jupyter server?"),
+            _("Do you really want to close this tab and stop the Jupyter server?"),
             _("Confirm Close"),
             wx.ICON_QUESTION | wx.YES_NO | wx.NO_DEFAULT,
         )
@@ -298,25 +311,233 @@ class JupyterPanel(wx.Panel, MainPageBase):
                 event.Veto()
             return
 
-        # Get server info
-        url = self.env.server.server_url
-        pid = self.env.server.pid
+        if self.env:
+            # Get server info before stopping
+            url = self.env.server_url
+            pid = self.env.pid
 
-        # Stop server and close panel
+            try:
+                self.env.stop()
+                if self.statusbar:
+                    self.statusbar.SetStatusText(
+                        _(
+                            "Jupyter server at {url} (PID: {pid}) has been stopped"
+                        ).format(url=url, pid=pid)
+                    )
+            except Exception as e:
+                wx.MessageBox(
+                    _("Failed to stop Jupyter server:\n{}").format(str(e)),
+                    _("Error"),
+                    wx.ICON_ERROR,
+                )
+                if event and hasattr(event, "Veto"):
+                    event.Veto()
+                return
+
+        self._onCloseWindow(event)
+
+    def SetStatusText(self, *args):
+        """Set text in the status bar."""
+        self.statusbar.SetStatusText(*args)
+
+    def GetStatusBar(self):
+        """Get statusbar."""
+        return self.statusbar
+
+
+class JupyterBrowserPanel(wx.Panel, MainPageBase):
+    """Lightweight panel for Jupyter running in external browser.
+
+    This panel doesn't require wx.html2 and just shows info about
+    the running Jupyter server with a button to stop it.
+    """
+
+    def __init__(
+        self,
+        parent,
+        giface,
+        id=wx.ID_ANY,
+        statusbar=None,
+        dockable=False,
+        workdir=None,
+        create_template=False,
+        **kwargs,
+    ):
+        super().__init__(parent=parent, id=id, **kwargs)
+        MainPageBase.__init__(self, dockable)
+
+        self.parent = parent
+        self._giface = giface
+        self.statusbar = statusbar
+        self.SetName("JupyterBrowser")
+
+        self.env = JupyterEnvironment(workdir=workdir, create_template=create_template)
+
+        self._layout()
+
+    def _layout(self):
+        """Create simple layout with message and controls."""
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_sizer.AddStretchSpacer(1)
+
+        # Info icon and message
+        info_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        info_icon = wx.StaticBitmap(
+            self,
+            bitmap=wx.ArtProvider.GetBitmap(wx.ART_INFORMATION, wx.ART_MESSAGE_BOX),
+        )
+        info_sizer.Add(info_icon, flag=wx.ALL, border=5)
+
+        info_text = wx.StaticText(
+            self,
+            label=_("Jupyter Notebook has been opened in your default browser."),
+        )
+        info_text.SetFont(info_text.GetFont().Bold())
+        info_sizer.Add(info_text, flag=wx.ALL | wx.ALIGN_CENTER_VERTICAL, border=5)
+
+        main_sizer.Add(info_sizer, flag=wx.ALL | wx.ALIGN_CENTER, border=10)
+
+        # Server details (will be populated after setup)
+        details_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.url_text = wx.StaticText(self, label="")
+        details_sizer.Add(self.url_text, flag=wx.ALL | wx.ALIGN_CENTER, border=3)
+
+        self.pid_text = wx.StaticText(self, label="")
+        details_sizer.Add(self.pid_text, flag=wx.ALL | wx.ALIGN_CENTER, border=3)
+
+        self.dir_text = wx.StaticText(self, label="")
+        details_sizer.Add(self.dir_text, flag=wx.ALL | wx.ALIGN_CENTER, border=3)
+
+        main_sizer.Add(details_sizer, flag=wx.ALL | wx.ALIGN_CENTER, border=10)
+
+        # Buttons
+        button_sizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        open_browser_btn = wx.Button(self, label=_("Open in Browser Again"))
+        open_browser_btn.Bind(wx.EVT_BUTTON, self.OnOpenBrowser)
+        button_sizer.Add(open_browser_btn, flag=wx.ALL, border=5)
+
+        stop_btn = wx.Button(self, label=_("Stop Jupyter Server"))
+        stop_btn.Bind(wx.EVT_BUTTON, self.OnStop)
+        button_sizer.Add(stop_btn, flag=wx.ALL, border=5)
+
+        main_sizer.Add(button_sizer, flag=wx.ALL | wx.ALIGN_CENTER, border=10)
+        main_sizer.AddStretchSpacer(2)
+
+        self.SetSizer(main_sizer)
+        self.Layout()
+
+    def SetUpEnvironment(self):
+        """Setup Jupyter environment and open in browser.
+
+        :return: bool: True if setup was successful, False otherwis
+        """
+
         try:
-            self.env.stop()
+            # Prepare files and start server
+            self.env.setup()
         except RuntimeError as e:
             wx.MessageBox(
-                _("Failed to stop Jupyter server at {url} (PID: {pid}):\n{err}").format(
-                    url=url, pid=pid, err=e
-                ),
-                caption=_("Error"),
-                style=wx.ICON_ERROR | wx.OK,
+                _("Failed to start Jupyter environment:\n{}").format(e),
+                _("Startup Error"),
+                wx.ICON_ERROR,
             )
-            return
-        self.SetStatusText(
-            _("Jupyter server at {url} (PID: {pid}) has been stopped").format(
-                url=url, pid=pid
-            )
+            return False
+
+        # Update UI with server info
+        self.url_text.SetLabel(_("Server URL: {}").format(self.env.server_url))
+        self.pid_text.SetLabel(_("Process ID: {}").format(self.env.pid))
+        self.dir_text.SetLabel(_("Working directory: {}").format(self.env.workdir))
+
+        self.Layout()
+
+        # Open in default browser
+        webbrowser.open(self.env.server_url)
+
+        return True
+
+    def OnOpenBrowser(self, event):
+        """Re-open the Jupyter server URL in browser."""
+        if self.env and self.env.server and self.env.server_url:
+            webbrowser.open(self.env.server_url)
+
+    def OnStop(self, event):
+        """Stop the Jupyter server and close the tab."""
+        if self.env:
+            # Get server info before stopping
+            url = self.env.server_url
+            pid = self.env.pid
+
+            try:
+                # Stop server and unregister it
+                self.env.stop()
+                self.statusbar.SetStatusText(
+                    _("Jupyter server at {url} (PID: {pid}) has been stopped").format(
+                        url=url, pid=pid
+                    )
+                )
+                # Close this tab/panel
+                parent_notebook = self.GetParent()
+                if hasattr(parent_notebook, "DeletePage"):
+                    # Find our page index and delete it
+                    for i in range(parent_notebook.GetPageCount()):
+                        if parent_notebook.GetPage(i) == self:
+                            parent_notebook.DeletePage(i)
+                            break
+            except Exception as e:
+                wx.MessageBox(
+                    _("Failed to stop server:\n{}").format(str(e)),
+                    _("Error"),
+                    wx.ICON_ERROR,
+                )
+
+    def OnCloseWindow(self, event=None):
+        """Cleanup when panel is being closed (called by parent notebook).
+
+        This method is called by mainnotebook when the tab is being closed.
+        """
+        confirm = wx.MessageBox(
+            _("Do you really want to close this tab and stop the Jupyter server?"),
+            _("Confirm Close"),
+            wx.ICON_QUESTION | wx.YES_NO | wx.NO_DEFAULT,
         )
+
+        if confirm != wx.YES:
+            if event and hasattr(event, "Veto"):
+                event.Veto()
+            return
+
+        if self.env:
+            # Get server info before stopping
+            url = self.env.server_url
+            pid = self.env.pid
+
+            try:
+                self.env.stop()
+                if self.statusbar:
+                    self.statusbar.SetStatusText(
+                        _(
+                            "Jupyter server at {url} (PID: {pid}) has been stopped"
+                        ).format(url=url, pid=pid)
+                    )
+            except Exception as e:
+                wx.MessageBox(
+                    _("Failed to stop Jupyter server:\n{}").format(str(e)),
+                    _("Error"),
+                    wx.ICON_ERROR,
+                )
+                if event and hasattr(event, "Veto"):
+                    event.Veto()
+                return
+
         self._onCloseWindow(event)
+
+    def SetStatusText(self, *args):
+        """Set text in the status bar."""
+        if self.statusbar:
+            self.statusbar.SetStatusText(*args)
+
+    def GetStatusBar(self):
+        """Get status bar."""
+        return self.statusbar
