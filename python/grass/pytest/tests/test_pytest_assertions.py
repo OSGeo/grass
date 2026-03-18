eae7b6abf62aca7192f3cd76f17f2fa8e1bd837a
Ayush Kumar <ayushkumarAk3115@gmail.com>
2026-03-19 01:24:43 +0530

    improve testing and apply feedback from maintainer

--- python/grass/pytest/tests/test_pytest_assertions.py
+++ python/grass/pytest/tests/test_pytest_assertions.py
@@ -1,9 +1,12 @@
+import pytest
 import grass.script as gs
-from grass.pytest.assertions import rasterExists
+from grass.pytest.assertions import raster_exists
 
 
-def test_rasterExists(session):
+def test_raster_exists(session):
     gs.run_command("r.mapcalc", expression="test_map = 1", env=session)
 
-    assert rasterExists("test_map", env=session)
-    assert not rasterExists("missing_map", env=session)
+    raster_exists("test_map", env=session)
+    
+    with pytest.raises(AssertionError, match="Raster map 'missing_map' not found."):
+        raster_exists("missing_map", env=session)
