f9a05362f8a1cb4f19658132cd0b5d47118c0e90
Ayush Kumar <ayushkumarAk3115@gmail.com>
2026-03-19 10:34:14 +0530

    improve code

--- python/grass/pytest/assertions.py
+++ python/grass/pytest/assertions.py
@@ -5,4 +5,5 @@ def raster_exists(name, env=None):
     """Assert that a raster map exists in the given mapset."""
     result = gs.find_file(name, element="cell", env=env)
     if not result["name"]:
-        raise AssertionError(f"Raster map '{name}' not found.")
+        msg = f"Raster map '{name}' not found."
+        raise AssertionError(msg)
