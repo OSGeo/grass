069f8b71ab5e269b403b9ce616343b090d749d31
Ayush Kumar <ayushkumarAk3115@gmail.com>
2026-03-19 12:32:59 +0530

    Use raw string

--- python/grass/pytest/tests/test_pytest_assertions.py
+++ python/grass/pytest/tests/test_pytest_assertions.py
@@ -7,5 +7,5 @@ def test_raster_exists(session):
     gs.run_command("r.mapcalc", expression="test_map = 1", env=session)
 
     raster_exists("test_map", env=session)
-    with pytest.raises(AssertionError, match="Raster map 'missing_map' not found."):
+    with pytest.raises(AssertionError, match=r"missing_map"):
         raster_exists("missing_map", env=session)
