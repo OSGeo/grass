5a31d549cc0150239894bd2b9e67250af87d38b5
Nicklas Larsson <n_larsson@yahoo.com>
2026-04-17 01:04:26 +0200

    lib: prefix macros UNUSED and FALLTHROUGH (#7309)

--- lib/db/stubs/create_table.c
+++ lib/db/stubs/create_table.c
@@ -1,7 +1,7 @@
 #include <grass/dbmi.h>
 #include <grass/dbstubs.h>
 
-int db__driver_create_table(dbTable *table UNUSED)
+int db__driver_create_table(dbTable *table G_UNUSED)
 {
     db_procedure_not_implemented("db_create_table");
     return DB_FAILED;
