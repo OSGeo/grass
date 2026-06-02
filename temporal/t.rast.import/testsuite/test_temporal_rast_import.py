e9d4189cfeee9a588d83e402c1dfcdb2b03b52f5
saket0187 <saketmalltheofficial08@gmail.com>
2026-06-02 15:22:13 +0530

    Fixed test file

--- temporal/t.rast.import/testsuite/test_temporal_rast_import.py
+++ temporal/t.rast.import/testsuite/test_temporal_rast_import.py
@@ -19,13 +19,17 @@ class TestRasterImport(TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """Remove the temporary region"""
-        cls.del_temp_region()
+        """Remove the imported data"""
         cls.runModule("t.remove", flags="df", inputs="A")
 
     def test_import(self):
         self.assertModule(
-            "t.rast.import", input=self.input_, output="A", basename="a", overwrite=True
+            "t.rast.import",
+            flags="o",
+            input=self.input_,
+            output="A",
+            basename="a",
+            overwrite=True,
         )
         tinfo = """start_time='2000-01-01 00:00:00'
                    end_time='2001-01-01 00:00:00'
@@ -34,8 +38,14 @@ class TestRasterImport(TestCase):
                    north=320000.0
                    south=10000.0
                    east=935000.0
-                   west=120000.0
-                """
+                   west=120000.0"""
 
-        info = SimpleModule("t.info", flags="g", input="A")
-        self.assertModuleKeyValue(module=info, reference=tinfo, precision=2, sep="=")
+        self.assertModuleKeyValue(
+            module="t.info", input="A", flags="g", reference=tinfo, precision=2, sep="="
+        )
+
+
+if __name__ == "__main__":
+    from grass.gunittest.main import test
+
+    test()
