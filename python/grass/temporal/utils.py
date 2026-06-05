256dbc2341d9e16a843d36a69a2991348195309a
ninsbl <stefan.blumentrath@gmx.de>
2026-06-05 22:51:24 +0200

    move TemporalEncoder to temporal utils

--- python/grass/temporal/utils.py
+++ python/grass/temporal/utils.py
@@ -1,14 +1,39 @@
 """Utility functions and classes for the temporal framework.
 
-(C) 2025 by the GRASS Development Team
+(C) 2026 by the GRASS Development Team
 This program is free software under the GNU General Public
 License (>=v2). Read the file COPYING that comes with GRASS
 for details.
 
 :authors: GRASS Development Team
 """
+import json
+from datetime import datetime
 
-# Re-export from canonical location for backward compatibility
-from grass.script.utils import TemporalJSONEncoder
+class TemporalJSONEncoder(json.JSONEncoder):
+    """Custom JSON encoder with datetime support for GRASS GIS.
 
-__all__ = ["TemporalJSONEncoder"]
+    Handles serialization of datetime objects to ISO 8601 format strings.
+    Can be used across GRASS modules that need JSON output with temporal data.
+
+    Example:
+
+    .. code-block:: pycon
+
+        >>> import json
+        >>> from datetime import datetime
+        >>> data = {"timestamp": datetime(2024, 1, 15, 10, 30)}
+        >>> json.dumps(data, cls=TemporalJSONEncoder)
+        '{"timestamp": "2024-01-15T10:30:00"}'
+    """
+
+    def default(self, obj):
+        """Override default JSON encoding for datetime objects.
+
+        :param obj: Object to encode
+        :return: ISO 8601 formatted string for datetime objects,
+                 or calls parent encoder for other types
+        """
+        if isinstance(obj, datetime):
+            return obj.isoformat()
+        return super().default(obj)
