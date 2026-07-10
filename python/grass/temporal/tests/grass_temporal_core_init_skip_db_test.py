97bcdb24639ef4a7d423ce339832a97aa683bc54
ninsbl <stefan.blumentrath@gmx.de>
2026-07-10 12:53:24 +0200

    add tests for STDS construction

--- python/grass/temporal/tests/grass_temporal_core_init_skip_db_test.py
+++ python/grass/temporal/tests/grass_temporal_core_init_skip_db_test.py
@@ -16,6 +16,19 @@ from grass.grassdb.create import create_mapset
 import grass.script as gs
 import grass.temporal as tgis
 
+ds_types_dict = {
+    "strds": ("strds"),
+    "str3ds": ("str3ds"),
+    "stvds": ("stvds"),
+    "rast": ("raster", "rast"),
+    "raster": ("raster", "rast"),
+    "raster_3d": ("raster_3d", "rast3d", "raster3d"),
+    "rast3d": ("raster_3d", "rast3d", "raster3d"),
+    "raster3d": ("raster_3d", "rast3d", "raster3d"),
+    "vect": ("vector", "vect"),
+    "vector": ("vector", "vect"),
+}
+
 
 @pytest.fixture
 def simple_mapset(tmp_path):
@@ -51,7 +64,7 @@ def test_init_creates_tgis_db_if_not_skipped(simple_mapset):
         assert (
             sqlite3.connect(tgis_db)
             .execute(
-                "SELECT name FROM sqlite_master WHERE type='table' AND name LIKE '%strds%';"
+                "SELECT name FROM sqlite_master WHERE type='table' AND name LIKE '%strds%';",
             )
             .fetchall()
             != []
@@ -74,3 +87,89 @@ def test_init_succeeds_without_db_creation(simple_mapset):
         assert tgis.get_sql_template_path().endswith("sql")
         assert not var_file.exists()
         assert not tgis_db.exists()
+
+
+@pytest.mark.parametrize("ident", [None, "test@c"])
+def test_stds_construction_without_tgis_db_initialization(
+    simple_mapset,
+    ident: str | None,
+):
+    """STDS objects can be constructed even if the temporal DB is not initialized.
+
+    Empty STDS objects are constructed in several temporal modules, also where a
+    temporal database is not necessarily initialized. This test verifies that
+    STDS objects can be constructed even when tgis is initialized with
+    tgis.init(skip_db_init=True).
+    Methods that require access to the temporal database (e.g., is_in_db())
+    are not tested (they would raise exceptions).
+    """
+    with gs.setup.init(simple_mapset):
+        tgis.init(skip_db_init=True)
+        # None of these must raise if the temporal database is not initialized
+        strds = tgis.SpaceTimeRasterDataset(ident)
+        assert "number_of_semantic_labels" in strds.metadata.D
+        tgis.SpaceTimeRaster3DDataset(ident)
+        tgis.SpaceTimeVectorDataset(ident)
+        # Test dataset_factory for all dataset types, which should be constructed without DB access
+        for ds_type in (
+            "strds",
+            "str3ds",
+            "stvds",
+            "rast",
+            "raster",
+            "raster_3d",
+            "rast3d",
+            "raster3d",
+            "vect",
+            "vector",
+        ):
+            valid_ds_types = ds_types_dict[ds_type]
+            stds = tgis.dataset_factory(ds_type, ident)
+            assert stds.get_id() == ident
+            assert stds.get_type() in valid_ds_types
+
+
+@pytest.mark.parametrize("ident", [None, "test"])
+def test_stds_construction_with_tgis_db_initialization(
+    simple_mapset,
+    ident: str | None,
+):
+    """STDS objects are constructed with access to the temporal DB if initialized.
+
+    This is the counterpart to test_stds_construction_without_tgis_db_initialization:
+    When tgis.init() is called without skip_db_init=True, the temporal database is
+    accessible and STDS objects can access TGIS metadata from the DB (not the dataset).
+    Methods that require access to the temporal database (e.g., is_in_db())
+    are tested to return successfully and valid.
+
+    """
+    with gs.setup.init(simple_mapset):
+        tgis.init()
+        dbif, _connection_state_changes = tgis.init_dbif(None)
+        if ident is not None:
+            ident = f"{ident}@{tgis.get_current_mapset()}"
+        assert tgis.get_tgis_metadata() is not None
+        assert tgis.get_tgis_db_version_from_metadata() == tgis.get_tgis_db_version()
+        tgis.get_current_mapset()
+        strds = tgis.SpaceTimeRasterDataset(ident)
+        assert strds.get_id() == ident
+        assert strds.is_in_db() is False
+        assert "number_of_semantic_labels" in strds.metadata.D
+        # Test dataset_factory for all dataset types, which should be constructed with DB access
+        for ds_type in (
+            "strds",
+            "str3ds",
+            "stvds",
+            "rast",
+            "raster",
+            "raster_3d",
+            "rast3d",
+            "raster3d",
+            "vect",
+            "vector",
+        ):
+            valid_ds_types = ds_types_dict[ds_type]
+            stds = tgis.dataset_factory(ds_type, ident)
+            assert stds.get_id() == ident
+            assert stds.get_type() in valid_ds_types
+            assert stds.is_in_db(dbif) is False
