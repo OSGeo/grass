31c1b2d0ea75c32fc9a41e110b2f1d042e7d157f
Anna Petrasova <kratochanna@gmail.com>
2026-07-17 17:47:52 -0400

    Apply suggestion from @petrasovaa

--- gui/wxpython/core/gconsole.py
+++ gui/wxpython/core/gconsole.py
@@ -198,8 +198,7 @@ class CmdThread(threading.Thread):
                     group="rasterLayer", key="colorTable", subkey="selection"
                 )
                 mapName = None
-                modules_to_exempt = {"r.mapcalc", "r.shade"}
-                if args[0][0] in modules_to_exempt:
+                if args[0][0] == "r.mapcalc":
                     try:
                         mapName = args[0][1].split("=", 1)[0].strip()
                     except (IndexError, AttributeError):
