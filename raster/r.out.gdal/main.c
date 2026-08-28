b0c656a8ca812d807ec27ef89c6ca6bc130cd5d9
ninsbl <stefan.blumentrath@gmx.de>
2026-08-28 16:10:50 +0200

    allow larger number of overviews

--- raster/r.out.gdal/main.c
+++ raster/r.out.gdal/main.c
@@ -238,7 +238,7 @@ int main(int argc, char *argv[])
     overviewopt = G_define_option();
     overviewopt->key = "overviews";
     overviewopt->type = TYPE_INTEGER;
-    overviewopt->options = "0-5";
+    overviewopt->options = "0-15";
     overviewopt->answer = "0";
     overviewopt->label =
         _("Number of overviews to create for the output dataset");
@@ -750,8 +750,8 @@ int main(int argc, char *argv[])
     /* overviews */
     if (overviewopt->answer) {
         n_overviews = atoi(overviewopt->answer);
-        if (n_overviews < 0 || n_overviews > 5) {
-            G_warning(_("Number of overviews must be between 0 and 5"));
+        if (n_overviews < 0 || n_overviews > 15) {
+            G_warning(_("Number of overviews must be between 0 and 15"));
             n_overviews = 0;
         }
     }
